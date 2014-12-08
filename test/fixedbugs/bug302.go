31e26ae43a09028174b6dccc3da444dee83fc9cc
Ian Lance Taylor <iant@golang.org>
2014-12-08 10:28:18 -0800

    test: write Go wrapper code so that bug302 can be run by run.go
    Change-Id: I50d85a65b22c0cfb4d2a078cee45cf7adb23ba77

--- test/fixedbugs/bug302.go
+++ test/fixedbugs/bug302.go
@@ -1,9 +1,47 @@
-// $G $D/bug302.dir/p.go && pack grc pp.a p.$A && $G $D/bug302.dir/main.go
-
-// NOTE: This test is not run by 'run.go' and so not run by all.bash.
-// To run this test you must use the ./run shell script.
+// run
 
 // Copyright 2010 The Go Authors.  All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+var thechar = map[string]string{
+	"arm": "5",
+	"amd64": "6",
+	"amd64p32": "6",
+	"386": "8",
+	"power64": "9",
+	"power64le": "9",
+}
+
+func main() {
+	a := thechar[runtime.GOARCH]
+	if a == "" {
+		fmt.Println("BUG: unknown GOARCH")
+		os.Exit(1)
+	}
+
+	run("go", "tool", a+"g", "fixedbugs/bug302.dir/p.go")
+	run("go", "tool", "pack", "grc", "pp.a", "p."+a)
+	run("go", "tool", a+"g", "fixedbugs/bug302.dir/main.go")
+	os.Remove("p."+a)
+	os.Remove("pp.a")
+	os.Remove("main."+a)
+}
+
+func run(cmd string, args ...string) {
+	out, err := exec.Command(cmd, args...).CombinedOutput()
+	if err != nil {
+		fmt.Println(string(out))
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
