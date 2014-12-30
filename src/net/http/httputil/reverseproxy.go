851cc1d49545d10100adbfa528f98aee06dea3e9
Peter Waller <p@pwaller.net>
2014-12-30 12:19:43 +0000

    net/http/httputil: ReverseProxy request cancellation
    If an inbound connection is closed, cancel the outbound http request.
    
    This is particularly useful if the outbound request may consume resources
    unnecessarily until it is cancelled.
    
    Fixes #8406
    
    Change-Id: I738c4489186ce342f7e21d0ea3f529722c5b443a

--- src/net/http/httputil/reverseproxy.go
+++ src/net/http/httputil/reverseproxy.go
@@ -109,6 +109,21 @@ func (p *ReverseProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	outreq := new(http.Request)
 	*outreq = *req // includes shallow copies of maps, but okay
 
+	if closeNotifier, ok := rw.(http.CloseNotifier); ok {
+
+		if tr, ok := transport.(*http.Transport); ok {
+			reqDone := make(chan struct{})
+			defer close(reqDone)
+			go func() {
+				select {
+				case <-closeNotifier.CloseNotify():
+					tr.CancelRequest(outreq)
+				case <-reqDone:
+				}
+			}()
+		}
+	}
+
 	p.Director(outreq)
 	outreq.Proto = "HTTP/1.1"
 	outreq.ProtoMajor = 1
