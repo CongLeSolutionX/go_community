5f742a380970d5464d3e1714e3e453584819bbb3
Martin Möhrmann <martisch@uos.de>
2015-01-01 13:19:12 +0100

    log: optimize itoa
    Reduce buffer to maximally needed size for conversion of 64bit integers.
    Reduce number of used integer divisions.
    
    benchmark         old ns/op     new ns/op     delta
    BenchmarkItoa     170           145           -14.71%
    
    Change-Id: I6d57a7feebf90f303be5952767107302eccf4631

--- src/log/log_test.go
+++ src/log/log_test.go
@@ -117,3 +117,16 @@ func TestFlagAndPrefixSetting(t *testing.T) {
 		t.Error("message did not match pattern")
 	}
 }
+
+func BenchmarkItoa(b *testing.B) {
+	dst := make([]byte, 0, 20)
+	for i := 0; i < b.N; i++ {
+		itoa(&dst, 2015, 4)  // year
+		itoa(&dst, 1, 2)     // month
+		itoa(&dst, 30, 2)    // day
+		itoa(&dst, 12, 2)    // hour
+		itoa(&dst, 56, 2)    // minute
+		itoa(&dst, 0, 2)     // second
+		itoa(&dst, 98765, 6) // microsecond
+	}
+}
