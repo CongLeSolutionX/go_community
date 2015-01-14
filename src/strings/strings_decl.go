5d204a253c822e6a601f787d5b51f7457322ceee
Alan Donovan <adonovan@google.com>
2015-01-14 18:09:36 -0500

    strings: add Compare(x, y string) int, for symmetry with bytes.Compare
    The implementation is the same assembly (or Go) routine.
    
    Change-Id: Ib937c461c24ad2d5be9b692b4eed40d9eb031412

--- src/strings/strings_decl.go
+++ src/strings/strings_decl.go
@@ -6,3 +6,7 @@ package strings
 
 // IndexByte returns the index of the first instance of c in s, or -1 if c is not present in s.
 func IndexByte(s string, c byte) int // ../runtime/asm_$GOARCH.s
+
+// Compare returns an integer comparing two strings lexicographically.
+// The result will be 0 if a==b, -1 if a < b, and +1 if a > b.
+func Compare(a, b string) int // ../runtime/noasm_arm.goc or ../runtime/asm_{386,amd64}.s
