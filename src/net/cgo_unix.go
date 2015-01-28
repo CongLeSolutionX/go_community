c0629d7ea55be35950f89216485e0a1126aa3383
Mikio Hara <mikioh.mikioh@gmail.com>
2015-01-28 20:08:41 +0900

    net: allow LookupAddr to use getnameinfo when cgo is enabled
    This change allows LookupAddr to use getnameinfo through cgo for working
    together with various name services other than DNS.
    
    Fixes #7855.
    
    Change-Id: I5b3b4aefe3d1b904541c3350865734d8cbb1c1c4

--- src/net/cgo_unix.go
+++ src/net/cgo_unix.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !netgo
+// +build cgo,!netgo
 // +build darwin dragonfly freebsd linux netbsd openbsd
 
 package net
@@ -77,7 +77,7 @@ func cgoLookupPTRName(addr string) ([]string, error, bool) {
 
 	ip := ParseIP(addr)
 	if ip == nil {
-		return nil, &AddrError{Err: "non-IP address", Addr: addr}, false
+		return nil, &AddrError{Err: "non-IP address", Addr: addr}, true
 	}
 	ptr, err := intsocket.GetnameinfoPTR(ip)
 	if err != nil {
