a19b98b35e52a139b11200c2c45545396ec4b24a
Mikio Hara <mikioh.mikioh@gmail.com>
2015-01-28 20:08:41 +0900

    net: allow LookupAddr to use getnameinfo when cgo is enabled
    This change allows LookupAddr to use getnameinfo through cgo for working
    together with various name services other than DNS.
    
    Fixes #7855.
    
    Change-Id: I5b3b4aefe3d1b904541c3350865734d8cbb1c1c4

---
+++ src/net/cgo_linuxsock.go
@@ -0,0 +1,37 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !netgo
+// +build android linux
+
+package net
+
+/*
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+*/
+import "C"
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+func cgoSockaddrInet4(ip IP) *C.struct_sockaddr {
+	sa4 := syscall.RawSockaddrInet4{
+		Family: syscall.AF_INET,
+	}
+	copy(sa4.Addr[:], ip)
+	return (*C.struct_sockaddr)(unsafe.Pointer(&sa4))
+}
+
+func cgoSockaddrInet6(ip IP) *C.struct_sockaddr {
+	sa6 := syscall.RawSockaddrInet6{
+		Family: syscall.AF_INET6,
+	}
+	copy(sa6.Addr[:], ip)
+	return (*C.struct_sockaddr)(unsafe.Pointer(&sa6))
+}
