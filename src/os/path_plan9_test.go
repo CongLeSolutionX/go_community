c0eadd20cbc83f657502c48f045fd13a9a37c6ec
Dave Cheney <dave@cheney.net>
2015-02-01 18:16:56 +1100

    os: fix test on plan9
    Declare a local EROFS symbol to work around the freeze of the syscall package.
    
    Change-Id: I1f1d364745bc9653c995514831ced379de115c38

---
+++ src/os/path_plan9_test.go
@@ -0,0 +1,12 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package os_test
+
+import (
+	"syscall"
+)
+
+// Define EROFS here as the syscall package is now frozen.
+const EROFS = syscall.NewError("read only file system")
