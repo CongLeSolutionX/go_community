30358b15c754b9ee782f295b14024a373e8ad345
Austin Clements <austin@google.com>
2015-03-27 17:01:53 -0400

    runtime: replace STW for enabling write barriers with ragged barrier
    Currently, we use a full stop-the-world around enabling write
    barriers. This is to ensure that all Gs have enabled write barriers
    before any blackening occurs (either in gcBgMarkWorker() or in
    gcAssistAlloc()).
    
    However, there's no need to bring the whole world to a synchronous
    stop to ensure this. This change replaces the STW with a ragged
    barrier that ensures each P has individually observed that write
    barriers should be enabled before GC performs any blackening.
    
    Change-Id: If2f129a6a55bd8bdd4308067af2b739f3fb41955

--- src/runtime/mgcmark.go
+++ src/runtime/mgcmark.go
@@ -171,7 +171,7 @@ func markroot(desc *parfor, i uint32) {
 // allowAssist is true, may assist GC scanning in proportion to the
 // allocations performed by this mutator since the last assist.
 //
-// It should only be called during gcphase == _GCmark.
+// It should only be called if gcAssistAlloc != 0.
 //go:nowritebarrier
 func gcAssistAlloc(size uintptr, allowAssist bool) {
 	// Find the G responsible for this assist.
