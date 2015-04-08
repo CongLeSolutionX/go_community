mwhudson/go

cmd/internal/ld: support for -buildmode=shared
Change-Id: Id4997d611ced29397133f14def6abc88aa9e811e

src/cmd/6l/obj.go
@@ -91,7 +91,7 @@ func archinit() {
 		ld.Linkmode = ld.LinkInternal
 	}
 
-	if ld.Buildmode == obj.Buildmode_CShared {
+	if ld.Buildmode == obj.Buildmode_CShared || ld.DynlinkingGo() {
 		ld.Linkmode = ld.LinkExternal
 	}
 

CongLeSolutionX/go_community

src/cmd/6l/obj.go
@@ -91,7 +91,7 @@ func archinit() {
 		ld.Linkmode = ld.LinkInternal
 	}
 
-	if ld.Buildmode == ld.BuildmodeCShared {
+	if ld.Buildmode == ld.BuildmodeCShared || ld.DynlinkingGo() {
 		ld.Linkmode = ld.LinkExternal
 	}
 