b0bf28d4306b628cfee3bab949d31817c31b4c52
Srdjan Petrovic <spetrovic@google.com>
2015-04-09 11:12:12 -0700

    runtime: signal forwarding
    Forward signals to signal handlers installed before Go installs its own,
    under certain circumstances.  In particular, as iant@ suggests, signals are
    forwarded iff:
       (1) a non-SIG_DFL signal handler existed before Go, and
       (2) signal is synchronous (i.e., one of SIGSEGV, SIGBUS, SIGFPE), and
       	(3a) signal occured on a non-Go thread, or
       	(3b) signal occurred on a Go thread but in CGo code.
    
    Supported only on Linux, for now.
    
    Tested on amd64 and arm.
    
    Change-Id: I403219ee47b26cf65da819fb86cf1ec04d3e25f5

---
+++ misc/cgo/testsigfwd/main.go
@@ -0,0 +1,33 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+// #include <pthread.h>
+// #include <signal.h>
+// #include <stdlib.h>
+// #include <stdio.h>
+//
+// static void sigsegv() {
+// 	pthread_kill(pthread_self(), SIGSEGV);
+// 	fprintf(stderr, "ERROR: NULL deref failed to raise SIGSEGV.\n");
+// 	exit(2);
+// }
+//
+// static void sighandler(int signum) {
+// 	if (signum == SIGSEGV) {
+// 		exit(0);  // success
+// 	}
+// }
+//
+// static void __attribute__ ((constructor)) sigsetup(void) {
+// 	struct sigaction act;
+// 	act.sa_handler = &sighandler;
+// 	sigaction(SIGSEGV, &act, 0);
+// }
+import "C"
+
+func main() {
+	C.sigsegv()
+}
