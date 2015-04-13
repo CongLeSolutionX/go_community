93ab116c285daf4ae20d7be8afcb4c418a6ad4de
Mikio Hara <mikioh.mikioh@gmail.com>
2015-04-13 19:29:45 +0900

    net: make use of net/internal/socket package
    This change replaces existing functions using cgo with exposed functions
    of net/internal/socket package. The functions requiring cgo, getaddrinfo
    and getnameinfo for now, are slightly different between platforms,
    libraries and the net/internal/socket package provides APIs that fill
    the gap for convenience.
    
    Change-Id: Iab2959bf8a81d62eca01da2a237818d9dd354e7f

--- src/net/cgo_unix.go
+++ src/net/cgo_unix.go
@@ -7,158 +7,88 @@
 
 package net
 
-/*
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-*/
-import "C"
-
 import (
+	intsocket "net/internal/socket"
 	"syscall"
-	"unsafe"
 )
 
-func cgoLookupHost(name string) (addrs []string, err error, completed bool) {
-	ip, err, completed := cgoLookupIP(name)
-	for _, p := range ip {
-		addrs = append(addrs, p.String())
+func cgoLookupHost(name string) ([]string, error, bool) {
+	sas, err := intsocket.GetaddrinfoAddr(name)
+	if err != nil {
+		return nil, err, true
 	}
-	return
+	var lits []string
+	for _, sa := range sas {
+		switch sa := sa.(type) {
+		case *syscall.SockaddrInet4:
+			lits = append(lits, IP(sa.Addr[:]).String())
+		case *syscall.SockaddrInet6:
+			lits = append(lits, IP(sa.Addr[:]).String())
+		}
+	}
+	return lits, nil, true
 }
 
-func cgoLookupPort(net, service string) (port int, err error, completed bool) {
+func cgoLookupPort(network, service string) (int, error, bool) {
 	acquireThread()
 	defer releaseThread()
 
-	var res *C.struct_addrinfo
-	var hints C.struct_addrinfo
-
-	switch net {
-	case "":
-		// no hints
-	case "tcp", "tcp4", "tcp6":
-		hints.ai_socktype = C.SOCK_STREAM
-		hints.ai_protocol = C.IPPROTO_TCP
-	case "udp", "udp4", "udp6":
-		hints.ai_socktype = C.SOCK_DGRAM
-		hints.ai_protocol = C.IPPROTO_UDP
-	default:
-		return 0, UnknownNetworkError(net), true
+	port, err := intsocket.GetaddrinfoPort(network, service)
+	if err != nil {
+		return 0, err, true
 	}
-	if len(net) >= 4 {
-		switch net[3] {
-		case '4':
-			hints.ai_family = C.AF_INET
-		case '6':
-			hints.ai_family = C.AF_INET6
-		}
-	}
-
-	s := C.CString(service)
-	defer C.free(unsafe.Pointer(s))
-	if C.getaddrinfo(nil, s, &hints, &res) == 0 {
-		defer C.freeaddrinfo(res)
-		for r := res; r != nil; r = r.ai_next {
-			switch r.ai_family {
-			default:
-				continue
-			case C.AF_INET:
-				sa := (*syscall.RawSockaddrInet4)(unsafe.Pointer(r.ai_addr))
-				p := (*[2]byte)(unsafe.Pointer(&sa.Port))
-				return int(p[0])<<8 | int(p[1]), nil, true
-			case C.AF_INET6:
-				sa := (*syscall.RawSockaddrInet6)(unsafe.Pointer(r.ai_addr))
-				p := (*[2]byte)(unsafe.Pointer(&sa.Port))
-				return int(p[0])<<8 | int(p[1]), nil, true
-			}
-		}
-	}
-	return 0, &AddrError{"unknown port", net + "/" + service}, true
+	return port, nil, true
 }
 
-func cgoLookupIPCNAME(name string) (addrs []IPAddr, cname string, err error, completed bool) {
+func cgoLookupIP(name string) ([]IPAddr, error, bool) {
 	acquireThread()
 	defer releaseThread()
 
-	var res *C.struct_addrinfo
-	var hints C.struct_addrinfo
-
-	hints.ai_flags = cgoAddrInfoFlags()
-	hints.ai_socktype = C.SOCK_STREAM
-
-	h := C.CString(name)
-	defer C.free(unsafe.Pointer(h))
-	gerrno, err := C.getaddrinfo(h, nil, &hints, &res)
-	if gerrno != 0 {
-		var str string
-		if gerrno == C.EAI_NONAME {
-			str = noSuchHost
-		} else if gerrno == C.EAI_SYSTEM {
-			if err == nil {
-				// err should not be nil, but sometimes getaddrinfo returns
-				// gerrno == C.EAI_SYSTEM with err == nil on Linux.
-				// The report claims that it happens when we have too many
-				// open files, so use syscall.EMFILE (too many open files in system).
-				// Most system calls would return ENFILE (too many open files),
-				// so at the least EMFILE should be easy to recognize if this
-				// comes up again. golang.org/issue/6232.
-				err = syscall.EMFILE
-			}
-			str = err.Error()
-		} else {
-			str = C.GoString(C.gai_strerror(gerrno))
-		}
-		return nil, "", &DNSError{Err: str, Name: name}, true
+	sas, err := intsocket.GetaddrinfoAddr(name)
+	if err != nil {
+		return nil, err, true
 	}
-	defer C.freeaddrinfo(res)
-	if res != nil {
-		cname = C.GoString(res.ai_canonname)
-		if cname == "" {
-			cname = name
-		}
-		if len(cname) > 0 && cname[len(cname)-1] != '.' {
-			cname += "."
+	var addrs []IPAddr
+	for _, sa := range sas {
+		switch sa := sa.(type) {
+		case *syscall.SockaddrInet4:
+			addrs = append(addrs, IPAddr{IP: copyIP(sa.Addr[:])})
+		case *syscall.SockaddrInet6:
+			addrs = append(addrs, IPAddr{IP: copyIP(sa.Addr[:]), Zone: zoneToString(int(sa.ZoneId))})
 		}
 	}
-	for r := res; r != nil; r = r.ai_next {
-		// We only asked for SOCK_STREAM, but check anyhow.
-		if r.ai_socktype != C.SOCK_STREAM {
-			continue
-		}
-		switch r.ai_family {
-		default:
-			continue
-		case C.AF_INET:
-			sa := (*syscall.RawSockaddrInet4)(unsafe.Pointer(r.ai_addr))
-			addr := IPAddr{IP: copyIP(sa.Addr[:])}
-			addrs = append(addrs, addr)
-		case C.AF_INET6:
-			sa := (*syscall.RawSockaddrInet6)(unsafe.Pointer(r.ai_addr))
-			addr := IPAddr{IP: copyIP(sa.Addr[:]), Zone: zoneToString(int(sa.Scope_id))}
-			addrs = append(addrs, addr)
-		}
+	return addrs, nil, true
+}
+
+func cgoLookupCNAME(name string) (string, error, bool) {
+	acquireThread()
+	defer releaseThread()
+
+	cname, err := intsocket.GetaddrinfoCNAME(name)
+	if err != nil {
+		return "", err, true
 	}
-	return addrs, cname, nil, true
+	return cname, nil, true
 }
 
-func cgoLookupIP(name string) (addrs []IPAddr, err error, completed bool) {
-	addrs, _, err, completed = cgoLookupIPCNAME(name)
-	return
+func cgoLookupPTRName(addr string) ([]string, error, bool) {
+	acquireThread()
+	defer releaseThread()
+
+	ip := ParseIP(addr)
+	if ip == nil {
+		return nil, &AddrError{Err: "non-IP address", Addr: addr}, false
+	}
+	ptr, err := intsocket.GetnameinfoPTR(ip)
+	if err != nil {
+		return nil, err, true
+	}
+	return []string{ptr}, nil, true
 }
 
-func cgoLookupCNAME(name string) (cname string, err error, completed bool) {
-	_, cname, err, completed = cgoLookupIPCNAME(name)
-	return
-}
-
-func copyIP(x IP) IP {
+func copyIP(x []byte) IP {
 	if len(x) < 16 {
-		return x.To16()
+		return IP(x).To16()
 	}
 	y := make(IP, len(x))
 	copy(y, x)
