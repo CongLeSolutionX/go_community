73e2f019e57ed6b50097d117633763725af409da
Rob Pike <r@golang.org>
2015-04-24 12:28:18 -0700

    cmd/go,cmd/doc: add "go doc"
    Add the new go doc command to the go command, installed in
    the tool directory.
    
    (Still to do: tests)
    
    Fix cmd/dist to remove old "package documentation" code that was
    stopping it from including cmd/go/doc.go in the build.
    
    Implement the doc command. Here is the help info from "go help doc":
    
    ===
    usage: go doc [-u] [package|[package.]symbol[.method]]
    
    Doc accepts at most one argument, indicating either a package, a symbol within a
    package, or a method of a symbol.
    
    	go doc
    	go doc <pkg>
    	go doc <sym>[.<method>]
    	go doc [<pkg>].<sym>[.<method>]
    
    Doc interprets the argument to see what it represents, determined by its syntax
    and which packages and symbols are present in the source directories of GOROOT and
    GOPATH.
    
    The first item in this list that succeeds is the one whose documentation is printed.
    For packages, the order of scanning is determined by the file system, however the
    GOROOT tree is always scanned before GOPATH.
    
    If there is no package specified or matched, the package in the current directory
    is selected, so "go doc" shows the documentation for the current package and
    "go doc Foo" shows the documentation for symbol Foo in the current package.
    
    Doc prints the documentation comments associated with the top-level item the
    argument identifies (package, type, method) followed by a one-line summary of each
    of the first-level items "under" that item (package-level declarations for a
    package, methods for a type, etc.)
    
    The package paths must be either a qualified path or a proper suffix of a path
    (see examples below). The go tool's usual package mechanism does not apply: package
    path elements like . and ...  are not implemented by go doc.
    
    When matching symbols, lower-case letters match either case but upper-case letters
    match exactly.
    
    Examples:
    	go doc
    		Show documentation for current package.
    	go doc Foo
    		Show documentation for Foo in the current package.
    		(Foo starts with a capital letter so it cannot match a package path.)
    	go doc json
    		Show documentation for the encoding/json package.
    	go doc json
    		Shorthand for encoding/json assuming only one json package
    		is present in the tree.
    	go doc json.Number (or go doc json.number)
    		Show documentation and method summary for json.Number.
    	go doc json.Number.Int64 (or go doc json.number.int64)
    		Show documentation for the Int64 method of json.Number.
    
    Flags:
    	-u
    		Show documentation for unexported as well as exported
    		symbols and methods.
    
    ===
    
    Still to do:
    
    0) Tests.
    1) When there are lists of things, sort them alphabetically.
    2) Decide what order to present items when presenting a full package.
    
    Change-Id: I83d409a68688a5445f54297a7e7c745f749b9e66

---
+++ src/cmd/doc/pkg.go
@@ -0,0 +1,459 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"go/ast"
+	"go/build"
+	"go/doc"
+	"go/format"
+	"go/parser"
+	"go/token"
+	"log"
+	"os"
+	"sort"
+	"unicode"
+	"unicode/utf8"
+)
+
+type Package struct {
+	name string         // Package name, json for encoding/json.
+	pkg  *ast.Package   // Parsed package.
+	file *ast.File      // Merged from all files in the package
+	fs   *token.FileSet // Needed for printing.
+}
+
+// parsePackage turns the build package we found into a parsed package
+// we can then use to generate documentation.
+func parsePackage(pkg *build.Package) *Package {
+	fs := token.NewFileSet()
+	// include tells parser.ParseDir which files to include.
+	// That means the file must be in the build package's GoFiles or CgoFiles
+	// list only (no tag-ignored files, tests, swig or other non-Go files).
+	include := func(info os.FileInfo) bool {
+		for _, name := range pkg.GoFiles {
+			if name == info.Name() {
+				return true
+			}
+		}
+		for _, name := range pkg.CgoFiles {
+			if name == info.Name() {
+				return true
+			}
+		}
+		return false
+	}
+	pkgs, err := parser.ParseDir(fs, pkg.Dir, include, parser.ParseComments)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Make sure they are all in one package.
+	pkgName := ""
+	for name := range pkgs {
+		if pkgName == "" {
+			pkgName = name
+		}
+		if name != pkgName {
+			log.Fatalf("multiple packages %s and %s in directory %s", name, pkgName, pkg.Dir)
+		}
+	}
+	if pkgName == "" {
+		log.Fatalf("no Go package found in directory %s", pkg.Dir)
+	}
+	if len(pkgs) != 1 {
+		log.Fatalf("multiple packages directory %s", pkg.Dir)
+	}
+
+	return &Package{
+		name: pkgName,
+		pkg:  pkgs[pkgName],
+		file: ast.MergePackageFiles(pkgs[pkgName], 0),
+		fs:   fs,
+	}
+}
+
+// emit prints the node.
+func (pkg *Package) emit(node ast.Node) {
+	var b bytes.Buffer
+	if node != nil {
+		err := format.Node(&b, pkg.fs, node)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if b.Len() > 0 && b.Bytes()[b.Len()-1] != '\n' {
+			b.WriteRune('\n')
+		}
+		os.Stdout.Write(b.Bytes())
+	}
+}
+
+var formatBuf bytes.Buffer // One instance to minimize allocation.
+
+// formatNode is a helper function for printing.
+func (pkg *Package) formatNode(node ast.Node) []byte {
+	formatBuf.Reset()
+	format.Node(&formatBuf, pkg.fs, node)
+	return formatBuf.Bytes()
+}
+
+// oneLineFunc prints a function declaration as a single line.
+func (pkg *Package) oneLineFunc(decl *ast.FuncDecl) {
+	decl.Doc = nil
+	decl.Body = nil
+	pkg.emit(decl)
+}
+
+// oneLineValueGenDecl prints a var or const declaration as a single line.
+func (pkg *Package) oneLineValueGenDecl(decl *ast.GenDecl, symbol string) {
+	decl.Doc = nil
+	dotDotDot := ""
+	if len(decl.Specs) > 1 {
+		dotDotDot = " ..."
+	}
+	// Find the first relevant spec.
+	for i, spec := range decl.Specs {
+		valueSpec := spec.(*ast.ValueSpec) // Must succeed; we can't mix types in one genDecl.
+		if symbol != "" {
+			if name, ok := valueSpec.Type.(*ast.Ident); !ok || !match(symbol, name.Name) {
+				continue
+			}
+		}
+		typ := ""
+		if valueSpec.Type != nil {
+			typ = fmt.Sprintf(" %s", pkg.formatNode(valueSpec.Type))
+		}
+		val := ""
+		if i < len(valueSpec.Values) && valueSpec.Values[i] != nil {
+			val = fmt.Sprintf(" = %s", pkg.formatNode(valueSpec.Values[i]))
+		}
+		fmt.Printf("%s %s%s%s%s\n", decl.Tok, valueSpec.Names[0], typ, val, dotDotDot)
+		break
+	}
+}
+
+// oneLineTypeDecl prints a type declaration as a single line.
+func (pkg *Package) oneLineTypeDecl(spec *ast.TypeSpec) {
+	spec.Doc = nil
+	spec.Comment = nil
+	switch spec.Type.(type) {
+	case *ast.InterfaceType:
+		fmt.Printf("type %s interface { ... }\n", spec.Name)
+	case *ast.StructType:
+		fmt.Printf("type %s struct { ... }\n", spec.Name)
+	default:
+		fmt.Printf("type %s %s\n", spec.Name, pkg.formatNode(spec.Type))
+	}
+}
+
+// packageDoc prints the docs for the package (package doc plus one-liners of the rest).
+// TODO: Sort the output.
+func (pkg *Package) packageDoc() {
+Loop:
+	for _, f := range pkg.pkg.Files {
+		if f.Doc != nil {
+			fmt.Printf("package %s\n\n", pkg.name)
+			doc.ToText(os.Stdout, f.Doc.Text(), "", "\t", 80)
+			fmt.Print("\n")
+			break Loop // First one counts.
+		}
+	}
+	for _, d := range pkg.file.Decls {
+		switch decl := d.(type) {
+		case *ast.GenDecl:
+			if spec, ok := decl.Specs[0].(*ast.ValueSpec); ok {
+				exported := true
+				for _, name := range spec.Names {
+					if !isExported(name.Name) {
+						exported = false
+						break
+					}
+				}
+				if exported {
+					pkg.oneLineValueGenDecl(decl, "")
+				}
+				continue
+			}
+			for _, spec := range decl.Specs {
+				switch spec := spec.(type) {
+				case *ast.ImportSpec:
+					continue // Nothing to do.
+				case *ast.TypeSpec:
+					if isExported(spec.Name.Name) {
+						pkg.oneLineTypeDecl(spec)
+					}
+				default:
+					log.Fatalf("unrecognized type spec %T", spec)
+				}
+			}
+		case *ast.FuncDecl:
+			// Exported functions only, not methods.
+			if isExported(decl.Name.Name) && decl.Recv == nil {
+				pkg.oneLineFunc(decl)
+			}
+		default:
+			log.Fatalf("can't handle node of type %T", decl)
+		}
+	}
+}
+
+// findDecl returns the declaration that holds the declaration for
+// the symbol, and returns it. It will be either be a function
+// declaration or a "general" declaration. If the latter, findDecl
+// also returns the "spec" that declares the actual symbol.
+func (pkg *Package) findDecl(symbol string) (*ast.FuncDecl, *ast.GenDecl, ast.Node) {
+	for _, d := range pkg.file.Decls {
+		switch decl := d.(type) {
+		case *ast.GenDecl:
+			for _, spec := range decl.Specs {
+				switch spec := spec.(type) {
+				case *ast.ImportSpec:
+					// Nothing to do.
+				case *ast.ValueSpec:
+					for _, name := range spec.Names {
+						if match(symbol, name.Name) {
+							return nil, decl, spec
+						}
+					}
+				case *ast.TypeSpec:
+					if match(symbol, spec.Name.Name) {
+						return nil, decl, spec
+					}
+				default:
+					log.Fatalf("unrecognized type spec %T", spec)
+				}
+			}
+		case *ast.FuncDecl:
+			if match(symbol, decl.Name.Name) {
+				return decl, nil, nil
+			}
+		default:
+			log.Fatalf("can't handle node of type %T", decl)
+		}
+	}
+	return nil, nil, nil
+}
+
+// findAllMethods returns the list of (exported) methods for the type symbol or *symbol.
+func (pkg *Package) findAllMethods(symbol string) (methods []*ast.FuncDecl) {
+	for _, d := range pkg.file.Decls {
+		switch fun := d.(type) {
+		case *ast.FuncDecl:
+			if pkg.matchMethod(fun, symbol, "") {
+				methods = append(methods, fun)
+			}
+		}
+	}
+	return methods
+}
+
+// findAllConstants returns the list of declarations of (exported) constants for the type symbol or *symbol.
+// The return value is a slice of genDecls whose specs are ValueSpecs.
+func (pkg *Package) findAllConstants(symbol string) (constants []*ast.GenDecl) {
+OuterLoop:
+	for _, d := range pkg.file.Decls {
+		switch genDecl := d.(type) {
+		case *ast.GenDecl:
+			// Is this a list of constants?
+			if genDecl.Tok != token.CONST {
+				continue
+			}
+			// Is the type of one of these the identifier "symbol"?
+			for _, spec := range genDecl.Specs {
+				valueSpec, ok := spec.(*ast.ValueSpec)
+				if !ok {
+					continue
+				}
+				if name, ok := valueSpec.Type.(*ast.Ident); ok && match(symbol, name.Name) {
+					constants = append(constants, genDecl)
+					continue OuterLoop // We're good; go to next declaration.
+				}
+			}
+		}
+	}
+	return constants
+}
+
+// symbolDoc prints the doc for symbol. If it is a type, this includes is methods,
+// factories (TODO) and associated constants.
+func (pkg *Package) symbolDoc(symbol string) {
+	funcDecl, genDecl, node := pkg.findDecl(symbol)
+	if funcDecl != nil {
+		// Symbol is a function.
+		funcDecl.Body = nil
+		pkg.emit(funcDecl)
+		return
+	}
+	if node == nil {
+		log.Fatalf("symbol %s not present in package %q", symbol, pkg.name)
+	}
+	switch node := node.(type) {
+	case *ast.TypeSpec:
+		trimUnexportedFields(node)
+		// If there are multiple types defined, reduce to just this one.
+		if len(genDecl.Specs) > 1 {
+			genDecl.Specs = []ast.Spec{node}
+		}
+		pkg.emit(genDecl)
+		// NO TOO MANY pkg.emit(node)
+	case *ast.ValueSpec:
+		pkg.emit(genDecl)
+	default:
+		log.Fatalf("can't doc node of type %T", node)
+	}
+	// Show associated methods.
+	methods := pkg.findAllMethods(symbol)
+	sort.Sort(methodList(methods))
+	for _, m := range methods {
+		m.Doc = nil
+		m.Body = nil
+		pkg.emit(m)
+	}
+	// Show associated constants. (Actually just the first one in any const block.)
+	constants := pkg.findAllConstants(symbol)
+	for _, genDecl := range constants {
+		pkg.oneLineValueGenDecl(genDecl, symbol)
+	}
+}
+
+// trimUnexportedFields modifies spec in place to elide unexported fields (unless
+// the unexported flag is set). If spec is not a structure declartion, nothing happens.
+func trimUnexportedFields(spec *ast.TypeSpec) {
+	if unexported {
+		// We're printing all fields.
+		return
+	}
+	// It must be a struct for us to care. (We show unexported methods in interfaces.)
+	structType, ok := spec.Type.(*ast.StructType)
+	if !ok {
+		return
+	}
+	trimmed := false
+	list := make([]*ast.Field, 0, len(structType.Fields.List))
+	for _, field := range structType.Fields.List {
+		// Trims if any is unexported. Fine in practice.
+		ok := true
+		for _, name := range field.Names {
+			if !isExported(name.Name) {
+				trimmed = true
+				ok = false
+				break
+			}
+		}
+		if ok {
+			list = append(list, field)
+		}
+	}
+	if trimmed {
+		unexportedField := &ast.Field{
+			Type: ast.NewIdent(""), // Hack: printer will treat this as a field with a named type.
+			Comment: &ast.CommentGroup{
+				List: []*ast.Comment{
+					&ast.Comment{
+						Text: "// Has unexported fields.\n",
+					},
+				},
+			},
+		}
+		list = append(list, unexportedField)
+		structType.Fields.List = list
+	}
+}
+
+// findMethod returns the declaration for symbol.method.
+func (pkg *Package) findMethod(symbol, method string) *ast.FuncDecl {
+	for _, d := range pkg.file.Decls {
+		switch fun := d.(type) {
+		case *ast.FuncDecl:
+			if pkg.matchMethod(fun, symbol, method) {
+				return fun
+			}
+		}
+	}
+	log.Fatalf("no such method %s.%s", symbol, method)
+	return nil
+}
+
+// matchMethod reports whether the function declaration is one for the specified
+// symbol and method name; the method name can be empty, meaning always match.
+func (pkg *Package) matchMethod(fun *ast.FuncDecl, symbol, method string) bool {
+	// Is it a method?
+	recv := fun.Recv
+	if recv == nil || len(recv.List) != 1 {
+		return false // Not a method (or strange somehow).
+	}
+	// Does the method name match?
+	if method != "" && !match(method, fun.Name.Name) {
+		return false
+	}
+	if !isExported(fun.Name.Name) {
+		return false
+	}
+	recvField := recv.List[0]
+	// Does the type match? Must be T or *T.
+	var name string
+	switch typ := recvField.Type.(type) {
+	case *ast.Ident:
+		name = typ.Name
+	case *ast.StarExpr:
+		name = typ.X.(*ast.Ident).Name
+	default:
+		log.Fatalf("unexpected receiver type %s", typ)
+	}
+	if !isExported(name) {
+		return false
+	}
+	if !match(symbol, name) {
+		return false
+	}
+	return true
+}
+
+// methodDoc prints the doc for symbol.method.
+func (pkg *Package) methodDoc(symbol, method string) {
+	fnDecl, _, node := pkg.findDecl(symbol)
+	if fnDecl != nil {
+		log.Fatalf("%s is a function; it has no methods", symbol)
+	}
+	if node == nil {
+		log.Fatalf("symbol %s not present in package %q", symbol, pkg.name)
+	}
+	var ok bool
+	if _, ok = node.(*ast.TypeSpec); !ok {
+		log.Fatalf("%s is not a type, cannot have method", symbol)
+	}
+	funcDecl := pkg.findMethod(symbol, method)
+	funcDecl.Body = nil
+	pkg.emit(funcDecl)
+}
+
+// match reports whether the user's symbol matches the program's.
+// A lower-case character in the user's string matches either case in the program's.
+// The program string must be exported.
+func match(user, program string) bool {
+	if !isExported(program) {
+		return false
+	}
+	for _, u := range user {
+		p, w := utf8.DecodeRuneInString(program)
+		program = program[w:]
+		if u == p {
+			continue
+		}
+		if unicode.IsLower(u) && unicode.ToUpper(u) == p {
+			continue
+		}
+		return false
+	}
+	return program == ""
+}
+
+// methodList lets us sort list of methods by name alphabetically.
+type methodList []*ast.FuncDecl
+
+func (m methodList) Len() int           { return len(m) }
+func (m methodList) Less(i, j int) bool { return m[i].Name.Name < m[j].Name.Name }
+func (m methodList) Swap(i, j int)      { m[i], m[j] = m[j], m[i] }
