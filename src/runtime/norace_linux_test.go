0955caed740c6725944e51bf6f9af3b6edf3a4b4
Srdjan Petrovic <spetrovic@google.com>
2015-04-28 16:53:19 -0700

    runtime: fix TestNewOSProc0 and re-enable it for android
    Change-Id: I176aae0cec051d6c0c848e74bf89b41aba8d7238

--- src/runtime/norace_linux_test.go
+++ src/runtime/norace_linux_test.go
@@ -19,16 +19,16 @@ var newOSProcDone bool
 //go:nosplit
 func newOSProcCreated() {
 	newOSProcDone = true
+	// Spin forever: functions passed to newosproc0 are supposed never to
+	// return.  (Note that we can't use a channel to block here as we don't have
+	// a valid G.)
+	for true {
+	}
 }
 
 // Can't be run with -race because it inserts calls into newOSProcCreated()
 // that require a valid G/M.
 func TestNewOSProc0(t *testing.T) {
-	if runtime.GOOS == "android" && runtime.GOARCH == "arm" {
-		// newosproc0 does not work for android/arm.
-		// See issue 10548.
-		t.Skipf("skipping on %v", runtime.GOOS)
-	}
 	runtime.NewOSProc0(0x800000, unsafe.Pointer(runtime.FuncPC(newOSProcCreated)))
 	check := time.NewTicker(1 * time.Second)
 	defer check.Stop()
