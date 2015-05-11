6250347e9f4b5198551267a39505baae8f8e8a2e
Michael Hudson-Doyle <michael.hudson@canonical.com>
2015-05-11 13:39:28 +1200

    cmd/go: support -buildmode=shared with gccgo
    Still some ugly corners.
    
    Change-Id: Id93b8ab42fa311ce32209734ec9a0813f8736e25

--- src/cmd/go/note.go
+++ src/cmd/go/note.go
@@ -10,10 +10,13 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"debug/elf"
 	"encoding/binary"
 	"fmt"
 	"io"
+	"strings"
 )
 
 func rnd(v int32, r int32) int32 {
@@ -40,6 +43,10 @@ func readwithpad(r io.Reader, sz int32) ([]byte, error) {
 	return data, nil
 }
 
+// readnote returns the ELF note with the given section and type from filename. The
+// layout of note sections is explained on page 2-4 of "Tool Interface Standard (TIS)
+// Executable and Linking Format (ELF) Specification Version 1.2" available at
+// http://refspecs.linuxbase.org/elf/elf.pdf.
 func readnote(filename, name string, typ int32) ([]byte, error) {
 	f, err := elf.Open(filename)
 	if err != nil {
@@ -81,4 +88,37 @@ func readnote(filename, name string, typ int32) ([]byte, error) {
 		}
 	}
 	return nil, nil
+}
+
+// readpkglist returns the list of packages that were built into the shared library
+// at shlibpath. For the native toolchain this list is stored, newline separated, in
+// an ELF note with name "GO\x00\x00" and type 1. For GCCGO it is extracted from the
+// .go_export section.
+func readpkglist(shlibpath string) (pkgs []*Package) {
+	var stk importStack
+	if _, gccgo := buildToolchain.(gccgoToolchain); gccgo {
+		f, _ := elf.Open(shlibpath)
+		sect := f.Section(".go_export")
+		data, _ := sect.Data()
+		scanner := bufio.NewScanner(bytes.NewBuffer(data))
+		for scanner.Scan() {
+			t := scanner.Text()
+			if strings.HasPrefix(t, "pkgpath ") {
+				t = strings.TrimPrefix(t, "pkgpath ")
+				t = strings.TrimSuffix(t, ";")
+				pkgs = append(pkgs, loadPackage(t, &stk))
+			}
+		}
+	} else {
+		pkglistbytes, err := readnote(shlibpath, "GO\x00\x00", 1)
+		if err != nil {
+			fatalf("readnote failed: %v", err)
+		}
+		scanner := bufio.NewScanner(bytes.NewBuffer(pkglistbytes))
+		for scanner.Scan() {
+			t := scanner.Text()
+			pkgs = append(pkgs, loadPackage(t, &stk))
+		}
+	}
+	return
 }
