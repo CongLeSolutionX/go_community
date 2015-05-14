6974a9e148c75c43446376061ad52e3763b5a88e
Didier Spezia <didier.06@gmail.com>
2015-05-14 16:44:58 +0000

    text/template: fix race condition on function maps
    The Template objects are supposed to be goroutine-safe once they
    have been parsed. This includes the text and html ones.
    
    For html/template, the escape mechanism is triggered at execution
    time. It may alter the internal structures of the template, so
    a mutex protects them against concurrent accesses.
    
    The text/template package is free of any synchronization primitive.
    
    A race condition may occur when nested templates are escaped:
    the escape algorithm alters the function maps of the associated
    text templates, while a concurrent template execution may access
    the function maps in read mode.
    
    The less invasive fix I have found is to introduce a RWMutex in
    text/template to protect the function maps. This is unfortunate
    but it should be effective.
    
    Fixes #9945
    
    Change-Id: I1edb73c0ed0f1fcddd2f1516230b548b92ab1269

--- src/text/template/funcs.go
+++ src/text/template/funcs.go
@@ -92,9 +92,12 @@ func goodFunc(typ reflect.Type) bool {
 // findFunction looks for a function in the template, and global map.
 func findFunction(name string, tmpl *Template) (reflect.Value, bool) {
 	if tmpl != nil && tmpl.common != nil {
+		tmpl.muFuncs.RLock()
 		if fn := tmpl.execFuncs[name]; fn.IsValid() {
+			tmpl.muFuncs.RUnlock()
 			return fn, true
 		}
+		tmpl.muFuncs.RUnlock()
 	}
 	if fn := builtinFuncs[name]; fn.IsValid() {
 		return fn, true
