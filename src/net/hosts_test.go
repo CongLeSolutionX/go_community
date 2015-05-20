99a16aa29448ad15e6d7c759e36d9702c2d2e39c
Mikio Hara <mikioh.mikioh@gmail.com>
2015-05-20 23:20:43 +0900

    net: fix handling for literal IPv6 address in local database
    The commit f00362b9ecf9fbca6f099493a4e8d1c6a030dee3 fixed the code path
    using getaddrinfo via cgo. This change fixes the remaining non-cgo code
    path.
    
    Fixes #8243.
    Fixes #8996.
    
    Change-Id: I48443611cbabed0d69667cc73911ba3de396fd44

--- src/net/hosts_test.go
+++ src/net/hosts_test.go
@@ -5,63 +5,95 @@
 package net
 
 import (
+	"reflect"
 	"sort"
 	"testing"
 )
 
-type hostTest struct {
-	host string
-	ips  []IP
-}
-
-var hosttests = []hostTest{
-	{"odin", []IP{
-		IPv4(127, 0, 0, 2),
-		IPv4(127, 0, 0, 3),
-		ParseIP("::2"),
-	}},
-	{"thor", []IP{
-		IPv4(127, 1, 1, 1),
-	}},
-	{"loki", []IP{}},
-	{"ullr", []IP{
-		IPv4(127, 1, 1, 2),
-	}},
-	{"ullrhost", []IP{
-		IPv4(127, 1, 1, 2),
-	}},
+var lookupStaticHostTests = []struct {
+	host  string
+	addrs []string
+}{
+	{
+		"broadcasthost",
+		[]string{"255.255.255.255"},
+	},
+	{
+		"odin",
+		[]string{"127.0.0.2", "127.0.0.3", "::2"},
+	},
+	{
+		"thor",
+		[]string{"127.1.1.1"},
+	},
+	{
+		"loki",
+		nil,
+	},
+	{
+		"ullr",
+		[]string{"127.1.1.2"},
+	},
+	{
+		"ullr.localdomain",
+		[]string{"127.1.1.2"},
+	},
+	{
+		"localhost",
+		[]string{"fe80::1"},
+	},
+	{
+		"ipv6-localhost",
+		[]string{"fe80::1%lo0"},
+	},
 }
 
 func TestLookupStaticHost(t *testing.T) {
-	p := hostsPath
-	hostsPath = "testdata/hosts"
-	for i := 0; i < len(hosttests); i++ {
-		tt := hosttests[i]
-		ips := lookupStaticHost(tt.host)
-		if len(ips) != len(tt.ips) {
-			t.Errorf("# of hosts = %v; want %v", len(ips), len(tt.ips))
-			continue
-		}
-		for k, v := range ips {
-			if tt.ips[k].String() != v {
-				t.Errorf("lookupStaticHost(%q) = %v; want %v", tt.host, v, tt.ips[k])
-			}
+	p := testHookHostsPath
+	testHookHostsPath = "testdata/hosts"
+	defer func() { testHookHostsPath = p }()
+
+	for i, tt := range lookupStaticHostTests {
+		addrs := lookupStaticHost(tt.host)
+		if !reflect.DeepEqual(addrs, tt.addrs) {
+			t.Errorf("#%d: got %v; want %v", i, addrs, tt.addrs)
 		}
 	}
-	hostsPath = p
+
+	testHookHostsPath = "testdata/hosts_singleline" // see golang.org/issue/6646
+	ttaddrs := []string{"127.0.0.2"}
+
+	addrs := lookupStaticHost("odin")
+	if !reflect.DeepEqual(addrs, ttaddrs) {
+		t.Errorf("got %v; want %v", addrs, ttaddrs)
+	}
 }
 
-// https://golang.org/issue/6646
-func TestSingleLineHostsFile(t *testing.T) {
-	p := hostsPath
-	hostsPath = "testdata/hosts_singleline"
+var lookupStaticAddrTests = []struct {
+	addr  string
+	hosts []string
+}{
+	{
+		"fe80::1",
+		[]string{"localhost"},
+	},
+	{
+		"fe80:0:0:0:0:0:0:1",
+		[]string{"localhost"},
+	},
+}
 
-	ips := lookupStaticHost("odin")
-	if len(ips) != 1 || ips[0] != "127.0.0.2" {
-		t.Errorf("lookupStaticHost = %v, want %v", ips, []string{"127.0.0.2"})
+func TestLookupStaticAddr(t *testing.T) {
+	p := testHookHostsPath
+	testHookHostsPath = "testdata/hosts"
+	defer func() { testHookHostsPath = p }()
+
+	for i, tt := range lookupStaticAddrTests {
+		hosts := lookupStaticAddr(tt.addr)
+		if !reflect.DeepEqual(hosts, tt.hosts) {
+			t.Errorf("#%d: got %v; want %v", i, hosts, tt.hosts)
+		}
 	}
-
-	hostsPath = p
 }
 
 func TestLookupHost(t *testing.T) {
