575c76ffac45ec9742088bdae67c47c4ddfb3741
Dave Cheney <dave@cheney.net>
2015-06-08 18:09:06 +1000

    test/fixedbugs: add test case for issue8101
    Update #8101
    
    Change-Id: Ic816873ab7c2d3aab8ef197216c8195cf4fe2974

---
+++ test/fixedbugs/issue8101.go
@@ -0,0 +1,14 @@
+// compile
+
+// Copyright 2015 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Issue 8101: cmd/5g: ICE regfree: reg R0 not allocated
+
+package p
+var c0 []chan int
+var s0 []int
+var s1 []**int
+var i = 0
+var boom = cap(c0[s0[len(c0[**(s1)[s0[s0[**s1[s0[s0[s0[i]]+1]-1]]]]])]+1])
