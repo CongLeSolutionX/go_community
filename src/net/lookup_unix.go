d87954f08f82750d00d712bd07938fb2b1a0172a
Mikio Hara <mikioh.mikioh@gmail.com>
2015-06-09 17:30:00 +0900

    net: don't return DNS query results including the second best records unconditionally
    This change prevents DNS query results using domain search list
    overtaking results not using the list unconditionally, which only
    happens when using builtin DNS stub resolver.
    
    The previous internal lookup funtions are splitted into lookupDNSAbs,
    lookupDNSSuff and lookupDNSUnsuff for iteration over a set of host
    names: FQDN or absolute FQDN, with domain label suffixes in search list,
    without domain label suffixes.
    
    Also simplifies, deflakes existing test cases for builtin DNS stub
    resolver.
    
    Fixes #11081.
    
    Change-Id: I9ff0640f69276e372d97e709b149ed5b153e8601

--- src/net/lookup_unix.go
+++ src/net/lookup_unix.go
@@ -96,7 +96,7 @@ func lookupSRV(service, proto, name string) (string, []*SRV, error) {
 	} else {
 		target = "_" + service + "._" + proto + "." + name
 	}
-	cname, rrs, err := lookup(target, dnsTypeSRV)
+	cname, rrs, err := lookupDNS(target, dnsTypeSRV)
 	if err != nil {
 		return "", nil, err
 	}
@@ -110,7 +110,7 @@ func lookupSRV(service, proto, name string) (string, []*SRV, error) {
 }
 
 func lookupMX(name string) ([]*MX, error) {
-	_, rrs, err := lookup(name, dnsTypeMX)
+	_, rrs, err := lookupDNS(name, dnsTypeMX)
 	if err != nil {
 		return nil, err
 	}
@@ -124,7 +124,7 @@ func lookupMX(name string) ([]*MX, error) {
 }
 
 func lookupNS(name string) ([]*NS, error) {
-	_, rrs, err := lookup(name, dnsTypeNS)
+	_, rrs, err := lookupDNS(name, dnsTypeNS)
 	if err != nil {
 		return nil, err
 	}
@@ -136,7 +136,7 @@ func lookupNS(name string) ([]*NS, error) {
 }
 
 func lookupTXT(name string) ([]string, error) {
-	_, rrs, err := lookup(name, dnsTypeTXT)
+	_, rrs, err := lookupDNS(name, dnsTypeTXT)
 	if err != nil {
 		return nil, err
 	}
@@ -156,7 +156,7 @@ func lookupAddr(addr string) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
-	_, rrs, err := lookup(arpa, dnsTypePTR)
+	_, rrs, err := lookupDNS(arpa, dnsTypePTR)
 	if err != nil {
 		return nil, err
 	}
