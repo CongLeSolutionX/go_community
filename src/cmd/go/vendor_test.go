e34d957628d323c0f662f080fdec3b7629f86679
Russ Cox <rsc@golang.org>
2015-06-09 12:09:12 -0700

    cmd/go: add preliminary support for vendor directories
    When GO15VENDOREXPERIMENT=1 is in the environment,
    this CL changes the resolution of import paths according to
    the Go 1.5 vendor proposal:
    
    	If there is a source directory d/vendor, then,
    	when compiling a source file within the subtree rooted at d,
    	import "p" is interpreted as import "d/vendor/p" if that exists.
    
    	When there are multiple possible resolutions,
    	the most specific (longest) path wins.
    
    	The short form must always be used: no import path can
    	contain “/vendor/” explicitly.
    
    	Import comments are ignored in vendored packages.
    
    The goal of these changes is to allow authors to vendor (copy) external
    packages into their source trees without any modifications to the code.
    This functionality has been achieved in tools like godep, nut, and gb by
    requiring GOPATH manipulation. This alternate directory-based approach
    eliminates the need for GOPATH manipulation and in keeping with the
    go command's use of directory layout-based configuration.
    
    The flag allows experimentation with these vendoring semantics once
    Go 1.5 is released, without forcing them on by default. If the experiment
    is deemed a success, the flag will default to true in Go 1.6 and then be
    removed in Go 1.7.
    
    For more details, see the original proposal by Keith Rarick at
    https://groups.google.com/d/msg/golang-dev/74zjMON9glU/dGhnoi2IMzsJ.
    
    Change-Id: I2c6527e777d14ac6dc43c53e4b3ff24f3279216e

---
+++ src/cmd/go/vendor_test.go
@@ -0,0 +1,67 @@
+// Copyright 2015 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Tests for vendoring semantics.
+
+package main_test
+
+import (
+	"bytes"
+	"fmt"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func TestVendor(t *testing.T) {
+	tg := testgo(t)
+	defer tg.cleanup()
+	tg.setenv("GOPATH", filepath.Join(tg.pwd(), "testdata"))
+	tg.setenv("GO15VENDOREXPERIMENT", "1")
+	tg.run("list", "-f", "{{.ImportPath}} {{.Imports}}", "vend/...")
+	want := `
+		vend [vend/vendor/p r]
+		vend/subdir [vend/vendor/p r]
+		vend/vendor/p []
+		vend/vendor/q []
+		vend/x [vend/x/vendor/p vend/vendor/q vend/x/vendor/r]
+		vend/x/vendor/p []
+		vend/x/vendor/r []
+	`
+	want = strings.Replace(want+"\t", "\n\t\t", "\n", -1)
+	want = strings.TrimPrefix(want, "\n")
+
+	have := tg.stdout.String()
+
+	if have != want {
+		// Output is sorted, so diff is easy to prepare.
+		var diff bytes.Buffer
+		have := splitLines(have)
+		want := splitLines(want)
+		for len(have) > 0 || len(want) > 0 {
+			if len(want) == 0 || len(have) > 0 && have[0] < want[0] {
+				fmt.Fprintf(&diff, "have: %s\n", have[0])
+				have = have[1:]
+				continue
+			}
+			if len(have) == 0 || len(want) > 0 && want[0] < have[0] {
+				fmt.Fprintf(&diff, "want: %s\n", want[0])
+				want = want[1:]
+				continue
+			}
+			fmt.Fprintf(&diff, "\t%s\n", want[0])
+			want = want[1:]
+			have = have[1:]
+		}
+		t.Errorf("incorrect go list output:\n%s", diff.String())
+	}
+}
+
+func splitLines(s string) []string {
+	x := strings.Split(s, "\n")
+	if x[len(x)-1] == "" {
+		x = x[:len(x)-1]
+	}
+	return x
+}
