812d62965f3098d89bbf57f9168911626df4f37b
Russ Cox <rsc@golang.org>
2015-06-19 17:04:56 -0400

    net/url: add RawPath field, a hint at the desired encoding of Path
    Historically we have declined to try to provide real support for URLs
    that contain %2F in the path, but they seem to be popping up more
    often, especially in (arguably ill-considered) REST APIs that shoehorn
    entire paths into individual path elements.
    
    The obvious thing to do is to introduce a URL.RawPath field that
    records the original encoding of Path and then consult it during
    URL.String and URL.RequestURI. The problem with the obvious thing
    is that it breaks backward compatibility: if someone parses a URL
    into u, modifies u.Path, and calls u.String, they expect the result
    to use the modified u.Path and not the original raw encoding.
    
    Split the difference by treating u.RawPath as a hint: the observation
    is that there are many valid encodings of u.Path. If u.RawPath is one
    of them, use it. Otherwise compute the encoding of u.Path as before.
    
    If a client does not use RawPath, the only change will be that String
    selects a different valid encoding sometimes (the original passed
    to Parse).
    
    This ensures that, for example, HTTP requests use the exact
    encoding passed to http.Get (or http.NewRequest, etc).
    
    Also add new URL.EscapedPath method for access to the actual
    escaped path. Clients should use EscapedPath instead of
    reading RawPath directly.
    
    All the old workarounds remain valid.
    
    Fixes #5777.
    Might help #9859.
    Fixes #7356.
    Fixes #8767.
    Fixes #8292.
    Fixes #8450.
    Fixes #4860.
    Fixes #10887.
    Fixes #3659.
    Fixes #8248.
    Fixes #6658.
    Reduces need for #2782.
    
    Change-Id: I77b88f14631883a7d74b72d1cf19b0073d4f5473

--- src/net/url/url_test.go
+++ src/net/url/url_test.go
@@ -13,7 +13,7 @@ import (
 
 type URLTest struct {
 	in        string
-	out       *URL
+	out       *URL   // expected parse; RawPath="" means same as Path
 	roundtrip string // expected result of reserializing the URL; empty means same as "in".
 }
 
@@ -41,11 +41,12 @@ var urltests = []URLTest{
 	{
 		"http://www.google.com/file%20one%26two",
 		&URL{
-			Scheme: "http",
-			Host:   "www.google.com",
-			Path:   "/file one&two",
+			Scheme:  "http",
+			Host:    "www.google.com",
+			Path:    "/file one&two",
+			RawPath: "/file%20one%26two",
 		},
-		"http://www.google.com/file%20one&two",
+		"",
 	},
 	// user
 	{
@@ -98,6 +99,7 @@ var urltests = []URLTest{
 			Scheme:   "http",
 			Host:     "www.google.com",
 			Path:     "/a b",
+			RawPath:  "/a%20b",
 			RawQuery: "q=c+d",
 		},
 		"",
@@ -369,6 +371,18 @@ var urltests = []URLTest{
 		},
 		"http://[fe80::1%25en01-._~]:8080/",
 	},
+	// alternate escapings of path survive round trip
+	{
+		"http://rest.rsc.io/foo%2fbar/baz%2Fquux?alt=media",
+		&URL{
+			Scheme:   "http",
+			Host:     "rest.rsc.io",
+			Path:     "/foo/bar/baz/quux",
+			RawPath:  "/foo%2fbar/baz%2Fquux",
+			RawQuery: "alt=media",
+		},
+		"",
+	},
 }
 
 // more useful string for debugging than fmt's struct printer
@@ -390,6 +404,9 @@ func DoTest(t *testing.T, parse func(string) (*URL, error), name string, tests [
 		if err != nil {
 			t.Errorf("%s(%q) returned error %s", name, tt.in, err)
 			continue
+		}
+		if tt.out.RawPath == "" {
+			tt.out.RawPath = tt.out.Path
 		}
 		if !reflect.DeepEqual(u, tt.out) {
 			t.Errorf("%s(%q):\n\thave %v\n\twant %v\n",
@@ -972,6 +989,16 @@ var requritests = []RequestURITest{
 			Opaque: "//other.example.com/%2F/%2F/",
 		},
 		"http://other.example.com/%2F/%2F/",
+	},
+	// better fix for issue 4860
+	{
+		&URL{
+			Scheme:  "http",
+			Host:    "example.com",
+			Path:    "/////",
+			RawPath: "/%2F/%2F/",
+		},
+		"/%2F/%2F/",
 	},
 	{
 		&URL{
