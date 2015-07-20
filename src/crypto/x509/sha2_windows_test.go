85029735a46db9f86656764be9618851754186bd
Alex Brainman <alex.brainman@gmail.com>
2015-07-20 11:56:49 +1000

    crypto/x509: skip one of TestSystemVerify tests on win7
    We already do that for all Windows XP and Windows Server 2003.
    Include Windows 7 and Windows Server 2008 R2 to the list.
    
    Fixes #11730
    
    DO NOT REVIEW
    
    Change-Id: I9e39fa0d6da4587beb956be5bc7db9a0a7aa9bfb

--- src/crypto/x509/sha2_windows_test.go
+++ src/crypto/x509/sha2_windows_test.go
@@ -11,9 +11,16 @@ func init() {
 	if err != nil {
 		return
 	}
-	if major := byte(v); major < 6 {
-		// Windows XP SP2 and Windows 2003 do not support SHA2.
+	major := byte(v)
+	minor := byte(v >> 8)
+	if major < 6 {
+		// Some Windows XP SP2 and Windows 2003 do not support SHA2.
 		// http://blogs.technet.com/b/pki/archive/2010/09/30/sha2-and-windows.aspx
+		supportSHA2 = false
+	}
+	if major == 6 && minor < 2 {
+		// Some Windows 7 and Windows Server 2008 R2 do not support SHA2.
+		// https://technet.microsoft.com/en-us/library/security/2949927.aspx
 		supportSHA2 = false
 	}
 }
