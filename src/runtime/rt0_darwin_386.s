ccd9c121f2e02914a713c5782de547fc77d31dcf
David Crawshaw <crawshaw@golang.org>
2015-07-20 12:33:39 -0400

    runtime: darwin/386 entrypoint for c-archive
    DO NOT REVIEW
    
    R=1.6, also there's a related linker error:
    ranlib: object: ivy-386.a(go.o) malformed object (addr field plus size of section 14 in LC_SEGMENT command 0 greater than than the segment's vmaddr plus vmsize)
    
    Change-Id: Ic22597b5e2824cffe9598cb9b506af3426c285fd

--- src/runtime/rt0_darwin_386.s
+++ src/runtime/rt0_darwin_386.s
@@ -12,5 +12,49 @@ TEXT _rt0_386_darwin(SB),NOSPLIT,$8
 	CALL	main(SB)
 	INT	$3
 
+// With -buildmode=c-archive, this symbol is called from a global constructor.
+TEXT _rt0_386_darwin_lib(SB),NOSPLIT,$0
+	PUSHL	BP
+	MOVL	SP, BP
+	PUSHL	BX
+	PUSHL	SI
+	PUSHL	DI
+
+	MOVL	8(BP), AX
+	MOVL	AX, _rt0_386_darwin_lib_argc<>(SB)
+	MOVL	12(BP), AX
+	MOVL	AX, _rt0_386_darwin_lib_argv<>(SB)
+
+	SUBL	$8, SP
+
+	// Create a new thread to do the runtime initialization and return.
+	MOVL	_cgo_sys_thread_create(SB), AX
+	MOVL	$_rt0_386_darwin_lib_go(SB), BX
+	MOVL	BX, 0(SP)
+	MOVL	$0, 4(SP)
+	CALL	AX
+
+	ADDL	$8, SP
+
+	POPL	DI
+	POPL	SI
+	POPL	BX
+	POPL	BP
+	RET
+
+TEXT _rt0_386_darwin_lib_go(SB),NOSPLIT,$12
+	MOVL	_rt0_386_darwin_lib_argc<>(SB), AX
+	MOVL	AX, 0(SP)
+	MOVL	_rt0_386_darwin_lib_argv<>(SB), AX
+	MOVL	AX, 4(SP)
+	MOVL	$runtime·rt0_go(SB), AX
+	CALL	AX
+	RET
+
+DATA _rt0_386_darwin_lib_argc<>(SB)/4, $0
+GLOBL _rt0_386_darwin_lib_argc<>(SB),NOPTR, $4
+DATA _rt0_386_darwin_lib_argv<>(SB)/4, $0
+GLOBL _rt0_386_darwin_lib_argv<>(SB),NOPTR, $4
+
 TEXT main(SB),NOSPLIT,$0
 	JMP	runtime·rt0_go(SB)
