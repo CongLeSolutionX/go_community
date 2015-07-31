0d12ca2fc1a95bf727a71368f6fe474aa3cfd8d3
Russ Cox <rsc@golang.org>
2015-07-30 22:05:51 -0400

    cmd/compile, runtime: fix placement of map bucket overflow pointer on nacl
    On most systems, a pointer is the worst case alignment, so adding
    a pointer field at the end of a struct guarantees there will be no
    padding added after that field (to satisfy overall struct alignment
    due to some more-aligned field also present).
    
    In the runtime, the map implementation needs a quick way to
    get to the overflow pointer, which is last in the bucket struct,
    so it uses size - sizeof(pointer) as the offset.
    
    NaCl/amd64p32 is the exception, as always.
    The worst case alignment is 64 bits but pointers are 32 bits.
    There's a long history that is not worth going into, but when
    we moved the overflow pointer to the end of the struct,
    we didn't get the padding computation right.
    The compiler computed the regular struct size and then
    on amd64p32 added another 32-bit field.
    And the runtime assumed it could step back two 32-bit fields
    (one 64-bit register size) to get to the overflow pointer.
    But in fact if the struct needed 64-bit alignment, the computation
    of the regular struct size would have added a 32-bit pad already,
    and then the code unconditionally added a second 32-bit pad.
    This placed the overflow pointer three words from the end, not two.
    The last two were padding, and since the runtime was consistent
    about using the second-to-last word as the overflow pointer,
    no harm done in the sense of overwriting useful memory.
    But writing the overflow pointer to a non-pointer word of memory
    means that the GC can't see the overflow blocks, so it will
    collect them prematurely. Then bad things happen.
    
    Correct all this in a few steps:
    
    1. Add an explicit check at the end of the bucket layout in the
    compiler that the overflow field is last in the struct, never
    followed by padding.
    
    2. When padding is needed on nacl (not always, just when needed),
    insert it before the overflow pointer, to preserve the "last in the struct"
    property.
    
    3. Let the compiler have the final word on the width of the struct,
    by inserting an explicit padding field instead of overwriting the
    results of the width computation it does.
    
    4. For the same reason (tell the truth to the compiler), set the type
    of the overflow field when we're trying to pretend its not a pointer
    (in this case the runtime maintains a list of the overflow blocks
    elsewhere).
    
    5. Make the runtime use "last in the struct" as its location algorithm.
    
    This fixes TestTraceStress on nacl/amd64p32.
    The 'bad map state' and 'invalid free list' failures no longer occur.
    
    Fixes #11838.
    
    Change-Id: If918887f8f252d988db0a35159944d2b36512f92

--- src/reflect/type.go
+++ src/reflect/type.go
@@ -1705,6 +1705,18 @@ func bucketOf(ktyp, etyp *rtype) *rtype {
 	// they're guaranteed to have bitmaps instead of GC programs.
 	var gcdata *byte
 	var ptrdata uintptr
+	var overflowPad uintptr
+
+	// On NaCl, pad if needed to make overflow end at the proper struct alignment.
+	// On other systems, align > ptrSize is not possible.
+	if runtime.GOARCH == "amd64p32" && (ktyp.align > ptrSize || etyp.align > ptrSize) {
+		overflowPad = ptrSize
+	}
+	size := bucketSize*(1+ktyp.size+etyp.size) + overflowPad + ptrSize
+	if size&uintptr(ktyp.align-1) != 0 || size&uintptr(etyp.align-1) != 0 {
+		panic("reflect: bad size computation in MapOf")
+	}
+
 	if kind != kindNoPointers {
 		nptr := (bucketSize*(1+ktyp.size+etyp.size) + ptrSize) / ptrSize
 		mask := make([]byte, (nptr+7)/8)
@@ -1741,16 +1753,17 @@ func bucketOf(ktyp, etyp *rtype) *rtype {
 			}
 		}
 		base += bucketSize * etyp.size / ptrSize
+		base += overflowPad / ptrSize
 
 		word := base
 		mask[word/8] |= 1 << (word % 8)
 		gcdata = &mask[0]
 		ptrdata = (word + 1) * ptrSize
-	}
 
-	size := bucketSize*(1+ktyp.size+etyp.size) + ptrSize
-	if runtime.GOARCH == "amd64p32" {
-		size += ptrSize
+		// overflow word must be last
+		if ptrdata != size {
+			panic("reflect: bad layout computation in MapOf")
+		}
 	}
 
 	b := new(rtype)
