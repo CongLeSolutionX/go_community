d4038476a2f62401a46d7f941abd2da052c03541
Joe Shaw <joe@joeshaw.org>
2015-08-06 11:21:53 -0400

    encoding/json, encoding/xml: 'omitempty' for empty structs
    Empty structs (such as time.Time) that are marked omitempty are omitted
    when marshaling to JSON and XML.
    
    For #11939
    For #4357
    Fixes #10648
    
    Change-Id: Ife16dda4ed808df4e927914369646b7ef37154da

--- src/encoding/xml/marshal_test.go
+++ src/encoding/xml/marshal_test.go
@@ -204,14 +204,21 @@ type OmitAttrTest struct {
 }
 
 type OmitFieldTest struct {
-	Int   int           `xml:",omitempty"`
-	Named int           `xml:"int,omitempty"`
-	Float float64       `xml:",omitempty"`
-	Uint8 uint8         `xml:",omitempty"`
-	Bool  bool          `xml:",omitempty"`
-	Str   string        `xml:",omitempty"`
-	Bytes []byte        `xml:",omitempty"`
-	Ptr   *PresenceTest `xml:",omitempty"`
+	Int    int           `xml:",omitempty"`
+	Named  int           `xml:"int,omitempty"`
+	Float  float64       `xml:",omitempty"`
+	Uint8  uint8         `xml:",omitempty"`
+	Bool   bool          `xml:",omitempty"`
+	Str    string        `xml:",omitempty"`
+	Bytes  []byte        `xml:",omitempty"`
+	Ptr    *PresenceTest `xml:",omitempty"`
+	Struct PresenceTest  `xml:",omitempty"`
+}
+
+type OmitFieldNonComparableTest struct {
+	Inner struct {
+		S []string
+	} `xml:",omitempty"`
 }
 
 type AnyTest struct {
@@ -838,14 +845,15 @@ var marshalTests = []struct {
 	// omitempty on fields
 	{
 		Value: &OmitFieldTest{
-			Int:   8,
-			Named: 9,
-			Float: 23.5,
-			Uint8: 255,
-			Bool:  true,
-			Str:   "str",
-			Bytes: []byte("byt"),
-			Ptr:   &PresenceTest{},
+			Int:    8,
+			Named:  9,
+			Float:  23.5,
+			Uint8:  255,
+			Bool:   true,
+			Str:    "str",
+			Bytes:  []byte("byt"),
+			Ptr:    &PresenceTest{},
+			Struct: PresenceTest{},
 		},
 		ExpectXML: `<OmitFieldTest>` +
 			`<Int>8</Int>` +
@@ -861,6 +869,10 @@ var marshalTests = []struct {
 	{
 		Value:     &OmitFieldTest{},
 		ExpectXML: `<OmitFieldTest></OmitFieldTest>`,
+	},
+	{
+		Value:     &OmitFieldNonComparableTest{},
+		ExpectXML: `<OmitFieldNonComparableTest></OmitFieldNonComparableTest>`,
 	},
 
 	// Test ",any"
