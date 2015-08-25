309afc6f7f41de84b0534a5596cccb0a0584f9a3
Todd Neal <todd@tneal.org>
2015-08-24 19:45:59 -0500

    cmd/compile: add support for a go:noinline directive
    Some tests need to disable inlining of a function.  It's currently done
    in one of a few ways (adding a function call, an empty switch, or a
    defer).  Add support for a less fragile 'go:noinline' directive that
    prevents inlining.
    
    Fixes #12140
    
    Change-Id: Ife444e13361b4a927709d81aa41e448f32eec8d4

--- src/cmd/compile/internal/gc/lex.go
+++ src/cmd/compile/internal/gc/lex.go
@@ -1624,6 +1624,11 @@ func getlinepragma() int {
 			return c
 		}
 
+		if verb == "go:noinline" {
+			noinline = true
+			return c
+		}
+
 		if verb == "go:systemstack" {
 			if compiling_runtime == 0 {
 				Yyerror("//go:systemstack only allowed in runtime")
