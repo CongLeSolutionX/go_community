7b4b69d59085cd64eb9387b95c731e5fd089eddd
Michael Hudson-Doyle <michael.hudson@canonical.com>
2015-08-28 12:09:13 +1200

    cmd/dist, cmd/go: run testshared on arm64
    And enable PIE in the go tool.
    
    Change-Id: Ibb60ccfe62518cde6e33080bbc78bfcbecff6a4e

--- src/cmd/go/build.go
+++ src/cmd/go/build.go
@@ -378,7 +378,7 @@ func buildModeInit() {
 			fatalf("-buildmode=pie not supported by gccgo")
 		} else {
 			switch platform {
-			case "android/arm", "linux/amd64", "android/amd64", "linux/ppc64le":
+			case "android/arm", "linux/amd64", "android/amd64", "linux/ppc64le", "linux/arm64":
 				codegenArg = "-shared"
 			default:
 				fatalf("-buildmode=pie not supported on %s\n", platform)
