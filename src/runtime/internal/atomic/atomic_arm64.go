35cf0bab49ce8aedb759034e1fc82c3c5e5662c2
Michael Matloob <matloob@google.com>
2015-09-02 20:25:41 -0400

    runtime: break atomics out into package runtime/internal/atomic
    So far, this CL only works on amd64. I'll update this CL for
    other archs soon.
    
    I've hacked together a workaround in cmd/go/pkg.go for extraneous
    packages being loaded.  I'm not familiar enough with the code
    to give a better solution. There's some other hacky stuff in the
    other toolchain files.
    
    There's also a segfault in the race detector test.
    
    Change-Id: I6698c8e3cf3834f55ce5824059f44d00dc8e3c2f

--- src/runtime/internal/atomic/atomic_arm64.go
+++ src/runtime/internal/atomic/atomic_arm64.go
@@ -2,40 +2,42 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package runtime
+// +build arm64
+
+package atomic
 
 import "unsafe"
 
 //go:noescape
-func xadd(ptr *uint32, delta int32) uint32
+func Xadd(ptr *uint32, delta int32) uint32
 
 //go:noescape
-func xadd64(ptr *uint64, delta int64) uint64
+func Xadd64(ptr *uint64, delta int64) uint64
 
 //go:noescape
-//go:linkname xadduintptr runtime.xadd64
-func xadduintptr(ptr *uintptr, delta uintptr) uintptr
+//go:linkname xadduintptr runtime/internal/atomic.Xadd64
+func Xadduintptr(ptr *uintptr, delta uintptr) uintptr
 
 //go:noescape
-func xchg(ptr *uint32, new uint32) uint32
+func Xchg(ptr *uint32, new uint32) uint32
 
 //go:noescape
-func xchg64(ptr *uint64, new uint64) uint64
+func Xchg64(ptr *uint64, new uint64) uint64
 
 //go:noescape
-func xchguintptr(ptr *uintptr, new uintptr) uintptr
+func Xchguintptr(ptr *uintptr, new uintptr) uintptr
 
 //go:noescape
-func atomicload(ptr *uint32) uint32
+func Atomicload(ptr *uint32) uint32
 
 //go:noescape
-func atomicload64(ptr *uint64) uint64
+func Atomicload64(ptr *uint64) uint64
 
 //go:noescape
-func atomicloadp(ptr unsafe.Pointer) unsafe.Pointer
+func Atomicloadp(ptr unsafe.Pointer) unsafe.Pointer
 
 //go:nosplit
-func atomicor8(addr *uint8, v uint8) {
+func Atomicor8(addr *uint8, v uint8) {
 	// TODO(dfc) implement this in asm.
 	// Align down to 4 bytes and use 32-bit CAS.
 	uaddr := uintptr(unsafe.Pointer(addr))
@@ -50,7 +52,7 @@ func atomicor8(addr *uint8, v uint8) {
 }
 
 //go:nosplit
-func atomicand8(addr *uint8, v uint8) {
+func Atomicand8(addr *uint8, v uint8) {
 	// TODO(dfc) implement this in asm.
 	// Align down to 4 bytes and use 32-bit CAS.
 	uaddr := uintptr(unsafe.Pointer(addr))
@@ -67,13 +69,13 @@ func atomicand8(addr *uint8, v uint8) {
 }
 
 //go:noescape
-func cas64(ptr *uint64, old, new uint64) bool
+func Cas64(ptr *uint64, old, new uint64) bool
 
 //go:noescape
-func atomicstore(ptr *uint32, val uint32)
+func Atomicstore(ptr *uint32, val uint32)
 
 //go:noescape
-func atomicstore64(ptr *uint64, val uint64)
+func Atomicstore64(ptr *uint64, val uint64)
 
 // NO go:noescape annotation; see atomic_pointer.go.
-func atomicstorep1(ptr unsafe.Pointer, val unsafe.Pointer)
+func Atomicstorep1(ptr unsafe.Pointer, val unsafe.Pointer)
