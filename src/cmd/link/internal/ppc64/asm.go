c547bc5fff78c36f65a14afcdfd419dec0f9e75d
Michael Hudson-Doyle <michael.hudson@canonical.com>
2015-09-08 15:21:58 +1200

    cmd/internal/obj, cmd/link: handle the fact that a few store/loads on ppc64 are DS form
    Change-Id: I4fe1af48ec1cd8a23e2f7f2a0257dc989ff7aced

--- src/cmd/link/internal/ppc64/asm.go
+++ src/cmd/link/internal/ppc64/asm.go
@@ -388,6 +388,39 @@ func archreloc(r *ld.Reloc, s *ld.LSym, val *int64) int {
 		}
 		return 0
 
+	case obj.R_ADDRPOWER_DS:
+		// We are spreading a 31-bit address across two instructions,
+		// putting the high (adjusted) part in the low 16 bits of the
+		// first instruction and the low part in the low 16 bits of the
+		// second instruction.
+		t := ld.Symaddr(r.Sym) + r.Add
+		if t < 0 || t >= 1<<31 {
+			ld.Ctxt.Diag("relocation for %s is too big (>=2G): %d", s.Name, ld.Symaddr(r.Sym))
+		}
+		var o1, o2 uint32
+		if ld.Ctxt.Arch.ByteOrder == binary.BigEndian {
+			o1 = uint32(*val >> 32)
+			o2 = uint32(*val)
+		} else {
+			o1 = uint32(*val)
+			o2 = uint32(*val >> 32)
+		}
+		if t&0x8000 != 0 {
+			t += 0x10000
+		}
+		if t&3 != 0 {
+			ld.Ctxt.Diag("bad DS reloc for %s: %d", s.Name, ld.Symaddr(r.Sym))
+		}
+		o1 |= (uint32(t) >> 16) & 0xffff
+		o2 |= uint32(t) & 0xffff
+
+		if ld.Ctxt.Arch.ByteOrder == binary.BigEndian {
+			*val = int64(o1)<<32 | int64(o2)
+		} else {
+			*val = int64(o2)<<32 | int64(o1)
+		}
+		return 0
+
 	case obj.R_CALLPOWER:
 		// Bits 6 through 29 = (S + A - P) >> 2
 
