67f0abe91126905523a9e38454aeb9615588d840
Yao Zhang <lunaria21@gmail.com>
2015-09-10 11:25:58 -0400

    runtime: added assembly part of linux/mips64{,le} support
    Change-Id: I9e94027ef66c88007107de2b2b75c3d7cf1352af

---
+++ src/runtime/rt0_linux_mips64x.s
@@ -0,0 +1,27 @@
+// +build linux
+// +build mips64 mips64le
+
+#include "textflag.h"
+
+TEXT _rt0_mips64_linux(SB),NOSPLIT,$0
+	JMP	_main<>(SB)
+
+TEXT _rt0_mips64le_linux(SB),NOSPLIT,$0
+	JMP	_main<>(SB)
+
+TEXT _main<>(SB),NOSPLIT,$-8
+	// In a statically linked binary, the stack contains argc,
+	// argv as argc string pointers followed by a NULL, envv as a
+	// sequence of string pointers followed by a NULL, and auxv.
+	// There is no TLS base pointer.
+#ifdef GOARCH_mips64
+	MOVW 4(R29), R1 // argc, big-endian ABI places int32 at offset 4
+#else
+	MOVW 0(R29), R1 // argc
+#endif
+	ADDV $8, R29, R2 // argv
+	JMP main(SB)
+
+TEXT main(SB),NOSPLIT,$-8
+	MOVV	$runtime·rt0_go(SB), R4
+	JMP	(R4)
