6d55dd366d0fb3fda5af7051aa107afd23c2b686
Rob Pike <r@golang.org>
2015-09-15 14:14:44 -0700

    bufio: allow Scanner to accept a user-provided buffer
    By analogy with io.CopyBuffer, add Scanner.Buffer, which lets
    the user give a buffer to the scanner. This buffer will not be
    reallocated; it is the caller's responsibility to provide a big enough
    buffer for whatever is required. In return, Scan guarantees it
    will not allocate more storage.
    
    We call it Buffer not SetBuffer for consistency with Split, which
    perhaps should have been called SetSplit; too late regardless.
    
    Both Buffer and Split panic if they are called after Scan. The
    panic in Split is new, but the comment on the method already
    said it needed to be called first, so we might as well add the
    verification while we're doing it for Buffer.
    
    This method allows precise user control of storage at the cost of more
    predetermination.
    
    Fixes #11702.
    
    Change-Id: I80e3d0e3830562fdabd4f7b08f322e1378248c39
    
    Change-Id: I955ff939426eecf499941e5324f330dfb4632ff5

--- src/bufio/scan.go
+++ src/bufio/scan.go
@@ -33,10 +33,12 @@ type Scanner struct {
 	maxTokenSize int       // Maximum size of a token; modified by tests.
 	token        []byte    // Last token returned by split.
 	buf          []byte    // Buffer used as argument to split.
+	userBuf      bool      // Buffer was provided by user; don't reallocate.
 	start        int       // First non-processed byte in buf.
 	end          int       // End of data in buf.
 	err          error     // Sticky error.
 	empties      int       // Count of successive empty tokens.
+	scanCalled   bool      // Scan has been called; buffer is in use.
 }
 
 // SplitFunc is the signature of the split function used to tokenize the
@@ -65,10 +67,14 @@ var (
 )
 
 const (
-	// MaxScanTokenSize is the maximum size used to buffer a token.
+	// MaxScanTokenSize is the maximum size used to buffer a token
+	// unless the user provides an explicit buffer, in which case that
+	// buffer's capacity determines the limit.
 	// The actual maximum token size may be smaller as the buffer
 	// may need to include, for instance, a newline.
 	MaxScanTokenSize = 64 * 1024
+
+	startBufSize = 4096 // Size of initial allocation for buffer.
 )
 
 // NewScanner returns a new Scanner to read from r.
@@ -78,7 +84,6 @@ func NewScanner(r io.Reader) *Scanner {
 		r:            r,
 		split:        ScanLines,
 		maxTokenSize: MaxScanTokenSize,
-		buf:          make([]byte, 4096), // Plausible starting size; needn't be large.
 	}
 }
 
@@ -112,6 +117,7 @@ func (s *Scanner) Text() string {
 // Scan panics if the split function returns 100 empty tokens without
 // advancing the input. This is a common error mode for scanners.
 func (s *Scanner) Scan() bool {
+	s.scanCalled = true
 	// Loop until we have a token.
 	for {
 		// See if we can get a token with what we already have.
@@ -158,11 +164,14 @@ func (s *Scanner) Scan() bool {
 		}
 		// Is the buffer full? If so, resize.
 		if s.end == len(s.buf) {
-			if len(s.buf) >= s.maxTokenSize {
+			if len(s.buf) >= s.maxTokenSize || s.userBuf {
 				s.setErr(ErrTooLong)
 				return false
 			}
 			newSize := len(s.buf) * 2
+			if newSize == 0 {
+				newSize = startBufSize
+			}
 			if newSize > s.maxTokenSize {
 				newSize = s.maxTokenSize
 			}
@@ -217,9 +226,28 @@ func (s *Scanner) setErr(err error) {
 	}
 }
 
-// Split sets the split function for the Scanner. If called, it must be
-// called before Scan. The default split function is ScanLines.
+// Buffer sets the buffer to use when scanning. If called, this buffer
+// is the only one that will be used; Scan will do no allocation. Also,
+// the maximum token size will be the capacity of the buffer.
+// If called, it must be called before Scan; it panics if Scan
+// has already been called.
+func (s *Scanner) Buffer(buf []byte) {
+	if s.scanCalled {
+		panic("Buffer called after Scan")
+	}
+	s.buf = buf[0:cap(buf)]
+	s.userBuf = true
+	s.maxTokenSize = cap(buf)
+}
+
+// Split sets the split function for the Scanner.
+// If called, it must be called before Scan; it panics if Scan
+// has already been called.
+// The default split function is ScanLines.
 func (s *Scanner) Split(split SplitFunc) {
+	if s.scanCalled {
+		panic("Split called after Scan")
+	}
 	s.split = split
 }
 
