673fa4c1d9933c074afd9f0ec66485d7b398080b
Rob Pike <r@golang.org>
2015-09-21 11:06:43 -0700

    cmd/vet: build the binary only once in the test
    Recent changes caused vet to build the binary for each Test function.
    This is wasteful and will become only more so as more tests are added.
    Use testing.Main to build only once.
    
    Verified that compilation errors still appear if the binary cannot be
    built.
    
    Before:
    	real	0m11.169s
    	user	0m18.328s
    	sys	0m2.152s
    
    After:
    	real	0m5.132s
    	user	0m9.404s
    	sys	0m1.168s
    
    Of course if the compiler were fast we might not notice, but vet is
    a big program and growing bigger all the time, as are the tests.
    
    Change-Id: I209a8fdcace94bc5cec946f5dd365d7191f44c02

--- src/cmd/vet/vet_test.go
+++ src/cmd/vet/vet_test.go
@@ -10,6 +10,8 @@ package main_test
 
 import (
 	"bytes"
+	"flag"
+	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -22,20 +24,40 @@ const (
 	binary  = "testvet.exe"
 )
 
-func CanRun(t *testing.T) bool {
-	// Plan 9 and Windows systems can't be guaranteed to have Perl and so can't run errchk.
+// We implement TestMain so we can build the test binary only once.
+func TestMain(m *testing.M) {
+	flag.Parse()
+	result := m.Run()
+	os.Remove(binary)
+	os.Exit(result)
+}
+
+func CanRun() bool {
 	switch runtime.GOOS {
 	case "plan9", "windows":
-		t.Skip("skipping test; no Perl on %q", runtime.GOOS)
+		// No Perl installed, can't run errcheck.
+		return false
+	case "nacl":
+		// Minimal and problematic file system.
 		return false
 	}
 	return true
 }
 
-func Build(t *testing.T) {
-	// go build
+var built = false
+
+func Build() {
+	if built || !CanRun() {
+		return
+	}
 	cmd := exec.Command("go", "build", "-o", binary)
-	run(cmd, t)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", output)
+		fmt.Fprintln(os.Stderr, "cannot build:", err)
+		os.Exit(2)
+	}
+	built = true
 }
 
 func Vet(t *testing.T, files []string) {
@@ -58,11 +80,10 @@ func Vet(t *testing.T, files []string) {
 //
 
 func TestVet(t *testing.T) {
-	if !CanRun(t) {
+	if !CanRun() {
 		t.Skip("cannot run on this environment")
 	}
-	Build(t)
-	defer os.Remove(binary)
+	Build()
 
 	// errchk ./testvet
 	gos, err := filepath.Glob(filepath.Join(dataDir, "*.go"))
@@ -78,23 +99,21 @@ func TestVet(t *testing.T) {
 }
 
 func TestDivergentPackagesExamples(t *testing.T) {
-	if !CanRun(t) {
+	if !CanRun() {
 		t.Skip("cannot run on this environment")
 	}
-	Build(t)
-	defer os.Remove(binary)
 
+	Build()
 	// errchk ./testvet
 	Vet(t, []string{"testdata/divergent/buf.go", "testdata/divergent/buf_test.go"})
 }
 
 func TestIncompleteExamples(t *testing.T) {
-	if !CanRun(t) {
+	if !CanRun() {
 		t.Skip("cannot run on this environment")
 	}
-	Build(t)
-	defer os.Remove(binary)
 
+	Build()
 	// errchk ./testvet
 	Vet(t, []string{"testdata/incomplete/examples_test.go"})
 }
@@ -115,18 +134,13 @@ func run(c *exec.Cmd, t *testing.T) bool {
 
 // TestTags verifies that the -tags argument controls which files to check.
 func TestTags(t *testing.T) {
-	// go build
-	cmd := exec.Command("go", "build", "-o", binary)
-	run(cmd, t)
-
-	defer os.Remove(binary)
-
+	Build()
 	args := []string{
 		"-tags=testtag",
 		"-v", // We're going to look at the files it examines.
 		"testdata/tagtest",
 	}
-	cmd = exec.Command("./"+binary, args...)
+	cmd := exec.Command("./"+binary, args...)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		t.Fatal(err)
