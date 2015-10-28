bbf40a397f7e4e6d922f8fa8328005e007f2bfb1
Keith Randall <khr@golang.org>
2015-10-28 10:40:47 -0700

    [dev.ssa] cmd/compile: fix printing of live information
    SSA generates ACALL assembly with the target in a *Sym.
    The old compiler generates both that *Sym and a *Node.
    Use the *Sym to print the live info so it works with both compilers.
    
    Change-Id: I0b12a161f83e76638604358c21b9f5abb31ce950

--- src/cmd/compile/internal/gc/plive.go
+++ src/cmd/compile/internal/gc/plive.go
@@ -19,6 +19,7 @@ import (
 	"cmd/internal/obj"
 	"fmt"
 	"sort"
+	"strings"
 )
 
 const (
@@ -1393,8 +1394,12 @@ func livenessepilogue(lv *Liveness) {
 				if msg != nil {
 					fmt_ = ""
 					fmt_ += fmt.Sprintf("%v: live at ", p.Line())
-					if p.As == obj.ACALL && p.To.Node != nil {
-						fmt_ += fmt.Sprintf("call to %s:", ((p.To.Node).(*Node)).Sym.Name)
+					if p.As == obj.ACALL && p.To.Sym != nil {
+						name := p.To.Sym.Name
+						if strings.HasPrefix(name, "\"\".") {
+							name = name[3:]
+						}
+						fmt_ += fmt.Sprintf("call to %s:", name)
 					} else if p.As == obj.ACALL {
 						fmt_ += "indirect call:"
 					} else {
