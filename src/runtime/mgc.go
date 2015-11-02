cdf428d5acb88e61612c7f4239946ba78bbebe4d
Michael Matloob <matloob@golang.org>
2015-11-02 14:09:24 -0500

    runtime: break atomics out into package runtime/internal/atomic
    status per arch:
    (all trybots are happy)
    amd64,  386, linux-arm: compiles, all tests pass
    ppc, arm64, darwin-arm: compiles successfully, haven't run tests
    
    this change a reasonably good state. Some bits that need extra attention:
    - the 'spinlock' in atomic.go is pretty dodgy
    
    immediate next steps:
    - test on arm64, arm, ppc
    
    Change-Id: I6698c8e3cf3834f55ce5824059f44d00dc8e3c2f

--- src/runtime/mgc.go
+++ src/runtime/mgc.go
@@ -120,7 +120,10 @@
 
 package runtime
 
-import "unsafe"
+import (
+	"runtime/internal/atomic"
+	"unsafe"
+)
 
 const (
 	_DebugGC         = 0
@@ -234,7 +237,7 @@ const (
 
 //go:nosplit
 func setGCPhase(x uint32) {
-	atomicstore(&gcphase, x)
+	atomic.Store(&gcphase, x)
 	writeBarrierEnabled = gcphase == _GCmark || gcphase == _GCmarktermination
 }
 
@@ -657,11 +660,11 @@ func (c *gcControllerState) findRunnableGCWorker(_p_ *p) *g {
 
 	decIfPositive := func(ptr *int64) bool {
 		if *ptr > 0 {
-			if xaddint64(ptr, -1) >= 0 {
+			if atomic.Xaddint64(ptr, -1) >= 0 {
 				return true
 			}
 			// We lost a race
-			xaddint64(ptr, +1)
+			atomic.Xaddint64(ptr, +1)
 		}
 		return false
 	}
@@ -715,7 +718,7 @@ func (c *gcControllerState) findRunnableGCWorker(_p_ *p) *g {
 		timeUsed := c.fractionalMarkTime + gcForcePreemptNS
 		if then > 0 && float64(timeUsed)/float64(then) > c.fractionalUtilizationGoal {
 			// Nope, we'd overshoot the utilization goal
-			xaddint64(&c.fractionalMarkWorkersNeeded, +1)
+			atomic.Xaddint64(&c.fractionalMarkWorkersNeeded, +1)
 			return nil
 		}
 		_p_.gcMarkWorkerMode = gcMarkWorkerFractionalMode
@@ -762,7 +765,7 @@ const gcOverAssistBytes = 1 << 20
 // A false negative simple does not start a GC, a false positive
 // will start a GC needlessly. Neither have correctness issues.
 func shouldtriggergc() bool {
-	return memstats.heap_live >= memstats.next_gc && atomicloaduint(&bggc.working) == 0
+	return memstats.heap_live >= memstats.next_gc && atomic.Loaduint(&bggc.working) == 0
 }
 
 // bgMarkSignal synchronizes the GC coordinator and background mark workers.
@@ -796,7 +799,7 @@ func (s *bgMarkSignal) wait() {
 // after calling complete in order to let the coordinator goroutine
 // run.
 func (s *bgMarkSignal) complete() bool {
-	if cas(&s.done, 0, 1) {
+	if atomic.Cas(&s.done, 0, 1) {
 		// This is the first worker to reach this completion point.
 		// Signal the main GC goroutine.
 		lock(&s.lock)
@@ -1072,7 +1075,7 @@ func gc(mode gcMode) {
 			// black invariant. Enable mutator assists to
 			// put back-pressure on fast allocating
 			// mutators.
-			atomicstore(&gcBlackenEnabled, 1)
+			atomic.Store(&gcBlackenEnabled, 1)
 
 			// Concurrent mark.
 			startTheWorldWithSema()
@@ -1108,8 +1111,8 @@ func gc(mode gcMode) {
 			// Rescan global data and BSS. Bump "jobs"
 			// down before "next" so workers won't try
 			// running root jobs until we set "next".
-			atomicstore(&work.markrootJobs, uint32(fixedRootCount+work.nDataRoots+work.nBSSRoots))
-			atomicstore(&work.markrootNext, fixedRootCount)
+			atomic.Store(&work.markrootJobs, uint32(fixedRootCount+work.nDataRoots+work.nBSSRoots))
+			atomic.Store(&work.markrootNext, fixedRootCount)
 		})
 
 		// Wait for this more aggressive background mark to complete.
@@ -1147,7 +1150,7 @@ func gc(mode gcMode) {
 
 	// World is stopped.
 	// Start marktermination which includes enabling the write barrier.
-	atomicstore(&gcBlackenEnabled, 0)
+	atomic.Store(&gcBlackenEnabled, 0)
 	gcBlackenPromptly = false
 	setGCPhase(_GCmarktermination)
 
@@ -1229,7 +1232,7 @@ func gc(mode gcMode) {
 	// Update timing memstats
 	now, unixNow := nanotime(), unixnanotime()
 	pauseNS += now - pauseStart
-	atomicstore64(&memstats.last_gc, uint64(unixNow)) // must be Unix time to make sense to user
+	atomic.Store64(&memstats.last_gc, uint64(unixNow)) // must be Unix time to make sense to user
 	memstats.pause_ns[memstats.numgc%uint32(len(memstats.pause_ns))] = uint64(pauseNS)
 	memstats.pause_end[memstats.numgc%uint32(len(memstats.pause_end))] = uint64(unixNow)
 	memstats.pause_total_ns += uint64(pauseNS)
@@ -1394,7 +1397,7 @@ func gcBgMarkWorker(p *p) {
 
 		startTime := nanotime()
 
-		decnwait := xadd(&work.nwait, -1)
+		decnwait := atomic.Xadd(&work.nwait, -1)
 		if decnwait == work.nproc {
 			println("runtime: work.nwait=", decnwait, "work.nproc=", work.nproc)
 			throw("work.nwait was > work.nproc")
@@ -1421,7 +1424,7 @@ func gcBgMarkWorker(p *p) {
 
 		// Was this the last worker and did we run out
 		// of work?
-		incnwait := xadd(&work.nwait, +1)
+		incnwait := atomic.Xadd(&work.nwait, +1)
 		if incnwait > work.nproc {
 			println("runtime: p.gcMarkWorkerMode=", p.gcMarkWorkerMode,
 				"work.nwait=", incnwait, "work.nproc=", work.nproc)
@@ -1444,13 +1447,13 @@ func gcBgMarkWorker(p *p) {
 		duration := nanotime() - startTime
 		switch p.gcMarkWorkerMode {
 		case gcMarkWorkerDedicatedMode:
-			xaddint64(&gcController.dedicatedMarkTime, duration)
-			xaddint64(&gcController.dedicatedMarkWorkersNeeded, 1)
+			atomic.Xaddint64(&gcController.dedicatedMarkTime, duration)
+			atomic.Xaddint64(&gcController.dedicatedMarkWorkersNeeded, 1)
 		case gcMarkWorkerFractionalMode:
-			xaddint64(&gcController.fractionalMarkTime, duration)
-			xaddint64(&gcController.fractionalMarkWorkersNeeded, 1)
+			atomic.Xaddint64(&gcController.fractionalMarkTime, duration)
+			atomic.Xaddint64(&gcController.fractionalMarkWorkersNeeded, 1)
 		case gcMarkWorkerIdleMode:
-			xaddint64(&gcController.idleMarkTime, duration)
+			atomic.Xaddint64(&gcController.idleMarkTime, duration)
 		}
 	}
 }
@@ -1462,7 +1465,7 @@ func gcMarkWorkAvailable(p *p) bool {
 	if p != nil && !p.gcw.empty() {
 		return true
 	}
-	if atomicload64(&work.full) != 0 {
+	if atomic.Load64(&work.full) != 0 {
 		return true // global work available
 	}
 	if work.markrootNext < work.markrootJobs {
@@ -1783,7 +1786,7 @@ func gchelper() {
 	}
 
 	nproc := work.nproc // work.nproc can change right after we increment work.ndone
-	if xadd(&work.ndone, +1) == nproc-1 {
+	if atomic.Xadd(&work.ndone, +1) == nproc-1 {
 		notewakeup(&work.alldone)
 	}
 	_g_.m.traceback = 0
