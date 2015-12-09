e957b448c66ce58138d8151afea59246d5b3699c
Joe Tsai <joetsai@digital-static.net>
2015-12-08 18:26:22 -0800

    compress/bzip2: fix benchmark to actually measure decompression rate
    Motivation:
    * Previously, the size of the compressed data was used for metrics,
    rather than the uncompressed size. This causes the library to appear
    to perform poorly relative to C or other implementation. Switch it
    to use the uncompressed size so that it matches how decompression
    benchmarks are usually done (like in compress/flate). This also makes
    it easier to compare bzip2 rates to other algorithms since they measure
    performance in this way.
    * Also, reset the timer after doing initialization work.
    
    Change-Id: I32112c2ee8e7391e658c9cf31039f70a689d9b9d

--- src/compress/bzip2/bzip2_test.go
+++ src/compress/bzip2/bzip2_test.go
@@ -192,7 +192,17 @@ func benchmarkDecode(b *testing.B, testfile int) {
 	if err != nil {
 		b.Fatal(err)
 	}
-	b.SetBytes(int64(len(compressed)))
+
+	// Determine the uncompressed size of testfile.
+	uncompressed, err := ioutil.ReadAll(NewReader(bytes.NewReader(compressed)))
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.SetBytes(int64(len(uncompressed)))
+	b.ReportAllocs()
+	b.ResetTimer()
+
 	for i := 0; i < b.N; i++ {
 		r := bytes.NewReader(compressed)
 		io.Copy(ioutil.Discard, NewReader(r))
