990ddb4559ddbf53d78377c47b9d8a81a61c4350
Marcel van Lohuizen <mpvl@golang.org>
2016-01-29 17:14:51 +0100

    testing: expose subtest and subbenchmark functionality
    Fixes #12166
    
    Change-Id: Ie62cba2c39beb5732447ba3688c93c08ef12abb5

--- src/testing/testing.go
+++ src/testing/testing.go
@@ -548,9 +548,9 @@ func tRunner(t *T, fn func(t *T)) {
 	t.finished = true
 }
 
-// run runs f as a subtest of t called name. It reports whether f succeeded.
+// Run runs f as a subtest of t called name. It reports whether f succeeded.
 // Run will block until all its parallel subtests have completed.
-func (t *T) run(name string, f func(t *T)) bool {
+func (t *T) Run(name string, f func(t *T)) bool {
 	testName, ok := t.context.match.fullName(&t.common, name)
 	if !ok {
 		return true
@@ -731,7 +731,7 @@ func RunTests(matchString func(pat, str string) (bool, error), tests []InternalT
 				if !matched {
 					continue
 				}
-				t.run(test.Name, test.F)
+				t.Run(test.Name, test.F)
 			}
 			// Run catching the signal rather than the tRunner as a separate
 			// goroutine to avoid adding a goroutine during the sequential
