386bad84bebdb7461e946274743954b333a6d6b6
Alexandru Moșoi <mosoi@google.com>
2016-02-10 00:27:33 +0100

    [dev.ssa] cmd/compile/internal/ssa: transform degenerate control blocks
    * In cases where we end up with emty branches like in
    if a then jmp b else jmp b;
    the flow can be replaced by a; jmp b.
    * The value a is not eliminted, but it could be.
    
    Updates #14277
    
    Change-Id: Id05552fbdf923b6f8ae3745eb4bd1646650ef62d

--- src/cmd/compile/internal/ssa/trim.go
+++ src/cmd/compile/internal/ssa/trim.go
@@ -7,31 +7,45 @@ package ssa
 // trim removes blocks with no code in them.
 // These blocks were inserted to remove critical edges.
 func trim(f *Func) {
-	i := 0
-	for _, b := range f.Blocks {
-		if b.Kind != BlockPlain || len(b.Values) != 0 || len(b.Preds) != 1 {
-			f.Blocks[i] = b
-			i++
-			continue
-		}
-		// TODO: handle len(b.Preds)>1 case.
+	for ok := true; ok; {
+		ok = false
 
-		// Splice b out of the graph.
-		pred := b.Preds[0]
-		succ := b.Succs[0]
-		for j, s := range pred.Succs {
-			if s == b {
-				pred.Succs[j] = succ
+		i := 0
+		for _, b := range f.Blocks {
+			// If a control block has the same successors then transform this
+			// block into a plain block and remove the jumps.
+			if b.Control != nil && b.Control.Type.IsFlags() &&
+				len(b.Succs) == 2 && b.Succs[0] == b.Succs[1] {
+				b.Kind = BlockPlain
+				b.Control = nil
+				b.Succs = b.Succs[:1]
+				b.Succs[0].removePred(b)
+			}
+			if b.Kind != BlockPlain || len(b.Values) != 0 || len(b.Preds) != 1 {
+				f.Blocks[i] = b
+				i++
+				continue
+			}
+			// TODO: handle len(b.Preds)>1 case.
+
+			// Splice b out of the graph.
+			ok = true
+			pred := b.Preds[0]
+			succ := b.Succs[0]
+			for j, s := range pred.Succs {
+				if s == b {
+					pred.Succs[j] = succ
+				}
+			}
+			for j, p := range succ.Preds {
+				if p == b {
+					succ.Preds[j] = pred
+				}
 			}
 		}
-		for j, p := range succ.Preds {
-			if p == b {
-				succ.Preds[j] = pred
-			}
+		for j := i; j < len(f.Blocks); j++ {
+			f.Blocks[j] = nil
 		}
+		f.Blocks = f.Blocks[:i]
 	}
-	for j := i; j < len(f.Blocks); j++ {
-		f.Blocks[j] = nil
-	}
-	f.Blocks = f.Blocks[:i]
 }
