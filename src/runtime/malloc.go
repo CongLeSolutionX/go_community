a7203b72012dd5b550f81e1e310f4fa42f494e7c
Rick Hudson <rlh@golang.org>
2016-02-09 09:38:44 -0500

    [dev.garbage] runtime: mark/allocation helper functions
    The gcmarkBits is a bit vector used by the GC to mark
    reachable objects. Once a GC cycle is complete the gcmarkBits
    swap places with the allocBits. allocBits is then used directly
    by malloc to locate free objects, thus avoiding the
    construction of a linked free list. This CL introduces a set
    of helper functions for manipulating gcmarkBits and allocBits
    that will be used by later CLs to realize the actual
    algorithm. Minimal attempts have been made to optimize these helper routines.
    
    Change-Id: I55ad6240ca32cd456e8ed4973c6970b3b882dd34

--- src/runtime/malloc.go
+++ src/runtime/malloc.go
@@ -775,7 +775,7 @@ func largeAlloc(size uintptr, flag uint32) *mspan {
 		throw("out of memory")
 	}
 	s.limit = uintptr(s.start)<<_PageShift + size
-	heapBitsForSpan(s.base()).initSpan(s.layout())
+	heapBitsForSpan(s.base()).initSpan(s)
 	return s
 }
 
