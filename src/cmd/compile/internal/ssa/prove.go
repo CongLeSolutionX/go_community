38f677dd0da9d3cbac5bb77676d389b1350b3204
Alexandru Moșoi <mosoi@google.com>
2016-02-19 12:14:42 +0100

    [dev.ssa] cmd/compile/internal/ssa: remove proven redundant controls.
    * It does very simple bounds checking elimination:
    for i := range a { foo(a[i]) }.
    * Improves on the following redundant expression:
    return a6 || (a6 || (a6 || a4)) || (a6 || (a4 || a6 || (false || a6)))
    
    I patched in CL 12960 that does bounds, nil and constant propagation
    to make sure this CL is not just redundant. Size of pkg/tool/linux_amd64/*
    (excluding compile which is affected by this change):
    
    -this -12960 95941968
    +this -12960 95655048 -286920
    -this +12960 95585264 -69784
    +this +12960 95518744 -66520
    
    While there is some overlap ~20% of the reduction is unique to the new pass.
    
    Change-Id: I98957427399145fb33693173fd4d5a8d71c7cc20

---
+++ src/cmd/compile/internal/ssa/prove.go
@@ -0,0 +1,115 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package ssa
+
+const (
+	lt uint = 1 << iota
+	eq
+	gt
+)
+
+const (
+	signed uint = 1 << iota
+	unsigned
+)
+
+var (
+	comp = map[BlockKind]uint{
+		BlockAMD64EQ:  eq,
+		BlockAMD64NE:  lt | gt,
+		BlockAMD64LT:  lt,
+		BlockAMD64LE:  lt | eq,
+		BlockAMD64GT:  gt,
+		BlockAMD64GE:  eq | gt,
+		BlockAMD64ULT: lt,
+		BlockAMD64ULE: lt | eq,
+		BlockAMD64UGT: gt,
+		BlockAMD64UGE: eq | gt,
+	}
+	// compatibility mask. we can put include the arch here if need.
+	sign = map[BlockKind]uint{
+		BlockAMD64EQ:  signed | unsigned,
+		BlockAMD64NE:  signed | unsigned,
+		BlockAMD64LT:  signed,
+		BlockAMD64LE:  signed,
+		BlockAMD64GT:  signed,
+		BlockAMD64GE:  signed,
+		BlockAMD64ULT: unsigned,
+		BlockAMD64ULE: unsigned,
+		BlockAMD64UGT: unsigned,
+		BlockAMD64UGE: unsigned,
+	}
+)
+
+func prove(f *Func) {
+	idom := dominators(f)
+	sdom := newSparseTree(f, idom)
+
+	for _, b := range f.Blocks {
+		if idom[b.ID] == nil {
+			continue // unreachable
+		}
+		maskb := comp[b.Kind]
+		if maskb == 0 {
+			continue // not a control block
+		}
+
+		succ := -1 // which successor is always taken
+		all := lt | eq | gt
+		mask := all // restrictions comming from ancestors
+		stop := false
+
+		for p := idom[b.ID]; p != nil && !stop; p = idom[p.ID] {
+			if p == b.Control.Block {
+				// The control is defined in this block, no ancestor
+				// can branch on it. We still have to run the logic below.
+				stop = true
+			}
+			if p.Control != b.Control {
+				continue
+			}
+			if sign[b.Kind]&sign[p.Kind] == 0 {
+				continue
+			}
+			maskp := comp[p.Kind]
+			if maskp == 0 {
+				continue
+			}
+
+			yes := sdom.isAncestorEq(p.Succs[0], b) && len(p.Succs[0].Preds) == 1
+			if yes {
+				mask &= maskp
+			}
+			no := sdom.isAncestorEq(p.Succs[1], b) && len(p.Succs[1].Preds) == 1
+			if no {
+				mask &= all ^ maskp
+			}
+			if yes && no {
+				b.Fatalf("block %s came from both branches of %s", b, p)
+			}
+
+			// p and b have the same control block and test compatible signs
+			if maskb&mask == mask {
+				b.Logf("(%s.%s) proved positive branch of %s from %s in %s\n", b.Kind, p.Kind, b, p, f.Name)
+				succ = 0
+				break
+			}
+			if (all^maskb)&mask == mask {
+				b.Logf("(%s.%s) proved negative branch of %s from %s in %s\n", b.Kind, p.Kind, b, p, f.Name)
+				succ = 1
+				break
+			}
+		}
+
+		if succ == 0 {
+			b.Kind = BlockFirst
+			b.Control = nil
+		} else if succ == 1 {
+			b.Kind = BlockFirst
+			b.Control = nil
+			b.Succs[0], b.Succs[1] = b.Succs[1], b.Succs[0]
+		}
+	}
+}
