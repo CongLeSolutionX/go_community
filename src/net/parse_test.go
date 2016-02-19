47a31b5b8e89dd9c5bc5ab4318de308aac42ae7d
Morten Siebuhr <sbhr@sbhr.dk>
2016-02-19 21:53:17 +0100

    net: fix looking up port numbers starting with numbers.
    Fixes #14322
    
    Change-Id: I1b90dbed434494723e261d84e73fe705e5c0507a

--- src/net/parse_test.go
+++ src/net/parse_test.go
@@ -99,3 +99,32 @@ func TestDtoi(t *testing.T) {
 		}
 	}
 }
+
+var parsePortTests = []struct {
+	service     string
+	port        int
+	needsLookup bool
+}{
+	{"", 0, false},
+	{"42", 42, false},
+
+	{"123456789", 123456789, false},
+	{"-123456789", -123456789, false},
+	{"-1", -1, false},
+	{"0", 0, false},
+
+	{"abc", 0, true},
+	{"9pfs", 0, true},
+	{"123badport", 0, true},
+	{"bad123port", 0, true},
+	{"badport123", 0, true},
+	{"123456789badport", 0, true},
+}
+
+func TestParsePort(t *testing.T) {
+	for _, tt := range parsePortTests {
+		if port, needsLookup := parsePort(tt.service); port != tt.port || needsLookup != tt.needsLookup {
+			t.Errorf("parsePort(%q) = %d, %t; want %d, %t", tt.service, port, needsLookup, tt.port, tt.needsLookup)
+		}
+	}
+}
