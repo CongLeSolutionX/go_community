a12d1b10381c7d2859ef6387107e60ae1757e987
Morten Siebuhr <sbhr@sbhr.dk>
2016-02-19 21:53:17 +0100

    net: fix looking up port numbers starting with numbers.
    LookupPort() correctly parses service names beginning with numerals by
    implementing a new parser, mainly taken from strconv/atoi.go.
    
    Also testes some previously undefined behaviours around port numbers
    larger than 65535 that previously could lead to some tests fail with
    EOPNOTSUPP (Operation Not Supported).
    
    Fixes #14322
    
    Change-Id: I1b90dbed434494723e261d84e73fe705e5c0507a

--- src/net/lookup_test.go
+++ src/net/lookup_test.go
@@ -612,6 +612,7 @@ var lookupPortTests = []struct {
 	{"tcp", "65536", 0, false},
 	{"udp", "-1", 0, false},
 	{"udp", "65536", 0, false},
+	{"tcp", "123456789", 0, false},
 
 	// Issue 13610: LookupPort("tcp", "")
 	{"tcp", "", 0, true},
@@ -632,7 +633,7 @@ func TestLookupPort(t *testing.T) {
 
 	for _, tt := range lookupPortTests {
 		if port, err := LookupPort(tt.network, tt.name); port != tt.port || (err == nil) != tt.ok {
-			t.Errorf("LookupPort(%q, %q) = %d, %v; want %d", tt.network, tt.name, port, err, tt.port)
+			t.Errorf("LookupPort(%q, %q) = %d, %v; want %d, error=%t", tt.network, tt.name, port, err, tt.port, !tt.ok)
 		}
 	}
 }
