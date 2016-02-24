41dcb7d06481e803571d739bc2a91e14f60ec5b1
Rick Hudson <rlh@golang.org>
2016-02-24 14:36:30 -0500

    [dev.garbage] runtime: add bit and cache ctz64 (count trailing zero)
    Add to each span a 64 bit cache (allocCache) of the allocBits
    at freeindex. allocCache is shifted such that the lowest bit
    corresponds to the bit freeindex. allocBits uses a 0 to
    indicate an object is free, on the other hand allocCache
    uses a 1 to indicate an object is free. This facilitates
    ctz64 (count trailing zero) which counts the number of 0s
    trailing the least significant 1. This is also the index of
    the least significant 1.
    
    Each span maintains a freeindex indicating the boundary
    between allocated objects and unallocated objects. allocCache
    is shifted as freeindex is incremented such that the low bit
    in allocCache corresponds to the bit a freeindex in the
    allocBits array.
    
    Currently ctz64 is written in Go using a for loop so it is
    not very efficient. Use of the hardware instruction will
    follow. With this in mind comparisons of the garbage
    benchmark are as follows.
    
    1.6 release        2.8 seconds
    dev:garbage branch 3.1 seconds.
    
    Profiling shows the go implementation of ctz64 takes up
    1% of the total time.
    
    Change-Id: If084ed9c3b1eda9f3c6ab2e794625cb870b8167f

--- src/runtime/mcentral.go
+++ src/runtime/mcentral.go
@@ -106,7 +106,18 @@ retry:
 havespan:
 	cap := int32((s.npages << _PageShift) / s.elemsize)
 	n := cap - int32(s.allocCount)
-	if n == 0 {
+	if n == 0 || s.freeindex == s.nelems || uintptr(s.allocCount) == s.nelems {
+		println("runtime: cacheSpan havespan:110  --- s=", s, " cap", cap, "s.nelems=", s.nelems, "thumbprint= 110", "n=", n, "s.sizeclass=", s.sizeclass,
+			"s.allocCount=", s.allocCount, "s.freeindex=", s.freeindex, "s.allocCache=", hex(s.allocCache), "s.elemsize=", s.elemsize,
+			"\ns.allobBits[0-7]=",
+			hex(s.allocBits[0]),
+			hex(s.allocBits[1]),
+			hex(s.allocBits[2]),
+			hex(s.allocBits[3]),
+			hex(s.allocBits[4]),
+			hex(s.allocBits[5]),
+			hex(s.allocBits[6]),
+			hex(s.allocBits[7]))
 		throw("span has no free objects")
 	}
 	usedBytes := uintptr(s.allocCount) * s.elemsize
@@ -123,6 +134,17 @@ havespan:
 		gcController.revise()
 	}
 	s.incache = true
+	freeByteBase := (s.freeindex / 64) * 64
+	whichByte := freeByteBase / 8
+	// Compliment the first 8 bytes and place in s.allocCache.
+	// Unlike in allocBits a 1 in s.allocCache means the object is not marked.
+
+	s.refillAllocCache(whichByte)
+
+	// adjust the allocCache so that s.freeindex corresponds to the low bit in
+	// s.allocCache
+	s.allocCache >>= s.freeindex % 64
+
 	return s
 }
 
@@ -159,8 +181,6 @@ func (c *mcentral) freeSpan(s *mspan, n int32, start gclinkptr, end gclinkptr, p
 		throw("freeSpan given cached span")
 	}
 
-	s.allocCount -= uint16(n)
-
 	if preserve {
 		// preserve is set only when called from MCentral_CacheSpan above,
 		// the span must be in the empty list.
@@ -192,8 +212,7 @@ func (c *mcentral) freeSpan(s *mspan, n int32, start gclinkptr, end gclinkptr, p
 
 	// s is completely freed, return it to the heap.
 	if s.prev == nil || s.list != &c.nonempty {
-		println("failed mCentral_FreeSpan s=", s, "s.prev=", s.prev, "s.list=", s.list, "list=", &c.nonempty)
-		//		throw("mCentral_FreeSpan")
+		throw("mCentral_FreeSpan")
 	}
 	c.nonempty.remove(s)
 	s.needzero = 1
