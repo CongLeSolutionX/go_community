b98c304f73531e7e1f7f1e9b9ee5d3864bedc7ef
Rick Hudson <rlh@golang.org>
2016-03-02 12:15:02 -0500

    [dev.garbage] runtime: remove heapBitsSweepSpan
    Prior to this CL the sweep phase was responsible for locating
    all objects that were about to be freed and calling a function
    to process the object. This was done by the function
    heapBitsSweepSpan. Part of processing included calls to
    tracefree and msanfree as well as counting how many objects
    were freed.
    
    The calls to tracefree and msanfree have been moved into the
    gcmalloc routine and called when the object is about to be
    reallocated. The counting of free objects has been optimized
    using an array based popcnt algorithm and if all the objects
    in a span are free then span is freed.
    
    Similarly the code to locate the next free object has been
    optimized to use an array based ctz (count trailing zero).
    Various hot paths in the allocation logic have been optimized.
    
    At this point the garbage benchmark is with 3% of the 1.6
    release.
    
    Change-Id: I00643c442e2ada1685c010c3447e4ea8537d2dfa

--- src/runtime/malloc.go
+++ src/runtime/malloc.go
@@ -496,6 +496,32 @@ const (
 	_FlagNoZero = 1 << 1 // don't zero memory
 )
 
+// nextFreeFast returns the next free object if one is quickly available.
+// Otherwise it returns 0.
+func (c *mcache) nextFreeFast(sizeclass int8) gclinkptr {
+	s := c.alloc[sizeclass]
+	ctzIndex := uint8(s.allocCache & 0xff)
+	if ctzIndex != 0 {
+		theBit := uint64(ctzVals[ctzIndex])
+		freeidx := s.freeindex // help the pre ssa compiler out here with cse.
+		result := freeidx + uintptr(theBit)
+		if result < s.nelems {
+			s.allocCache >>= (theBit + 1)
+			freeidx = result + 1
+			if freeidx%64 == 0 && freeidx != s.nelems {
+				// We just incremented s.freeindex so it isn't 0 so we are moving to the next aCache
+				whichByte := freeidx / 8
+				s.refillAllocCache(whichByte)
+			}
+			s.freeindex = freeidx
+			v := gclinkptr(result*s.elemsize + s.base())
+			s.allocCount++
+			return v
+		}
+	}
+	return 0
+}
+
 // nextFree returns the next free object from the cached span if one is available.
 // Otherwise it refills the cache with a span with an available object and
 // returns that object along with a flag indicating that this was a heavy
@@ -648,7 +674,10 @@ func mallocgc(size uintptr, typ *_type, flags uint32) unsafe.Pointer {
 			}
 			// Allocate a new maxTinySize block.
 			var v gclinkptr
-			v, shouldhelpgc = c.nextFree(tinySizeClass)
+			v = c.nextFreeFast(tinySizeClass)
+			if v == 0 {
+				v, shouldhelpgc = c.nextFree(tinySizeClass)
+			}
 			x = unsafe.Pointer(v)
 			(*[2]uint64)(x)[0] = 0
 			(*[2]uint64)(x)[1] = 0
@@ -668,7 +697,10 @@ func mallocgc(size uintptr, typ *_type, flags uint32) unsafe.Pointer {
 			}
 			size = uintptr(class_to_size[sizeclass])
 			var v gclinkptr
-			v, shouldhelpgc = c.nextFree(sizeclass)
+			v = c.nextFreeFast(sizeclass)
+			if v == 0 {
+				v, shouldhelpgc = c.nextFree(sizeclass)
+			}
 			x = unsafe.Pointer(v)
 			if flags&flagNoZero == 0 {
 				//RLHXXX				v.ptr().next = 0
@@ -727,6 +759,20 @@ func mallocgc(size uintptr, typ *_type, flags uint32) unsafe.Pointer {
 		systemstack(func() {
 			gcmarknewobject_m(uintptr(x), size)
 		})
+	}
+
+	// The object x is about to be reused but tracefree and msanfree
+	// need to be informed that the previous
+	// It is quite possible that this object is being allocated out of a
+	// fresh span and that there is no preceeding call to racemalloc with
+	// this object. If this is an issue then initialization of the
+	// fresh span needs to leave some crumbs around that can be used to
+	// avoid these calls.
+	if debug.allocfreetrace != 0 {
+		tracefree(unsafe.Pointer(x), size)
+	}
+	if msanenabled {
+		msanfree(unsafe.Pointer(x), size)
 	}
 
 	if raceenabled {
