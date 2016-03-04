9af02b667d513d511afe4c3d02ee0d5a29267aab
Rhys Hiltner <rhys@justin.tv>
2016-03-03 22:37:14 -0800

    cmd/link/internal/ld: don't panic on short buildid
    When the linker was written in C, command line arguments were passed
    around as null-terminated byte arrays which encouraged checking
    characters one at a time. In Go, that can easily lead to
    out-of-bounds panics.
    
    Use the more idiomatic strings.HasPrefix when checking cmd/link's -B
    argument to avoid the panic, and replace the manual hex decode with
    use of the encoding/hex package.
    
    Fixes #14636
    
    Change-Id: I45f765bbd8cf796fee1a9a3496178bf76b117827

---
+++ test/fixedbugs/issue14636.go
@@ -0,0 +1,34 @@
+// +build !nacl,!android,!darwin darwin,!arm
+// run
+
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bytes"
+	"log"
+	"os/exec"
+)
+
+func main() {
+	run("go", "tool", "link", "-B", "")     // -B argument must start with 0x:
+	run("go", "tool", "link", "-B", "0")    // -B argument must start with 0x: 0
+	run("go", "tool", "link", "-B", "0x")   // usage: link [options] main.o
+	run("go", "tool", "link", "-B", "0x0")  // -B argument must have even number of digits: 0x0
+	run("go", "tool", "link", "-B", "0x00") // usage: link [options] main.o
+}
+
+func run(name string, args ...string) {
+	cmd := exec.Command(name, args...)
+	out, err := cmd.CombinedOutput()
+	if err == nil {
+		log.Fatalf("expected cmd/link to fail")
+	}
+
+	if bytes.HasPrefix(out, []byte("panic")) {
+		log.Fatalf("cmd/link panicked:\n%s", out)
+	}
+}
