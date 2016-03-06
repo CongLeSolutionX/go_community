b836b5f14d99af76a11033fa451eb251d0914158
Quentin Renard <contact@asticode.com>
2016-03-06 17:27:50 +0100

    net/http: improve performance for parsePostForm
    Remove the use of io.ReadAll in http.parsePostForm to avoid converting
    the whole input from []byte to string and not performing well
    space-allocated-wise.
    
    Instead a new function called parsePostFormURLEncoded is used and is
    fed directly an io.Reader that is parsed using a bufio.Reader.
    
    Benchmark:
    
    name         old time/op    new time/op    delta
    PostQuery-4    3.01µs ± 1%    2.61µs ± 3%  -13.33%  (p=0.029 n=4+4)
    
    name         old alloc/op   new alloc/op   delta
    PostQuery-4    1.04kB ± 0%    0.53kB ± 0%  -49.11%  (p=0.029 n=4+4)
    
    name         old allocs/op  new allocs/op  delta
    PostQuery-4      6.00 ± 0%      5.00 ± 0%  -16.67%  (p=0.029 n=4+4)
    
    Fixes #14655
    
    Change-Id: I112c263d4221d959ed6153cfe88bc57a2aa8ea73

--- src/net/http/request.go
+++ src/net/http/request.go
@@ -1015,18 +1015,8 @@ func parsePostForm(r *Request) (vs url.Values, err error) {
 			maxFormSize = int64(10 << 20) // 10 MB is a lot of text.
 			reader = io.LimitReader(r.Body, maxFormSize+1)
 		}
-		b, e := ioutil.ReadAll(reader)
-		if e != nil {
-			if err == nil {
-				err = e
-			}
-			break
-		}
-		if int64(len(b)) > maxFormSize {
-			err = errors.New("http: POST too large")
-			return
-		}
-		vs, e = url.ParseQuery(string(b))
+		vs = make(url.Values)
+		e := parsePostFormURLEncoded(reader, vs, maxFormSize)
 		if err == nil {
 			err = e
 		}
@@ -1039,6 +1029,81 @@ func parsePostForm(r *Request) (vs url.Values, err error) {
 		// in TestParseMultipartFormOrder and others.
 	}
 	return
+}
+
+// parsePostFormURLEncoded reads from r, the reader of a POST form to populate vs which is a url-type values.
+// maxFormSize indicates the maximum number of bytes that will be read from r.
+func parsePostFormURLEncoded(r io.Reader, vs url.Values, maxFormSize int64) error {
+	// Create a bufio.Reader from net/http bufioReaderPool sync.Pool
+	br := newBufioReader(r)
+	defer putBufioReader(br)
+	var delim byte
+	var key string
+	var readSize int64
+	for {
+		// Set delimiter
+		if delim != byte('=') {
+			delim = byte('=')
+		} else {
+			delim = byte('&')
+		}
+
+		// Read until next delimiter
+		b, err := br.ReadBytes(delim)
+		if err != nil && err != io.EOF && err != bufio.ErrBufferFull {
+			return err
+		}
+		readSize += int64(len(b))
+		if readSize >= maxFormSize {
+			return errors.New("http: POST too large")
+		}
+
+		// Remove delimiter
+		if len(b) > 0 && b[len(b)-1] == delim {
+			b = b[:len(b)-1]
+		}
+
+		// Check for orphan keys
+		if delim == byte('=') && bytes.Contains(b, []byte("&")) {
+			// Split on "&"
+			split := bytes.Split(b, []byte("&"))
+
+			// Store orphan keys
+			for i := 0; i < len(split)-1; i++ {
+				// Query unescape orphan key
+				key, err2 := url.QueryUnescape(string(split[i]))
+				if err2 != nil {
+					return err
+				}
+
+				// Store key
+				vs[key] = append(vs[key], "")
+			}
+
+			// Update slice
+			b = split[len(split)-1]
+		}
+
+		// Query unescape read value
+		s, err2 := url.QueryUnescape(string(b))
+		if err2 != nil {
+			return err
+		}
+
+		// Store key or populate vs
+		if delim == byte('=') {
+			key = s
+		} else {
+			vs[key] = append(vs[key], s)
+			key = ""
+		}
+
+		// Check for end of reader
+		if err == io.EOF {
+			break
+		}
+	}
+	return nil
 }
 
 // ParseForm parses the raw query from the URL and updates r.Form.
