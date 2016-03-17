25fc04d1afbbc249cbf73cc540a7027f6fd7e027
Michel Lespinasse <walken@google.com>
2016-03-17 06:18:13 -0700

    cmd/compile: emit itabs and itablinks
    See #14874
    
    This change tells the compiler to emit itab and itablink symbols in
    situations where they could be useful; however the compiled code does
    not actually make use of the new symbols yet.
    
    Change-Id: I0db3e6ec0cb1f3b7cebd4c60229e4a48372fe586

--- src/cmd/compile/internal/gc/walk.go
+++ src/cmd/compile/internal/gc/walk.go
@@ -997,6 +997,10 @@ opswitch:
 			break
 		}
 
+		if isdirectiface(n.Left.Type) {
+			itabnamesym(n.Left.Type, n.Type)
+		}
+
 		var ll []*Node
 		if !Isinter(n.Left.Type) {
 			ll = append(ll, typename(n.Left.Type))
