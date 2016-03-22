98049271e6203b5f84f0620b10aaa1f48b4e7e34
Klaus Post <klauspost@gmail.com>
2016-03-22 10:11:48 +0100

    compress/flate: replace "Best Speed" with specialized version
    This replaces level 1 with a specialized versions that
    trades a typical typical reduction loss about 1-2%
    for a 1.5x throughput increase.
    
    Discussion: https://groups.google.com/forum/#!topic/golang-dev/XYgHX9p8IOk
    
    Compared to master:
    
    name                    old time/op    new time/op    delta
    EncodeDigitsSpeed1e4-8     302µs ± 2%     156µs ± 1%   -48.40%   (p=0.000 n=10+9)
    EncodeDigitsSpeed1e5-8    3.60ms ± 1%    1.48ms ± 3%   -58.77%  (p=0.000 n=10+10)
    EncodeDigitsSpeed1e6-8    37.3ms ± 1%    14.8ms ± 2%   -60.33%   (p=0.000 n=9+10)
    EncodeTwainSpeed1e4-8      292µs ± 1%     178µs ± 1%   -39.17%    (p=0.000 n=9+8)
    EncodeTwainSpeed1e5-8     2.56ms ± 1%    1.53ms ± 2%   -40.29%  (p=0.000 n=10+10)
    EncodeTwainSpeed1e6-8     25.2ms ± 2%    15.2ms ± 1%   -39.72%  (p=0.000 n=10+10)
    
    name                    old speed      new speed      delta
    EncodeDigitsSpeed1e4-8  33.1MB/s ± 2%  64.2MB/s ± 1%   +93.79%   (p=0.000 n=10+9)
    EncodeDigitsSpeed1e5-8  27.8MB/s ± 1%  67.4MB/s ± 3%  +142.59%  (p=0.000 n=10+10)
    EncodeDigitsSpeed1e6-8  26.8MB/s ± 1%  67.6MB/s ± 2%  +152.11%   (p=0.000 n=9+10)
    EncodeTwainSpeed1e4-8   34.2MB/s ± 1%  56.2MB/s ± 1%   +64.38%    (p=0.000 n=9+8)
    EncodeTwainSpeed1e5-8   39.0MB/s ± 1%  65.3MB/s ± 2%   +67.48%  (p=0.000 n=10+10)
    EncodeTwainSpeed1e6-8   39.7MB/s ± 2%  65.8MB/s ± 1%   +65.90%  (p=0.000 n=10+10)
    
    Complex benchmarks:
    
    https://docs.google.com/spreadsheets/d/1VLxi-ac0BAtf735HyH3c1xRulbkYYUkFecKdLPH7NIQ/edit?usp=sharing
    
    Summary:
    
    Web content:     1.5% less compression reduction. 1.5x speedup.
    Go object files: 0.9% less compression reduction. 1.2x speedup.
    Go source files: 1.2% less compression reduction. 1.5x speedup.
    Disk Image:      1.6% less compression reduction. 1.7x speedup.
    JSON stream:     0.4% less compression reduction. 0.9x speedup.
    enwik9:          2.1% less compression reduction. 1.4x speedup.
    gob stream:      0.8% less compression reduction. 1.1x speedup.
    10gb:            1.9% less compression reduction. 2.2x speedup.
    random data:     0.0% compression improvement.    5.1x speedup.
    
    Change-Id: I04a80354263d7ea937d5c85dc43478df603d2037

---
+++ src/compress/flate/deflatefast.go
@@ -0,0 +1,164 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package flate
+
+// This encoding algorithm, which prioritizes speed over encoding size,
+// is based on Snappy LZ77 matcher: github.com/golang/snappy
+
+const maxOffset = 1 << logMaxOffsetSize // Maximum deflate offset
+const tableBits = 16                    // Bits used in the table
+const tableSize = 1 << tableBits        // Size of the table
+
+func newEncState() *encState {
+	return &encState{cur: 1}
+}
+
+// encState maintains the table for matches
+// and a copy of the previous block.
+type encState struct {
+	table [tableSize]uint32
+	block [maxStoreBlockSize]byte
+	prev  []byte
+	cur   int
+}
+
+// Encode encodes a block of bytes, and outputs a token
+// representation of the input.
+func (e *encState) encode(dst []token, src []byte) []token {
+	// Return early if src is short.
+	if len(src) <= 4 {
+		if len(src) != 0 {
+			dst = emitLiteral(dst, src)
+		}
+		e.cur += len(src)
+		e.prev = nil
+		return dst
+	}
+
+	// Ensure that e.cur doesn't wrap, mainly an issue on 32 bits.
+	if e.cur > 1<<30 {
+		e.cur = 1
+	}
+
+	var s int   // The iterator position.
+	var t int   // The last position with the same hash as s.
+	var lit int // The start position of any pending literal bytes.
+
+	for s+3 < len(src) {
+		srcS := src[s : s+4]
+		// Update the hash table.
+		h := uint32(srcS[0]) | uint32(srcS[1])<<8 | uint32(srcS[2])<<16 | uint32(srcS[3])<<24
+		p := &e.table[(h*0x1e35a7bd)>>(32-tableBits)]
+		// We need to to store values in [-1, inf) in table.
+		// To save some initialization time, we make sure that
+		// e.cur is always > 0.
+		t, *p = int(*p)-e.cur, uint32(s+e.cur)
+
+		// If t is positive, the match starts in the current block
+		if t >= 0 {
+			offset := uint(s - t - 1)
+			// Check that the offset is valid and that we match at least 4 bytes
+			if offset >= (maxOffset - 1) {
+				s += 1 + ((s - lit) >> 5)
+				continue
+			}
+			srcT := src[t : t+4]
+			if srcS[0] != srcT[0] || srcS[1] != srcT[1] || srcS[2] != srcT[2] || srcS[3] != srcT[3] {
+				s += 1 + ((s - lit) >> 5)
+				continue
+			}
+			// Otherwise, we have a match. First, emit any pending literal bytes.
+			if lit != s {
+				dst = emitLiteral(dst, src[lit:s])
+			}
+			// Extend the match to be as long as possible.
+			s0 := s
+			s1 := s + maxMatchLength
+			if s1 > len(src) {
+				s1 = len(src)
+			}
+			s, t = s+4, t+4
+			for s < s1 && src[s] == src[t] {
+				s++
+				t++
+			}
+			// Emit the matched bytes.
+			dst = append(dst, matchToken(uint32(s-s0-3), uint32(s-t-minOffsetSize)))
+			lit = s
+			continue
+		}
+		// We found a match in the previous block.
+		tp := len(e.prev) + t
+		if tp < 0 || t > -5 || s-t >= maxOffset {
+			s += 1 + ((s - lit) >> 5)
+			continue
+		}
+		ep := e.prev[tp : tp+4]
+		if srcS[0] != ep[0] || srcS[1] != ep[1] || srcS[2] != ep[2] || srcS[3] != ep[3] {
+			s += 1 + ((s - lit) >> 5)
+			continue
+		}
+		// Otherwise, we have a match. First, emit any pending literal bytes.
+		if lit != s {
+			dst = emitLiteral(dst, src[lit:s])
+		}
+		// Extend the match to be as long as possible.
+		s0 := s
+		s1 := s + maxMatchLength
+		if s1 > len(src) {
+			s1 = len(src)
+		}
+		s, tp = s+4, tp+4
+	loop:
+		for s < s1 && src[s] == e.prev[tp] {
+			s++
+			tp++
+			if tp == len(e.prev) {
+				t = 0
+				// continue in current buffer
+				for s < s1 && src[s] == src[t] {
+					s++
+					t++
+				}
+				break loop
+			}
+		}
+		// Emit the copied bytes.
+		if t < 0 {
+			t = tp - len(e.prev)
+		}
+		dst = append(dst, matchToken(uint32(s-s0-3), uint32(s-t-minOffsetSize)))
+		lit = s
+	}
+
+	// Emit any final pending literal bytes and return.
+	if lit != len(src) {
+		dst = emitLiteral(dst, src[lit:])
+	}
+	e.cur += len(src)
+
+	// Store this block, if it was full length.
+	if len(src) == maxStoreBlockSize {
+		copy(e.block[:], src)
+		e.prev = e.block[:]
+	} else {
+		e.prev = nil
+	}
+
+	return dst
+}
+
+// emitLiteral writes a literal chunk and returns the number of bytes written.
+func emitLiteral(dst []token, lit []byte) []token {
+	for _, v := range lit {
+		dst = append(dst, token(v))
+	}
+	return dst
+}
+
+// Reset resets the encoding state.
+func (e *encState) Reset() {
+	e.prev = nil
+}
