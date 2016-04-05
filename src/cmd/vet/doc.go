a0317529a5aa5739afca6604a00e338e767566d3
Aliaksandr Valialkin <valyala@gmail.com>
2016-04-05 18:42:07 +0300

    icmd/vet: improved checking for variadic Println-like functions
    - Automatically determine the first argument to check.
    - Skip checking matching non-variadic functions.
    - Skip checking matching functions accepting non-interface{}
      variadic arguments.
    - Removed fragile 'magic' code for special cases such as math.Log
      and error interface.
    
    Fixes #15067
    Fixes #15099
    
    Change-Id: Ib313557f18b12b36daa493f4b02c598b9503b55b

--- src/cmd/vet/doc.go
+++ src/cmd/vet/doc.go
@@ -189,12 +189,7 @@ These flags configure the behavior of vet:
 		Verbose mode
 	-printfuncs
 		A comma-separated list of print-like functions to supplement the
-		standard list.  Each entry is in the form Name:N where N is the
-		zero-based argument position of the first argument involved in the
-		print: either the format or the first print argument for non-formatted
-		prints.  For example, if you have Warn and Warnf functions that
-		take an io.Writer as their first argument, like Fprintf,
-			-printfuncs=Warn:1,Warnf:1
+		standard list.
 		For more information, see the discussion of the -printf flag.
 	-shadowstrict
 		Whether to be strict about shadowing; can be noisy.
