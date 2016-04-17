349da0d25912221eadcddc06e5465bf5df1f9c84
Austin Clements <austin@google.com>
2016-04-17 11:42:37 -0400

    runtime: simplify/optimize allocate-black a bit
    Currently allocating black switches to the system stack (which is
    probably a historical accident) and atomically updates the global
    bytes marked stat. Since we're about to depend on this much more,
    optimize it a bit by putting it back on the regular stack and updating
    the per-P bytes marked stat, which gets lazily folded into the global
    bytes marked stat.
    
    Change-Id: Ibbe16e5382d3fd2256e4381f88af342bf7020b04

--- src/runtime/malloc.go
+++ src/runtime/malloc.go
@@ -707,9 +707,7 @@ func mallocgc(size uintptr, typ *_type, flags uint32) unsafe.Pointer {
 	// This may be racing with GC so do it atomically if there can be
 	// a race marking the bit.
 	if gcphase == _GCmarktermination || gcBlackenPromptly {
-		systemstack(func() {
-			gcmarknewobject_m(uintptr(x), size, scanSize)
-		})
+		gcmarknewobject(uintptr(x), size, scanSize)
 	}
 
 	if raceenabled {
