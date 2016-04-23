8dc59e96944e4012902f3ce56832c09ce59d863d
Aliaksandr Valialkin <valyala@gmail.com>
2016-04-23 21:00:38 +0300

    cmd/vet: improve checking unkeyed fields in composite literals
    - Simplified the code.
    
    - Removed types for slice aliases from composite literals' whitelist, since they
    are properly handled by vet.
    
    - Added missing types to whitelist, which generate the majority
    of 'composite literal uses unkeyed fields' warnings in standard go packages,
    so it is unlikely they will be fixed soon.
    
    Fixes #15408
    Updates #9171
    Updates #11041
    
    Change-Id: Ia1806c9eb3f327c09d2e28da4ffdb233b5a159b0

--- src/cmd/vet/composite.go
+++ src/cmd/vet/composite.go
@@ -10,6 +10,7 @@ import (
 	"cmd/vet/internal/whitelist"
 	"flag"
 	"go/ast"
+	"go/types"
 	"strings"
 )
 
@@ -25,102 +26,65 @@ func init() {
 // checkUnkeyedLiteral checks if a composite literal is a struct literal with
 // unkeyed fields.
 func checkUnkeyedLiteral(f *File, node ast.Node) {
-	c := node.(*ast.CompositeLit)
-	typ := c.Type
+	cl := node.(*ast.CompositeLit)
+
+	typ := f.pkg.types[cl].Type
+	if typ == nil {
+		// Cannot determine composite literals' type. Skip it.
+		return
+	}
+	typeName := typ.String()
+	if *compositeWhiteList && whitelist.UnkeyedLiteral[typeName] {
+		// Skip whitelisted types.
+		return
+	}
 	for {
-		if typ1, ok := c.Type.(*ast.ParenExpr); ok {
-			typ = typ1
-			continue
+		underTyp := typ.Underlying()
+		if types.Identical(typ, underTyp) {
+			break
 		}
-		break
+		typ = underTyp
 	}
-
-	switch typ.(type) {
-	case *ast.ArrayType:
-		return
-	case *ast.MapType:
-		return
-	case *ast.StructType:
-		return // a literal struct type does not need to use keys
-	case *ast.Ident:
-		// A simple type name like t or T does not need keys either,
-		// since it is almost certainly declared in the current package.
-		// (The exception is names being used via import . "pkg", but
-		// those are already breaking the Go 1 compatibility promise,
-		// so not reporting potential additional breakage seems okay.)
+	if _, ok := typ.(*types.Struct); !ok {
+		// Skip non-struct composite literals.
 		return
 	}
-
-	// Otherwise the type is a selector like pkg.Name.
-	// We only care if pkg.Name is a struct, not if it's a map, array, or slice.
-	isStruct, typeString := f.pkg.isStruct(c)
-	if !isStruct {
-		return
-	}
-
-	if typeString == "" { // isStruct doesn't know
-		typeString = f.gofmt(typ)
-	}
-
-	// It's a struct, or we can't tell it's not a struct because we don't have types.
 
 	// Check if the CompositeLit contains an unkeyed field.
 	allKeyValue := true
-	for _, e := range c.Elts {
+	for _, e := range cl.Elts {
 		if _, ok := e.(*ast.KeyValueExpr); !ok {
-			if cl, ok := e.(*ast.CompositeLit); !ok || cl.Type != nil {
-				allKeyValue = false
-				break
-			}
+			allKeyValue = false
+			break
 		}
 	}
 	if allKeyValue {
+		// All the composite literal fields are keyed.
 		return
 	}
 
-	// Check that the CompositeLit's type has the form pkg.Typ.
-	s, ok := c.Type.(*ast.SelectorExpr)
-	if !ok {
-		return
-	}
-	pkg, ok := s.X.(*ast.Ident)
-	if !ok {
+	if isLocalType(f, typeName) {
+		// Allow unkeyed locally defined composite literals.
 		return
 	}
 
-	// Convert the package name to an import path, and compare to a whitelist.
-	path := pkgPath(f, pkg.Name)
-	if path == "" {
-		f.Badf(c.Pos(), "unresolvable package for %s.%s literal", pkg.Name, s.Sel.Name)
-		return
-	}
-	typeName := path + "." + s.Sel.Name
-	if *compositeWhiteList && whitelist.UnkeyedLiteral[typeName] {
-		return
-	}
-
-	f.Bad(c.Pos(), typeString+" composite literal uses unkeyed fields")
+	f.Badf(cl.Pos(), "%s composite literal uses unkeyed fields", typeName)
 }
 
-// pkgPath returns the import path "image/png" for the package name "png".
-//
-// This is based purely on syntax and convention, and not on the imported
-// package's contents. It will be incorrect if a package name differs from the
-// leaf element of the import path, or if the package was a dot import.
-func pkgPath(f *File, pkgName string) (path string) {
-	for _, x := range f.file.Imports {
-		s := strings.Trim(x.Path.Value, `"`)
-		if x.Name != nil {
-			// Catch `import pkgName "foo/bar"`.
-			if x.Name.Name == pkgName {
-				return s
-			}
-		} else {
-			// Catch `import "pkgName"` or `import "foo/bar/pkgName"`.
-			if s == pkgName || strings.HasSuffix(s, "/"+pkgName) {
-				return s
-			}
-		}
+func isLocalType(f *File, typeName string) bool {
+	if strings.HasPrefix(typeName, "struct{") {
+		// Anonymous type is local.
+		return true
 	}
-	return ""
+
+	pkgname := f.pkg.path
+	if strings.HasPrefix(typeName, pkgname+".") {
+		return true
+	}
+
+	// Treat types as local inside test packages with _test name suffix.
+	if strings.HasSuffix(pkgname, "_test") {
+		pkgname = pkgname[:len(pkgname)-len("_test")]
+	}
+	return strings.HasPrefix(typeName, pkgname+".")
 }
