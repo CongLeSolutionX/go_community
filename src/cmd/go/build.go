1c0bead5312d88aa7360a26cccdfca42f86f1300
Jess Frazelle <me@jessfraz.com>
2016-05-20 16:17:27 -0700

    cmd/go: updates go get to return exit status 0 for test file only pkgs
    Updates the behavior of `go get` to return exit status 0 when a
    requested package only contains test files.
    
    Fixes #15093
    
    Change-Id: I76b80517d58748090f5e8c6f41178361e2d7ca54

--- src/cmd/go/build.go
+++ src/cmd/go/build.go
@@ -1279,9 +1279,12 @@ func (b *builder) do(root *action) {
 		if err != nil {
 			if err == errPrintedOutput {
 				setExitStatus(2)
+			} else if _, ok := err.(*build.NoGoError); ok && len(a.p.TestGoFiles) > 0 {
+				// Ignore the "no buildable Go source files" error for a package with only test files.
 			} else {
 				errorf("%s", err)
 			}
+
 			a.failed = true
 		}
 
@@ -1365,7 +1368,7 @@ func (b *builder) build(a *action) (err error) {
 	}
 
 	defer func() {
-		if err != nil && err != errPrintedOutput {
+		if _, ok := err.(*build.NoGoError); !ok && err != nil && err != errPrintedOutput {
 			err = fmt.Errorf("go build %s: %v", a.p.ImportPath, err)
 		}
 	}()
