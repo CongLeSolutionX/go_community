875a32666969dc82267a573015aa2085181a3e92
Keith Randall <khr@golang.org>
2016-05-24 15:43:25 -0700

    cmd/compile: log rules to a file for rule coverage tool
    When rules are generated with -log, log rule application to a file.
    
    The file is opened in append mode so multiple calls to the compiler
    union their logs.
    
    Dedup rule logging so we don't get so much data.
    
    Change-Id: Ib35c7c85bf58e5909ea9231043f8cbaa6bf278b7

--- src/cmd/compile/internal/ssa/rewrite.go
+++ src/cmd/compile/internal/ssa/rewrite.go
@@ -7,6 +7,8 @@ package ssa
 import (
 	"fmt"
 	"math"
+	"os"
+	"path/filepath"
 )
 
 func applyRewrite(f *Func, rb func(*Block) bool, rv func(*Value, *Config) bool) {
@@ -357,3 +359,35 @@ func clobber(v *Value) bool {
 	// Note: leave v.Block intact.  The Block field is used after clobber.
 	return true
 }
+
+// ruleLog logs the use of the rule s. This will only be enabled if
+// rewrite rules were generated with the -log option, see gen/rulegen.go.
+func ruleLog(s string) {
+	// Remove duplicates. Otherwise, it is lots of data. If we want coverage
+	// weights, maybe we log until a certain number of repeats.
+	if ruleDups == nil {
+		ruleDups = map[string]struct{}{}
+	}
+	if _, ok := ruleDups[s]; ok {
+		return
+	}
+	ruleDups[s] = struct{}{}
+	if ruleFile == nil {
+		// Open a log file to write log to. We open in append
+		// mode because all.bash runs the compiler lots of times,
+		// and we want the concatenation of all of those logs.
+		// This means, of course, that users need to rm the old log
+		// to get fresh data.
+		// TODO: all.bash runs compilers in parallel. Need to synchronize logging somehow?
+		w, err := os.OpenFile(filepath.Join(os.Getenv("GOROOT"), "src", "rulelog"),
+			os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		if err != nil {
+			panic(err)
+		}
+		ruleFile = w
+	}
+	ruleFile.WriteString(s)
+}
+
+var ruleDups map[string]struct{}
+var ruleFile *os.File
