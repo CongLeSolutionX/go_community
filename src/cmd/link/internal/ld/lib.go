46850243e391c015aab52904e8b5c58aac18464c
David Crawshaw <crawshaw@golang.org>
2016-05-24 13:40:02 -0400

    cmd/link: ensure -fuse-ld=gold uses gold
    Fixes #15696
    
    Change-Id: I134e918dc56f79a72a04aa54f415371884113d2a

--- src/cmd/link/internal/ld/lib.go
+++ src/cmd/link/internal/ld/lib.go
@@ -1137,6 +1137,16 @@ func hostlink() {
 			//
 			// In both cases, switch to gold.
 			argv = append(argv, "-fuse-ld=gold")
+
+			// If gold is not installed, gcc will silently switch
+			// back to ld.bfd. So we parse the version information
+			// and provide a useful error if gold is missing.
+			cmd := exec.Command(extld, "-fuse-ld=gold", "-Wl,--version")
+			if out, err := cmd.CombinedOutput(); err != nil {
+				if !bytes.Contains(out, []byte("GNU gold")) {
+					log.Fatalf("external linker needs to be gold, but it is not: %s", out)
+				}
+			}
 		}
 	}
 
