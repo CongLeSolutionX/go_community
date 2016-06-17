1620ba33a5641b80399c1197537beb852e489f30
Josh Bleecher Snyder <josharian@gmail.com>
2016-06-17 12:28:31 -0700

    cmd/internal/obj: add opcode space safety check
    This CL adds a safety mechanism
    for changing the number of opcodes
    available per architecture.
    
    A subsequent CL will actually make the change.
    
    Change-Id: I6332ed5514f2f153c54d11b7da0cc8a6be1c8066

--- src/cmd/internal/obj/link.go
+++ src/cmd/internal/obj/link.go
@@ -304,7 +304,8 @@ const (
 	ABaseMIPS64
 	ABaseS390X
 
-	AMask = 1<<12 - 1 // AND with this to use the opcode as an array index.
+	AllowedOpCodes = 1 << 12   // The number of opcodes available for any given architecture.
+	AMask          = 1<<12 - 1 // AND with this to use the opcode as an array index.
 )
 
 // An LSym is the sort of symbol that is written to an object file.
