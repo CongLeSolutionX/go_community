55805723008bd78a112621cea5517897dc4036a1
Cherry Zhang <cherryyz@google.com>
2016-06-27 16:54:57 -0400

    [dev.ssa] cmd/compile: ensure alignment for Zero and Move in SSA for ARM
    Encode the size and the alignment into AuxInt of Zero and Move ops.
    On AMD64, we simply don't look at the alignment. On ARM and PPC64, we
    only generate aligned stores.
    
    Updates #15365.
    
    Change-Id: Ifdcc205c364f67c4516b9adebfe7d50d223b6863

--- src/cmd/compile/internal/ssa/op.go
+++ src/cmd/compile/internal/ssa/op.go
@@ -125,6 +125,34 @@ func (x ValAndOff) add(off int64) int64 {
 	return makeValAndOff(x.Val(), x.Off()+off)
 }
 
+// SizeAndAlign holds both the size and the alignment of a type,
+// used in Zero and Move ops.
+// The high 32 bits hold the alignment.
+// The low 32 bits hold the size.
+type SizeAndAlign int64
+
+func (x SizeAndAlign) Size() int64 {
+	return int64(int32(x))
+}
+func (x SizeAndAlign) Align() int64 {
+	return int64(x) >> 32
+}
+func (x SizeAndAlign) Int64() int64 {
+	return int64(x)
+}
+func (x SizeAndAlign) String() string {
+	return fmt.Sprintf("size=%d,align=%d", x.Size(), x.Align())
+}
+func MakeSizeAndAlign(size, align int64) SizeAndAlign {
+	if size != int64(int32(size)) {
+		panic("size too big in SizeAndAlign")
+	}
+	if align != int64(int32(align)) {
+		panic("alignment too big in SizeAndAlign")
+	}
+	return SizeAndAlign(size | align<<32)
+}
+
 func (op Op) isTupleGenerator() bool {
 	switch op {
 	case OpAdd32carry, OpSub32carry, OpMul32uhilo,
