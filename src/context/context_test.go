34d75c306cd4948ecc6cd1e050f46b04df6f579b
Jack Lindamood <jlindamo@justin.tv>
2016-07-15 13:28:27 -0700

    context: add test for WithDeadline() in the past
    Adds a test case for calling context.WithDeadline() where the deadline
    exists in the past.  This change increases the code coverage of the
    context package.
    
    Change-Id: Ib486bf6157e779fafd9dab2b7364cdb5a06be36e

--- src/context/context_test.go
+++ src/context/context_test.go
@@ -255,6 +255,9 @@ func TestDeadline(t *testing.T) {
 	o = otherContext{c}
 	c, _ = WithDeadline(o, time.Now().Add(4*time.Second))
 	testDeadline(c, "WithDeadline+otherContext+WithDeadline", 2*time.Second, t)
+
+	c, _ = WithDeadline(Background(), time.Now().Add(-time.Millisecond))
+	testDeadline(c, "WithDeadline+inthepast", time.Second, t)
 }
 
 func TestTimeout(t *testing.T) {
