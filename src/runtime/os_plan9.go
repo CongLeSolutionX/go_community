5087262a14ee47459bc1869bc2f80d3ae30dcf6e
Austin Clements <austin@google.com>
2016-07-18 21:40:02 -0400

    runtime: fetch physical page size from the OS
    DO NOT SUBMIT. For 1.8.
    
    Currently the physical page size assumed by the runtime is hard-coded.
    On Linux the runtime at least fetches the OS page size during init and
    sanity checks against the hard-coded value, but they may still differ.
    On other OSes we wouldn't even notice.
    
    Add support on all OSes to fetch the actual OS physical page size
    during runtime init and lift the sanity check of PhysPageSize from the
    Linux init code to general malloc init.
    
    Updates #12480 and #10180.
    
    Change-Id: I065f2834bc97c71d3208edc17fd990ec9058b6da

--- src/runtime/os_plan9.go
+++ src/runtime/os_plan9.go
@@ -217,6 +217,51 @@ func getproccount() int32 {
 	return ncpu
 }
 
+var devswap = []byte("/dev/swap\x00")
+var pagesize = []byte(" pagesize\n")
+
+func getPageSize() uintptr {
+	var buf [2048]byte
+	var pos int
+	fd := open(&devswap[0], _OREAD, 0)
+	if fd < 0 {
+		return 0
+	}
+	for pos < len(buf) {
+		n := read(fd, unsafe.Pointer(&buf[pos]), int32(len(buf)-pos))
+		if n <= 0 {
+			break
+		}
+		pos += int(n)
+	}
+	closefd(fd)
+	text := buf[:pos]
+	// Find "<n> pagesize" line.
+	bol := 0
+	for i, c := range text {
+		if c == '\n' {
+			bol = i + 1
+		}
+		if bytesHasPrefix(text[i:], pagesize) {
+			// Parse number at the beginning of this line.
+			return uintptr(_atoi(text[bol:]))
+		}
+	}
+	return 0
+}
+
+func bytesHasPrefix(s, prefix []byte) bool {
+	if len(s) < len(prefix) {
+		return false
+	}
+	for i, p := range prefix {
+		if s[i] != p {
+			return false
+		}
+	}
+	return true
+}
+
 var pid = []byte("#c/pid\x00")
 
 func getpid() uint64 {
