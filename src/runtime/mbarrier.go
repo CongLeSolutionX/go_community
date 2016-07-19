806fddf267ebdb8999f25e0656428813de838944
Rick Hudson <rlh@golang.org>
2016-07-19 15:05:00 -0400

    [dev.garbage] runtime: ROC write barrier code
    ROC (request oriented collector) employs a write barrier
    that maintains an invariant that all published objects
    have their allocation bit set. This CL implements the
    code that maintains this invariant.
    
    The high level approach is to examine the referenced
    object and the referent object holding the slot. If the
    referent has its allocation bit set and the reference
    does not then installing the reference in the referent
    is a publishing action. Before this can happen the
    publishing code transitively traces the unpublished
    object's references ensuring the are also published.
    The straight foward recursive implementation grows
    the stack at unfortunate times so we borrow much of
    the mark logic buffer data structures to implement
    a interative approach.
    
    After all reachable objects have been marked as public
    the reference is installed. It is important not to
    install the reference until the unpublished objects have
    had their mark bits set for two reasons:
    
    1) Unpublished objects are local and marking them
       before they are published does not require
       synchronization with other goroutines. It is a
       totally local operation.
    
    2) If we installed the reference before we did the
       transitive publishing then another goroutine could
       race ahead of the publisher, manipulate pointers,
       and break the algorithm.
    
    This code is not needed or turned on at the moment
    since it is only meaningful when used on concert with
    the rest of the ROC code.
    
    Change-Id: I747909432e65bb8417eb83333953581500eeb974

--- src/runtime/mbarrier.go
+++ src/runtime/mbarrier.go
@@ -99,11 +99,43 @@ import (
 // global during mark termination.
 //
 //go:nowritebarrierrec
+//go:systemstack
 func gcmarkwb_m(slot *uintptr, ptr uintptr) {
 	if writeBarrier.needed {
 		if ptr != 0 && inheap(ptr) {
 			shade(ptr)
 		}
+	}
+
+	if ptr == 0 {
+		return
+	}
+
+	// ROC: publish local ptrs being written into public slots.
+	if debug.gcroc >= 1 {
+		// local -> public
+		// local -> local
+		if !isPublic(uintptr(unsafe.Pointer(slot))) {
+			// If local slot then we are done.
+			return
+		}
+		// public -> public
+		if isPublic(ptr) {
+			// if ptr is public we are done
+			return
+		}
+
+		if inheap(ptr) {
+			// public -> local
+			// Turn into a public -> public
+			makePublic(ptr, spanOf(ptr))
+
+			publish(ptr)
+			if !isPublic(ptr) {
+				throw("published ptr but it is still not public")
+			}
+		}
+		return
 	}
 }
 
@@ -138,11 +170,11 @@ func writebarrierptr_nostore1(dst *uintptr, src uintptr) {
 // but if we do that, Go inserts a write barrier on *dst = src.
 //go:nosplit
 func writebarrierptr(dst *uintptr, src uintptr) {
-	*dst = src
 	if writeBarrier.cgo {
 		cgoCheckWriteBarrier(dst, src)
 	}
-	if !writeBarrier.needed {
+	if !writeBarrier.roc && !writeBarrier.needed {
+		*dst = src // We need to do the write barrier before the store so publication works.
 		return
 	}
 	if src != 0 && src < sys.PhysPageSize {
@@ -152,6 +184,7 @@ func writebarrierptr(dst *uintptr, src uintptr) {
 		})
 	}
 	writebarrierptr_nostore1(dst, src)
+	*dst = src // We need to do the write barrier before the store so publication works.
 }
 
 // Like writebarrierptr, but the store has already been applied.
@@ -161,7 +194,7 @@ func writebarrierptr_nostore(dst *uintptr, src uintptr) {
 	if writeBarrier.cgo {
 		cgoCheckWriteBarrier(dst, src)
 	}
-	if !writeBarrier.needed {
+	if !writeBarrier.needed && !writeBarrier.roc {
 		return
 	}
 	if src != 0 && src < sys.PhysPageSize {
@@ -171,8 +204,14 @@ func writebarrierptr_nostore(dst *uintptr, src uintptr) {
 }
 
 // typedmemmove copies a value of type t to dst from src.
+// before it does the memmove it must publish
 //go:nosplit
 func typedmemmove(typ *_type, dst, src unsafe.Pointer) {
+	if debug.gcroc >= 1 && isPublic(uintptr(dst)) && typ.kind&kindNoPointers == 0 {
+		// if dst is public we need to publish before we
+		// do the copy.
+		heapBitsBulkPublish(uintptr(src), uintptr(dst), typ.size)
+	}
 	memmove(dst, src, typ.size)
 	if writeBarrier.cgo {
 		cgoCheckMemmove(typ, dst, src, 0, typ.size)
@@ -196,7 +235,7 @@ func reflect_typedmemmovepartial(typ *_type, dst, src unsafe.Pointer, off, size 
 	if writeBarrier.cgo {
 		cgoCheckMemmove(typ, dst, src, off, size)
 	}
-	if !writeBarrier.needed || typ.kind&kindNoPointers != 0 || size < sys.PtrSize {
+	if (!writeBarrier.needed && !writeBarrier.roc) || typ.kind&kindNoPointers != 0 || size < sys.PtrSize {
 		return
 	}
 
@@ -216,7 +255,7 @@ func reflect_typedmemmovepartial(typ *_type, dst, src unsafe.Pointer, off, size 
 // the copy and write barrier appear atomic to GC.
 //go:nosplit
 func callwritebarrier(typ *_type, frame unsafe.Pointer, framesize, retoffset uintptr) {
-	if !writeBarrier.needed || typ == nil || typ.kind&kindNoPointers != 0 || framesize-retoffset < sys.PtrSize {
+	if (!writeBarrier.needed && !writeBarrier.roc) || typ == nil || typ.kind&kindNoPointers != 0 || framesize-retoffset < sys.PtrSize {
 		return
 	}
 	heapBitsBulkBarrier(uintptr(add(frame, retoffset)), framesize-retoffset)
@@ -255,7 +294,7 @@ func typedslicecopy(typ *_type, dst, src slice) int {
 	// compiler only emits calls to typedslicecopy for types with pointers,
 	// and growslice and reflect_typedslicecopy check for pointers
 	// before calling typedslicecopy.
-	if !writeBarrier.needed {
+	if !writeBarrier.needed && !writeBarrier.roc {
 		memmove(dstp, srcp, uintptr(n)*typ.size)
 		return n
 	}
