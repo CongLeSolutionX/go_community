bf1c5f7404710a0559c203da88a1e0455286df33
Josh Bleecher Snyder <josharian@gmail.com>
2016-07-21 09:02:13 -0700

    syscall: unify unix/amd64 asm implementations
    DO NOT REVIEW
    
    [code freeze]
    
    Updates #11041
    
    Change-Id: I77e5ca0b61ffc530ee46848721a177867c81d548

--- src/syscall/asm_unix_amd64.s
+++ src/syscall/asm_unix_amd64.s
@@ -1,3 +1,8 @@
+// +build netbsd
+// +build freebsd
+// +build openbsd
+// +build dragonfly
+
 // Copyright 2009 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
@@ -9,7 +14,7 @@
 #include "funcdata.h"
 
 //
-// System call support for AMD64, NetBSD
+// System call support for AMD64 unixes
 //
 
 // func Syscall(trap int64, a1, a2, a3 int64) (r1, r2, err int64);
