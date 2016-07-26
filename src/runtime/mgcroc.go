f126074a3dd1b9b9a9b17625f9e34c8d62e25d69
Rick Hudson <rlh@golang.org>
2016-07-26 13:55:36 -0400

    [dev.garbage] runtime: recycle local objects in a ROC epoch
    There are three basic operations involved in the ROC algorithm.
    This CL provides recycleG. The other two are publishG and startG
    are introduced in other CLs.
    
    recycleG returns all local objects in a ROC epoch to the runtime
    logic to be reused by the allocator. recycleG is called when
    the goroutine associated with a ROC epoch is being exited and
    the local objects will no longer be reachable. It can also be
    called when all local object reachable from a goroutine that
    is being preempted have been made public.
    
    When the goroutine is restarted the reachable objects will
    still be available but the unreachable local ones will have
    been recycled.
    
    Change-Id: I5c8235ca20478a91741acded3f84ed31b768ac4b

--- src/runtime/mgcroc.go
+++ src/runtime/mgcroc.go
@@ -21,6 +21,11 @@
 
 package runtime
 
+import (
+	"runtime/internal/atomic"
+	"unsafe"
+)
+
 // startG establishes a new ROC epoch associated with the
 // currently running G.
 // The epoch associated with any previous G will be discarded.
@@ -135,4 +140,172 @@ func (c *mcache) publishG() {
 	c.tiny = 0
 	c.tinyoffset = 0
 	releasem(mp)
+}
+
+// rocTrace tracks the bytes ROC recovers.
+type rocTrace struct {
+	recoveredBytes    uint64
+	recoveredBytesAll uint64
+}
+
+var rocData rocTrace
+
+// recycleG recycles spans that were used for allocation in the
+// ROC epoch that is ending. The span's allocBits reflect whether
+// an object is public or local. Objects that have become public
+// since the start of the ROC epoch have been marked.
+// Local objects that are now no longer reachable will have
+// a clear allocBit and be available for allocation.
+// The actual recycle is done by setting each spans freeindex
+// back to the startindex associated with the span.
+// The caller must have done an acquirem so this routine can't
+// switch Ps.
+func (c *mcache) recycleG() {
+	if !writeBarrier.roc {
+		throw("in recycleNormal but writeBarrier.roc is false")
+	}
+	_g_ := getg().m.curg
+	recycleValid := _g_ != nil && _g_.rocvalid && _g_.rocgcnum == memstats.numgc && isGCoff()
+
+	if !recycleValid {
+		systemstack(c.publishG)
+		if _g_ != nil {
+			_g_.rocvalid = false // reset in startG
+			_g_.rocgcnum = 0
+		}
+		return
+	}
+
+	if c.rocgoid != _g_.goid {
+		println("c.rocgoid=", c.rocgoid, "_g_.goid=", _g_.goid)
+		throw("c.rocgoid != _g_.goid")
+	}
+
+	// Count of the number of objects recovered using ROC
+	recoveredBytes := int64(0)
+	for i := range c.alloc {
+		if c.alloc[i] == &emptymspan {
+			continue
+		}
+		if !c.alloc[i].incache {
+			throw("c.alloc[i].incache should be true")
+		}
+		next := c.alloc[i]
+		for s := next; s != nil; s = next {
+			next = s.nextUsedSpan
+			s.nextUsedSpan = nil
+			if s == &emptymspan {
+				throw("s == &emptymspan")
+			}
+			if s == nil {
+				throw("s is == for some reason.")
+			}
+			if s.elemsize == 0 {
+				throw("s.elemsize == 0")
+			}
+			if s.allocBits == nil {
+				throw("s.allocBits == nil")
+			}
+			if s != c.alloc[i] && s.incache {
+				println("runtime: c.alloc[i].base()=", hex(c.alloc[i].base()),
+					"runtime: s.base()=", hex(s.base()))
+				// only the first span is considered in an mcache
+				throw("recycleG encounters span that should not be incache")
+			}
+
+			// As an optimization move s.startindex past all objects that are now public
+			for ii := s.startindex; ii < s.freeindex; ii++ {
+				if s.isFree(ii) {
+					break
+				}
+				s.startindex++ // no sense in rolling back over public objects, set startindex and then freeindex to first free object.
+			}
+
+			for ii := s.startindex; ii < s.freeindex; ii++ {
+				if s.isFree(ii) {
+					recoveredBytes += int64(s.elemsize)
+				}
+			}
+
+			s.smashDebugHelper() // this increases the chance of triggering a bug
+
+			s.freeindex = s.startindex // The actual recycle step.
+			s.rollbackCount++
+			s.rollbackAllocCount()
+			if s.freeindex == s.nelems {
+				s.allocCache = 0 // Clear it since this span is full.
+			} else {
+				// Reset allocCache
+				if s.freeindex > s.nelems {
+					throw("s.freeindx > s.nelems")
+				}
+				freeByteBase := s.freeindex &^ (64 - 1)
+				whichByte := freeByteBase / 8
+				if whichByte > s.nelems/8 {
+					throw("whichByte > s.nelems / 8")
+				}
+				s.refillAllocCache(whichByte)
+				// adjust the allocCache so that s.freeindex corresponds to the low bit in
+				// s.allocCache
+				s.allocCache >>= s.freeindex % 64
+			}
+			// If this span is not the active alloc span
+			// either free it if has no alloced objects or simply uncache
+			// it if it has available space for new objects.
+			if s != c.alloc[i] {
+				mheap_.central[i].mcentral.releaseROCSpan(s)
+			}
+		}
+	}
+
+	// Large objects, one per span.
+	// abort rollback of largeAllocSpans
+	for s := c.largeAllocSpans; s != nil; s, s.nextUsedSpan = s.nextUsedSpan, nil {
+		// aborting rollback so just release the spans after adjusting allocCount to s.nelems.
+		if s.freeindex != s.nelems {
+			throw("s.freeindex != s.nelems and span is on ROC incache used largeAllocSpan list.")
+		}
+		if s.allocated() != s.nelems {
+			throw("s.allocated() != s.nelems and span is on ROC incache used largerAllocSpan list.")
+		}
+		if s.isFree(0) {
+			s.allocCount = 0
+			s.nelems = 1
+			recoveredBytes += int64(s.elemsize)
+			mheap_.freeSpan(s, 1)
+		} else {
+			s.startindex = s.freeindex
+			s.allocCount = s.nelems
+		}
+	}
+
+	atomic.Xadd64(&rocData.recoveredBytes, int64(recoveredBytes)) // TBD make available, perhaps as part of gctrace=2
+
+	c.largeAllocSpans = nil
+
+	if _g_ != nil {
+		_g_.rocvalid = false
+		_g_.rocgcnum = 0
+	}
+	// clean up tiny logic
+	c.tiny = 0
+	c.tinyoffset = 0
+}
+
+// smashDebugHelper will obliterate the contents of any free objects
+// in the hopes that this will cause the program to abort quickly and
+// make debugging easier.
+func (s *mspan) smashDebugHelper() {
+	if debug.gcroc >= 2 {
+		// Smash object between s.startindex and freeindex
+		for i := s.startindex; i < s.freeindex; i++ {
+			if s.isFree(i) {
+				words := s.elemsize / unsafe.Sizeof(uintptr(0))
+				for j := uintptr(0); j < words; j++ {
+					ptr := (*uintptr)(unsafe.Pointer(s.base() + i*s.elemsize + j*unsafe.Sizeof(uintptr(0))))
+					*ptr = uintptr(0xdeada11c)
+				}
+			}
+		}
+	}
 }
