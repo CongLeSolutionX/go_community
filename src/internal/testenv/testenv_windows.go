d21201a4f1ebf073e2251d7c1dfe2c20bc3bfd7d
Hiroshi Ioka <hirochachacha@gmail.com>
2016-08-18 19:07:59 +0900

    internal/testenv: add HasSymlink/MustHaveSymlink
    os package and path/filepath package have duplicated code for
    checking symlink supports in test code.
    This CL tries to simplify such test code.
    
    Change-Id: I0371488337f5e951eca699852daab9ccb16ddd62

---
+++ src/internal/testenv/testenv_windows.go
@@ -0,0 +1,36 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package testenv provides information about what functionality
+// is available in different testing environments run by the Go team.
+//
+// It is an internal package because these details are specific
+// to the Go team's test setup (on build.golang.org) and not
+// fundamental to tests in general.
+package testenv
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"syscall"
+)
+
+func init() {
+	tmpdir, err := ioutil.TempDir("", "symtest")
+	if err != nil {
+		panic("failed to create temp directory: " + err.Error())
+	}
+	defer os.RemoveAll(tmpdir)
+
+	err = os.Symlink("target", filepath.Join(tmpdir, "symlink"))
+	if err != nil {
+		err = err.(*os.LinkError).Err
+		switch err {
+		case syscall.EWINDOWS, syscall.ERROR_PRIVILEGE_NOT_HELD:
+			windowsHasSymlink = false
+		}
+	}
+	os.Remove("target")
+}
