8892cc8f296f72428f757aeaa6949b46d37d7477
David Crawshaw <crawshaw@golang.org>
2016-08-26 09:04:27 -0400

    misc/cgo/testplugin: add test of -buildmode=plugin
    Change-Id: Ie9fea9814c850b084562ab2349b54d9ad9fa1f4a

---
+++ misc/cgo/testplugin/test.bash
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Copyright 2016 The Go Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+set -e
+
+if [ ! -f src/host/host.go ]; then
+	cwd=$(pwd)
+	echo "misc/cgo/testplugin/test.bash is running in $cwd" 1>&2
+	exit 1
+fi
+
+goos=$(go env GOOS)
+goarch=$(go env GOARCH)
+goroot=$(go env GOROOT)
+if [ ! -d "$goroot" ]; then
+	echo 'misc/cgo/testcshared/test.bash cannot find GOROOT' 1>&2
+	echo '$GOROOT:' "$GOROOT" 1>&2
+	echo 'go env GOROOT:' "$goroot" 1>&2
+	exit 1
+fi
+
+function cleanup() {
+	rm -f plugin1 host pkg
+}
+trap cleanup EXIT
+
+rm -rf pkg
+
+GOPATH=$(pwd) go build -buildmode=plugin plugin1
+GOPATH=$(pwd) go build host
+
+LD_LIBRARY_PATH=$(pwd) ./host
