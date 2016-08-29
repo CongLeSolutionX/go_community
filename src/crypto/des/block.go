2a379fe4c2c4de1e1d95e0768568d0dcebf11dec
Mura Li <mura_li@castech.com.tw>
2016-08-29 10:22:20 +0800

    crypto/des: improve the throughput of DES
    DO NOT REVIEW
    
    1. The calculations of columns and rows of the sbox are moved to init()
    and done only once, so that the feistel function does not need to repeat
    them in the hotspot.
    
    2. Manually inline and unroll expandBlock().
    
    AMD64 3.2GHz:
    benchmark               old ns/op     new ns/op     delta
    BenchmarkEncrypt-16     504           283           -43.85%
    BenchmarkDecrypt-16     504           274           -45.63%
    
    benchmark               old MB/s     new MB/s     speedup
    BenchmarkEncrypt-16     15.87        28.23        1.78x
    BenchmarkDecrypt-16     15.85        29.13        1.84x
    
    Size of des.a           29818        29676
    
    ARMv6 133MHz:
    benchmark            old ns/op     new ns/op     delta
    BenchmarkEncrypt     60054         23881         -60.23%
    BenchmarkDecrypt     59669         23497         -60.62%
    
    benchmark            old MB/s     new MB/s     speedup
    BenchmarkEncrypt     0.13         0.33         2.54x
    BenchmarkDecrypt     0.13         0.34         2.62x
    
    Size of des.a        27980        27860
    
    Change-Id: I408eb0f23ca4abc3c5b9d694477c83b79bf255af

--- src/crypto/des/block.go
+++ src/crypto/des/block.go
@@ -8,6 +8,389 @@ import (
 	"encoding/binary"
 )
 
+func __cryptBlock(subkeys []uint64, dst, src []byte, decrypt bool) {
+	b := binary.BigEndian.Uint64(src)
+	b = permuteInitialBlock(b)
+	left, right := uint32(b>>32), uint32(b)
+
+	{
+		var block uint64
+		var sBoxResult uint32
+
+		// 0
+		src := (right << 5) | (right >> 27)
+		block = uint64(src&0x3f)<<42 |
+			uint64(((src<<4)|(src>>28))&0x3f)<<36 |
+			uint64(((src<<8)|(src>>24))&0x3f)<<30 |
+			uint64(((src<<12)|(src>>20))&0x3f)<<24 |
+			uint64(((src<<16)|(src>>16))&0x3f)<<18 |
+			uint64(((src<<20)|(src>>12))&0x3f)<<12 |
+			uint64(((src<<24)|(src>>8))&0x3f)<<6 |
+			uint64(((src<<28)|(src>>4)))&0x3f
+		sBoxLocations := subkeys[0] ^ block
+
+		sBoxResult = feistelBox[0][uint8(sBoxLocations>>42)&0x3f] ^
+			feistelBox[1][uint8(sBoxLocations>>36)&0x3f] ^
+			feistelBox[2][uint8(sBoxLocations>>30)&0x3f] ^
+			feistelBox[3][uint8(sBoxLocations>>24)&0x3f] ^
+			feistelBox[4][uint8(sBoxLocations>>18)&0x3f] ^
+			feistelBox[5][uint8(sBoxLocations>>12)&0x3f] ^
+			feistelBox[6][uint8(sBoxLocations>>6)&0x3f] ^
+			feistelBox[7][uint8(sBoxLocations)&0x3f]
+
+		left, right = right, left^sBoxResult
+
+		// 1
+		src = (right << 5) | (right >> 27)
+		block = uint64(src&0x3f)<<42 |
+			uint64(((src<<4)|(src>>28))&0x3f)<<36 |
+			uint64(((src<<8)|(src>>24))&0x3f)<<30 |
+			uint64(((src<<12)|(src>>20))&0x3f)<<24 |
+			uint64(((src<<16)|(src>>16))&0x3f)<<18 |
+			uint64(((src<<20)|(src>>12))&0x3f)<<12 |
+			uint64(((src<<24)|(src>>8))&0x3f)<<6 |
+			uint64(((src<<28)|(src>>4)))&0x3f
+		sBoxLocations = subkeys[1] ^ block
+
+		sBoxResult = feistelBox[0][uint8(sBoxLocations>>42)&0x3f] ^
+			feistelBox[1][uint8(sBoxLocations>>36)&0x3f] ^
+			feistelBox[2][uint8(sBoxLocations>>30)&0x3f] ^
+			feistelBox[3][uint8(sBoxLocations>>24)&0x3f] ^
+			feistelBox[4][uint8(sBoxLocations>>18)&0x3f] ^
+			feistelBox[5][uint8(sBoxLocations>>12)&0x3f] ^
+			feistelBox[6][uint8(sBoxLocations>>6)&0x3f] ^
+			feistelBox[7][uint8(sBoxLocations)&0x3f]
+
+		left, right = right, left^sBoxResult
+
+		// 2
+		src = (right << 5) | (right >> 27)
+		block = uint64(src&0x3f)<<42 |
+			uint64(((src<<4)|(src>>28))&0x3f)<<36 |
+			uint64(((src<<8)|(src>>24))&0x3f)<<30 |
+			uint64(((src<<12)|(src>>20))&0x3f)<<24 |
+			uint64(((src<<16)|(src>>16))&0x3f)<<18 |
+			uint64(((src<<20)|(src>>12))&0x3f)<<12 |
+			uint64(((src<<24)|(src>>8))&0x3f)<<6 |
+			uint64(((src<<28)|(src>>4)))&0x3f
+		sBoxLocations = subkeys[2] ^ block
+
+		sBoxResult = feistelBox[0][uint8(sBoxLocations>>42)&0x3f] ^
+			feistelBox[1][uint8(sBoxLocations>>36)&0x3f] ^
+			feistelBox[2][uint8(sBoxLocations>>30)&0x3f] ^
+			feistelBox[3][uint8(sBoxLocations>>24)&0x3f] ^
+			feistelBox[4][uint8(sBoxLocations>>18)&0x3f] ^
+			feistelBox[5][uint8(sBoxLocations>>12)&0x3f] ^
+			feistelBox[6][uint8(sBoxLocations>>6)&0x3f] ^
+			feistelBox[7][uint8(sBoxLocations)&0x3f]
+
+		left, right = right, left^sBoxResult
+
+		// 3
+		src = (right << 5) | (right >> 27)
+		block = uint64(src&0x3f)<<42 |
+			uint64(((src<<4)|(src>>28))&0x3f)<<36 |
+			uint64(((src<<8)|(src>>24))&0x3f)<<30 |
+			uint64(((src<<12)|(src>>20))&0x3f)<<24 |
+			uint64(((src<<16)|(src>>16))&0x3f)<<18 |
+			uint64(((src<<20)|(src>>12))&0x3f)<<12 |
+			uint64(((src<<24)|(src>>8))&0x3f)<<6 |
+			uint64(((src<<28)|(src>>4)))&0x3f
+		sBoxLocations = subkeys[3] ^ block
+
+		sBoxResult = feistelBox[0][uint8(sBoxLocations>>42)&0x3f] ^
+			feistelBox[1][uint8(sBoxLocations>>36)&0x3f] ^
+			feistelBox[2][uint8(sBoxLocations>>30)&0x3f] ^
+			feistelBox[3][uint8(sBoxLocations>>24)&0x3f] ^
+			feistelBox[4][uint8(sBoxLocations>>18)&0x3f] ^
+			feistelBox[5][uint8(sBoxLocations>>12)&0x3f] ^
+			feistelBox[6][uint8(sBoxLocations>>6)&0x3f] ^
+			feistelBox[7][uint8(sBoxLocations)&0x3f]
+
+		left, right = right, left^sBoxResult
+
+		// 4
+		src = (right << 5) | (right >> 27)
+		block = uint64(src&0x3f)<<42 |
+			uint64(((src<<4)|(src>>28))&0x3f)<<36 |
+			uint64(((src<<8)|(src>>24))&0x3f)<<30 |
+			uint64(((src<<12)|(src>>20))&0x3f)<<24 |
+			uint64(((src<<16)|(src>>16))&0x3f)<<18 |
+			uint64(((src<<20)|(src>>12))&0x3f)<<12 |
+			uint64(((src<<24)|(src>>8))&0x3f)<<6 |
+			uint64(((src<<28)|(src>>4)))&0x3f
+		sBoxLocations = subkeys[4] ^ block
+
+		sBoxResult = feistelBox[0][uint8(sBoxLocations>>42)&0x3f] ^
+			feistelBox[1][uint8(sBoxLocations>>36)&0x3f] ^
+			feistelBox[2][uint8(sBoxLocations>>30)&0x3f] ^
+			feistelBox[3][uint8(sBoxLocations>>24)&0x3f] ^
+			feistelBox[4][uint8(sBoxLocations>>18)&0x3f] ^
+			feistelBox[5][uint8(sBoxLocations>>12)&0x3f] ^
+			feistelBox[6][uint8(sBoxLocations>>6)&0x3f] ^
+			feistelBox[7][uint8(sBoxLocations)&0x3f]
+
+		left, right = right, left^sBoxResult
+
+		// 5
+		src = (right << 5) | (right >> 27)
+		block = uint64(src&0x3f)<<42 |
+			uint64(((src<<4)|(src>>28))&0x3f)<<36 |
+			uint64(((src<<8)|(src>>24))&0x3f)<<30 |
+			uint64(((src<<12)|(src>>20))&0x3f)<<24 |
+			uint64(((src<<16)|(src>>16))&0x3f)<<18 |
+			uint64(((src<<20)|(src>>12))&0x3f)<<12 |
+			uint64(((src<<24)|(src>>8))&0x3f)<<6 |
+			uint64(((src<<28)|(src>>4)))&0x3f
+		sBoxLocations = subkeys[5] ^ block
+
+		sBoxResult = feistelBox[0][uint8(sBoxLocations>>42)&0x3f] ^
+			feistelBox[1][uint8(sBoxLocations>>36)&0x3f] ^
+			feistelBox[2][uint8(sBoxLocations>>30)&0x3f] ^
+			feistelBox[3][uint8(sBoxLocations>>24)&0x3f] ^
+			feistelBox[4][uint8(sBoxLocations>>18)&0x3f] ^
+			feistelBox[5][uint8(sBoxLocations>>12)&0x3f] ^
+			feistelBox[6][uint8(sBoxLocations>>6)&0x3f] ^
+			feistelBox[7][uint8(sBoxLocations)&0x3f]
+
+		left, right = right, left^sBoxResult
+
+		// 6
+		src = (right << 5) | (right >> 27)
+		block = uint64(src&0x3f)<<42 |
+			uint64(((src<<4)|(src>>28))&0x3f)<<36 |
+			uint64(((src<<8)|(src>>24))&0x3f)<<30 |
+			uint64(((src<<12)|(src>>20))&0x3f)<<24 |
+			uint64(((src<<16)|(src>>16))&0x3f)<<18 |
+			uint64(((src<<20)|(src>>12))&0x3f)<<12 |
+			uint64(((src<<24)|(src>>8))&0x3f)<<6 |
+			uint64(((src<<28)|(src>>4)))&0x3f
+		sBoxLocations = subkeys[6] ^ block
+
+		sBoxResult = feistelBox[0][uint8(sBoxLocations>>42)&0x3f] ^
+			feistelBox[1][uint8(sBoxLocations>>36)&0x3f] ^
+			feistelBox[2][uint8(sBoxLocations>>30)&0x3f] ^
+			feistelBox[3][uint8(sBoxLocations>>24)&0x3f] ^
+			feistelBox[4][uint8(sBoxLocations>>18)&0x3f] ^
+			feistelBox[5][uint8(sBoxLocations>>12)&0x3f] ^
+			feistelBox[6][uint8(sBoxLocations>>6)&0x3f] ^
+			feistelBox[7][uint8(sBoxLocations)&0x3f]
+
+		left, right = right, left^sBoxResult
+
+		// 7
+		src = (right << 5) | (right >> 27)
+		block = uint64(src&0x3f)<<42 |
+			uint64(((src<<4)|(src>>28))&0x3f)<<36 |
+			uint64(((src<<8)|(src>>24))&0x3f)<<30 |
+			uint64(((src<<12)|(src>>20))&0x3f)<<24 |
+			uint64(((src<<16)|(src>>16))&0x3f)<<18 |
+			uint64(((src<<20)|(src>>12))&0x3f)<<12 |
+			uint64(((src<<24)|(src>>8))&0x3f)<<6 |
+			uint64(((src<<28)|(src>>4)))&0x3f
+		sBoxLocations = subkeys[7] ^ block
+
+		sBoxResult = feistelBox[0][uint8(sBoxLocations>>42)&0x3f] ^
+			feistelBox[1][uint8(sBoxLocations>>36)&0x3f] ^
+			feistelBox[2][uint8(sBoxLocations>>30)&0x3f] ^
+			feistelBox[3][uint8(sBoxLocations>>24)&0x3f] ^
+			feistelBox[4][uint8(sBoxLocations>>18)&0x3f] ^
+			feistelBox[5][uint8(sBoxLocations>>12)&0x3f] ^
+			feistelBox[6][uint8(sBoxLocations>>6)&0x3f] ^
+			feistelBox[7][uint8(sBoxLocations)&0x3f]
+
+		left, right = right, left^sBoxResult
+
+		// 8
+		src = (right << 5) | (right >> 27)
+		block = uint64(src&0x3f)<<42 |
+			uint64(((src<<4)|(src>>28))&0x3f)<<36 |
+			uint64(((src<<8)|(src>>24))&0x3f)<<30 |
+			uint64(((src<<12)|(src>>20))&0x3f)<<24 |
+			uint64(((src<<16)|(src>>16))&0x3f)<<18 |
+			uint64(((src<<20)|(src>>12))&0x3f)<<12 |
+			uint64(((src<<24)|(src>>8))&0x3f)<<6 |
+			uint64(((src<<28)|(src>>4)))&0x3f
+		sBoxLocations = subkeys[8] ^ block
+
+		sBoxResult = feistelBox[0][uint8(sBoxLocations>>42)&0x3f] ^
+			feistelBox[1][uint8(sBoxLocations>>36)&0x3f] ^
+			feistelBox[2][uint8(sBoxLocations>>30)&0x3f] ^
+			feistelBox[3][uint8(sBoxLocations>>24)&0x3f] ^
+			feistelBox[4][uint8(sBoxLocations>>18)&0x3f] ^
+			feistelBox[5][uint8(sBoxLocations>>12)&0x3f] ^
+			feistelBox[6][uint8(sBoxLocations>>6)&0x3f] ^
+			feistelBox[7][uint8(sBoxLocations)&0x3f]
+
+		left, right = right, left^sBoxResult
+
+		// 9
+		src = (right << 5) | (right >> 27)
+		block = uint64(src&0x3f)<<42 |
+			uint64(((src<<4)|(src>>28))&0x3f)<<36 |
+			uint64(((src<<8)|(src>>24))&0x3f)<<30 |
+			uint64(((src<<12)|(src>>20))&0x3f)<<24 |
+			uint64(((src<<16)|(src>>16))&0x3f)<<18 |
+			uint64(((src<<20)|(src>>12))&0x3f)<<12 |
+			uint64(((src<<24)|(src>>8))&0x3f)<<6 |
+			uint64(((src<<28)|(src>>4)))&0x3f
+		sBoxLocations = subkeys[9] ^ block
+
+		sBoxResult = feistelBox[0][uint8(sBoxLocations>>42)&0x3f] ^
+			feistelBox[1][uint8(sBoxLocations>>36)&0x3f] ^
+			feistelBox[2][uint8(sBoxLocations>>30)&0x3f] ^
+			feistelBox[3][uint8(sBoxLocations>>24)&0x3f] ^
+			feistelBox[4][uint8(sBoxLocations>>18)&0x3f] ^
+			feistelBox[5][uint8(sBoxLocations>>12)&0x3f] ^
+			feistelBox[6][uint8(sBoxLocations>>6)&0x3f] ^
+			feistelBox[7][uint8(sBoxLocations)&0x3f]
+
+		left, right = right, left^sBoxResult
+
+		// 10
+		src = (right << 5) | (right >> 27)
+		block = uint64(src&0x3f)<<42 |
+			uint64(((src<<4)|(src>>28))&0x3f)<<36 |
+			uint64(((src<<8)|(src>>24))&0x3f)<<30 |
+			uint64(((src<<12)|(src>>20))&0x3f)<<24 |
+			uint64(((src<<16)|(src>>16))&0x3f)<<18 |
+			uint64(((src<<20)|(src>>12))&0x3f)<<12 |
+			uint64(((src<<24)|(src>>8))&0x3f)<<6 |
+			uint64(((src<<28)|(src>>4)))&0x3f
+		sBoxLocations = subkeys[10] ^ block
+
+		sBoxResult = feistelBox[0][uint8(sBoxLocations>>42)&0x3f] ^
+			feistelBox[1][uint8(sBoxLocations>>36)&0x3f] ^
+			feistelBox[2][uint8(sBoxLocations>>30)&0x3f] ^
+			feistelBox[3][uint8(sBoxLocations>>24)&0x3f] ^
+			feistelBox[4][uint8(sBoxLocations>>18)&0x3f] ^
+			feistelBox[5][uint8(sBoxLocations>>12)&0x3f] ^
+			feistelBox[6][uint8(sBoxLocations>>6)&0x3f] ^
+			feistelBox[7][uint8(sBoxLocations)&0x3f]
+
+		left, right = right, left^sBoxResult
+
+		// 11
+		src = (right << 5) | (right >> 27)
+		block = uint64(src&0x3f)<<42 |
+			uint64(((src<<4)|(src>>28))&0x3f)<<36 |
+			uint64(((src<<8)|(src>>24))&0x3f)<<30 |
+			uint64(((src<<12)|(src>>20))&0x3f)<<24 |
+			uint64(((src<<16)|(src>>16))&0x3f)<<18 |
+			uint64(((src<<20)|(src>>12))&0x3f)<<12 |
+			uint64(((src<<24)|(src>>8))&0x3f)<<6 |
+			uint64(((src<<28)|(src>>4)))&0x3f
+		sBoxLocations = subkeys[11] ^ block
+
+		sBoxResult = feistelBox[0][uint8(sBoxLocations>>42)&0x3f] ^
+			feistelBox[1][uint8(sBoxLocations>>36)&0x3f] ^
+			feistelBox[2][uint8(sBoxLocations>>30)&0x3f] ^
+			feistelBox[3][uint8(sBoxLocations>>24)&0x3f] ^
+			feistelBox[4][uint8(sBoxLocations>>18)&0x3f] ^
+			feistelBox[5][uint8(sBoxLocations>>12)&0x3f] ^
+			feistelBox[6][uint8(sBoxLocations>>6)&0x3f] ^
+			feistelBox[7][uint8(sBoxLocations)&0x3f]
+
+		left, right = right, left^sBoxResult
+
+		// 12
+		src = (right << 5) | (right >> 27)
+		block = uint64(src&0x3f)<<42 |
+			uint64(((src<<4)|(src>>28))&0x3f)<<36 |
+			uint64(((src<<8)|(src>>24))&0x3f)<<30 |
+			uint64(((src<<12)|(src>>20))&0x3f)<<24 |
+			uint64(((src<<16)|(src>>16))&0x3f)<<18 |
+			uint64(((src<<20)|(src>>12))&0x3f)<<12 |
+			uint64(((src<<24)|(src>>8))&0x3f)<<6 |
+			uint64(((src<<28)|(src>>4)))&0x3f
+		sBoxLocations = subkeys[12] ^ block
+
+		sBoxResult = feistelBox[0][uint8(sBoxLocations>>42)&0x3f] ^
+			feistelBox[1][uint8(sBoxLocations>>36)&0x3f] ^
+			feistelBox[2][uint8(sBoxLocations>>30)&0x3f] ^
+			feistelBox[3][uint8(sBoxLocations>>24)&0x3f] ^
+			feistelBox[4][uint8(sBoxLocations>>18)&0x3f] ^
+			feistelBox[5][uint8(sBoxLocations>>12)&0x3f] ^
+			feistelBox[6][uint8(sBoxLocations>>6)&0x3f] ^
+			feistelBox[7][uint8(sBoxLocations)&0x3f]
+
+		left, right = right, left^sBoxResult
+
+		// 13
+		src = (right << 5) | (right >> 27)
+		block = uint64(src&0x3f)<<42 |
+			uint64(((src<<4)|(src>>28))&0x3f)<<36 |
+			uint64(((src<<8)|(src>>24))&0x3f)<<30 |
+			uint64(((src<<12)|(src>>20))&0x3f)<<24 |
+			uint64(((src<<16)|(src>>16))&0x3f)<<18 |
+			uint64(((src<<20)|(src>>12))&0x3f)<<12 |
+			uint64(((src<<24)|(src>>8))&0x3f)<<6 |
+			uint64(((src<<28)|(src>>4)))&0x3f
+		sBoxLocations = subkeys[13] ^ block
+
+		sBoxResult = feistelBox[0][uint8(sBoxLocations>>42)&0x3f] ^
+			feistelBox[1][uint8(sBoxLocations>>36)&0x3f] ^
+			feistelBox[2][uint8(sBoxLocations>>30)&0x3f] ^
+			feistelBox[3][uint8(sBoxLocations>>24)&0x3f] ^
+			feistelBox[4][uint8(sBoxLocations>>18)&0x3f] ^
+			feistelBox[5][uint8(sBoxLocations>>12)&0x3f] ^
+			feistelBox[6][uint8(sBoxLocations>>6)&0x3f] ^
+			feistelBox[7][uint8(sBoxLocations)&0x3f]
+
+		left, right = right, left^sBoxResult
+
+		// 14
+		src = (right << 5) | (right >> 27)
+		block = uint64(src&0x3f)<<42 |
+			uint64(((src<<4)|(src>>28))&0x3f)<<36 |
+			uint64(((src<<8)|(src>>24))&0x3f)<<30 |
+			uint64(((src<<12)|(src>>20))&0x3f)<<24 |
+			uint64(((src<<16)|(src>>16))&0x3f)<<18 |
+			uint64(((src<<20)|(src>>12))&0x3f)<<12 |
+			uint64(((src<<24)|(src>>8))&0x3f)<<6 |
+			uint64(((src<<28)|(src>>4)))&0x3f
+		sBoxLocations = subkeys[14] ^ block
+
+		sBoxResult = feistelBox[0][uint8(sBoxLocations>>42)&0x3f] ^
+			feistelBox[1][uint8(sBoxLocations>>36)&0x3f] ^
+			feistelBox[2][uint8(sBoxLocations>>30)&0x3f] ^
+			feistelBox[3][uint8(sBoxLocations>>24)&0x3f] ^
+			feistelBox[4][uint8(sBoxLocations>>18)&0x3f] ^
+			feistelBox[5][uint8(sBoxLocations>>12)&0x3f] ^
+			feistelBox[6][uint8(sBoxLocations>>6)&0x3f] ^
+			feistelBox[7][uint8(sBoxLocations)&0x3f]
+
+		left, right = right, left^sBoxResult
+
+		// 15
+		src = (right << 5) | (right >> 27)
+		block = uint64(src&0x3f)<<42 |
+			uint64(((src<<4)|(src>>28))&0x3f)<<36 |
+			uint64(((src<<8)|(src>>24))&0x3f)<<30 |
+			uint64(((src<<12)|(src>>20))&0x3f)<<24 |
+			uint64(((src<<16)|(src>>16))&0x3f)<<18 |
+			uint64(((src<<20)|(src>>12))&0x3f)<<12 |
+			uint64(((src<<24)|(src>>8))&0x3f)<<6 |
+			uint64(((src<<28)|(src>>4)))&0x3f
+		sBoxLocations = subkeys[15] ^ block
+
+		sBoxResult = feistelBox[0][uint8(sBoxLocations>>42)&0x3f] ^
+			feistelBox[1][uint8(sBoxLocations>>36)&0x3f] ^
+			feistelBox[2][uint8(sBoxLocations>>30)&0x3f] ^
+			feistelBox[3][uint8(sBoxLocations>>24)&0x3f] ^
+			feistelBox[4][uint8(sBoxLocations>>18)&0x3f] ^
+			feistelBox[5][uint8(sBoxLocations>>12)&0x3f] ^
+			feistelBox[6][uint8(sBoxLocations>>6)&0x3f] ^
+			feistelBox[7][uint8(sBoxLocations)&0x3f]
+
+		left, right = right, left^sBoxResult
+
+	}
+
+	preOutput := (uint64(right) << 32) | uint64(left)
+	binary.BigEndian.PutUint64(dst, permuteFinalBlock(preOutput))
+}
+
 func cryptBlock(subkeys []uint64, dst, src []byte, decrypt bool) {
 	b := binary.BigEndian.Uint64(src)
 	b = permuteInitialBlock(b)
@@ -40,17 +423,37 @@ func decryptBlock(subkeys []uint64, dst, src []byte) {
 
 // DES Feistel function
 func feistel(right uint32, key uint64) (result uint32) {
-	sBoxLocations := key ^ expandBlock(right)
+
+	var block uint64
+
+	// Expand block:
+	// First, Left-rotate 5 bits the 'right' variable.
+	// Then left-rotate 4 bits of 'src' and take the 6 bits on the right as a sub-block.
+	// Finally, combine 8 sub-blcoks(6 bits each) into a 48 bits block.
+	src := (right << 5) | (right >> 27)
+
+	block = uint64(src&0x3f)<<42 |
+		uint64(((src<<4)|(src>>28))&0x3f)<<36 |
+		uint64(((src<<8)|(src>>24))&0x3f)<<30 |
+		uint64(((src<<12)|(src>>20))&0x3f)<<24 |
+		uint64(((src<<16)|(src>>16))&0x3f)<<18 |
+		uint64(((src<<20)|(src>>12))&0x3f)<<12 |
+		uint64(((src<<24)|(src>>8))&0x3f)<<6 |
+		uint64(((src<<28)|(src>>4)))&0x3f
+
+	// Calculate the sbox locations from the 48 bits block
+	sBoxLocations := key ^ block
+
 	var sBoxResult uint32
-	for i := uint8(0); i < 8; i++ {
-		sBoxLocation := uint8(sBoxLocations>>42) & 0x3f
-		sBoxLocations <<= 6
-		// row determined by 1st and 6th bit
-		// column is middle four bits
-		row := (sBoxLocation & 0x1) | ((sBoxLocation & 0x20) >> 4)
-		column := (sBoxLocation >> 1) & 0xf
-		sBoxResult ^= feistelBox[i][16*row+column]
-	}
+	sBoxResult = feistelBox[0][uint8(sBoxLocations>>42)&0x3f] ^
+		feistelBox[1][uint8(sBoxLocations>>36)&0x3f] ^
+		feistelBox[2][uint8(sBoxLocations>>30)&0x3f] ^
+		feistelBox[3][uint8(sBoxLocations>>24)&0x3f] ^
+		feistelBox[4][uint8(sBoxLocations>>18)&0x3f] ^
+		feistelBox[5][uint8(sBoxLocations>>12)&0x3f] ^
+		feistelBox[6][uint8(sBoxLocations>>6)&0x3f] ^
+		feistelBox[7][uint8(sBoxLocations)&0x3f]
+
 	return sBoxResult
 }
 
@@ -73,25 +476,16 @@ func init() {
 			for j := 0; j < 16; j++ {
 				f := uint64(sBoxes[s][i][j]) << (4 * (7 - uint(s)))
 				f = permuteBlock(f, permutationFunction[:])
-				feistelBox[s][16*i+j] = uint32(f)
+
+				// row determined by 1st and 6th bit
+				// column is middle four bits
+				row := uint8(((i & 2) << 4) | i&1)
+				col := uint8(j << 1)
+				t := row | col
+				feistelBox[s][t] = uint32(f)
 			}
 		}
 	}
-}
-
-// expandBlock expands an input block of 32 bits,
-// producing an output block of 48 bits.
-func expandBlock(src uint32) (block uint64) {
-	// rotate the 5 highest bits to the right.
-	src = (src << 5) | (src >> 27)
-	for i := 0; i < 8; i++ {
-		block <<= 6
-		// take the 6 bits on the right
-		block |= uint64(src) & (1<<6 - 1)
-		// advance by 4 bits.
-		src = (src << 4) | (src >> 28)
-	}
-	return
 }
 
 // permuteInitialBlock is equivalent to the permutation defined
