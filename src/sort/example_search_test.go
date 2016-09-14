c267a5a972ac54e2c53b57fd236db1f272ce5b7c
Suyash <dextrous93@gmail.com>
2016-09-14 22:36:58 +0530

    sort: add example for sort.Search
    this adds an example showing off the different ways of using sort.Search
    
    play: https://play.golang.org/p/eAj5gZs3QE
    
    Change-Id: Iaa08b4501691f37908317fdcf2e618fbe9f99ade

---
+++ src/sort/example_search_test.go
@@ -0,0 +1,97 @@
+package sort_test
+
+import (
+	"fmt"
+	"sort"
+)
+
+// This example shows off different capabilities of the Search function
+// also at https://play.golang.org/p/eAj5gZs3QE
+func Example_search() {
+	a := []int{1, 3, 6, 10, 15, 21, 28, 36, 45, 55}
+	x := 5
+
+	// lower bound in go: the position of the smallest integer greater than or equal to an element
+	lb := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+
+	if lb < len(a) {
+		fmt.Printf("lower bound for %d at index: %d, value: %d\n", x, lb, a[lb])
+	} else {
+		fmt.Printf("lower bound for value %d doesn't exist\n", x)
+	}
+	// OUTPUT: lower bound for value 5 at index: 2, value: 6
+
+	// for int slices SearchInts by default calculates the lower bound
+	lb = sort.SearchInts(a, x)
+
+	if lb < len(a) {
+		fmt.Printf("lower bound for %d at index: %d, value: %d\n", x, lb, a[lb])
+	} else {
+		fmt.Printf("lower bound for value %d doesn't exist\n", x)
+	}
+	// OUTPUT: lower bound for value 5 at index: 2, value: 6
+
+	// upper bound in go: the position of the smallest integer greater than an element
+	x = 6
+	ub := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+
+	if ub < len(a) {
+		fmt.Printf("upper bound for %d at index: %d, value: %d\n", x, ub, a[ub])
+	} else {
+		fmt.Printf("upper bound for value %d doesn't exist\n", x)
+	}
+	// OUTPUT: upper bound for value 6 at index: 3, value: 10
+
+	// if the array is sorted in descending order
+	// Search can still be used to find lower bounds and upper bounds
+
+	a = []int{55, 45, 36, 28, 21, 15, 10, 6, 3, 1}
+
+	// lower bound: the first number before a number definitely less than an element
+	lb = sort.Search(len(a), func(i int) bool {
+		return a[i] < x
+	}) - 1
+
+	if lb >= 0 {
+		fmt.Printf("lower bound for %d at index: %d, value: %d\n", x, lb, a[lb])
+	} else {
+		fmt.Printf("lower bound for value %d doesn't exist\n", x)
+	}
+	// OUTPUT: lower bound for value 6 at index: 7, value: 6
+
+	// upper bound: the first number before a number less than or equal to an element
+	ub = sort.Search(len(a), func(i int) bool {
+		return a[i] <= 6
+	}) - 1
+
+	if ub >= 0 {
+		fmt.Printf("upper bound for value %d at index: %d, value: %d\n", x, ub, a[ub])
+	} else {
+		fmt.Printf("upper bound for value %d doesn't exist\n", 6)
+	}
+	// OUTPUT: upper bound for value 6 at index: 6, value: 10
+
+	// binary search in go
+	// this is different compared to other languages,
+	// as seen above, the return type of the Search functions in go is int not bool,
+	// so an explicit check is needed
+	a = []int{1, 3, 6, 10, 15, 21, 28, 36, 45, 55}
+	if in := sort.SearchInts(a, x); in < len(a) && a[in] == x {
+		fmt.Printf("found %d at index %d in %v\n", x, in, a)
+	} else {
+		fmt.Printf("%d not found in %v\n", x, a)
+	}
+	// OUTPUT: found 6 at index 2 in [1 3 6 10 15 21 28 36 45 55]
+
+	x = 5
+	if in := sort.SearchInts(a, x); in < len(a) && a[in] == x {
+		fmt.Printf("found %d at index %d in %v\n", x, in, a)
+	} else {
+		fmt.Printf("%d not found in %v\n", x, a)
+	}
+	// OUTPUT: 5 not found in [1 3 6 10 15 21 28 36 45 55]
+}
