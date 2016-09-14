7c0a3b6ecf8e34a2c99afeb71b6854e958c3255c
Suyash <dextrous93@gmail.com>
2016-09-14 22:36:58 +0530

    sort: add example for sort.Search
    This adds examples showing the different ways of using sort.Search.
    
    Change-Id: Iaa08b4501691f37908317fdcf2e618fbe9f99ade

---
+++ src/sort/example_search_test.go
@@ -0,0 +1,110 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package sort_test
+
+import (
+	"fmt"
+	"sort"
+)
+
+func ExampleSearch() {
+	a := []int{1, 3, 6, 10, 15, 21, 28, 36, 45, 55}
+	x := 6
+
+	// Search returns the index of the lower bound,
+	// so an explicit check for equality is needed
+	if in := sort.Search(len(a), func(i int) bool { return a[i] >= x }); in < len(a) && a[in] == x {
+		fmt.Printf("found %d at index %d in %v\n", x, in, a)
+	} else {
+		fmt.Printf("%d not found in %v\n", x, a)
+	}
+	// Output:
+	// found 6 at index 2 in [1 3 6 10 15 21 28 36 45 55]
+}
+
+func ExampleSearch_descendingOrder() {
+	a := []int{55, 45, 36, 28, 21, 15, 10, 6, 3, 1}
+	x := 6
+
+	if in := sort.Search(len(a), func(i int) bool { return a[i] < x }) - 1; in >= 0 && a[in] == x {
+		fmt.Printf("found %d at index %d in %v\n", x, in, a)
+	} else {
+		fmt.Printf("%d not found in %v\n", x, a)
+	}
+	// Output:
+	// found 6 at index 7 in [55 45 36 28 21 15 10 6 3 1]
+}
+
+func ExampleSearch_lowerBound() {
+	a := []int{1, 3, 6, 10, 15, 21, 28, 36, 45, 55}
+	x := 5
+
+	// Lower Bound: the position of the smallest integer greater than or equal to an element
+	lb := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+
+	if lb < len(a) {
+		fmt.Printf("lower bound for %d at index: %d, value: %d\n", x, lb, a[lb])
+	} else {
+		fmt.Printf("lower bound for value %d doesn't exist\n", x)
+	}
+	// Output:
+	// lower bound for 5 at index: 2, value: 6
+}
+
+func ExampleSearch_lowerBound_descendingOrder() {
+	a := []int{55, 45, 36, 28, 21, 15, 10, 6, 3, 1}
+	x := 6
+
+	// Lower Bound for descending order array: the first number before a number definitely less than an element
+	lb := sort.Search(len(a), func(i int) bool {
+		return a[i] < x
+	}) - 1
+
+	if lb >= 0 {
+		fmt.Printf("lower bound for %d at index: %d, value: %d\n", x, lb, a[lb])
+	} else {
+		fmt.Printf("lower bound for value %d doesn't exist\n", x)
+	}
+	// Output:
+	// lower bound for 6 at index: 7, value: 6
+}
+
+func ExampleSearch_upperBound() {
+	a := []int{1, 3, 6, 10, 15, 21, 28, 36, 45, 55}
+	x := 6
+
+	// Upper Bound: the position of the smallest integer greater than an element
+	ub := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+
+	if ub < len(a) {
+		fmt.Printf("upper bound for %d at index: %d, value: %d\n", x, ub, a[ub])
+	} else {
+		fmt.Printf("upper bound for value %d doesn't exist\n", x)
+	}
+	// Output:
+	// upper bound for 6 at index: 3, value: 10
+}
+
+func ExampleSearch_upperBound_descendingOrder() {
+	a := []int{55, 45, 36, 28, 21, 15, 10, 6, 3, 1}
+	x := 6
+
+	// Upper Bound for descending order array: the first number before a number less than or equal to an element
+	ub := sort.Search(len(a), func(i int) bool {
+		return a[i] <= x
+	}) - 1
+
+	if ub >= 0 {
+		fmt.Printf("upper bound for %d at index: %d, value: %d\n", x, ub, a[ub])
+	} else {
+		fmt.Printf("upper bound for value %d doesn't exist\n", 6)
+	}
+	// Output:
+	// upper bound for 6 at index: 6, value: 10
+}
