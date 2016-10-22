7509798363aa79bdab7da7a0b0381df976771d07
Brad Fitzpatrick <bradfitz@golang.org>
2016-10-22 07:25:21 -0700

    testing: add T.Context method
    From the doc comment:
    
    Context returns the context for the current test or benchmark.
    The context is cancelled when the test or benchmark finishes.
    A goroutine started during a test or benchmark can wait for the
    context's Done channel to become readable as a signal that the
    test or benchmark is over, so that the goroutine can exit.
    
    Fixes #16221.
    Fixes #17552.
    
    Change-Id: I657df946be2c90048cc74615436c77c7d9d1226c

--- src/context/context_test.go
+++ src/context/context_test.go
@@ -2,9 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package context
+package context_test
 
 import (
+	. "context"
 	"fmt"
 	"math/rand"
 	"runtime"
