c5db7453e11ef7ff794ec8fb2c97f5ed6e6910ef
David du Colombier <0intro@gmail.com>
2016-10-24 16:58:13 +0200

    net: implement asynchonous cancelable I/O on Plan 9
    This change is an experimental implementation of asynchronous
    cancelable I/O operations on Plan 9, which are required to
    implement deadlines.
    
    There are no asynchronous syscalls on Plan 9. I/O operations
    are performed with blocking pread and pwrite syscalls.
    
    Implementing deadlines in Go requires a way to interrupt
    I/O operations.
    
    It is possible to interrupt reads and writes on a TCP connection
    by forcing the closure of the TCP connection. This approach
    has been used successfully in CL 31390.
    
    However, we can't implement deadlines with this method, since
    we require to be able to reuse the connection after the timeout.
    
    On Plan 9, I/O operations are interrupted when the process
    receives a note. We can rely on this behavior to implement
    a more generic approach.
    
    When doing an I/O operation (read or write), we start the I/O in
    its own process, then wait for the result asynchronously. The
    process is able to handle the "hangup" note. When receiving the
    "hangup" note, the currently running I/O operation is canceled
    and the process returns.
    
    This way, deadlines can be implemented by sending an "hangup"
    note to the process running the blocking I/O operation, after
    the expiration of a timer.
    
    DO NOT SUBMIT
    
    Change-Id: I414f72c7a9a4f9b8f9c09ed3b6c269f899d9b430

---
+++ src/net/fd_io_plan9.go
@@ -0,0 +1,83 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package net
+
+import (
+	"os"
+	"runtime"
+	"sync"
+	"syscall"
+)
+
+func runtime_ignoreHangup()
+func runtime_unignoreHangup()
+
+// asyncIO implements asynchronous cancelable I/O.
+type asyncIO struct {
+	res chan result
+
+	// mu guards the pid field.
+	mu sync.Mutex
+
+	// pid holds the process id of
+	// the process running the IO operation.
+	pid int
+}
+
+type result struct {
+	n   int
+	err error
+}
+
+// NewAsyncIO returns a new asyncIO that performs I/O
+// operation by calling fn, which must do one and only one
+// interruptible system call.
+func newAsyncIO(fn func([]byte) (int, error), b []byte) *asyncIO {
+	var aio asyncIO
+	aio.res = make(chan result)
+	aio.mu.Lock()
+	go func() {
+		// Lock the current goroutine to its process
+		// and store the pid in io so that Cancel can
+		// interrupt it. We register a signal channel so
+		// that the signal does not take down the entire
+		// Go runtime, but we don't need to receive on it.
+		runtime.LockOSThread()
+		runtime_ignoreHangup()
+		aio.pid = os.Getpid()
+		aio.mu.Unlock()
+
+		n, err := fn(b)
+
+		aio.mu.Lock()
+		aio.pid = -1
+		runtime_unignoreHangup()
+		aio.mu.Unlock()
+
+		aio.res <- result{n, err}
+	}()
+	return &aio
+}
+
+// Cancel interrupts the I/O operation, causing
+// the Wait function to return.
+func (aio *asyncIO) Cancel() {
+	aio.mu.Lock()
+	defer aio.mu.Unlock()
+	if aio.pid == -1 {
+		return
+	}
+	proc, err := os.FindProcess(aio.pid)
+	if err != nil {
+		return
+	}
+	proc.Signal(syscall.Note("hangup"))
+}
+
+// Wait for the I/O operation to complete.
+func (aio *asyncIO) Wait() (int, error) {
+	res := <-aio.res
+	return res.n, res.err
+}
