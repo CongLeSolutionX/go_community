cf54a8001c85602773cc377b29f256203e49732b
Brad Fitzpatrick <bradfitz@golang.org>
2016-11-16 19:08:25 +0000

    math/rand: make floating point tests shorter on mips and mipsle
    Like GOARM=5 does.
    
    Fixes #17944
    
    Change-Id: Ica2a54a90fbd4a29471d1c6009ace2fcc5e82a73

--- src/math/rand/rand_test.go
+++ src/math/rand/rand_test.go
@@ -328,13 +328,23 @@ func TestExpTables(t *testing.T) {
 	}
 }
 
+func hasSlowFloatingPoint() bool {
+	switch runtime.GOARCH {
+	case "arm":
+		return os.Getenv("GOARM") == "5"
+	case "mips", "mipsle":
+		return true
+	}
+	return false
+}
+
 func TestFloat32(t *testing.T) {
 	// For issue 6721, the problem came after 7533753 calls, so check 10e6.
 	num := int(10e6)
 	// But do the full amount only on builders (not locally).
 	// But ARM5 floating point emulation is slow (Issue 10749), so
 	// do less for that builder:
-	if testing.Short() && (testenv.Builder() == "" || runtime.GOARCH == "arm" && os.Getenv("GOARM") == "5") {
+	if testing.Short() && (testenv.Builder() == "" || hasSlowFloatingPoint()) {
 		num /= 100 // 1.72 seconds instead of 172 seconds
 	}
 
