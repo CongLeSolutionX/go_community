8d7b446c02ac62b060e4b79f3cb0ba0d89545521
Quentin Smith <quentin@golang.org>
2016-11-30 15:16:37 -0500

    crypto/x509: read Darwin trust settings for root CAs
    Darwin separately stores bits indicating whether a root certificate
    should be trusted; this changes Go to read and use those when
    initializing SystemCertPool.
    
    Unfortunately this dramatically slows down initialization - it takes
    about half a second to check the trust settings for every certificate.
    
    Change-Id: If681c514047afe5e1a68de6c9d40ceabbce54755

--- src/crypto/x509/root_darwin.go
+++ src/crypto/x509/root_darwin.go
@@ -6,7 +6,12 @@
 
 package x509
 
-import "os/exec"
+import (
+	"bytes"
+	"encoding/pem"
+	"os/exec"
+	"sync"
+)
 
 func (c *Certificate) systemVerify(opts *VerifyOptions) (chains [][]*Certificate, err error) {
 	return nil, nil
@@ -20,6 +25,45 @@ func execSecurityRoots() (*CertPool, error) {
 	}
 
 	roots := NewCertPool()
-	roots.AppendCertsFromPEM(data)
+	var mtx sync.Mutex
+	add := func(cert *Certificate) {
+		mtx.Lock()
+		defer mtx.Unlock()
+		roots.AddCert(cert)
+	}
+	blockCh := make(chan *pem.Block)
+	var wg sync.WaitGroup
+	for i := 0; i < 4; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for block := range blockCh {
+				cmd := exec.Command("/usr/bin/security", "verify-cert", "-c", "/dev/stdin", "-l", "-q")
+				cmd.Stdin = bytes.NewReader(pem.EncodeToMemory(block))
+				if err := cmd.Run(); err == nil {
+					// Non-zero exit means untrusted
+					cert, err := ParseCertificate(block.Bytes)
+					if err != nil {
+						continue
+					}
+
+					add(cert)
+				}
+			}
+		}()
+	}
+	for len(data) > 0 {
+		var block *pem.Block
+		block, data = pem.Decode(data)
+		if block == nil {
+			break
+		}
+		if block.Type != "CERTIFICATE" || len(block.Headers) != 0 {
+			continue
+		}
+		blockCh <- block
+	}
+	close(blockCh)
+	wg.Wait()
 	return roots, nil
 }
