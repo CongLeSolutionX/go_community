65211b44e77f392b660157d63651817c75a542db
Joe Tsai <joetsai@digital-static.net>
2017-02-02 00:14:25 -0800

    net/url: document the package better
    Changes made:
    * Actually mention RFC 3986 on the URL type.
    * Adjust the documented form for a URL to make it more obvious what
    happens when the scheme is missing.
    * Add examples to Parse for how certain URLs are parsed into their
    various component fields.
    * Remove references to Go1.5. We are sufficiently far along enough
    that this distinction no longer matters.
    * Remove the "Opaque" example which provides a hacky and misleading
    use of the Opaque field. This workaround is no longer necessary
    since the obvious approach just works:
    	// The raw string "/%2f/" will be sent as expected.
    	req, _ := http.NewRequest("GET", "https://example.com/%2f/")
    
    Fixes #18824
    
    Change-Id: Ie33d27222e06025ce8025f8a0f04b601aaee1513

--- src/net/url/url.go
+++ src/net/url/url.go
@@ -309,9 +309,12 @@ func escape(s string, mode encoding) string {
 }
 
 // A URL represents a parsed URL (technically, a URI reference).
+// The format format generally follows RFC 3986, except for rare exceptions
+// due to legacy reasons.
+//
 // The general form represented is:
 //
-//	scheme://[userinfo@]host/path[?query][#fragment]
+//	[scheme:][//[userinfo@]host]/path[?query][#fragment]
 //
 // URLs that do not start with a slash after the scheme are interpreted as:
 //
@@ -320,27 +323,20 @@ func escape(s string, mode encoding) string {
 // Note that the Path field is stored in decoded form: /%47%6f%2f becomes /Go/.
 // A consequence is that it is impossible to tell which slashes in the Path were
 // slashes in the raw URL and which were %2f. This distinction is rarely important,
-// but when it is, code must not use Path directly.
-//
-// Go 1.5 introduced the RawPath field to hold the encoded form of Path.
-// The Parse function sets both Path and RawPath in the URL it returns,
-// and URL's String method uses RawPath if it is a valid encoding of Path,
-// by calling the EscapedPath method.
-//
-// In earlier versions of Go, the more indirect workarounds were that an
-// HTTP server could consult req.RequestURI and an HTTP client could
-// construct a URL struct directly and set the Opaque field instead of Path.
-// These still work as well.
+// but when it is, code must not use Path directly. If URL decoding was
+// performed while parsing, the original string is stored in RawPath.
+// If the RawPath is populated and properly decodes to Path, then its value
+// will be used by by the String method.
 type URL struct {
 	Scheme     string
-	Opaque     string    // encoded opaque data
+	Opaque     string    // encoded opaque data; must not start with a "/"
 	User       *Userinfo // username and password information
 	Host       string    // host or host:port
-	Path       string
-	RawPath    string // encoded path hint (Go 1.5 and later only; see EscapedPath method)
-	ForceQuery bool   // append a query ('?') even if RawQuery is empty
-	RawQuery   string // encoded query values, without '?'
-	Fragment   string // fragment for references, without '#'
+	Path       string    // hierarchal path; must not start with a "//"
+	RawPath    string    // encoded path hint (see EscapedPath method)
+	ForceQuery bool      // append a query ('?') even if RawQuery is empty
+	RawQuery   string    // encoded query values, without '?'
+	Fragment   string    // fragment for references, without '#'
 }
 
 // User returns a Userinfo containing the provided username
@@ -351,11 +347,11 @@ func User(username string) *Userinfo {
 
 // UserPassword returns a Userinfo containing the provided username
 // and password.
-// This functionality should only be used with legacy web sites.
-// RFC 2396 warns that interpreting Userinfo this way
-// ``is NOT RECOMMENDED, because the passing of authentication
-// information in clear text (such as URI) has proven to be a
-// security risk in almost every case where it has been used.''
+//
+// This should only be used with legacy web sites as RFC 2396 warns:
+//	This practice is NOT RECOMMENDED, because the passing of authentication
+//	information in clear text (such as URI) has proven to be a security risk
+//	in almost every case where it has been used.
 func UserPassword(username, password string) *Userinfo {
 	return &Userinfo{username, password, true}
 }
