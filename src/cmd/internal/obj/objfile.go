95106c4680eb38f5c3fef5e4547487c08f11afa0
Matthew Dempsky <mdempsky@google.com>
2017-02-06 15:52:36 -0800

    cmd/internal/dwarf: use []*Var instead of linked lists
    Change-Id: I202b29495ca1aaf3c52879fa99fdc0a4b86703af

--- src/cmd/internal/obj/objfile.go
+++ src/cmd/internal/obj/objfile.go
@@ -561,7 +561,7 @@ func gendwarf(ctxt *Link, text []*LSym) []*LSym {
 		dw = append(dw, dsym)
 		dsym.Type = SDWARFINFO
 		dsym.Set(AttrDuplicateOK, s.DuplicateOK())
-		var vars dwarf.Var
+		var vars []*dwarf.Var
 		var abbrev int
 		var offs int32
 		for a := s.Autom; a != nil; a = a.Link {
@@ -583,23 +583,17 @@ func gendwarf(ctxt *Link, text []*LSym) []*LSym {
 			default:
 				continue
 			}
+
 			typename := dwarf.InfoPrefix + a.Gotype.Name[len("type."):]
-			dwvar := &dwarf.Var{
+			vars = append(vars, &dwarf.Var{
 				Name:   a.Asym.Name,
 				Abbrev: abbrev,
-				Offset: int32(offs),
+				Offset: offs,
 				Type:   Linklookup(ctxt, typename, 0),
-			}
-			dws := &vars.Link
-			for ; *dws != nil; dws = &(*dws).Link {
-				if offs <= (*dws).Offset {
-					break
-				}
-			}
-			dwvar.Link = *dws
-			*dws = dwvar
+			})
 		}
-		dwarf.PutFunc(dctxt, dsym, s.Name, s.Version == 0, s, s.Size, vars.Link)
+		sort.Stable(dwarf.VarsByOffset(vars))
+		dwarf.PutFunc(dctxt, dsym, s.Name, s.Version == 0, s, s.Size, vars)
 	}
 	return dw
 }
