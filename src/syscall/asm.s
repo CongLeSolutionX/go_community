96e32598fb7526acfddb9672e46efe8b55e99f93
Sameer Ajmani <sameer@golang.org>
2017-02-10 09:22:35 -0500

    syscall: delete the "use" function and calls in non-generated files.
    Delete use stub from asm.s, leaving only a dummy file.
    Deleting the file causes Windows build to fail.
    
    Fixes #16607
    
    Change-Id: Ic5a55e042e588f1e1bc6605a3d309d1eabdeb288

--- src/syscall/asm.s
+++ src/syscall/asm.s
@@ -2,7 +2,4 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-#include "textflag.h"
-
-TEXT ·use(SB),NOSPLIT,$0
-	RET
+// Dummy asm file.
