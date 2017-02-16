4ed77c825000532b1b390970422c1507ea8cfcc0
Marcel van Lohuizen <mpvl@golang.org>
2017-02-16 16:14:16 +0100

    net/http: import updated idna package and adjust request.go
    Custom logic from request.go has been removed.
    
    Created by running: “go run gen.go -core” from x/text.
    
    Fixes golang/go#17268
    
    Change-Id: Ie440d6ae30288352283d303e5126e5837f11bece

--- src/vendor/golang_org/x/text/unicode/norm/normalize.go
+++ src/vendor/golang_org/x/text/unicode/norm/normalize.go
@@ -1,12 +1,11 @@
+// Copied from the golang.org/x/text repo; DO NOT EDIT
+
 // Copyright 2011 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:generate go run maketables.go triegen.go
-//go:generate go run maketables.go triegen.go -test
-
 // Package norm contains types and functions for normalizing Unicode strings.
-package norm // import "golang.org/x/text/unicode/norm"
+package norm // import "golang_org/x/text/unicode/norm"
 
 import (
 	"unicode/utf8"
