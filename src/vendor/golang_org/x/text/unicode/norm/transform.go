5c87efe07cbb7ee9e1b6e9fb6655ddefbca12fc8
Marcel van Lohuizen <mpvl@golang.org>
2017-02-16 16:14:16 +0100

    net/http: import updated idna package and adjust request.go
    Custom logic from request.go has been removed.
    
    Created by running: “go run gen.go -core” from x/text.
    
    Fixes golang/go#17268
    
    Change-Id: Ie440d6ae30288352283d303e5126e5837f11bece

--- src/vendor/golang_org/x/text/unicode/norm/transform.go
+++ src/vendor/golang_org/x/text/unicode/norm/transform.go
@@ -1,3 +1,5 @@
+// Copied by running go generate in golang.org/x/text; DO NOT EDIT
+
 // Copyright 2013 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
