7fe048eadca367e377dacffaf10bf16ec56b7e57
Matthew Dempsky <mdempsky@google.com>
2017-03-11 17:17:15 -0800

    cmd/internal/obj: remove unneeded Addr.Node and Prog.Opt fields
    Change-Id: I218b241c32a5948b66ad0d95ecc368648cf4ddf5

--- src/cmd/compile/internal/gc/ssa.go
+++ src/cmd/compile/internal/gc/ssa.go
@@ -4594,13 +4594,11 @@ func AddAux2(a *obj.Addr, v *ssa.Value, offset int64) {
 	case *ssa.ArgSymbol:
 		n := sym.Node.(*Node)
 		a.Name = obj.NAME_PARAM
-		a.Node = n
 		a.Sym = Linksym(n.Orig.Sym)
 		a.Offset += n.Xoffset
 	case *ssa.AutoSymbol:
 		n := sym.Node.(*Node)
 		a.Name = obj.NAME_AUTO
-		a.Node = n
 		a.Sym = Linksym(n.Sym)
 		a.Offset += n.Xoffset
 	default:
@@ -4708,7 +4706,6 @@ func AutoVar(v *ssa.Value) (*Node, int64) {
 func AddrAuto(a *obj.Addr, v *ssa.Value) {
 	n, off := AutoVar(v)
 	a.Type = obj.TYPE_MEM
-	a.Node = n
 	a.Sym = Linksym(n.Sym)
 	a.Reg = int16(Thearch.REGSP)
 	a.Offset = n.Xoffset + off
@@ -4725,7 +4722,6 @@ func (s *SSAGenState) AddrScratch(a *obj.Addr) {
 	}
 	a.Type = obj.TYPE_MEM
 	a.Name = obj.NAME_AUTO
-	a.Node = s.ScratchFpMem
 	a.Sym = Linksym(s.ScratchFpMem.Sym)
 	a.Reg = int16(Thearch.REGSP)
 	a.Offset = s.ScratchFpMem.Xoffset
