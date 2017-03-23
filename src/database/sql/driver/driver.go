49f1d5b96c906454fe410e6460bf9cb83fc090cf
Daniel Theophanes <kardianos@gmail.com>
2017-03-23 13:17:59 -0700

    database/sql: allow drivers to support custom arg types
    Previously all arguments were passed through driver.IsValid.
    This checked arguments against a few fundamental go types and
    prevented others from being passed in as arguments.
    
    The new interface driver.NamedValueChecker may be implemented
    by both driver.Stmt and driver.Conn. This allows
    this new interface to completely supersede the
    driver.ColumnConverter interface as it can be used for
    checking arguments known to a prepared statement and
    arbitrary query arguments. The NamedValueChecker may be
    skipped with driver.ErrSkip after all special cases are
    exhausted to use the default argument converter.
    
    In addition if driver.ErrRemoveArgument is returned
    the argument will not be passed to the query at all,
    useful for passing in driver specific per-query options.
    
    Add a canonical Out argument wrapper to be passed
    to OUTPUT parameters. This will unify checks that need to
    be written in the NameValueChecker.
    
    The statement number check is also moved to the argument
    converter so the NamedValueChecker may remove arguments
    passed to the query.
    
    Fixes #13567
    Fixes #18079
    Updates #18417
    Updates #17834
    Updates #16235
    Updates #13067
    Updates #19797
    
    Change-Id: I89088bd9cca4596a48bba37bfd20d987453ef237

--- src/database/sql/driver/driver.go
+++ src/database/sql/driver/driver.go
@@ -262,9 +262,39 @@ type StmtQueryContext interface {
 	QueryContext(ctx context.Context, args []NamedValue) (Rows, error)
 }
 
+// ErrOmitArgument may be returned from NamedValueChecker to instruct the
+// sql package to not pass the argument to the driver query interface.
+// Return when accepting query specific options or structures that aren't
+// SQL query arguments.
+var ErrOmitArgument = errors.New("driver: omit argument from query")
+
+// NamedValueChecker may be optionally implemented by Conn or Stmt. It provides
+// the driver more control to handle go and database types beyond the default
+// types allowed.
+//
+// First any available Stmt is checked for this interface.
+// Then it will check if Conn implements this interface.
+// Lastly ColumnConverter is checked for if a Stmt is available.
+//
+// If ErrOmitArgument is returned, the NamedValue will not be included in
+// the final query arguments. This may be used to pass special options to
+// the query itself.
+//
+// If ErrSkip is returned the column converter error checking
+// path is used for the argument. Drivers may wish to return ErrSkip after
+// they have exhausted their own special cases.
+type NamedValueChecker interface {
+	// NamedValueCheck is called before passing arguments to the driver
+	// and is called in place of any ColumnConverter. NamedValueCheck must do type
+	// validation and conversion as appropriate for the driver.
+	NamedValueCheck(*NamedValue) error
+}
+
 // ColumnConverter may be optionally implemented by Stmt if the
 // statement is aware of its own columns' types and can convert from
 // any type to a driver Value.
+//
+// Deprecated: Drivers should implement NamedValueChecker.
 type ColumnConverter interface {
 	// ColumnConverter returns a ValueConverter for the provided
 	// column index. If the type of a specific column isn't known
