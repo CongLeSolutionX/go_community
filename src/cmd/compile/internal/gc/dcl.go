56707f326852e1e9352d87003540517fa417fdee
Josh Bleecher Snyder <josharian@gmail.com>
2017-03-24 11:33:29 -0700

    cmd/compile: simplify funcsyms
    Sym.Fsym is used only to avoid adding duplicate
    entries to funcsyms, but that is easily
    accomplished by detecting the first lookup
    vs subsequent lookups of the func sym name.
    
    This avoids creating an unnecessary ONAME node
    during funcsym, which eliminates a dependency
    in the backend on Curfn and lineno.
    
    It also makes the code a lot simpler and clearer.
    
    Updates #15756
    
    Passes toolstash-check -all.
    No compiler performance changes.
    funcsymname does generate garbage via string
    concatenation, but it is not called very much,
    and this CL also eliminates allocation of several
    Nodes and Names.
    
    Change-Id: I7116c78fa39d975b7bd2c65a1d228749cf0dd46b

--- src/cmd/compile/internal/gc/dcl.go
+++ src/cmd/compile/internal/gc/dcl.go
@@ -1175,22 +1175,30 @@ func funccompile(n *Node) {
 	dclcontext = PEXTERN
 }
 
-func funcsym(s *Sym) *Sym {
-	if s.Fsym != nil {
-		return s.Fsym
-	}
-
-	s1 := Pkglookup(s.Name+"·f", s.Pkg)
-	if !Ctxt.Flag_dynlink && s1.Def == nil {
-		s1.Def = newfuncname(s1)
-		s1.Def.Func.Shortname = s
-		funcsyms = append(funcsyms, s1.Def)
-	}
-	s.Fsym = s1
-	return s1
+func (s *Sym) funcsymname() string {
+	return s.Name + "·f"
 }
 
+// funcsym returns s·f.
+func funcsym(s *Sym) *Sym {
+	sf, existed := s.Pkg.LookupOK(s.funcsymname())
+	// Don't export s·f when compiling for dynamic linking.
+	// When dynamically linking, the necessary function
+	// symbols will be created explicitly with makefuncsym.
+	if !Ctxt.Flag_dynlink && !existed {
+		funcsyms = append(funcsyms, s)
+	}
+	return sf
+}
+
+// makefuncsym ensures that s·f is exported.
+// It is only used with -dynlink.
+// When not compiling for dynamic linking,
+// the necessary symbols are exported lazily with funcsym.
 func makefuncsym(s *Sym) {
+	if !Ctxt.Flag_dynlink {
+		Fatalf("makefuncsym dynlink")
+	}
 	if isblanksym(s) {
 		return
 	}
@@ -1199,13 +1207,9 @@ func makefuncsym(s *Sym) {
 		// not get a funcsym.
 		return
 	}
-	s1 := funcsym(s)
-	if s1.Def != nil {
-		return
+	if _, existed := s.Pkg.LookupOK(s.funcsymname()); !existed {
+		funcsyms = append(funcsyms, s)
 	}
-	s1.Def = newfuncname(s1)
-	s1.Def.Func.Shortname = s
-	funcsyms = append(funcsyms, s1.Def)
 }
 
 type nowritebarrierrecChecker struct {
