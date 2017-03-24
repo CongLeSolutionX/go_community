dc99f2e3496d4213d32fffbb6bedf8421ec93f9d
Peter Armitage <pja@google.com>
2017-03-24 16:42:46 +0000

    cmd/vet: check for missing error assignments in if statements
    If a variable called "err" is declared in the outer scope, code
    like this will compile without error:
    
    if f(); err != nil {
    	...
    }
    
    however we have seen that this is almost always a mistake, and
    instead the code should be
    
    if err := f(); err != nil {
    	...
    }
    
    False positives seem rare. It's possible, though unusual, to make
    f() a closure over the err variable.
    
    This check does not attempt to detect if f() returns an error. It
    simply looks for a variable called "err" that is being compared
    to nil in a if statement, where the init statement was a function
    call.
    
    Change-Id: Ibf94bdb98322c2bfcb04bf369917dc0f7d3ff8c8

---
+++ src/cmd/vet/missingErr.go
@@ -0,0 +1,62 @@
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+This file contains the code to check for missing error assignments in if
+statements. For example
+
+	if f(); err != nil {
+		...
+	}
+
+will compile if err is declared in the outer scope. But it is likely that
+the programmer meant
+
+	if err := f(); err != nil {
+		...
+	}
+*/
+
+package main
+
+import "go/ast"
+
+func init() {
+	register("missingerr",
+		"check for possibly missing error assignments in if statements",
+		checkMissingErr,
+		ifStmt)
+}
+
+// checkMissingErr looks for a function call without assignment in the init
+// statement and an error comparison in the condition expression.
+// It's likely that the init statement is missing an assignment to the
+// error variable.
+func checkMissingErr(f *File, node ast.Node) {
+	is := node.(*ast.IfStmt)
+	es, ok := is.Init.(*ast.ExprStmt)
+	if !ok {
+		return
+	}
+	_, ok = es.X.(*ast.CallExpr)
+	if !ok {
+		return
+	}
+	be, ok := is.Cond.(*ast.BinaryExpr)
+	if !ok {
+		return
+	}
+	lhs, ok := be.X.(*ast.Ident)
+	if !ok {
+		return
+	}
+	rhs, ok := be.Y.(*ast.Ident)
+	if !ok {
+		return
+	}
+	if lhs.Name == "err" && rhs.Name == "nil" {
+		f.Badf(lhs.Pos(), "possibly missing error assignment")
+	}
+	return
+}
---
+++ src/cmd/vet/testdata/missingErr.go
@@ -0,0 +1,44 @@
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This file contains tests for the missing error assignment checker.
+
+package testdata
+
+func f() {
+	err := g()
+	if err != nil {
+	}
+	if g(); err != nil { // ERROR "possibly missing error assignment"
+	}
+	if g(); err == nil { // ERROR "possibly missing error assignment"
+	}
+	if g(); err != g() {
+	}
+	if g(); g() != nil {
+	}
+	if err = g(); err != nil {
+	}
+	if h(); err != nil { // ERROR "possibly missing error assignment"
+	}
+	if err != nil {
+	}
+}
+
+func f2() {
+	// Avoids warnings from the shadow checker.
+
+	if _, err := h(); err != nil {
+	}
+	if err := g(); err != nil {
+	}
+}
+
+func g() error {
+	return nil
+}
+
+func h() (int, error) {
+	return 0, nil
+}
