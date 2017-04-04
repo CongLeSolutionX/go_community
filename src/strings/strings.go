1bc00c6cb8e1d7581923d0806998ceb7afd97dc7
Martin Möhrmann <moehrmann@google.com>
2017-04-04 19:14:02 +0200

    strings: slim down Fields (Option 2)
    DO NOT SUBMIT
    - for discussion
    - would slow down FieldsFunc
    
    name                      old time/op    new time/op    delta
    Fields/ASCII/16              115ns ± 1%     196ns ± 1%   +69.70%  (p=0.000 n=10+10)
    Fields/ASCII/256             798ns ± 1%    1930ns ± 1%  +141.69%  (p=0.000 n=10+10)
    Fields/ASCII/4096           12.6µs ± 1%    30.2µs ± 1%  +139.07%  (p=0.000 n=10+9)
    Fields/ASCII/65536           234µs ± 1%     537µs ± 1%  +129.46%  (p=0.000 n=10+10)
    Fields/ASCII/1048576        5.31ms ± 1%   10.03ms ± 1%   +88.91%  (p=0.000 n=9+10)
    Fields/Mixed/16              169ns ± 1%     255ns ± 1%   +50.98%  (p=0.000 n=9+8)
    Fields/Mixed/256            1.17µs ± 1%    2.36µs ± 1%  +102.03%  (p=0.000 n=10+10)
    Fields/Mixed/4096           18.7µs ± 1%    42.5µs ± 1%  +127.09%  (p=0.000 n=9+9)
    Fields/Mixed/65536           387µs ± 1%     754µs ± 1%   +94.65%  (p=0.000 n=9+10)
    Fields/Mixed/1048576        7.22ms ± 2%   13.38ms ± 2%   +85.28%  (p=0.000 n=10+10)
    FieldsFunc/ASCII/16          276ns ± 2%     319ns ± 1%   +15.43%  (p=0.000 n=9+10)
    FieldsFunc/ASCII/256        3.69µs ± 1%    4.35µs ± 1%   +17.97%  (p=0.000 n=9+10)
    FieldsFunc/ASCII/4096       59.7µs ± 1%    69.2µs ± 1%   +15.85%  (p=0.000 n=10+10)
    FieldsFunc/ASCII/65536       958µs ± 1%    1110µs ± 1%   +15.85%  (p=0.000 n=8+9)
    FieldsFunc/ASCII/1048576    16.1ms ± 2%    18.7ms ± 1%   +15.80%  (p=0.000 n=10+10)
    FieldsFunc/Mixed/16          303ns ± 1%     339ns ± 1%   +11.78%  (p=0.000 n=9+10)
    FieldsFunc/Mixed/256        3.74µs ± 1%    4.42µs ± 1%   +18.10%  (p=0.000 n=10+10)
    FieldsFunc/Mixed/4096       64.7µs ± 1%    75.4µs ± 1%   +16.45%  (p=0.000 n=10+9)
    FieldsFunc/Mixed/65536      1.07ms ± 1%    1.20ms ± 1%   +12.82%  (p=0.000 n=10+10)
    FieldsFunc/Mixed/1048576    18.1ms ± 1%    20.5ms ± 1%   +13.18%  (p=0.000 n=9+9)
    
    Change-Id: I14720efc844bdf7b9008b046ca0a054a36d45cdb

--- src/strings/strings.go
+++ src/strings/strings.go
@@ -296,112 +296,7 @@ var asciiSpace = [256]uint8{'\t': 1, '\n': 1, '\v': 1, '\f': 1, '\r': 1, ' ': 1}
 // characters, as defined by unicode.IsSpace, returning an array of substrings of s or an
 // empty list if s contains only white space.
 func Fields(s string) []string {
-	// First count the fields.
-	// This is an exact count if s is ASCII, otherwise it is an approximation.
-	n := 0
-	wasSpace := 1
-	// setBits is used to track which bits are set in the bytes of s.
-	setBits := uint8(0)
-	for i := 0; i < len(s); i++ {
-		r := s[i]
-		setBits |= r
-		isSpace := int(asciiSpace[r])
-		n += wasSpace & ^isSpace
-		wasSpace = isSpace
-	}
-
-	if setBits < utf8.RuneSelf { // ASCII fast path
-		a := make([]string, n)
-		na := 0
-		fieldStart := 0
-		i := 0
-		// Skip spaces in the front of the input.
-		for i < len(s) && asciiSpace[s[i]] != 0 {
-			i++
-		}
-		fieldStart = i
-		for i < len(s) {
-			if asciiSpace[s[i]] == 0 {
-				i++
-				continue
-			}
-			a[na] = s[fieldStart:i]
-			na++
-			i++
-			// Skip spaces in between fields.
-			for i < len(s) && asciiSpace[s[i]] != 0 {
-				i++
-			}
-			fieldStart = i
-		}
-		if fieldStart < len(s) { // Last field might end at EOF.
-			a[na] = s[fieldStart:]
-		}
-		return a
-	}
-
-	// Some runes in the input string are not ASCII.
-	// Same general approach as in the ASCII path but
-	// uses DecodeRuneInString and unicode.IsSpace if
-	// a non-ASCII rune needs to be decoded and checked
-	// if it corresponds to a space.
-	a := make([]string, 0, n)
-	fieldStart := 0
-	i := 0
-	// Skip spaces in the front of the input.
-	for i < len(s) {
-		if c := s[i]; c < utf8.RuneSelf {
-			if asciiSpace[c] == 0 {
-				break
-			}
-			i++
-		} else {
-			r, w := utf8.DecodeRuneInString(s[i:])
-			if !unicode.IsSpace(r) {
-				break
-			}
-			i += w
-		}
-	}
-	fieldStart = i
-	for i < len(s) {
-		if c := s[i]; c < utf8.RuneSelf {
-			if asciiSpace[c] == 0 {
-				i++
-				continue
-			}
-			a = append(a, s[fieldStart:i])
-			i++
-		} else {
-			r, w := utf8.DecodeRuneInString(s[i:])
-			if !unicode.IsSpace(r) {
-				i += w
-				continue
-			}
-			a = append(a, s[fieldStart:i])
-			i += w
-		}
-		// Skip spaces in between fields.
-		for i < len(s) {
-			if c := s[i]; c < utf8.RuneSelf {
-				if asciiSpace[c] == 0 {
-					break
-				}
-				i++
-			} else {
-				r, w := utf8.DecodeRuneInString(s[i:])
-				if !unicode.IsSpace(r) {
-					break
-				}
-				i += w
-			}
-		}
-		fieldStart = i
-	}
-	if fieldStart < len(s) { // Last field might end at EOF.
-		a = append(a, s[fieldStart:])
-	}
-	return a
+	return fieldsFunc_(s, unicode.IsSpace, true)
 }
 
 // FieldsFunc splits the string s at each run of Unicode code points c satisfying f(c)
@@ -410,12 +305,20 @@ func Fields(s string) []string {
 // FieldsFunc makes no guarantees about the order in which it calls f(c).
 // If f does not return consistent results for a given c, FieldsFunc may crash.
 func FieldsFunc(s string, f func(rune) bool) []string {
+	return fieldsFunc_(s, f, false)
+}
+
+func fieldsFunc_(s string, f func(rune) bool, isSpace bool) []string {
 	// First count the fields.
 	n := 0
 	inField := false
-	for _, rune := range s {
+	for _, r := range s {
 		wasInField := inField
-		inField = !f(rune)
+		if isSpace && r < utf8.RuneSelf {
+			inField = asciiSpace[r] == 0
+		} else {
+			inField = !f(r)
+		}
 		if inField && !wasInField {
 			n++
 		}
@@ -424,9 +327,15 @@ func FieldsFunc(s string, f func(rune) bool) []string {
 	// Now create them.
 	a := make([]string, n)
 	na := 0
+	fret := false
 	fieldStart := -1 // Set to -1 when looking for start of field.
-	for i, rune := range s {
-		if f(rune) {
+	for i, r := range s {
+		if isSpace && r < utf8.RuneSelf {
+			fret = asciiSpace[r] == 1
+		} else {
+			fret = f(r)
+		}
+		if fret {
 			if fieldStart >= 0 {
 				a[na] = s[fieldStart:i]
 				na++
