83b14bc682c50c7665af3223eaafb403458d243d
Mikio Hara <mikioh.mikioh@gmail.com>
2017-04-19 05:35:58 +0900

    net/http: replace SOCKS client implementation
    DO NOT SUBMIT
    UNTIL CL 38278 LANDS
    
    This change replaces the vendored socks client implementation with the
    bundle of golang.org/x/net/internal/socks package which contains fixes
    for 19354 and 11682.
    
    golang.org/x/net/internal/socks becomes socks_bundle.go.
    
    At git rev NNNNNN.
    
    Updates #18508.
    Updates #19354.
    Fixes #19688.
    
    Change-Id: I8cf6c3f5eb87c24685a7592be015729f84fbed77

--- src/net/http/transport.go
+++ src/net/http/transport.go
@@ -29,7 +29,6 @@ import (
 	"time"
 
 	"golang_org/x/net/lex/httplex"
-	"golang_org/x/net/proxy"
 )
 
 // DefaultTransport is the default implementation of Transport and is
@@ -994,23 +993,6 @@ func (t *Transport) getConn(treq *transportRequest, cm connectMethod) (*persistC
 	}
 }
 
-type oneConnDialer <-chan net.Conn
-
-func newOneConnDialer(c net.Conn) proxy.Dialer {
-	ch := make(chan net.Conn, 1)
-	ch <- c
-	return oneConnDialer(ch)
-}
-
-func (d oneConnDialer) Dial(network, addr string) (net.Conn, error) {
-	select {
-	case c := <-d:
-		return c, nil
-	default:
-		return nil, io.EOF
-	}
-}
-
 func (t *Transport) dialConn(ctx context.Context, cm connectMethod) (*persistConn, error) {
 	pconn := &persistConn{
 		t:             t,
@@ -1069,18 +1051,26 @@ func (t *Transport) dialConn(ctx context.Context, cm connectMethod) (*persistCon
 		// Do nothing. Not using a proxy.
 	case cm.proxyURL.Scheme == "socks5":
 		conn := pconn.conn
-		var auth *proxy.Auth
-		if u := cm.proxyURL.User; u != nil {
-			auth = &proxy.Auth{}
-			auth.User = u.Username()
-			auth.Password, _ = u.Password()
-		}
-		p, err := proxy.SOCKS5("", cm.addr(), auth, newOneConnDialer(conn))
+		d, err := socksNewDialer("tcp", conn.RemoteAddr().String())
 		if err != nil {
 			conn.Close()
 			return nil, err
 		}
-		if _, err := p.Dial("tcp", cm.targetAddr); err != nil {
+		d.ProxyDial = func(_ context.Context, _, _ string) (net.Conn, error) {
+			return conn, nil
+		}
+		if u := cm.proxyURL.User; u != nil {
+			auth := &socksUsernamePassword{
+				Username: u.Username(),
+			}
+			auth.Password, _ = u.Password()
+			d.AuthMethods = []socksAuthMethod{
+				socksAuthMethodNotRequired,
+				socksAuthMethodUsernamePassword,
+			}
+			d.Authenticate = auth.Authenticate
+		}
+		if _, err := d.DialContext(ctx, "tcp", cm.targetAddr); err != nil {
 			conn.Close()
 			return nil, err
 		}
