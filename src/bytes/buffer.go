2321f71db82550018352dc1d25ea951601e0a6db
Marvin Stenger <marvin.stenger94@gmail.com>
2017-05-07 10:43:17 +0200

    bytes: optimize Buffer's Write, WriteString, WriteByte, and WriteRune
    In the common case, the grow method only needs to reslice the internal
    buffer. Making another function call to grow can be expensive when Write
    is called very often with small pieces of data (like a byte or rune).
    Thus, we add a tryGrowByReslice method that is inlineable so that we can
    avoid an extra call in most cases.
    
    name         old time/op   new time/op   delta
    WriteByte-4   35.5µs ± 2%   17.5µs ± 1%   -50.81%  (p=0.000 n=10+9)
    WriteRune-4   55.8µs ± 1%   39.1µs ± 2%   -29.94%  (p=0.000 n=7+10)
    
    name         old speed     new speed     delta
    WriteByte-4  115MB/s ± 2%  235MB/s ± 1%  +103.29%  (p=0.000 n=10+9)
    WriteRune-4  220MB/s ± 1%  315MB/s ± 2%   +42.74%  (p=0.000 n=7+10)
    
    Fixes #17857
    
    Change-Id: I08dfb10a1c7e001817729dbfcc951bda12fe8814

--- src/bytes/buffer.go
+++ src/bytes/buffer.go
@@ -93,6 +93,17 @@ func (b *Buffer) Reset() {
 	b.lastRead = opInvalid
 }
 
+// tryGrowByReslice is a inlineable version of grow for the fast-case where the
+// internal buffer only needs to be re-sliced.
+// It returns the index where bytes should be written and whether it succeeded.
+func (b *Buffer) tryGrowByReslice(n int) (int, bool) {
+	if l := len(b.buf); l+n <= cap(b.buf) {
+		b.buf = b.buf[:l+n]
+		return l, true
+	}
+	return 0, false
+}
+
 // grow grows the buffer to guarantee space for n more bytes.
 // It returns the index where bytes should be written.
 // If the buffer can't grow it will panic with ErrTooLarge.
@@ -102,7 +113,9 @@ func (b *Buffer) grow(n int) int {
 	if m == 0 && b.off != 0 {
 		b.Reset()
 	}
-	if len(b.buf)+n > cap(b.buf) {
+	if i, ok := b.tryGrowByReslice(n); ok {
+		return i
+	} else {
 		var buf []byte
 		if b.buf == nil && n <= len(b.bootstrap) {
 			buf = b.bootstrap[0:]
@@ -120,9 +133,9 @@ func (b *Buffer) grow(n int) int {
 		}
 		b.buf = buf
 		b.off = 0
+		b.buf = b.buf[:m+n]
+		return m
 	}
-	b.buf = b.buf[0 : b.off+m+n]
-	return b.off + m
 }
 
 // Grow grows the buffer's capacity, if necessary, to guarantee space for
@@ -143,7 +156,10 @@ func (b *Buffer) Grow(n int) {
 // buffer becomes too large, Write will panic with ErrTooLarge.
 func (b *Buffer) Write(p []byte) (n int, err error) {
 	b.lastRead = opInvalid
-	m := b.grow(len(p))
+	m, ok := b.tryGrowByReslice(len(p))
+	if !ok {
+		m = b.grow(len(p))
+	}
 	return copy(b.buf[m:], p), nil
 }
 
@@ -152,7 +168,10 @@ func (b *Buffer) Write(p []byte) (n int, err error) {
 // buffer becomes too large, WriteString will panic with ErrTooLarge.
 func (b *Buffer) WriteString(s string) (n int, err error) {
 	b.lastRead = opInvalid
-	m := b.grow(len(s))
+	m, ok := b.tryGrowByReslice(len(s))
+	if !ok {
+		m = b.grow(len(s))
+	}
 	return copy(b.buf[m:], s), nil
 }
 
@@ -244,7 +263,10 @@ func (b *Buffer) WriteTo(w io.Writer) (n int64, err error) {
 // ErrTooLarge.
 func (b *Buffer) WriteByte(c byte) error {
 	b.lastRead = opInvalid
-	m := b.grow(1)
+	m, ok := b.tryGrowByReslice(1)
+	if !ok {
+		m = b.grow(1)
+	}
 	b.buf[m] = c
 	return nil
 }
@@ -259,7 +281,10 @@ func (b *Buffer) WriteRune(r rune) (n int, err error) {
 		return 1, nil
 	}
 	b.lastRead = opInvalid
-	m := b.grow(utf8.UTFMax)
+	m, ok := b.tryGrowByReslice(utf8.UTFMax)
+	if !ok {
+		m = b.grow(utf8.UTFMax)
+	}
 	n = utf8.EncodeRune(b.buf[m:m+utf8.UTFMax], r)
 	b.buf = b.buf[:m+n]
 	return n, nil
