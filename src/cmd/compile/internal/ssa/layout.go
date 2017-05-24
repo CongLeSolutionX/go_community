5eb9d6bd0e6beca0306f28f1fa942e2dd46ce562
David Chase <drchase@google.com>
2017-05-24 14:03:38 -0400

    cmd/compile: experiment w/ loop rotation as part of layout
    Probably not for 1.9.  Making a CL for benchmarking on other
    machines and not forgetting.
    
    When laying out blocks, when the first transition into a
    loop is detected, attempt to find a better first block
    than the loop header itself.
    
    Might make loop rotation phase unnecessary, might do a
    better job.  Or not.
    
    Added test to choose "best" bottom block based on postorder
    number of exit (larger is better).
    
    Change-Id: Ic92993b083bd022e75d9948773d960b0cb36bdea

--- src/cmd/compile/internal/ssa/layout.go
+++ src/cmd/compile/internal/ssa/layout.go
@@ -4,6 +4,11 @@
 
 package ssa
 
+type segment struct {
+	first, last *Block
+	parent      *segment
+}
+
 // layout orders basic blocks in f with the goal of minimizing control flow instructions.
 // After this phase returns, the order of f.Blocks matters and is the order
 // in which those blocks will appear in the assembly output.
@@ -11,13 +16,57 @@ func layout(f *Func) {
 	order := make([]*Block, 0, f.NumBlocks())
 	scheduled := make([]bool, f.NumBlocks())
 	idToBlock := make([]*Block, f.NumBlocks())
-	indegree := make([]int, f.NumBlocks())
+	indegree := make([]int32, f.NumBlocks())
+	b2PO := make([]int32, f.NumBlocks()) // For each block, its PO#, or -1 if not reachable
+
 	posdegree := f.newSparseSet(f.NumBlocks()) // blocks with positive remaining degree
 	defer f.retSparseSet(posdegree)
 	zerodegree := f.newSparseSet(f.NumBlocks()) // blocks with zero remaining degree
 	defer f.retSparseSet(zerodegree)
 	exit := f.newSparseSet(f.NumBlocks()) // exit blocks
 	defer f.retSparseSet(exit)
+
+	ln := f.loopnest()
+	ln.findExits()
+	ln.calculateDepths()
+	ln.findBlocks()
+	ln.assembleChildren()
+
+	for i := range b2PO {
+		b2PO[i] = -1
+	}
+	for i, b := range ln.po {
+		b2PO[b.ID] = int32(i)
+	}
+
+	// loopSegments := make(map[*loop]*segment)
+
+	// var visitLoop func(l *loop)
+	// visitLoop = func(l *loop) {
+	// 	for _, inner := range l.children {
+	// 		visitLoop(inner)
+	// 	}
+	// 	var bestLastBlock *Block
+	// 	// the "best" last block is conditional,
+	// 	// with one branch to the header of the current block,
+	// 	// and the other branch an exit.
+	// 	// the "best" exit is one that is "nearest"
+	// 	// meaning a good choice to directly follow.
+	// 	// For example, that has the larger post-order number.
+	// 	for _, b := range l.blocks {
+
+	// 	}
+	// }
+
+	// // iterate over top level loops,
+	// // recursively visited interior loops,
+	// // and layout loops from inner to outer.
+	// for _, l := range ln.loops {
+	// 	if l.outer != nil {
+	// 		continue
+	// 	}
+
+	// }
 
 	// Initialize indegree of each block
 	for _, b := range f.Blocks {
@@ -28,7 +77,7 @@ func layout(f *Func) {
 			exit.add(b.ID)
 			continue
 		}
-		indegree[b.ID] = len(b.Preds)
+		indegree[b.ID] = int32(len(b.Preds))
 		if len(b.Preds) == 0 {
 			zerodegree.add(b.ID)
 		} else {
@@ -41,6 +90,17 @@ blockloop:
 	for {
 		// add block to schedule
 		b := idToBlock[bid]
+		if len(order) > 0 {
+			// Check for transition into new loop, if so, attempt to pre-rotate
+			if bl, bendl := ln.b2l[b.ID], ln.b2l[order[len(order)-1].ID]; bl != nil &&
+				(bendl == nil || bl != bendl && bl.depth <= bendl.depth) {
+				bstart := ln.goodtop(bl, b2PO)
+				if bstart != nil && !scheduled[bstart.ID] {
+					b = bstart
+					bid = b.ID
+				}
+			}
+		}
 		order = append(order, b)
 		scheduled[bid] = true
 		if len(order) == len(f.Blocks) {
@@ -74,7 +134,7 @@ blockloop:
 
 		// Use degree for now.
 		bid = 0
-		mindegree := f.NumBlocks()
+		mindegree := int32(f.NumBlocks())
 		for _, e := range order[len(order)-1].Succs {
 			c := e.b
 			if scheduled[c.ID] || c.Kind == BlockExit {
@@ -117,4 +177,88 @@ blockloop:
 		}
 	}
 	f.Blocks = order
+}
+
+// goodtop attempts to find a good first block for the linearized layout of loop l.
+func (ln *loopnest) goodtop(l *loop, b2PO []int32) *Block {
+	var dominatesOne, dominatesAll, dOneExit, dAllExit *Block
+
+	dom := ln.f.Idom()
+blocks:
+	for _, b := range l.blocks {
+		if b.Kind != BlockIf {
+			continue
+		}
+		b0 := b.Succs[0].Block()
+		b1 := b.Succs[1].Block()
+		l0 := ln.b2l[b0.ID]
+		l1 := ln.b2l[b1.ID]
+		if l0 == l1 {
+			// note one successor must be in loop, so both are in loop if equal.
+			continue
+		}
+		if (l0 == l || l0.depth < l.depth) && (l1 == l || l1.depth < l.depth) {
+			// one or both successors are nested within l
+			continue
+		}
+		bExit := b0
+		if l1 != l {
+			bExit = b1
+		}
+
+		// Therefore one successor is an exit.
+		if dominatesOne == nil || b2PO[bExit.ID] > b2PO[dOneExit.ID] {
+			dominatesOne = b
+			dOneExit = bExit
+		}
+		for _, e := range l.header.Preds {
+			be := e.Block()
+			if ln.sdom.isAncestorEq(l.header, be) {
+				// be is the source of a backedge, does b dominate it?
+				if !ln.sdom.isAncestorEq(b, be) {
+					continue blocks
+				}
+			}
+		}
+		if dominatesAll == nil || b2PO[bExit.ID] > b2PO[dAllExit.ID] {
+			dominatesAll = b
+			dAllExit = bExit
+		}
+	}
+	if dominatesAll != nil {
+		// pick the successor that is in the loop for top of loop.
+		return dominatesAll.Succs[ln.inLoopSuccessorIndex(l, dominatesAll)].Block()
+	}
+	if dominatesOne != nil {
+		// look for BlockPlain predecessors of in-loop successor of dominatesOne,
+		// iterate on their dominators as long as they do not dominate B,
+		// iterate up dominators to B.  Ideally find A such that one successor
+		// dominates B, the other dominates the in-loop successor of B (e.g.,
+		// the root of a diamond).
+		inloop := ln.inLoopSuccessorIndex(l, dominatesOne)
+		inloopBlock := dominatesOne.Succs[inloop].Block()
+		for _, p := range inloopBlock.Preds {
+			pb := p.Block()
+			if pb.Kind == BlockPlain {
+				// Should always terminate on loop header
+				for ib := pb; ib != nil && !ln.sdom.isAncestorEq(ib, dominatesOne); ib = dom[ib.ID] {
+					pb = ib
+				}
+				// This is a little uncertain -- could make branches towards dominatesOne be less likely.
+				return pb
+			}
+		}
+		// Could come here for case of A -> (B,C); B -> (X, C); triangle, not diamond.
+		return inloopBlock
+	}
+	return nil
+}
+
+func (ln *loopnest) inLoopSuccessorIndex(l *loop, b *Block) int {
+	s0 := b.Succs[0].Block()
+	l0 := ln.b2l[s0.ID]
+	if l0 == nil || l0.depth > l.depth || l0 != l && l0.depth == l.depth {
+		return 1
+	}
+	return 0
 }
