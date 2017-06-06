3e206cf9ee846f54b2ca2160bb1c5c570e333140
Mikio Hara <mikioh.mikioh@gmail.com>
2017-06-06 11:12:34 +0900

    net: update documentation on IP.IsUnspecified
    Fixes #19344.
    
    Change-Id: Ic6fc7485cb50bfae99fda69d0cd9c4ae434af4c3

--- src/net/ip.go
+++ src/net/ip.go
@@ -108,7 +108,9 @@ var (
 	IPv6linklocalallrouters    = IP{0xff, 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x02}
 )
 
-// IsUnspecified reports whether ip is an unspecified address.
+// IsUnspecified reports whether ip is an unspecified address, either
+// the IPv4 address "0.0.0.0" or the IPv6 address "::", which
+// represents the absence of an address.
 func (ip IP) IsUnspecified() bool {
 	return ip.Equal(IPv4zero) || ip.Equal(IPv6unspecified)
 }
