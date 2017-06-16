7f7eefaf6d863d2db519d164bf0ce190921d2d76
Wei Xiao <wei.xiao@arm.com>
2017-06-16 06:45:14 +0000

    bytes: add optimized countByte for arm64
    Use SIMD instructions when counting a single byte.
    Inspired from runtime IndexByte implementation.
    
    Benchmark results of bytes, where 1 byte in every 8 is the one we are looking:
    
    name               old time/op   new time/op    delta
    CountSingle/10-8    96.1ns ± 1%    38.8ns ± 0%    -59.64%  (p=0.000 n=9+7)
    CountSingle/32-8     172ns ± 2%      36ns ± 1%    -79.27%  (p=0.000 n=10+10)
    CountSingle/4K-8    18.2µs ± 1%     0.9µs ± 0%    -95.17%  (p=0.000 n=9+10)
    CountSingle/4M-8    18.4ms ± 0%     0.9ms ± 0%    -95.00%  (p=0.000 n=10+9)
    CountSingle/64M-8    284ms ± 4%      19ms ± 0%    -93.40%  (p=0.000 n=10+10)
    
    name               old speed     new speed      delta
    CountSingle/10-8   104MB/s ± 1%   258MB/s ± 0%   +147.99%  (p=0.000 n=9+10)
    CountSingle/32-8   185MB/s ± 1%   897MB/s ± 1%   +385.33%  (p=0.000 n=9+10)
    CountSingle/4K-8   225MB/s ± 1%  4658MB/s ± 0%  +1967.40%  (p=0.000 n=9+10)
    CountSingle/4M-8   228MB/s ± 0%  4555MB/s ± 0%  +1901.71%  (p=0.000 n=10+9)
    CountSingle/64M-8  236MB/s ± 4%  3575MB/s ± 0%  +1414.69%  (p=0.000 n=10+10)
    
    Change-Id: Ifccb51b3c8658c49773fe05147c3cf3aead361e5

--- src/runtime/asm_arm64.s
+++ src/runtime/asm_arm64.s
@@ -889,6 +889,74 @@ equal:
 	MOVB	R0, ret+48(FP)
 	RET
 
+TEXT bytes·countByte(SB),NOSPLIT,$0-40
+	MOVD	b+0(FP), R0
+	MOVD	b_len+8(FP), R2
+	MOVBU	c+24(FP), R1
+	ADD	$40, RSP, R8
+	B	runtime·countByte<>(SB)
+
+// input:
+//   R0: data
+//   R1: byte sought
+//   R2: data len
+//   R8: address to put result
+TEXT runtime·countByte<>(SB),NOSPLIT,$0
+	MOVD	$0, R11
+	CBZ	R2, done
+	ANDS	$0x1f, R0, R9
+	BEQ	chunk
+	// Work with not 32-byte aligned head
+	BIC	$0x1f, R0, R3
+	ADD	$0x20, R3
+head_loop:
+	MOVBU.P	1(R0), R5
+	CMP	R5, R1
+	CINC	EQ, R11, R11
+	SUB	$1, R2, R2
+	CBZ	R2, done
+	CMP	R0, R3
+	BNE	head_loop
+	// Work with 32-byte aligned chunks
+chunk:
+	BIC	$0x1f, R2, R9
+	// The first chunk can also be the last
+	CBZ	R9, tail
+	ADD	R0, R9, R3
+	MOVD	$1, R5
+	VMOV	R5, V5.B16
+	SUB	R9, R2, R2
+	VMOV	R1, V0.B16
+	// Count the target byte in 32-byte chunk
+chunk_loop:
+	VLD1.P	(R0), [V1.B16, V2.B16]
+	CMP	R0, R3
+	VCMEQ	V0.B16, V1.B16, V3.B16
+	VCMEQ	V0.B16, V2.B16, V4.B16
+	// Clear the higher 7 bits
+	VAND	V5.B16, V3.B16, V3.B16
+	VAND	V5.B16, V4.B16, V4.B16
+	// Count lanes match the requested byte
+	VADDP	V4.B16, V3.B16, V6.B16 // 32B->16B
+	VADDP	V6.B16, V6.B16, V6.B16 // 16B->8B
+	VADDP	V6.B16, V6.B16, V6.B16 // 8B->4B
+	VADDP	V6.B16, V6.B16, V6.B16 // 4B->2B
+	VADDP	V6.B16, V6.B16, V6.B16 // 2B->1B
+	VMOV	V6.B[0], R6
+	ADD	R6, R11, R11
+	BNE	chunk_loop
+	CBZ	R2, done
+	// Work with not 32-byte aligned tail
+tail:
+	MOVBU.P	1(R0), R5
+	SUB	$1, R2, R2
+	CMP	R5, R1
+	CINC	EQ, R11, R11
+	CBNZ	R2, tail
+done:
+	MOVD	R11, (R8)
+	RET
+
 TEXT runtime·return0(SB), NOSPLIT, $0
 	MOVW	$0, R0
 	RET
