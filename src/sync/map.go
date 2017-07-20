4ae00dae6dbc4c8313b2ae8cc4ca9389111a09c5
Michael Stapelberg <stapelberg@google.com>
2017-07-20 08:16:36 -0700

    sync: update Map documentation with usage rule of thumb
    As per bcmills’s lightning talk at GopherCon 2017:
    https://docs.google.com/presentation/d/1WA_GsCR2p9r15auhY6OgFk3Ls9tbsARRyzifQoBRGwQ/edit
    
    Change-Id: I12dd0daa608af175d110298780f32c6dc5e1e0a0

--- src/sync/map.go
+++ src/sync/map.go
@@ -12,6 +12,10 @@ import (
 // Map is a concurrent map with amortized-constant-time loads, stores, and deletes.
 // It is safe for multiple goroutines to call a Map's methods concurrently.
 //
+// Use a Map when you have stable write-once, read-many keys, which you are
+// accessing from concurrent loops. Otherwise, prefer a read-write mutex, which
+// has acceptable performance and better type-safety.
+//
 // The zero Map is valid and empty.
 //
 // A Map must not be copied after first use.
