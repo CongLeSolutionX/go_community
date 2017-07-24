e880b25c9068f58fadd5db06ab1a135c733b0840
Guilherme Rezende <guilhermebr@gmail.com>
2017-07-24 17:04:02 -0300

    net/mail: parse cases when phrase has special chars without quotes
    Change-Id: I00b6667fb5fee78559a391815f58760a2baea90e

--- src/net/mail/message.go
+++ src/net/mail/message.go
@@ -190,7 +190,7 @@ func (a *Address) String() string {
 	// Add quotes if needed
 	quoteLocal := false
 	for i, r := range local {
-		if isAtext(r, false) {
+		if isAtext(r, false, false) {
 			continue
 		}
 		if r == '.' {
@@ -490,12 +490,11 @@ func (p *addrParser) consumeAtom(dot bool, permissive bool) (atom string, err er
 Loop:
 	for {
 		r, size := utf8.DecodeRuneInString(p.s[i:])
-
 		switch {
 		case size == 1 && r == utf8.RuneError:
 			return "", fmt.Errorf("mail: invalid utf-8 in address: %q", p.s)
 
-		case size == 0 || !isAtext(r, dot):
+		case size == 0 || !isAtext(r, dot, permissive):
 			break Loop
 
 		default:
@@ -580,12 +579,15 @@ func (e charsetError) Error() string {
 
 // isAtext reports whether r is an RFC 5322 atext character.
 // If dot is true, period is included.
-func isAtext(r rune, dot bool) bool {
+func isAtext(r rune, dot, permissive bool) bool {
 	switch r {
 	case '.':
 		return dot
 
-	case '(', ')', '<', '>', '[', ']', ':', ';', '@', '\\', ',', '"': // RFC 5322 3.2.3. specials
+	case '(', ')', '[', ']', ':', ';', '@', '\\', ',': // RFC 5322 3.2.3. specials
+		return permissive
+
+	case '<', '>', '"': // RFC 5322 3.2.3. specials
 		return false
 	}
 	return isVchar(r)
