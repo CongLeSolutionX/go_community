cb2c41ace07363635b7471f3893514d6d4f7dc68
Seiji Takahashi <timaki.st@gmail.com>
2017-07-30 21:00:09 +0900

    cmd/go/internal/test: Fix go test -list command to show all examples
    fix #21205
    
    Change-Id: I9683e17bdb6016a9b75f483b65c9b68027521952
    
    [fix] changed interface for test and added test
    
    Change-Id: I81b001eb42cbf2a5d5b7b82eb63548b22f501be5

--- src/testing/testing_test.go
+++ src/testing/testing_test.go
@@ -2,17 +2,96 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package testing_test
+package testing
 
 import (
+	"flag"
+	"fmt"
 	"os"
-	"testing"
+	"reflect"
+	"regexp"
 )
 
 // This is exactly what a test would do without a TestMain.
 // It's here only so that there is at least one package in the
 // standard library with a TestMain, so that code is executed.
 
-func TestMain(m *testing.M) {
+func TestMain(m *M) {
 	os.Exit(m.Run())
+}
+
+func TestListTests(t *T) {
+	var matchString = func(pat, str string) (bool, error) {
+		if ok, err := regexp.MatchString(pat, str); err != nil || !ok {
+			return false, err
+		}
+		return true, nil
+	}
+
+	testInternal := struct {
+		tests      []InternalTest
+		benchmarks []InternalBenchmark
+		examples   []InternalExample
+	}{
+		tests: []InternalTest{
+			InternalTest{
+				Name: "TestSimple",
+				F: func(t *T) {
+					_ = fmt.Sprint("Test simple")
+				},
+			},
+		},
+		benchmarks: []InternalBenchmark{
+			InternalBenchmark{
+				Name: "BenchmarkSimple",
+				F: func(b *B) {
+					b.StopTimer()
+					b.StartTimer()
+					for i := 0; i < b.N; i++ {
+						_ = fmt.Sprint("Test for bench")
+					}
+				},
+			},
+		},
+		examples: []InternalExample{
+			InternalExample{
+				Name: "ExampleSimple",
+				F: func() {
+					fmt.Println("Test with Output.")
+
+					// Output: Test with Output.
+				},
+				Output:    "Test with Output.\n",
+				Unordered: false,
+			},
+			InternalExample{
+				Name: "ExampleWithEmptyOutput",
+				F: func() {
+					fmt.Println("")
+
+					// Output:
+				},
+				Output:    "",
+				Unordered: false,
+			},
+		},
+	}
+
+	testCases := []struct {
+		listFlag string
+		output   []string
+	}{
+		{"Test", []string{"TestSimple"}},
+		{"Benchmark", []string{"BenchmarkSimple"}},
+		{"Example", []string{"ExampleSimple", "ExampleWithEmptyOutput"}},
+	}
+
+	for _, tc := range testCases {
+		flag.Set("test.list", tc.listFlag)
+		flag.Parse()
+		results, err := listTests(matchString, testInternal.tests, testInternal.benchmarks, testInternal.examples)
+		if !reflect.DeepEqual(results, tc.output) || err != nil {
+			t.Errorf("Test case for list &s failed.", tc.listFlag)
+		}
+	}
 }
