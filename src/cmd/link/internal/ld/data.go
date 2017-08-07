de5b58c7b13e8a0d009d566ab41346d24b6fa860
Milan Knezevic <milan.knezevic@mips.com>
2017-08-07 19:50:10 +0200

    cmd/link: add support for -buildmode=c-shared for GOARCH=mips{,le}
    Change-Id: Ib3ca655d001a0381a3f13b6e7c2cd7bf1bbd0d61

--- src/cmd/link/internal/ld/data.go
+++ src/cmd/link/internal/ld/data.go
@@ -177,7 +177,7 @@ func relocsym(ctxt *Link, s *sym.Symbol) {
 		// We need to be able to reference dynimport symbols when linking against
 		// shared libraries, and Solaris and Darwin need it always
 		if ctxt.HeadType != objabi.Hsolaris && ctxt.HeadType != objabi.Hdarwin && r.Sym != nil && r.Sym.Type == sym.SDYNIMPORT && !ctxt.DynlinkingGo() && !r.Sym.Attr.SubSymbol() {
-			if !(ctxt.Arch.Family == sys.PPC64 && ctxt.LinkMode == LinkExternal && r.Sym.Name == ".TOC.") {
+			if !(ctxt.LinkMode == LinkExternal && ((ctxt.Arch.Family == sys.PPC64 && r.Sym.Name == ".TOC.") || (ctxt.Arch.Family == sys.MIPS && r.Sym.Name == "_gp_disp"))) {
 				Errorf(s, "unhandled relocation for %s (type %d (%s) rtype %d (%s))", r.Sym.Name, r.Sym.Type, r.Sym.Type, r.Type, sym.RelocName(ctxt.Arch, r.Type))
 			}
 		}
@@ -1936,7 +1936,7 @@ func assignAddress(ctxt *Link, sect *sym.Section, n int, s *sym.Symbol, va uint6
 		// Create new section, set the starting Vaddr
 		sect = addsection(ctxt.Arch, &Segtext, ".text", 05)
 		sect.Vaddr = va
-		s.Sect = sect
+		//sym.Sect = sect // TODO mips c-shared
 
 		// Create a symbol for the start of the secondary text sections
 		ctxt.Syms.Lookup(fmt.Sprintf("runtime.text.%d", n), 0).Sect = sect
