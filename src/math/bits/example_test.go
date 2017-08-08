59d5cca89c92df43d0a5b97f660f90b077670652
Seshachalam Malisetti <abbiya@gmail.com>
2017-08-08 23:56:05 +0530

    math:bits add examples for TrailingZeros functions
    Change-Id: If275b4668c9e151fc5d9c725629cba699e81e806

--- src/math/bits/example_test.go
+++ src/math/bits/example_test.go
@@ -76,3 +76,127 @@ func ExampleOnesCount64() {
 	// 1110
 	// 3
 }
+
+func ExampleTrailingZeros() {
+	fmt.Printf("%b\n", 0)
+	fmt.Println(bits.TrailingZeros(0))
+	fmt.Printf("%b\n", 1)
+	fmt.Println(bits.TrailingZeros(1))
+	fmt.Printf("%b\n", 256)
+	fmt.Println(bits.TrailingZeros(256))
+	fmt.Printf("%b\n", 65535)
+	fmt.Println(bits.TrailingZeros(65535))
+	// Output:
+	// 0
+	// 64
+	// 1
+	// 0
+	// 100000000
+	// 8
+	// 1111111111111111
+	// 0
+}
+
+func ExampleTrailingZeros8() {
+	fmt.Printf("%b\n", 0)
+	fmt.Println(bits.TrailingZeros8(0))
+	fmt.Printf("%b\n", 254)
+	fmt.Println(bits.TrailingZeros8(254))
+	// Output:
+	// 0
+	// 8
+	// 11111110
+	// 1
+}
+
+func ExampleTrailingZeros16() {
+	fmt.Printf("%b\n", 0)
+	fmt.Println(bits.TrailingZeros16(0))
+	fmt.Printf("%b\n", 254)
+	fmt.Println(bits.TrailingZeros16(254))
+	fmt.Printf("%b\n", 255)
+	fmt.Println(bits.TrailingZeros16(255))
+	fmt.Printf("%b\n", 65534)
+	fmt.Println(bits.TrailingZeros16(65534))
+	fmt.Printf("%b\n", 65535)
+	fmt.Println(bits.TrailingZeros16(65535))
+	// Output:
+	// 0
+	// 16
+	// 11111110
+	// 1
+	// 11111111
+	// 0
+	// 1111111111111110
+	// 1
+	// 1111111111111111
+	// 0
+}
+
+func ExampleTrailingZeros32() {
+	fmt.Printf("%b\n", 0)
+	fmt.Println(bits.TrailingZeros32(0))
+	fmt.Printf("%b\n", 254)
+	fmt.Println(bits.TrailingZeros32(254))
+	fmt.Printf("%b\n", 255)
+	fmt.Println(bits.TrailingZeros32(255))
+	fmt.Printf("%b\n", 65534)
+	fmt.Println(bits.TrailingZeros32(65534))
+	fmt.Printf("%b\n", 65535)
+	fmt.Println(bits.TrailingZeros32(65535))
+	fmt.Printf("%b\n", 2147483648)
+	fmt.Println(bits.TrailingZeros32(2147483648))
+	fmt.Printf("%b\n", 4294967295)
+	fmt.Println(bits.TrailingZeros32(4294967295))
+	// Output:
+	// 0
+	// 32
+	// 11111110
+	// 1
+	// 11111111
+	// 0
+	// 1111111111111110
+	// 1
+	// 1111111111111111
+	// 0
+	// 10000000000000000000000000000000
+	// 31
+	// 11111111111111111111111111111111
+	// 0
+}
+
+func ExampleTrailingZeros64() {
+	fmt.Printf("%b\n", 0)
+	fmt.Println(bits.TrailingZeros64(0))
+	fmt.Printf("%b\n", 254)
+	fmt.Println(bits.TrailingZeros64(254))
+	fmt.Printf("%b\n", 255)
+	fmt.Println(bits.TrailingZeros64(255))
+	fmt.Printf("%b\n", 65534)
+	fmt.Println(bits.TrailingZeros64(65534))
+	fmt.Printf("%b\n", 65535)
+	fmt.Println(bits.TrailingZeros64(65535))
+	fmt.Printf("%b\n", 2147483648)
+	fmt.Println(bits.TrailingZeros64(2147483648))
+	fmt.Printf("%b\n", 4294967295)
+	fmt.Println(bits.TrailingZeros64(4294967295))
+	fmt.Printf("%b\n", 9223372036854775807)
+	fmt.Println(bits.TrailingZeros64(9223372036854775807))
+	// Output:
+	// 0
+	// 64
+	// 11111110
+	// 1
+	// 11111111
+	// 0
+	// 1111111111111110
+	// 1
+	// 1111111111111111
+	// 0
+	// 10000000000000000000000000000000
+	// 31
+	// 11111111111111111111111111111111
+	// 0
+	// 111111111111111111111111111111111111111111111111111111111111111
+	// 0
+}
