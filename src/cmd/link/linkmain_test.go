172f68c3b3987e59ad3568d500a1f81db3608d77
Seiji Takahashi <timaki.st@gmail.com>
2017-08-10 02:03:17 +0900

    cmd/link: changed `var main = func() { }` to alert on build
    Fixes #21256
    
    This is another solution for the issue.
    On the below commit, I tried once to fix with validation in cmd/compile.
    But I accepted a review that I should try to validate the case in cmd/link.
    https://go-review.googlesource.com/c/53450
    
    Change-Id: I7ac0f9afb7951a840875d57501d3adbdbfa59e91

---
+++ src/cmd/link/linkmain_test.go
@@ -0,0 +1,45 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This program generates a test to verify that a program can be
+// successfully linked even when there are very large text
+// sections present.
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"internal/testenv"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func TestInvalidMainDecl(t *testing.T) {
+	testenv.MustHaveGoBuild(t)
+
+	var w bytes.Buffer
+	tmpdir, err := ioutil.TempDir("", "invalidmaindecl")
+
+	defer os.RemoveAll(tmpdir)
+
+	fmt.Fprintf(&w, "package main\n")
+	fmt.Fprintf(&w, "\nvar main = func() { }\n")
+
+	err = ioutil.WriteFile(tmpdir+"/invalidmaindecl.go", w.Bytes(), 0666)
+	if err != nil {
+		t.Fatalf("can't write output: %v\n", err)
+	}
+
+	// Build and run with internal linking.
+	os.Chdir(tmpdir)
+	cmd := exec.Command(testenv.GoToolPath(t), "build", "-o", "invalidmaindecl")
+	out, err := cmd.CombinedOutput()
+	if err == nil || !strings.Contains(string(out), "main.main should be declared as a function") {
+		t.Fatalf("Build failed for invalidmaindecl program: %v, output: %s", err, out)
+	}
+}
