d93fa8e34f6d5b8a32f4192672d5f9171daa2045
Seiji Takahashi <timaki.st@gmail.com>
2017-08-10 02:03:17 +0900

    cmd/link: changed `var main = func() { }` to alert on build
    Fixes #21256
    
    This is another solution for the issue.
    On the below commit, I tried once to fix with validation in cmd/compile.
    But I accepted a review that I should try to validate the case in cmd/link.
    https://golang.org/cl/53450
    
    Change-Id: I7ac0f9afb7951a840875d57501d3adbdbfa59e91

---
+++ src/cmd/link/linkmain_test.go
@@ -0,0 +1,29 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"internal/testenv"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func TestInvalidMainDecl(t *testing.T) {
+	testenv.MustHaveGoBuild(t)
+
+	src := filepath.Join("testdata", "invalidmaindecl.go")
+	dst := filepath.Join("testdata", "invalidmaindecl")
+
+	cmd := exec.Command(testenv.GoToolPath(t), "build", "-o", dst, src)
+	out, err := cmd.CombinedOutput()
+	if err == nil {
+		t.Error("build succeeded unexpectedly")
+	}
+	if !strings.Contains(string(out), "main.main must be a function") {
+		t.Errorf("Build failed for invalidmaindecl program: %v, output: %s", err, out)
+	}
+}
