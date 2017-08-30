dfc8df16a7edc91f35964ec62736503ffbdfabbc
Hiroshi Ioka <hirochachacha@gmail.com>
2017-08-30 11:14:40 +0900

    cmd/internal/goobj: add tests
    Change-Id: I4a0fe1c8625e7e9adfd84ac6910da83d0268f928

---
+++ src/cmd/internal/goobj/testdata/make.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Copyright 2017 The Go Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+set -e
+
+goos=$(go env GOOS)
+goarch=$(go env GOARCH)
+
+function cleanup() {
+	rm -f go1.o go2.o
+}
+trap cleanup EXIT
+
+go tool compile -o go1.o go1.go
+go tool compile -o go2.o go2.go
+
+cp go1.o $goos-$goarch-goobj
+go tool pack c $goos-$goarch-archive go1.o go2.o
