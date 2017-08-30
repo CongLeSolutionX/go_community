b576cacf256f3df78c03d68731ccd172cb59310e
Hiroshi Ioka <hirochachacha@gmail.com>
2017-08-30 11:14:40 +0900

    cmd/internal/goobj: add tests
    Change-Id: I4a0fe1c8625e7e9adfd84ac6910da83d0268f928

---
+++ src/cmd/internal/goobj/goobj_test.go
@@ -0,0 +1,88 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package goobj
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func TestParseGoobj(t *testing.T) {
+	matches, err := filepath.Glob("testdata/*-goobj")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, path := range matches {
+		tmp := strings.Split(filepath.Base(path), "-")
+		if len(tmp) != 3 {
+			t.Fatalf("unexpected filepath %s", path)
+		}
+		goarch := tmp[1]
+		f, err := os.Open(path)
+		if err != nil {
+			t.Fatal(err)
+		}
+		p, err := Parse(f, "mypkg")
+		if err != nil {
+			t.Error(err)
+		}
+		if p.Arch != goarch {
+			t.Errorf("%s: got %v, want %v", path, goarch, p.Arch)
+		}
+		var found bool
+		for _, s := range p.Syms {
+			if s.Name == "mypkg.go1" {
+				found = true
+				break
+			}
+		}
+		if !found {
+			t.Errorf(`%s: symbol "mypkg.go1" not found`, path)
+		}
+	}
+}
+
+func TestParseArchive(t *testing.T) {
+	matches, err := filepath.Glob("testdata/*-archive")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, path := range matches {
+		tmp := strings.Split(filepath.Base(path), "-")
+		if len(tmp) != 3 {
+			t.Fatalf("unexpected filepath %s", path)
+		}
+		goarch := tmp[1]
+		f, err := os.Open(path)
+		if err != nil {
+			t.Fatal(err)
+		}
+		p, err := Parse(f, "mypkg")
+		if err != nil {
+			t.Error(err)
+		}
+		if p.Arch != goarch {
+			t.Errorf("%s: got %v, want %v", path, goarch, p.Arch)
+		}
+		var found1 bool
+		var found2 bool
+		for _, s := range p.Syms {
+			if s.Name == "mypkg.go1" {
+				found1 = true
+			}
+			if s.Name == "mypkg.go2" {
+				found2 = true
+			}
+		}
+		if !found1 {
+			t.Errorf(`%s: symbol "mypkg.go1" not found`, path)
+		}
+		if !found2 {
+			t.Errorf(`%s: symbol "mypkg.go2" not found`, path)
+		}
+	}
+}
