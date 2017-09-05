8097b9903fa53cd7bc6e3c1ea4709e02f35e2817
Elbert Fliek <efliek@gmail.com>
2017-09-05 20:21:41 +0200

    time: add an example to the NewTicker function
    Change-Id: Idad9cdee36679373ee223ff2bd4c021ea0afcce1

--- src/time/example_test.go
+++ src/time/example_test.go
@@ -127,6 +127,14 @@ func ExampleDate() {
 	// Output: Go launched at 2009-11-10 15:00:00 -0800 PST
 }
 
+func ExampleNewTicker() {
+	ticker := time.NewTicker(time.Second)
+	start := time.Now()
+	for range ticker.C {
+		fmt.Printf("tick: %v\n", time.Since(start))
+	}
+}
+
 func ExampleTime_Format() {
 	// Parse a time value from a string in the standard Unix format.
 	t, err := time.Parse(time.UnixDate, "Sat Mar  7 11:06:39 PST 2015")
