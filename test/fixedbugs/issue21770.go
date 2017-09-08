172f0230ae8cdf68c8b2cd879fd08cb8e9ebbcae
Than McIntosh <thanm@google.com>
2017-09-08 11:11:19 -0400

    test: add test that caused gccgo incorrect compilation
    Updates #21770
    
    Change-Id: Ic31c3bdae30797f406f25c737b83bbe2de1ed1da

---
+++ test/fixedbugs/issue21770.go
@@ -0,0 +1,18 @@
+// errorcheck
+
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package p
+
+type PP **struct{ f int }
+
+func f() {
+	// anonymous type
+	var p **struct{ f int }
+	p.f = 0 // ERROR "field"
+	// named type
+	var p2 PP
+	p2.f = 0 // ERROR "field"
+}
