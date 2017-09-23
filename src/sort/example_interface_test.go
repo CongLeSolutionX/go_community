d2e47114d0389f42933225b090ecfbcd75446682
Agniva De Sarker <agnivade@yahoo.co.in>
2017-09-23 22:29:42 +0530

    sort: change main example to use Slice instead of Sort
    - This is to let sort.Slice get more prominence so that newbies
    can use this function for the most common use case.
    
    Fixes #21989
    
    Change-Id: I0b180cc20256f5f09065b722e191c508c872f4f7

--- src/sort/example_interface_test.go
+++ src/sort/example_interface_test.go
@@ -18,14 +18,6 @@ func (p Person) String() string {
 	return fmt.Sprintf("%s: %d", p.Name, p.Age)
 }
 
-// ByAge implements sort.Interface for []Person based on
-// the Age field.
-type ByAge []Person
-
-func (a ByAge) Len() int           { return len(a) }
-func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
-
 func Example() {
 	people := []Person{
 		{"Bob", 31},
@@ -35,7 +27,9 @@ func Example() {
 	}
 
 	fmt.Println(people)
-	sort.Sort(ByAge(people))
+	sort.Slice(people, func(i, j int) bool {
+		return people[i].Age < people[j].Age
+	})
 	fmt.Println(people)
 
 	// Output:
