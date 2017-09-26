7a5b35ba2e134636cb36848b7af66ed1a6e80bf0
Jean-Francois Cantin <jfcantin@gmail.com>
2017-09-26 16:07:27 -0600

    path/filepath: add examples for IsAbs, Base, Clean, Dir, Walk
    Fix: #22052
    
    Change-Id: Ia056871b35ecc1a8c5ac891402fc1c5702731623

--- src/path/filepath/example_unix_test.go
+++ src/path/filepath/example_unix_test.go
@@ -8,6 +8,8 @@ package filepath_test
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"path/filepath"
 )
 
@@ -78,4 +80,191 @@ func ExampleJoin() {
 	// a/b/c
 	// a/b/c
 	// a/b/c
+}
+func ExampleIsAbs() {
+	paths := []string{
+		"",
+		"/",
+		"/usr/bin/gcc",
+		"..",
+		"/a/../bb",
+		".",
+		"./",
+		"lala",
+	}
+	fmt.Println("On Unix:")
+	for _, p := range paths {
+		isAbs := filepath.IsAbs(p)
+		fmt.Printf("input: %q\n\tisAbs: %t\n", p, isAbs)
+	}
+	// Output:
+	// On Unix:
+	// input: ""
+	// 	isAbs: false
+	// input: "/"
+	// 	isAbs: true
+	// input: "/usr/bin/gcc"
+	// 	isAbs: true
+	// input: ".."
+	// 	isAbs: false
+	// input: "/a/../bb"
+	// 	isAbs: true
+	// input: "."
+	// 	isAbs: false
+	// input: "./"
+	// 	isAbs: false
+	// input: "lala"
+	// 	isAbs: false
+}
+
+func ExampleBase() {
+	paths := []string{
+		"",
+		".",
+		"/.",
+		"/",
+		"////",
+		"x/",
+		"abc",
+		"abc/def",
+		"a/b/.x",
+		"a/b/c.",
+		"a/b/c.x",
+	}
+	fmt.Println("On Unix:")
+	for _, p := range paths {
+		base := filepath.Base(p)
+		fmt.Printf("input: %q\n\tbase: %q\n", p, base)
+	}
+	// Output:
+	// On Unix:
+	// input: ""
+	// 	base: "."
+	// input: "."
+	// 	base: "."
+	// input: "/."
+	// 	base: "."
+	// input: "/"
+	// 	base: "/"
+	// input: "////"
+	// 	base: "/"
+	// input: "x/"
+	// 	base: "x"
+	// input: "abc"
+	// 	base: "abc"
+	// input: "abc/def"
+	// 	base: "def"
+	// input: "a/b/.x"
+	// 	base: ".x"
+	// input: "a/b/c."
+	// 	base: "c."
+	// input: "a/b/c.x"
+	// 	base: "c.x"
+}
+
+func ExampleClean() {
+	paths := []string{
+		"",
+		".",
+		"../..",
+		"a/b/c",
+		"abc/def/",
+		"./",
+		"../../",
+		"abc//def///ghi//",
+		"abc/def/ghi/../jkl",
+		"/abc/def/../..",
+		"abc//./../def",
+		"abc/../../././../def",
+	}
+	fmt.Println("On Unix:")
+	for _, p := range paths {
+		clean := filepath.Clean(p)
+		fmt.Printf("input: %q\n\tclean: %q\n", p, clean)
+	}
+	// Output:
+	// On Unix:
+	// input: ""
+	// 	clean: "."
+	// input: "."
+	// 	clean: "."
+	// input: "../.."
+	// 	clean: "../.."
+	// input: "a/b/c"
+	// 	clean: "a/b/c"
+	// input: "abc/def/"
+	// 	clean: "abc/def"
+	// input: "./"
+	// 	clean: "."
+	// input: "../../"
+	// 	clean: "../.."
+	// input: "abc//def///ghi//"
+	// 	clean: "abc/def/ghi"
+	// input: "abc/def/ghi/../jkl"
+	// 	clean: "abc/def/jkl"
+	// input: "/abc/def/../.."
+	// 	clean: "/"
+	// input: "abc//./../def"
+	// 	clean: "def"
+	// input: "abc/../../././../def"
+	// 	clean: "../../def"
+}
+
+func ExampleDir() {
+	paths := []string{
+		"",
+		".",
+		"/.",
+		"/",
+		"////",
+		"/foo",
+		"x/",
+		"abc",
+		"abc/def",
+		"a/b/.x",
+		"a/b/c.",
+		"a/b/c.x",
+	}
+	fmt.Println("On Unix:")
+	for _, p := range paths {
+		dir := filepath.Dir(p)
+		fmt.Printf("input: %q\n\tdir: %q\n", p, dir)
+	}
+	// Output:
+	// On Unix:
+	// input: ""
+	// 	dir: "."
+	// input: "."
+	// 	dir: "."
+	// input: "/."
+	// 	dir: "/"
+	// input: "/"
+	// 	dir: "/"
+	// input: "////"
+	// 	dir: "/"
+	// input: "/foo"
+	// 	dir: "/"
+	// input: "x/"
+	// 	dir: "x"
+	// input: "abc"
+	// 	dir: "."
+	// input: "abc/def"
+	// 	dir: "abc"
+	// input: "a/b/.x"
+	// 	dir: "a/b"
+	// input: "a/b/c."
+	// 	dir: "a/b"
+	// input: "a/b/c.x"
+	// 	dir: "a/b"
+}
+
+func ExampleWalk() {
+	dir := "dir/to/walk"
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		fmt.Printf("visited file: %q in rootdir: %q\n", path, dir)
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
