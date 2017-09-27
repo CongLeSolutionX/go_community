ca174ee487a6ed398bff7056a8934c6139f6f145
Tim Cooper <tim.cooper@layeh.com>
2017-09-26 21:14:03 -0300

    text/template: add break, continue actions in ranges
    Adds the two range control actions "break" and "continue". They act the
    same as the Go keywords break and continue, but are simplified in that
    only the innermost range statement can be broken out of or continued.
    
    Fixes #20531
    
    Change-Id: I4412b3bbfd4dadb0ab74ae718e308c1ac7a0a1e9

--- src/text/template/doc.go
+++ src/text/template/doc.go
@@ -110,6 +110,14 @@ data, defined in detail in the corresponding sections that follow.
 		T0 is executed; otherwise, dot is set to the successive elements
 		of the array, slice, or map and T1 is executed.
 
+	{{break}}
+		Breaks out of the innermost range action. Can only be used
+		inside of a range.
+
+	{{continue}}
+		Begins the next iteration of the innermost range action. Can
+		only be used inside of a range.
+
 	{{template "name"}}
 		The template with the specified name is executed with nil data.
 
