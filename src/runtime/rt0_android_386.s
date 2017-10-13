dea972410dccb6e8c85b4385e1178f9b3c2d978b
Ian Lance Taylor <iant@golang.org>
2017-10-12 19:46:42 -0700

    runtime: unify 386 entry point code
    Unify the 386 entry point code as much as possible.
    
    The main function could not be unified because on Windows 386 it is
    called _main. Putting main in asm_386.s caused multiple definition
    errors when using the external linker.
    
    Add the _lib entry point to various operating systems.  A future CL
    will enable c-archive/c-shared mode for those targets.
    
    Fix _rt0_386_windows_lib_go--it was passing arguments as though it
    were amd64.
    
    Change-Id: Ic73f1c95cdbcbea87f633f4a29bbc218a5db4f58

--- src/runtime/rt0_android_386.s
+++ src/runtime/rt0_android_386.s
@@ -4,29 +4,18 @@
 
 #include "textflag.h"
 
-TEXT _rt0_386_android(SB),NOSPLIT,$8
-	MOVL	8(SP), AX  // argc
-	LEAL	12(SP), BX  // argv
-	MOVL	AX, 0(SP)
-	MOVL	BX, 4(SP)
-	CALL	main(SB)
-	INT	$3
+TEXT _rt0_386_android(SB),NOSPLIT,$-8
+	JMP	_rt0_386(SB)
 
 TEXT _rt0_386_android_lib(SB),NOSPLIT,$0
 	PUSHL	$_rt0_386_android_argv(SB)  // argv
 	PUSHL	$1  // argc
-	CALL	_rt0_386_linux_lib(SB)
-	POPL	AX
-	POPL	AX
-	RET
+	JMP	_rt0_386_lib(SB)
 
-DATA _rt0_386_android_argv+0x00(SB)/4,$_rt0_386_android_argv0(SB)
-DATA _rt0_386_android_argv+0x04(SB)/4,$0  // argv terminate
-DATA _rt0_386_android_argv+0x08(SB)/4,$0  // envp terminate
-DATA _rt0_386_android_argv+0x0c(SB)/4,$0  // auxv terminate
-GLOBL _rt0_386_android_argv(SB),NOPTR,$0x10
+TEXT main(SB),NOSPLIT,$-8
+	// Remove the return address from the stack.
+	// rt0_go doesn't expect it to be there.
+	ADDL	$4, SP
+	JMP	runtime·rt0_go(SB)
 
 // TODO: wire up necessary VDSO (see os_linux_386.go)
-
-DATA _rt0_386_android_argv0(SB)/8, $"gojni"
-GLOBL _rt0_386_android_argv0(SB),RODATA,$8
