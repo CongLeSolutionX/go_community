81997fee565b05024e7a619c34692923f8180088
Radek Sohlich <sohlich@gmail.com>
2017-10-31 17:59:29 +0100

    os/signal: improve documentation for the Notify function
    It is easy to miss the documentation information that no arguments
    in the Notify function means that the Notify will catch all possible signals.
    So the example was added with explicit comment above the Notify usage.
    
    Fixes #22257
    
    Change-Id: Ia6a16dd4a419f7c77d89020ca5db85979b5b474e

--- src/os/signal/example_test.go
+++ src/os/signal/example_test.go
@@ -21,3 +21,19 @@ func ExampleNotify() {
 	s := <-c
 	fmt.Println("Got signal:", s)
 }
+
+func ExampleNotify_noSignalDefined() {
+	// Set up channel on which to send signal notifications.
+	// The buffered channel is used
+	// to avoid missing the signal.
+	c := make(chan os.Signal, 1)
+
+	// No signals are defined in
+	// Notify arguments. This way
+	// all signals would be catched.
+	signal.Notify(c)
+
+	// Block until any signal is received.
+	s := <-c
+	fmt.Println("Got signal:", s)
+}
