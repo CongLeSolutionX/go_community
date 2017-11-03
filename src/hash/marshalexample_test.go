a8283e39476d806e6378e2a9dfa7d8dcdb231f6d
Tim Cooper <tim.cooper@layeh.com>
2017-11-03 20:17:08 -0300

    hash: add marshaling, unmarshaling example
    Example usage of functionality implemented in CL 66710.
    
    Change-Id: I87d6e4d2fb7a60e4ba1e6ef02715480eb7e8f8bd

---
+++ src/hash/marshalexample_test.go
@@ -0,0 +1,46 @@
+package hash_test
+
+import (
+	"crypto/sha256"
+	"encoding"
+	"fmt"
+	"log"
+)
+
+func Example_binaryMarshaler() {
+	const (
+		input1 = "The tunneling gopher digs downwards, "
+		input2 = "unaware of what he will find."
+	)
+
+	first := sha256.New()
+	first.Write([]byte(input1))
+
+	marshaler, ok := first.(encoding.BinaryMarshaler)
+	if !ok {
+		log.Fatal("first does not implement encoding.BinaryMarshaler")
+	}
+	state, err := marshaler.MarshalBinary()
+	if err != nil {
+		log.Fatal("unable to marshal hash:", err)
+	}
+
+	second := sha256.New()
+
+	unmarshaler, ok := second.(encoding.BinaryUnmarshaler)
+	if !ok {
+		log.Fatal("second does not implement encoding.BinaryUnmarshaler")
+	}
+	if err := unmarshaler.UnmarshalBinary(state); err != nil {
+		log.Fatal("unable to unmarshal hash:", err)
+	}
+
+	first.Write([]byte(input2))
+	second.Write([]byte(input2))
+
+	fmt.Printf("First sum:  %2x\n", first.Sum(nil))
+	fmt.Printf("Second sum: %2x\n", second.Sum(nil))
+	// Output:
+	// First sum:  57d51a066f3a39942649cd9a76c77e97ceab246756ff3888659e6aa5a07f4a52
+	// Second sum: 57d51a066f3a39942649cd9a76c77e97ceab246756ff3888659e6aa5a07f4a52
+}
