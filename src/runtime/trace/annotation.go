f034caa9eb5de0f710780cb7c25059242248b6c0
Hana Kim <hakim@google.com>
2017-11-09 11:39:10 -0500

    runtime/trace: user annotation API
    This CL presents the proposed user annotation API skeleton.
    
    Design doc https://goo.gl/iqJfJ3
    
    Implementation CLs are followed.
    
    The API introduces three basic building blocks. Log, Span, and Task.
    
    Log is for basic logging. When called, the message will be recorded
    to the trace along with timestamp, goroutine id, and stack info.
    
       trace.Log(ctx, messageType message)
    
    Span can be thought as an extension of log to record interesting
    time interval during a goroutine's execution. A span is local to a
    goroutine by definition.
    
       trace.WithSpan(ctx, "very expensive op", func(ctx context) {
          /* do something very expensive */
       })
    
    Task is higher-level concept that aids tracing of complex operations
    that encompass multiple goroutines or are asynchronous.
    For example, an RPC request, a HTTP request, a file write, or a
    batch job can be traced with a Task.
    
    Note we chose to design the API around context.Context so it allows
    easier integration with other tracing tools, often designed around
    context.Context as well. Log and WithSpan APIs recognize the task
    information embedded in the context and record it in the trace as
    well. That allows the Go execution tracer to associate and group
    the spans and log messages based on the task information.
    
    In order to create a Task,
    
       ctx, end := trace.NewContext(ctx, "a task")
       defer end()
    
    The Go execution tracer measures the time between the task created
    and the task ended for the task latency.
    
    More discussion history in golang.org/cl/59572.
    
    R=go1.11
    
    Change-Id: I59a937048294dafd23a75cf1723c6db461b193cd

---
+++ src/runtime/trace/annotation.go
@@ -0,0 +1,174 @@
+package trace
+
+import (
+	"context"
+	"fmt"
+	"sync/atomic"
+	_ "unsafe"
+)
+
+type traceContextKey struct{}
+
+// NewContext creates a child context with a new task instance with
+// the type taskType. If the input context contains a task, the
+// new task is its subtask.
+//
+// The taskType is used to classify task instances. Analysis tools
+// like the Go execution tracer may assume there are only a bounded
+// number of unique task types in the system.
+//
+// The returned end function is used to mark the task's end.
+// Tools like the Go execution trace measure the latency of each task
+// using the task creation time and the end time determined by the task
+// end mark, and provide the latency distribution per task type.
+// In case the end function is called multiple times, only the first
+// call is used in the latency measurement.
+//
+//   ctx, taskEnd := trace.NewContext(ctx, "awesome task")
+//   trace.WithSpan(ctx, prepWork)
+//   // preparation of the task
+//   go func() {  // continue processing the task in a separate goroutine.
+//       defer taskEnd()
+//       trace.WithSpan(ctx, remainingWork)
+//   }
+//
+// This API is EXPERIMENTAL and may change in the future.
+func NewContext(pctx context.Context, taskType string) (ctx context.Context, end func()) {
+	pid := fromContext(pctx).id
+	id := newID()
+	userTaskCreate(id, pid, taskType)
+	s := &task{id: id}
+	return context.WithValue(pctx, traceContextKey{}, s), func() {
+		userTaskEnd(id)
+	}
+
+	// We allocate a new task and the end function even when
+	// the tracing is disabled because the context and the detach
+	// function can be used across trace enable/disable boundaries,
+	// which complicates the problem.
+	//
+	// For example, consider the following scenario:
+	//   - trace is enabled.
+	//   - trace.WithSpan is called, so a new context ctx
+	//     with a new span is created.
+	//   - trace is disabled.
+	//   - trace is enabled again.
+	//   - trace APIs with the ctx is called. Is the ID in the task
+	//   a valid one to use?
+	//
+	// TODO(hyangah): reduce the overhead at least when
+	// tracing is disabled. Maybe the id can embed a tracing
+	// round number and ignore ids generated from previous
+	// tracing round.
+}
+
+func fromContext(ctx context.Context) *task {
+	if s, ok := ctx.Value(traceContextKey{}).(*task); ok {
+		return s
+	}
+	return &bgTask
+}
+
+type task struct {
+	id uint64
+	// TODO(hyangah): record parent id?
+}
+
+func newID() uint64 {
+	// TODO(hyangah): implement
+	return 0
+}
+
+var bgTask = task{id: uint64(0)}
+
+// Log emits a one-off event with the given key-value message. The execution
+// tracer allows analysis by filtering and grouping goroutines or spans.
+// Keys can be empty and the API assumes there are only handful number of
+// unique keys in the system.
+//
+// This API is EXPERIMENTAL and may change in the future.
+func Log(ctx context.Context, key, value string) {
+	id := fromContext(ctx).id
+	userLog(id, key, value)
+}
+
+// Logf is like Log, but the value is formatted using the specified format spec.
+//
+// This API is EXPERIMENTAL and may change in the future.
+func Logf(ctx context.Context, key, format string, args ...interface{}) {
+	if IsEnabled() {
+		Log(ctx, key, fmt.Sprintf(format, args...))
+	}
+}
+
+const (
+	spanStartCode = uint64(0)
+	spanEndCode   = uint64(1)
+)
+
+// WithSpan starts a span, the time interval during which the calling goroutine
+// is working on behalf of the task in the given context, and runs the given
+// function.
+//
+// If the context doesn't carry a task, the span is considered
+// a span for the background task.
+//
+// This API is EXPERIMENTAL and may change in the future.
+func WithSpan(ctx context.Context, name string, fn func(context.Context)) {
+	// NOTE:
+	// WithSpan helps avoiding misuse of the API but in practice,
+	// this is very restrictive:
+	// - Use of WithSpan makes the stack traces captured from
+	//   span start and end are identical.
+	// - Refactoring the existing code to use WithSpan is sometimes
+	//   hard and makes the code less readable.
+	//     e.g. code block nested deep in the loop with various
+	//          exit point with return values
+	// - Refactoring the code to use this API with closure can
+	//   cause different GC behavior such as retaining some parameters
+	//   longer.
+	// This causes more churns in code than I hoped, and sometimes
+	// makes the code less readable.
+
+	id := fromContext(ctx).id
+	userSpan(id, spanStartCode, name)
+	defer userSpan(id, spanEndCode, name)
+	fn(ctx)
+}
+
+// StartSpan starts a span and returns a function for marking the
+// end of the span. The span end function must be called from the
+// same goroutine where the span was started. Spans started prior
+// to this span must end after this span ends. This is a lower-level
+// API than WithSpan, which should be used instead whenever possible.
+func StartSpan(ctx context.Context, name string) func() {
+	id := fromContext(ctx).id
+	userSpan(id, spanStartCode, name)
+	return func() { userSpan(id, spanEndCode, name) }
+}
+
+// IsEnabled returns whether tracing is enabled.
+// The information is advisory only. The tracing status
+// may have changed by the time this function returns.
+//
+// This API is EXPERIMENTAL and may change in the future.
+func IsEnabled() bool {
+	enabled := atomic.LoadInt32(&tracing.enabled)
+	return enabled == 1
+}
+
+//
+// Function bodies are defined in runtime/trace.go
+//
+
+// emits UserTaskCreate event.
+func userTaskCreate(internalID, internalParentID uint64, name string)
+
+// emits UserTaskEnd event.
+func userTaskEnd(internalID uint64)
+
+// emits UserSpan event.
+func userSpan(internalID, mode uint64, name string)
+
+// emits UserLog event.
+func userLog(internalID uint64, key, val string)
