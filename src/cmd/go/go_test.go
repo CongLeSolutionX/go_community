0188e66232804e2a5b952ae16bb6f19102480611
Hiroshi Ioka <hirochachacha@gmail.com>
2017-12-19 23:10:59 +0900

    cmd/go: make -x output commands executable again
    The command `clang -fno-caret-diagnostics -c -x c -` works
    when stdin = /dev/null. But it doesn't work when stdin = /dev/tty.
    Appending `cat /dev/null |` makes the command complete.
    
    Fixes #21427
    
    Change-Id: I3b2b89d26e7ce023492ef23b6d117fa30175c57b

--- src/cmd/go/go_test.go
+++ src/cmd/go/go_test.go
@@ -4796,7 +4796,15 @@ func TestExecBuildX(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	out, err = exec.Command("/usr/bin/env", "bash", "-x", sh).CombinedOutput()
+	tty, err := os.Open("/dev/tty")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer tty.Close()
+
+	cmd := exec.Command("/usr/bin/env", "bash", "-x", sh)
+	cmd.Stdin = tty // prohibit to use stdin in the shell script.
+	out, err = cmd.CombinedOutput()
 	if err != nil {
 		t.Fatalf("/bin/sh %s: %v\n%s", sh, err, out)
 	}
