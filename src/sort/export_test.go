ba6117254a57943296693e9faa67e962cc5ed44a
Neven Sajko <nsajko@gmail.com>
2018-02-09 20:54:13 +0100

    sort: implement asymptotically optimal Stable
    The new implementation is a merge sort with the merge based on Kim, P.S.
    and Kutzner, A., 2008, April. Ratio based stable in-place merging. In
    International Conference on Theory and Applications of Models of
    Computation (pp. 246-257). Springer, Berlin, Heidelberg.
    
    The new algorithm starts being used with array size 2000. The code may
    be slower for input arrays with low numbers of distinct elements. In
    that case the new code is still a lot faster than for arrays with more
    distinct elements, but may be a lot slower than the old code on the same
    input arrays. The brightest point of the benchmarks is Stable1e6-8,
    which showcases the decrease in run time from 6.59s to 4.93s for a large
    array with enough distinct elements, while BMStable1e6Sawtooth1Copy-8
    and BMStable1e6Sawtooth19Copy-8 demonstrate the "pathology" of certain
    input types for which the old code is significantly faster.
    
    New benchmarks were added for pathologic input types and for finding the
    limiting size value from where the new algorithm should be used.
    
    Benchmark results on Intel Core i7-3632QM with Linux AMD64:
    
    name                                     old time/op  new time/op   delta
    StableString1K-8                          260µs ± 4%    232µs ± 3%   -10.92%  (p=0.000 n=10+10)
    StableInt1K-8                             113µs ± 2%     83µs ± 2%   -26.46%  (p=0.000 n=10+10)
    StableInt1K_Slice-8                      99.2µs ± 2%   71.0µs ± 0%   -28.47%  (p=0.000 n=9+7)
    StableInt64K-8                           9.55ms ± 2%   9.91ms ± 2%    +3.74%  (p=0.000 n=10+10)
    Stable1e2-8                               113µs ± 3%    123µs ± 6%    +8.59%  (p=0.000 n=9+10)
    Stable1e4-8                              34.3ms ± 6%   30.2ms ± 3%   -12.02%  (p=0.000 n=10+10)
    Stable1e6-8                               6.59s ± 1%    4.93s ± 0%   -25.15%  (p=0.000 n=10+8)
    BMStable2e3Rand10Copy-8                   716µs ± 4%    713µs ± 2%      ~     (p=0.971 n=10+10)
    BMStable6e3Rand11Copy-8                  2.62ms ± 2%   2.38ms ± 2%    -8.85%  (p=0.000 n=10+9)
    BMStable1e4Sawtooth1Copy-8                517µs ± 1%   1103µs ± 2%  +113.59%  (p=0.000 n=9+9)
    BMStable1e4Sawtooth4Copy-8               1.58ms ± 2%   2.31ms ± 2%   +45.99%  (p=0.000 n=9+10)
    BMStable1e4Sawtooth5Copy-8               1.85ms ± 2%   2.22ms ± 1%   +19.92%  (p=0.000 n=10+9)
    BMStable1e4Sawtooth6Copy-8               1.95ms ± 2%   2.30ms ± 1%   +17.81%  (p=0.000 n=10+10)
    BMStable1e6Sawtooth1Copy-8               70.1ms ± 1%  175.1ms ± 1%  +149.63%  (p=0.000 n=9+10)
    BMStable1e6Sawtooth8Copy-8                276ms ± 1%    289ms ± 1%    +4.91%  (p=0.000 n=9+9)
    BMStable1e6Sawtooth9Copy-8                286ms ± 2%    308ms ± 0%    +7.66%  (p=0.000 n=10+8)
    BMStable1e6Sawtooth12Copy-8               286ms ± 1%    235ms ± 1%   -18.03%  (p=0.000 n=8+10)
    BMStable1e6Sawtooth12ReverseFirstHalf-8   346ms ± 3%    254ms ± 3%   -26.53%  (p=0.000 n=10+10)
    BMStable1e6Sawtooth15Dither-8             268ms ± 0%    238ms ± 1%   -11.22%  (p=0.000 n=8+9)
    BMStable1e6Sawtooth19Copy-8              77.8ms ± 1%  167.1ms ± 1%  +114.79%  (p=0.000 n=10+9)
    BMStable1e6Stagger12ReverseFirstHalf-8    741ms ± 1%    465ms ± 0%   -37.27%  (p=0.000 n=10+9)
    BMStable1e6Plateau19Copy-8               16.2ms ± 1%   19.1ms ± 1%   +18.12%  (p=0.000 n=8+10)
    
    Fixes #7610
    
    Change-Id: I190970c13353f2d44078a872b8e8bbc3d777e8ed

--- src/sort/export_test.go
+++ src/sort/export_test.go
@@ -7,3 +7,11 @@ package sort
 func Heapsort(data Interface) {
 	heapSort(data, 0, data.Len())
 }
+
+func SimpleMerge(data Interface, m, b, z int) {
+	simpleMerge(data, 0, m, b, z)
+}
+
+func SimpleMergeBufBigSmall(data Interface, m, b, buf int) {
+	simpleMergeBufBigSmall(data, 0, m, b, buf)
+}
