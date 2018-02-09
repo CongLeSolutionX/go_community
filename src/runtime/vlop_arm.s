7ae148408976699344bb5adecbeeac32a09edb38
Cherry Zhang <cherryyz@google.com>
2018-02-09 12:47:33 -0500

    cmd/compile: CALLudiv on nacl/arm doesn't clobber R12
    On nacl/arm, R12 is clobbered by the RET instruction in function
    that has a frame. runtime.udiv doesn't have a frame, so it does
    not clobber R12.
    
    Change-Id: I0de448749f615908f6659e92d201ba3eb2f8266d

--- src/runtime/vlop_arm.s
+++ src/runtime/vlop_arm.s
@@ -102,6 +102,9 @@ TEXT runtime·_sfloatpanic(SB),NOSPLIT,$-4
 #define Ra	R11
 
 // Be careful: Ra == R11 will be used by the linker for synthesized instructions.
+// Note: this function does not have a frame. If it ever becomes having a frame,
+// the RET instruction will clobber R12 on nacl, and the compiler's register
+// allocator needs to know.
 TEXT runtime·udiv(SB),NOSPLIT,$-4
 	MOVBU	runtime·hardDiv(SB), Ra
 	CMP	$0, Ra
