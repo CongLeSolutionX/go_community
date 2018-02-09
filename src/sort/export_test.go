b786aaddef9dcaf6a24dc77a323cccdae2b58018
Neven Sajko <nsajko@gmail.com>
2018-02-09 20:54:13 +0100

    sort: implement asymptotically optimal Stable
    DO NOT REVIEW
    
    The new implementation is a merge sort with the merge based on Kim, P.S.
    and Kutzner, A., 2008, April. Ratio based stable in-place merging. In
    International Conference on Theory and Applications of Models of
    Computation (pp. 246-257). Springer, Berlin, Heidelberg.
    
    Benchmark results on Intel Linux AMD64:
    
    name                                     old time/op  new time/op   delta
    StableString1K-8                          260µs ± 4%    299µs ± 3%   +15.00%  (p=0.000 n=10+10)
    StableInt1K-8                             113µs ± 2%    135µs ± 1%   +19.39%  (p=0.000 n=10+9)
    StableInt1K_Slice-8                      99.2µs ± 2%   98.0µs ± 3%    -1.18%  (p=0.040 n=9+9)
    StableInt64K-8                           9.55ms ± 2%   9.64ms ± 1%    +0.92%  (p=0.019 n=10+10)
    Stable1e2-8                               113µs ± 3%    165µs ± 1%   +46.34%  (p=0.000 n=9+10)
    Stable1e4-8                              34.3ms ± 6%   30.6ms ± 4%   -10.86%  (p=0.000 n=10+10)
    Stable1e6-8                               6.59s ± 1%    4.90s ± 0%   -25.65%  (p=0.000 n=10+9)
    BMStable2e3Rand10Copy-8                   716µs ± 4%    708µs ± 2%      ~     (p=0.278 n=10+9)
    BMStable6e3Rand11Copy-8                  2.62ms ± 2%   2.39ms ± 2%    -8.80%  (p=0.000 n=10+10)
    BMStable1e4Sawtooth1Copy-8                517µs ± 1%   1687µs ± 2%  +226.56%  (p=0.000 n=9+9)
    BMStable1e4Sawtooth4Copy-8               1.58ms ± 2%   2.30ms ± 3%   +45.31%  (p=0.000 n=9+10)
    BMStable1e4Sawtooth5Copy-8               1.85ms ± 2%   2.24ms ± 2%   +20.80%  (p=0.000 n=10+10)
    BMStable1e4Sawtooth6Copy-8               1.95ms ± 2%   2.33ms ± 2%   +19.45%  (p=0.000 n=10+10)
    BMStable1e6Sawtooth1Copy-8               70.1ms ± 1%  259.6ms ± 2%  +270.14%  (p=0.000 n=9+10)
    BMStable1e6Sawtooth8Copy-8                276ms ± 1%    290ms ± 1%    +5.31%  (p=0.000 n=9+9)
    BMStable1e6Sawtooth9Copy-8                286ms ± 2%    311ms ± 2%    +8.73%  (p=0.000 n=10+9)
    BMStable1e6Sawtooth12Copy-8               286ms ± 1%    238ms ± 2%   -17.01%  (p=0.000 n=8+10)
    BMStable1e6Sawtooth12ReverseFirstHalf-8   346ms ± 3%    256ms ± 3%   -25.94%  (p=0.000 n=10+10)
    BMStable1e6Sawtooth15Dither-8             268ms ± 0%    240ms ± 2%   -10.41%  (p=0.000 n=8+10)
    BMStable1e6Sawtooth19Copy-8              77.8ms ± 1%  167.6ms ± 1%  +115.43%  (p=0.000 n=10+10)
    BMStable1e6Stagger12ReverseFirstHalf-8    741ms ± 1%    469ms ± 2%   -36.66%  (p=0.000 n=10+9)
    BMStable1e6Plateau19Copy-8               16.2ms ± 1%   19.3ms ± 1%   +19.55%  (p=0.000 n=8+10)
    
    The benchmarks indicate that the new code starts outperforming the old
    code between array sizes 2000 and 6000, except when there are little
    distinct elements. In the latter case the new code is still a lot faster
    than for arrays with more distinct elements, but may be a lot slower
    than the old code on the same input arrays. The brightest point of the
    benchmarks is Stable1e6-8, which showcases the decrease in run time from
    6.59s to 4.90s for a large array with enough distinct elements, while
    BMStable1e6Sawtooth1Copy-8 and BMStable1e6Sawtooth19Copy-8 demonstrate
    the "pathology" of certain input types for which the old code is hugely
    faster. I will try to even out the "pathologies" with further tuning.
    
    Change-Id: I190970c13353f2d44078a872b8e8bbc3d777e8ed

--- src/sort/export_test.go
+++ src/sort/export_test.go
@@ -7,3 +7,15 @@ package sort
 func Heapsort(data Interface) {
 	heapSort(data, 0, data.Len())
 }
+
+func ExtractDist(data Interface, c int) {
+	extractDist(data, 0, data.Len(), c)
+}
+
+func HL(data Interface, m, b, z int) {
+	hL(data, 0, m, b, z)
+}
+
+func HLBufBigSmall(data Interface, m, b, buf int) {
+	hLBufBigSmall(data, 0, m, b, buf)
+}
