9982aba732d888384c88d506eea3d0aa5db8f74c
Yuval Pavel Zholkover <paulzhol@gmail.com>
2018-02-13 15:29:33 +0200

    runtime/internal/atomic: move ARMv7 memory barriers into macros
    Restore the DMB_* macros removed in http://golang.org/cl/93637 into runtime/internal/atomic/asm_arm.h
    to be #included into runtime/asm_arm.s and runtime/internal/atomic/asm_arm.s.
    
    Updates #23777
    
    Change-Id: Ice8fcbe74e95be339dd83e9e05c52c00b9615330

---
+++ src/runtime/internal/atomic/asm_arm.h
@@ -0,0 +1,23 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#define _armv7_test \
+	MOVB	runtime·goarm(SB), R11; \
+	CMP	$7, R11; \
+	BLT	2(PC)
+
+#define DMB_ISH \
+	_armv7_test; \
+	/* ARMv7 dmb ish */ \
+	WORD	$0xf57ff05b
+
+#define DMB_ISHST \
+	_armv7_test; \
+	/* ARMv7 dmb ishst */ \
+	WORD	$0xf57ff05a
+
+#define DMB_ST \
+	_armv7_test; \
+	/* ARMv7 dmb st */ \
+	WORD	$0xf57ff05e
