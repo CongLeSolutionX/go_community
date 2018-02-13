7ce5fafcbb00cdcfbe3884733101e94b8425c8f8
Yuval Pavel Zholkover <paulzhol@gmail.com>
2018-02-13 15:29:33 +0200

    runtime/internal/atomic, sync/atomic: unify memory barrier macros
    Move the #defines for DMB_ISHST, DMB_ST and DMB_ISH into asm_arm.h. Install asm_arm.h in pkg/include.
    Add an additional check for GOARM=6 and use coprocessor cp15's Data Memory Barrier Register,
    instead of the DMB instruction.
    http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.ddi0360f/CHDGIJFB.html
    http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.ddi0360f/I1014942.html
    GOOS=darwin is not updated to use the macros, as it always assumes ARMv7.
    
    http://golang.org/cl/13363 disabled SMP on ARMv6 because armPublicationBarrier was ARMv7 only, enable it back.
    
    Updates #23777
    Updates #23778
    
    Change-Id: Ice8fcbe74e95be339dd83e9e05c52c00b9615330

--- src/runtime/os_freebsd_arm.go
+++ src/runtime/os_freebsd_arm.go
@@ -8,13 +8,6 @@ var hardDiv bool // TODO: set if a hardware divider is available
 
 func checkgoarm() {
 	// TODO(minux): FP checks like in os_linux_arm.go.
-
-	// osinit not called yet, so ncpu not set: must use getncpu directly.
-	if getncpu() > 1 && goarm < 7 {
-		print("runtime: this system has multiple CPUs and must use\n")
-		print("atomic synchronization instructions. Recompile using GOARM=7.\n")
-		exit(1)
-	}
 }
 
 //go:nosplit
