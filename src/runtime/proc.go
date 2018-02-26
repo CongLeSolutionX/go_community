319091679ed1c6d5724b5166a096782e325e6816
Ian Lance Taylor <iant@golang.org>
2018-02-26 14:03:47 -0800

    runtime: report profiling samples in a VDSO as _VDSO
    Currently if a profiling signal arrives while executing within a VDSO
    the profiler will report _ExternalCode, which is needlessly confusing
    for a pure Go program. Report _VDSO instead, which should at least
    point in the right direction.
    
    Updates #24142
    
    Change-Id: Iacd935db3c4c782150b3809aaa675a71799b1c9c

--- src/runtime/proc.go
+++ src/runtime/proc.go
@@ -3576,6 +3576,7 @@ func _ExternalCode()              { _ExternalCode() }
 func _LostExternalCode()          { _LostExternalCode() }
 func _GC()                        { _GC() }
 func _LostSIGPROFDuringAtomic64() { _LostSIGPROFDuringAtomic64() }
+func _VDSO()                      { _VDSO() }
 
 // Counts SIGPROFs received while in atomic64 critical section, on mips{,le}
 var lostAtomic64Count uint64
@@ -3716,7 +3717,9 @@ func sigprof(pc, sp, lr uintptr, gp *g, mp *m) {
 			// If all of the above has failed, account it against abstract "System" or "GC".
 			n = 2
 			// "ExternalCode" is better than "etext".
-			if pc > firstmoduledata.etext {
+			if inVDSOPage(pc) {
+				pc = funcPC(_VDSO) + sys.PCQuantum
+			} else if pc > firstmoduledata.etext {
 				pc = funcPC(_ExternalCode) + sys.PCQuantum
 			}
 			stk[0] = pc
