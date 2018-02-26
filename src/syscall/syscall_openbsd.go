ad1ef9355273f680d0921afd073ae15e1033e7cd
Tobias Klauser <tklauser@distanz.ch>
2018-02-26 11:15:41 +0100

    syscall: remove outdated TODO comments
    Error returns for linux/arm syscalls are handled since a long time.
    
    Remove another list of unimplemented syscalls, following CL 96315.
    
    The root-only check in TestSyscallNoError was shown to be sufficient as
    part of CL 84485 already.
    
    NetBSD and OpenBSD do not implement the sendfile syscall.
    
    Change-Id: I07efc3c3203537a4142707385f31b59dc0ecca42

--- src/syscall/syscall_openbsd.go
+++ src/syscall/syscall_openbsd.go
@@ -79,7 +79,6 @@ func Getdirentries(fd int, buf []byte, basep *uintptr) (n int, err error) {
 	return getdents(fd, buf)
 }
 
-// TODO
 func sendfile(outfd int, infd int, offset *int64, count int) (written int, err error) {
 	return -1, ENOSYS
 }
