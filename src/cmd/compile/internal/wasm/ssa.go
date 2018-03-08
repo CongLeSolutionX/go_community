600a50caeed659b666ea54aa08403e2eedec8436
Richard Musiol <mail@richard-musiol.de>
2018-03-09 00:14:58 +0100

    cmd/compile: add wasm stack optimization
    Go's SSA instructions only operate on registers. For example, an add
    instruction would read two registers, do the addition and then write
    to a register. WebAssembly's instructions, on the other hand, operate
    on the stack. The add instruction first pops two values from the stack,
    does the addition, then pushes the result to the stack. To fulfill
    Go's semantics, one needs to map Go's single add instruction to
    4 WebAssembly instructions:
    - Push the value of local variable A to the stack
    - Push the value of local variable B to the stack
    - Do addition
    - Write value from stack to local variable C
    
    Now consider that B was set to the constant 42 before the addition:
    - Push constant 42 to the stack
    - Write value from stack to local variable B
    
    This works, but is inefficient. Instead, the stack is used directly
    by inlining instructions if possible. With inlining it becomes:
    - Push the value of local variable A to the stack (add)
    - Push constant 42 to the stack (constant)
    - Do addition (add)
    - Write value from stack to local variable C (add)
    
    Note that the two SSA instructions can not be generated sequentially
    anymore, because their WebAssembly instructions are interleaved.
    
    Design doc: https://docs.google.com/document/d/131vjr4DH6JFnb-blm_uRdaC0_Nv3OUwjEY5qVCxCup4
    
    Updates #18892
    
    Change-Id: Ie35e1c0bebf4985fddda0d6330eb2066f9ad6dec

--- src/cmd/compile/internal/wasm/ssa.go
+++ src/cmd/compile/internal/wasm/ssa.go
@@ -113,6 +113,10 @@ func ssaGenBlock(s *gc.SSAGenState, b, next *ssa.Block) {
 
 	// Entry point for the next block. Used by the JMP in goToBlock.
 	s.Prog(wasm.ARESUMEPOINT)
+
+	if s.WasmStackSize != 0 {
+		panic("wasm: bad stack")
+	}
 }
 
 func ssaGenValue(s *gc.SSAGenState, v *ssa.Value) {
@@ -185,10 +189,16 @@ func ssaGenValue(s *gc.SSAGenState, v *ssa.Value) {
 		gc.AddrAuto(&p.To, v)
 
 	default:
-		if v.Type.IsMemory() {
+		if v.WasmStack {
+			s.WasmStackSize++
+		}
+		if v.Type.IsMemory() || v.WasmStack {
 			return
 		}
 		ssaGenValueOnStack(s, v)
+		if s.WasmStackSize != 0 {
+			panic("wasm: bad stack")
+		}
 		setReg(s, v.Reg())
 	}
 }
@@ -328,6 +338,13 @@ func ssaGenValueOnStack(s *gc.SSAGenState, v *ssa.Value) {
 }
 
 func getReg32(s *gc.SSAGenState, v *ssa.Value) {
+	if v.WasmStack {
+		s.WasmStackSize--
+		ssaGenValueOnStack(s, v)
+		s.Prog(wasm.AI32WrapI64)
+		return
+	}
+
 	reg := v.Reg()
 	getReg(s, reg)
 	if reg != wasm.REG_SP {
@@ -336,6 +353,12 @@ func getReg32(s *gc.SSAGenState, v *ssa.Value) {
 }
 
 func getReg64(s *gc.SSAGenState, v *ssa.Value) {
+	if v.WasmStack {
+		s.WasmStackSize--
+		ssaGenValueOnStack(s, v)
+		return
+	}
+
 	reg := v.Reg()
 	getReg(s, reg)
 	if reg == wasm.REG_SP {
