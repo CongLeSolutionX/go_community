7484a2aa4db06961186d93c97349c39663181a5e
Ben Shi <powerman1st@163.com>
2018-03-20 08:25:08 +0000

    cmd/internal/obj/arm64: support SWPD/SWPW/SWPH/SWPB
    SWPD/SWPW/SWPH/SWPB were introduced in ARMv8.1. They swap content
    of register and memory atomically. And their difference is
    SWPD: 64-bit double word data
    SWPW: 32-bit word data (zero extended to 64-bit)
    SWPH: 16-bit half word data (zero extended to 64-bit)
    SWPB: 8-bit byte data (zero extended to 64-bit)
    
    This CL implements them in the arm64 assembler.
    
    Change-Id: I2d9fb2310674bd92693531210e187143e7eed602

--- src/cmd/asm/internal/asm/testdata/arm64.s
+++ src/cmd/asm/internal/asm/testdata/arm64.s
@@ -573,6 +573,16 @@ again:
 	STPW	(R3, R4), x(SB)
 	STPW	(R3, R4), x+8(SB)
 
+// SWPD/SWPW/SWPH/SWPB
+	SWPD	(R6), R5, R7        // c78025f8
+	SWPD	(RSP), R5, R7       // e78325f8
+	SWPW	(R6), R5, R7        // c78025b8
+	SWPW	(RSP), R5, R7       // e78325b8
+	SWPH	(R6), R5, R7        // c7802578
+	SWPH	(RSP), R5, R7       // e7832578
+	SWPB	(R6), R5, R7        // c7802538
+	SWPB	(RSP), R5, R7       // e7832538
+
 // END
 //
 //	LTYPEE comma
