014b823cdb02e1461b2ae474b0b82bddd6a8aa08
David Chase <drchase@google.com>
2018-03-23 22:46:06 -0400

    cmd/compile: assign and preserve statement boundaries.
    A pass run after ssa building (before any other optimization)
    identifies the "first" ssa node for each statement.
    Other "noise" nodes are tagged as being never appropriate
    for a statement boundary (e.g., VarKill, VarDef, Phi).
    Rewrite and deadcode are modified to move the statement
    boundaries forward whenever possible if a boundary-tagged
    ssa value is removed; never-boundary nodes are ignored in
    this search (some operations involving constants are also
    tagged as never-boundary and also ignored because they are
    likely to be moved or removed during optimization).
    
    Code generation treats all nodes except those explicitly
    marked as statement boundaries as "not statement" nodes.
    
    Line number html conversion was modified to make statement
    boundary nodes a bit more obvious by prepending a "+".
    
    The code in fuse.go that glued together the value slices
    of two blocks produced a result that depended on the
    former capacities (not lengths) of the two slices.  This
    causes differences in the 386 bootstrap, and also can
    sometimes put values into an order that does a worse job
    of preserving statement boundaries when values are removed.
    
    This CL worsens the optimized-debugging experience with Delve;
    we need to work with the delve team to get so that they can
    use the is_stmt marks that we're emitting now.
    
    Portions of two delve tests that had caught problems were
    incorporated into ssa/debug_test.go.  There are some
    opportunities to do better with optimized code, but the
    nest-ing is not lying or overly jumpy.
    
    Over 4 CLs, compilebench geomean measured
    binary size increase of 4% and compile time
    (both user and wall) increase of 1.5%.
    Most of this (3.2%, 0.9%) comes in the last CL.
    Most of the user-time increase is in Flate.
    
    Trybot failures for linux-amd64 did not reproduce on
    linux-amd64 despite 10 or more non-cached repeat tests.
    
    Change-Id: I359c94e06843f1eb41f9da437bd614885aa9644a

---
+++ src/cmd/compile/internal/ssa/numberlines.go
@@ -0,0 +1,127 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package ssa
+
+import (
+	"cmd/internal/src"
+)
+
+// isPoorStatementStart determines whether a statement
+// is likely to disappear in a future rewrite and hence
+// would be a poor place to "start" that statement.
+func isPoorStatementStart(v *Value, i int, b *Block) bool {
+	// If the value is the last one in the block, too bad, it will have to do
+	// (this assumes that the value ordering vaguely corresponds to the source program execution order, which tends to be true directly after "build")
+	if i >= len(b.Values)-1 {
+		return false
+	}
+	// These are the likely-ephemeral/fragile opcodes expected to vanish in a rewrite.
+	switch v.Op {
+	case OpAddr, OpOffPtr, OpStructSelect, OpConst8, OpConst16, OpConst32, OpConst64, OpConst32F, OpConst64F:
+	default:
+		return false
+	}
+
+	// Look ahead to see what the line number is on the next thing that could be a boundary.
+	for j := i + 1; j < len(b.Values); j++ {
+		if b.Values[j].Pos.IsStmt() == src.LicoNotStmt { // ignore non-statements
+			continue
+		}
+		if b.Values[j].Pos.Line() == v.Pos.Line() {
+			return true
+		}
+		return false
+	}
+	return false
+}
+
+// notStmtBoundary indicates which value opcodes can never be a statement
+// boundary because they don't correespond to a user's understand of a
+// statement boundary.  Called from *Value.reset(), located here to keep
+// all the statement boundary heuristics in one place.
+func notStmtBoundary(op Op) bool {
+	switch op {
+	case OpPhi, OpVarKill, OpVarDef:
+		// not OpCopy; rewrite overwrites ops w/ OpCopy, but their Pos can be a statement boundary that needs to be preserved by movement to another Value.
+		return true
+	}
+	return false
+}
+
+func numberLines(f *Func) {
+	po := f.Postorder()
+	endlines := make(map[ID]int)
+
+	// Visit in reverse post order so that all non-loop predecessors come first.
+	for j := len(po) - 1; j >= 0; j-- {
+		b := po[j]
+		// Find the first interesting position and check to see if it differs from any predecessor
+		firstPos := src.NoXPos
+		firstPosIndex := -1
+		for i, v := range b.Values {
+			if v.Pos.IsStmt() != src.LicoNotStmt {
+				if isPoorStatementStart(v, i, b) {
+					continue
+				}
+				firstPosIndex = i
+				firstPos = v.Pos
+				v.Pos = firstPos.WithDefaultStmt() // default to default
+				break
+			}
+		}
+
+		if firstPosIndex == -1 { // Effectively empty block, consider preds.
+			line := -1
+			for _, p := range b.Preds {
+				pbi := p.Block().ID
+				if endlines[pbi] != line {
+					if line == -1 {
+						line = endlines[pbi]
+						continue
+					} else {
+						line = -1
+						break
+					}
+
+				}
+			}
+			endlines[b.ID] = line
+			continue
+		}
+		// check predecessors for any difference
+		if len(b.Preds) == 0 { // Don't forget the entry block
+			b.Values[firstPosIndex].Pos = firstPos.WithIsStmt()
+		} else {
+			for _, p := range b.Preds {
+				pbi := p.Block().ID
+				if endlines[pbi] != int(firstPos.Line()) {
+					b.Values[firstPosIndex].Pos = firstPos.WithIsStmt()
+					break
+				}
+			}
+		}
+		// iterate forward setting each new (interesting) position as a statement boundary.
+		for i := firstPosIndex + 1; i < len(b.Values); i++ {
+			v := b.Values[i]
+			if v.Pos.IsStmt() == src.LicoNotStmt {
+				continue
+			}
+			if isPoorStatementStart(v, i, b) {
+				continue
+			}
+			if v.Pos.Line() != firstPos.Line() {
+				firstPos = v.Pos
+				v.Pos = v.Pos.WithIsStmt()
+			} else {
+				v.Pos = v.Pos.WithDefaultStmt()
+			}
+		}
+		endlines[b.ID] = int(firstPos.Line())
+		if b.Control != nil && b.Pos.IsStmt() != src.LicoNotStmt && b.Pos.Line() != firstPos.Line() {
+			b.Pos = b.Pos.WithIsStmt()
+			// endlines[b.ID] = b.Pos.Line()
+		}
+	}
+}
