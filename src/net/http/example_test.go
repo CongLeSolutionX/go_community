b44ea25fbb8232bbcabfe60e5122ababd85864de
Filippo Valsorda <filippo@golang.org>
2018-03-30 19:09:59 -0400

    net/http: fix and normalize the [Server.][ListenAnd]Serve[TLS] docs
    The only inaccurate part was the HTTP/2 caveat in Server.ServeTLS, which
    only applies to the plain Serve variant.
    
    Fixes #24607
    
    Change-Id: Ie5f207d0201f09db27bf81b75535e5f6fdaf91e2

--- src/net/http/example_test.go
+++ src/net/http/example_test.go
@@ -137,3 +137,26 @@ func ExampleServer_Shutdown() {
 
 	<-idleConnsClosed
 }
+
+func ExampleListenAndServeTLS() {
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte("This is an example server.\n"))
+	})
+
+	// One can use generate_cert.go in crypto/tls to generate cert.pem and key.pem.
+	log.Printf("About to listen on 10443. Go to https://127.0.0.1:10443/")
+	err := http.ListenAndServeTLS(":10443", "cert.pem", "key.pem", nil)
+	log.Fatal(err)
+}
+
+func ExampleListenAndServe() {
+	// hello world, the web server
+
+	HelloServer := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "hello, world!\n")
+	}
+
+	http.HandleFunc("/hello", HelloServer)
+	log.Fatal(http.ListenAndServe(":12345", nil))
+}
