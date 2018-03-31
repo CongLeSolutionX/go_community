d3c9f59d0191054494ff5776b27ce1866f63c7ff
Richard Musiol <mail@richard-musiol.de>
2018-03-31 23:14:17 +0200

    runtime: add js/wasm architecture
    This commit adds the js/wasm architecture to the runtime package.
    Currently WebAssembly has no support for threads yet, see
    https://github.com/WebAssembly/design/issues/1073. Because of that,
    there is no preemption of goroutines and no sysmon goroutine.
    
    Design doc: https://docs.google.com/document/d/131vjr4DH6JFnb-blm_uRdaC0_Nv3OUwjEY5qVCxCup4
    About WebAssembly assembly files: https://docs.google.com/document/d/1GRmy3rA4DiYtBlX-I1Jr_iHykbX8EixC3Mq0TCYqbKc
    
    Updates #18892
    
    Change-Id: I7f12d21b5180500d55ae9fd2f7e926a1731db391

---
+++ src/runtime/rt0_js_wasm.s
@@ -0,0 +1,45 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "textflag.h"
+
+TEXT _rt0_wasm_js(SB),NOSPLIT,$0
+	MOVD $10000, SP // FIXME
+
+	Get SP
+	Get R0 // argc
+	I64ExtendUI32
+	I64Store $0
+
+	Get SP
+	Get R1 // argv
+	I64ExtendUI32
+	I64Store $8
+
+	I32Const $runtime·rt0_go(SB)
+	I32Const $16
+	I32ShrU
+	Set PC_F
+
+// Call the function for the current PC_F. Repeat until SP=0 indicates program end.
+// The WebAssembly stack may unwind, e.g. when switching goroutines.
+// The Go stack on the linear memory is then used to jump to the correct functions
+// with this loop, without having to restore the full WebAssembly stack.
+loop:
+	Loop
+		Get SP
+		I32Eqz
+		If
+			Return
+		End
+
+		Get PC_F
+		CallIndirect $0
+		Drop
+
+		Br loop
+	End
+
+TEXT _rt0_wasm_js_lib(SB),NOSPLIT,$0
+	UNDEF
