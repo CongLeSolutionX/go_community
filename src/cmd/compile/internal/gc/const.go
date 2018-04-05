63aae42179ac833e3dbd49aca7b9414083bdd4f8
Daniel Martí <mvdan@mvdan.cc>
2018-04-05 14:45:38 +0100

    cmd/compile: remove last manual node copies
    When I added the Node.copy method, I converted most of the occurrences
    but missed a few.
    
    One of them, used only for gdata, was an unnecessary copy given that
    gdata does not modify the node it is passed.
    
    No allocation changes in compilebench.
    
    Passes toolstash -cmp on std cmd.
    
    Change-Id: I7fba5212377b75c6d6b3785e594a30568ff0732e

--- src/cmd/compile/internal/gc/const.go
+++ src/cmd/compile/internal/gc/const.go
@@ -1199,9 +1199,7 @@ func setconst(n *Node, v Val) {
 	// Ensure n.Orig still points to a semantically-equivalent
 	// expression after we rewrite n into a constant.
 	if n.Orig == n {
-		var ncopy Node
-		n.Orig = &ncopy
-		ncopy = *n
+		n.Orig = n.copy()
 	}
 
 	*n = Node{
