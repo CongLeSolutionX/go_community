464d55096593114e8bf30fe593e88696b9787edf
Alberto Donizetti <alb.donizetti@gmail.com>
2018-04-13 12:19:20 +0200

    cmd/compile: move Issue 16214 test from asm_test file
    The asm_test.go file implements the old code generation test harness,
    and will likely be deleted in the near future. Move the Issue16214
    test in the fixedbugs_test.go file.
    
    Change-Id: I2a0c72bd36f0359b7baf75b5d1ba647cc84feb46

--- src/cmd/compile/internal/gc/asm_test.go
+++ src/cmd/compile/internal/gc/asm_test.go
@@ -253,38 +253,3 @@ var plan9AMD64Tests = []*asmTest{
 		pos: []string{"\tMOVQ\t\"\"\\.a\\+[0-9]+\\(SP\\), (AX|CX)", "\tMOVQ\t(AX|CX), \"\"\\.b\\+[0-9]+\\(SP\\)"},
 	},
 }
-
-// TestLineNumber checks to make sure the generated assembly has line numbers
-// see issue #16214
-func TestLineNumber(t *testing.T) {
-	testenv.MustHaveGoBuild(t)
-	dir, err := ioutil.TempDir("", "TestLineNumber")
-	if err != nil {
-		t.Fatalf("could not create directory: %v", err)
-	}
-	defer os.RemoveAll(dir)
-
-	src := filepath.Join(dir, "x.go")
-	err = ioutil.WriteFile(src, []byte(issue16214src), 0644)
-	if err != nil {
-		t.Fatalf("could not write file: %v", err)
-	}
-
-	cmd := exec.Command(testenv.GoToolPath(t), "tool", "compile", "-S", "-o", filepath.Join(dir, "out.o"), src)
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		t.Fatalf("fail to run go tool compile: %v", err)
-	}
-
-	if strings.Contains(string(out), "unknown line number") {
-		t.Errorf("line number missing in assembly:\n%s", out)
-	}
-}
-
-var issue16214src = `
-package main
-
-func Mod32(x uint32) uint32 {
-	return x % 3 // frontend rewrites it as HMUL with 2863311531, the LITERAL node has unknown Pos
-}
-`
