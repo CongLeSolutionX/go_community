4c1998e376e1393a95b254eda4cdb40a7f6d3004
Brian Kessler <brian.m.kessler@gmail.com>
2018-04-19 09:08:24 -0600

    math/big: handle negative moduli in ModInverse
    Currently, there is no check for a negative modulus in ModInverse.
    Negative moduli are passed internally to GCD, which returns 0 for
    negative arguments. Mod is symmetric with respect to negative moduli,
    so the calculation can be done by just negating the modulus before
    passing the arguments to GCD.
    
    Fixes #24949
    
    Change-Id: Ifd1e64c9b2343f0489c04ab65504e73a623378c7

--- src/math/big/int.go
+++ src/math/big/int.go
@@ -661,8 +661,12 @@ func (z *Int) Rand(rnd *rand.Rand, n *Int) *Int {
 // ModInverse sets z to the multiplicative inverse of g in the ring ℤ/nℤ
 // and returns z. If g and n are not relatively prime, the result is undefined.
 func (z *Int) ModInverse(g, n *Int) *Int {
+	// GCD expects parameters a and b to be > 0.
+	if n.neg {
+		var n2 Int
+		n = n2.Neg(n)
+	}
 	if g.neg {
-		// GCD expects parameters a and b to be > 0.
 		var g2 Int
 		g = g2.Mod(g, n)
 	}
