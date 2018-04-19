6ec3434705332de765f955f1c4c85b8cb879f67d
Zheng Dayu <davidzheng23@gmail.com>
2018-04-19 12:07:13 +0800

    text/template: improve comment example in doc
    Move original example back..
    Fixes #24767
    
    Change-Id: I0f8f67032f47293a080f392cae0a50c64be338e4

--- src/text/template/doc.go
+++ src/text/template/doc.go
@@ -68,7 +68,8 @@ Here is the list of actions. "Arguments" and "pipelines" are evaluations of
 data, defined in detail in the corresponding sections that follow.
 
 */
-//	{{- /* a comment */ -}}
+//	{{/* a comment */}}
+//	{{- /* a comment with white space trimmed from preceding and following text */ -}}
 //		A comment; discarded. May contain newlines.
 //		Comments do not nest and must start and end at the
 //		delimiters, as shown here.
