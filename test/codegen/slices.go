2020560016588f7354f00df95dae57270a081edb
Martin Möhrmann <moehrmann@google.com>
2018-04-26 18:30:11 +0200

    cmd/compile: optimize append(x, make([]T, y)...) slice extension
    DO NOT SUBMIT
    - working on benchmarks
    
    Fixes #21266
    
    Change-Id: Idc3077665f63cbe89762b590c5967a864fd1c07f

---
+++ test/codegen/slices.go
@@ -0,0 +1,36 @@
+// asmcheck
+
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package codegen
+
+// This file contains code generation tests related to the handling of
+// slice types.
+
+// ------------------ //
+//      Extension     //
+// ------------------ //
+
+// Issue #21266 - avoid makeslice in append(x, make([]T, y)...)
+
+func SliceExtensionConst(s []int) []int {
+	// amd64:`.*memclrNoHeapPointers`
+	return append(s, make([]int, 1<<2)...)
+}
+
+func SliceExtensionPointer(s []*int, l int) []*int {
+	// amd64:`.*memclrHasPointers`
+	return append(s, make([]*int, 10)...)
+}
+
+func SliceExtensionVar(s []byte, l int) []byte {
+	// amd64:`.*memclrNoHeapPointers`
+	return append(s, make([]byte, l)...)
+}
+
+func SliceExtensionInt64(s []int, l64 int64) []int {
+	// 386:-`.*memclr.*`
+	return append(s, make([]int, l64)...)
+}
