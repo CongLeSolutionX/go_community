6c4f7841effd1864b05173fc928ffed610ec7d10
Daniel Martí <mvdan@mvdan.cc>
2018-05-04 11:22:53 +0700

    cmd/vet: avoid false positives with non-comments
    vet's buildtag check looks for malformed build tag comments. Since these
    can appear in Go files as well as non-Go files (such as assembly files),
    it must read the file line by line instead of using go/token or go/ast
    directly.
    
    However, this method runs into false positives if there are any lines in
    the code that look like comments, but are not. For example:
    
    	$ cat f.go
    	package main
    	const foo = `
    	//+build ignore
    	`
    	$ go vet f.go
    	./f.go:3: +build comment must appear before package clause and be followed by a blank line
    
    This bug has been popping up more frequently since vet started being run
    with go test, so it is important to make the check as precise as
    possible.
    
    To avoid the false positive, when checking a Go file, cross-check that a
    line that looks like a comment actually corresponds to a comment in the
    go/ast syntax tree. Since vet already obtains the syntax trees for all
    the Go files, it checks, this change means very little extra work for
    the check.
    
    While at it, add a badf helper function to simplify the code that
    reports warnings in the buildtag check.
    
    Fixes #13533.
    
    Change-Id: I484a16da01363b409ec418c313634171bf85250b

--- src/cmd/vet/buildtag.go
+++ src/cmd/vet/buildtag.go
@@ -19,11 +19,36 @@ var (
 )
 
 // checkBuildTag checks that build tags are in the correct location and well-formed.
-func checkBuildTag(name string, data []byte) {
+func checkBuildTag(f *File) {
 	if !vet("buildtags") {
 		return
 	}
-	lines := bytes.SplitAfter(data, nl)
+	// badf is like File.Badf, but it uses a line number instead of
+	// token.Pos.
+	badf := func(line int, format string, args ...interface{}) {
+		format = "%s:%d:" + format + "\n"
+		args = append([]interface{}{f.name, line}, args...)
+		fmt.Fprintf(os.Stderr, format, args...)
+		setExit(1)
+	}
+
+	// we must look at the raw lines, as build tags may appear in non-Go
+	// files such as assembly files.
+	lines := bytes.SplitAfter(f.content, nl)
+
+	// lineWithComment records all source lines that contain a //-style
+	// comment in a Go source file. If the current source file is not Go,
+	// the map is nil.
+	var lineWithComment map[int]bool
+	if f.file != nil {
+		lineWithComment = make(map[int]bool)
+		for _, group := range f.file.Comments {
+			for _, comment := range group.List {
+				line := f.fset.Position(comment.Pos()).Line
+				lineWithComment[line] = true
+			}
+		}
+	}
 
 	// Determine cutpoint where +build comments are no longer valid.
 	// They are valid in leading // comments in the file followed by
@@ -46,18 +71,23 @@ func checkBuildTag(name string, data []byte) {
 		if !bytes.HasPrefix(line, slashSlash) {
 			continue
 		}
+		if lineWithComment != nil && !lineWithComment[i+1] {
+			// This is a line in a Go source file that looks like a
+			// comment, but actually isn't - such as part of a raw
+			// string.
+			continue
+		}
+
 		text := bytes.TrimSpace(line[2:])
 		if bytes.HasPrefix(text, plusBuild) {
 			fields := bytes.Fields(text)
 			if !bytes.Equal(fields[0], plusBuild) {
 				// Comment is something like +buildasdf not +build.
-				fmt.Fprintf(os.Stderr, "%s:%d: possible malformed +build comment\n", name, i+1)
-				setExit(1)
+				badf(i+1, "possible malformed +build comment")
 				continue
 			}
 			if i >= cutoff {
-				fmt.Fprintf(os.Stderr, "%s:%d: +build comment must appear before package clause and be followed by a blank line\n", name, i+1)
-				setExit(1)
+				badf(i+1, "+build comment must appear before package clause and be followed by a blank line")
 				continue
 			}
 			// Check arguments.
@@ -65,15 +95,13 @@ func checkBuildTag(name string, data []byte) {
 			for _, arg := range fields[1:] {
 				for _, elem := range strings.Split(string(arg), ",") {
 					if strings.HasPrefix(elem, "!!") {
-						fmt.Fprintf(os.Stderr, "%s:%d: invalid double negative in build constraint: %s\n", name, i+1, arg)
-						setExit(1)
+						badf(i+1, "invalid double negative in build constraint: %s", arg)
 						break Args
 					}
 					elem = strings.TrimPrefix(elem, "!")
 					for _, c := range elem {
 						if !unicode.IsLetter(c) && !unicode.IsDigit(c) && c != '_' && c != '.' {
-							fmt.Fprintf(os.Stderr, "%s:%d: invalid non-alphanumeric build constraint: %s\n", name, i+1, arg)
-							setExit(1)
+							badf(i+1, "invalid non-alphanumeric build constraint: %s", arg)
 							break Args
 						}
 					}
@@ -83,8 +111,7 @@ func checkBuildTag(name string, data []byte) {
 		}
 		// Comment with +build but not at beginning.
 		if bytes.Contains(line, plusBuild) && i < cutoff {
-			fmt.Fprintf(os.Stderr, "%s:%d: possible malformed +build comment\n", name, i+1)
-			setExit(1)
+			badf(i+1, "possible malformed +build comment")
 			continue
 		}
 	}
