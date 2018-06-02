5d7b99a28d663ed841cd834b39374ca4ed8ec99e
Travis Beatty <travisby@gmail.com>
2018-06-02 06:55:48 -0400

    test: run tests for unsafe again
    We had stopped running the tests in safe/ after moving to the run.go
    framework.  This adds them back in
    
    Fixes #25668
    
    Change-Id: Id28b17c359a9627372c6b3a873ecef844d0af7a7

--- test/safe/usesafe.go
+++ test/safe/usesafe.go
@@ -1,8 +1,14 @@
-// $G $D/pkg.go && pack grcS pkg.a pkg.$A 2> /dev/null && rm pkg.$A && $G -I . -u $D/main.go
-// rm -f pkg.a
+// errorcheckdir -u
 
 // Copyright 2012 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package ignored
+package main
+
+// can't use local path with -u, use -I. instead
+import "pkg"
+
+func main() {
+	print(pkg.Float32bits(1.0))
+}
