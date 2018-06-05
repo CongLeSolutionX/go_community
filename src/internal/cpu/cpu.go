3e9714f35ebb8fcabe3cb40df1d09adca4f52f3f
Martin Möhrmann <moehrmann@google.com>
2018-06-05 08:14:57 +0200

    runtime: replace sys.CacheLineSize by corresponding internal/cpu const and vars
    DO NOT SUBMIT:
    - golang.org/cl/126601 should add cpu.CacheLinePadSize first
    
    sys here is runtime/internal/sys.
    
    Replace uses of sys.CacheLineSize for padding by
    cpu.CacheLinePad or cpu.CacheLinePadSize.
    Replace other uses of sys.CacheLineSize by cpu.CacheLineSize.
    Remove now unused sys.CacheLineSize.
    
    Updates #25203
    
    Change-Id: I1daf410fe8f6c0493471c2ceccb9ca0a5a75ed8f

--- src/internal/cpu/cpu.go
+++ src/internal/cpu/cpu.go
@@ -14,6 +14,8 @@ var DebugOptions bool
 // CacheLinePad is used to pad structs to avoid false sharing.
 type CacheLinePad struct{ _ [CacheLineSize]byte }
 
+const CacheLinePadSize = CacheLineSize
+
 var X86 x86
 
 // The booleans in x86 contain the correspondingly named cpuid feature bit.
