6100c2da05c921e868483613663f73c9d38628c1
Akhil Indurti <aindurti@gmail.com>
2018-06-15 19:30:30 -0400

    crypto/rand: Add package-level examples
    Previously, there were no package-level examples for crypto/rand, and
    the only example for the package was under rand.Read.
    This change
        1. Hoists the rand.Read example up to the package-level.
        2. Adds a package-level example to demonstrate how to use rand.Read
    to implement a cipher (specifically a one-time pad).
        3. Adds a package-level example to demonstrate how to use rand.Prime
    to do perform plain-old RSA key generation.
    
    Updates #25326.
    
    Change-Id: Ie15d3de0e66d83b2e89e0a213569ce23148633dc

--- src/crypto/rand/example_test.go
+++ src/crypto/rand/example_test.go
@@ -8,11 +8,12 @@ import (
 	"bytes"
 	"crypto/rand"
 	"fmt"
+	"math/big"
 )
 
 // This example reads 10 cryptographically secure pseudorandom numbers from
 // rand.Reader and writes them to a byte slice.
-func ExampleRead() {
+func Example() {
 	c := 10
 	b := make([]byte, c)
 	_, err := rand.Read(b)
@@ -25,4 +26,72 @@ func ExampleRead() {
 
 	// Output:
 	// false
+}
+
+// ExampleOneTimePad demonstrates how to use rand.Read to implement
+// a One-Time Pad cipher (https://wikipedia.org/wiki/One-time_pad).
+func Example_oneTimePad() {
+	plaintext := []byte("Something something gopher...")
+	n := len(plaintext)
+	key := make([]byte, n)
+	ciphertext := make([]byte, n)
+
+	// Read n random bytes into key. A one-time pad requires the key
+	// to be at least as long as the plaintext.
+	if _, err := rand.Read(key); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Original: %x\n", plaintext)
+
+	// Encrypt message by XOR'ing the plaintext and the key.
+	for i := 0; i < n; i++ {
+		ciphertext[i] = plaintext[i] ^ key[i]
+	}
+	fmt.Printf("Encrypted: %x\n", ciphertext)
+
+	// Decrypt message by XOR'ing the ciphertext and the key.
+	for i := 0; i < n; i++ {
+		ciphertext[i] = ciphertext[i] ^ key[i]
+	}
+	fmt.Printf("Decrypted: %x\n", ciphertext)
+}
+
+// ExampleRSA demonstrates how to use rand.Prime to implement plain-old
+// RSA key generation (https://wikipedia.org/wiki/RSA_(cryptosystem)#Key_generation).
+// This is a proof-of-concept, and not meant to be used in production.
+func Example_rSA() {
+	// Choose two random primes p and q
+	bits := 100
+	p, err := rand.Prime(rand.Reader, bits)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	q, err := rand.Prime(rand.Reader, bits)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Let n = p * q
+	var n big.Int
+	n.Mul(p, q)
+
+	// Let totient = (p - 1) * (q - 1)
+	one := big.NewInt(1)
+	p.Sub(p, one)
+	q.Sub(q, one)
+	var totient big.Int
+	totient.Mul(p, q)
+
+	// Let e be coprime to the totient
+	e := big.NewInt(3)
+
+	// Let d satisfy d*e ≡ 1 mod totient
+	var d big.Int
+	d.ModInverse(e, &totient)
+
+	fmt.Printf("Public Key-Pair: (%s, %s)\n", n.Text(10), e.Text(10))
+	fmt.Printf("Private Key-Pair: (%s, %s)\n", n.Text(10), d.Text(10))
 }
