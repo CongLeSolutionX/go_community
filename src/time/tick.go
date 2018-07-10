e4ef875899e1622d8ea4dc361922f3a94034b5bc
Caleb Martinez <accounts@calebmartinez.com>
2018-07-10 13:32:59 +0000

    time: add clarification to Time.Stop documentation
    Fixes #26220
    
    Change-Id: I05703912be594d985ad3ccb3a9757ae21ec738ab
    GitHub-Last-Rev: 3a9d4ecb9313a8569c720a4bd95e5e06bdfcc425
    GitHub-Pull-Request: golang/go#26293

--- src/time/tick.go
+++ src/time/tick.go
@@ -40,8 +40,8 @@ func NewTicker(d Duration) *Ticker {
 }
 
 // Stop turns off a ticker. After Stop, no more ticks will be sent.
-// Stop does not close the channel, to prevent a read from the channel succeeding
-// incorrectly.
+// Stop does not close the channel. This prevents a concurrent goroutine
+// (reading from said channel) from seeing a read succeed incorrectly.
 func (t *Ticker) Stop() {
 	stopTimer(&t.r)
 }
