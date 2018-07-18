cce685e2ce9992a0382beac1567e72a4248126d6
Jack <jackxbritton@gmail.com>
2018-07-18 04:02:20 +0000

    filepath: updates doc to give case where WalkFunc info arg may be nil
    If a filepath.WalkFunc is called with an non-nil err argument, it's possible
    that the info argument will be nil. The comment above filepath.WalkFunc now
    reflects this.
    
    Fixes #26425
    
    Change-Id: Ib9963b3344587d2993f1698c5a801f2d1286856b
    GitHub-Last-Rev: 4f7ec7a3d4c6bea2aa4c6f8f5d130cd1931963a9
    GitHub-Pull-Request: golang/go#26435

--- src/path/filepath/path.go
+++ src/path/filepath/path.go
@@ -334,19 +334,20 @@ func Rel(basepath, targpath string) (string, error) {
 var SkipDir = errors.New("skip this directory")
 
 // WalkFunc is the type of the function called for each file or directory
-// visited by Walk. The path argument contains the argument to Walk as a
-// prefix; that is, if Walk is called with "dir", which is a directory
-// containing the file "a", the walk function will be called with argument
-// "dir/a". The info argument is the os.FileInfo for the named path.
+// visited by Walk. The path argument contains the argument to Walk as a prefix;
+// that is, if Walk is called with "dir", which is a directory containing the
+// file "a", the walk function will be called with argument "dir/a". The info
+// argument is the os.FileInfo for the named path.
 //
 // If there was a problem walking to the file or directory named by path, the
-// incoming error will describe the problem and the function can decide how
-// to handle that error (and Walk will not descend into that directory). If
-// an error is returned, processing stops. The sole exception is when the function
-// returns the special value SkipDir. If the function returns SkipDir when invoked
-// on a directory, Walk skips the directory's contents entirely.
-// If the function returns SkipDir when invoked on a non-directory file,
-// Walk skips the remaining files in the containing directory.
+// incoming error will describe the problem and the function can decide how to
+// handle that error (and Walk will not descend into that directory). In the
+// case of an error, the info argument may be nil. If an error is returned,
+// processing stops. The sole exception is when the function returns the special
+// value SkipDir. If the function returns SkipDir when invoked on a directory,
+// Walk skips the directory's contents entirely. If the function returns SkipDir
+// when invoked on a non-directory file, Walk skips the remaining files in the
+// containing directory.
 type WalkFunc func(path string, info os.FileInfo, err error) error
 
 var lstat = os.Lstat // for testing
