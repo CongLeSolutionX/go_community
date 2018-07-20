2eb8fd14e26eeeaa95e041a96fb36bbd75c974a0
templexxx <lucas1x1x@gmail.com>
2018-07-21 04:18:08 +0800

    cipher: update xor(using SIMD)
    cpu: Intel(R) Core(TM) i7-7700HQ CPU @ 2.80GHz
    
    Benchmark: xor
    
    name                   old time/op    new time/op     delta
    XORBytes/8Bytes-8        8.21ns ± 1%     7.51ns ± 3%    -8.50%  (p=0.008 n=5+5)
    XORBytes/128Bytes-8      17.9ns ± 1%     11.4ns ± 2%   -36.20%  (p=0.008 n=5+5)
    XORBytes/2048Bytes-8      187ns ± 1%       79ns ± 1%   -57.94%  (p=0.008 n=5+5)
    XORBytes/32768Bytes-8    2.87µs ± 1%     1.38µs ± 3%   -51.91%  (p=0.008 n=5+5)
    
    name                   old speed      new speed       delta
    XORBytes/8Bytes-8       974MB/s ± 1%   1065MB/s ± 3%    +9.30%  (p=0.008 n=5+5)
    XORBytes/128Bytes-8    7.15GB/s ± 0%  11.22GB/s ± 3%   +56.87%  (p=0.008 n=5+5)
    XORBytes/2048Bytes-8   10.9GB/s ± 1%   26.1GB/s ± 1%  +138.13%  (p=0.008 n=5+5)
    XORBytes/32768Bytes-8  11.4GB/s ± 1%   23.8GB/s ± 2%  +107.95%  (p=0.008 n=5+5)
    
    Benchmark: cipher
    
    name               old time/op    new time/op    delta
    AESGCMSeal1K-8        269ns ± 6%     271ns ±12%     ~     (p=0.968 n=5+5)
    AESGCMOpen1K-8        242ns ± 1%     241ns ± 1%     ~     (p=0.444 n=5+5)
    AESGCMSign8K-8        869ns ± 0%     875ns ± 2%     ~     (p=0.143 n=5+5)
    AESGCMSeal8K-8       1.64µs ± 6%    1.60µs ± 5%     ~     (p=0.548 n=5+5)
    AESGCMOpen8K-8       1.48µs ± 2%    1.48µs ± 2%     ~     (p=0.667 n=5+5)
    AESCFBEncrypt1K-8    1.88µs ± 5%    1.71µs ± 4%   -8.71%  (p=0.008 n=5+5)
    AESCFBDecrypt1K-8    1.76µs ± 1%    1.65µs ± 3%   -5.94%  (p=0.016 n=4+5)
    AESOFB1K-8           1.10µs ± 4%    1.03µs ± 2%   -6.57%  (p=0.008 n=5+5)
    AESCTR1K-8           1.24µs ± 1%    1.18µs ± 1%   -4.84%  (p=0.008 n=5+5)
    AESCBCEncrypt1K-8    1.74µs ± 0%    1.23µs ± 1%  -29.20%  (p=0.008 n=5+5)
    AESCBCDecrypt1K-8    1.28µs ± 1%    1.14µs ± 2%  -10.80%  (p=0.008 n=5+5)
    
    name               old speed      new speed      delta
    AESGCMSeal1K-8     3.81GB/s ± 6%  3.79GB/s ±11%     ~     (p=1.000 n=5+5)
    AESGCMOpen1K-8     4.23GB/s ± 1%  4.23GB/s ± 1%     ~     (p=0.690 n=5+5)
    AESGCMSign8K-8     9.43GB/s ± 0%  9.36GB/s ± 2%     ~     (p=0.151 n=5+5)
    AESGCMSeal8K-8     5.01GB/s ± 6%  5.13GB/s ± 4%     ~     (p=0.548 n=5+5)
    AESGCMOpen8K-8     5.54GB/s ± 2%  5.54GB/s ± 2%     ~     (p=0.548 n=5+5)
    AESCFBEncrypt1K-8   543MB/s ± 5%   595MB/s ± 4%   +9.53%  (p=0.008 n=5+5)
    AESCFBDecrypt1K-8   580MB/s ± 1%   617MB/s ± 3%   +6.34%  (p=0.016 n=4+5)
    AESOFB1K-8          925MB/s ± 4%   990MB/s ± 2%   +6.98%  (p=0.008 n=5+5)
    AESCTR1K-8          821MB/s ± 1%   863MB/s ± 1%   +5.09%  (p=0.008 n=5+5)
    AESCBCEncrypt1K-8   588MB/s ± 1%   831MB/s ± 1%  +41.26%  (p=0.008 n=5+5)
    AESCBCDecrypt1K-8   799MB/s ± 1%   895MB/s ± 2%  +12.11%  (p=0.008 n=5+5)
    
    Change-Id: I42e6ba66c23dad853d33c924fca7b0ed805cefdd

---
+++ src/crypto/cipher/xor_amd64.s
@@ -0,0 +1,68 @@
+#include "textflag.h"
+
+#define dst BX // dst address
+#define src0 SI // a address
+#define src1 CX // b address
+#define len DX // len of vect
+#define pos R8 // job position in vect
+
+#define not_aligned_len R12
+#define src_val0 R13
+#define src_val1 R14
+
+// func xorBytesSSE2(dst, a, b []byte, n int)
+TEXT ·xorBytesSSE2(SB), NOSPLIT, $0
+	MOVQ  d+0(FP), dst
+	MOVQ  s0+24(FP), src0
+	MOVQ  s1+48(FP), src1
+	MOVQ  l+72(FP), len   // AND $31 & len
+	TESTQ $15, len        // if not zero jump to not_aligned
+	JNZ   not_aligned
+
+aligned:
+	MOVQ $0, pos
+
+loop16b:
+	MOVOU (src0)(pos*1), X0
+	MOVOU (src1)(pos*1), X1
+	PXOR  X1, X0
+	MOVOU X0, (dst)(pos*1)
+	ADDQ  $16, pos
+	CMPQ  len, pos
+	JNE   loop16b
+	RET
+
+loop_1b:
+	MOVB  -1(src0)(len*1), src_val0
+	MOVB  -1(src1)(len*1), src_val1
+	XORB  src_val1, src_val0
+	MOVB  src_val0, -1(dst)(len*1)
+	SUBQ  $1, len
+	TESTQ $7, len
+	JNZ   loop_1b
+	CMPQ  len, $0
+	JE    ret
+	TESTQ $15, len
+	JZ    aligned
+
+not_aligned:
+	TESTQ $7, len              // AND $7 & len
+	JNE   loop_1b              // if not zero jump to loop_1b
+	MOVQ  len, not_aligned_len
+	ANDQ  $15, not_aligned_len
+
+loop_8b:
+	MOVQ -8(src0)(len*1), src_val0
+	MOVQ -8(src1)(len*1), src_val1
+	XORQ src_val1, src_val0
+	MOVQ src_val0, -8(dst)(len*1)
+	SUBQ $8, len
+	SUBQ $8, not_aligned_len
+	JG   loop_8b
+
+	CMPQ len, $16
+	JGE  aligned
+	RET
+
+ret:
+	RET
