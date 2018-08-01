f3f453ac20797d0e4b54c11aa1c62715f2fd9978
Jean de Klerk <deklerk@google.com>
2018-07-31 18:10:42 -0700

    testing: stream log output in verbose mode
    Fixes #24929
    
    Change-Id: Icc426068cd73b75b78001f55e1e5d81ccebbe854

--- src/testing/testing.go
+++ src/testing/testing.go
@@ -616,6 +616,11 @@ func (c *common) logDepth(s string, depth int) {
 			panic("Log in goroutine after " + c.name + " has completed")
 		}
 	} else {
+		if c.chatty {
+			fmt.Printf("%s: %s", c.name, c.decorate(s))
+			return
+		}
+
 		c.output = append(c.output, c.decorate(s, depth+1)...)
 	}
 }
