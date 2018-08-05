1928d3b673ee1cb37e26a01b7af0360f683a18be
Kevin Burke <kev@inburke.com>
2018-08-05 08:25:23 -0700

    go/format: benchmark performance on large autogen file
    While working with golang.org/x/text/unicode/norm I noticed the file
    containing generated Unicode tables is slow to format, and would
    probably make for a good benchmark. The file has the advantage of
    existing in the source tree.
    
        name                   time/op
        FormatUnicodeTables-4     160ms ± 2%
    
        name                   speed
        FormatUnicodeTables-4  2.18MB/s ± 2%
    
        name                   alloc/op
        FormatUnicodeTables-4    25.6MB ± 0%
    
        name                   allocs/op
        FormatUnicodeTables-4      611k ± 0%
    
    Updates golang/go#26528.
    
    Change-Id: Id952a938a73605acf8b3a0fbb4fa2b0e848b9b0e

---
+++ src/go/format/benchmark_test.go
@@ -0,0 +1,32 @@
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package format_test
+
+import (
+	"go/format"
+	"io/ioutil"
+	"path/filepath"
+	"runtime"
+	"testing"
+)
+
+var sink []byte
+
+func BenchmarkFormatUnicodeTables(b *testing.B) {
+	tableFile := filepath.Join(runtime.GOROOT(), "src", "vendor", "golang_org", "x", "text", "unicode", "norm", "tables.go")
+	data, err := ioutil.ReadFile(tableFile)
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		sink, err = format.Source(data)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
