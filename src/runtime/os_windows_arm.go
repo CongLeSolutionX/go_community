b0f10daaeace48c5e5df295f2f5996b882e1abe9
Jordan Rhee <jordanrh@microsoft.com>
2018-08-08 14:44:42 -0700

    runtime: support windows/arm
    Updates #26148
    
    Change-Id: I8f68b2c926c7b11dc86c9664ed7ff2d2f78b64b4

---
+++ src/runtime/os_windows_arm.go
@@ -0,0 +1,37 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package runtime
+
+const (
+	_AT_PLATFORM = 15 //  introduced in at least 2.6.11
+
+	_HWCAP_VFP   = 1 << 6  // introduced in at least 2.6.11
+	_HWCAP_VFPv3 = 1 << 13 // introduced in 2.6.30
+	_HWCAP_IDIVA = 1 << 17
+)
+
+var hwcap uint32 // set by archauxv
+var hardDiv bool // set if a hardware divider is available
+
+//go:nosplit
+func cputicks() int64 {
+	return nanotime()
+}
+
+func archauxv(tag, val uintptr) {
+	switch tag {
+	case _AT_HWCAP: // CPU capability bit flags
+		hwcap = uint32(val)
+		hardDiv = (hwcap & _HWCAP_IDIVA) != 0
+	}
+}
+
+func checkgoarm() {
+	if goarm < 7 {
+		print("Need atomic synchronization instructions, coprocessor ",
+			"access instructions. Recompile using GOARM=7.\n")
+		exit(1)
+	}
+}
