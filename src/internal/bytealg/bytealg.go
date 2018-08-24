b7b383f4f5273814170578394e0b2213bf32bfef
Martin Möhrmann <moehrmann@google.com>
2018-08-24 11:02:00 +0200

    internal/bytealg: align cpu feature variable offset naming with the runtime
    Add an "offset_" prefix to all cpu feature variable offset constants to
    signify that they are not boolean cpu feature variables.
    
    Change-Id: I6e22a79ebcbe6e2ae54c4ac8764f9260bb3223ff

--- src/internal/bytealg/bytealg.go
+++ src/internal/bytealg/bytealg.go
@@ -11,11 +11,12 @@ import (
 
 // Offsets into internal/cpu records for use in assembly.
 const (
-	x86_HasSSE2   = unsafe.Offsetof(cpu.X86.HasSSE2)
-	x86_HasSSE42  = unsafe.Offsetof(cpu.X86.HasSSE42)
-	x86_HasAVX2   = unsafe.Offsetof(cpu.X86.HasAVX2)
-	x86_HasPOPCNT = unsafe.Offsetof(cpu.X86.HasPOPCNT)
-	s390x_HasVX   = unsafe.Offsetof(cpu.S390X.HasVX)
+	offset_x86_HasSSE2   = unsafe.Offsetof(cpu.X86.HasSSE2)
+	offset_x86_HasSSE42  = unsafe.Offsetof(cpu.X86.HasSSE42)
+	offset_x86_HasAVX2   = unsafe.Offsetof(cpu.X86.HasAVX2)
+	offset_x86_HasPOPCNT = unsafe.Offsetof(cpu.X86.HasPOPCNT)
+
+	offset_s390x_HasVX = unsafe.Offsetof(cpu.S390X.HasVX)
 )
 
 // MaxLen is the maximum length of the string to be searched for (argument b) in Index.
