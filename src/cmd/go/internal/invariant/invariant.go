a06e4d003286e566a7415d3f69d6b397ec78e710
Bryan C. Mills <bcmills@google.com>
2018-09-18 15:37:42 -0400

    cmd/go/internal/invariant: collect invariant violations
    Sample output:
    
        ~/go/src$ go build -o failgo cmd/go && ./failgo
        internal error at /home/bcmills/go/src/cmd/go/main.go:87: Invariants should not fail intentionally.
        Please report this, ideally with steps to reproduce, at https://golang.org/issue.
    
        Go is a tool for managing Go source code.
        […]
    
    In previous code reviews, I have suggested that we add panics to detect
    invariant violations during testing, and Russ has sometimes been reluctant to
    add them out of concern that such panics may needlessly fail commands that could
    otherwise succeed.
    
    This change adds an experimental package for collecting invariant violations.
    Its behavior can be customized (exactly once) via a hook. That way, we can panic
    during tests, but only log violations (and return an explicit boolean to the
    caller for recovery) in the production code path.
    
    The fact that the hook can be customized makes the invariant package
    intentionally useless for control flow: unlike a panic, an invariant cannot be
    recovered reliably.
    
    The fact that the hook can return control to the caller helps to mitigate
    https://golang.org/doc/faq#assertions: since the hook may merely log the
    violation and return to the caller, they are still prompted to consider how the
    program should proceed in case of error.
    
    If this package proves useful and usable, I intend to turn it into a proposal
    for standard library integration. I believe it is a candidate for the standard
    library because there should be only one customization point per binary, the
    testing package should make use of that customization point automatically for
    test binaries, and the compiler should synthesize descriptions for simple
    conditions rather than requiring an explicit string in user code.
    
    Change-Id: Iffaa17aba2e3319ee9a9d19b6386e7e99c96be38

---
+++ src/cmd/go/internal/invariant/invariant.go
@@ -0,0 +1,91 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package invariant reports invariant violations robustly.
+package invariant
+
+import (
+	"fmt"
+	"sync"
+)
+
+func defaultAction(desc string) {
+	if len(desc) > 0 {
+		panic("invariant violated: " + desc)
+	}
+	panic("invariant violated")
+}
+
+var (
+	mu        sync.Mutex
+	reporters []func(string)
+)
+
+// ReportViolationsTo registers f as a callback to be invoked if an invariant check fails,
+// instead of the default action and after any previous ReportTo callbacks.
+//
+// If any such callback panics, no further callbacks will be invoked for the
+// violation. If all callbacks return normally, calls to Check or Checkf for
+// violated invariants will return false.
+func ReportViolationsTo(f func(desc string)) {
+	Check(f != nil, "OnViolation argument must be non-nil.")
+	mu.Lock()
+	reporters = append(reporters, f)
+	mu.Unlock()
+}
+
+// Check verifies that ok is true.
+// If it is, Check returns true immediately.
+//
+// Otherwise, Check reports that the invariant, optionally described by
+// fmt.Sprint(args...), was violated.
+//
+// If a hook has been registered by a call to OnViolation, that hook is invoked
+// directly from Check.
+// Otherwise, Check takes an unspecified action (normally resulting in a panic,
+// test failure, or program termination) to report the violation.
+// If the hook function returns, Check returns false.
+func Check(ok bool, v ...interface{}) bool {
+	if ok {
+		return true
+	}
+
+	desc := fmt.Sprint(v...)
+
+	mu.Lock()
+	rs := reporters
+	mu.Unlock()
+
+	if len(rs) == 0 {
+		defaultAction(desc)
+	} else {
+		for _, f := range rs {
+			f(desc)
+		}
+	}
+	return false
+}
+
+// Checkf is like Check, but formats the arguments using fmt.Sprintf instead of
+// fmt.Sprint.
+func Checkf(ok bool, format string, args ...interface{}) bool {
+	if ok {
+		return true
+	}
+
+	desc := fmt.Sprintf(format, args...)
+
+	mu.Lock()
+	rs := reporters
+	mu.Unlock()
+
+	if len(rs) == 0 {
+		defaultAction(desc)
+	} else {
+		for _, f := range rs {
+			f(desc)
+		}
+	}
+	return false
+}
