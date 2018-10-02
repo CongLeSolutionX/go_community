9af94711bc1ef0a48fe4f59925919123047a141d
David Chase <drchase@google.com>
2018-10-02 16:17:56 -0400

    cmd/compile: add option for user-supplied list of file/line/column specials
    DO NOT SUBMIT
    
    This is partly for estimating escape analysis upside from tracking
    interface methods called on parameters, and partly to play with
    a mechanism we might use for feedback-directed optimization.
    
    This is not in a terribly clean state, but submitting it puts
    it someplace safer than my local sandbox.
    
    The code to capture this information for benchmarks is not
    correct; it reports wrong (too small) information.
    
    Change-Id: I0d264aa1b74b40d1bd7d6337ce9a4340bc0747ba

--- src/cmd/compile/internal/gc/walk.go
+++ src/cmd/compile/internal/gc/walk.go
@@ -5,11 +5,14 @@
 package gc
 
 import (
+	"cmd/compile/internal/ssa"
 	"cmd/compile/internal/types"
 	"cmd/internal/objabi"
+	"cmd/internal/src"
 	"cmd/internal/sys"
 	"encoding/binary"
 	"fmt"
+	"path/filepath"
 	"strings"
 )
 
@@ -197,7 +200,7 @@ func walkstmt(n *Node) *Node {
 				yyerror("%v escapes to heap, not allowed in runtime.", v)
 			}
 			if prealloc[v] == nil {
-				prealloc[v] = callnew(v.Type)
+				prealloc[v] = callnew(v.Type, n.Pos)
 			}
 			nn := nod(OAS, v.Name.Param.Heapaddr, prealloc[v])
 			nn.SetColas(true)
@@ -1156,7 +1159,7 @@ opswitch:
 			r = typecheck(r, Erv)
 			n = r
 		} else {
-			n = callnew(n.Type.Elem())
+			n = callnew(n.Type.Elem(), n.Pos)
 		}
 
 	case OADDSTR:
@@ -1338,6 +1341,12 @@ opswitch:
 				argtype = types.Types[TINT]
 			}
 
+			if ssa.UserClaim(ssa.ClaimTagAlloc, n.Pos, Ctxt) != 0 {
+				fnname += "Notable"
+			}
+
+			maybeReportAllocation(n.Pos, fnname)
+
 			fn := syslook(fnname)
 			fn = substArgTypes(fn, t.Elem()) // any-1
 			n = mkcall1(fn, t, init, typename(t.Elem()), conv(len, argtype), conv(cap, argtype))
@@ -1402,7 +1411,7 @@ opswitch:
 			if n.Esc == EscNone && len(sc) <= maxImplicitStackVarSize {
 				a = nod(OADDR, temp(t), nil)
 			} else {
-				a = callnew(t)
+				a = callnew(t, n.Pos)
 			}
 			p := temp(t.PtrTo()) // *[n]byte
 			init.Append(typecheck(nod(OAS, p, a), Etop))
@@ -1919,16 +1928,31 @@ func walkprint(nn *Node, init *Nodes) *Node {
 	return r
 }
 
-func callnew(t *types.Type) *Node {
+func callnew(t *types.Type, pos src.XPos) *Node {
 	if t.NotInHeap() {
 		yyerror("%v is go:notinheap; heap allocation disallowed", t)
 	}
 	dowidth(t)
-	fn := syslook("newobject")
+	fnname := "newobject"
+	if ssa.UserClaim(ssa.ClaimTagAlloc, pos, Ctxt) != 0 {
+		fnname += "Notable"
+	}
+	fn := syslook(fnname)
+
+	maybeReportAllocation(pos, fnname)
+
 	fn = substArgTypes(fn, t)
 	v := mkcall1(fn, types.NewPtr(t), nil, typename(t))
 	v.SetNonNil(true)
 	return v
+}
+
+func maybeReportAllocation(pos src.XPos, fnname string) {
+	if testopt&TESTOPT_REPORT_ALLOC != 0 {
+		file, line, column, _ := Ctxt.OutermostPos(pos).FormatFileLineCol() // match linestr in gc/subr.go
+		file = filepath.Base(file)
+		fmt.Printf("%s,%s,%d,%d,%d,\"%s\"\n", "alloc", file, line, column, 1, fnname)
+	}
 }
 
 // isReflectHeaderDataField reports whether l is an expression p.Data
