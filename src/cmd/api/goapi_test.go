5709df8116c83d2785ff030fd7aa2ffa45439a35
Meng Zhuo <mengzhuo1203@gmail.com>
2018-10-03 23:30:49 +0800

    cmd/api: explicit tagKey with GOOS and GOARCH
    The origin tagKey is just dirname if no tags input which will cause
    pkgCache missmatch if other imported pkg explicit on GOARCH or GOOS
    
    This CL will add GOOS and GOARCH to tagKey
    
    Fixes #8425
    Fixes #21181
    
    Change-Id: Ifc189cf6746d753ad7c7e5bb60621297fc0a4e35

--- src/cmd/api/goapi_test.go
+++ src/cmd/api/goapi_test.go
@@ -188,3 +188,19 @@ func BenchmarkAll(b *testing.B) {
 		}
 	}
 }
+
+func TestIssue21181(t *testing.T) {
+	for _, c := range contexts {
+		c.Compiler = build.Default.Compiler
+	}
+	for _, context := range contexts {
+		w := NewWalker(context, "testdata/src/issue21181")
+		pkg, err := w.Import("p")
+		if err != nil {
+			t.Fatal(err)
+			t.Logf("(%s-%s) %s %v", context.GOOS, context.GOARCH,
+				pkg.Name(), w.imported)
+		}
+		w.export(pkg)
+	}
+}
