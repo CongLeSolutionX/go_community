f04b740fc6c7117937d2289f460c009357a74054
Michael Anthony Knyszek <mknyszek@google.com>
2018-10-04 15:33:08 +0000

    runtime: scavenge large spans before heap growth
    This change scavenges the largest spans before growing the heap for
    physical pages to "make up" for the newly-mapped space which,
    presumably, will be touched.
    
    This approach to scavenging helps reduce the RSS of an application by
    returning some of the memory left over as a result of fragmentation
    back to the OS more eagerly.
    
    Change-Id: Iab60790be05935865fc71f793cb9323ab00a18bd

--- src/runtime/mgclarge.go
+++ src/runtime/mgclarge.go
@@ -244,6 +244,46 @@ func (root *mTreap) removeSpan(span *mspan) {
 	root.removeNode(t)
 }
 
+// scavengeLargest scavenges npages worth of physical pages from spans in root
+// starting from the largest span and working down. It then takes those spans
+// and places them in scav.
+func scavengeLargest(unscav *mTreap, scav *mTreap, npages uintptr) {
+	t := unscav.treap
+	for npages > 0 {
+		if t == nil {
+			return
+		}
+		// Just grab the largest free span.
+		for t.right != nil {
+			t = t.right
+		}
+		s := t.spanKey
+		// If the largest free span is more than we need, find and remove
+		// the best fit and quit.
+		if s.npages >= npages {
+			s = unscav.remove(npages)
+			s.scavenge()
+			scav.insert(s)
+			return
+		}
+		released := s.scavenge()
+		// released is in bytes, so just round up to the nearest number
+		// of pages released.
+		npages -= (released + pageSize - 1) / pageSize
+		p := t.parent
+		unscav.removeNode(t)
+		scav.insert(s)
+		// We can start from the p, since only the subtree of p was
+		// modified by t's removal. If t was the root (no parent)
+		// just start again from the root.
+		if p != nil {
+			t = p
+		} else {
+			t = unscav.treap
+		}
+	}
+}
+
 // scavenge visits each node in the unscav treap and scavenges the
 // treapNode's span. It then removes the scavenged span from
 // itself and adds it into scav before continuing.
