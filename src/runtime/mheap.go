6703e552e56ebbaa89d8d19da9da4b1ddd5079d5
Michael Anthony Knyszek <mknyszek@google.com>
2018-10-04 15:33:08 +0000

    runtime: scavenge large spans before heap growth
    This change scavenges the largest spans before growing the heap for
    physical pages to "make up" for the newly-mapped space which,
    presumably, will be touched.
    
    In theory, this approach to scavenging helps reduce the RSS of an
    application by marking fragments in memory as reclaimable to the OS
    more eagerly than before. In practice this may not necessarily be
    true, depending on how sysUnused is implemented for each platform.
    
    Fixes #14045.
    
    Change-Id: Iab60790be05935865fc71f793cb9323ab00a18bd

--- src/runtime/mheap.go
+++ src/runtime/mheap.go
@@ -933,6 +933,14 @@ func (h *mheap) grow(npage uintptr) bool {
 		return false
 	}
 
+	// Scavenge some pages out of the free treap to make up for
+	// the virtual memory space we just allocated. We prefer to
+	// scavenge the largest spans first since the cost of scavenging
+	// is proportional to the number of sysUnused() calls rather than
+	// the number of pages released, so we make fewer of those calls
+	// with larger spans.
+	scavengeLargest(&h.free, &h.scav, size/pageSize)
+
 	// Create a fake "in use" span and free it, so that the
 	// right coalescing happens.
 	s := (*mspan)(h.spanalloc.alloc())
@@ -1110,6 +1118,55 @@ func scavenge1(unscav *mTreap, t *treapNode, scav *mTreap, now, limit uint64) ui
 	next := unscav.removeNode(t)
 	scav.insert(s)
 	return released + scavenge1(unscav, next, scav, now, limit)
+}
+
+// scavengeLargest scavenges npages worth of physical pages from spans in unscav
+// starting from the largest span and working down. It then takes those spans
+// and places them in scav.
+func scavengeLargest(unscav *mTreap, scav *mTreap, npages uintptr) {
+	t := unscav.treap
+	for npages > 0 {
+		if t == nil {
+			return
+		}
+		// Just grab the largest free span.
+		for t.right != nil {
+			t = t.right
+		}
+		s := t.spanKey
+		start, end := s.physPageBounds()
+		if end-start == 0 {
+			// Since we're going in order of largest-to-smallest span, this
+			// means all other spans are no bigger than s, so we should stop
+			// here before we accidentally iterate over the whole treap.
+			return
+		}
+		// If the largest free span is more than we need, find and remove
+		// the best fit and quit.
+		if end-start >= npages {
+			s = unscav.remove(npages)
+			if s != nil {
+				s.scavenge()
+				scav.insert(s)
+			}
+			return
+		}
+		r := s.scavenge()
+		// r is in bytes, so just round up to the nearest number of pages
+		// released.
+		npages -= (r + pageSize - 1) / pageSize
+		p := t.parent
+		unscav.removeNode(t)
+		scav.insert(s)
+		// We can start from the p, since only the subtree of p was
+		// modified by t's removal. If t was the root (no parent)
+		// just start again from the root.
+		if p != nil {
+			t = p
+		} else {
+			t = unscav.treap
+		}
+	}
 }
 
 func (h *mheap) scavenge(k int32, now, limit uint64) {
