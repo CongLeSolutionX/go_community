985d0b3b9d8d4e22847b920629b764a49c1cba71
Alex Brainman <alex.brainman@gmail.com>
2018-10-21 14:57:58 +1100

    path/filepath: change IsAbs("NUL") to return true
    This CL changes IsAbs to return true for "NUL" and other Windows
    reserved filenames (search
    https://docs.microsoft.com/en-us/windows/desktop/fileio/naming-a-file
    for NUL for details). os.Open("NUL") and os.Stat("NUL") work
    regardless of what current directory is, and it is mistake to join
    "NUL" with current directory when building full path. Changing
    IsAbs("NUL") to return true fixes that mistake.
    
    Fixes #28035
    
    Change-Id: Ife8f8aee48400702613ede8fc6834fd43e6e0f03

--- src/path/filepath/path_windows.go
+++ src/path/filepath/path_windows.go
@@ -13,8 +13,58 @@ func isSlash(c uint8) bool {
 	return c == '\\' || c == '/'
 }
 
+// reservedNames lists reserved Windows names. Search for PRN in
+// https://docs.microsoft.com/en-us/windows/desktop/fileio/naming-a-file
+// for details.
+var reservedNames = []string{
+	"CON", "PRN", "AUX", "NUL",
+	"COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9",
+	"LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9",
+}
+
+// toSmallLeter converts c into small letter, otherwise it returns c.
+func toSmallLeter(c uint8) uint8 {
+	if 'A' <= c && c <= 'Z' {
+		return c + 'a' - 'A'
+	}
+	return c
+}
+
+// sameReservedName returns true if a and b refer to the same Windows reserved name.
+func sameReservedName(a, b string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	if len(a) == 0 {
+		return false
+	}
+	for i := range a {
+		if toSmallLeter(a[i]) != toSmallLeter(b[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+// isReservedName returns true, if path is Windows reserved name.
+// See reservedNames for the full list.
+func isReservedName(path string) bool {
+	if len(path) == 0 {
+		return false
+	}
+	for _, reserved := range reservedNames {
+		if sameReservedName(path, reserved) {
+			return true
+		}
+	}
+	return false
+}
+
 // IsAbs reports whether the path is absolute.
 func IsAbs(path string) (b bool) {
+	if isReservedName(path) {
+		return true
+	}
 	l := volumeNameLen(path)
 	if l == 0 {
 		return false
