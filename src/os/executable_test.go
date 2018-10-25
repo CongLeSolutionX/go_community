3fd46c2e50091edbff34afc59b3d6d8bc74506b5
Clément Chigot <clement.chigot@atos.net>
2018-10-25 10:02:37 +0200

    os: fix tests for AIX
    This commits fixes tests for AIX inside os package.
    
    "hostname" command on AIX returns "name.domain" and not only "name".
    So, "hostname -s" must be called.
    
    Change-Id: I75e193bcb6ad607ce54ad99aabbed9839012f707

--- src/os/executable_test.go
+++ src/os/executable_test.go
@@ -36,7 +36,7 @@ func TestExecutable(t *testing.T) {
 	// forge argv[0] for child, so that we can verify we could correctly
 	// get real path of the executable without influenced by argv[0].
 	cmd.Args = []string{"-", "-test.run=XXXX"}
-	if runtime.GOOS == "openbsd" {
+	if runtime.GOOS == "openbsd" || runtime.GOOS == "aix" {
 		// OpenBSD relies on argv[0]
 		cmd.Args[0] = fn
 	}
