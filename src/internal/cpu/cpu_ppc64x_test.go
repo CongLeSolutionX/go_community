810017596e881d1a530290070dd302f424f2188f
Martin Möhrmann <moehrmann@google.com>
2018-10-26 18:09:42 +0200

    internal/cpu: remove unused and not required ppc64(le) feature detection
    Minimum Go requirement for ppc64(le) architecture support is POWER8.
    https://github.com/golang/go/wiki/MinimumRequirements#ppc64-big-endian
    
    Reduce CPU features supported in internal/cpu to those needed to
    test minimum requirements for ppc64(le). No current internal/cpu
    feature variables are used to guard code from using unsupported
    instructions.
    
    Reducing the set of detected CPU features for ppc64(le) makes
    implementing Go support for new operating systems easier as
    CPU feature detection for ppc64(le) needs operating system support
    (e.g. hwcap on Linux and getsystemcfg syscall on AIX).
    
    Change-Id: Ic4c17b31610970e481cd139c657da46507391d1d

--- src/internal/cpu/cpu_ppc64x_test.go
+++ src/internal/cpu/cpu_ppc64x_test.go
@@ -15,19 +15,4 @@ func TestPPC64minimalFeatures(t *testing.T) {
 	if !PPC64.IsPOWER8 {
 		t.Fatalf("IsPOWER8 expected true, got false")
 	}
-	if !PPC64.HasVMX {
-		t.Fatalf("HasVMX expected true, got false")
-	}
-	if !PPC64.HasDFP {
-		t.Fatalf("HasDFP expected true, got false")
-	}
-	if !PPC64.HasVSX {
-		t.Fatalf("HasVSX expected true, got false")
-	}
-	if !PPC64.HasISEL {
-		t.Fatalf("HasISEL expected true, got false")
-	}
-	if !PPC64.HasVCRYPTO {
-		t.Fatalf("HasVCRYPTO expected true, got false")
-	}
 }
