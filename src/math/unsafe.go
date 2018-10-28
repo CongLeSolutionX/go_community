78294a5ec2d3bbda624cce7c52010e834bea1e4c
Aaron Cannon <cannona@fireantproductions.com>
2018-10-28 12:34:52 -0500

    math: document Float*bits and Float*frombits to specify the order of bits
    Float32bits and Float64bits do not specify the order of bits they return, and
    Float32frombits and Float64frombits likewise do not specify the order of bits
    they expect. This change rectifies this situation.
    
    Fixes #27736
    
    Change-Id: Iae49aac68abb436e569b77f0b2c1cf4248b39355

--- src/math/unsafe.go
+++ src/math/unsafe.go
@@ -7,15 +7,27 @@ package math
 import "unsafe"
 
 // Float32bits returns the IEEE 754 binary representation of f.
+// the most significant bit of the return value will contain the sign bit,
+// the next 8 most significant bits the exponent,
+// and the 23 least significant bits the significand or mantissa.
 func Float32bits(f float32) uint32 { return *(*uint32)(unsafe.Pointer(&f)) }
 
 // Float32frombits returns the floating point number corresponding
 // to the IEEE 754 binary representation b.
+// The most significant bit of b should contain the sign bit,
+// the next 8 most significant bits the exponent,
+// and the 23 least significant bits the significand or mantissa.
 func Float32frombits(b uint32) float32 { return *(*float32)(unsafe.Pointer(&b)) }
 
 // Float64bits returns the IEEE 754 binary representation of f.
+// the most significant bit of the return value will contain the sign bit,
+// the next 12 most significant bits the exponent,
+// and the 51 least significant bits the significand or mantissa.
 func Float64bits(f float64) uint64 { return *(*uint64)(unsafe.Pointer(&f)) }
 
 // Float64frombits returns the floating point number corresponding
 // the IEEE 754 binary representation b.
+// The most significant bit of b should contain the sign bit,
+// the next 12 most significant bits the exponent,
+// and the 51 least significant bits the significand or mantissa.
 func Float64frombits(b uint64) float64 { return *(*float64)(unsafe.Pointer(&b)) }
