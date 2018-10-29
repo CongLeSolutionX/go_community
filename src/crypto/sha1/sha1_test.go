813bece279437e44a0a1a81014d7566d7eb21fdf
David Benjamin <davidben@google.com>
2018-10-28 22:54:39 -0500

    crypto/sha1: add ConstantTimeSumWithData
    The ConstantTimeSum operation does not provide a full Lucky 13
    countermeasure.
    
    In TLS CBC padding, we have some unauthenticated input where some secret
    prefix is the plaintext. We must compute HMAC on that prefix without
    leaking its length.
    
    ConstantTimeSum attempts to protect d.nx, or plaintextLen % BlockSize.
    This value is indeed secret, but it was set by a preceding
    Write(in[:plaintextLen]) call which took no precautions.
    
    While we could implement a ConstantTimeWrite, it would need to treat
    d.nx secret to account for the caller calling ConstantTimeWrite several
    times in a row. This is complexity which is not needed by TLS. In TLS,
    the only secret length is the final Write() call.
    
    Instead, we replace ConstantTimeSum with ConstantTimeSumWithData, which
    incorporates a final secret-length Write into the hash but assumes all
    prior Write lengths are public. This is sufficient to implement the MAC
    calculation step for TLS.
    
    The next commit will make crypto/tls use this function in its HMAC
    computation.
    
    Updates #27071
    
    Change-Id: I3f2b9ffe25e43f15dc441afb752a03ea8118c49a

--- src/crypto/sha1/sha1_test.go
+++ src/crypto/sha1/sha1_test.go
@@ -88,6 +88,27 @@ func TestGolden(t *testing.T) {
 			}
 			c.Reset()
 		}
+
+		// Test constant-time operation with up to 20+256 bytes of
+		// excess, matching the possible overheads in TLS CBC ciphers.
+		// (20 bytes of MAC and up to 256 bytes of padding.)
+		const maxExtra = 20 + 256
+		padded := make([]byte, len(g.in)+maxExtra)
+		copy(padded, []byte(g.in))
+		for i := 0; i < maxExtra; i++ {
+			padded[len(g.in)+i] = byte(i)
+		}
+		for start := 0; start <= BlockSize && start <= len(g.in); start++ {
+			for extra := 0; extra < maxExtra; extra++ {
+				io.WriteString(c, g.in[:start])
+				sum := c.(*digest).ConstantTimeSumWithData(nil, padded[start:len(g.in)+extra], len(g.in)-start)
+				s := fmt.Sprintf("%x", sum)
+				if s != g.out {
+					t.Fatalf("sha1[4][%d][%d](%s) = %s want %s", start, extra, g.in, s, g.out)
+				}
+				c.Reset()
+			}
+		}
 	}
 }
 
