5eec5a3e2b9ae6755ce819ad80dae06dd9cec2f4
Tobias Klauser <tklauser@distanz.ch>
2018-11-02 09:12:08 +0100

    os: add support for long path names on aix RemoveAll
    Follow CL 146020 and enable RemoveAll based on Unlinkat and Openat on
    aix.
    
    DO NOT SUBMIT
    
    Updates #27029
    
    Change-Id: I78b34ed671166ee6fa651d5f2025b88548ee6c68

--- src/internal/syscall/unix/at_aix.go
+++ src/internal/syscall/unix/at_aix.go
@@ -2,20 +2,18 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build aix,solaris
-
 package unix
 
 import "syscall"
 
-// Implemented as sysvicall6 in runtime/syscall_solaris.go.
+// Implemented in runtime/syscall_aix.go.
 func syscall6(trap, nargs, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2 uintptr, err syscall.Errno)
 
-//go:cgo_import_dynamic libc_fstatat fstatat "libc.so"
-//go:cgo_import_dynamic libc_openat openat "libc.so"
-//go:cgo_import_dynamic libc_unlinkat unlinkat "libc.so"
+//go:cgo_import_dynamic libc_fstatat fstatat "libc.a/shr_64.so"
+//go:cgo_import_dynamic libc_openat openat "libc.a/shr_64.so"
+//go:cgo_import_dynamic libc_unlinkat unlinkat "libc.a/shr_64.so"
 
 const (
 	AT_REMOVEDIR        = 0x1
-	AT_SYMLINK_NOFOLLOW = 0x1000
+	AT_SYMLINK_NOFOLLOW = 0x1
 )
