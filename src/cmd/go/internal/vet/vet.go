997e0058352def1ed941bfc621b4413fb7c26dd9
Alan Donovan <adonovan@google.com>
2018-11-02 11:27:53 -0400

    cmd/go: make 'go vet -help' show how to list vet tool's flags
    Example:
    	$ go vet -help
    	usage: go vet [-n] [-x] [build flags] [vet flags] [packages]
    	Run 'go help vet' for details.
    	Run 'go tool vet -help' for vet tool's flags.
    
    	$ GOVETTOOL=~/bin/myvet go vet -help
    	usage: go vet [-n] [-x] [build flags] [vet flags] [packages]
    	Run 'go help vet' for details.
    	Run '/home/me/bin/myvet -help' for vet tool's flags.
    
    Change-Id: Ieb79dfe29e1df074f865bc9a9d47b44199675d7d

--- src/cmd/go/internal/vet/vet.go
+++ src/cmd/go/internal/vet/vet.go
@@ -38,7 +38,7 @@ See also: go fmt, go fix.
 func runVet(cmd *base.Command, args []string) {
 	modload.LoadTests = true
 
-	vetFlags, pkgArgs := vetFlags(cmd.Usage, args)
+	vetFlags, pkgArgs := vetFlags(vetUsage, args)
 
 	work.BuildInit()
 	work.VetFlags = vetFlags
