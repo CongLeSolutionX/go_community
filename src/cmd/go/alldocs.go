51bba597ccb70296d88b199a991e660cfce78c3a
Ali Rizvi-Santiago <arizvisa@gmail.com>
2018-11-13 23:23:59 +0000

    cmd/go: allow the user to specify ar via an environment variable
    This allows one to customize which ar to use by fetching its path
    from the environment. This way one can swap it out for a
    different implementation.
    
    cmd/go/internal/work/gccgo.go: updated refs with environment var
    
    Change-Id: I40d8cbd8a69e97b5254e66081d9bf0b726c10366
    GitHub-Last-Rev: d47eba9777f9121bb69b7c71758d779c9708ce8e
    GitHub-Pull-Request: golang/go#28746

--- src/cmd/go/alldocs.go
+++ src/cmd/go/alldocs.go
@@ -1500,6 +1500,9 @@
 // 		The command to use to compile C++ code.
 // 	PKG_CONFIG
 // 		Path to pkg-config tool.
+// 	AR
+// 		The command to use to create, modify, and extract from library archives.
+// 		Only used when the compiler is gccgo ('-compiler=gccgo').
 //
 // Architecture-specific environment variables:
 //
--- src/cmd/go/internal/help/helpdoc.go
+++ src/cmd/go/internal/help/helpdoc.go
@@ -546,6 +546,9 @@ Environment variables for use with cgo:
 		The command to use to compile C++ code.
 	PKG_CONFIG
 		Path to pkg-config tool.
+	AR
+		The command to use to create, modify, and extract from library archives.
+		Only used when the compiler is gccgo ('-compiler=gccgo').
 
 Architecture-specific environment variables:
 
