84aa2b8c6f23b2af8399cc627c4df0986aacb4d0
Mark Pulford <mark@kyne.com.au>
2018-11-30 20:50:56 +1100

    cmd/cgo: add test for frames lost via SetCgoTraceback
    Updates #29034
    
    Change-Id: If8ff8ea5708aa739053e6975579325ef0b2714f0

---
+++ misc/cgo/test/issue29034w.go
@@ -0,0 +1,12 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build windows
+
+package cgotest
+
+import "testing"
+
+func test29034(t *testing.T) {
+}
