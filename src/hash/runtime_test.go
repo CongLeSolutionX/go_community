05a4cebd5ab0419ba01410e6166dedff564056a4
Alan Donovan <adonovan@google.com>
2018-12-19 16:02:03 -0500

    hash: Runtime, an API for the hash function used by the Go runtime
    Fixes golang/go#28322
    
    Change-Id: I489b2d91240676a0c0f07e555a9cf892ca1ffb6b

---
+++ src/hash/runtime_test.go
@@ -0,0 +1,52 @@
+package hash_test
+
+import (
+	"hash"
+	"io"
+	"testing"
+)
+
+func TestRuntimeHash(t *testing.T) {
+	h := hash.NewRuntime()
+	io.WriteString(h, "abc")
+	io.WriteString(h, "def")
+	h1 := h.Sum(nil)
+
+	h.Reset()
+	io.WriteString(h, "abc")
+	io.WriteString(h, "def")
+	h2 := h.Sum(nil)
+
+	if string(h1) != string(h2) {
+		t.Errorf("hashes differ: %08x, %08x", h1, h2)
+	}
+
+	// Writing the same message in different chunks
+	// changes the hash ("with high probability").
+	// TODO: is that desirable?
+	h.Reset()
+	io.WriteString(h, "ab")
+	io.WriteString(h, "cdef")
+	h3 := h.Sum(nil)
+
+	if string(h1) == string(h3) {
+		t.Errorf("hashes same: %08x, %08x", h1, h2)
+	}
+}
+
+func BenchmarkRuntimeHashNoAllocs(b *testing.B) {
+	b.ReportAllocs()
+
+	var sum [8]byte
+	data := []byte("abc")
+
+	// This loop should not allocate memory.
+	// This property relies on h.Write being a direct call;
+	// an indirect call would cause data to be heap-allocated.
+	for i := 0; i < b.N; i++ {
+		h := hash.NewRuntime()
+		h.Write(data)
+		h.WriteString("abc")
+		_ = h.Sum(sum[:0])
+	}
+}
