9c0088a531052c90d4503056fa38883f7bc1a5ea
Josh Bleecher Snyder <josharian@gmail.com>
2018-12-30 08:22:27 -1000

    strconv: make docs for Itoa and Atoi slightly higher level
    Fixes #29461
    
    Change-Id: I5db8bc80e5bd0778dced8471581c67e66853aada

--- src/strconv/itoa.go
+++ src/strconv/itoa.go
@@ -30,7 +30,7 @@ func FormatInt(i int64, base int) string {
 	return s
 }
 
-// Itoa is shorthand for FormatInt(int64(i), 10).
+// Itoa returns the same result as FormatInt(int64(i), 10).
 func Itoa(i int) string {
 	return FormatInt(int64(i), 10)
 }
