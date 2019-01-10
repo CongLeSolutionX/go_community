2913c504c77535d10d1e4428c8bfd94052994bd4
Vladimir Kuzmin <vkuzmin@uber.com>
2019-01-09 23:02:59 -0800

    net/http: Status Codes range checks
    Adds functions to check if http StatusCode is in the following ranges
    https://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml
    
    1xx: Informational - Request received, continuing process
    2xx: Success - The action was successfully received, understood, and accepted
    3xx: Redirection - Further action must be taken in order to complete the request
    4xx: Client Error - The request contains bad syntax or cannot be fulfilled
    5xx: Server Error - The server failed to fulfill an apparently valid request
    
    Fixes #29652
    
    Change-Id: I3c664258d62f11cad87c7140bff5e04c012ecc2c

---
+++ src/net/http/status_test.go
@@ -0,0 +1,62 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package http_test
+
+import (
+	. "net/http"
+	"testing"
+)
+
+func TestStatusType(t *testing.T) {
+	code := StatusContinue
+	if !IsInformational(code) {
+		t.Errorf("Status %s is not 1xx: Informational", StatusText(code))
+	}
+
+	code = StatusCreated
+	if IsInformational(code) {
+		t.Errorf("Status %s is 1xx: Informational", StatusText(code))
+	}
+
+	code = StatusOK
+	if !IsSuccess(code) {
+		t.Errorf("Status %s is not 2xx: Success", StatusText(code))
+	}
+
+	code = StatusProcessing
+	if IsSuccess(code) {
+		t.Errorf("Status %s is 2xx: Success", StatusText(code))
+	}
+
+	code = StatusFound
+	if !IsRedirection(code) {
+		t.Errorf("Status %s is not 3xx: Redirection", StatusText(code))
+	}
+
+	code = StatusNotFound
+	if IsRedirection(code) {
+		t.Errorf("Status %s is 3xx: Redirection", StatusText(code))
+	}
+
+	code = StatusBadRequest
+	if !IsClientError(code) {
+		t.Errorf("Status %s is not 4xx: ClientError", StatusText(code))
+	}
+
+	code = StatusGatewayTimeout
+	if IsClientError(code) {
+		t.Errorf("Status %s is 4xx: ClientError", StatusText(code))
+	}
+
+	code = StatusInternalServerError
+	if !IsServerError(code) {
+		t.Errorf("Status %s is not 5xx: ServerError", StatusText(code))
+	}
+
+	code = StatusRequestURITooLong
+	if IsServerError(code) {
+		t.Errorf("Status %s is 5xx: ServerError", StatusText(code))
+	}
+}
