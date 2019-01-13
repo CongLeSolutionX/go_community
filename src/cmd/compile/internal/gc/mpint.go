fc5fb457ca11c226224cb97c7481c3df9e0e21f6
Robert Griesemer <gri@golang.org>
2019-01-12 20:33:58 -0800

    cmd/compile: accept new Go2 number literals
    This CL introduces compiler support for the new binary and octal integer
    literals, hexadecimal floats, and digit separators for all number literals.
    
    The new Go 2 number literal scanner accepts the following liberal format:
    
    number   = [ prefix ] digits [ "." digits ] [ exponent ] [ "i" ] .
    prefix   = "0" [ "b" |"B" | "o" | "O" | "x" | "X" ] .
    digits   = { digit | "_" } .
    exponent = ( "e" | "E" | "p" | "P" ) [ "+" | "-" ] digits .
    
    If the number starts with "0x" or "0X", digit is any hexadecimal digit;
    otherwise, digit is any decimal digit. If the accepted number is not valid,
    errors are reported accordingly.
    
    See the new test cases in scanner_test.go for a selection of valid and
    invalid numbers and the respective error messages.
    
    R=Go1.13
    
    Updates #12711.
    Updates #19308.
    Updates #28493.
    Updates #29008.
    
    Change-Id: Ic8febc7bd4dc5186b16a8c8897691e81125cf0ca

--- src/cmd/compile/internal/gc/mpint.go
+++ src/cmd/compile/internal/gc/mpint.go
@@ -7,6 +7,7 @@ package gc
 import (
 	"fmt"
 	"math/big"
+	"strings"
 )
 
 // implements integer arithmetic
@@ -281,6 +282,12 @@ func (a *Mpint) SetInt64(c int64) {
 }
 
 func (a *Mpint) SetString(as string) {
+	// TODO(gri) remove this code once math/big.Int.SetString can handle 0o-octals and separators
+	as = strings.Replace(as, "_", "", -1) // strip separators
+	if len(as) >= 2 && as[0] == '0' && (as[1] == 'o' || as[1] == 'O') {
+		as = "0" + as[2:]
+	}
+
 	_, ok := a.Val.SetString(as, 0)
 	if !ok {
 		// required syntax is [+-][0[x]]d*
@@ -305,4 +312,12 @@ func (a *Mpint) GoString() string {
 
 func (a *Mpint) String() string {
 	return fmt.Sprintf("%#x", &a.Val)
+}
+
+func stripSep(s string) string {
+	// avoid making a copy if there are no separators (common case)
+	if strings.IndexByte(s, '_') >= 0 {
+		return strings.Replace(s, "_", "", -1)
+	}
+	return s
 }
