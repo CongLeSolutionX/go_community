503d41d624c3806929cb7b99de162df5652f888b
erifan01 <eric.fang@arm.com>
2019-01-15 09:19:58 +0000

    cmd/compile: intrinsify math/bits.ReverseBytes16 on arm
    This CL optimizes ReverseBytes16 with REV16 instruction for arm v6+.
    For arm v5, keeps the instruction sequence the same with that generated
    by Go compiler.
    This CL also makes ReverseBytes an alias of ReverseBytes32 and
    ReverseBytes64 on arm and arm64, respectively.
    
    Benchmarks on arm v5:
    name              old time/op  new time/op  delta
    ReverseBytes-8    3.00ns ± 0%  1.98ns ± 0%  -34.00%  (p=0.000 n=10+10)
    ReverseBytes16-8  3.00ns ± 0%  1.64ns ± 1%  -45.23%  (p=0.000 n=10+10)
    ReverseBytes32-8  1.98ns ± 0%  1.98ns ± 0%     ~     (all equal)
    ReverseBytes64-8  2.41ns ± 0%  2.41ns ± 0%     ~     (all equal)
    
    Benchmarks on arm v6 and above:
    ReverseBytes-8    3.00ns ± 0%  1.50ns ± 0%  -50.00%  (p=0.000 n=10+10)
    ReverseBytes16-8  3.00ns ± 0%  1.50ns ± 0%  -50.00%  (p=0.000 n=10+10)
    ReverseBytes32-8  1.50ns ± 0%  1.36ns ± 0%   -9.33%  (p=0.000 n=10+10)
    ReverseBytes64-8  1.50ns ± 0%  1.50ns ± 1%     ~     (p=0.412 n=10+8)
    
    Change-Id: I819e633c9a9d308f8e476fb0c82d73fb73dd019f

--- src/cmd/compile/internal/gc/ssa.go
+++ src/cmd/compile/internal/gc/ssa.go
@@ -3310,14 +3310,15 @@ func init() {
 	alias("math/bits", "ReverseBytes64", "runtime/internal/sys", "Bswap64", all...)
 	alias("math/bits", "ReverseBytes32", "runtime/internal/sys", "Bswap32", all...)
 	// ReverseBytes inlines correctly, no need to intrinsify it.
-	// ReverseBytes16 lowers to a rotate, no need for anything special here.
-	// Arm64 doesn't have a 16-bit rotate instruction, but there is a REV16 instruction
-	// for ReverseBytes16, so intrinsify ReverseBytes16 on arm64.
+	// ReverseBytes16 lowers to a rotate, no need for anything special here except
+	// those architectures that can't lower it to a rotate.
+	alias("math/bits", "ReverseBytes", "math/bits", "ReverseBytes32", sys.ArchARM)
+	alias("math/bits", "ReverseBytes", "math/bits", "ReverseBytes64", sys.ArchARM64)
 	addF("math/bits", "ReverseBytes16",
 		func(s *state, n *Node, args []*ssa.Value) *ssa.Value {
 			return s.newValue1(ssa.OpBswap16, types.Types[TUINT16], args[0])
 		},
-		sys.ARM64)
+		sys.ARM, sys.ARM64)
 	addF("math/bits", "Len64",
 		func(s *state, n *Node, args []*ssa.Value) *ssa.Value {
 			return s.newValue1(ssa.OpBitLen64, types.Types[TINT], args[0])
