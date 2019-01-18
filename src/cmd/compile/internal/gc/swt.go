79739945de05c2f00759fdd967a0545bc5cdd6af
Gergely Brautigam <skarlso777@gmail.com>
2019-01-18 21:43:56 +0100

    cmd/compile: suppress typecheck errors in a type switch case with broken type
    If a type switch case expression has failed typechecking, the case body is
    likely to also fail with confusing or spurious errors. Suppress
    typechecking the case body when this happens.
    
    Fixes golang/go#28926
    
    Change-Id: Idfdb9d5627994f2fd90154af1659e9a92bf692c4

--- src/cmd/compile/internal/gc/swt.go
+++ src/cmd/compile/internal/gc/swt.go
@@ -110,6 +110,7 @@ func typecheckswitch(n *Node) {
 
 	var def, niltype *Node
 	for _, ncase := range n.List.Slice() {
+		caseExprBroken := false
 		if ncase.List.Len() == 0 {
 			// default
 			if def != nil {
@@ -125,6 +126,7 @@ func typecheckswitch(n *Node) {
 				ls[i1] = typecheck(ls[i1], ctxExpr|Etype)
 				n1 = ls[i1]
 				if n1.Type == nil || t == nil {
+					caseExprBroken = true
 					continue
 				}
 
@@ -207,7 +209,12 @@ func typecheckswitch(n *Node) {
 				ncase.Rlist.SetFirst(nvar)
 			}
 		}
-
+		if caseExprBroken {
+			// The case expression failed typechecking.
+			// Avoid spurious/confusing errors by not also typechecking the case body.
+			// See golang.org/issue/28926.
+			continue
+		}
 		typecheckslice(ncase.Nbody.Slice(), ctxStmt)
 	}
 	switch top {
