080ad8b7e177c2a144a8bb47d2ad72d903115b81
Iskander Sharipov <quasilyte@gmail.com>
2019-01-31 13:00:51 +0300

    all: remove commented-out print statements
    Those print statements are not a good debug helpers
    and only clutter the code.
    
    Change-Id: Ifbf450a04e6fa538af68e6352c016728edb4119a

--- src/cmd/dist/test.go
+++ src/cmd/dist/test.go
@@ -1114,7 +1114,6 @@ func (t *tester) runPending(nextTest *distTest) {
 	var last *distTest
 	for ended < len(worklist) {
 		for started < len(worklist) && started-ended < maxbg {
-			//println("start", started)
 			w := worklist[started]
 			started++
 			w.start <- !t.failed || t.keepGoing
@@ -1135,7 +1134,6 @@ func (t *tester) runPending(nextTest *distTest) {
 		if vflag > 1 {
 			errprintf("%s\n", strings.Join(w.cmd.Args, " "))
 		}
-		//println("wait", ended)
 		ended++
 		<-w.end
 		os.Stdout.Write(w.out)
