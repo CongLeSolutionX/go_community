6083093287437ad078dcd9d8e7c574d0caad15f8
Jiajun Huang <gansteed@gmail.com>
2019-02-12 07:02:07 +0000

    net/http: close connection if `Connection: close` is set
    As https://tools.ietf.org/html/rfc2616#page-117 says:
    
    HTTP/1.1 defines the "close" connection option for the sender to signal that the connection will be closed after completion of the response.
    
    But it does not specify the connection should be closed by client or server, for safety, if `Connection: close`
    is set in response header, server should close the connection after write data. Recently we have been
    affect by this issue: we write a lots of data to client, but the client does not close the connection(
    keep-alive is set by default in HTTP/1.1), so memory is still hold by `response`, and memory is used up.
    
    Change-Id: I756f81ec85ab5667be8b4cf45625e53a8f7dd36c
    GitHub-Last-Rev: fa6a81050b3c599bcdac2ad1946ddd25a30b076a
    GitHub-Pull-Request: golang/go#28936

--- src/net/http/server.go
+++ src/net/http/server.go
@@ -1540,6 +1540,10 @@ func (w *response) bodyAllowed() bool {
 // bufferBeforeChunkingSize smaller and having bufio's fast-paths deal
 // with this instead.
 func (w *response) Write(data []byte) (n int, err error) {
+	if w.closeAfterReply {
+		defer w.conn.Close()
+	}
+
 	return w.write(len(data), data, "")
 }
 
