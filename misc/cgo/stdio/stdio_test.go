748732c654282295077def5b1b4cf34f93afc9f2
Bryan C. Mills <bcmills@google.com>
2019-02-22 12:14:23 -0500

    misc/cgo/stdio: fix tests in module mode
    Updates #30228
    
    Change-Id: I4d213c6fe68c47ccb877f13b55128e035f76a26b

---
+++ misc/cgo/stdio/stdio_test.go
@@ -0,0 +1,70 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package stdio_test
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func TestTestRun(t *testing.T) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	GOPATH, err := ioutil.TempDir("", "cgostdio")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(GOPATH)
+	os.Setenv("GOPATH", GOPATH)
+
+	modRoot := filepath.Join(GOPATH, "src", "cgostdio")
+	if err := os.MkdirAll(modRoot, 0777); err != nil {
+		t.Fatal(err)
+	}
+	if err := ioutil.WriteFile(filepath.Join(modRoot, "go.mod"), []byte("module cgostdio\n"), 0666); err != nil {
+		t.Fatal(err)
+	}
+	err = filepath.Walk("testdata", func(path string, info os.FileInfo, err error) error {
+		if err != nil || path == "testdata" {
+			return err
+		}
+
+		dstPath := modRoot + strings.TrimPrefix(path, "testdata")
+		if info.IsDir() {
+			return os.Mkdir(dstPath, 0777)
+		}
+
+		data, err := ioutil.ReadFile(filepath.Join(cwd, path))
+		if err != nil {
+			return err
+		}
+		return ioutil.WriteFile(dstPath, data, info.Mode())
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	out, err := exec.Command("go", "env", "GOROOT").Output()
+	if err != nil {
+		t.Fatal(err)
+	}
+	GOROOT := string(bytes.TrimSpace(out))
+
+	cmd := exec.Command("go", "run", filepath.Join(GOROOT, "test", "run.go"), "-", ".")
+	cmd.Dir = modRoot
+	out, err = cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("%s: %s\n%s", strings.Join(cmd.Args, " "), err, out)
+	}
+	t.Logf("%s:\n%s", strings.Join(cmd.Args, " "), out)
+}
--- src/cmd/dist/test.go
+++ src/cmd/dist/test.go
@@ -610,7 +610,7 @@ func (t *tester) registerTests() {
 			name:    "cgo_stdio",
 			heading: "../misc/cgo/stdio",
 			fn: func(dt *distTest) error {
-				t.addCmd(dt, "misc/cgo/stdio", "go", "run", filepath.Join(os.Getenv("GOROOT"), "test/run.go"), "-", ".")
+				t.addCmd(dt, "misc/cgo/stdio", t.goTest())
 				return nil
 			},
 		})
