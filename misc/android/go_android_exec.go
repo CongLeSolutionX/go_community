809a9bd594b61353a542010c199a746ad35d64a7
Elias Naur <mail@eliasnaur.com>
2019-02-25 10:52:42 +0100

    misc/android: serialize adb commands on android emulators
    Android emulator builders are soon to join the trybot set. To avoid
    flaky runs, work around a longstanding adb bug where concurrent adb
    commands sometimes fail.
    
    I haven't seen the problem on actual devices until recently. It seems
    that the recently added "adb wait-for-device" can introduce flakyness
    with errors such as:
    
    adb: error: failed to get feature set: protocol fault (couldn't read status): Connection reset by peer
    
    Instead of working around that, give up and serialize use of adb
    everywhere.
    
    Fixes #23795
    Updates #23824
    
    Change-Id: If347c9981fa32ff8a1e14b7454f122ef682450a6

--- misc/android/go_android_exec.go
+++ misc/android/go_android_exec.go
@@ -60,6 +60,19 @@ func main() {
 	log.SetFlags(0)
 	log.SetPrefix("go_android_exec: ")
 
+	// Concurrent use of adb is flaky, so serialize adb commands.
+	// See https://github.com/golang/go/issues/23795 or
+	// https://issuetracker.google.com/issues/73230216.
+	lockPath := filepath.Join(os.TempDir(), "go_android_exec-adb-lock")
+	lock, err := os.OpenFile(lockPath, os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer lock.Close()
+	if err := syscall.Flock(int(lock.Fd()), syscall.LOCK_EX); err != nil {
+		log.Fatal(err)
+	}
+
 	// In case we're booting a device or emulator alongside androidtest.bash
 	// wait for it to be ready. adb wait-for-device is not enough, we have to
 	// wait for sys.boot_completed.
