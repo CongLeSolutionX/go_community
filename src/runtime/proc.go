3017cb39b316200575cbfa143c08fb74e2a55c99
Andrew Bonventre <andybons@golang.org>
2019-03-14 18:54:57 +0000

    Revert "[release-branch.go1.11] runtime: don't clear lockedExt on locked M when G exits"
    This reverts commit f500f13d72de3ca3201ec2678a7f88524353bede.
    
    Reason for revert: This is causing failures on the linux/arm64 port. Example: https://build.golang.org/log/11807e3767832fa8571129f2e361297db3098a63
    
    Change-Id: I79a06caa798187359e20f170bc723c43a21e1198

--- src/runtime/proc.go
+++ src/runtime/proc.go
@@ -2774,6 +2774,7 @@ func goexit0(gp *g) {
 		print("invalid m->lockedInt = ", _g_.m.lockedInt, "\n")
 		throw("internal lockOSThread error")
 	}
+	_g_.m.lockedExt = 0
 	gfput(_g_.m.p.ptr(), gp)
 	if locked {
 		// The goroutine may have locked this thread because
@@ -2784,10 +2785,6 @@ func goexit0(gp *g) {
 		// the thread.
 		if GOOS != "plan9" { // See golang.org/issue/22227.
 			gogo(&_g_.m.g0.sched)
-		} else {
-			// Clear lockedExt on plan9 since we may end up re-using
-			// this thread.
-			_g_.m.lockedExt = 0
 		}
 	}
 	schedule()
