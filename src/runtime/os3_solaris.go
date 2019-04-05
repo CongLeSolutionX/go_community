3f6f92beb57a04c111d605ef05f8a2fd63ee663a
Ian Lance Taylor <iant@golang.org>
2019-04-05 11:42:37 -0700

    runtime: change read and write to return negative errno value
    The internal read and write functions used to return -1 on error;
    change them to return a negative errno value instead.
    This will be used by later CLs in this series.
    
    For most targets this is a simplification, although for ones that call
    into libc it is a complication.
    
    Updates #27707
    
    Change-Id: Id02bf9487f03e7e88e4f2b85e899e986738697ad

--- src/runtime/os3_solaris.go
+++ src/runtime/os3_solaris.go
@@ -442,7 +442,11 @@ func raiseproc(sig uint32) /* int32 */ {
 
 //go:nosplit
 func read(fd int32, buf unsafe.Pointer, nbyte int32) int32 {
-	return int32(sysvicall3(&libc_read, uintptr(fd), uintptr(buf), uintptr(nbyte)))
+	r1, err := sysvicall3Err(&libc_read, uintptr(fd), uintptr(buf), uintptr(nbyte))
+	if c := int32(r1); c >= 0 {
+		return c
+	}
+	return -int32(err)
 }
 
 //go:nosplit
@@ -500,7 +504,11 @@ func usleep(µs uint32) {
 
 //go:nosplit
 func write1(fd uintptr, buf unsafe.Pointer, nbyte int32) int32 {
-	return int32(sysvicall3(&libc_write, uintptr(fd), uintptr(buf), uintptr(nbyte)))
+	r1, err := sysvicall3Err(&libc_write, fd, uintptr(buf), uintptr(nbyte))
+	if c := int32(r1); c >= 0 {
+		return c
+	}
+	return -int32(err)
 }
 
 func osyield1()
