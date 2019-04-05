2ab23acbc6e6dcd351601d8a6119bfe27727757f
Ian Lance Taylor <iant@golang.org>
2019-04-05 11:42:37 -0700

    runtime: change read and write to return negative errno value
    The internal read and write functions used to return -1 on error;
    change them to return a negative errno value instead.
    This will be used by later CLs in this series.
    
    For most targets this is a simplification, although for ones that call
    into libc it is a complication.
    
    Updates #27707
    
    Change-Id: Id02bf9487f03e7e88e4f2b85e899e986738697ad

--- src/runtime/crash_unix_test.go
+++ src/runtime/crash_unix_test.go
@@ -18,6 +18,7 @@ import (
 	"strings"
 	"syscall"
 	"testing"
+	"unsafe"
 )
 
 // sigquit is the signal to send to kill a hanging testdata program.
@@ -30,6 +31,29 @@ func init() {
 		// it's blocked. Use SIGKILL instead. See issue
 		// #19196 for an example of when this happens.
 		sigquit = syscall.SIGKILL
+	}
+}
+
+func TestBadOpen(t *testing.T) {
+	// make sure we get the correct error code if open fails. Same for
+	// read/write/close on the resulting -1 fd. See issue 10052.
+	nonfile := []byte("/notreallyafile")
+	fd := runtime.Open(&nonfile[0], 0, 0)
+	if fd != -1 {
+		t.Errorf("open(\"%s\")=%d, want -1", string(nonfile), fd)
+	}
+	var buf [32]byte
+	r := runtime.Read(-1, unsafe.Pointer(&buf[0]), int32(len(buf)))
+	if r != -int32(syscall.EBADF) {
+		t.Errorf("read()=%d, want %d", r, -int32(syscall.EBADF))
+	}
+	w := runtime.Write(^uintptr(0), unsafe.Pointer(&buf[0]), int32(len(buf)))
+	if w != -int32(syscall.EBADF) {
+		t.Errorf("write()=%d, want %d", w, -int32(syscall.EBADF))
+	}
+	c := runtime.Close(-1)
+	if c != -1 {
+		t.Errorf("close()=%d, want -1", c)
 	}
 }
 
