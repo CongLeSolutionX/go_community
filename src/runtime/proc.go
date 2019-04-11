5c69309efb587a2980ff368651c8046e48a56248
Ian Lance Taylor <iant@golang.org>
2019-04-10 21:04:36 -0700

    runtime: add new modtimer function
    This adds a new field to P, adjustTimers, that tells the P that one of
    its existing timers was modified to be earlier, and that it therefore
    needs to resort them.
    
    Updates #27707
    
    Change-Id: I4c5f5b51ed116f1d898d3f87cdddfa1b552337f8

--- src/runtime/proc.go
+++ src/runtime/proc.go
@@ -4119,6 +4119,7 @@ func (pp *p) destroy() {
 		// The world is stopped so we don't need to hold timersLock.
 		moveTimers(plocal, pp.timers)
 		pp.timers = nil
+		pp.adjustTimers = 0
 	}
 	// If there's a background worker, make it runnable and put
 	// it on the global queue so it can clean itself up.
