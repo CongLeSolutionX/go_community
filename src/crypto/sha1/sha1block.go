e887187c134653df5b7d40d62f731606835d30d1
Udalov Max <re.udalov@gmail.com>
2019-04-13 16:18:15 +0300

    crypto/sha1: use math/bits.RotateLeft32 instead of ad-hoc implementation.
    This makes code more idiomatic and shows small performance gains of generic benchmarks.
    
    Updates: #31456
    
    Benchstat:
    name            old time/op    new time/op    delta
    Hash8Bytes-8       275ns ± 4%     276ns ± 5%    ~     (p=0.672 n=9+10)
    Hash320Bytes-8    1.46µs ± 5%    1.40µs ± 1%  -4.06%  (p=0.000 n=10+10)
    Hash1K-8          3.99µs ± 5%    3.86µs ± 1%  -3.43%  (p=0.016 n=10+8)
    Hash8K-8          28.9µs ± 0%    29.5µs ± 5%  +1.88%  (p=0.000 n=10+9)
    
    name            old speed      new speed      delta
    Hash8Bytes-8    28.8MB/s ± 9%  28.9MB/s ± 5%    ~     (p=0.853 n=10+10)
    Hash320Bytes-8   220MB/s ± 5%   229MB/s ± 1%  +4.13%  (p=0.000 n=10+10)
    Hash1K-8         257MB/s ± 5%   266MB/s ± 1%  +3.44%  (p=0.016 n=10+8)
    Hash8K-8         283MB/s ± 0%   278MB/s ± 4%  -1.81%  (p=0.000 n=10+9)
    
    Change-Id: Iee63aa042614e3bbeda9aaf5236180d4153f03c4

--- src/crypto/sha1/sha1block.go
+++ src/crypto/sha1/sha1block.go
@@ -4,6 +4,10 @@
 
 package sha1
 
+import (
+	"math/bits"
+)
+
 const (
 	_K0 = 0x5A827999
 	_K1 = 0x6ED9EBA1
@@ -33,8 +37,8 @@ func blockGeneric(dig *digest, p []byte) {
 		i := 0
 		for ; i < 16; i++ {
 			f := b&c | (^b)&d
-			a5 := a<<5 | a>>(32-5)
-			b30 := b<<30 | b>>(32-30)
+			a5 := bits.RotateLeft32(a, 5)
+			b30 := bits.RotateLeft32(b, 30)
 			t := a5 + f + e + w[i&0xf] + _K0
 			a, b, c, d, e = t, a, b30, c, d
 		}
@@ -43,8 +47,8 @@ func blockGeneric(dig *digest, p []byte) {
 			w[i&0xf] = tmp<<1 | tmp>>(32-1)
 
 			f := b&c | (^b)&d
-			a5 := a<<5 | a>>(32-5)
-			b30 := b<<30 | b>>(32-30)
+			a5 := bits.RotateLeft32(a, 5)
+			b30 := bits.RotateLeft32(b, 30)
 			t := a5 + f + e + w[i&0xf] + _K0
 			a, b, c, d, e = t, a, b30, c, d
 		}
@@ -52,8 +56,8 @@ func blockGeneric(dig *digest, p []byte) {
 			tmp := w[(i-3)&0xf] ^ w[(i-8)&0xf] ^ w[(i-14)&0xf] ^ w[(i)&0xf]
 			w[i&0xf] = tmp<<1 | tmp>>(32-1)
 			f := b ^ c ^ d
-			a5 := a<<5 | a>>(32-5)
-			b30 := b<<30 | b>>(32-30)
+			a5 := bits.RotateLeft32(a, 5)
+			b30 := bits.RotateLeft32(b, 30)
 			t := a5 + f + e + w[i&0xf] + _K1
 			a, b, c, d, e = t, a, b30, c, d
 		}
@@ -62,8 +66,8 @@ func blockGeneric(dig *digest, p []byte) {
 			w[i&0xf] = tmp<<1 | tmp>>(32-1)
 			f := ((b | c) & d) | (b & c)
 
-			a5 := a<<5 | a>>(32-5)
-			b30 := b<<30 | b>>(32-30)
+			a5 := bits.RotateLeft32(a, 5)
+			b30 := bits.RotateLeft32(b, 30)
 			t := a5 + f + e + w[i&0xf] + _K2
 			a, b, c, d, e = t, a, b30, c, d
 		}
@@ -71,8 +75,8 @@ func blockGeneric(dig *digest, p []byte) {
 			tmp := w[(i-3)&0xf] ^ w[(i-8)&0xf] ^ w[(i-14)&0xf] ^ w[(i)&0xf]
 			w[i&0xf] = tmp<<1 | tmp>>(32-1)
 			f := b ^ c ^ d
-			a5 := a<<5 | a>>(32-5)
-			b30 := b<<30 | b>>(32-30)
+			a5 := bits.RotateLeft32(a, 5)
+			b30 := bits.RotateLeft32(b, 30)
 			t := a5 + f + e + w[i&0xf] + _K3
 			a, b, c, d, e = t, a, b30, c, d
 		}
