70d7dfbc3ec12cd054c2378b3b0e8a0ed995cc67
Matthew Dempsky <mdempsky@google.com>
2019-04-16 15:52:05 -0700

    internal/poll: avoid unnecessary memory allocation in Writev
    Writev was allocating a new []syscall.Iovec every call, rather than
    reusing the cached copy available at *fd.iovec.
    
    Fixes #26663.
    
    Change-Id: I5967b0d82dc671ce0eaf4ec36cc2a0e46eadde02

--- src/internal/poll/writev.go
+++ src/internal/poll/writev.go
@@ -21,10 +21,11 @@ func (fd *FD) Writev(v *[][]byte) (int64, error) {
 		return 0, err
 	}
 
-	var iovecs []syscall.Iovec
-	if fd.iovecs != nil {
-		iovecs = *fd.iovecs
+	if fd.iovecs == nil {
+		fd.iovecs = new([]syscall.Iovec)
 	}
+	iovecs := fd.iovecs
+
 	// TODO: read from sysconf(_SC_IOV_MAX)? The Linux default is
 	// 1024 and this seems conservative enough for now. Darwin's
 	// UIO_MAXIOV also seems to be 1024.
@@ -33,28 +34,27 @@ func (fd *FD) Writev(v *[][]byte) (int64, error) {
 	var n int64
 	var err error
 	for len(*v) > 0 {
-		iovecs = iovecs[:0]
+		*iovecs = (*iovecs)[:0]
 		for _, chunk := range *v {
 			if len(chunk) == 0 {
 				continue
 			}
-			iovecs = append(iovecs, syscall.Iovec{Base: &chunk[0]})
+			*iovecs = append(*iovecs, syscall.Iovec{Base: &chunk[0]})
 			if fd.IsStream && len(chunk) > 1<<30 {
-				iovecs[len(iovecs)-1].SetLen(1 << 30)
+				(*iovecs)[len(*iovecs)-1].SetLen(1 << 30)
 				break // continue chunk on next writev
 			}
-			iovecs[len(iovecs)-1].SetLen(len(chunk))
-			if len(iovecs) == maxVec {
+			(*iovecs)[len(*iovecs)-1].SetLen(len(chunk))
+			if len(*iovecs) == maxVec {
 				break
 			}
 		}
-		if len(iovecs) == 0 {
+		if len(*iovecs) == 0 {
 			break
 		}
-		fd.iovecs = &iovecs // cache
 
 		var wrote uintptr
-		wrote, err = writev(fd.Sysfd, iovecs)
+		wrote, err = writev(fd.Sysfd, *iovecs)
 		if wrote == ^uintptr(0) {
 			wrote = 0
 		}
