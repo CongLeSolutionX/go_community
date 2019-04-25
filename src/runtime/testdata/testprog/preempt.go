893f84ac1320c7c9c99c2fc5aea2af75fc69ac88
Austin Clements <austin@google.com>
2019-04-25 14:10:29 -0400

    runtime: add a test for asynchronous safe points
    This adds a test of preempting a loop containing no synchronous safe
    points for STW and stack scanning.
    
    We couldn't add this test earlier because it requires scheduler, STW,
    and stack scanning preemption to all be working.
    
    For #10958, #24543.
    
    Change-Id: I73292db78ca3d14aab11bdafd26d03986920ef0a

---
+++ src/runtime/testdata/testprog/preempt.go
@@ -0,0 +1,48 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"runtime"
+	"runtime/debug"
+	"sync"
+	"sync/atomic"
+)
+
+func init() {
+	register("AsyncPreempt", AsyncPreempt)
+}
+
+func AsyncPreempt() {
+	// Run with just 1 GOMAXPROCS so the runtime is required to
+	// use scheduler preemption.
+	runtime.GOMAXPROCS(1)
+	// Disable GC so we have complete control of what we're testing.
+	debug.SetGCPercent(-1)
+
+	// Start a goroutine with no sync safe-points.
+	var ready, done uint32
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		for atomic.LoadUint32(&done) == 0 {
+			atomic.StoreUint32(&ready, 1)
+		}
+		wg.Done()
+	}()
+
+	// Wait for the goroutine to stop passing through sync
+	// safe-points.
+	for atomic.LoadUint32(&ready) == 0 {
+		runtime.Gosched()
+	}
+
+	// Run a GC, which will have to stop the goroutine for STW and
+	// for stack scanning. If this doesn't work, the test will
+	// deadlock and timeout.
+	runtime.GC()
+
+	println("OK")
+}
