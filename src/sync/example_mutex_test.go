44cb273525151e342eb4e111d64de01656ff14a0
BigMikes <giulio.micheloni@gmail.com>
2019-04-29 00:37:30 +0200

    sync: add examples sync.Mutex and sync.RWMutex
    Mutex is missing a proper example. The proposed example
    shows how to allocate a new mutex by reference, in order
    to avoid copying it, and how to make sure that Unlock()
    is finally called by using 'defer' statement.
    
    Change-Id: Ie495db0438339970b3c9cd7aa52f10e4b4cfe071

---
+++ src/sync/example_mutex_test.go
@@ -0,0 +1,31 @@
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package sync_test
+
+import (
+	"fmt"
+	"sync"
+)
+
+func ExampleMutex() {
+	sharedData := 0
+	mutex := &sync.Mutex{}
+	done := make(chan bool)
+	for i := 0; i < 10; i++ {
+		go func() {
+			mutex.Lock()
+			defer mutex.Unlock()
+			sharedData++
+			done <- true
+		}()
+	}
+	for i := 0; i < 10; i++ {
+		<-done
+	}
+	fmt.Println("SharedData value:", sharedData)
+
+	// Output:
+	// SharedData value: 10
+}
