4eb491b6685da8a32072fd0c9de7733e10bb6d58
Elias Naur <mail@eliasnaur.com>
2019-04-30 20:08:37 +0200

    misc/ios: add clang wrapper for self-hosted iOS builds
    Similar to the existing clangwrap.sh for building and testing Go
    on iOS from a macOS host, this CL adds a clang wrapper suitable
    for self-hosted iOS builds.
    
    The iOS kernel refuses to run unsigned binaries, which include shell
    scripts. This wrapper is therefore a Go program that runs clang
    with the appropriate flags. The wrapper attempts to detect whether
    clang produced a binary, in which case it is signed using the ldid
    utility.
    
    With this wrapper we avoid embedding knowledge of iOS code signing
    into into cmd/dist and cmd/go.
    
    Updates #31722
    
    Change-Id: I5ce33bede89009b4ae6bb2ef8480d929cedc82f1

---
+++ misc/ios/clangsign.go
@@ -0,0 +1,43 @@
+package main
+
+// A clang wrapper that signs binaries with the ldid utility
+// after building. Signing is required when running on self-hosted
+// iOS (virtual) devices.
+// Use -ldflags="-X main.sdkpath=<path to iPhoneOS.sdk>" when building
+// the wrapper.
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+var sdkpath = ""
+
+func main() {
+	if sdkpath == "" {
+		fmt.Fprintf(os.Stderr, "no SDK is set; use -ldflags=\"-X main.sdkpath=<sdk path>\" when building this wrapper.\n")
+		os.Exit(1)
+	}
+	args := os.Args[1:]
+	cmd := exec.Command("clang", "-isysroot", "/var/root/sdks/iPhoneOS11.2.sdk", "-mios-version-min=6.0")
+	cmd.Args = append(cmd.Args, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		if err, ok := err.(*exec.ExitError); ok {
+			os.Exit(err.ExitCode())
+		}
+		os.Exit(1)
+	}
+	for i, a := range args[:len(args)-1] {
+		if a != "-o" {
+			continue
+		}
+		// ldid -S will will fail if the file is not an executable, so
+		// ignore errors.
+		exec.Command("ldid", "-S", args[i+1]).Run()
+		break
+	}
+	os.Exit(0)
+}
