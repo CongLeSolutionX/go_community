3472b4fce8bf03ff107c24b19293ad45d717dd0a
Bryan C. Mills <bcmills@google.com>
2019-05-31 17:22:13 -0400

    cmd/go/internal/renameio: add a ReadFile function
    ReadFile is a drop-in replacement for ioutil.ReadFile that works
    around Windows filesystem flakiness under load.
    
    A followup CL will replace uses of ioutil.ReadFile in cmd/go with this
    function.
    
    Updates #32188
    
    Change-Id: I232ba893b132bdc84cd7b0edde436165a69e1aa8

--- src/cmd/go/internal/renameio/renameio.go
+++ src/cmd/go/internal/renameio/renameio.go
@@ -12,7 +12,6 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
-	"time"
 )
 
 const patternSuffix = ".tmp"
@@ -62,23 +61,13 @@ func WriteToFile(filename string, data io.Reader, perm os.FileMode) (err error) 
 		return err
 	}
 
-	var start time.Time
-	for {
-		err := os.Rename(f.Name(), filename)
-		if err == nil || !isAccessDeniedError(err) {
-			return err
-		}
+	return rename(f.Name(), filename)
+}
 
-		// Windows seems to occasionally trigger spurious "Access is denied" errors
-		// here (see golang.org/issue/31247). We're not sure why. It's probably
-		// worth a little extra latency to avoid propagating the spurious errors.
-		if start.IsZero() {
-			start = time.Now()
-		} else if time.Since(start) >= 500*time.Millisecond {
-			return err
-		}
-		time.Sleep(5 * time.Millisecond)
-	}
+// ReadFile is like ioutil.ReadFile, but on Windows retries spurious errors that
+// may occur if the file is concurrently replaced.
+func ReadFile(filename string) ([]byte, error) {
+	return readFile(filename)
 }
 
 // tempFile creates a new temporary file with given permission bits.
