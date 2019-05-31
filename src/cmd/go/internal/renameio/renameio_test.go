0b07d10709b4271c9fc206a884266b0b77c9f931
Bryan C. Mills <bcmills@google.com>
2019-05-31 17:22:13 -0400

    cmd/go/internal/renameio: add a concurrent stress test
    This attempts to reproduce the filesystem failure in #32188 more
    reliably in a self-contained unit test.
    
    DO NOT REVIEW
    DO NOT SUBMIT
    
    The failure has not yet been reproduced.
    
    Updates #32188
    
    Change-Id: I91c5f35ca9b65effd885f05bdb6abdb0cfd5779d

--- src/cmd/go/internal/renameio/renameio_test.go
+++ src/cmd/go/internal/renameio/renameio_test.go
@@ -2,43 +2,69 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package renameio writes files atomically by renaming temporary files.
-
-//+build !nacl,!plan9,!windows,!js
-
 package renameio
 
 import (
+	"encoding/binary"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"path/filepath"
-	"syscall"
+	"sync"
 	"testing"
+	"time"
 )
 
-func TestWriteFileModeAppliesUmask(t *testing.T) {
+func TestConcurrentReadsAndWrites(t *testing.T) {
 	dir, err := ioutil.TempDir("", "renameio")
 	if err != nil {
-		t.Fatalf("Failed to create temporary directory: %v", err)
-	}
-
-	const mode = 0644
-	const umask = 0007
-	defer syscall.Umask(syscall.Umask(umask))
-
-	file := filepath.Join(dir, "testWrite")
-	err = WriteFile(file, []byte("go-build"), mode)
-	if err != nil {
-		t.Fatalf("Failed to write file: %v", err)
+		t.Fatal(err)
 	}
 	defer os.RemoveAll(dir)
+	path := filepath.Join(dir, "blob.bin")
 
-	fi, err := os.Stat(file)
-	if err != nil {
-		t.Fatalf("Stat %q (looking for mode %#o): %s", file, mode, err)
+	const chunkWords = 8 << 10
+	buf := make([]byte, 2*chunkWords*8)
+	for i := uint64(0); i < 2*chunkWords; i++ {
+		binary.LittleEndian.PutUint64(buf[i*8:], i)
 	}
 
-	if fi.Mode()&os.ModePerm != 0640 {
-		t.Errorf("Stat %q: mode %#o want %#o", file, fi.Mode()&os.ModePerm, 0640)
+	n := 1000
+	var wg sync.WaitGroup
+	for ; n > 0; n-- {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			time.Sleep(time.Duration(rand.Intn(100)) * time.Microsecond)
+			offset := rand.Intn(chunkWords)
+			chunk := buf[offset*8 : (offset+chunkWords)*8]
+			if err := WriteFile(path, chunk, 0666); err != nil {
+				t.Error(err)
+			}
+
+			time.Sleep(time.Duration(rand.Intn(100)) * time.Microsecond)
+			data, err := ioutil.ReadFile(path)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			if len(data) != 8*chunkWords {
+				t.Errorf("read %d bytes, but each write is a %d-byte file", len(data), 8*chunkWords)
+				return
+			}
+
+			u := binary.LittleEndian.Uint64(data)
+			for i := 1; i < chunkWords; i++ {
+				next := binary.LittleEndian.Uint64(data[i*8:])
+				if next != u+1 {
+					t.Errorf("wrote sequential integers, but read integer out of sequence at offset %d", i)
+					return
+				}
+				u = next
+			}
+		}()
 	}
+
+	wg.Wait()
 }
