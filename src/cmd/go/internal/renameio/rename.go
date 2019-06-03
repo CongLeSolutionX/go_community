ffe52faa74ff2598d5009839100394554c56fcfd
Bryan C. Mills <bcmills@google.com>
2019-06-03 12:54:15 -0400

    cmd/go/internal/renameio: move Windows special-case logic into its own file
    DO NOT REVIEW
    DO NOT SUBMIT
    
    (Change is still being tested.)
    
    Fixes #32188
    
    Change-Id: I232ba893b132bdc84cd7b0edde436165a69e1aa8

---
+++ src/cmd/go/internal/renameio/rename.go
@@ -0,0 +1,13 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//+build !windows
+
+package renameio
+
+import "os"
+
+func rename(oldpath, newpath string) error {
+	return os.Rename(oldpath, newpath)
+}
