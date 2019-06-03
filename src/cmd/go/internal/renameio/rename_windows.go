2736a1ecedb1ffa63a31a1f5536241086b311272
Bryan C. Mills <bcmills@google.com>
2019-06-03 12:54:15 -0400

    cmd/go/internal/renameio: use ReplaceFile instead of MoveFileEx on Windows
    DO NOT REVIEW
    DO NOT SUBMIT
    
    (Change is still being tested.)
    
    Fixes #32188
    
    Change-Id: I232ba893b132bdc84cd7b0edde436165a69e1aa8

---
+++ src/cmd/go/internal/renameio/rename_windows.go
@@ -0,0 +1,25 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package renameio
+
+import (
+	"internal/syscall/windows"
+	"os"
+	_ "unsafe"
+)
+
+//go:linkname os_fixLongPath os.fixLongPath
+func os_fixLongPath(path string) string
+
+// rename is like os.Rename, but uses ReplaceFile instead of MoveFileEx.
+//
+// TODO(bcmills): For Go 1.14, try changing os.Rename itself to do this.
+func rename(oldpath, newpath string) error {
+	e := windows.Replace(os_fixLongPath(oldpath), os_fixLongPath(newpath))
+	if e != nil {
+		return &os.LinkError{Op: "rename", Old: oldpath, New: newpath, Err: e}
+	}
+	return nil
+}
