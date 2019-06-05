1a2c0c361af502b51aefe0ffdb24b758e2c8219e
David Chase <drchase@google.com>
2019-06-05 14:53:28 -0400

    cmd/compile: add -smallframes gc flag for GC latency diagnosis
    Shrinks the size of things that can be stack allocated from
    10M to 128k for declared variables and from 64k to 16k for
    implicit allocations (new(T), &T{}, etc).
    
    Usage: "go build -gcflags -smallstacks hello.go"
    
    A GOEXPERIMENT version of this caused only one problem, when
    a gc-should-detect-oversize-stack test no longer had an
    oversized stack to detect.
    
    Includes test to verify behavior.
    
    Updates #27732.
    
    Change-Id: I1255d484331e77185e07c78389a8b594041204c2

--- src/cmd/compile/internal/gc/main.go
+++ src/cmd/compile/internal/gc/main.go
@@ -261,13 +261,19 @@ func Main(archInit func(*Arch)) {
 	flag.StringVar(&mutexprofile, "mutexprofile", "", "write mutex profile to `file`")
 	flag.StringVar(&benchfile, "bench", "", "append benchmark times to `file`")
 	flag.BoolVar(&newescape, "newescape", true, "enable new escape analysis")
+	flag.BoolVar(&smallFrames, "smallframes", false, "reduce the size limit for stack allocated objects")
 	flag.BoolVar(&Ctxt.UseBASEntries, "dwarfbasentries", Ctxt.UseBASEntries, "use base address selection entries in DWARF")
 	objabi.Flagparse(usage)
 
 	// Record flags that affect the build result. (And don't
 	// record flags that don't, since that would cause spurious
 	// changes in the binary.)
-	recordFlags("B", "N", "l", "msan", "race", "shared", "dynlink", "dwarflocationlists", "newescape", "dwarfbasentries")
+	recordFlags("B", "N", "l", "msan", "race", "shared", "dynlink", "dwarflocationlists", "newescape", "dwarfbasentries", "smallFrames")
+
+	if smallFrames {
+		maxStackVarSize = 128 * 1024
+		maxImplicitStackVarSize = 16 * 1024
+	}
 
 	Ctxt.Flag_shared = flag_dynlink || flag_shared
 	Ctxt.Flag_dynlink = flag_dynlink
