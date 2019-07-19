68d28ef82f12ab9795854e2e7864be0e727eef8e
benito <benitogf@users.noreply.github.com>
2019-07-19 10:00:23 +0000

    sync: add examples for Map
    Fixes #20973
    
    Change-Id: Icd757348ca0e68158cd954b8e3097375a047da65
    GitHub-Last-Rev: 1a1ccd1a17b398befd3bcd354aa5e20231e62499
    GitHub-Pull-Request: golang/go#30152

--- src/sync/example_test.go
+++ src/sync/example_test.go
@@ -6,6 +6,7 @@ package sync_test
 
 import (
 	"fmt"
+	"strconv"
 	"sync"
 )
 
@@ -56,4 +57,51 @@ func ExampleOnce() {
 	}
 	// Output:
 	// Only once
+}
+
+func ExampleMap() {
+	var barn sync.Map
+	var wg sync.WaitGroup
+	// Store
+	barn.Store("cow", "moo")
+	barn.Store("chicken", "cluck")
+	barn.Store("gopher", "go")
+
+	wg.Add(1)
+
+	// search goroutine
+	// try to load the gopher key of the map
+	// exit the coroutine when it's not found
+	go func() {
+		i := 0
+		for {
+			// Load
+			_, found := barn.Load("gopher")
+			if !found {
+				fmt.Println("floop the gopher", strconv.Itoa(i))
+				wg.Done()
+				break
+			}
+			i++
+		}
+	}()
+
+	// Range & Delete
+	barn.Range(func(key interface{}, value interface{}) bool {
+		if key.(string) == "gopher" {
+			fmt.Println("found the gopher")
+			barn.Delete(key.(string)) // cast the key interface
+			return false              // stop iteration
+		}
+		return true // continue to the next iteration in the range
+	})
+
+	// at this point we wait for goroutine to exit
+	// using a regular map this application would have a race condition
+	// notice that the number of iterations can greatly differ depending on the platform
+	wg.Wait()
+
+	// Output:
+	// found the gopher
+	// floop the gopher {number of iterations on the search goroutine}
 }
