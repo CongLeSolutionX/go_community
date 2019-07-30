96979d1f5c45909d560e6bc311a960b030fee366
Jeremy Faller <jeremy@golang.org>
2019-07-30 17:28:29 -0400

    cmd/link: make PCIter available to compiler
    Change-Id: I922a97d0f25d52ea70cd974008a063d4e7af34a7

--- src/cmd/internal/obj/pcln.go
+++ src/cmd/internal/obj/pcln.go
@@ -384,3 +384,69 @@ func linkpcln(ctxt *Link, cursym *LSym) {
 		}
 	}
 }
+
+// PCIter iterates over encoded pcdata tables.
+type PCIter struct {
+	p       []byte
+	Pc      uint32
+	Nextpc  uint32
+	Pcscale uint32
+	Value   int32
+	start   bool
+	Done    bool
+}
+
+// newPCIter creates a PCIter and configures it for ctxt's architecture.
+func NewPCIter(Pcscale uint32) *PCIter {
+	it := new(PCIter)
+	it.Pcscale = Pcscale
+	return it
+}
+
+// Next advances it to the Next pc.
+func (it *PCIter) Next() {
+	it.Pc = it.Nextpc
+	if it.Done {
+		return
+	}
+	if len(it.p) == 0 {
+		it.Done = true
+		return
+	}
+
+	// Value delta
+	val, n := binary.Varint(it.p)
+	if n <= 0 {
+		log.Fatalf("bad Value varint in pciterNext: read %v", n)
+	}
+	it.p = it.p[n:]
+
+	if val == 0 && !it.start {
+		it.Done = true
+		return
+	}
+
+	it.start = false
+	it.Value += int32(val)
+
+	// pc delta
+	pc, n := binary.Uvarint(it.p)
+	if n <= 0 {
+		log.Fatalf("bad pc varint in pciterNext: read %v", n)
+	}
+	it.p = it.p[n:]
+
+	it.Nextpc = it.Pc + uint32(pc)*it.Pcscale
+}
+
+// init prepares it to iterate over p,
+// and advances it to the first pc.
+func (it *PCIter) Init(p []byte) {
+	it.p = p
+	it.Pc = 0
+	it.Nextpc = 0
+	it.Value = -1
+	it.start = true
+	it.Done = false
+	it.Next()
+}
