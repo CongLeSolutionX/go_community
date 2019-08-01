5f8a785122304aaab015aa99a8d2afb9150df005
Burak Serdar <bserdar@computer.org>
2019-08-01 19:50:08 +0000

    encoding/json: memoize keys during decode
    The existing decoder creates a new string for every key.
    With this change, decoder keeps keys in a map[string]string
    and uses a single copy for repeated keys.
    
    Fixes #32779
    
    Change-Id: I8a0557283e67d144a6f2bc5c04ef9a63fc9c7dcf
    GitHub-Last-Rev: ec41836b1f26e904d4eab258344d3ffdf4428c91
    GitHub-Pull-Request: golang/go#33416

--- src/encoding/json/decode.go
+++ src/encoding/json/decode.go
@@ -275,6 +275,10 @@ type decodeState struct {
 	// safeUnquote is the number of current string literal bytes that don't
 	// need to be unquoted. When negative, no bytes need unquoting.
 	safeUnquote int
+
+	// this keeps field keys to prevent allocating multiple copies of
+	// the same strings. For all entries, literals[k]=k
+	literals map[string]string
 }
 
 // readIndex returns the position of the last byte read.
@@ -295,6 +299,11 @@ func (d *decodeState) init(data []byte) *decodeState {
 
 	// Reuse the allocated space for the FieldStack slice.
 	d.errorContext.FieldStack = d.errorContext.FieldStack[:0]
+
+	// Initialize literals if necessary
+	if d.literals == nil {
+		d.literals = make(map[string]string)
+	}
 	return d
 }
 
@@ -1162,6 +1171,13 @@ func (d *decodeState) objectInterface() map[string]interface{} {
 		key, ok := d.unquote(item)
 		if !ok {
 			panic(phasePanicMsg)
+		}
+
+		// Store key in literals map
+		if s, ok := d.literals[key]; !ok {
+			d.literals[key] = key
+		} else {
+			key = s
 		}
 
 		// Read : before value.
