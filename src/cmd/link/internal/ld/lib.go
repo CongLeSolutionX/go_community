3463880557d1630aadd6db14527a04514e4d6bed
Jeremy Faller <jeremy@golang.org>
2019-08-09 11:36:03 -0400

    cmd/link: add notion of multiple compilation units per package
    As we move the debug_line generation into the compiler, we need to
    upgrade the notion of compilationUnit to not just be on a per package
    basis.  That won't be the case as it will be impossible for all
    compilationUnits to have the same set of files names used to build the
    debug_lines table. (For example, assembled files in a package don't know
    about any files but themselves, so the debug_lines table could only
    reference themseves. As such, we need to break the 1:1 relationship
    between compUnit and package.)
    
    Change-Id: I2e517bb6c01de0115bbf777af828a2fe59c09ce8

--- src/cmd/link/internal/ld/lib.go
+++ src/cmd/link/internal/ld/lib.go
@@ -1624,6 +1624,9 @@ func ldobj(ctxt *Link, f *bio.Reader, lib *sym.Library, length int64, pn string,
 	c4 := bgetc(f)
 	f.MustSeek(start, 0)
 
+	unit := &sym.CompilationUnit{Lib: lib}
+	lib.Units = append(lib.Units, unit)
+
 	magic := uint32(c1)<<24 | uint32(c2)<<16 | uint32(c3)<<8 | uint32(c4)
 	if magic == 0x7f454c46 { // \x7F E L F
 		ldelf := func(ctxt *Link, f *bio.Reader, pkg string, length int64, pn string) {
