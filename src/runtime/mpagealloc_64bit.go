31af260b0ec95c0dadf0660376a086d3bd46ef87
Michael Anthony Knyszek <mknyszek@google.com>
2019-08-12 22:30:39 +0000

    runtime: add new page allocator constants and description
    This change is the first of a series of changes which replace the
    current page allocator (which is based on the contents of mgclarge.go
    and some of mheap.go) with one based on free/used bitmaps.
    
    It adds in the key constants for the page allocator as well as a comment
    describing the implementation.
    
    Updates #35112.
    
    Change-Id: I839d3a07f46842ad379701d27aa691885afdba63

---
+++ src/runtime/mpagealloc_64bit.go
@@ -0,0 +1,12 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build amd64 arm64 mips64 mips64le ppc64 ppc64le s390x
+
+package runtime
+
+const (
+	// The number of levels in the radix tree.
+	summaryLevels = 5
+)
