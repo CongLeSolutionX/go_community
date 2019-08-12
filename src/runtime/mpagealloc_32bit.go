2837a2dbdb27c1d921102c3096d01945a66e8c7c
Michael Anthony Knyszek <mknyszek@google.com>
2019-08-12 22:30:39 +0000

    runtime: add new page allocator constants and description
    This change is the first of a series of changes which replace the
    current page allocator (which is based on the contents of mgclarge.go
    and some of mheap.go) with one based on free/used bitmaps.
    
    It adds in the key constants for the page allocator as well as a comment
    describing the implementation.
    
    Updates #35112.
    
    Change-Id: I839d3a07f46842ad379701d27aa691885afdba63

---
+++ src/runtime/mpagealloc_32bit.go
@@ -0,0 +1,17 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build 386 arm mips mipsle wasm
+
+// wasm is a treated as a 32-bit architecture for the purposes of the page
+// allocator, even though it has 64-bit pointers. This is because any wasm
+// pointer always has its top 32 bits as zero, so the effective heap address
+// space is only 2^32 bytes in size (see heapAddrBits).
+
+package runtime
+
+const (
+	// The number of levels in the radix tree.
+	summaryLevels = 4
+)
