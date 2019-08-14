935772b48d3cc6985baff9aefa6c2cda8167e6c6
Michael Anthony Knyszek <mknyszek@google.com>
2019-08-14 16:32:12 +0000

    runtime: add new page allocator core
    This change adds a new bitmap-based allocator to the runtime with tests.
    It does not yet integrate the page allocator into the runtime and thus
    this change is almost purely additive.
    
    Updates #35112.
    
    Change-Id: Ic3d024c28abee8be8797d3918116a80f901cc2bf

--- src/runtime/mpagealloc_32bit.go
+++ src/runtime/mpagealloc_32bit.go
@@ -11,7 +11,90 @@
 
 package runtime
 
+import "unsafe"
+
 const (
 	// The number of levels in the radix tree.
 	summaryLevels = 4
+
+	// Constants for testing.
+	pageAlloc32Bit = 1
+	pageAlloc64Bit = 0
 )
+
+// See comment in mpagealloc_64bit.go.
+var levelBits = [summaryLevels]uint{
+	summaryL0Bits,
+	summaryLevelBits,
+	summaryLevelBits,
+	summaryLevelBits,
+}
+
+// See comment in mpagealloc_64bit.go.
+var levelShift = [summaryLevels]uint{
+	heapAddrBits - summaryL0Bits,
+	heapAddrBits - summaryL0Bits - 1*summaryLevelBits,
+	heapAddrBits - summaryL0Bits - 2*summaryLevelBits,
+	heapAddrBits - summaryL0Bits - 3*summaryLevelBits,
+}
+
+// See comment in mpagealloc_64bit.go.
+var levelLogPages = [summaryLevels]uint{
+	logMallocChunkPages + 3*summaryLevelBits,
+	logMallocChunkPages + 2*summaryLevelBits,
+	logMallocChunkPages + 1*summaryLevelBits,
+	logMallocChunkPages,
+}
+
+func (s *pageAlloc) sysInit(sysStat *uint64) {
+	// Calculate how much memory all our entries will take up.
+	//
+	// This should be around 12 KiB or less.
+	totalSize := uintptr(0)
+	for l := 0; l < summaryLevels; l++ {
+		totalSize += (uintptr(1) << (heapAddrBits - levelShift[l])) * mallocSumBytes
+	}
+	totalSize = alignUp(totalSize, physPageSize)
+
+	// Reserve memory for all levels in one go. There shouldn't be much for 32-bit.
+	reservation := sysReserve(nil, totalSize)
+	if reservation == nil {
+		throw("failed to reserve summary structure space")
+	}
+	// There isn't much. Just map it and mark it as used immediately.
+	sysMap(reservation, totalSize, sysStat)
+	sysUsed(reservation, totalSize)
+
+	// Iterate over the reservation and cut it up into slices.
+	//
+	// Maintain i as the byte offset from reservation where
+	// the new slice should start.
+	i := uintptr(0)
+	for l := 0; l < summaryLevels; l++ {
+		r := unsafe.Pointer(uintptr(reservation) + i)
+		e := 1 << (heapAddrBits - levelShift[l])
+
+		// Put this reservation into a slice.
+		sl := notInHeapSlice{(*notInHeap)(r), 0, e}
+		s.summary[l] = *(*[]mallocSum)(unsafe.Pointer(&sl))
+
+		i += uintptr(e) * mallocSumBytes
+	}
+}
+
+func (s *pageAlloc) sysGrow(base, size uintptr, _ *uint64) {
+	// Round up to chunks, since we can't deal with increments smaller
+	// than chunks.
+	base = alignDown(base, mallocChunkBytes)
+	size = alignUp(size, mallocChunkBytes)
+
+	// Walk up the tree and update the summary slices.
+	for l := len(s.summary) - 1; l >= 0; l-- {
+		// Update the summary slices with a new upper-bound. This ensures
+		// we get tight bounds checks on at least the top bound.
+		_, hi := addrsToSummaryRange(l, base, base+size-1)
+		if hi > len(s.summary[l]) {
+			s.summary[l] = s.summary[l][:hi]
+		}
+	}
+}
