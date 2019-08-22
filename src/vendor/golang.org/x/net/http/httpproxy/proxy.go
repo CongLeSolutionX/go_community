ec89ac6a3c8ff4e10b04a9e4e506ee640b29f323
Mark Fowler <mark@twoshortplanks.com>
2019-08-22 12:07:45 +0100

    httpproxy: document all loopbacks are special cased not just localhost
    Simple documentation change to document that loopback addresses have
    the same behavior as explictly "localhost"
    
    Fixes #33695
    
    Change-Id: I27cf627a01855195262072b68d38b5c1c6bb33dc

--- src/vendor/golang.org/x/net/http/httpproxy/proxy.go
+++ src/vendor/golang.org/x/net/http/httpproxy/proxy.go
@@ -115,7 +115,8 @@ func getEnvAny(names ...string) string {
 // defined by NO_PROXY.
 //
 // As a special case, if req.URL.Host is "localhost" (with or without a
-// port number), then a nil URL and nil error will be returned.
+// port number) or other loopback address, then a nil URL and nil error
+// will be returned.
 func (cfg *Config) ProxyFunc() func(reqURL *url.URL) (*url.URL, error) {
 	// Preprocess the Config settings for more efficient evaluation.
 	cfg1 := &config{
