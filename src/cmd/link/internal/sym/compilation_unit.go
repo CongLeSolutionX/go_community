e382efc6d2e1298b67e87faaf711c92e755d6273
Jeremy Faller <jeremy@golang.org>
2019-08-22 12:18:28 -0400

    cmd/compile: update object file format for DWARF file table
    In CL 188317, we generate the debug_lines in the compiler, and created a
    new symbol to hold the file table. Using a symbol was a mistake and
    added lots of complexity. Rather, much easier was just changing the
    objectfile format. Here I output the file table to a new object file
    format, and read it back in the linker. The next change will move the
    .debug_lines over to the new structure -- and will show any speedups
    this approach has.
    
    Change-Id: Ibee192e80b86ff6af36467a0b1c26ee747dfee37

--- src/cmd/link/internal/sym/compilation_unit.go
+++ src/cmd/link/internal/sym/compilation_unit.go
@@ -10,13 +10,14 @@ import "cmd/internal/dwarf"
 // debug-related data. We create a CompilationUnit per Object file in a
 // package (so, one for all the go code, one for each assembly file, etc.).
 type CompilationUnit struct {
-	Pkg       string        // our package
-	Lib       *Library      // Our library
-	Consts    *Symbol       // Package constants DIEs
-	PCs       []dwarf.Range // PC ranges, relative to textp[0]
-	DWInfo    *dwarf.DWDie  // CU root DIE
-	FuncDIEs  []*Symbol     // Function DIE subtrees
-	AbsFnDIEs []*Symbol     // Abstract function DIE subtrees
-	RangeSyms []*Symbol     // symbols for debug_range
-	Textp     []*Symbol     // text symbols in this CU
+	Pkg            string        // our package
+	Lib            *Library      // Our library
+	Consts         *Symbol       // Package constants DIEs
+	PCs            []dwarf.Range // PC ranges, relative to textp[0]
+	DWInfo         *dwarf.DWDie  // CU root DIE
+	FuncDIEs       []*Symbol     // Function DIE subtrees
+	AbsFnDIEs      []*Symbol     // Abstract function DIE subtrees
+	RangeSyms      []*Symbol     // symbols for debug_range
+	Textp          []*Symbol     // text symbols in this CU
+	DWARFFileTable []string      // The file table used to generate the .debug_lines
 }
