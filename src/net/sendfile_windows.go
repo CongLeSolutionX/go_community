1988f2d0355c4ef2fe0b71592073d4f471e48bbb
Emmanuel T Odeke <emmanuel@orijtech.com>
2019-08-29 23:07:43 -0700

    net: handle >=2GiB files with sendfile on Windows
    CL 187037 applied a fix to handle the case where
    files larger than 2GiB were not being sendfile-d,
    in one shot.
    This CL is an update to invoke sendfile in chunks
    for files larger than 2GiB, with each chunk being
    a maximum of 2GiB.
    
    Fixes #33193.
    
    Change-Id: If57c25bc289aec82b748890ac1ac4f55798d6a5e

--- src/net/sendfile_windows.go
+++ src/net/sendfile_windows.go
@@ -18,10 +18,8 @@ import (
 // non-EOF error.
 //
 // if handled == false, sendFile performed no work.
-//
-// Note that sendfile for windows does not support >2GB file.
 func sendFile(fd *netFD, r io.Reader) (written int64, err error, handled bool) {
-	var n int64 = 0 // by default, copy until EOF
+	var n int64 = 0 // by default, copy until EOF.
 
 	lr, ok := r.(*io.LimitedReader)
 	if ok {
@@ -29,20 +27,45 @@ func sendFile(fd *netFD, r io.Reader) (written int64, err error, handled bool) {
 		if n <= 0 {
 			return 0, nil, true
 		}
-		// TransmitFile can be invoked in one call with at most
-		// 2,147,483,646 bytes: the maximum value for a 32-bit integer minus 1.
-		// See https://docs.microsoft.com/en-us/windows/win32/api/mswsock/nf-mswsock-transmitfile
-		const maxSendBytes = 0x7fffffff - 1
-		if n > maxSendBytes {
-			return 0, nil, false
-		}
 	}
+
 	f, ok := r.(*os.File)
 	if !ok {
 		return 0, nil, false
 	}
 
-	done, err := poll.SendFile(&fd.pfd, syscall.Handle(f.Fd()), n)
+	// As per https://docs.microsoft.com/en-us/windows/win32/api/mswsock/nf-mswsock-transmitfile
+	// TransmitFile can be invoked in one call with at most
+	// 2,147,483,646 bytes: the maximum value for a 32-bit integer minus 1.
+	const maxChunkSizePerCall = int64(0x7fffffff - 1)
+
+	switch {
+	case n <= maxChunkSizePerCall:
+		// The file is within sendfile's limits.
+		return doSendFile(fd, lr, f, n)
+
+	default:
+		// Now invoke doSendFile on the file in chunks upto 2GiB per chunk.
+		for lr.N > 0 { // lr.N is decremented in every successful invocation of doSendFile.
+			chunkSize := maxChunkSize
+			if chunkSize > lr.N {
+				chunkSize = lr.N
+			}
+			var nw int64
+			nw, err, handled = doSendFile(fd, lr, f, chunkSize)
+			if !handled || err != nil {
+				// TODO: (@odeke-em, @alexbrainman) what should we do if !handled
+				// in the middle of sending chunks?
+				return
+			}
+			written += nw
+		}
+		return
+	}
+}
+
+func doSendFile(fd *netFD, lr *io.LimitedReader, f *os.File, remain int64) (written int64, err error, handled bool) {
+	done, err := poll.SendFile(&fd.pfd, syscall.Handle(f.Fd()), remain)
 
 	if err != nil {
 		return 0, wrapSyscallError("transmitfile", err), false
