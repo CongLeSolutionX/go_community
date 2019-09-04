f3519b92b3d6f1965a8b89470982f66112308f49
Michael Anthony Knyszek <mknyszek@google.com>
2019-09-04 16:12:10 +0000

    runtime: remove old page allocator
    This change removes the old page allocator from the runtime.
    
    Updates #35112.
    
    Change-Id: Ib20e1c030f869b6318cd6f4288a9befdbae1b771

--- src/runtime/malloc.go
+++ src/runtime/malloc.go
@@ -315,9 +315,6 @@ const (
 	//
 	// This should agree with minZeroPage in the compiler.
 	minLegalPointer uintptr = 4096
-
-	// Whether to use the old page allocator or not.
-	oldPageAllocator = false
 )
 
 // physPageSize is the size in bytes of the OS's physical pages.
@@ -731,11 +728,10 @@ mapped:
 				throw("out of memory allocating heap arena metadata")
 			}
 		}
-		if !oldPageAllocator {
-			// Mark every page as scavenged to begin with.
-			for c := 0; c < mallocChunksPerArena; c++ {
-				r.pageAlloc[c].scavenged.setAll()
-			}
+
+		// Mark every page as scavenged to begin with.
+		for c := 0; c < mallocChunksPerArena; c++ {
+			r.pageAlloc[c].scavenged.setAll()
 		}
 
 		// Add the arena to the arenas list.
