2f1b4f8225cc3de0732933afe4f8883f20e46ad7
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2019-09-05 11:15:59 +0700

    cmd/compile: handle loopdepth in looping label
    loopdepth is incremented when entering a looping label but is never
    decremented before leaving. That causes variables outside of looping
    label to be considered as at the same loopdepth with it.
    
    Fixing it by decrementing loopdepth when encountering the corresponding
    OGOTO node of looping label.
    
    Fixes #22438
    
    Change-Id: I39b306544a6c0ee3fcbebbe0d0ee735cb71773e6

---
+++ test/escape6.go
@@ -0,0 +1,42 @@
+// errorcheck -0 -m
+
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Test escape analysis with label.
+
+package p
+
+func g() bool
+
+//go:noescape
+func h(*int)
+
+func f1() {
+	var p *int
+	if g() {
+	x:
+		goto x
+	} else {
+		p = new(int) // ERROR "f1 new\(int\) does not escape$"
+	}
+	h(p)
+}
+
+func f2() {
+	var p, q *int
+	{
+	x:
+		if g() {
+			goto x
+		}
+	}
+	p = new(int) // ERROR "f2 new\(int\) does not escape"
+	h(p)
+	goto y
+
+y:
+	q = new(int) // ERROR "f2 new\(int\) does not escape"
+	h(q)
+}
