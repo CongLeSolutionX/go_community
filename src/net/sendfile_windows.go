4ec6ae91e9a3d052bb15e8d082a2a8d212013fe4
Emmanuel T Odeke <emmanuel@orijtech.com>
2019-09-08 12:03:42 -0700

    net: permit sendFile on more than *io.LimitedReader on Windows
    CL 192518 was a minimal simplification to get sendfile
    on Windows to work with chunked files, but as I had mentioned,
    it could use some simplification.
    
    This CL improves it by:
    * Removing the decrement of lr.N (the *io.LimitedReader), on
    each invocation of doSendFile
    * If the reader is not an *io.LimitedReader, since the underlying
    reader is anyways an *os.File, we fallback and stat that
    file to determine the file size and then also invoke the chunked
    sendFile on the underlying reader. This issue existed even
    before the prior CL.
    * Adding stateful helpers to track the remaining bytes.
    
    Thus if the callers of sendFile don't send an *io.LimitedReader,
    but have a huge file, we can still invoke the chunked sendFile
    on it.
    
    Change-Id: I97a67c712d558c84ced716d8df98b040cd7ed7f7

--- src/net/sendfile_windows.go
+++ src/net/sendfile_windows.go
@@ -21,18 +21,44 @@ import (
 func sendFile(fd *netFD, r io.Reader) (written int64, err error, handled bool) {
 	var n int64 = 0 // by default, copy until EOF.
 
+	// afterEachSend and remaining maintain the state of the current
+	// number of written and remaining bytes, per SendFile invocation.
+	var afterEachSend func(written int64)
+	var remaining func() int64
+
 	lr, ok := r.(*io.LimitedReader)
 	if ok {
 		n, r = lr.N, lr.R
 		if n <= 0 {
 			return 0, nil, true
 		}
+		afterEachSend = func(written int64) {
+			lr.N -= written
+		}
+		remaining = func() int64 { return lr.N }
 	}
 
 	f, ok := r.(*os.File)
 	if !ok {
 		return 0, nil, false
 	}
+
+	if n == 0 || afterEachSend == nil || remaining == nil { // We haven't yet infered the size.
+		// Try to infer the file size from stat-ing instead.
+		fi, err := f.Stat()
+		if err != nil {
+			return 0, err, false
+		}
+		n = fi.Size()
+		afterEachSend = func(written int64) {
+			if written > 0 {
+				n -= written
+			}
+		}
+		remaining = func() int64 { return n }
+	}
+
+	fh := syscall.Handle(f.Fd())
 
 	// TransmitFile can be invoked in one call with at most
 	// 2,147,483,646 bytes: the maximum value for a 32-bit integer minus 1.
@@ -42,21 +68,27 @@ func sendFile(fd *netFD, r io.Reader) (written int64, err error, handled bool) {
 	switch {
 	case n <= maxChunkSizePerCall:
 		// The file is within sendfile's limits.
-		written, err = doSendFile(fd, lr, f, n)
+		written, err = doSendFile(fd, fh, n)
+		afterEachSend(written)
 
 	default:
 		// Now invoke doSendFile on the file in chunks of upto 2GiB per chunk.
-		for lr.N > 0 { // lr.N is decremented in every successful invocation of doSendFile.
+		for {
+			nLeft := remaining()
+			if nLeft <= 0 {
+				break
+			}
 			chunkSize := maxChunkSizePerCall
-			if chunkSize > lr.N {
-				chunkSize = lr.N
+			if chunkSize > nLeft {
+				chunkSize = nLeft
 			}
 			var nw int64
-			nw, err = doSendFile(fd, lr, f, chunkSize)
+			nw, err = doSendFile(fd, fh, chunkSize)
 			if err != nil {
 				break
 			}
 			written += nw
+			afterEachSend(nw)
 		}
 	}
 
@@ -66,14 +98,10 @@ func sendFile(fd *netFD, r io.Reader) (written int64, err error, handled bool) {
 }
 
 // doSendFile is a helper to invoke poll.SendFile.
-// It will decrement lr.N by the number of written bytes.
-func doSendFile(fd *netFD, lr *io.LimitedReader, f *os.File, remain int64) (written int64, err error) {
-	done, err := poll.SendFile(&fd.pfd, syscall.Handle(f.Fd()), remain)
+func doSendFile(fd *netFD, fh syscall.Handle, remain int64) (written int64, err error) {
+	done, err := poll.SendFile(&fd.pfd, fh, remain)
 	if err != nil {
 		return 0, wrapSyscallError("transmitfile", err)
-	}
-	if lr != nil {
-		lr.N -= int64(done)
 	}
 	return int64(done), nil
 }
