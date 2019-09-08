c568f8016060df082a8f3bae26539505b609ba8a
Emmanuel T Odeke <emmanuel@orijtech.com>
2019-09-08 12:03:42 -0700

    internal/poll, net: permit sendFile on more than *io.LimitedReader on Windows
    CL 192518 was a minimal simplification to get sendfile
    on Windows to work with chunked files, but as I had mentioned,
    I would add even more improvements.
    
    This CL improves it by:
    * If the reader is not an *io.LimitedReader, since the underlying
    reader is anyways an *os.File, we fallback and stat that
    file to determine the file size and then also invoke the chunked
    sendFile on the underlying reader. This issue existed even
    before the prior CL.
    * Extracting the chunked TransmitFile logic and moving it directly
    into internal/poll.SendFile.
    
    Thus if the callers of net.sendFile don't send an *io.LimitedReader,
    but have a huge file, we can still invoke the chunked
    internal/poll.SendFile on it and now even direct invokers can send
    files >2GiB.
    
    Updates #33193.
    
    Change-Id: I97a67c712d558c84ced716d8df98b040cd7ed7f7

--- src/internal/poll/sendfile_windows.go
+++ src/internal/poll/sendfile_windows.go
@@ -7,7 +7,7 @@ package poll
 import "syscall"
 
 // SendFile wraps the TransmitFile call.
-func SendFile(fd *FD, src syscall.Handle, n int64) (int64, error) {
+func SendFile(fd *FD, src syscall.Handle, size int64) (written int64, err error) {
 	if fd.kind == kindPipe {
 		// TransmitFile does not work with pipes
 		return 0, syscall.ESPIPE
@@ -18,27 +18,50 @@ func SendFile(fd *FD, src syscall.Handle, n int64) (int64, error) {
 	}
 	defer fd.writeUnlock()
 
-	o := &fd.wop
-	o.qty = uint32(n)
-	o.handle = src
+	// TransmitFile can be invoked in one call with at most
+	// 2,147,483,646 bytes: the maximum value for a 32-bit integer minus 1.
+	// See https://docs.microsoft.com/en-us/windows/win32/api/mswsock/nf-mswsock-transmitfile
+	const maxChunkSizePerCall = int64(0x7fffffff - 1)
 
-	// TODO(brainman): skip calling syscall.Seek if OS allows it
-	curpos, err := syscall.Seek(o.handle, 0, 1)
-	if err != nil {
-		return 0, err
-	}
+	n := size
+	for n > 0 {
+		chunkSize := maxChunkSizePerCall
+		if chunkSize > n {
+			chunkSize = n
+		}
 
-	o.o.Offset = uint32(curpos)
-	o.o.OffsetHigh = uint32(curpos >> 32)
+		o := &fd.wop
+		o.qty = uint32(chunkSize)
+		o.handle = src
 
-	done, err := wsrv.ExecIO(o, func(o *operation) error {
-		return syscall.TransmitFile(o.fd.Sysfd, o.handle, o.qty, 0, &o.o, nil, syscall.TF_WRITE_BEHIND)
-	})
-	if err == nil {
+		// TODO(brainman): skip calling syscall.Seek if OS allows it
+		curpos, err := syscall.Seek(o.handle, 0, 1)
+		if err != nil {
+			return 0, err
+		}
+
+		o.o.Offset = uint32(curpos)
+		o.o.OffsetHigh = uint32(curpos >> 32)
+
+		nw, err := wsrv.ExecIO(o, func(o *operation) error {
+			return syscall.TransmitFile(o.fd.Sysfd, o.handle, o.qty, 0, &o.o, nil, syscall.TF_WRITE_BEHIND)
+		})
+		if err != nil {
+			return written, err
+		}
+
+		nw64 := int64(nw)
+
 		// Some versions of Windows (Windows 10 1803) do not set
 		// file position after TransmitFile completes.
 		// So just use Seek to set file position.
-		_, err = syscall.Seek(o.handle, curpos+int64(done), 0)
+		if _, err = syscall.Seek(o.handle, curpos+nw64, 0); err != nil {
+			return written, err
+		}
+
+		n -= nw64
+		written += nw64
 	}
-	return int64(done), err
+
+	return
 }
