ace4c790a8c1319a08e858b5a79dfae6ddeb7f64
Michael Anthony Knyszek <mknyszek@google.com>
2019-09-10 18:53:51 +0000

    runtime: count scavenged bits for new allocation for new page allocator
    This change makes it so that the new page allocator returns the number
    of pages that are scavenged in a new allocation so that mheap can update
    memstats appropriately.
    
    The accounting could be embedded into mallocbits, but that would make
    the new allocator more difficult to test.
    
    Change-Id: I0f94f563d7af2458e6d534f589d2e7dd6af26d12

--- src/runtime/export_test.go
+++ src/runtime/export_test.go
@@ -744,6 +744,7 @@ func (b *MallocBits) AllocRange(i, n int)       { (*mallocBits)(b).allocRange(i,
 func (b *MallocBits) Free1(i int)               { (*mallocBits)(b).free1(i) }
 func (b *MallocBits) Free(i, n int)             { (*mallocBits)(b).free(i, n) }
 func (b *MallocBits) Summarize(n int) MallocSum { return MallocSum((*mallocBits)(b).summarize(n)) }
+func (b *MallocBits) PopcntRange(i, n int) int  { return (*pageBits)(b).popcntRange(i, n) }
 
 // Expose non-trivial alloc helpers for testing.
 func SetConsecBits64(x uint64, i, n int) uint64   { return setConsecBits64(x, i, n) }
@@ -770,8 +771,12 @@ func (d *MallocData) ScavengeRange(i, n int) {
 //go:notinheap
 type PageAlloc pageAlloc
 
-func (p *PageAlloc) Alloc(npages uintptr) uintptr { return (*pageAlloc)(p).alloc(npages) }
-func (p *PageAlloc) Free(base, npages uintptr)    { (*pageAlloc)(p).free(base, npages) }
+func (p *PageAlloc) Alloc(npages uintptr) (uintptr, uintptr) {
+	return (*pageAlloc)(p).alloc(npages)
+}
+func (p *PageAlloc) Free(base, npages uintptr) {
+	(*pageAlloc)(p).free(base, npages)
+}
 func (p *PageAlloc) Bounds() (uint, uint) {
 	return uint((*pageAlloc)(p).start), uint((*pageAlloc)(p).end)
 }
