270f292bc0974c31d33ed361cde0f42937803da0
Michael Anthony Knyszek <mknyszek@google.com>
2019-09-12 18:24:56 +0000

    runtime: add option to scavenge with lock held throughout
    This change adds a "locked" parameter to scavenge() and scavengeone()
    which allows these methods to be run with the heap lock acquired, and
    synchronously with respect to others which acquire the heap lock.
    
    This mode is necessary for both heap-growth scavenging (multiple
    asynchornous scavengers here could be problematic) and
    debug.FreeOSMemory.
    
    Updates #35112.
    
    Change-Id: I24eea8e40f971760999c980981893676b4c9b666

--- src/runtime/export_test.go
+++ src/runtime/export_test.go
@@ -808,9 +808,9 @@ func (p *PageAlloc) HasChunk(i ChunkIdx) bool {
 func (p *PageAlloc) MallocBits(i ChunkIdx) *MallocBits {
 	return (*MallocBits)(&((*pageAlloc)(p).chunks[i].mallocBits))
 }
-func (p *PageAlloc) Scavenge(nbytes uintptr) (r uintptr) {
+func (p *PageAlloc) Scavenge(nbytes uintptr, locked bool) (r uintptr) {
 	systemstack(func() {
-		r = (*pageAlloc)(p).scavenge(nbytes)
+		r = (*pageAlloc)(p).scavenge(nbytes, locked)
 	})
 	return
 }
