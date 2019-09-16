1c8e54559d7982e7e7d4315ffa855cde892a7bc7
Michael Anthony Knyszek <mknyszek@google.com>
2019-09-16 21:23:24 +0000

    runtime: add per-p page allocation cache
    This change adds a per-p free page cache which the page allocator may
    allocate out of without a lock. The change also introduces a completely
    lockless page allocator fast path.
    
    Although the cache contains at most 64 pages (and usually less), the
    vast majority (85%+) of page allocations are exactly 1 page in size.
    
    Change-Id: I170bf0a9375873e7e3230845eb1df7e5cf741b78

--- src/runtime/runtime2.go
+++ src/runtime/runtime2.go
@@ -529,6 +529,7 @@ type p struct {
 	sysmontick  sysmontick // last tick observed by sysmon
 	m           muintptr   // back-link to associated m (nil if idle)
 	mcache      *mcache
+	pcache      pageCache
 	raceprocctx uintptr
 
 	deferpool    [5][]*_defer // pool of available defer structs of different sizes (see panic.go)
