dd1c9a7d3582525e9465b18c7a1bf33351a8b93d
Cherry Zhang <cherryyz@google.com>
2019-09-23 22:44:02 -0400

    [dev.link] cmd/link: move some work after deadcode
    This is in preparation of delaying loading symbol content to
    after dead code elimination.
    
    Change-Id: Ia4ce82d9a42cc1a76c82f6d0eb9a54b0325ee3ba

--- src/cmd/link/internal/ld/main.go
+++ src/cmd/link/internal/ld/main.go
@@ -208,9 +208,11 @@ func Main(arch *sys.Arch, theArch Arch) {
 	}
 	ctxt.loadlib()
 
-	ctxt.dostrdata()
 	deadcode(ctxt)
+	ctxt.linksetup()
+	ctxt.dostrdata()
 	//ctxt.dumpsyms() // XXX
+
 	dwarfGenerateDebugInfo(ctxt)
 	if objabi.Fieldtrack_enabled != 0 {
 		fieldtrack(ctxt)
