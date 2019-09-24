fd3cfbba6ac51a29afae0b11cddc449587fccb23
Cherry Zhang <cherryyz@google.com>
2019-09-24 17:31:12 -0400

    [dev.link] cmd/link: load full symbol contents after deadcode pass
    If the new object file format is used, now we laod full symbol
    contents after the deadcode pass, for reachable symbols only.
    We still load some informations early, like relocations and the
    contents of type symbols, which are used in the deadcode pass.
    If we rewrite deadcode to use index directly, we could delay more
    of the loading (to sym.Symbol), and perhaps delay the creation of
    sym.Symbol.
    
    TODO: internal linking with host objects doesn't work yet.
    
    Change-Id: I7d4880e8f150e8709ffac277e62191623440e4cf

--- src/cmd/link/internal/objfile/objfile2.go
+++ src/cmd/link/internal/objfile/objfile2.go
@@ -223,7 +223,142 @@ func preprocess(arch *sys.Arch, s *sym.Symbol) {
 	}
 }
 
-func LoadFull(l *Loader, r *obj.Reader, lib *sym.Library, syms *sym.Symbols, localSymVersion int, libByPkg map[string]*sym.Library) {
+// Load relocations for building the dependency graph in deadcode pass.
+// For now, we load symbol types, relocations, gotype, and the contents
+// of type symbols, which are needed in deadcode.
+func LoadReloc(l *Loader, r *obj.Reader, lib *sym.Library, localSymVersion int, libByPkg map[string]*sym.Library) {
+	// PkgIdx
+	pkglist := r.Pkglist()
+
+	pkgprefix := objabi.PathToPrefix(lib.Pkg) + "."
+	istart := l.StartIndex(r)
+
+	resolveSymRef := func(s obj.OSymRef) *sym.Symbol {
+		var rr *obj.Reader
+		switch p := s.PkgIdx; p {
+		case obj.PkgIdxInvalid:
+			if s.SymIdx != 0 {
+				panic("bad sym ref")
+			}
+			return nil
+		case obj.PkgIdxNone:
+			// Resolve by name
+			i := int(s.SymIdx-1) + r.NSym()
+			osym := obj.OSym{}
+			osym.Read(r, r.SymOff(i))
+			name := strings.Replace(osym.Name, "\"\".", pkgprefix, -1)
+			v := abiToVer(osym.ABI, localSymVersion)
+			nv := nameVer{name, v}
+			i = l.symsByName[nv]
+			return l.Syms[i]
+		case obj.PkgIdxSelf:
+			rr = r
+		default:
+			if p < obj.PkgIdxRefBase {
+				panic("bad sym ref")
+			}
+			pkg := pkglist[p-obj.PkgIdxRefBase]
+			rr = libByPkg[pkg].Readers[0].Reader // typically Readers[0] is go object (others are asm)
+		}
+		i := l.ToGlobal(rr, int(s.SymIdx-1))
+		return l.Syms[i]
+	}
+
+	for i, n := 0, r.NSym()+r.NNonpkgdef(); i < n; i++ {
+		s := l.Syms[istart+i]
+		if s == nil || s.Name == "" {
+			continue
+		}
+
+		osym := obj.OSym{}
+		osym.Read(r, r.SymOff(i))
+		name := strings.Replace(osym.Name, "\"\".", pkgprefix, -1)
+		if s.Name != name { // Sanity check. We can remove it in the final version.
+			fmt.Println("name mismatch:", lib, i, s.Name, name)
+			panic("name mismatch")
+		}
+
+		if s.Type != 0 && s.Type != sym.SXREF {
+			fmt.Println("symbol already processed:", lib, i, s)
+			panic("symbol already processed")
+		}
+
+		t := sym.AbiSymKindToSymKind[objabi.SymKind(osym.Type)]
+		if t == sym.SXREF {
+			log.Fatalf("bad sxref")
+		}
+		if t == 0 {
+			log.Fatalf("missing type for %s in %s", s.Name, lib)
+		}
+		if t == sym.SBSS && (s.Type == sym.SRODATA || s.Type == sym.SNOPTRBSS) {
+			t = s.Type
+		}
+		s.Type = t
+
+		// Reloc
+		nreloc := r.NReloc(i)
+		s.R = make([]sym.Reloc, nreloc)
+		for j := range s.R {
+			rel := obj.OReloc{}
+			rel.Read(r, r.RelocOff(i, j))
+			s.R[j] = sym.Reloc{
+				Off:  rel.Off,
+				Siz:  rel.Siz,
+				Type: objabi.RelocType(rel.Type),
+				Add:  rel.Add,
+				Sym:  resolveSymRef(rel.Sym),
+			}
+		}
+
+		// XXX deadcode needs symbol data for type symbols. Read it now.
+		if strings.HasPrefix(name, "type.") {
+			s.P = r.BytesAt(r.DataOff(i), r.DataSize(i))
+			s.Size = int64(osym.Siz)
+		}
+
+		// Aux symbol
+		naux := r.NAux(i)
+		for j := 0; j < naux; j++ {
+			a := obj.OAux{}
+			a.Read(r, r.AuxOff(i, j))
+			switch a.Type {
+			case obj.AuxGotype:
+				typ := resolveSymRef(a.Sym)
+				if typ != nil {
+					s.Gotype = typ
+				}
+			case obj.AuxFuncdata:
+				pc := s.FuncInfo
+				if pc == nil {
+					pc = &sym.FuncInfo{Funcdata: make([]*sym.Symbol, 0, 4)}
+					s.FuncInfo = pc
+				}
+				pc.Funcdata = append(pc.Funcdata, resolveSymRef(a.Sym))
+			}
+		}
+
+		if s.Type == sym.STEXT {
+			dupok := osym.Flag&obj.SymFlagDupok != 0
+			if !dupok {
+				if s.Attr.OnList() {
+					log.Fatalf("symbol %s listed multiple times", s.Name)
+				}
+				s.Attr |= sym.AttrOnList
+				lib.Textp = append(lib.Textp, s)
+			} else {
+				// there may ba a dup in another package
+				// put into a temp list and add to text later
+				lib.DupTextSyms = append(lib.DupTextSyms, s)
+			}
+		}
+	}
+}
+
+// Load full contents.
+// TODO: For now, some contents are already load in LoadReloc. Maybe
+// we should combine LoadReloc back into this, once we rewrite deadcode
+// pass to use index directly.
+func LoadFull(l *Loader, r *obj.Reader, lib *sym.Library, localSymVersion int, libByPkg map[string]*sym.Library) {
 	// PkgIdx
 	pkglist := r.Pkglist()
 
@@ -267,11 +402,16 @@ func LoadFull(l *Loader, r *obj.Reader, lib *sym.Library, syms *sym.Symbols, loc
 		if s == nil || s.Name == "" {
 			continue
 		}
+		if !s.Attr.Reachable() && (s.Type < sym.SDWARFSECT || s.Type > sym.SDWARFLINES) {
+			// No need to load unreachable symbols.
+			// XXX DWARF symbols may be used but are not marked reachable.
+			continue
+		}
 
 		osym := obj.OSym{}
 		osym.Read(r, r.SymOff(i))
 		name := strings.Replace(osym.Name, "\"\".", pkgprefix, -1)
-		if s.Name != name {
+		if s.Name != name { // Sanity check. We can remove it in the final version.
 			fmt.Println("name mismatch:", lib, i, s.Name, name)
 			panic("name mismatch")
 		}
@@ -279,53 +419,26 @@ func LoadFull(l *Loader, r *obj.Reader, lib *sym.Library, syms *sym.Symbols, loc
 		dupok := osym.Flag&obj.SymFlagDupok != 0
 		local := osym.Flag&obj.SymFlagLocal != 0
 		makeTypelink := osym.Flag&obj.SymFlagTypelink != 0
-		nreloc := r.NReloc(i)
 		datasize := r.DataSize(i)
 		size := osym.Siz
-
-		t := sym.AbiSymKindToSymKind[objabi.SymKind(osym.Type)]
-		if s.Type != 0 && s.Type != sym.SXREF {
-			fmt.Println("symbol already processed:", lib, i, s)
-			panic("symbol already processed")
-		}
 
 		// Symbol data
 		s.P = r.BytesAt(r.DataOff(i), datasize)
 
-		// Reloc
-		s.R = make([]sym.Reloc, nreloc)
-		for j := range s.R {
-			rel := obj.OReloc{}
-			rel.Read(r, r.RelocOff(i, j))
-			s.R[j] = sym.Reloc{
-				Off:  rel.Off,
-				Siz:  rel.Siz,
-				Type: objabi.RelocType(rel.Type),
-				Add:  rel.Add,
-				Sym:  resolveSymRef(rel.Sym),
-			}
-		}
-
 		// Aux symbol info
 		var isym uint32
-		funcdata := make([]obj.OSymRef, 0, 4)
 		naux := r.NAux(i)
 		for j := 0; j < naux; j++ {
 			a := obj.OAux{}
 			a.Read(r, r.AuxOff(i, j))
 			switch a.Type {
-			case obj.AuxGotype:
-				typ := resolveSymRef(a.Sym)
-				if typ != nil {
-					s.Gotype = typ
-				}
+			case obj.AuxGotype, obj.AuxFuncdata:
+				// already loaded
 			case obj.AuxFuncInfo:
 				if a.Sym.PkgIdx != obj.PkgIdxSelf {
 					panic("funcinfo symbol not defined in current package")
 				}
 				isym = a.Sym.SymIdx
-			case obj.AuxFuncdata:
-				funcdata = append(funcdata, a.Sym)
 			default:
 				panic("unknown aux type")
 			}
@@ -335,16 +448,6 @@ func LoadFull(l *Loader, r *obj.Reader, lib *sym.Library, syms *sym.Symbols, loc
 		if dupok {
 			s.Attr |= sym.AttrDuplicateOK
 		}
-		if t == sym.SXREF {
-			log.Fatalf("bad sxref")
-		}
-		if t == 0 {
-			log.Fatalf("missing type for %s in %s", s.Name, lib)
-		}
-		if t == sym.SBSS && (s.Type == sym.SRODATA || s.Type == sym.SNOPTRBSS) {
-			t = s.Type
-		}
-		s.Type = t
 		if s.Size < int64(size) {
 			s.Size = int64(size)
 		}
@@ -353,17 +456,6 @@ func LoadFull(l *Loader, r *obj.Reader, lib *sym.Library, syms *sym.Symbols, loc
 
 		if s.Type != sym.STEXT {
 			continue
-		}
-		if !dupok {
-			if s.Attr.OnList() {
-				log.Fatalf("symbol %s listed multiple times", s.Name)
-			}
-			s.Attr |= sym.AttrOnList
-			lib.Textp = append(lib.Textp, s)
-		} else {
-			// there may ba a dup in another package
-			// put into a temp list and add to text later
-			lib.DupTextSyms = append(lib.DupTextSyms, s)
 		}
 
 		// FuncInfo
@@ -388,15 +480,16 @@ func LoadFull(l *Loader, r *obj.Reader, lib *sym.Library, syms *sym.Symbols, loc
 		}
 
 		info.Pcdata = append(info.Pcdata, info.PcdataEnd) // for the ease of knowing where it ends
-		pc := &sym.FuncInfo{
-			Args:        int32(info.Args),
-			Locals:      int32(info.Locals),
-			Pcdata:      make([]sym.Pcdata, len(info.Pcdata)-1), // -1 as we appended one above
-			Funcdata:    make([]*sym.Symbol, len(info.Funcdataoff)),
-			Funcdataoff: make([]int64, len(info.Funcdataoff)),
-			File:        make([]*sym.Symbol, len(info.File)),
+		pc := s.FuncInfo
+		if pc == nil {
+			pc = &sym.FuncInfo{}
+			s.FuncInfo = pc
 		}
-		s.FuncInfo = pc
+		pc.Args = int32(info.Args)
+		pc.Locals = int32(info.Locals)
+		pc.Pcdata = make([]sym.Pcdata, len(info.Pcdata)-1) // -1 as we appended one above
+		pc.Funcdataoff = make([]int64, len(info.Funcdataoff))
+		pc.File = make([]*sym.Symbol, len(info.File))
 		pc.Pcsp.P = r.BytesAt(pcdataBase+info.Pcsp, int(info.Pcfile-info.Pcsp))
 		pc.Pcfile.P = r.BytesAt(pcdataBase+info.Pcfile, int(info.Pcline-info.Pcfile))
 		pc.Pcline.P = r.BytesAt(pcdataBase+info.Pcline, int(info.Pcinline-info.Pcline))
@@ -404,8 +497,7 @@ func LoadFull(l *Loader, r *obj.Reader, lib *sym.Library, syms *sym.Symbols, loc
 		for k := range pc.Pcdata {
 			pc.Pcdata[k].P = r.BytesAt(pcdataBase+info.Pcdata[k], int(info.Pcdata[k+1]-info.Pcdata[k]))
 		}
-		for k := range pc.Funcdata {
-			pc.Funcdata[k] = resolveSymRef(funcdata[k])
+		for k := range pc.Funcdataoff {
 			pc.Funcdataoff[k] = int64(info.Funcdataoff[k])
 		}
 		for k := range pc.File {
