f2ddd68b06368917c6ce6f90d355ff709ca16457
Than McIntosh <thanm@google.com>
2019-09-26 09:53:37 -0400

    cmd/compile: don't emit autom's into object file
    Don't write Autom records when writing a function to the object file;
    we no longer need them in the linker for DWARF processing. So as to
    keep the object file format unchanged, write out a zero-length list of
    automs to the object, as opposed to removing all references.
    
    Updates #34554.
    
    Change-Id: I42a1d67207ea7114ae4f3a315cf37effba57f190

--- src/cmd/internal/obj/link.go
+++ src/cmd/internal/obj/link.go
@@ -211,9 +211,6 @@ const (
 	// A reference to name@GOT(SB) is a reference to the entry in the global offset
 	// table for 'name'.
 	NAME_GOTREF
-	// Indicates auto that was optimized away, but whose type
-	// we want to preserve in the DWARF debug info.
-	NAME_DELETED_AUTO
 	// Indicates that this is a reference to a TOC anchor.
 	NAME_TOCREF
 )
