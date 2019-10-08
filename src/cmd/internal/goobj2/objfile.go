1ec3c1c637389b605fd57d0aab9bdbea198424f4
Cherry Zhang <cherryyz@google.com>
2019-10-07 21:10:41 -0400

    [dev.link] cmd/internal/obj, cmd/link: use aux symbol for DWARF symbols
    Use the auxiliary symbol mechanism to connect the text symbol and
    its associated DWARF symbols. This way, the linker can track the
    DWARF symbols from the text symbol, without looking up special
    names.
    
    Currently, in the linker this is only used in the deadcode pass
    to track which DWARF symbols are used and need to load. Later
    passes still use name lookup for now.
    
    Change-Id: I2fe49f3b1f0ecc1472ae8aa93907cff740022d8d

--- src/cmd/internal/goobj2/objfile.go
+++ src/cmd/internal/goobj2/objfile.go
@@ -278,8 +278,12 @@ const (
 	AuxGotype = iota
 	AuxFuncInfo
 	AuxFuncdata
+	AuxDwarfInfo
+	AuxDwarfLoc
+	AuxDwarfRanges
+	AuxDwarfLines
 
-	// TODO: more. DWARF? Pcdata?
+	// TODO: more. Pcdata?
 )
 
 func (a *Aux) Write(w *Writer) {
