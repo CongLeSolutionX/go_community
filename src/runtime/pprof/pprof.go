27f36fe1faaf75d121e2fc67798909aca0cf55cf
Ian Lance Taylor <iant@golang.org>
2019-10-18 23:16:16 -0700

    runtime/pprof: update example comment to check error on Close
    Programs should always check the error return of Closee for a file
    opened for writing. Update the example code in the comment to do this.
    
    Change-Id: I2ff6866ff1fe23b47c54268ac8e182210cc876c5

--- src/runtime/pprof/pprof.go
+++ src/runtime/pprof/pprof.go
@@ -28,7 +28,11 @@
 //            if err != nil {
 //                log.Fatal("could not create CPU profile: ", err)
 //            }
-//            defer f.Close()
+//            defer func() {
+//                if err := f.Close(); err != nil {
+//                     log.Print("error closing CPU profile: ", err)
+//                }
+//            }()
 //            if err := pprof.StartCPUProfile(f); err != nil {
 //                log.Fatal("could not start CPU profile: ", err)
 //            }
@@ -42,7 +46,11 @@
 //            if err != nil {
 //                log.Fatal("could not create memory profile: ", err)
 //            }
-//            defer f.Close()
+//            defer func() {
+//                 if err := f.Close(); err != nil {
+//                      log.Print("error closing memory profile: ", err)
+//                 }
+//            }()
 //            runtime.GC() // get up-to-date statistics
 //            if err := pprof.WriteHeapProfile(f); err != nil {
 //                log.Fatal("could not write memory profile: ", err)
