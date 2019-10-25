64bd71178b737d4066d3ca2c81cc183b76e9574d
Bryan C. Mills <bcmills@google.com>
2019-10-25 15:37:00 -0400

    os: use an actual RemoveAll failure in TestRemoveAllWithMoreErrorThanReqSize
    Previously we injected an error, and the injection points were
    (empirically) not realistic on some platforms.
    
    Instead, we now make the directory read-only, which (on most
    platforms) suffices to prevent the removal of its files.
    
    DO NOT SUBMIT: Need to re-enable short-mode skip after TryBots.
    
    Fixes #35117
    Updates #29921
    
    Change-Id: Ica4e2818566f8c14df3eed7c3b8de5c0abeb6963

--- src/os/removeall_test.go
+++ src/os/removeall_test.go
@@ -5,7 +5,6 @@
 package os_test
 
 import (
-	"errors"
 	"fmt"
 	"io/ioutil"
 	. "os"
@@ -410,15 +409,8 @@ func TestRemoveUnreadableDir(t *testing.T) {
 // Issue 29921
 func TestRemoveAllWithMoreErrorThanReqSize(t *testing.T) {
 	if testing.Short() {
-		t.Skip("skipping in short mode")
-	}
-
-	defer func(oldHook func(error) error) {
-		*RemoveAllTestHook = oldHook
-	}(*RemoveAllTestHook)
-
-	*RemoveAllTestHook = func(err error) error {
-		return errors.New("error from RemoveAllTestHook")
+		// TODO(bcmills): Re-enable skip after TryBots.
+		// t.Skip("skipping in short mode")
 	}
 
 	tmpDir, err := ioutil.TempDir("", "TestRemoveAll-")
@@ -429,26 +421,55 @@ func TestRemoveAllWithMoreErrorThanReqSize(t *testing.T) {
 
 	path := filepath.Join(tmpDir, "_TestRemoveAllWithMoreErrorThanReqSize_")
 
-	// Make directory with 1025 files and remove.
+	// Make directory with 1025 read-only files.
 	if err := MkdirAll(path, 0777); err != nil {
 		t.Fatalf("MkdirAll %q: %s", path, err)
 	}
+	var paths []string
 	for i := 0; i < 1025; i++ {
 		fpath := filepath.Join(path, fmt.Sprintf("file%d", i))
 		fd, err := Create(fpath)
 		if err != nil {
 			t.Fatalf("create %q: %s", fpath, err)
 		}
+		paths = append(paths, fpath)
 		fd.Close()
 	}
 
-	// This call should not hang
-	if err := RemoveAll(path); err == nil {
-		t.Fatal("Want error from RemoveAllTestHook, got nil")
+	// Make the parent directory read-only. On some platforms, this is what
+	// prevents os.Remove from removing the files within that directory.
+	if err := Chmod(path, 0555); err != nil {
+		t.Fatal(err)
+	}
+	defer Chmod(path, 0755)
+
+	// This call should not hang, even on a platform that disallows file deletion
+	// from read-only directories.
+	err = RemoveAll(path)
+
+	if Getuid() == 0 {
+		// On many platforms, root can remove files from read-only directories.
+		return
+	}
+	if err == nil {
+		t.Fatal("RemoveAll(<read-only directory>) = nil; want error")
 	}
 
-	// We hook to inject error, but the actual files must be deleted
-	if _, err := Lstat(path); err == nil {
-		t.Fatal("directory must be deleted even with removeAllTetHook run")
+	dir, err := Open(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer dir.Close()
+
+	if runtime.GOOS == "windows" {
+		// Marking a directory in Windows does not prevent the os package from
+		// creating or removing files within it.
+		// (See https://golang.org/issue/35042.)
+		return
+	}
+
+	names, _ := dir.Readdirnames(1025)
+	if len(names) < 1025 {
+		t.Fatalf("RemoveAll(<read-only directory>) unexpectedly removed %d read-only files from that directory", 1025-len(names))
 	}
 }
