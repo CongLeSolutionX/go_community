4a6a660e254807ed31ecae5d7935ea42a2660a3f
Cherry Zhang <cherryyz@google.com>
2019-10-28 00:50:39 -0400

    cmd/compile, cmd/internal/obj/ppc64: mark unsafe points
    We'll use CTR as a scratch register for call injection. Mark code
    sequences that use CTR as unsafe for async preemption. Currently
    it is only used in LoweredZero and LoweredMove. It is unfortunate
    that they are nonpreemptible. But I think it is still better than
    using LR for call injection and marking all leaf functions
    nonpreemptible.
    
    Also mark the prologue of large frame functions nonpreemptible,
    as we write below SP.
    
    Change-Id: I05a75431499f3f4b2f23651a7b17f7fcf2afbe06

--- src/cmd/compile/internal/ssa/opGen.go
+++ src/cmd/compile/internal/ssa/opGen.go
@@ -24539,6 +24539,7 @@ var opcodeTable = [...]opInfo{
 		argLen:         2,
 		clobberFlags:   true,
 		faultOnNilArg0: true,
+		unsafePoint:    true,
 		reg: regInfo{
 			inputs: []inputInfo{
 				{0, 8}, // R3
@@ -24553,6 +24554,7 @@ var opcodeTable = [...]opInfo{
 		clobberFlags:   true,
 		faultOnNilArg0: true,
 		faultOnNilArg1: true,
+		unsafePoint:    true,
 		reg: regInfo{
 			inputs: []inputInfo{
 				{0, 8},  // R3
