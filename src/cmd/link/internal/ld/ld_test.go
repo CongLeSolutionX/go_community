588718f113393e8042480c28a6b012f09b518627
Than McIntosh <thanm@google.com>
2019-10-28 11:51:13 -0400

    cmd/link: directly exec archive command if external tmpdir
    When linking a Go archive, if the archiver invocation is the very last
    thing that needs to happen in the link (no "atexit" cleanups required
    remove the locally created tmpdir) then call syscall.Exec to invoke
    the archiver command instead of the usual exec.Command. This has the
    effect of reducing peak memory use for the linker overall, since we
    don't be holding onto all of the linker's live memory while the
    archiver is running.
    
    Change-Id: Ibbe22d8d67a70cc2a4f91c68aab56d19fb77c393

--- src/cmd/link/internal/ld/ld_test.go
+++ src/cmd/link/internal/ld/ld_test.go
@@ -5,10 +5,13 @@
 package ld
 
 import (
+	"fmt"
 	"internal/testenv"
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"runtime"
 	"strings"
 	"testing"
 )
@@ -67,5 +70,63 @@ func TestUndefinedRelocErrors(t *testing.T) {
 	}
 	for unexpected, n := range unexpectedErrors {
 		t.Errorf("unexpected error: %s (x%d)", unexpected, n)
+	}
+}
+
+const carchiveSrcText = `
+package main
+
+// int fortytwo;
+import "C"
+
+var v int
+
+//export GoFunc
+func GoFunc() {
+	v = int(C.fortytwo)
+}
+
+func main() {
+}
+`
+
+func TestArchiveBuildInvokeWithExec(t *testing.T) {
+	t.Parallel()
+	testenv.MustHaveGoBuild(t)
+	testenv.MustHaveCGO(t)
+	switch runtime.GOOS {
+	case "openbsd", "windows":
+		t.Skip("c-archive unsupported")
+	}
+	dir, err := ioutil.TempDir("", "go-build")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	srcfile := filepath.Join(dir, "test.go")
+	arfile := filepath.Join(dir, "test.a")
+	if err := ioutil.WriteFile(srcfile, []byte(carchiveSrcText), 0666); err != nil {
+		t.Fatal(err)
+	}
+
+	ldf := fmt.Sprintf("-ldflags=-v -tmpdir=%s", dir)
+	argv := []string{"build", "-buildmode=c-archive", "-o", arfile, ldf, srcfile}
+	out, err := exec.Command(testenv.GoToolPath(t), argv...).CombinedOutput()
+	if err != nil {
+		t.Fatalf("build failure: %s\n%s\n", err, string(out))
+	}
+
+	found := false
+	const want = "invoking archiver with syscall.Exec"
+	for _, l := range strings.Split(string(out), "\n") {
+		if strings.HasPrefix(l, want) {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		t.Errorf("expected '%s' in -v output, got:\n%s\n", want, string(out))
 	}
 }
