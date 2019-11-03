b9096ee8bc9240e4309c3a0cbbc4a662efccffe8
Joel Sing <joel@sing.id.au>
2019-11-04 04:58:37 +1100

    runtime: add support for linux/riscv64
    Based on riscv-go port.
    
    Updates #27532
    
    Change-Id: If522807a382130be3c8d40f4b4c1131d1de7c9e3

--- src/runtime/malloc.go
+++ src/runtime/malloc.go
@@ -502,6 +502,12 @@ func mallocinit() {
 		// allocation at 0x40 << 32 because when using 4k pages with 3-level
 		// translation buffers, the user address space is limited to 39 bits
 		// On darwin/arm64, the address space is even smaller.
+		//
+		// Linux on riscv64 provides a 38-bit address space to user processes
+		// (the full address space is 39-bit on minimal conforming implementations,
+		// but the kernel takes half of that); the default shared library base is
+		// 0x20_0000_0000 so don't conflict with that either.
+		//
 		// On AIX, mmaps starts at 0x0A00000000000000 for 64-bit.
 		// processes.
 		for i := 0x7f; i >= 0; i-- {
@@ -511,6 +517,8 @@ func mallocinit() {
 				p = uintptr(i)<<40 | uintptrMask&(0x0013<<28)
 			case GOARCH == "arm64":
 				p = uintptr(i)<<40 | uintptrMask&(0x0040<<32)
+			case GOOS == "linux" && GOARCH == "riscv64":
+				p = uintptr(i)<<40 | uintptrMask&(0x0021<<32)
 			case GOOS == "aix":
 				if i == 0 {
 					// We don't use addresses directly after 0x0A00000000000000
