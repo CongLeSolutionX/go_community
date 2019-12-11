b1ec81d4dfc2d3da6cbb8a219b9faee9a1826df2
Than McIntosh <thanm@google.com>
2019-12-11 14:36:17 -0500

    [dev.link] cmd/link: add SymbolBuilder helper
    Add SymbolBuilder helper type -- this type provides a set of methods
    intended to make it easy to manipulate the content of a symbol (type,
    relocations, data, etc).
    
    Change-Id: I579bf8d04650e66d33a9780a6c2347a576c94c6f

--- src/cmd/link/internal/loader/loader.go
+++ src/cmd/link/internal/loader/loader.go
@@ -209,7 +209,11 @@ type Loader struct {
 	flags uint32
 
 	strictDupMsgs int // number of strict-dup warning/errors, when FlagStrictDups is enabled
+
+	elfsetstring elfsetstringFunc
 }
+
+type elfsetstringFunc func(s *sym.Symbol, str string, off int)
 
 // extSymPayload holds the payload (data + relocations) for linker-synthesized
 // external symbols.
@@ -228,7 +232,7 @@ const (
 	FlagStrictDups = 1 << iota
 )
 
-func NewLoader(flags uint32) *Loader {
+func NewLoader(flags uint32, elfsetstring elfsetstringFunc) *Loader {
 	nbuiltin := goobj2.NBuiltin()
 	return &Loader{
 		start:         make(map[*oReader]Sym),
@@ -243,6 +247,7 @@ func NewLoader(flags uint32) *Loader {
 		extStaticSyms: make(map[nameVer]Sym),
 		builtinSyms:   make([]Sym, nbuiltin),
 		flags:         flags,
+		elfsetstring:  elfsetstring,
 	}
 }
 
