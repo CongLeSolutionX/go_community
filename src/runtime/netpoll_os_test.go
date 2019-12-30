6ed1c5f66d289eda6721cca7bceb65465a2313e7
Andy Pan <panjf2000@gmail.com>
2019-12-30 03:40:47 +0000

    runtime: converge duplicate calls of netpollBreak to one
    There might be some concurrent and duplicate calls of `netpollBreak`, trying to wake up a net-poller. If one has called `netpollBreak` and that waking event hasn't been received by epollwait/kevent/..., then the subsequent calls of `netpollBreak` ought to be ignored and these calls will be converged to one.
    
    Benchmarks go1.13.5 darwin/amd64:
    
    benchmark-func           time-cost/op (old)  time-cost/op (new)  time-delta  mem-cost/B (old)
    BenchmarkNetpollBreak-4  29668ns ±1%         3131ns ±2%          -89.45%     154B ±13%
    
    mem-cost/B (new)  mem-delta
    0B ±0%            -100%
    
    Change-Id: I3cf757a5d6edc5a99adad7aea3baee4b7f2a8f5c
    GitHub-Last-Rev: 372d747e7416a6a4fdff418d26b28b7685280570
    GitHub-Pull-Request: golang/go#36294

---
+++ src/runtime/netpoll_os_test.go
@@ -0,0 +1,26 @@
+package runtime_test
+
+import (
+	"runtime"
+	"sync"
+	"testing"
+)
+
+var wg sync.WaitGroup
+
+func init() {
+	runtime.NetpollGenericInit()
+}
+
+func BenchmarkNetpollBreak(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < 10; j++ {
+			wg.Add(1)
+			go func() {
+				runtime.NetpollBreak()
+				wg.Done()
+			}()
+		}
+	}
+	wg.Wait()
+}
