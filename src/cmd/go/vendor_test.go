2b4b48483fc77cb9ebf15b3c1a01d00084f12e8d
Michael Matloob <matloob@golang.org>
2020-01-09 18:05:38 -0500

    cmd/go: convert TestLegacyModGet to the script framework
    I think this test needs to be split up eventually. It's one of
    the longest tests.
    
    Part of converting all tests to script framework to improve
    test parallelism.
    
    Updates #36320
    Updates #17751
    
    Change-Id: If2168fac040d78fd0ec3dcbdef2affd2a8f48f6d

--- src/cmd/go/vendor_test.go
+++ src/cmd/go/vendor_test.go
@@ -7,8 +7,6 @@
 package main_test
 
 import (
-	"internal/testenv"
-	"os"
 	"path/filepath"
 	"strings"
 	"testing"
@@ -27,60 +25,4 @@ func TestVendorGOPATH(t *testing.T) {
 	tg.cd(cd)
 	tg.run("run", "hello.go")
 	tg.grepStdout("hello, world", "missing hello world output")
-}
-
-func TestLegacyModGet(t *testing.T) {
-	testenv.MustHaveExternalNetwork(t)
-	testenv.MustHaveExecPath(t, "git")
-
-	tg := testgo(t)
-	defer tg.cleanup()
-	tg.makeTempdir()
-	tg.setenv("GOPATH", tg.path("d1"))
-	tg.run("get", "vcs-test.golang.org/git/modlegacy1-old.git/p1")
-	tg.run("list", "-f", "{{.Deps}}", "vcs-test.golang.org/git/modlegacy1-old.git/p1")
-	tg.grepStdout("new.git/p2", "old/p1 should depend on new/p2")
-	tg.grepStdoutNot("new.git/v2/p2", "old/p1 should NOT depend on new/v2/p2")
-	tg.run("build", "vcs-test.golang.org/git/modlegacy1-old.git/p1", "vcs-test.golang.org/git/modlegacy1-new.git/p1")
-
-	tg.setenv("GOPATH", tg.path("d2"))
-
-	tg.must(os.RemoveAll(tg.path("d2")))
-	tg.run("get", "github.com/rsc/vgotest5")
-	tg.run("get", "github.com/rsc/vgotest4")
-	tg.run("get", "github.com/myitcv/vgo_example_compat")
-
-	if testing.Short() {
-		return
-	}
-
-	tg.must(os.RemoveAll(tg.path("d2")))
-	tg.run("get", "github.com/rsc/vgotest4")
-	tg.run("get", "github.com/rsc/vgotest5")
-	tg.run("get", "github.com/myitcv/vgo_example_compat")
-
-	tg.must(os.RemoveAll(tg.path("d2")))
-	tg.run("get", "github.com/rsc/vgotest4", "github.com/rsc/vgotest5")
-	tg.run("get", "github.com/myitcv/vgo_example_compat")
-
-	tg.must(os.RemoveAll(tg.path("d2")))
-	tg.run("get", "github.com/rsc/vgotest5", "github.com/rsc/vgotest4")
-	tg.run("get", "github.com/myitcv/vgo_example_compat")
-
-	tg.must(os.RemoveAll(tg.path("d2")))
-	tg.run("get", "github.com/myitcv/vgo_example_compat")
-	tg.run("get", "github.com/rsc/vgotest4", "github.com/rsc/vgotest5")
-
-	pkgs := []string{"github.com/myitcv/vgo_example_compat", "github.com/rsc/vgotest4", "github.com/rsc/vgotest5"}
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 3; j++ {
-			for k := 0; k < 3; k++ {
-				if i == j || i == k || k == j {
-					continue
-				}
-				tg.must(os.RemoveAll(tg.path("d2")))
-				tg.run("get", pkgs[i], pkgs[j], pkgs[k])
-			}
-		}
-	}
 }
