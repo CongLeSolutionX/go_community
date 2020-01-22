db64d1f57010b08bc2223b894f6beac510174bd7
Kévin Dunglas <dunglas@gmail.com>
2020-01-22 09:23:43 +0000

    net/http/httputil: add support for X-Forwarded-Proto, X-Forwarded-Host and an option to not trust forwarded headers in ReverseProxy
    In addition to X-Forwarded-For, which is already supported but undocumented
    (#36672), this patch adds support for the X-Forwarded-Proto (https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-Proto)
    and X-Forwarded-Host (https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-Host)
    headers in ReverseProxy.
    It also introduces a new option, ReverseProxy.TrustForwardedHeaders
    that must be explicitly set to true to trust forwarded headers coming from
    the client or previous proxies.
    
    Setting these headers is a standard behavior expected from reverse
    proxies, especially when X-Forwarded-For is set (which is already the
    case).
    
    The new ReverseProxy.TrustForwardedHeaders option fixes a potential
    common security issue in user land code. It forces the user to explicitly
    opt-in to trust reverse proxies in front of the Go application using
    ReverseProxy.
    
    The current behavior of blindly trusting the X-Forwarded-For header
    provided by the client is very dangerous.
    For instance, because of the current (undocumented) behavior, the Vulcain
    gateway server (https://vulcain.rocks) was sensible to a potential security
    issue.
    
    If this flag is not set to true, previous values set by the client or
    untrusted proxies will be discarded (replaced by the values computed
    by ReverseProxy).
    Technically, this is a BC break, but, in my opinion, it is a necessary one
    to improve the security of the whole Go ecosystem using this module.
    
    Change-Id: I5f9dc73afdfbc33ac87f27592ec0103fe6f4df42
    GitHub-Last-Rev: c0b06fb704dc4adaf9ed1ebd8d0a6fdcde69ddb7
    GitHub-Pull-Request: golang/go#36678

--- src/net/http/httputil/reverseproxy.go
+++ src/net/http/httputil/reverseproxy.go
@@ -25,13 +25,10 @@ import (
 // sends it to another server, proxying the response back to the
 // client.
 //
-// ReverseProxy automatically sets the client IP as the value of the
-// X-Forwarded-For header.
-// If an X-Forwarded-For header already exists, the client IP is
-// appended to the existing values.
-// To prevent IP spoofing, be sure to delete any pre-existing
-// X-Forwarded-For header coming from the client or
-// an untrusted proxy.
+// ReverseProxy automatically sets the X-Forwarded-For,
+// X-Forwarded-Host and X-Forwarded-Proto headers.
+// Previous values of these headers can be preversed by
+// setting TrustForwardedHeaders to true.
 type ReverseProxy struct {
 	// Director must be a function which modifies
 	// the request into a new request to be sent
@@ -84,6 +81,20 @@ type ReverseProxy struct {
 	// If nil, the default is to log the provided error and return
 	// a 502 Status Bad Gateway response.
 	ErrorHandler func(http.ResponseWriter, *http.Request, error)
+
+	// TrustForwardedHeaders specifies if X-Forwarded-For,
+	// X-Forwarded-Proto and X-Forwarded-Host headers coming from
+	// the previous proxy must be trusted or not.
+	//
+	// If true, existing values of X-Forwarded-Proto and
+	// X-Forwarded-Host will be preserved, and the current client IP
+	// will be appended to the list in X-Forwarded-For. In this case
+	// be sure that these 3 headers are removed from the request if
+	// sent by the client to prevent spoofing attacks.
+	//
+	// If false, values of these headers will be set regardless of
+	// any existing value.
+	TrustForwardedHeaders bool
 }
 
 // A BufferPool is an interface for getting and returning temporary
@@ -242,6 +253,23 @@ func (p *ReverseProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	if reqUpType != "" {
 		outreq.Header.Set("Connection", "Upgrade")
 		outreq.Header.Set("Upgrade", reqUpType)
+	}
+
+	if _, ok := outreq.Header["X-Forwarded-Proto"]; !ok || !p.TrustForwardedHeaders {
+		proto := "https"
+		if req.TLS == nil {
+			proto = "http"
+		}
+
+		outreq.Header.Set("X-Forwarded-Proto", proto)
+	}
+
+	if _, ok := outreq.Header["X-Forwarded-Host"]; !ok || !p.TrustForwardedHeaders {
+		outreq.Header.Set("X-Forwarded-Host", outreq.Host)
+	}
+
+	if !p.TrustForwardedHeaders {
+		outreq.Header.Del("X-Forwarded-For")
 	}
 
 	if clientIP, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
