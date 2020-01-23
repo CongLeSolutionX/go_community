f0f4d838e3e9b45846989c794a1d98535b372c08
Jeremy Faller <jeremy@golang.org>
2020-01-23 12:08:21 -0500

    [dev.link] cmd/link: add telemetry
    DO NOT SUBMIT
    
    Create some telemetry infrastructure for measuring linker progress.
    
    As it currently stands (Jan 23, 2020), it's unlikely this will be the
    final telemetry by which we will measure our performance with the linker
    rewrite; however, this CL is a probably a good starting point.
    
    Change-Id: Id557f3bfae0c02b4d1c7174432806512aca42c86

---
+++ src/cmd/link/internal/benchmark/bench.go
@@ -0,0 +1,131 @@
+// Copyright 2020 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package benchmark
+
+import (
+	"fmt"
+	"io"
+	"runtime"
+	"time"
+	"unicode"
+)
+
+type Flags int
+
+const (
+	NoGC Flags = 0
+	GC         = 1 << iota
+)
+
+type Metrics struct {
+	gc      Flags
+	marks   []*mark
+	curMark *mark
+}
+
+type mark struct {
+	name              string
+	startM, endM, gcM runtime.MemStats
+	startT, endT      time.Time
+}
+
+// New creates a new Metrics object.
+//
+// Typical usage should look like:
+//
+// func main() {
+//   bench := benchmark.New(true, benchmark.GC)
+//   defer bench.Report(os.Stdout)
+//   // etc
+//   bench.Start("foo")
+//   foo()
+//   bench.Start("bar")
+//   bar()
+// }
+func New(enabled bool, gc Flags) *Metrics {
+	if !enabled {
+		return nil
+	}
+	if gc == GC {
+		runtime.GC()
+	}
+	return &Metrics{gc: gc}
+}
+
+// Report reports the metrics.
+// Closes the currently Start(ed) range, and writes the report to the given io.Writer.
+func (m *Metrics) Report(w io.Writer) {
+	if m == nil {
+		return
+	}
+
+	m.closeMark()
+
+	gcString := ""
+	if m.gc == GC {
+		gcString = "_GC"
+	}
+
+	var totTime time.Duration
+	for _, curMark := range m.marks {
+		dur := curMark.endT.Sub(curMark.startT)
+		totTime += dur
+		fmt.Fprintf(w, "%s 1 %d ms/op", makeBenchString(curMark.name+gcString), dur.Milliseconds())
+		fmt.Fprintf(w, "\t%d B/op", curMark.endM.TotalAlloc-curMark.startM.TotalAlloc)
+		fmt.Fprintf(w, "\t%d allocs/op", curMark.endM.Mallocs-curMark.startM.Mallocs)
+		if m.gc == GC {
+			fmt.Fprintf(w, "\t%d live-B", curMark.gcM.HeapAlloc)
+		} else {
+			fmt.Fprintf(w, "\t%d heap-B", curMark.endM.HeapAlloc)
+		}
+		fmt.Fprintf(w, "\n")
+	}
+	fmt.Fprintf(w, "%s 1 %d ns/op\n", makeBenchString("total time"+gcString), totTime.Nanoseconds())
+}
+
+// Starts marks the beginning of a new measurement phase.
+// Once a metric is started, it continues until either a Report is issued, or another Start is called.
+func (m *Metrics) Start(name string) {
+	if m == nil {
+		return
+	}
+	m.closeMark()
+	m.curMark = &mark{name: name}
+	// Unlikely we need to a GC here, as one was likely just done in closeMark.
+	runtime.ReadMemStats(&m.curMark.startM)
+	m.curMark.startT = time.Now()
+}
+
+func (m *Metrics) closeMark() {
+	if m == nil || m.curMark == nil {
+		return
+	}
+	m.curMark.endT = time.Now()
+	runtime.ReadMemStats(&m.curMark.endM)
+	if m.gc == GC {
+		runtime.GC()
+		runtime.ReadMemStats(&m.curMark.gcM)
+	}
+	m.marks = append(m.marks, m.curMark)
+	m.curMark = nil
+}
+
+// makeBenchString makes a benchmark string consumable by Go's benchmarking tools.
+func makeBenchString(name string) string {
+	needCap := true
+	ret := []rune("Benchmark")
+	for _, r := range name {
+		if unicode.IsSpace(r) {
+			needCap = true
+			continue
+		}
+		if needCap {
+			r = unicode.ToUpper(r)
+			needCap = false
+		}
+		ret = append(ret, r)
+	}
+	return string(ret)
+}
