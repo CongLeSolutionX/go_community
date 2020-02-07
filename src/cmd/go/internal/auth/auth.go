636adeedfb5cbfca7c75269d9de15e8c8d933667
David Howden <dhowden@gmail.com>
2020-02-08 08:59:45 +1100

    cmd/go/internal/auth: fix .netrc lookup for URLs with specified port
    The .netrc spec [1] defines credentials based on "machine name", so remove specified ports
    from URL before looking for a match.
    
    [1] https://www.gnu.org/software/inetutils/manual/html_node/The-_002enetrc-file.html
    
    Fixes #37130
    
    Change-Id: Iab993afba26c927454d6166111ad1e1a53dbce43

--- src/cmd/go/internal/auth/auth.go
+++ src/cmd/go/internal/auth/auth.go
@@ -5,15 +5,28 @@
 // Package auth provides access to user-provided authentication credentials.
 package auth
 
-import "net/http"
+import (
+	"net"
+	"net/http"
+)
 
 // AddCredentials fills in the user's credentials for req, if any.
 // The return value reports whether any matching credentials were found.
 func AddCredentials(req *http.Request) (added bool) {
+	// Host from req.URL.Host can be of the form host:post, but netrc spec
+	// defines machine to be "machine name", so check for :port and remove
+	// before matching to netrc machine names.
+	// https://www.gnu.org/software/inetutils/manual/html_node/The-_002enetrc-file.html
+	host, _, err := net.SplitHostPort(req.URL.Host)
+	if err != nil {
+		// Missing port (or invalid), leave as-is.
+		host = req.URL.Host
+	}
+
 	// TODO(golang.org/issue/26232): Support arbitrary user-provided credentials.
 	netrcOnce.Do(readNetrc)
 	for _, l := range netrc {
-		if l.machine == req.URL.Host {
+		if l.machine == host {
 			req.SetBasicAuth(l.login, l.password)
 			return true
 		}
