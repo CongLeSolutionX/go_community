55cb74165305b433174d353945e87004934b4574
Michael Anthony Knyszek <mknyszek@google.com>
2020-02-10 23:11:30 +0000

    runtime: avoid re-scanning scavenged and untouched memory
    Currently the scavenger will reset to the top of the heap every GC. This
    means if it scavenges a bunch of memory which doesn't get used again,
    it's going to keep re-scanning that memory on subsequent cycles. This
    problem is especially bad when it comes to heap spikes: suppose an
    application's heap spikes to 2x its steady-state size. The scavenger
    will run over the top half of that heap even if the heap shrinks, for
    the rest of the application's lifetime.
    
    To fix this, we maintain two numbers: a "free" high watermark, which
    represents the highest address freed to the page allocator in that
    cycle, and a "scavenged" low watermark, which represents how low of an
    address the scavenger got to when scavenging. If the "free" watermark
    exceeds the "scavenged" watermark, then we pick the "free" watermark as
    the new "top of the heap" for the scavenger when starting the next
    scavenger cycle. Otherwise, we have the scavenger pick up where it left
    off.
    
    With this mechanism, we only ever re-scan scavenged memory if a random
    page gets freed very high up in the heap address space while most of the
    action is happening in the lower parts. This case should be exceedingly
    unlikely because the page reclaimer walks over the heap from low address
    to high addresses, and we use a first-fit address-ordered allocation
    policy.
    
    Updates #35788.
    
    Change-Id: Id335603b526ce3a0eb79ef286d1a4e876abc9cab

--- src/runtime/mgcscavenge.go
+++ src/runtime/mgcscavenge.go
@@ -450,9 +450,29 @@ func (s *pageAlloc) scavengeStartGen() {
 		printScavTrace(s.scav.gen, s.scav.released, false)
 	}
 	s.inUse.cloneInto(&s.scav.inUse)
+
+	// Pick the new starting address for the scavenger cycle.
+	var startAddr uintptr
+	if s.scav.scavLWM < s.scav.freeHWM {
+		// The "free" high watermark exceeds the "scavenged" low watermark,
+		// so there are free scavengable pages in parts of the address space
+		// that the scavenger already searched, the high watermark being the
+		// highest one. Pick that as our new starting point to ensure we
+		// see those pages.
+		startAddr = s.scav.freeHWM
+	} else {
+		// The "free" high watermark does not exceed the "scavenged" low
+		// watermark. This means the allocator didn't free any memory in
+		// the range we scavenged last cycle, so we might as well continue
+		// scavenging from where we were.
+		startAddr = s.scav.scavLWM
+	}
+	s.scav.inUse.removeAbove(startAddr)
 	s.scav.reservationBytes = alignUp(s.inUse.totalBytes, pallocChunkBytes) / scavengeReservationShards
 	s.scav.gen++
 	s.scav.released = 0
+	s.scav.freeHWM = 0
+	s.scav.scavLWM = maxSearchAddr
 }
 
 // scavengeReserve reserves a contiguous range of the address space
@@ -663,6 +683,11 @@ func (s *pageAlloc) scavengeRangeLocked(ci chunkIdx, base, npages uint) uintptr 
 
 	// Compute the full address for the start of the range.
 	addr := chunkBase(ci) + uintptr(base)*pageSize
+
+	// Update the scavenge low watermark.
+	if addr < s.scav.scavLWM {
+		s.scav.scavLWM = addr
+	}
 
 	// Only perform the actual scavenging if we're not in a test.
 	// It's dangerous to do so otherwise.
