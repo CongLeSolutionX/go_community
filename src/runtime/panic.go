36aed1b8a1202c0d64e76083ed465b8ec367b299
Dan Scales <danscales@google.com>
2020-03-05 12:46:04 -0800

    runtime: fix problem with repeated panic/recover/re-panics and open-coded defers
    In the open-code defer implementation, we add defer struct elements to the defer
    chain on-the-fly at panic time to represent stack frames that contain open-coded
    defers. This allows us to process non-open-coded and open-coded defers in the
    correct order. However, if a recover happens, an unprocessed defer element
    representing an open-coded defer will become likely become stale, since open-coded
    defers are processed inline when not in panic mode.
    
    The current code was only removing the top-most open-coded defer from the defer
    chain during recovery. However, with recursive functions that do repeated
    panic-recover-repanic, multiple stale entries can accumulate on the chain. So, we
    just adjust the loop to process the entire chain. Since this is at panic/recover
    case, it is fine to scan through the entire chain (which should usually have few
    elements in it, since most defers are open-coded).
    
    This fix is quite simple.  It is probably suitable for 1.14.1.
    
    The added test fails with a SEGV without the fix, because it tries to run a stale
    open-code defer entry (and the stack has changed).
    
    Fixes #37664.
    
    Change-Id: I8e3da5d610b5e607411451b66881dea887f7484d

--- src/runtime/panic.go
+++ src/runtime/panic.go
@@ -1003,11 +1003,11 @@ func gopanic(e interface{}) {
 			atomic.Xadd(&runningPanicDefers, -1)
 
 			if done {
-				// Remove any remaining non-started, open-coded defer
-				// entry after a recover (there's at most one, if we just
-				// ran a non-open-coded defer), since the entry will
-				// become out-dated and the defer will be executed
-				// normally.
+				// Remove any remaining non-started, open-coded
+				// defer entries after a recover, since the
+				// corresponding defers will be executed normally
+				// (inline), so any such entry will become
+				// out-dated.
 				d := gp._defer
 				var prev *_defer
 				for d != nil {
@@ -1025,8 +1025,9 @@ func gopanic(e interface{}) {
 						} else {
 							prev.link = d.link
 						}
+						newd := d.link
 						freedefer(d)
-						break
+						d = newd
 					} else {
 						prev = d
 						d = d.link
