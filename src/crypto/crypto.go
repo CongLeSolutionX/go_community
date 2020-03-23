7b0a747fcebd4dabd06699f9e4d78d1f8d5a37a0
Katie Hockman <katie@golang.org>
2020-03-23 10:11:43 -0400

    crypto: implement Hash.String
    Fixes #33430
    
    Change-Id: I323323b3136dd7b408005c3bb5ea05e3b566bd38

--- src/crypto/crypto.go
+++ src/crypto/crypto.go
@@ -11,6 +11,8 @@ import (
 	"strconv"
 )
 
+//go:generate stringer -type=Hash
+
 // Hash identifies a cryptographic hash function that is implemented in another
 // package.
 type Hash uint
@@ -18,6 +20,51 @@ type Hash uint
 // HashFunc simply returns the value of h so that Hash implements SignerOpts.
 func (h Hash) HashFunc() Hash {
 	return h
+}
+
+func (h Hash) String() string {
+	switch h {
+	case MD4:
+		return "MD4"
+	case MD5:
+		return "MD5"
+	case SHA1:
+		return "SHA-1"
+	case SHA224:
+		return "SHA-224"
+	case SHA256:
+		return "SHA-256"
+	case SHA384:
+		return "SHA-384"
+	case SHA512:
+		return "SHA-512"
+	case MD5SHA1:
+		return "MD5+SHA1"
+	case RIPEMD160:
+		return "RIPEMD-160"
+	case SHA3_224:
+		return "SHA-3/224"
+	case SHA3_256:
+		return "SHA-3/256"
+	case SHA3_384:
+		return "SHA-3/384"
+	case SHA3_512:
+		return "SHA-3/512"
+	case SHA512_224:
+		return "SHA-512/224"
+	case SHA512_256:
+		return "SHA-512/256"
+	case BLAKE2s_256:
+		return "BLAKE2s-256"
+	case BLAKE2b_256:
+		return "BLAKE2b-256"
+	case BLAKE2b_384:
+		return "BLAKE2b-384"
+	case BLAKE2b_512:
+		return "BLAKE2b-512"
+	default:
+		panic("unreachable")
+	}
 }
 
 const (
