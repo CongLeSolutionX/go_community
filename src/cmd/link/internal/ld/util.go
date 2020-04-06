a053e459ede29ffb1d81eeaf14a4a29eab175ec0
Than McIntosh <thanm@google.com>
2020-04-06 16:05:00 -0400

    [dev.link] cmd/link: add new -dumpsymsat linker option
    When debugging linker problems, it is common to instrument the linker
    at various spots to dump out information on selected symbols, as a
    means of discovering what's happening during the link. This patch
    introduces a general-purpose symbol dumping debug utility, intended to
    make it possible to grab debugging dumps without having to edit and
    rebuild the linker, or keep patches lying around.
    
    The -dumpsymsat flag takes advantage of the recently added linked
    phase benchmarking infrastructure, meaning that you can request a
    symbol dump at each point the benchmark code marks the start of a
    linker phase.
    
    Flag argument is of the form
    
       ^?stype[,stype]*@phase[,phase]*
    
    where 'stype' is a symbol type (ex: STEXT), the '^' is a tag used to
    request relocation dumping, 'phase' is a linker phase (ex: "pclntab",
    or "dodata"). The keyword 'all' can be substituted for a symbol type
    or a phase name. Examples:
    
    - dump STEXT symbols (with relocs) and SNOPTRBSS symbols at dodata + final:
    
       ^STEXT,SNOPTRBSS@dodata,final
    
    - dump all symbols at pclntab:
    
       all@pclntab
    
    - dump SELFROSECT symbols at all phases:
    
       SELFROSECT@all
    
    The symbol dump format looks roughly like:
    
        %S236: runtime.assertE2I2<1> t=STEXT sz=163 val=0
          + R0: R_TLS_LE  o=5 a=0 tgt=<nil>
          + R1: R_CALL    o=63 a=0 tgt=runtime.getitab
          + R2: R_CALL    o=154 a=0 tgt=runtime.morestack_noctxt
    
    where 236 is the symbol index and '%' indicates reachability.
    
    Change-Id: I772181948252819dce1919b718ef068640e83b6f

--- src/cmd/link/internal/ld/util.go
+++ src/cmd/link/internal/ld/util.go
@@ -5,11 +5,13 @@
 package ld
 
 import (
+	"cmd/link/internal/benchmark"
 	"cmd/link/internal/loader"
 	"cmd/link/internal/sym"
 	"encoding/binary"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var atExitFuncs []func()
@@ -120,3 +122,312 @@ type byName []*sym.Symbol
 func (s byName) Len() int           { return len(s) }
 func (s byName) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 func (s byName) Less(i, j int) bool { return s[i].Name < s[j].Name }
+
+// dumpSyms is a helper for the -dumpsymsat linker command line option.
+type dumpSyms struct {
+	phases   []string
+	stypes   []string
+	dorelocs []bool
+	ctxt     *Link
+	which    string
+}
+
+func (ds *dumpSyms) dumpBeforeLoadlibfull() {
+	l := ds.ctxt.loader
+	syms := []loader.Sym{}
+	switch ds.which {
+	case "all":
+		for i := 1; i < l.NSym(); i++ {
+			syms = append(syms, loader.Sym(i))
+		}
+	case "text":
+		syms = ds.ctxt.Textp2
+	}
+	secti := make(map[*sym.Section]int)
+	sections := []*sym.Section{nil}
+	for _, s := range syms {
+		sect := l.SymSect(s)
+		if sect != nil {
+			if secti[sect] == 0 {
+				secti[sect] = len(sections)
+				sections = append(sections, sect)
+			}
+		}
+	}
+	if len(secti) > 0 {
+		fmt.Fprintf(os.Stderr, "Sections:\n")
+		for i, sect := range sections[1:] {
+			fmt.Fprintf(os.Stderr, " %d %s\n", i, sect.Name)
+		}
+	}
+	for _, s := range syms {
+		sn := l.SymName(s)
+		st := l.SymType(s)
+		doit, dorelocs := ds.includeSym(st)
+		if !doit {
+			continue
+		}
+		rch := " "
+		if l.AttrReachable(s) {
+			rch = "%"
+		}
+		sver := l.SymVersion(s)
+		sval := l.SymValue(s)
+		sz := l.SymSize(s)
+		dlen := len(l.Data(s))
+		fmt.Fprintf(os.Stderr, "%sS%d: %s<%d> t=%s sz=%d dlen=%d val=%d\n", rch, s, sn, sver, st.String(), sz, dlen, sval)
+		if dorelocs {
+			relocs := l.Relocs(s)
+			for ri := 0; ri < relocs.Count(); ri++ {
+				r := relocs.At2(ri)
+				rsrs := "<nil>"
+				if r.Sym() != 0 {
+					rsrs = l.SymName(r.Sym())
+				}
+				fmt.Fprintf(os.Stderr, "  + R%d: %-9s o=%d a=%d tgt=%s\n", ri, r.Type().String(), r.Off(), r.Add(), rsrs)
+			}
+		}
+	}
+}
+
+func (ds *dumpSyms) dumpAfterLoadlibfull() {
+	secti := make(map[*sym.Section]int)
+	sections := []*sym.Section{nil}
+	si := make(map[*sym.Symbol]int)
+	l := ds.ctxt.loader
+	for i := 1; i < len(l.Syms); i++ {
+		s := l.Syms[i]
+		if s == nil {
+			continue
+		}
+		si[s] = i
+		if s.Sect != nil {
+			if secti[s.Sect] == 0 {
+				secti[s.Sect] = len(sections)
+				sections = append(sections, s.Sect)
+			}
+		}
+	}
+
+	syms := ds.ctxt.Syms.Allsym
+	switch ds.which {
+	case "data":
+		syms = ds.ctxt.datap
+	case "text":
+		syms = ds.ctxt.Textp
+	}
+	if len(secti) > 0 {
+		fmt.Fprintf(os.Stderr, "Sections:\n")
+		for i, sect := range sections[1:] {
+			fmt.Fprintf(os.Stderr, " %d %s\n", i, sect.Name)
+		}
+	}
+	for _, s := range syms {
+		foundType := false
+		dorelocs := false
+		for i := range ds.stypes {
+			st := ds.stypes[i]
+			if st == s.Type.String() || st == "all" {
+				foundType = true
+				dorelocs = ds.dorelocs[i]
+				break
+			}
+		}
+		if !foundType {
+			continue
+		}
+		rch := " "
+		if s.Attr.Reachable() {
+			rch = "%"
+		}
+		fmt.Fprintf(os.Stderr, "%sS%d: %s<%d> t=%s sz=%d len(s.P)=%d val=%d sect=%d\n", rch, si[s], s.Name, s.Version, s.Type.String(), s.Size, len(s.P), s.Value, secti[s.Sect])
+		if dorelocs {
+			for ri := 0; ri < len(s.R); ri++ {
+				r := &s.R[ri]
+				rt := r.Type
+				rsrs := "<nil>"
+				if r.Sym != nil {
+					rsrs = r.Sym.String()
+				}
+				fmt.Fprintf(os.Stderr, "  + R%d: %-9s o=%d a=%d tgt=%s\n", ri, rt.String(), r.Off, r.Add, rsrs)
+			}
+		}
+	}
+}
+
+func (ds *dumpSyms) includeSym(t sym.SymKind) (bool, bool) {
+	for i := range ds.stypes {
+		st := ds.stypes[i]
+		if st == t.String() || st == "all" {
+			return true, ds.dorelocs[i]
+		}
+	}
+	return false, false
+}
+
+func (ds *dumpSyms) Start(which string) {
+	foundPhase := false
+	for _, p := range ds.phases {
+		if which == p || p == "all" {
+			foundPhase = true
+			break
+		}
+	}
+	if !foundPhase {
+		return
+	}
+	ctxt := ds.ctxt
+	fmt.Fprintf(os.Stderr, "\nSymbols dump before '%s':\n", which)
+	if ctxt.loader == nil || len(ctxt.loader.Syms) == 0 {
+		return
+	}
+	// HACK: peek at the context to detect whether we're dumping before
+	// or after loadlibfull.
+	if ctxt.lookup == nil {
+		ds.dumpBeforeLoadlibfull()
+	} else {
+		ds.dumpAfterLoadlibfull()
+	}
+}
+
+func dumpSymsError() {
+	fmt.Fprintf(os.Stderr, "malformed argument to -dumpsymsat; should be of form '^?stype[,stype]*[|selector]@phase[,phase]*'\n")
+	usage()
+}
+
+func makeDumpSyms(ctxt *Link) *dumpSyms {
+	if *dumpSymsFlag != "" {
+		// Vett the flag value. Flag argument is of the form
+		//
+		// ^?stype[,stype]*{selector}?@phase[,phase]*
+		//
+		// where 'stype' is either a symbol type (ex: STEXT) or a list
+		// such as "data"/"text", the '^' is a flag requesting dump of
+		// relocations, and 'phase' is a linker phase (ex: "pclntab",
+		// or "dodata"). The 'selector' clause is optional, if present
+		// it can be set to 'data' or 'text' to visit symbols on
+		// ctxt.data or ctxt.Textp instead of all syms. The keyword 'all' can be
+		// substituted for a symbol type or a phase name. Examples:
+		//
+		// Dump text symbols (with relocs) and NOPTRBSS symbols at dodata + final:
+		//
+		//    ^STEXT,SNOPTRBSS@dodata,final
+		//
+		// Dump all symbols at pclntab:
+		//
+		//    all@pclntab
+		//
+		// Dump all ctxt.datap symbols at address:
+		//
+		//    all{data}@address
+		//
+		// Dump SELFROSECT symbols at all phases:
+		//
+		//    SELFROSECT@all
+		//
+		phases := []string{}
+		stypes := []string{}
+		dorelocs := []bool{}
+		selector := "all"
+		halves := strings.Split(*dumpSymsFlag, "@")
+		if len(halves) != 2 {
+			dumpSymsError()
+		}
+		// symbol types
+		stypesec := halves[0]
+		switch {
+		case strings.HasSuffix(stypesec, "{data}"):
+			selector = "data"
+			stypesec = stypesec[:len(stypesec)-len("{data}")]
+		case strings.HasSuffix(stypesec, "{text}"):
+			selector = "text"
+			stypesec = stypesec[:len(stypesec)-len("{text}")]
+		case strings.HasSuffix(stypesec, "{all}"):
+			selector = "all"
+			stypesec = stypesec[:len(stypesec)-len("{all}")]
+		}
+		left := strings.Split(stypesec, ",")
+		if len(left) == 0 {
+			dumpSymsError()
+		}
+		for _, c := range left {
+			if len(c) == 0 {
+				dumpSymsError()
+			}
+			typ := c
+			rel := false
+			if c[0] == '^' {
+				typ = c[1:]
+				rel = true
+			}
+			stypes = append(stypes, typ)
+			dorelocs = append(dorelocs, rel)
+		}
+		// phases
+		right := strings.Split(halves[1], ",")
+		if len(right) == 0 {
+			dumpSymsError()
+		}
+		for _, p := range right {
+			if len(p) == 0 {
+				dumpSymsError()
+			}
+			phases = append(phases, p)
+		}
+		return &dumpSyms{
+			phases:   phases,
+			dorelocs: dorelocs,
+			stypes:   stypes,
+			ctxt:     ctxt,
+			which:    selector,
+		}
+	}
+	return nil
+}
+
+func makeBenchMetrics() *benchmark.Metrics {
+	if *benchmarkFlag != "" {
+		// enable benchmarking
+		if *benchmarkFlag == "mem" {
+			return benchmark.New(benchmark.GC, *benchmarkFileFlag)
+		} else if *benchmarkFlag == "cpu" {
+			return benchmark.New(benchmark.NoGC, *benchmarkFileFlag)
+		} else {
+			Errorf(nil, "unknown benchmark flag: %q", *benchmarkFlag)
+			usage()
+		}
+	}
+	return nil
+}
+
+// atPhaseStart keeps track of things to do at the start of
+// each linker phase (benchmarking, symbol dumping).
+type atPhaseStart struct {
+	bench   *benchmark.Metrics
+	sdumper *dumpSyms
+}
+
+func makeAtPhaseStart(ctxt *Link) *atPhaseStart {
+	return &atPhaseStart{
+		bench:   makeBenchMetrics(),
+		sdumper: makeDumpSyms(ctxt),
+	}
+}
+
+func (aps *atPhaseStart) Start(which string) {
+	if aps.bench != nil {
+		aps.bench.Start(which)
+	}
+	if aps.sdumper != nil {
+		aps.sdumper.Start(which)
+	}
+}
+
+func (aps *atPhaseStart) Report(f *os.File) {
+	if aps.bench != nil {
+		aps.bench.Report(f)
+	}
+	if aps.sdumper != nil {
+		aps.sdumper.Start("final")
+	}
+}
