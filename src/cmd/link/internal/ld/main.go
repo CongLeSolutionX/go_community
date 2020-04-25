a0bbf1ab9b0ce8faf1e9279c80c9bdff91a337b7
Cherry Zhang <cherryyz@google.com>
2020-04-25 14:17:10 -0400

    [dev.link] cmd/link: convert dwarfcompress to using the loader
    DO NOT SUBMIT. Need all platforms using new dodata.
    
    Change-Id: I34f806b54e8e0985a30ef38ea4324352aabfc845

--- src/cmd/link/internal/ld/main.go
+++ src/cmd/link/internal/ld/main.go
@@ -303,8 +303,6 @@ func Main(arch *sys.Arch, theArch Arch) {
 		bench.Start("dodata")
 		ctxt.dodata2(symGroupType)
 	}
-	bench.Start("loadlibfull")
-	ctxt.loadlibfull() // XXX do it here for now
 	if !*flagnewDoData {
 		bench.Start("dodata")
 		ctxt.dodata()
@@ -315,6 +313,8 @@ func Main(arch *sys.Arch, theArch Arch) {
 	dwarfcompress(ctxt)
 	bench.Start("layout")
 	filesize := ctxt.layout(order)
+	bench.Start("loadlibfull")
+	ctxt.loadlibfull() // XXX do it here for now
 
 	// Write out the output file.
 	// It is split into two parts (Asmb and Asmb2). The first
