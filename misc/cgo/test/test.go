cb4423450b36dc6453d9c5f523c333b2289fb150
Matthew Dempsky <mdempsky@google.com>
2020-04-24 18:04:20 -0700

    cmd/cgo: use type aliases for #define type macros
    Cgo's initial design for handling "#define foo int*" involved
    rewriting "C.foo" to "*_Ctype_int" everywhere. But now that we have
    type aliases, we can declare "type _Ctype_foo = *_Ctype_int" once, and
    then rewrite "C.foo" to just "_Ctype_foo".
    
    This is important for go/types's UsesCgo mode, where go/types needs to
    be able to figure out a type for each C.foo identifier using only the
    information written into _cgo_gotypes.go.
    
    Fixes #38649.
    
    Change-Id: Ia0f8c2d82df81efb1be5bc26195ea9154c0af871

--- misc/cgo/test/test.go
+++ misc/cgo/test/test.go
@@ -897,6 +897,9 @@ static uint16_t issue31093F(uint16_t v) { return v; }
 
 // issue 32579
 typedef struct S32579 { unsigned char data[1]; } S32579;
+
+// issue 38649
+#define netbsd_gid unsigned int
 */
 import "C"
 
@@ -2192,3 +2195,6 @@ func test32579(t *testing.T) {
 		t.Errorf("&s[0].data[0] failed: got %d, want %d", s[0].data[0], 1)
 	}
 }
+
+// issue 38649
+var _ C.netbsd_gid
