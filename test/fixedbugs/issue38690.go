d56c7f663387f0b789dd04906a0f98a9c75d356d
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2020-04-28 00:41:02 +0700

    cmd/compile: do not emit code for discardable blank fields
    Fixes #38690
    
    Change-Id: I3544daf617fddc0f89636265c113001178d16b0c

---
+++ test/fixedbugs/issue38690.go
@@ -0,0 +1,53 @@
+// compile
+
+// Copyright 2020 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Make sure that literal value can be passed to struct
+// blank field of array/struct type, see issue #38690.
+
+package main
+
+type A1 = [0]int
+type A2 = [1]int
+
+type S1 struct{}
+
+type S2 struct {
+	x int
+}
+
+type S3 = struct{}
+
+type S4 = struct{ x int }
+
+type S struct {
+	x int
+	_ [0]int
+	_ [1]int
+	_ A1
+	_ A2
+	_ S1
+	_ S2
+	_ S3
+	_ S4
+}
+
+func main() {
+	f1()
+	f2()
+}
+
+func f1() {
+	_ = S{1, [0]int{}, [1]int{1}, A1{}, A2{1}, S1{}, S2{1}, S3{}, S4{1}}
+}
+
+func f2() {
+	defer func() {
+		if recover() == nil {
+			panic("expected panic, got nil")
+		}
+	}()
+	_ = S{1, [0]int{}, [1]int{1}, A1{}, A2{1}, S1{}, S2{1}, S3{}, func() S4 { panic("") }()}
+}
