d89ae2509a20ee699ab7590dad76b1c98b528032
Robert Griesemer <gri@golang.org>
2020-04-28 16:54:44 -0700

    strconv: implement parseFloatPrefix returning no. of bytes consumed
    parseFloatPrefix will make it easier to implement ParseComplex.
    
    Verified that there's no relevant performance impact:
    Benchmarks run on a "quiet" MacBook Pro, 3.3GHz Dual-Core Intel Core i7,
    with 16GB 2133MHz LPDDR3 RAM running macOS 10.15.4.
    
    name                  old time/op  new time/op  delta
    Atof64Decimal-4       38.2ns ± 4%  38.4ns ± 3%    ~     (p=0.802 n=5+5)
    Atof64Float-4         41.1ns ± 3%  43.0ns ± 1%  +4.77%  (p=0.008 n=5+5)
    Atof64FloatExp-4      71.9ns ± 3%  70.1ns ± 1%    ~     (p=0.063 n=5+5)
    Atof64Big-4            124ns ± 5%   119ns ± 0%    ~     (p=0.143 n=5+4)
    Atof64RandomBits-4    57.2ns ± 1%  55.7ns ± 2%  -2.66%  (p=0.016 n=4+5)
    Atof64RandomFloats-4  56.8ns ± 1%  56.9ns ± 4%    ~     (p=0.556 n=4+5)
    Atof32Decimal-4       35.4ns ± 5%  35.9ns ± 0%    ~     (p=0.127 n=5+5)
    Atof32Float-4         39.6ns ± 7%  40.3ns ± 1%    ~     (p=0.135 n=5+5)
    Atof32FloatExp-4      73.7ns ± 7%  71.9ns ± 0%    ~     (p=0.175 n=5+4)
    Atof32Random-4         103ns ± 6%    98ns ± 2%  -5.03%  (p=0.008 n=5+5)
    
    Updates #36771.
    
    Change-Id: I8ff66b582ae8b468d89c9ffc35c569c735cf0341

--- src/strconv/atof_test.go
+++ src/strconv/atof_test.go
@@ -479,6 +479,26 @@ func initAtofOnce() {
 	}
 }
 
+func TestParseFloatPrefix(t *testing.T) {
+	for i := 0; i < len(atoftests); i++ {
+		test := &atoftests[i]
+		if test.err != nil {
+			continue
+		}
+		// adding these suffixes should not invalidate the number
+		for _, suffix := range []string{" ", "+", "-", "q", "(", ">"} {
+			in := test.in + suffix
+			_, n, err := ParseFloatPrefix(in, 64)
+			if err != nil {
+				t.Errorf("ParseFloatPrefix(%q, 64): err = %v; want no error", in, err)
+			}
+			if n != len(test.in) {
+				t.Errorf("ParseFloatPrefix(%q, 64): n = %d; want %d", in, n, len(test.in))
+			}
+		}
+	}
+}
+
 func testAtof(t *testing.T, opt bool) {
 	initAtof()
 	oldopt := SetOptimize(opt)
