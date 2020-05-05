b486b5e60656cbde8b6332bae840b449d432ac67
Jeremy Faller <jeremy@golang.org>
2020-05-05 12:34:10 -0400

    [dev.link] cmd/link: convert ppc64 archreloc over to Loader
    Change-Id: I68945a8284fb3dd9ceb5a9cd774b5b4b91e63ce0

--- src/cmd/link/internal/ld/elf2.go
+++ src/cmd/link/internal/ld/elf2.go
@@ -65,6 +65,7 @@ func elfrelocsect2(ctxt *Link, sect *sym.Section, syms []*sym.Symbol) {
 			esr := ElfSymForReloc(ctxt, r.Xsym)
 			if esr == 0 {
 				Errorf(s, "reloc %d (%s) to non-elf symbol %s (outer=%s) %d (%s)", r.Type, sym.RelocName(ctxt.Arch, r.Type), r.Sym.Name, r.Xsym.Name, r.Sym.Type, r.Sym.Type)
+				panic("HERE")
 			}
 			if !r.Xsym.Attr.Reachable() {
 				Errorf(s, "unreachable reloc %d (%s) target %v", r.Type, sym.RelocName(ctxt.Arch, r.Type), r.Xsym.Name)
--- src/cmd/link/internal/ld/xcoff.go
+++ src/cmd/link/internal/ld/xcoff.go
@@ -10,6 +10,7 @@ import (
 	"cmd/link/internal/loader"
 	"cmd/link/internal/sym"
 	"encoding/binary"
+	"fmt"
 	"io/ioutil"
 	"math/bits"
 	"path/filepath"
@@ -1122,7 +1123,7 @@ func Xcoffadddynrel2(target *Target, ldr *loader.Loader, syms *ArchSyms, s loade
 		sym2: s,
 		roff: r.Off(),
 	}
-	targ := r.Sym()
+	targ := ldr.ResolveABIAlias(r.Sym())
 	var targType sym.SymKind
 	if targ != 0 {
 		targType = ldr.SymType(targ)
@@ -1142,6 +1143,9 @@ func Xcoffadddynrel2(target *Target, ldr *loader.Loader, syms *ArchSyms, s loade
 				}
 			}
 		} else if t := ldr.SymType(s); t == sym.SDATA || t == sym.SNOPTRDATA || t == sym.SBUILDINFO || t == sym.SXCOFFTOC {
+			if ldr.SymSect(targ) == nil {
+				panic(fmt.Sprintf("name %s", ldr.SymName(targ)))
+			}
 			switch ldr.SymSect(targ).Seg {
 			default:
 				ldr.Errorf(s, "unknown segment for .loader relocation with symbol %s", ldr.SymName(targ))
