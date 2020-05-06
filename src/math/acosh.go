26aa7d91bda384059fb688b1675a05806093b9f9
surechen <surechen17@gmail.com>
2020-05-06 20:04:35 +0800

    math: remove redundant local variable Ln2
    Use the const variable Ln2 in math/const.go for function acosh.
    
    Change-Id: I5381d03dd3142c227ae5773ece9be6c8f377615e

--- src/math/acosh.go
+++ src/math/acosh.go
@@ -43,8 +43,7 @@ func Acosh(x float64) float64
 
 func acosh(x float64) float64 {
 	const (
-		Ln2   = 6.93147180559945286227e-01 // 0x3FE62E42FEFA39EF
-		Large = 1 << 28                    // 2**28
+		Large = 1 << 28 // 2**28
 	)
 	// first case is special case
 	switch {
