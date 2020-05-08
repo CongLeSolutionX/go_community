dc1bbba0d64eea72e4fb396c0ef12c4e0cec0b62
Steven Hartland <steven.hartland@multiplay.co.uk>
2020-05-08 12:09:00 +0000

    cmd/link: fix mode parameter to fallocate on Linux
    Fix the mode parameter to fallocate on Linux which is the operation mode
    and not the file mode as with os.OpenFile.
    
    Also handle syscall.EINTR and use syscall.ENOTSUP instead of custom
    string handling.
    
    Fixes #38950
    
    Change-Id: Ieed20d9ab5c8a49be51c9f9a42b7263f394a5261

--- src/cmd/link/internal/ld/outbuf_mmap.go
+++ src/cmd/link/internal/ld/outbuf_mmap.go
@@ -10,13 +10,18 @@ import (
 	"syscall"
 )
 
-func (out *OutBuf) Mmap(filesize uint64) error {
-	err := out.fallocate(filesize)
+func (out *OutBuf) Mmap(filesize uint64) (err error) {
+	for {
+		if err = out.fallocate(filesize); err != syscall.EINTR {
+			break
+		}
+	}
 	if err != nil {
 		// Some file systems do not support fallocate. We ignore that error as linking
 		// can still take place, but you might SIGBUS when you write to the mmapped
 		// area.
-		if err.Error() != fallocateNotSupportedErr {
+		errno, ok := err.(syscall.Errno)
+		if !ok || (errno != syscall.ENOTSUP && errno != syscall.EOPNOTSUPP) {
 			return err
 		}
 	}
