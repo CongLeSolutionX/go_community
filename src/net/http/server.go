4210eb21d3c49fbb394cc0e6ca30b75c330ff3fb
Andrew Gerrand <adg@golang.org>
2020-05-13 10:39:11 +1000

    net/http: handle Request.URL.RawPath in StripPrefix
    The StripPrefix wrapper strips a prefix string from the request's
    URL.Path field, but doesn't touch the RawPath field. This leads to the
    confusing situation when StripPrefix handles a request with URL.RawPath
    populated (due to some escaped characters in the request path) and the
    wrapped request's RawPath contains the prefix but Path does not.
    
    This change modifies StripPrefix to strip the prefix from both Path and
    RawPath. If there are escaped characters in the prefix part of the
    request URL the stripped handler serves a 404 instead of invoking the
    underlying handler with a mismatched Path/RawPath pair.
    
    This is a backward incompatible change for a very small minority of
    requests; I would be surprised if anyone is depending on this behavior,
    but it is possible. If that's the case, we could make a more
    conservative change where the RawPath is trimmed if possible, but when
    the prefix contains escaped characters then we don't 404 but rather send
    through the invalid Path/RawPath pair as before.
    
    Fixes #24366
    
    Change-Id: I7030b8c183a3dfce307bc0272bba9a18df4cfe08

--- src/net/http/server.go
+++ src/net/http/server.go
@@ -2072,12 +2072,15 @@ func StripPrefix(prefix string, h Handler) Handler {
 		return h
 	}
 	return HandlerFunc(func(w ResponseWriter, r *Request) {
-		if p := strings.TrimPrefix(r.URL.Path, prefix); len(p) < len(r.URL.Path) {
+		p := strings.TrimPrefix(r.URL.Path, prefix)
+		rp := strings.TrimPrefix(r.URL.RawPath, prefix)
+		if len(p) < len(r.URL.Path) && (r.URL.RawPath == "" || len(rp) < len(r.URL.RawPath)) {
 			r2 := new(Request)
 			*r2 = *r
 			r2.URL = new(url.URL)
 			*r2.URL = *r.URL
 			r2.URL.Path = p
+			r2.URL.RawPath = rp
 			h.ServeHTTP(w, r2)
 		} else {
 			NotFound(w, r)
