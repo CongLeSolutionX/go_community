f89c2e3db40d5d771732485586a47f87f8ac3cc9
diaxu01 <dianhong.xu@arm.com>
2020-05-20 08:49:59 +0000

    cmd/compile: optimize ARM64's code with REV16
    1. Fuse some patterns into rev16/rev16w instruction.
    
    Pattern1:
        (c & 0xff00ff00)>>8 | (c & 0x00ff00ff)<<8
    To:
        rev16w c
    
    Pattern2:
        (c & 0xff00ff00ff00ff00)>>8 | (c & 0x00ff00ff00ff00ff)<<8
    To:
        rev16 c
    
    2. Fuse shift, addition and store instructions into rev16 and store
    instructions.
    
    Pattern:
        b[0], b[1], b[2], b[3] = byte(w>>8), byte(w), byte(w>>24),byte(w>>16)
        b[4], b[5], b[6], b[7] = byte(w>>40), byte(w>>32), byte(w>>56), byte(w>>48)
    To:
       movd ptr (rev16 w) mem
    
    3. Benchmark improvement.
    name            old time/op        new time/op        delta
    pkg:cmd/compile/internal/ssa goos:linux goarch:arm64
    StoreRev16-224  35.300000ns +- 0%  17.200000ns +- 0%  -51.27%  (p=0.008 n=5+5)
    GenRev16D-224   58.400000ns +- 0%  30.800000ns +- 0%     ~     (p=0.079 n=4+5)
    GenRev16W-224   58.400000ns +- 0%  30.800000ns +- 0%  -47.26%  (p=0.000 n=5+4)
    [Geo mean]       49.377997ns        25.363545ns       -48.63%
    
    Change-Id: I7b8e4310177ead08df6ee640af31c578df727509

--- src/cmd/compile/internal/ssa/rewrite_test.go
+++ src/cmd/compile/internal/ssa/rewrite_test.go
@@ -36,3 +36,98 @@ func TestSubFlags(t *testing.T) {
 		t.Errorf("subFlags32(0,1).ult() returned false")
 	}
 }
+
+func genREV16D(c uint64) uint64 {
+	var b uint64
+	b = ((c & 0xff00ff00ff00ff00) >> 8) | ((c & 0x00ff00ff00ff00ff) << 8)
+	return b
+}
+
+func genREV16W(c uint32) uint32 {
+	var b uint32
+	b = ((c & 0xff00ff00) >> 8) | ((c & 0x00ff00ff) << 8)
+	return b
+}
+
+func TestREV16D(t *testing.T) {
+	var ref1 uint64 = 0x8f7f6f5f4f3f2f1f
+	var ref2 uint64 = 0x7f8f5f6f3f4f1f2f
+	var test1 uint64
+
+	test1 = genREV16D(ref2)
+	if test1 != ref1 {
+		t.Errorf("want %08x given %08x", ref1, test1)
+	}
+}
+
+func TestREV16W(t *testing.T) {
+	var ref1 uint32 = 0x4f3f2f1f
+	var ref2 uint32 = 0x3f4f1f2f
+	var test1 uint32
+
+	test1 = genREV16W(ref2)
+	if test1 != ref1 {
+		t.Errorf("want %#x given %#x", ref1, test1)
+	}
+}
+
+func storeREV16(u uint64, p []uint8, i int) {
+	s := p[i : i+8 : i+8]
+	s[0] = uint8(u >> 8)
+	s[1] = uint8(u)
+	s[2] = uint8(u >> 24)
+	s[3] = uint8(u >> 16)
+	s[4] = uint8(u >> 40)
+	s[5] = uint8(u >> 32)
+	s[6] = uint8(u >> 56)
+	s[7] = uint8(u >> 48)
+}
+
+func TestStoreREV16(t *testing.T) {
+	p := make([]uint8, 32)
+	want := [8]uint8{0x2f, 0x1f, 0x4f, 0x3f, 0x6f, 0x5f, 0x8f, 0x7f}
+	var ref uint64 = 0x8f7f6f5f4f3f2f1f
+
+	for i := 0; i <= 24; i = i + 8 {
+		storeREV16(ref, p, i)
+		for j, v := range want {
+			if v != p[i+j] {
+				t.Errorf("want %#x given %#x", v, p[i+j])
+			}
+		}
+	}
+}
+
+func storeSmall(p []uint8, ref uint64, count int) {
+	for i := 0; i < count; i = i + 8 {
+		storeREV16(ref, p, i)
+	}
+}
+
+func BenchmarkStoreRev16(b *testing.B) {
+	p := make([]uint8, 65)
+	var ref uint64 = 0x8f7f6f5f4f3f2f1f
+	for n := 0; n < b.N; n++ {
+		storeSmall(p, ref, 64)
+	}
+}
+
+func BenchmarkGenRev16D(b *testing.B) {
+	p := make([]uint64, 65)
+	var ref uint64 = 0x8f7f6f5f4f3f2f1f
+	for n := 0; n < b.N; n++ {
+		for i := 0; i < 65; i++ {
+			p[i] = genREV16D(ref)
+		}
+	}
+}
+
+func BenchmarkGenRev16W(b *testing.B) {
+	p := make([]uint32, 65)
+	var ref uint32 = 0x4f3f2f1f
+	for n := 0; n < b.N; n++ {
+		for i := 0; i < 65; i++ {
+			p[i] = genREV16W(ref)
+		}
+	}
+}
