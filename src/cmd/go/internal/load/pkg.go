597b44f07bc9741d325d167f4ad21ef083618187
Michael Matloob <matloob@golang.org>
2020-05-29 13:25:22 -0400

    cmd/go: assign missing import errors to importing package
    MissingImportErrors are created on the imported package, but they
    should instead be reported in the importing package. Those errors
    belong bettor on the importing package because it is the importer's
    "fault" that the package that they are importing doesn't exist,
    either because the import path doesn't exist at all, or because it
    doesn't exist in the module being imported.
    
    The error is created on the dummy imported package because it's determined
    that that package doesn't exist while the imported package is being
    built. To move the error to the correct package, we have it implement
    a method "ParentError" that returns an error that can be assigned
    to the parent. Then when the imports are being checked in internal/load.load,
    errors matching that interface can be bubbled up.
    
    One alternative to creating this interface could be to move the definition
    of ImportMissingError to internal/load, so that internal/load could
    use it without creating a package graph cycle between internal/load and
    internal/modload.
    
    Fixes #26909
    
    Change-Id: Id97451c03af193df9daffe2f7520e47bbd256c12

--- src/cmd/go/internal/load/pkg.go
+++ src/cmd/go/internal/load/pkg.go
@@ -1797,6 +1797,22 @@ func (p *Package) load(path string, stk *ImportStack, importPos []token.Position
 		}
 		p1 := LoadImport(path, p.Dir, p, stk, p.Internal.Build.ImportPos[path], ResolveImport)
 
+		// Set import pos to pos of import we're examining, to report errors on import
+		parentImportPos := importPos
+		importPos = p.Internal.Build.ImportPos[path]
+		// Check p1's error here because it should be reported on the importing package.
+		if p1.Error != nil {
+			if err, ok := p1.Error.Err.(interface {
+				ParentError() error
+				error
+			}); ok {
+				stk.Push(path) // Push the imported package onto the stack, so it shows on the error stack.
+				setError(err)
+				stk.Pop()
+			}
+		}
+		importPos = parentImportPos
+
 		path = p1.ImportPath
 		importPaths[i] = path
 		if i < len(p.Imports) {
