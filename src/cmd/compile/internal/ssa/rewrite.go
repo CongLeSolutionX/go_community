249df0c41742d4d7f5c09f6cf725b36cd09d4512
David Chase <drchase@google.com>
2020-06-12 13:48:26 -0400

    cmd/compile: introduce special ssa Aux type for calls
    This is prerequisite to moving call expansion later into SSA,
    anx probably a good idea anyway.  Passes tests.
    
    This is the first minimal CL that does a 1-for-1 substitution
    of *ssa.AuxCall for *obj.LSym.  Next step (next CL) is to make
    this change for all calls so that additional information can
    be stored in AuxCall.
    
    Change-Id: Ia3a7715648fd9fb1a176850767a726e6f5b959eb

--- src/cmd/compile/internal/ssa/rewrite.go
+++ src/cmd/compile/internal/ssa/rewrite.go
@@ -394,14 +394,13 @@ func canMergeLoad(target, load *Value) bool {
 }
 
 // symNamed reports whether sym's name is name.
-func symNamed(sym Sym, name string) bool {
-	return sym.String() == name
+func symNamed(call *AuxCall, name string) bool {
+	return call.Fn.String() == name
 }
 
-// isSameSym reports whether sym is the same as the given named symbol
-func isSameSym(sym interface{}, name string) bool {
-	s, ok := sym.(fmt.Stringer)
-	return ok && s.String() == name
+// isSameCall reports whether sym is the same as the given named symbol
+func isSameCall(sym interface{}, name string) bool {
+	return sym.(*AuxCall).Fn.String() == name
 }
 
 // nlz returns the number of leading zeros.
@@ -705,6 +704,9 @@ func auxToSym(i interface{}) Sym {
 func auxToType(i interface{}) *types.Type {
 	return i.(*types.Type)
 }
+func auxToCall(i interface{}) *AuxCall {
+	return i.(*AuxCall)
+}
 func auxToS390xCCMask(i interface{}) s390x.CCMask {
 	return i.(s390x.CCMask)
 }
@@ -716,6 +718,9 @@ func stringToAux(s string) interface{} {
 	return s
 }
 func symToAux(s Sym) interface{} {
+	return s
+}
+func callToAux(s *AuxCall) interface{} {
 	return s
 }
 func typeToAux(t *types.Type) interface{} {
@@ -742,7 +747,7 @@ func uaddOvf(a, b int64) bool {
 
 // de-virtualize an InterCall
 // 'sym' is the symbol for the itab
-func devirt(v *Value, sym Sym, offset int64) *obj.LSym {
+func devirt(v *Value, sym Sym, offset int64) *AuxCall {
 	f := v.Block.Func
 	n, ok := sym.(*obj.LSym)
 	if !ok {
@@ -756,7 +761,10 @@ func devirt(v *Value, sym Sym, offset int64) *obj.LSym {
 			f.Warnl(v.Pos, "couldn't de-virtualize call")
 		}
 	}
-	return lsym
+	if lsym == nil {
+		return nil
+	}
+	return &AuxCall{Fn: lsym}
 }
 
 // isSamePtr reports whether p1 and p2 point to the same address.
@@ -1376,7 +1384,7 @@ func registerizable(b *Block, typ *types.Type) bool {
 }
 
 // needRaceCleanup reports whether this call to racefuncenter/exit isn't needed.
-func needRaceCleanup(sym Sym, v *Value) bool {
+func needRaceCleanup(sym *AuxCall, v *Value) bool {
 	f := v.Block.Func
 	if !f.Config.Race {
 		return false
@@ -1390,7 +1398,7 @@ func needRaceCleanup(sym Sym, v *Value) bool {
 			case OpStaticCall:
 				// Check for racefuncenter will encounter racefuncexit and vice versa.
 				// Allow calls to panic*
-				s := v.Aux.(fmt.Stringer).String()
+				s := v.Aux.(*AuxCall).Fn.String()
 				switch s {
 				case "runtime.racefuncenter", "runtime.racefuncexit",
 					"runtime.panicdivide", "runtime.panicwrap",
