385cad57a1fdfb7ef448883a53c79c24989213a6
Michael Matloob <matloob@golang.org>
2020-06-18 11:58:46 -0400

    cmd/go: mark trace flows between actions
    This could help make it easier to identify blocking
    dependencies when examining traces. Flows can be turned
    off when viewing traces to remove potential distractions.
    
    Updates #38714
    
    Change-Id: I66ab00317f93afb762769707edd2d9233aa11669

--- src/cmd/go/internal/trace/trace.go
+++ src/cmd/go/internal/trace/trace.go
@@ -55,6 +55,34 @@ func Goroutine(ctx context.Context) context.Context {
 	return context.WithValue(ctx, traceKey{}, traceContext{tc.t, tc.t.getNextTID()})
 }
 
+// Flow marks a flow indicating that the 'to' span depends on the 'from' span.
+// Flow should be called while the 'to' span is in progress.
+func Flow(ctx context.Context, from *Span, to *Span) {
+	tc, ok := getTraceContext(ctx)
+	if !ok || from == nil || to == nil {
+		return
+	}
+
+	id := tc.t.getNextFlowID()
+	tc.t.writeEvent(&traceviewer.Event{
+		Name:     from.name + " -> " + to.name,
+		Category: "flow",
+		ID:       id,
+		Time:     float64(from.end.UnixNano()) / float64(time.Microsecond),
+		Phase:    "s",
+		TID:      from.tid,
+	})
+	tc.t.writeEvent(&traceviewer.Event{
+		Name:      from.name + " -> " + to.name,
+		Category:  "flow", // TODO(matloob): Add Category to Flow?
+		ID:        id,
+		Time:      float64(to.start.UnixNano()) / float64(time.Microsecond),
+		Phase:     "f",
+		TID:       to.tid,
+		BindPoint: "e",
+	})
+}
+
 type Span struct {
 	t *tracer
 
@@ -80,7 +108,8 @@ func (s *Span) Done() {
 type tracer struct {
 	evch chan *traceviewer.Event
 
-	nextTID uint64
+	nextTID    uint64
+	nextFlowID uint64
 }
 
 func (t *tracer) writeEvent(ev *traceviewer.Event) {
@@ -90,6 +119,10 @@ func (t *tracer) writeEvent(ev *traceviewer.Event) {
 func (t *tracer) getNextTID() uint64 {
 	// Subtract 1 to start numbering from zero.
 	return atomic.AddUint64(&t.nextTID, 1) - 1
+}
+
+func (t *tracer) getNextFlowID() uint64 {
+	return atomic.AddUint64(&t.nextFlowID, 1)
 }
 
 // traceKey is the context key for tracing information. It is unexported to prevent collisions with context keys defined in
