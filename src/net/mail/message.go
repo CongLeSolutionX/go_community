7773133984897c8d7e0d53fb30bcf40809ece5c5
Constantin Konstantinidis <constantinkonstantinidis@gmail.com>
2020-06-26 09:53:01 +0200

    net/mail: add comment on CFWS support
    Updates #22661
    
    Change-Id: I66e96b551e3dda677bcefa2dc40adb3a110cb17b

--- src/net/mail/message.go
+++ src/net/mail/message.go
@@ -10,7 +10,7 @@ extended by RFC 6532.
 Notable divergences:
 	* Obsolete address formats are not parsed, including addresses with
 	  embedded route information.
-	* The full range of spacing (the CFWS syntax element) is not supported,
+	* The full range of spacing (the CFWS syntax element) is not supported in obsolete addresses,
 	  such as breaking addresses across lines.
 	* No unicode normalization is performed.
 	* The special characters ()[]:;@\, are allowed to appear unquoted in names.
