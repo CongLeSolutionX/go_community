97937751af4fcd12cd69c5eabc1f42722e615135
Russ Cox <rsc@golang.org>
2020-06-29 10:19:22 -0400

    cmd/vet: look at ignored files in buildtag check
    DO NOT REVIEW - must be made in the real x/tools and vendored back
    
    The buildtag check only looks at files for which the +build constraints pass.
    A malformed constraint like // +build !! will never pass.
    The original vet reported problems like this, but when vet moved
    to the new analysis framework, it lost this ability - there's a TODO.
    
    This CL fixes the long-standing TODO, changing the check to look
    at ignored source files as well as selected ones.
    
    For Go 1.16.
    
    Change-Id: Ic9d13b66652f5bd69aaabfd73f3f59a0eebc3fd6

--- src/cmd/vendor/golang.org/x/tools/go/analysis/passes/buildtag/buildtag.go
+++ src/cmd/vendor/golang.org/x/tools/go/analysis/passes/buildtag/buildtag.go
@@ -33,6 +33,11 @@ func runBuildTag(pass *analysis.Pass) (interface{}, error) {
 			return nil, err
 		}
 	}
+	for _, name := range pass.IgnoredFiles {
+		if err := checkOtherFile(pass, name); err != nil {
+			return nil, err
+		}
+	}
 	return nil, nil
 }
 
@@ -132,13 +137,6 @@ func checkLine(line string, pastCutoff bool) error {
 }
 
 func checkArguments(fields []string) error {
-	// The original version of this checker in vet could examine
-	// files with malformed build tags that would cause the file to
-	// be always ignored by "go build". However, drivers for the new
-	// analysis API will analyze only the files selected to form a
-	// package, so these checks will never fire.
-	// TODO(adonovan): rethink this.
-
 	for _, arg := range fields[1:] {
 		for _, elem := range strings.Split(arg, ",") {
 			if strings.HasPrefix(elem, "!!") {
