4710f93e879e5bf976160da7eb100692b89b012e
Michael Anthony Knyszek <mknyszek@google.com>
2020-07-23 21:02:05 +0000

    runtime: make distributed/local malloc stats the source-of-truth
    This change makes it so that various local malloc stats (excluding
    heap_scan and local_tinyallocs) are no longer written first to mheap
    fields but are instead accessed directly from each mcache.
    
    This change is part of a move toward having stats be distributed, and
    cleaning up some old code related to the stats.
    
    Note that because there's no central source-of-truth, when an mcache
    dies, it must donate its stats to another mcache. It's always safe to
    donate to the mcache for the 0th P, so do that.
    
    Change-Id: I2556093dbc27357cb9621c9b97671f3c00aa1173

--- src/runtime/proc.go
+++ src/runtime/proc.go
@@ -4293,7 +4293,7 @@ func (pp *p) destroy() {
 		pp.mspancache.len = 0
 		pp.pcache.flush(&mheap_.pages)
 	})
-	freemcache(pp.mcache)
+	freemcache(pp.mcache, allp[0].mcache)
 	pp.mcache = nil
 	gfpurge(pp)
 	traceProcFree(pp)
