4cf954c34a914c355f4cfa88b7128aad4106e713
Bryan C. Mills <bcmills@google.com>
2020-07-24 17:43:55 -0400

    cmd/go/internal/modload: drop requirements on excluded versions
    Previously, when we encountered an excluded version in any module's
    requirements, we would resolve it to the next higher version.
    Unfortunately, the meaning of “the next higher version” can change
    over time.
    
    Moreover, users who use 'exclude' directives normally either already
    require some higher version (using the 'exclude' directive to prune
    out invalid requirements from some intermediate version), or already
    require some lower version (using the 'exclude' directive to prevent
    'go get -u' from upgrading to a known-bad version). In both of these
    cases, resolving an upgrade for the excluded version is needless work
    even in the best case: it adds work for the 'go' command when there is
    already a perfectly usable selected version of the module in the
    requirement graph.
    
    Instead, we now interpret the 'exclude' directive as dropping all
    references to the excluded version.
    
    This implements the approach described in
    https://golang.org/issue/36465#issuecomment-572694990.
    
    Fixes #36465
    Updates #36460
    
    Change-Id: Ibf0187daced417b4cc23b97125826778658e4b0f

--- src/cmd/go/internal/modload/init.go
+++ src/cmd/go/internal/modload/init.go
@@ -386,8 +386,8 @@ func InitMod() {
 		legacyModInit()
 	}
 
-	modFileToBuildList()
 	setDefaultBuildMod()
+	modFileToBuildList()
 	if cfg.BuildMod == "vendor" {
 		readVendorList()
 		checkVendorConsistency()
@@ -465,7 +465,15 @@ func modFileToBuildList() {
 
 	list := []module.Version{Target}
 	for _, r := range modFile.Require {
-		list = append(list, r.Mod)
+		if index != nil && index.exclude[r.Mod] {
+			if cfg.BuildMod == "mod" {
+				fmt.Fprintf(os.Stderr, "go: dropping requirement on excluded version %s %s\n", r.Mod.Path, r.Mod.Version)
+			} else {
+				fmt.Fprintf(os.Stderr, "go: ignoring requirement on excluded version %s %s\n", r.Mod.Path, r.Mod.Version)
+			}
+		} else {
+			list = append(list, r.Mod)
+		}
 	}
 	buildList = list
 }
