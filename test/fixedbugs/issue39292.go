46c264212de50df256203208ee0c6ac06380885f
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2020-07-27 12:08:56 +0700

    cmd/compile: re-check escape info when initializing wrapper struct of closure
    When generating wrapper struct for closure, we inherit escape info from
    the closure itself. This can lead to wrong escaping decision. The
    genearted struct has the form:
    
    	clos = &struct{F uintptr; R T}{M.T·f, x}
    
    for x.M with x has type T. Here, x.M can be stack allocated, but the
    clos may be not. We need to double check whether clos must be heap
    allocated or not and set the escape class correctly.
    
    Fixes #39292
    
    Change-Id: I14c1e591681d9d92317bb2396d6cf5207aa93e08

---
+++ test/fixedbugs/issue39292.go
@@ -0,0 +1,19 @@
+// compile
+
+// Copyright 2020 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+type t struct {
+	b [10000]int
+}
+
+func (t) f() {
+}
+
+func main() {
+	x := t{}.f
+	x()
+}
