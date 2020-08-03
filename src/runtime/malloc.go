d429e9cc49b6de1785198da6dccbae1cb75e8a35
Michael Anthony Knyszek <mknyszek@google.com>
2020-08-03 20:08:25 +0000

    runtime: add helper for getting an mcache in allocation contexts
    This change adds a function getMCache which returns the current P's
    mcache if it's available, and otherwise tries to get mcache0 if we're
    bootstrapping. This function will come in handy as we need to replicate
    this behavior in multiple places in future changes.
    
    Change-Id: I536073d6f6dc6c6390269e613ead9f8bcb6e7f98

--- src/runtime/malloc.go
+++ src/runtime/malloc.go
@@ -965,19 +965,7 @@ func mallocgc(size uintptr, typ *_type, needzero bool) unsafe.Pointer {
 
 	shouldhelpgc := false
 	dataSize := size
-	var c *mcache
-	if mp.p != 0 {
-		c = mp.p.ptr().mcache
-	} else {
-		// We will be called without a P while bootstrapping,
-		// in which case we use mcache0, which is set in mallocinit.
-		// mcache0 is cleared when bootstrapping is complete,
-		// by procresize.
-		c = mcache0
-		if c == nil {
-			throw("malloc called with no P")
-		}
-	}
+	c := getMCache()
 	var span *mspan
 	var x unsafe.Pointer
 	noscan := typ == nil || typ.ptrdata == 0
