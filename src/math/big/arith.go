35283f477b164e84ec46771f198be9570d80014a
SparrowLii <liyuancylx@gmail.com>
2020-08-25 16:33:50 +0800

    math/big: treble the speed of divWVW and optimize the div of nats by replacing the division with multiplicative inverse
    Division is much slower than multiplication, as we know. The algorithm for dividing 128 digits by 64 digits in math/bits.go/Div64, which has a certain impact on the efficiency of nats division, requires two divisions. And the method of using multiplication by multiplicative inverse and replacing division with it can increase the speed of divWVW algorithm by three times, and at the same time increase the speed of nats division.The benchmark test data is as follows.
    
    name                     old time/op    new time/op    delta
    DivWVW/1-4                 13.1ns ± 4%    13.3ns ± 4%      ~     (p=0.444 n=5+5)
    DivWVW/2-4                 48.6ns ± 1%    51.2ns ± 2%    +5.39%  (p=0.008 n=5+5)
    DivWVW/3-4                 82.0ns ± 1%    69.7ns ± 1%   -15.03%  (p=0.008 n=5+5)
    DivWVW/4-4                  116ns ± 1%      71ns ± 2%   -38.88%  (p=0.008 n=5+5)
    DivWVW/5-4                  152ns ± 1%      84ns ± 4%   -44.70%  (p=0.008 n=5+5)
    DivWVW/10-4                 319ns ± 1%     155ns ± 4%   -51.50%  (p=0.008 n=5+5)
    DivWVW/100-4               3.44µs ± 3%    1.30µs ± 8%   -62.30%  (p=0.008 n=5+5)
    DivWVW/1000-4              33.8µs ± 0%    10.9µs ± 1%   -67.74%  (p=0.008 n=5+5)
    DivWVW/10000-4              343µs ± 4%     111µs ± 5%   -67.63%  (p=0.008 n=5+5)
    DivWVW/100000-4            3.35ms ± 1%    1.25ms ± 3%   -62.79%  (p=0.008 n=5+5)
    QuoRem-4                   3.08µs ± 2%    2.21µs ± 4%   -28.40%  (p=0.008 n=5+5)
    ModSqrt225_Tonelli-4        444µs ± 2%     457µs ± 3%      ~     (p=0.095 n=5+5)
    ModSqrt225_3Mod4-4          136µs ± 1%     138µs ± 3%      ~     (p=0.151 n=5+5)
    ModSqrt231_Tonelli-4        473µs ± 3%     483µs ± 4%      ~     (p=0.548 n=5+5)
    ModSqrt231_5Mod8-4          164µs ± 9%     169µs ±12%      ~     (p=0.421 n=5+5)
    Sqrt-4                     36.8µs ± 1%    28.6µs ± 0%   -22.17%  (p=0.016 n=5+4)
    Div/20/10-4                50.0ns ± 3%    51.3ns ± 6%      ~     (p=0.238 n=5+5)
    Div/40/20-4                49.8ns ± 2%    51.3ns ± 6%      ~     (p=0.222 n=5+5)
    Div/100/50-4               85.8ns ± 4%    86.5ns ± 5%	   ~     (p=0.246 n=5+5)
    Div/200/100-4               335ns ± 3%     296ns ± 2%   -11.60%  (p=0.008 n=5+5)
    Div/400/200-4               442ns ± 2%     359ns ± 5%   -18.81%  (p=0.008 n=5+5)
    Div/1000/500-4              858ns ± 3%     643ns ± 6%   -25.06%  (p=0.008 n=5+5)
    Div/2000/1000-4            1.70µs ± 3%    1.28µs ± 4%   -24.80%  (p=0.008 n=5+5)
    Div/20000/10000-4          45.0µs ± 5%    41.8µs ± 4%    -7.17%  (p=0.016 n=5+5)
    Div/200000/100000-4        1.51ms ± 7%    1.43ms ± 3%    -5.42%  (p=0.016 n=5+5)
    Div/2000000/1000000-4      57.6ms ± 4%    57.5ms ± 3%      ~     (p=1.000 n=5+5)
    Div/20000000/10000000-4     2.08s ± 3%     2.04s ± 1%      ~     (p=0.095 n=5+5)
    
    name                     old speed      new speed      delta
    DivWVW/1-4               4.87GB/s ± 4%  4.80GB/s ± 4%      ~     (p=0.310 n=5+5)
    DivWVW/2-4               2.63GB/s ± 1%  2.50GB/s ± 2%    -5.07%  (p=0.008 n=5+5)
    DivWVW/3-4               2.34GB/s ± 1%  2.76GB/s ± 1%   +17.70%  (p=0.008 n=5+5)
    DivWVW/4-4               2.21GB/s ± 1%  3.61GB/s ± 2%   +63.42%  (p=0.008 n=5+5)
    DivWVW/5-4               2.10GB/s ± 2%  3.81GB/s ± 4%   +80.89%  (p=0.008 n=5+5)
    DivWVW/10-4              2.01GB/s ± 0%  4.13GB/s ± 4%  +105.91%  (p=0.008 n=5+5)
    DivWVW/100-4             1.86GB/s ± 2%  4.95GB/s ± 7%  +165.63%  (p=0.008 n=5+5)
    DivWVW/1000-4            1.89GB/s ± 0%  5.86GB/s ± 1%  +209.96%  (p=0.008 n=5+5)
    DivWVW/10000-4           1.87GB/s ± 4%  5.76GB/s ± 5%  +208.96%  (p=0.008 n=5+5)
    DivWVW/100000-4          1.91GB/s ± 1%  5.14GB/s ± 3%  +168.85%  (p=0.008 n=5+5)
    
    Change-Id: I049f1196562b20800e6ef8a6493fd147f93ad830

--- src/math/big/arith.go
+++ src/math/big/arith.go
@@ -60,12 +60,6 @@ func nlz(x Word) uint {
 	return uint(bits.LeadingZeros(uint(x)))
 }
 
-// q = (u1<<_W + u0 - r)/v
-func divWW_g(u1, u0, v Word) (q, r Word) {
-	qq, rr := bits.Div(uint(u1), uint(u0), uint(v))
-	return Word(qq), Word(rr)
-}
-
 // The resulting carry c is either 0 or 1.
 func addVV_g(z, x, y []Word) (c Word) {
 	// The comment near the top of this file discusses this for loop condition.
@@ -207,10 +201,142 @@ func addMulVVW_g(z, x []Word, y Word) (c Word) {
 	return
 }
 
-func divWVW_g(z []Word, xn Word, x []Word, y Word) (r Word) {
-	r = xn
-	for i := len(z) - 1; i >= 0; i-- {
-		z[i], r = divWW_g(r, x[i], y)
+// q = ( x1 << _W + x0 - r)/y. Ensure x1<y to avoid overflow.
+// inv = floor(( _B^2 - 1 ) / d - _B). d = y << nlz(y).
+// From now on we equate _B with B, X = x << nlz(y), realQ = x / y = X / d, x1=X/B, x0=X mod B
+//
+// Q1: Why inv needs to subtract B?
+// A1: Because B/2 <= d <= B-1, so B + 1 <= ( B^2 - 1) / d <= 2 * (B^2 - 1) / B ==> 1 <= ( B^2 - 1) / d - B <= ( B^2 - 2) / B
+// 		==> 1 <= inv <= B-1. We can just put inv into an uint.
+//
+// Q2: How to get quotient use inv?
+// A2: We calculate (qq,q0) = floor(X/B)*inv+X.
+//	  Let realQ = X / d = (B*x1 + x0)/d. We want floor(realQ).
+//	  So  (qq,q0) <= X/B*(B^2-1)/d-B*X/B+X = realQ*(B-1/B) <= realQ*B. So qq <= realQ.
+//	  Meanwhile (qq,q0) >= (X/B-1)*((B^2-1)/d-B-1) = realQ*((B^2-1)/B)-X/B-(B^2-1)/d+B+1 = realQ*B-realQ/B-X/B-(B^2-1)/d+B+1
+//		because reaqQ/B<1, X/B<B, (B^2-1)/d <= 2*(B^2-1)/B <= 2*B
+//		So (qq,q0) >= realQ*B-1-B-2*B+B+1 = (realQ-2)*B. So qq >= realQ-2
+//	  Then we run qq++. Let realQ-1 <= qq <= realQ+1
+//
+// Q3: Why and how should we adjust qq to make it the quotient we want?
+// A3: First calculate rr = x0<< - d*qq = (B*x1+x0)-d*qq mod B.(qq has plus one). Note that rr doesn't necessarily represent the remainder.
+//	  Because floor(realQ)-1 = floor(realQ-1), floor(realQ)+1 = floor(realQ+1)
+//	  So floor(realQ)-1 <= qq <= floor(realQ)+1
+//	  (1) if qq=floor(realQ)+1, real Remainder -d<= r < 0. Next we prove rr >= q0:
+//			Prove1: because -d<= r <0
+//					so rr = ( B*x1 + x0) - d*qq +B
+//					because q0 = inv*x1 + B*x1 +x0 +B- B*qq, and qq=(B*x1+x0-r)/d
+//					so rr-q0=B*x1 - d*qq - inv*x1 + B*qq >= (B-d)*(B*x1+x0-r)/d+(Bd-B^2-d)/d*x1
+//							=[x1+(B-d)(x0-r)]/d.
+//					because B-d>=0,r<0, so rr>=q0.
+//		  so we adjust qq--, rr+=d. as B> rr >=B-d, rr+d mod B <d, we get the floor(realQ)
+//	  (2) if qq=floor(realQ), real Remainder 0<= r < d. Next we prove when rr=r >= q0, rr+d <B:
+//			prove2: rr = B*x1 +x0 -qq*d, q0=inv*x1 + B*x1 +x0 +B- B*qq,rr>=q0
+//					let B^2-1 = qd*d + rd, so we can get qq*(B-d)-(qd-B)*x1-B>=0
+//					because qq=(B*x1+x0-r)/d
+//					so (B*x1+x0-r)(B-d)-(B^2-1-rd-Bd)*x1-Bd>=0 ==> (1+rd)*x1+(B-d)(x0-r)>=Bd
+//						==> r<= x0-[Bd-(1+rd)*x1]/(B-d)
+//					so r+d<=B <== (x0+d)(B-d)+(1+rd)*x1<B^2
+//					because 1+rd<=d,x1<d, so (1+rd)*x1<d^2
+//					so <== (B-d)x0+Bd<B^2 <== B^2-B+d <B^2 <== d < B
+//			so when r<q0, we adjust nothing. When r>=q0, first adjust q--, then adjust q++, we get the floor(realQ).
+//	  (3) if qq=floor(realQ)-1, we prove real Remainder can only be [d,B), can't be [B, 2b):
+//			prove3: r = B*x1+x0-qq*d < B ==> qq > (B*x1+x0-B)/d
+//					let B^2-1 = qd*d +rd, then qq = (x1*qd +x0 +B -q0)/B
+//					so qq > (B*x1+x0-B)/d <==  x1*(B^2-1-rd)+x0d+Bd-q0d-B^2*x1-B*x0+B^2>0
+//					<== B^2-(B-d)x0-(1+rd)*x1+Bd-q0*d>0
+//					because x0<B, (1+rd)*x1<d^2
+//					so <== 2B*d -d^2 -q0*d>0 <== 2B > d+q0. prove over.
+//			so when qq=floor(realQ)-1, we just adjust qq++, then get the floor(realQ).
+func divWW(x1, x0, y Word, inv uint) (q, r Word) {
+	shift := nlz(y)
+	if shift != 0 {
+		x1 = (x1<<shift | x0>>(_W-shift))
+		x0 <<= shift
+		y <<= shift
 	}
+	d := uint(y)
+	// refers to Q2
+	qq, q0 := bits.Mul(uint(x1), inv)
+	q0, cc := bits.Add(q0, uint(x0), 0)
+	qq, _ = bits.Add(qq, uint(x1), cc)
+	qq++
+
+	//  refers to Q3.
+	rr := uint(x0) - d*qq
+
+	if rr >= q0 {
+		qq--
+		rr += d
+	}
+
+	if rr >= d {
+		qq++
+		rr -= d
+	}
+	rr >>= shift
+	return Word(qq), Word(rr)
+}
+
+func divWVW(z []Word, xn Word, x []Word, y Word) (r Word) {
+	r = xn
+	if len(x) == 1 {
+		qq, rr := bits.Div(uint(r), uint(x[0]), uint(y))
+		z[0] = Word(qq)
+		return Word(rr)
+	}
+	inv := getInvert(y)
+	for i := len(z) - 1; i >= 0; i-- {
+		z[i], r = divWW(r, x[i], y, inv)
+	}
+	return r
+}
+
+// getInvert return the inverse of the divisor. inv = floor(( _B^2 - 1 ) / u1 - _B). u1 = d1 << nlz(d1).
+func getInvert(d1 Word) (inv uint) {
+	// ( _B^2 - 1) / u1 - _B = ( _B * ^u1 + _M) / u1.
+	const halfSize = _W >> 1
+	const mask = uint((1 << halfSize) - 1)
+	shift := nlz(d1)
+	u1 := uint(d1) << shift
+	ul := u1 & mask      // low bits of u1
+	uh := u1 >> halfSize // high bits of u1
+
+	qh := ^u1 / uh // high bits of the quotient, which is floor((( _B ^ 1/2) * ^u1 + mask) / u1). similar with bits.go:533
+
+	r := ((^u1 - qh*uh) << halfSize) | mask // let b = ( _B ^ 1/2) , the remainder r =  b * (^u) + b-1 - qh * (b * uh + ul) = b (^u - qh * uh) + b-1 - qh * ul
+
+	// conditionally adjust qh to ensure that the remainder is positive.
+	// As u1 = d1 << nlz(d1), 2 * u1 >= _B > qh * ul, we need to subtract one from qh at most twice.
+	p := qh * ul
+	if r < p {
+		qh--
+		r += u1
+		if r >= u1 { // if no overflow occurred
+			if r < p {
+				qh--
+				r += u1
+			}
+		}
+	}
+	r -= p
+
+	// low half of quotient, which is floor((( _B ^ 1/2) * r +mask) / u1).
+	p = (r>>halfSize)*qh + r  // the trick that qh is a suitable inverse of uh now. same as p = ( r / uh) << halfSize (bits.go:544)
+	ql := (p >> halfSize) + 1 // only need the high bits of p, plus one to ensure ql not less than real ql.
+
+	// conditionally adjust ql, similar with proves in divWW.
+	r = (r << halfSize) + mask - ql*u1
+
+	if r >= (p << halfSize) {
+		ql--
+		r += u1
+	}
+	if r >= u1 {
+		ql++
+		r -= u1
+	}
+
+	inv = (qh << halfSize) + ql
 	return
 }
