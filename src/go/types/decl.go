8eaa391bf360f7f6b33295ba111ce08ac512ec17
Robert Griesemer <gri@golang.org>
2020-08-25 21:58:37 -0700

    [dev.go2go] go/types: disallow empty type parameter lists
    When using unified parameter list syntax, an empty type parameter
    list in a type declaration is impossible (it means a slice type).
    For symmetry, and because it doesn't make sense to support it,
    disallow empty type parameter lists in function declarations.
    
    Change-Id: Ifc783245a54d276eb22232a7a74be4d47a7d33fe

--- src/go/types/decl.go
+++ src/go/types/decl.go
@@ -603,10 +603,15 @@ func (check *Checker) typeDecl(obj *TypeName, tdecl *ast.TypeSpec, def *Named) {
 }
 
 func (check *Checker) collectTypeParams(list *ast.FieldList) (tparams []*TypeName) {
+	// Type parameter lists must not be empty.
+	if list.NumFields() == 0 {
+		check.errorf(list.Closing, "empty type parameter list")
+		return
+	}
+
 	// Declare type parameters up-front, with empty interface as type bound.
-	// If we use interfaces as type bounds, the scope of type parameters starts at
-	// the beginning of the type parameter list (so we can have mutually recursive
-	// parameterized interfaces).
+	// The scope of type parameters starts at the beginning of the type parameter
+	// list (so we can have mutually recursive parameterized interfaces).
 	for _, f := range list.List {
 		tparams = check.declareTypeParams(tparams, f.Names)
 	}
