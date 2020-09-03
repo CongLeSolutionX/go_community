vedantroy/go

[dev.link] implement wasmimport directive
Go programs can now use the //go:wasmimport [module name] [function
name] directive to import functions from the Web Assembly runtime.

Fixes #38248

Change-Id: I27ea4b6f240f59f1a2286eed5652ed1c528cabe3

src/cmd/compile/internal/gc/gsubr.go
@@ -33,6 +33,7 @@ package gc
 import (
 	"cmd/compile/internal/ssa"
 	"cmd/compile/internal/types"
+	"cmd/internal/goobj"
 	"cmd/internal/obj"
 	"cmd/internal/objabi"
 	"cmd/internal/src"
@@ -271,6 +272,27 @@ func (f *Func) initLSym(hasBody bool) {
 			asym.SetABI(aliasABI)
 			asym.Set(obj.AttrDuplicateOK, true)
 			Ctxt.ABIAliases = append(Ctxt.ABIAliases, asym)
+		}
+	}
+
+	wi := f.wasmimport
+	if wi != nil && objabi.GOARCH == "wasm" {
+		if f.lsym.Func == nil {
+			f.lsym.Func = &obj.FuncInfo{}
+		}
+		if wi.module == "go" {
+			f.lsym.Func.WasmImport = &goobj.WasmImport{
+				Module: wi.module,
+				Name:   wi.name,
+				Params: []goobj.WasmField{{Type: goobj.WasmI32}},
+			}
+		} else {
+			f.lsym.Func.WasmImport = &goobj.WasmImport{
+				Module:  wi.module,
+				Name:    wi.name,
+				Params:  f.wasmfields.Params,
+				Results: f.wasmfields.Results,
+			}
 		}
 	}
 

src/cmd/compile/internal/gc/noder.go
@@ -15,6 +15,7 @@ import (
 
 	"cmd/compile/internal/syntax"
 	"cmd/compile/internal/types"
+	"cmd/internal/goobj"
 	"cmd/internal/obj"
 	"cmd/internal/objabi"
 	"cmd/internal/src"
@@ -511,12 +512,19 @@ func (p *noder) funcDecl(fun *syntax.FuncDecl) *Node {
 	f.Func.Nname.Name.Defn = f
 	f.Func.Nname.Name.Param.Ntype = t
 
+	isWasmImport := false
 	if pragma, ok := fun.Pragma.(*Pragma); ok {
 		f.Func.Pragma = pragma.Flag & FuncPragmas
 		if pragma.Flag&Systemstack != 0 && pragma.Flag&Nosplit != 0 {
 			yyerrorl(f.Pos, "go:nosplit and go:systemstack cannot be combined")
 		}
 		pragma.Flag &^= FuncPragmas
+		if pragma.wasmimport != nil {
+			isWasmImport = true
+			f.Func.Pragma |= Noescape
+			f.Func.Pragma |= Nosplit
+			f.Func.wasmimport = pragma.wasmimport
+		}
 		p.checkUnused(pragma)
 	}
 
@@ -530,6 +538,9 @@ func (p *noder) funcDecl(fun *syntax.FuncDecl) *Node {
 		if f.Func.Pragma&Noescape != 0 {
 			yyerrorl(f.Pos, "can only use //go:noescape with external func implementations")
 		}
+		if isWasmImport {
+			yyerrorl(f.Pos, "cannot have function body with //go:wasmimport")
+		}
 	} else {
 		if pure_go || strings.HasPrefix(f.funcname(), "init.") {
 			// Linknamed functions are allowed to have no body. Hopefully
@@ -541,7 +552,7 @@ func (p *noder) funcDecl(fun *syntax.FuncDecl) *Node {
 					break
 				}
 			}
-			if !isLinknamed {
+			if !isLinknamed && !isWasmImport {
 				yyerrorl(f.Pos, "missing function body")
 			}
 		}
@@ -1502,8 +1513,19 @@ var allowedStdPragmas = map[string]bool{
 
 // *Pragma is the value stored in a syntax.Pragma during parsing.
 type Pragma struct {
-	Flag PragmaFlag  // collected bits
-	Pos  []PragmaPos // position of each individual flag
+	Flag       PragmaFlag  // collected bits
+	Pos        []PragmaPos // position of each individual flag
+	wasmimport *wasmimport
+}
+
+type wasmimport struct {
+	module string
+	name   string
+}
+
+type wasmfields struct {
+	Results []goobj.WasmField
+	Params  []goobj.WasmField
 }
 
 type PragmaPos struct {
@@ -1552,6 +1574,17 @@ func (p *noder) pragma(pos syntax.Pos, blankLine bool, text string, old syntax.P
 	}
 
 	switch {
+	case strings.HasPrefix(text, "go:wasmimport"):
+		f := strings.Fields(text)
+		if len(f) != 3 {
+			p.error(syntax.Error{Pos: pos, Msg: "usage: //go:wasmimport [module name] [import name]"})
+		}
+		module := f[1]
+		name := f[2]
+		pragma.wasmimport = &wasmimport{
+			module: module,
+			name:   name,
+		}
 	case strings.HasPrefix(text, "go:linkname "):
 		f := strings.Fields(text)
 		if !(2 <= len(f) && len(f) <= 3) {

src/cmd/compile/internal/gc/pgen.go
@@ -8,6 +8,7 @@ import (
 	"cmd/compile/internal/ssa"
 	"cmd/compile/internal/types"
 	"cmd/internal/dwarf"
+	"cmd/internal/goobj"
 	"cmd/internal/obj"
 	"cmd/internal/objabi"
 	"cmd/internal/src"
@@ -207,7 +208,17 @@ func funccompile(fn *Node) {
 	// assign parameter offsets
 	dowidth(fn.Type)
 
-	if fn.Nbody.Len() == 0 {
+	wi := fn.Func.wasmimport
+	hasWasmImport := wi != nil && objabi.GOARCH == "wasm"
+
+	if hasWasmImport && wi.module != "go" {
+		fn.Func.wasmfields = &wasmfields{
+			Params:  toWasmFields(fn.Type.Params().FieldSlice()),
+			Results: toWasmFields(fn.Type.Results().FieldSlice()),
+		}
+	}
+
+	if fn.Nbody.Len() == 0 && !hasWasmImport {
 		// Initialize ABI wrappers if necessary.
 		fn.Func.initLSym(false)
 		emitptrargsmap(fn)
@@ -794,3 +805,25 @@ type symByName []*types.Sym
 func (a symByName) Len() int           { return len(a) }
 func (a symByName) Less(i, j int) bool { return a[i].Name < a[j].Name }
 func (a symByName) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func toWasmFields(fields []*types.Field) []goobj.WasmField {
+	wfs := make([]goobj.WasmField, len(fields))
+	for i, f := range fields {
+		t := f.Type
+		switch {
+		case t.IsInteger() && t.Width == 4:
+			wfs[i].Type = goobj.WasmI32
+		case t.IsInteger() && t.Width == 8:
+			wfs[i].Type = goobj.WasmI64
+		case t.IsFloat() && t.Width == 4:
+			wfs[i].Type = goobj.WasmF32
+		case t.IsFloat() && t.Width == 8:
+			wfs[i].Type = goobj.WasmF64
+		case t.IsPtr():
+			wfs[i].Type = goobj.WasmPtr
+		default:
+			Fatalf("wasm import has bad function signature")
+		}
+		wfs[i].Offset = f.Offset
+	}
+	return wfs
+}

src/cmd/internal/obj/wasm/wasmobj.go
@@ -6,6 +6,7 @@ package wasm
 
 import (
 	"bytes"
+	"cmd/internal/goobj"
 	"cmd/internal/obj"
 	"cmd/internal/objabi"
 	"cmd/internal/sys"
@@ -100,7 +101,6 @@ var unaryDst = map[obj.As]bool{
 	ATee:          true,
 	ACall:         true,
 	ACallIndirect: true,
-	ACallImport:   true,
 	ABr:           true,
 	ABrIf:         true,
 	ABrTable:      true,
@@ -188,6 +188,74 @@ func preprocess(ctxt *obj.Link, s *obj.LSym, newprog obj.ProgAlloc) {
 	}
 	s.Func.Args = s.Func.Text.To.Val.(int32)
 	s.Func.Locals = int32(framesize)
+
+	wi := s.Func.WasmImport
+	if wi != nil {
+		/*
+					   Functions generated from //go:wasmimport have garbage in their bodies
+			           for some reason. Discard the garbage, expect for the first instruction (TEXT).
+		*/
+		p := s.Func.Text
+		p.Link = nil
+		to := obj.Addr{
+			Type: obj.TYPE_MEM,
+			Name: obj.NAME_EXTERN,
+			Sym:  s,
+		}
+		if wi.Module == "go" {
+			p = appendp(p, AGet, regAddr(REG_SP))
+			p = appendp(p, ACall, to)
+			p.Mark = WasmImport
+		} else {
+			if len(wi.Results) > 1 {
+				panic("invalid results type") // impossible until multi-value proposal has landed
+			}
+			if len(wi.Results) == 1 {
+				p = appendp(p, AGet, regAddr(REG_SP)) // address has to be before the value
+			}
+			for _, f := range wi.Params {
+				p = appendp(p, AGet, regAddr(REG_SP))
+				f.Offset += 8
+				switch f.Type {
+				case goobj.WasmI32:
+					p = appendp(p, AI32Load, constAddr(f.Offset))
+				case goobj.WasmI64:
+					p = appendp(p, AI64Load, constAddr(f.Offset))
+				case goobj.WasmF32:
+					p = appendp(p, AF32Load, constAddr(f.Offset))
+				case goobj.WasmF64:
+					p = appendp(p, AF64Load, constAddr(f.Offset))
+				case goobj.WasmPtr:
+					p = appendp(p, AI64Load, constAddr(f.Offset))
+					p = appendp(p, AI32WrapI64)
+				default:
+					panic("bad param type")
+				}
+			}
+			p = appendp(p, ACall, to)
+			p.Mark = WasmImport
+			if len(wi.Results) == 1 {
+				f := wi.Results[0]
+				f.Offset += 8
+				switch f.Type {
+				case goobj.WasmI32:
+					p = appendp(p, AI32Store, constAddr(f.Offset))
+				case goobj.WasmI64:
+					p = appendp(p, AI64Store, constAddr(f.Offset))
+				case goobj.WasmF32:
+					p = appendp(p, AF32Store, constAddr(f.Offset))
+				case goobj.WasmF64:
+					p = appendp(p, AF64Store, constAddr(f.Offset))
+				case goobj.WasmPtr:
+					p = appendp(p, AI64ExtendI32U)
+					p = appendp(p, AI64Store, constAddr(f.Offset))
+				default:
+					panic("bad result type")
+				}
+			}
+		}
+		p = appendp(p, obj.ARET)
+	}
 
 	if s.Func.Text.From.Sym.Wrapper() {
 		// if g._panic != nil && g._panic.argp == FP {
@@ -421,7 +489,6 @@ func preprocess(ctxt *obj.Link, s *obj.LSym, newprog obj.ProgAlloc) {
 		case obj.ACALL, ACALLNORESUME:
 			call := *p
 			p.As = obj.ANOP
-
 			pcAfterCall := call.Link.Pc
 			if call.To.Sym == sigpanic {
 				pcAfterCall-- // sigpanic expects to be called without advancing the pc
@@ -702,12 +769,6 @@ func preprocess(ctxt *obj.Link, s *obj.LSym, newprog obj.ProgAlloc) {
 			default:
 				panic("bad MOV type")
 			}
-
-		case ACallImport:
-			p.As = obj.ANOP
-			p = appendp(p, AGet, regAddr(REG_SP))
-			p = appendp(p, ACall, obj.Addr{Type: obj.TYPE_MEM, Name: obj.NAME_EXTERN, Sym: s})
-			p.Mark = WasmImport
 		}
 	}
 

CongLeSolutionX/go_community

src/cmd/compile/internal/gc/gsubr.go
@@ -274,6 +274,27 @@ func (f *Func) initLSym(hasBody bool) {
 		}
 	}
 
+	wi := f.wasmimport
+	if wi != nil && objabi.GOARCH == "wasm" {
+		if f.lsym.Func == nil {
+			f.lsym.Func = &obj.FuncInfo{}
+		}
+		if wi.module == "go" {
+			f.lsym.Func.WasmImport = &obj.WasmImport{
+				Module: wi.module,
+				Name:   wi.name,
+				Params: []obj.WasmField{{Type: obj.WasmI32}},
+			}
+		} else {
+			f.lsym.Func.WasmImport = &obj.WasmImport{
+				Module:  wi.module,
+				Name:    wi.name,
+				Params:  f.wasmfields.Params,
+				Results: f.wasmfields.Results,
+			}
+		}
+	}
+
 	if !hasBody {
 		// For body-less functions, we only create the LSym.
 		return

src/cmd/compile/internal/gc/noder.go
@@ -511,12 +511,19 @@ func (p *noder) funcDecl(fun *syntax.FuncDecl) *Node {
 	f.Func.Nname.Name.Defn = f
 	f.Func.Nname.Name.Param.Ntype = t
 
+	isWasmImport := false
 	if pragma, ok := fun.Pragma.(*Pragma); ok {
 		f.Func.Pragma = pragma.Flag & FuncPragmas
 		if pragma.Flag&Systemstack != 0 && pragma.Flag&Nosplit != 0 {
 			yyerrorl(f.Pos, "go:nosplit and go:systemstack cannot be combined")
 		}
 		pragma.Flag &^= FuncPragmas
+		if pragma.wasmimport != nil {
+			isWasmImport = true
+			f.Func.Pragma |= Noescape
+			f.Func.Pragma |= Nosplit
+			f.Func.wasmimport = pragma.wasmimport
+		}
 		p.checkUnused(pragma)
 	}
 
@@ -530,6 +537,9 @@ func (p *noder) funcDecl(fun *syntax.FuncDecl) *Node {
 		if f.Func.Pragma&Noescape != 0 {
 			yyerrorl(f.Pos, "can only use //go:noescape with external func implementations")
 		}
+		if isWasmImport {
+			yyerrorl(f.Pos, "cannot have function body with //go:wasmimport")
+		}
 	} else {
 		if pure_go || strings.HasPrefix(f.funcname(), "init.") {
 			// Linknamed functions are allowed to have no body. Hopefully
@@ -541,7 +551,7 @@ func (p *noder) funcDecl(fun *syntax.FuncDecl) *Node {
 					break
 				}
 			}
-			if !isLinknamed {
+			if !isLinknamed && !isWasmImport {
 				yyerrorl(f.Pos, "missing function body")
 			}
 		}
@@ -1502,8 +1512,19 @@ var allowedStdPragmas = map[string]bool{
 
 // *Pragma is the value stored in a syntax.Pragma during parsing.
 type Pragma struct {
-	Flag PragmaFlag  // collected bits
-	Pos  []PragmaPos // position of each individual flag
+	Flag       PragmaFlag  // collected bits
+	Pos        []PragmaPos // position of each individual flag
+	wasmimport *wasmimport
+}
+
+type wasmimport struct {
+	module string
+	name   string
+}
+
+type wasmfields struct {
+	Results []obj.WasmField
+	Params  []obj.WasmField
 }
 
 type PragmaPos struct {
@@ -1552,6 +1573,17 @@ func (p *noder) pragma(pos syntax.Pos, blankLine bool, text string, old syntax.P
 	}
 
 	switch {
+	case strings.HasPrefix(text, "go:wasmimport"):
+		f := strings.Fields(text)
+		if len(f) != 3 {
+			p.error(syntax.Error{Pos: pos, Msg: "usage: //go:wasmimport [module name] [import name]"})
+		}
+		module := f[1]
+		name := f[2]
+		pragma.wasmimport = &wasmimport{
+			module: module,
+			name:   name,
+		}
 	case strings.HasPrefix(text, "go:linkname "):
 		f := strings.Fields(text)
 		if !(2 <= len(f) && len(f) <= 3) {

src/cmd/compile/internal/gc/pgen.go
@@ -207,7 +207,17 @@ func funccompile(fn *Node) {
 	// assign parameter offsets
 	dowidth(fn.Type)
 
-	if fn.Nbody.Len() == 0 {
+	wi := fn.Func.wasmimport
+	hasWasmImport := wi != nil && objabi.GOARCH == "wasm"
+
+	if hasWasmImport && wi.module != "go" {
+		fn.Func.wasmfields = &wasmfields{
+			Params:  toWasmFields(fn.Type.Params().FieldSlice()),
+			Results: toWasmFields(fn.Type.Results().FieldSlice()),
+		}
+	}
+
+	if fn.Nbody.Len() == 0 && !hasWasmImport {
 		// Initialize ABI wrappers if necessary.
 		fn.Func.initLSym(false)
 		emitptrargsmap(fn)
@@ -794,3 +804,25 @@ type symByName []*types.Sym
 func (a symByName) Len() int           { return len(a) }
 func (a symByName) Less(i, j int) bool { return a[i].Name < a[j].Name }
 func (a symByName) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func toWasmFields(fields []*types.Field) []obj.WasmField {
+	wfs := make([]obj.WasmField, len(fields))
+	for i, f := range fields {
+		t := f.Type
+		switch {
+		case t.IsInteger() && t.Width == 4:
+			wfs[i].Type = obj.WasmI32
+		case t.IsInteger() && t.Width == 8:
+			wfs[i].Type = obj.WasmI64
+		case t.IsFloat() && t.Width == 4:
+			wfs[i].Type = obj.WasmF32
+		case t.IsFloat() && t.Width == 8:
+			wfs[i].Type = obj.WasmF64
+		case t.IsPtr():
+			wfs[i].Type = obj.WasmPtr
+		default:
+			Fatalf("wasm import has bad function signature")
+		}
+		wfs[i].Offset = f.Offset
+	}
+	return wfs
+}

src/cmd/internal/obj/wasm/wasmobj.go
@@ -100,7 +100,6 @@ var unaryDst = map[obj.As]bool{
 	ATee:          true,
 	ACall:         true,
 	ACallIndirect: true,
-	ACallImport:   true,
 	ABr:           true,
 	ABrIf:         true,
 	ABrTable:      true,
@@ -136,7 +135,7 @@ var (
 
 const (
 	/* mark flags */
-	WasmImport = 1 << 0
+	IsWasmImport = 1 << 0
 )
 
 func instinit(ctxt *obj.Link) {
@@ -188,6 +187,77 @@ func preprocess(ctxt *obj.Link, s *obj.LSym, newprog obj.ProgAlloc) {
 	}
 	s.Func.Args = s.Func.Text.To.Val.(int32)
 	s.Func.Locals = int32(framesize)
+
+	wi := s.Func.WasmImport
+	if wi != nil {
+		auxSym := &obj.LSym{}
+		wi.WriteToSymContent(ctxt, auxSym)
+		s.Func.WasmImportSym = auxSym
+		/*
+		  Functions generated from //go:wasmimport have garbage in their bodies
+		  for some reason. Discard the garbage, expect for the first instruction (TEXT).
+		*/
+		p := s.Func.Text
+		p.Link = nil
+		to := obj.Addr{
+			Type: obj.TYPE_MEM,
+			Name: obj.NAME_EXTERN,
+			Sym:  s,
+		}
+		if wi.Module == "go" {
+			p = appendp(p, AGet, regAddr(REG_SP))
+			p = appendp(p, ACall, to)
+			p.Mark = IsWasmImport
+		} else {
+			if len(wi.Results) > 1 {
+				panic("invalid results type") // impossible until multi-value proposal has landed
+			}
+			if len(wi.Results) == 1 {
+				p = appendp(p, AGet, regAddr(REG_SP)) // address has to be before the value
+			}
+			for _, f := range wi.Params {
+				p = appendp(p, AGet, regAddr(REG_SP))
+				f.Offset += 8
+				switch f.Type {
+				case obj.WasmI32:
+					p = appendp(p, AI32Load, constAddr(f.Offset))
+				case obj.WasmI64:
+					p = appendp(p, AI64Load, constAddr(f.Offset))
+				case obj.WasmF32:
+					p = appendp(p, AF32Load, constAddr(f.Offset))
+				case obj.WasmF64:
+					p = appendp(p, AF64Load, constAddr(f.Offset))
+				case obj.WasmPtr:
+					p = appendp(p, AI64Load, constAddr(f.Offset))
+					p = appendp(p, AI32WrapI64)
+				default:
+					panic("bad param type")
+				}
+			}
+			p = appendp(p, ACall, to)
+			p.Mark = IsWasmImport
+			if len(wi.Results) == 1 {
+				f := wi.Results[0]
+				f.Offset += 8
+				switch f.Type {
+				case obj.WasmI32:
+					p = appendp(p, AI32Store, constAddr(f.Offset))
+				case obj.WasmI64:
+					p = appendp(p, AI64Store, constAddr(f.Offset))
+				case obj.WasmF32:
+					p = appendp(p, AF32Store, constAddr(f.Offset))
+				case obj.WasmF64:
+					p = appendp(p, AF64Store, constAddr(f.Offset))
+				case obj.WasmPtr:
+					p = appendp(p, AI64ExtendI32U)
+					p = appendp(p, AI64Store, constAddr(f.Offset))
+				default:
+					panic("bad result type")
+				}
+			}
+		}
+		p = appendp(p, obj.ARET)
+	}
 
 	if s.Func.Text.From.Sym.Wrapper() {
 		// if g._panic != nil && g._panic.argp == FP {
@@ -421,7 +491,6 @@ func preprocess(ctxt *obj.Link, s *obj.LSym, newprog obj.ProgAlloc) {
 		case obj.ACALL, ACALLNORESUME:
 			call := *p
 			p.As = obj.ANOP
-
 			pcAfterCall := call.Link.Pc
 			if call.To.Sym == sigpanic {
 				pcAfterCall-- // sigpanic expects to be called without advancing the pc
@@ -702,12 +771,6 @@ func preprocess(ctxt *obj.Link, s *obj.LSym, newprog obj.ProgAlloc) {
 			default:
 				panic("bad MOV type")
 			}
-
-		case ACallImport:
-			p.As = obj.ANOP
-			p = appendp(p, AGet, regAddr(REG_SP))
-			p = appendp(p, ACall, obj.Addr{Type: obj.TYPE_MEM, Name: obj.NAME_EXTERN, Sym: s})
-			p.Mark = WasmImport
 		}
 	}
 
@@ -1009,7 +1072,7 @@ func assemble(ctxt *obj.Link, s *obj.LSym, newprog obj.ProgAlloc) {
 				r := obj.Addrel(s)
 				r.Off = int32(w.Len())
 				r.Type = objabi.R_CALL
-				if p.Mark&WasmImport != 0 {
+				if p.Mark&IsWasmImport != 0 {
 					r.Type = objabi.R_WASMIMPORT
 				}
 				r.Sym = p.To.Sym