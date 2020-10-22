20fd59415d6c95c06c690e3c5019d16fa58c5096
Michael Anthony Knyszek <mknyszek@google.com>
2020-10-22 16:29:04 +0000

    [dev.regabi] reflect,runtime: support amd64 register ABI for reflect.Call
    This change, as-is, should pass all.bash as the int/float register
    counnts are set to zero. I've been independently testing assembly code
    for the other cases. That's the next patch.
    
    For #40724.
    
    Change-Id: I8aa089a5aa5a31b72e56b3d9388dd3f82203985b

--- src/internal/abi/abi.go
+++ src/internal/abi/abi.go
@@ -4,9 +4,49 @@
 
 package abi
 
+import "unsafe"
+
 // RegArgs is a struct that has space for each argument
 // and return value register on the current architecture.
+//
+// Assembly code knows the layout of the first two fields
+// of RegArgs.
+//
+// RegArgs also contains additional space to hold pointers
+// when it may not be safe to keep them only in the integer
+// register space otherwise.
 type RegArgs struct {
-	Ints   [IntArgRegs]uintptr
-	Floats [FloatArgRegs]uint64
+	Ints   [IntArgRegs]uintptr  // untyped integer registers
+	Floats [FloatArgRegs]uint64 // untyped float registers
+
+	// Fields above this point are known to assembly.
+
+	// ReturnPtrs is a space that duplicates Ints but with pointer type,
+	// used to make pointers returned in registers visible to the
+	// GC by making the type unsafe.Pointer.
+	//
+	// ReturnIsPtr is a bitmap that indicates which registers
+	// contain or will contain pointers. The i'th bit indicates whether
+	// the i'th register contains or will contain a valid Go pointer.
+	ReturnIsPtr IntArgRegBitmap
+	ReturnPtrs  [IntArgRegs]unsafe.Pointer
+}
+
+// IntArgRegBitmap is a bitmap large enough to hold one bit per
+// integer arugment/return register.
+type IntArgRegBitmap [(IntArgRegs + 7) / 8]uint8
+
+// Set sets the i'th bit of the bitmap to 1.
+func (b *IntArgRegBitmap) Set(i int) {
+	b[i/8] |= uint8(1) << (i % 8)
+}
+
+// Get returns whether the i'th bit of the bitmap is set.
+//
+// nosplit because it's called in extremely sensitive contexts, like
+// on the reflectcall return path.
+//
+//go:nosplit
+func (b *IntArgRegBitmap) Get(i int) bool {
+	return b[i/8]&(uint8(1)<<(i%8)) != 0
 }
