d0ed42901963fc43bd5960be7e736d8682ddb398
Bryan C. Mills <bcmills@google.com>
2020-10-26 17:37:20 +0000

    Revert "cmd/compiler,cmd/go,sync: add internal {LoadAcq,StoreRel}64 on ppc64"
    This reverts CL 42196.
    
    Reason for revert: the new "internal" special case in cmd/go caused a regression for 'gopls'.
    
    Fixes #42196
    
    Change-Id: I48dd58be68a6f073a5f779de559a3f183e4d7b99

--- src/runtime/internal/atomic/atomic_mipsx.go
+++ src/runtime/internal/atomic/atomic_mipsx.go
@@ -133,9 +133,6 @@ func Loadp(ptr unsafe.Pointer) unsafe.Pointer
 func LoadAcq(ptr *uint32) uint32
 
 //go:noescape
-func LoadAcquintptr(ptr *uintptr) uintptr
-
-//go:noescape
 func And8(ptr *uint8, val uint8)
 
 //go:noescape
@@ -152,9 +149,6 @@ func StorepNoWB(ptr unsafe.Pointer, val unsafe.Pointer)
 
 //go:noescape
 func StoreRel(ptr *uint32, val uint32)
-
-//go:noescape
-func StoreReluintptr(ptr *uintptr, val uintptr)
 
 //go:noescape
 func CasRel(addr *uint32, old, new uint32) bool
