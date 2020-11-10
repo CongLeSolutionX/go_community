fd4e8e86109bb44ddc1e31802b6e2b3b4cd79bfb
Andrew G. Morgan <agm@google.com>
2020-11-09 19:28:04 -0800

    syscall: improve TestSetuidEtc() /proc/ parsing against races
    TestSetuidEtc() was failing sporadically on linux-ppc64. From the
    three https://build.golang.org/ logs, it looked like the logged
    errors could be associated with threads dying, but proc reads
    were, in some way, racing with their demise. As such, the test
    now explicitly forces threads to exit as part of the test loop.
    
    This revealed that races of this type can happen on non-ppc64
    systems, and that os.IsNotExist(err) was not a sufficient
    error condition test for a thread's status file disappearing.
    This change includes a fix for that.
    
    The actual issue on linux-ppc64 appears to be tied to PID reuse
    on whatever the build test environment is for linux-ppc64-buildlet.
    I suspect this can happen on any linux system, especially where
    the container has a limited PID range.
    
    The fix for this, limited to the test (the runtime syscall support
    is unchanged), is to confirm that the Pid for the interogated
    thread's /proc/<TID>/status file confirms that it is still
    associated with the test-process' PID.
    
    linux-ppc64-buildlet:
      go/bin/go test syscall -run=TestSetuidEtc -count=10000
      ok      syscall 104.285s
    
    Fixes #42462
    
    Change-Id: I55c84ab8361003570a405fa52ffec4949bf91113

--- misc/cgo/test/issue1435.go
+++ misc/cgo/test/issue1435.go
@@ -9,6 +9,8 @@ package cgotest
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
+	"runtime"
 	"strings"
 	"syscall"
 	"testing"
@@ -62,29 +64,68 @@ import "C"
 // compareStatus is used to confirm the contents of the thread
 // specific status files match expectations.
 func compareStatus(filter, expect string) error {
-	expected := filter + "\t" + expect
+	expected := filter + expect
 	pid := syscall.Getpid()
 	fs, err := ioutil.ReadDir(fmt.Sprintf("/proc/%d/task", pid))
 	if err != nil {
 		return fmt.Errorf("unable to find %d tasks: %v", pid, err)
 	}
+	expectedProc := fmt.Sprintf("Pid:\t%d", pid)
+	foundAThread := false
 	for _, f := range fs {
 		tf := fmt.Sprintf("/proc/%s/status", f.Name())
 		d, err := ioutil.ReadFile(tf)
 		if err != nil {
+			if os.IsNotExist(err) || strings.HasSuffix(err.Error(), ": no such process") {
+				// We are racing against threads dying, which
+				// is out of our control, so ignore the
+				// missing file and skip to the next one.
+				continue
+			}
 			return fmt.Errorf("unable to read %q: %v", tf, err)
 		}
 		lines := strings.Split(string(d), "\n")
 		for _, line := range lines {
+			// Different kernel vintages pad differently.
+			line = strings.TrimSpace(line)
+			if strings.HasPrefix(line, "Pid:\t") {
+				// On loaded systems, it is possible
+				// for a TID to be reused really
+				// quickly. As such, we need to
+				// validate that the thread status
+				// info we just read is a task of the
+				// same process PID as we are
+				// currently running, and not a
+				// recently terminated thread
+				// resurfaced in a different process.
+				if line != expectedProc {
+					break
+				}
+				// Fall through in the unlikely case
+				// that filter at some point is
+				// "Pid:\t".
+			}
 			if strings.HasPrefix(line, filter) {
 				if line != expected {
-					return fmt.Errorf("%s %s (bad)\n", tf, line)
+					return fmt.Errorf("%q got:%q want:%q (bad) [pid=%d file:'%s' %v]\n", tf, line, expected, pid, string(d), expectedProc)
 				}
+				foundAThread = true
 				break
 			}
 		}
 	}
+	if !foundAThread {
+		return fmt.Errorf("found no thread /proc/<TID>/status files for process %q", expectedProc)
+	}
 	return nil
+}
+
+// killAThread locks the goroutine to an OS thread and exits; this
+// causes a thread to terminate.
+func killAThread(c <-chan struct{}) {
+	runtime.LockOSThread()
+	<-c
+	return
 }
 
 // test1435 test 9 glibc implemented setuid/gid syscall functions are
@@ -141,6 +182,9 @@ func test1435(t *testing.T) {
 	}
 
 	for i, v := range vs {
+		c := make(chan struct{})
+		go killAThread(c)
+		close(c)
 		if err := v.fn(); err != nil {
 			t.Errorf("[%d] %q failed: %v", i, v.call, err)
 			continue
