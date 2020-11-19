da6cd7c97ec28b892a9078b7e5d0799159f0640b
Andy Pan <panjf2000@gmail.com>
2020-11-19 19:09:14 +0800

    internal/poll: implement a pipe pool for splice() call
    In scenarios where splice() is called, splice() is usually called not just once, but many times,
    which means that a lot of pipes will be created and destroyed frequently, costing an amount of system resources
    and slowing down performance, thus I suggest that we add a pipe pool for reusing pipes.
    
    Fixes #42740
    
    Change-Id: Idff654b7264342084e089b5ba796c87c380c471b

---
+++ src/internal/poll/export_linux_test.go
@@ -0,0 +1,21 @@
+// Copyright 2020 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Export guts for testing on linux.
+// Since testing imports os and os imports internal/poll,
+// the internal/poll tests can not be in package poll.
+
+package poll
+
+var (
+	GetPipe    = getPipe
+	PutPipe    = putPipe
+	GetPipeFds = func(p *SplicePipe) (int, int) {
+		return p.rfd, p.wfd
+	}
+	NewPipe     = newPipe
+	DestroyPipe = destroyPipe
+)
+
+type SplicePipe = splicePipe
