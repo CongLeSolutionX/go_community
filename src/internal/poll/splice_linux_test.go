71e4b72657d22d51274911efcca6635a1ab6aa40
Andy Pan <panjf2000@gmail.com>
2020-11-19 19:09:14 +0800

    internal/poll: implement a pipe pool for splice() call
    In scenarios where splice() is called, splice() is usually called not just once, but many times,
    which means that a lot of pipes will be created and destroyed frequently, costing an amount of system resources
    and slowing down performance, thus I suggest that we add a pipe pool for reusing pipes.
    
    Fixes #42740
    
    Change-Id: Idff654b7264342084e089b5ba796c87c380c471b

---
+++ src/internal/poll/splice_linux_test.go
@@ -0,0 +1,32 @@
+package poll_test
+
+import (
+	"internal/poll"
+	"internal/syscall/unix"
+	"runtime"
+	"syscall"
+	"testing"
+	"time"
+)
+
+func TestSplicePipePool(t *testing.T) {
+	p, _, err := poll.GetPipe()
+	if err != nil {
+		t.Skip("failed to create pipe skip")
+	}
+	prfd, pwfd := poll.GetPipePair(p)
+	poll.PutPipe(p)
+	p = nil
+
+	// Trigger a garbage collection to free the pipe in sync.Pool and test whether or not
+	// the pipe buffer has been closed as we expected.
+	runtime.GC()
+	time.Sleep((100 + 10) * time.Millisecond)
+	runtime.GC()
+	time.Sleep((2*100 + 10) * time.Millisecond)
+	_, _, errno1 := syscall.Syscall(unix.FcntlSyscall, uintptr(prfd), syscall.F_GETFD, 0)
+	_, _, errno2 := syscall.Syscall(unix.FcntlSyscall, uintptr(pwfd), syscall.F_GETFD, 0)
+	if errno1 == 0 || errno2 == 0 {
+		t.Fatalf("pipe is still open, prfd errno: %d, pwfd errno: %d\n", errno1, errno2)
+	}
+}
