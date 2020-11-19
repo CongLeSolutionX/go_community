d10dd84416cad325b8b0402c344e9953b4b7a05b
Andy Pan <panjf2000@gmail.com>
2020-11-19 19:09:14 +0800

    internal/poll: implement a pipe pool for splice() call
    In scenarios where splice() is called, splice() is usually called not just once, but many times,
    which means that a lot of pipes will be created and destroyed frequently, costing an amount of system resources
    and slowing down performance, thus I suggest that we add a pipe pool for reusing pipes.
    
    Benchmark tests:
    
    goos: linux
    goarch: amd64
    pkg: internal/poll
    cpu: AMD EPYC 7K62 48-Core Processor
    
    name                  old time/op    new time/op    delta
    SplicePipe-8            1.36µs ± 1%    0.02µs ± 0%   -98.57%  (p=0.001 n=7+7)
    SplicePipeParallel-8     747ns ± 4%       4ns ± 0%   -99.41%  (p=0.001 n=7+7)
    
    name                  old alloc/op   new alloc/op   delta
    SplicePipe-8             24.0B ± 0%      0.0B       -100.00%  (p=0.001 n=7+7)
    SplicePipeParallel-8     24.0B ± 0%      0.0B       -100.00%  (p=0.001 n=7+7)
    
    name                  old allocs/op  new allocs/op  delta
    SplicePipe-8              1.00 ± 0%      0.00       -100.00%  (p=0.001 n=7+7)
    SplicePipeParallel-8      1.00 ± 0%      0.00       -100.00%  (p=0.001 n=7+7)
    
    Fixes #42740
    
    Change-Id: Idff654b7264342084e089b5ba796c87c380c471b

---
+++ src/internal/poll/splice_linux_test.go
@@ -0,0 +1,100 @@
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package poll_test
+
+import (
+	"internal/poll"
+	"runtime"
+	"syscall"
+	"testing"
+	"time"
+)
+
+// checkPipes returns true if all pipes are closed properly, false otherwise.
+func checkPipes(fds [][2]int) bool {
+	for _, fd := range fds {
+		// Check whether the each pipe has been closed.
+		err1 := syscall.FcntlFlock(uintptr(fd[0]), syscall.F_GETFD, nil)
+		err2 := syscall.FcntlFlock(uintptr(fd[1]), syscall.F_GETFD, nil)
+		if err1 == nil || err2 == nil {
+			return false
+		}
+	}
+	return true
+}
+
+func TestSplicePipePool(t *testing.T) {
+	const N = 64
+loop:
+	for try := 0; try < 3; try++ {
+		if try == 1 && testing.Short() {
+			break
+		}
+
+		var (
+			p   *poll.SplicePipe
+			ps  []*poll.SplicePipe
+			fds [][2]int
+			err error
+		)
+		for i := 0; i < N; i++ {
+			p, _, err = poll.GetPipe()
+			if err != nil {
+				t.Skip("failed to create pipe, skip this test")
+			}
+			prfd, pwfd := poll.GetPipeFds(p)
+			fds = append(fds, [2]int{prfd, pwfd})
+			ps = append(ps, p)
+		}
+		for _, p = range ps {
+			poll.PutPipe(p)
+		}
+		ps = nil
+
+		// Trigger garbage collection to free the pipe in sync.Pool and test whether or not
+		// the pipe buffer has been closed as we expected.
+		for i := 0; i < 5; i++ {
+			runtime.GC()
+			time.Sleep(time.Duration(i*100+10) * time.Millisecond)
+			if checkPipes(fds) {
+				continue loop
+			}
+		}
+		t.Fatal("at least one pipe is still open")
+	}
+}
+
+func BenchmarkSplicePipe(b *testing.B) {
+	b.Run("SplicePipeWithPool", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			p, _, _ := poll.GetPipe()
+			poll.PutPipe(p)
+		}
+	})
+	b.Run("SplicePipeWithoutPool", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			p := poll.NewPipe()
+			poll.DestroyPipe(p)
+		}
+	})
+}
+
+func BenchmarkSplicePipePoolParallel(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			p, _, _ := poll.GetPipe()
+			poll.PutPipe(p)
+		}
+	})
+}
+
+func BenchmarkSplicePipeNativeParallel(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			p := poll.NewPipe()
+			poll.DestroyPipe(p)
+		}
+	})
+}
