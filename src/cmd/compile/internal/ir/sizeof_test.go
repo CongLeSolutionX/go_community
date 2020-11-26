95eae8ef88e0576f6b6261b6399940e9c5a66487
Russ Cox <rsc@golang.org>
2020-11-26 00:47:44 -0500

    [dev.regabi] cmd/compile: add ir.Closure, ir.ClosureRead
    Closures are another reference to Funcs,
    and it cleans up the code quite a bit to be clear about types.
    
    OCLOSUREVAR is renamed to OCLOSUREREAD to make
    clearer that it is unrelated to the list Func.ClosureVars.
    
    Passes buildall w/ toolstash -cmp.
    
    Change-Id: Id0d28df2d4d6e9954e34df7a39ea226995eee937

--- src/cmd/compile/internal/ir/sizeof_test.go
+++ src/cmd/compile/internal/ir/sizeof_test.go
@@ -20,9 +20,9 @@ func TestSizeof(t *testing.T) {
 		_32bit uintptr     // size on 32bit platforms
 		_64bit uintptr     // size on 64bit platforms
 	}{
-		{Func{}, 152, 296},
-		{Name{}, 36, 224},
-		{node{}, 84, 144},
+		{Func{}, 172, 288},
+		{Name{}, 136, 224},
+		{node{}, 88, 144},
 	}
 
 	for _, tt := range tests {
