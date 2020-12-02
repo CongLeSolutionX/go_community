65ad87b5cefad1436ae55abf3a66425cb1c2e3ed
Russ Cox <rsc@golang.org>
2020-12-02 12:49:20 -0500

    io/fs: add Sub
    Sub provides a convenient way to refer to a subdirectory
    automatically in future operations, like Unix's chdir(2).
    
    The CL also includes updates to fstest to check Sub implementations.
    
    As part of updating fstest, I changed the meaning of TestFS's
    expected list to introduce a special case: if you list no expected files,
    that means the FS must be empty. In general it's OK not to list all
    the expected files, but if you list none, that's almost certainly a
    mistake - if your FS were broken and empty, you wouldn't find out.
    Making no expected files mean "must be empty" makes the mistake
    less likely - if your file system ever worked, then your test will keep
    it working.
    
    That change found a testing bug: embedtest was making exactly
    that mistake.
    
    Fixes #42322.
    
    Change-Id: I63fd4aa866b30061a0e51ca9a1927e576d6ec41e

---
+++ src/io/fs/sub_test.go
@@ -0,0 +1,45 @@
+// Copyright 2020 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package fs_test
+
+import (
+	. "io/fs"
+	"testing"
+)
+
+type subOnly struct{ SubFS }
+
+func (subOnly) Open(name string) (File, error) { return nil, ErrNotExist }
+
+func TestSub(t *testing.T) {
+	check := func(desc string, sub FS, err error) {
+		t.Helper()
+		if err != nil {
+			t.Errorf("Sub(sub): %v", err)
+			return
+		}
+		data, err := ReadFile(sub, "goodbye.txt")
+		if string(data) != "goodbye, world" || err != nil {
+			t.Errorf(`ReadFile(%s, "goodbye.txt" = %q, %v, want %q, nil`, desc, string(data), err, "goodbye, world")
+		}
+
+		dirs, err := ReadDir(sub, ".")
+		if err != nil || len(dirs) != 1 || dirs[0].Name() != "goodbye.txt" {
+			var names []string
+			for _, d := range dirs {
+				names = append(names, d.Name())
+			}
+			t.Errorf(`ReadDir(%s, ".") = %v, %v, want %v, nil`, desc, names, err, []string{"goodbye.txt"})
+		}
+	}
+
+	// Test that Sub uses the method when present.
+	sub, err := Sub(subOnly{testFsys}, "sub")
+	check("subOnly", sub, err)
+
+	// Test that Sub uses Open when the method is not present.
+	sub, err = Sub(openOnly{testFsys}, "sub")
+	check("openOnly", sub, err)
+}
