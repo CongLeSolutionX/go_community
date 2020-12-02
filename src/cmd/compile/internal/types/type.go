1099bb7fde432896bd8729592475d8f8a95b8489
Than McIntosh <thanm@google.com>
2020-12-02 16:13:45 -0500

    [dev.regabi] cmd/internal/types: add IsScalar query method
    Add method Type.IsScalar() method, which returns TRUE
    for numeric and pointer-shaped types, false for composites
    such as string/array/slice/struct.
    
    Change-Id: Ie53c71c07c5b3fbae11b48addd172343dc6bf3fd

--- src/cmd/compile/internal/types/type.go
+++ src/cmd/compile/internal/types/type.go
@@ -1333,6 +1333,21 @@ func (t *Type) IsEmptyInterface() bool {
 	return t.IsInterface() && t.NumFields() == 0
 }
 
+// IsScalar returns TRUE if 't' is a scalar Go type: either a numeric
+// type (bool/int*/float*/complex*) or something pointer-shaped. Note
+// that struct and array types consisting of a single scalar element
+// are not considered scalar.
+func (t *Type) IsScalar() bool {
+	switch t.kind {
+	case TBOOL, TINT8, TUINT8, TINT16, TUINT16, TINT32,
+		TUINT32, TINT64, TUINT64, TINT, TUINT,
+		TUINTPTR, TCOMPLEX64, TCOMPLEX128, TFLOAT32, TFLOAT64,
+		TMAP, TPTR, TUNSAFEPTR, TCHAN, TFUNC:
+		return true
+	}
+	return false
+}
+
 func (t *Type) PtrTo() *Type {
 	return NewPtr(t)
 }
