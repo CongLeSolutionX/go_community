222c2938f8f0d9b4d8a3f2acb8b47792c0f4a1b8
Russ Cox <rsc@golang.org>
2020-12-03 14:06:41 -0500

    [dev.regabi] cmd/compile: use ir.EditChildren for inline rewriting
    This CL rephrases the general inlining rewriter in terms of ir.EditChildren.
    It is the final part of the code that was processing arbitrary nodes using
    Left, SetLeft, and so on. After this CL, there should be none left except
    for the implementations of DoChildren and EditChildren, which fall next.
    
    Passes buildall w/ toolstash -cmp.
    
    Change-Id: I9c36053360cd040710716f0b39397a80114be713

--- src/cmd/compile/internal/ir/expr.go
+++ src/cmd/compile/internal/ir/expr.go
@@ -147,6 +147,17 @@ func (n *BinaryExpr) SetOp(op Op) {
 	}
 }
 
+// A CallUse records how the result of the call is used:
+type CallUse int
+
+const (
+	_ CallUse = iota
+
+	CallUseExpr // single expression result is used
+	CallUseList // list of results are used
+	CallUseStmt // results not used - call is a statement
+)
+
 // A CallExpr is a function call X(Args).
 type CallExpr struct {
 	miniExpr
@@ -156,6 +167,7 @@ type CallExpr struct {
 	Rargs    Nodes // TODO(rsc): Delete.
 	body     Nodes // TODO(rsc): Delete.
 	DDD      bool
+	Use      CallUse
 	noInline bool
 }
 
