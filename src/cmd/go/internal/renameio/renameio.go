8806dbba237b5c06b0bd00c67458b726d0962acf
Michael Matloob <matloob@golang.org>
2020-12-11 17:03:17 -0500

    cmd/go: remove some fsyncs when writing files
    cache.Trim, dowloadZip, rewriteVersionList, writeDiskCache all use
    renameio.WriteFile to write their respective files to disk. For the
    uses in cache.Trim and downloadZip, instead do of renameio.WriteFile,
    do a truncate to the length of the file, then write the relevant bytes
    so that a corrupt file (which would contain null bytes because of
    the truncate) could be detected. For rewriteVersionList, use
    lockedfile.Transform to do the write (which does a truncate as part of
    the write too. writeDiskCache stays the same in this CL.
    
    There is a possibility of corrupt files in the cache (which was true
    even before this CL) so later CLs will add facilities to clear corrupt
    files in the cache.
    
    Change-Id: I0d0bda40095e4cb898314315bf313e71650d8d25

--- src/cmd/go/internal/renameio/renameio.go
+++ src/cmd/go/internal/renameio/renameio.go
@@ -6,7 +6,6 @@
 package renameio
 
 import (
-	"bytes"
 	"io"
 	"io/fs"
 	"math/rand"
@@ -16,23 +15,6 @@ import (
 
 	"cmd/go/internal/robustio"
 )
-
-const patternSuffix = ".tmp"
-
-// Pattern returns a glob pattern that matches the unrenamed temporary files
-// created when writing to filename.
-func Pattern(filename string) string {
-	return filepath.Join(filepath.Dir(filename), filepath.Base(filename)+patternSuffix)
-}
-
-// WriteFile is like ioutil.WriteFile, but first writes data to an arbitrary
-// file in the same directory as filename, then renames it atomically to the
-// final name.
-//
-// That ensures that the final location, if it exists, is always a complete file.
-func WriteFile(filename string, data []byte, perm fs.FileMode) (err error) {
-	return WriteToFile(filename, bytes.NewReader(data), perm)
-}
 
 // WriteToFile is a variant of WriteFile that accepts the data as an io.Reader
 // instead of a slice.
@@ -67,23 +49,10 @@ func WriteToFile(filename string, data io.Reader, perm fs.FileMode) (err error) 
 	return robustio.Rename(f.Name(), filename)
 }
 
-// ReadFile is like ioutil.ReadFile, but on Windows retries spurious errors that
-// may occur if the file is concurrently replaced.
-//
-// Errors are classified heuristically and retries are bounded, so even this
-// function may occasionally return a spurious error on Windows.
-// If so, the error will likely wrap one of:
-// 	- syscall.ERROR_ACCESS_DENIED
-// 	- syscall.ERROR_FILE_NOT_FOUND
-// 	- internal/syscall/windows.ERROR_SHARING_VIOLATION
-func ReadFile(filename string) ([]byte, error) {
-	return robustio.ReadFile(filename)
-}
-
 // tempFile creates a new temporary file with given permission bits.
 func tempFile(dir, prefix string, perm fs.FileMode) (f *os.File, err error) {
 	for i := 0; i < 10000; i++ {
-		name := filepath.Join(dir, prefix+strconv.Itoa(rand.Intn(1000000000))+patternSuffix)
+		name := filepath.Join(dir, prefix+strconv.Itoa(rand.Intn(1000000000))+".tmp")
 		f, err = os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_EXCL, perm)
 		if os.IsExist(err) {
 			continue
