ad6fcd71d427de0a602d0c7b2b9cf3427c6d4cbe
Michael Matloob <matloob@golang.org>
2020-12-21 16:07:29 -0500

    [dev.cmdgo] cmd/go: remove fsync from modfetch.writeDiskCache
    This change removes the last usage of renameio.WriteFile, which is
    used in modfetch.WriteDiskCache. Instead, the file is written using
    lockedfile operations. It also removes renameio.WriteFile and its
    tests, because it is no longer used.
    
    Change-Id: I623196632c2c6793b003e65b1b9594d0b9869010

--- src/cmd/go/internal/renameio/renameio.go
+++ src/cmd/go/internal/renameio/renameio.go
@@ -6,15 +6,7 @@
 package renameio
 
 import (
-	"bytes"
-	"io"
-	"io/fs"
-	"math/rand"
-	"os"
 	"path/filepath"
-	"strconv"
-
-	"cmd/go/internal/robustio"
 )
 
 const patternSuffix = ".tmp"
@@ -23,53 +15,4 @@ const patternSuffix = ".tmp"
 // created when writing to filename.
 func Pattern(filename string) string {
 	return filepath.Join(filepath.Dir(filename), filepath.Base(filename)+patternSuffix)
-}
-
-// WriteFile is like ioutil.WriteFile, but first writes data to an arbitrary
-// file in the same directory as filename, then renames it atomically to the
-// final name.
-//
-// That ensures that the final location, if it exists, is always a complete file.
-func WriteFile(filename string, data []byte, perm fs.FileMode) (err error) {
-	f, err := tempFile(filepath.Dir(filename), filepath.Base(filename), perm)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		// Only call os.Remove on f.Name() if we failed to rename it: otherwise,
-		// some other process may have created a new file with the same name after
-		// that.
-		if err != nil {
-			f.Close()
-			os.Remove(f.Name())
-		}
-	}()
-
-	if _, err := io.Copy(f, bytes.NewReader(data)); err != nil {
-		return err
-	}
-	// Sync the file before renaming it: otherwise, after a crash the reader may
-	// observe a 0-length file instead of the actual contents.
-	// See https://golang.org/issue/22397#issuecomment-380831736.
-	if err := f.Sync(); err != nil {
-		return err
-	}
-	if err := f.Close(); err != nil {
-		return err
-	}
-
-	return robustio.Rename(f.Name(), filename)
-}
-
-// tempFile creates a new temporary file with given permission bits.
-func tempFile(dir, prefix string, perm fs.FileMode) (f *os.File, err error) {
-	for i := 0; i < 10000; i++ {
-		name := filepath.Join(dir, prefix+strconv.Itoa(rand.Intn(1000000000))+".tmp")
-		f, err = os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_EXCL, perm)
-		if os.IsExist(err) {
-			continue
-		}
-		break
-	}
-	return
 }
