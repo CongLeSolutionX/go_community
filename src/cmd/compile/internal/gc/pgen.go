51891e9a0dffedf266d2fca6c714a8fad4c765a8
Russ Cox <rsc@golang.org>
2020-12-22 17:55:07 -0500

    [dev.regabi] cmd/compile: split out package liveness [generated]
    [git-generate]
    
    cd src/cmd/compile/internal/gc
    rm -rf ../liveness
    rf '
    	# AutoVar is essentially an ssa helper; move it there.
    	mv AutoVar value.go
    	mv value.go cmd/compile/internal/ssa
    
    	# Export liveness API and unexport non-API.
    	mv LivenessMap Map
    	mv Map.vals Map.Vals
    	mv Map.deferreturn Map.DeferReturn
    	mv livenessShouldTrack ShouldTrack
    	mv onebitwalktype1 SetTypeBits
    	mv allUnsafe IsUnsafe
    	mv liveness Compute
    	mv BlockEffects blockEffects
    	mv Liveness liveness
    
    	mv emitptrargsmap WriteFuncMap
    	mv WriteFuncMap plive.go
    
    	mv bvset.go plive.go cmd/compile/internal/liveness
    '
    
    Change-Id: I3b86e5025bd9d32a7e19f44714fa16be4125059e

--- src/cmd/compile/internal/gc/pgen.go
+++ src/cmd/compile/internal/gc/pgen.go
@@ -6,8 +6,8 @@ package gc
 
 import (
 	"cmd/compile/internal/base"
-	"cmd/compile/internal/bitvec"
 	"cmd/compile/internal/ir"
+	livenesspkg "cmd/compile/internal/liveness"
 	"cmd/compile/internal/ssa"
 	"cmd/compile/internal/typecheck"
 	"cmd/compile/internal/types"
@@ -29,36 +29,6 @@ import (
 var (
 	compilequeue []*ir.Func // functions waiting to be compiled
 )
-
-func emitptrargsmap(fn *ir.Func) {
-	if ir.FuncName(fn) == "_" || fn.Sym().Linkname != "" {
-		return
-	}
-	lsym := base.Ctxt.Lookup(fn.LSym.Name + ".args_stackmap")
-	nptr := int(fn.Type().ArgWidth() / int64(types.PtrSize))
-	bv := bitvec.New(int32(nptr) * 2)
-	nbitmap := 1
-	if fn.Type().NumResults() > 0 {
-		nbitmap = 2
-	}
-	off := wobj.Uint32(lsym, 0, uint32(nbitmap))
-	off = wobj.Uint32(lsym, off, uint32(bv.N))
-
-	if ir.IsMethod(fn) {
-		onebitwalktype1(fn.Type().Recvs(), 0, bv)
-	}
-	if fn.Type().NumParams() > 0 {
-		onebitwalktype1(fn.Type().Params(), 0, bv)
-	}
-	off = wobj.BitVec(lsym, off, bv)
-
-	if fn.Type().NumResults() > 0 {
-		onebitwalktype1(fn.Type().Results(), 0, bv)
-		off = wobj.BitVec(lsym, off, bv)
-	}
-
-	wobj.Global(lsym, int32(off), obj.RODATA|obj.LOCAL)
-}
 
 // cmpstackvarlt reports whether the stack variable a sorts before b.
 //
@@ -213,7 +183,7 @@ func funccompile(fn *ir.Func) {
 	if len(fn.Body) == 0 {
 		// Initialize ABI wrappers if necessary.
 		initLSym(fn, false)
-		emitptrargsmap(fn)
+		livenesspkg.WriteFuncMap(fn)
 		return
 	}
 
@@ -254,7 +224,7 @@ func compile(fn *ir.Func) {
 	for _, n := range fn.Dcl {
 		switch n.Class_ {
 		case ir.PPARAM, ir.PPARAMOUT, ir.PAUTO:
-			if livenessShouldTrack(n) && n.Addrtaken() {
+			if livenesspkg.ShouldTrack(n) && n.Addrtaken() {
 				dtypesym(n.Type())
 				// Also make sure we allocate a linker symbol
 				// for the stack object data, for the same reason.
