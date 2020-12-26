a30ad57c8814d3caabcd4ed52d13e77512d366ad
Dan Scales <danscales@google.com>
2020-12-26 11:14:18 -0800

    [dev.typeparams] Conversion of types2 types to types, compare with existing type
    If -G=3 option is provided, then the noder adds a type to most nodes it
    generates. For each declaration or expression, we get the types2 type
    from the noder.typeInfo type maps. We then transform that types2 type
    to a 'types' type using new function typeExpr2() conversion function. (A
    bunch of the code in typeExpr2() came from an experimental CL from
    Matthew.) Then we set the type on the Node to this converted type and
    mark the type as being derived from types2.
    
    To verify correctness, we compare types in miniExpr.SetType() and
    Func.SetType(). That is, if we set a type from typechecker.go and there is
    already a type on a node derived from types2, we compare the types using
    (*Type).Compare. If the types compare identically, then we just keep the
    types2-derived type. Otherwise, we print a message and set the type
    computed by typechecker.go. So, when we are compiling programs with
    -G=3, we are using the types2-derived type for most of the nodes in the
    Noder graph for the remainder of the compilation.
    
    I added a new test g3flag.go in the go/test directory. It compiles a list of files
    from the test directory with the -G=3 directory and fails if there is any error.
    For about five files, it actually does a full "go run -G=3" to test that the
    compiled file actually runs without error. This test will be run automatically by
    all.bash, thus ensuring that -G=3 won't be broken.
    
    The big areas that I don't deal with yet are forward or circular references among
    types, and some cases involving untyped constants.
    
    Change-Id: I1f7d0b209623f8f5e04a124849e3dd283b1aae83

---
+++ test/g3flag.go
@@ -0,0 +1,160 @@
+// run
+
+// Copyright 2020 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Test compiling the below listed files (cfiles) in the test directory with the
+// -G=3 option and make sure there are no errors or panics. This is to test the
+// translation of types2 types to regular types, and the user of these types later
+// in the compiler. A few files (rfiles) are fully compiled and run with -G=3.
+package main
+
+import (
+	"flag"
+	"log"
+	"os/exec"
+)
+
+var cfiles []string = []string{
+	"index0.go",
+	"gc.go",
+	"args.go",
+	"escape_hash_maphash.go",
+	"func6.go",
+	"import.go",
+	"inline_math_bits_rotate.go",
+	"gc1.go",
+	"compos.go",
+	"closure4.go",
+	"int_lit.go",
+	"varinit.go",
+	"parentype.go",
+	"bom.go",
+	"struct0.go",
+	"inline_variadic.go",
+	"escape_selfassign.go",
+	"reflectmethod5.go",
+	"defernil.go",
+	"reflectmethod6.go",
+	"inline_math_bits_rotate.go",
+	"reflectmethod4.go",
+	"reflectmethod1.go",
+	"deferprint.go",
+	"func8.go",
+	"align.go",
+	"initempty.go",
+	"char_lit.go",
+	"strcopy.go",
+	"escape_goto.go",
+	"goprint.go",
+	"sliceopt.go",
+	"crlf.go",
+	"const3.go",
+	"method3.go",
+	"devirt.go",
+	"reflectmethod3.go",
+	"reflectmethod2.go",
+	"linkx.go",
+	"env.go",
+	"intrinsic_atomic.go",
+	"alias1.go",
+	"nilptr5_aix.go",
+	"nilptr5.go",
+	"alg.go",
+	"convert.go",
+	"for.go",
+	"sinit_run.go",
+	"gcstring.go",
+	"escape_runtime_atomic.go",
+	"defer.go",
+	"nilptr4.go",
+	"escape_sync_atomic.go",
+	"sieve.go",
+	"inline_literal.go",
+	"strength.go",
+	"escape_struct_return.go",
+	"simassign.go",
+	"live_syscall.go",
+	"simassign.go",
+	"divide.go",
+	"recover2.go",
+	"nul1.go",
+	"initialize.go",
+	"notinheap.go",
+	"initcomma.go",
+	"stackobj.go",
+	"uintptrescapes3.go",
+	"const4.go",
+	"turing.go",
+	"typeswitch1.go",
+	"decl.go",
+	"deferfin.go",
+	"escape4.go",
+	"if.go",
+	"defererrcheck.go",
+	"stackobj3.go",
+	"func5.go",
+	"live1.go",
+	"func.go",
+	"escape4.go",
+	"if.go",
+	"indirect.go",
+	"defererrcheck.go",
+	"stackobj3.go",
+	"stringrange.go",
+	"mapclear.go",
+	"inline_sync.go",
+	"rename.go",
+	"235.go",
+	"print.go",
+	"chancap.go",
+	"typeswitch1.go",
+	"linkx_run.go",
+	"phiopt.go",
+	"closure.go",
+	"stack.go",
+	"linkmain_run.go",
+	"escape_level.go",
+	"slicecap.go",
+	"winbatch.go",
+	"opt_branchlikely.go",
+	"bigalg.go",
+	"escape_unsafe.go",
+	"recover4.go",
+	"uintptrescapes2.go",
+	"switch.go",
+}
+
+var rfiles []string = []string{
+	"closure1.go",
+	"sigchld.go",
+	"func7.go",
+	"printbig.go",
+	"gc1.go",
+	"helloworld.go",
+}
+
+var verbose = flag.Bool("v", false, "show output for successful compiles")
+
+func main() {
+	flag.Parse()
+	for _, s := range cfiles {
+		out, err := exec.Command("go", "tool", "compile", "-G=3", s).CombinedOutput()
+		if err != nil {
+			log.Fatalf("go tool compile -G=3 %s: %s\n%s", s, err, out)
+		}
+		if *verbose {
+			log.Printf("go tool compile -G=3 %s:\n%s", s, out)
+		}
+	}
+	for _, s := range rfiles {
+		out, err := exec.Command("go", "run", "-gcflags=-G=3", s).CombinedOutput()
+		if err != nil {
+			log.Fatalf("go run -gcflags=-G=3 %s: %s\n%s", s, err, out)
+		}
+		if *verbose {
+			log.Printf("go run -gcflag=-G=3 %s:\n%s", s, out)
+		}
+	}
+}
