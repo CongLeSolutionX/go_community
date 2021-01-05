01dd3c5ba638884c1b2d12af0e59f69e127bbe5f
fanzha02 <fannie.zhang@arm.com>
2021-01-05 17:52:43 +0800

    runtime, syscall: add calls to asan functions
    Add explicit address sanitizer instrumentation to the runtime and
    syscall packages. The compiler does not instrument the runtime
    package. It does instrument the syscall package, but we need to add
    a couple of cases that it can't see.
    
    Refer to the implementation of the asan malloc runtime library,
    this patch also allocates extra memory as the redzone, around the
    returned memory region, and marks the redzone as unaddressable to
    detect the overflows or underflows.
    
    Updates #44853.
    
    Change-Id: I2753d1cc1296935a66bf521e31ce91e35fcdf798

--- src/runtime/stack.go
+++ src/runtime/stack.go
@@ -422,6 +422,9 @@ func stackalloc(n uint32) stack {
 	if msanenabled {
 		msanmalloc(v, uintptr(n))
 	}
+	if asanenabled {
+		asanunpoison(v, uintptr(n))
+	}
 	if stackDebug >= 1 {
 		print("  allocated ", v, "\n")
 	}
@@ -458,6 +461,9 @@ func stackfree(stk stack) {
 	}
 	if msanenabled {
 		msanfree(v, n)
+	}
+	if asanenabled {
+		asanpoison(v, n)
 	}
 	if n < _FixedStack<<_NumStackOrders && n < _StackCacheSize {
 		order := uint8(0)
