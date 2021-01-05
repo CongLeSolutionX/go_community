3a0f5fe758cd6505b49ea81aad16306bb181daa7
fanzha02 <fannie.zhang@arm.com>
2021-01-05 17:52:43 +0800

    cmd/dist: add asan tests in misc/cgo/testsanitizers package
    Add asan tests to check the use of Go with -asan option.
    
    Currenly, the address sanitizer in Go only checks for error
    memory access to heap objects.
    
    TODO: Enable check for error memory access to global objects.
    
    Updates #44853.
    
    Change-Id: I83579f229f117b5684a369fc8f365f4dea140648

---
+++ misc/cgo/testsanitizers/testdata/asan3_fail.go
@@ -0,0 +1,24 @@
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+/*
+#include <stdlib.h>
+#include <stdio.h>
+int *p;
+
+void test(int *a) {
+	// Heap out of bounds.
+	int c = a[5];        // BOOM
+	// We shouldn't get here; asan should stop us first.
+	printf("a[5]=%d\n", c);
+}
+*/
+import "C"
+
+func main() {
+	cIntSlice := []C.int{200, 201, 203, 203, 204}
+	C.test(&cIntSlice[0])
+}
