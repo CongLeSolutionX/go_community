7ee19baca0ccf1001b50a41caf889a304d77399d
pschou <pschou@users.noreply.github.com>
2021-01-05 00:13:53 +0000

    time: fix a todo, add NewTickerRuntimeOffset RuntimeStarted
    Change-Id: I507824ea136280dd6a551a238049556743f7c798
    GitHub-Last-Rev: c590f9f58220b04a5ffd3ad09389ed0964f72520
    GitHub-Pull-Request: golang/go#43488

--- src/time/tick.go
+++ src/time/tick.go
@@ -40,6 +40,40 @@ func NewTicker(d Duration) *Ticker {
 	return t
 }
 
+// NewTickerStartingAt returns a new Ticker containing a channel
+// that will send the time on the channel after each tick. The period
+// of the ticks is specified by the duration argument. The ticker
+// will adjust the time interval or drop ticks to make up for slow
+// receivers.  The duration d must be greater than zero; if not,
+// NewTicker will panic, and start is the time for the first tick
+// to fire.
+//
+// Note: Since this method uses time.Sub to derive the first tick,
+// if the system is put to sleep or crosses a leap second this time
+// may be off by a second or more.
+//
+// Stop the ticker to release associated resources.
+func NewTickerStartingAt(d Duration, start Time) *Ticker {
+        if d <= 0 {
+                panic(errors.New("non-positive interval for NewTicker"))
+        }
+        // Give the channel a 1-element time buffer.
+        // If the client falls behind while reading, we drop ticks
+        // on the floor until the client catches up.
+        c := make(chan Time, 1)
+        t := &Ticker{
+                C: c,
+                r: runtimeTimer{
+                        when:   startNano + int64(start.Sub(RuntimeStarted())),
+                        period: int64(d),
+                        f:      sendTime,
+                        arg:    c,
+                },
+        }
+        startTimer(&t.r)
+        return t
+}
+
 // Stop turns off a ticker. After Stop, no more ticks will be sent.
 // Stop does not close the channel, to prevent a concurrent goroutine
 // reading from the channel from seeing an erroneous "tick".
