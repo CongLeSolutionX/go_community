d46546bd3051464b9771848b06d071690c3021b0
Fannie Zhang <fannie.zhang@arm.com>
2021-01-06 08:57:44 +0000

    cmd/go: add -asan option
    The -asan option compiles Go code to use the address sanitizer.  This is
    intended for use when linking with C/C++ code compiled with -fsanitize=address.
    When memory blocks are passed back and forth between C/C++ and Go, code in
    both languages will agree as to whether the memory is validly allocated or not,
    and will report errors for any use of invalid memory.
    
    Updates #44853.
    
    Change-Id: I0209002ef795cc1c823daae557fb80c906158db3

--- src/cmd/go/internal/load/pkg.go
+++ src/cmd/go/internal/load/pkg.go
@@ -1620,6 +1620,7 @@ var cgoSyscallExclude = map[string]bool{
 	"runtime/cgo":  true,
 	"runtime/race": true,
 	"runtime/msan": true,
+	"runtime/asan": true,
 }
 
 var foldPath = make(map[string]string)
@@ -2228,6 +2229,10 @@ func LinkerDeps(p *Package) []string {
 	// Using memory sanitizer forces an import of runtime/msan.
 	if cfg.BuildMSan {
 		deps = append(deps, "runtime/msan")
+	}
+	// Using address sanitizer forces an import of runtime/asan.
+	if cfg.BuildASan {
+		deps = append(deps, "runtime/asan")
 	}
 
 	return deps
