7c10303241b0d35c463ddcb721a5555331e0ab5d
David Chase <drchase@google.com>
2021-01-08 10:15:36 -0500

    [dev.regabi] cmd/compile: refactor abiutils from "gc" into new "abi"
    Needs to be visible to ssagen, and might as well start clean to avoid
    creating a lot of accidental dependencies.
    
    Change-Id: I5570e1e6a463e303c5e2dc84e8dd4125e7c1adcc

--- src/cmd/compile/internal/abi/abiutilsaux_test.go
+++ src/cmd/compile/internal/abi/abiutilsaux_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package gc
+package abi
 
 // This file contains utility routines and harness infrastructure used
 // by the ABI tests in "abiutils_test.go".
