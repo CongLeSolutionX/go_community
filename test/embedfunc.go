9750f10fe35982c49e619fb3daddff65a33fbda9
Russ Cox <rsc@golang.org>
2021-01-08 17:02:41 -0500

    cmd/compile: require 'go 1.16' go.mod line for //go:embed
    This will produce better errors when earlier versions of
    Go compile code using //go:embed. (The import will cause
    a compilation error but then the go command will add to
    the output that the Go toolchain in use looks too old
    and maybe that's the problem.)
    
    This CL also adds a test for disallowing embed of a var inside a func.
    It's a bit too difficult to rebase down into that CL.
    
    The build system configuration check is delayed in order to
    make it possible to use errorcheck for these tests.
    
    Change-Id: I12ece4ff2d8d53380b63f54866e8f3497657d54c

---
+++ test/embedfunc.go
@@ -0,0 +1,13 @@
+// errorcheck -lang=go1.15
+
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package p
+
+func f() {
+	//go:embed x.txt // ERROR "go:embed cannot apply to var inside func"
+	var x string
+	_ = x
+}
