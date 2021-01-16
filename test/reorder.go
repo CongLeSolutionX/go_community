d76fc9acd10bbb92def9a5523543ecd955a94dd3
Matthew Dempsky <mdempsky@google.com>
2021-01-16 03:27:17 -0800

    [dev.regabi] cmd/compile: cleanup OAS2FUNC ordering
    Currently, to ensure OAS2FUNC results are assigned in the correct
    order, they're always assigned to temporary variables. However, these
    temporary variables are typed based on the destination type, which may
    require an interface conversion. This means walk may have to then
    introduce a second set of temporaries to ensure result parameters are
    all copied out of the results area, before it emits calls to runtime
    conversion functions.
    
    That's just silly. Instead, this CL changes order to allocate the
    result temporaries with the same type as the function returns in the
    first place, and then assign them one at a time to their destinations,
    with conversions as needed.
    
    Change-Id: I9f4e962425fe3c5e3305adbbfeae2c7f253ec365

--- test/reorder.go
+++ test/reorder.go
@@ -20,6 +20,7 @@ func main() {
 	p7()
 	p8()
 	p9()
+	p10()
 }
 
 var gx []int
@@ -148,4 +149,12 @@ func checkOAS2XXX(x bool, s string) {
 		fmt.Printf("%s; got=(false); want=(true)\n", s)
 		panic("failed")
 	}
+}
+
+//go:noinline
+func fp() (*int, int) { return nil, 42 }
+
+func p10() {
+	p := new(int)
+	p, *p = fp()
 }
