167a37bc112672f7f7a58dc67e6f13e769029880
Michael Anthony Knyszek <mknyszek@google.com>
2021-01-28 15:23:05 +0000

    reflect: add register ABI support for makeFuncStub and methodValueCall
    DO NOT SUBMIT
    
    It won't work until we have pragmas for specifying specific Go functions
    as having the internal ABI.
    
    Change-Id: I4888b50ae1f46794516e2b47ca3d84f4e89d0372

--- src/reflect/abi.go
+++ src/reflect/abi.go
@@ -9,6 +9,10 @@ import (
 	"unsafe"
 )
 
+// Redefine abi.RegArgs here so that the structure is visible
+// to the assembler.
+type reflectRegArgs abi.RegArgs
+
 // abiStep represents an ABI "instruction." Each instruction
 // describes one part of how to translate between a Go value
 // in memory and a call frame.
@@ -304,11 +308,15 @@ type abiDesc struct {
 	// passed to reflectcall.
 	stackPtrs *bitVector
 
-	// outRegPtrs is a bitmap whose i'th bit indicates
-	// whether the i'th integer result register contains
-	// a pointer. Used by reflectcall to make result
-	// pointers visible to the GC.
-	outRegPtrs abi.IntArgRegBitmap
+	// inRegPtrs is a bitmap whose i'th bit indicates
+	// whether the i'th integer argument register contains
+	// a pointer. Used by makeFuncStub and methodValueCall
+	// to make result pointers visible to the GC.
+	//
+	// outRegPtrs is the same, but for result values.
+	// Used by reflectcall to make result pointers visible
+	// to the GC.
+	inRegPtrs, outRegPtrs abi.IntArgRegBitmap
 }
 
 func (a *abiDesc) dump() {
@@ -336,6 +344,10 @@ func newAbiDesc(t *funcType, rcvr *rtype) abiDesc {
 	// Compute gc program & stack bitmap for stack arguments
 	stackPtrs := new(bitVector)
 
+	// Compute the stack frame pointer bitmap and register
+	// pointer bitmap for arguments.
+	inRegPtrs := abi.IntArgRegBitmap{}
+
 	// Compute abiSeq for input parameters.
 	var in abiSeq
 	if rcvr != nil {
@@ -355,6 +367,12 @@ func newAbiDesc(t *funcType, rcvr *rtype) abiDesc {
 			spill = align(spill, uintptr(arg.align))
 			spill += arg.size
 			addTypeBits(stackPtrs, stkStep.stkOff, arg)
+		} else {
+			for _, st := range out.stepsForValue(i) {
+				if st.kind == abiStepPointer {
+					inRegPtrs.Set(st.ireg)
+				}
+			}
 		}
 	}
 	spill = align(spill, ptrSize)
@@ -391,5 +409,5 @@ func newAbiDesc(t *funcType, rcvr *rtype) abiDesc {
 	// Undo the faking from earlier so that stackBytes
 	// is accurate.
 	out.stackBytes -= retOffset
-	return abiDesc{in, out, stackCallArgsSize, retOffset, spill, stackPtrs, outRegPtrs}
+	return abiDesc{in, out, stackCallArgsSize, retOffset, spill, stackPtrs, inRegPtrs, outRegPtrs}
 }
