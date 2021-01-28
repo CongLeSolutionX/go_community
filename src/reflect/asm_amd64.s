699399862f3760e1cb135b8c7c8676f4cd67f831
Michael Anthony Knyszek <mknyszek@google.com>
2021-01-28 15:23:05 +0000

    reflect: add register ABI support for makeFuncStub and methodValueCall
    DO NOT SUBMIT
    
    It won't work until we have pragmas for specifying specific Go functions
    as having the internal ABI.
    
    Change-Id: I4888b50ae1f46794516e2b47ca3d84f4e89d0372

--- src/reflect/asm_amd64.s
+++ src/reflect/asm_amd64.s
@@ -4,6 +4,7 @@
 
 #include "textflag.h"
 #include "funcdata.h"
+#include "go_asm.h"
 
 // makeFuncStub is the code half of the function returned by MakeFunc.
 // See the comment on the declaration of makeFuncStub in makefunc.go
@@ -11,15 +12,32 @@
 // No arg size here; runtime pulls arg map out of the func value.
 // makeFuncStub must be ABIInternal because it is placed directly
 // in function values.
-TEXT ·makeFuncStub<ABIInternal>(SB),(NOSPLIT|WRAPPER),$32
+// The frame size is enormous because this function calls into callReflect
+// which expects an internal/abi.RegArgs and we set that up in the argument
+// space so any pointers passed through are visible to the GC via callReflect's
+// stack map. callReflect then returns another internal/abi.RegArgs value
+// (modifying the argument and returning is unsafe, because the compiler
+// could optimize that away). That's then 32 bytes for the first few arguments
+// to callReflect, then 276 bytes for the calling RegArgs, and 276 bytes for
+// the returned RegArgs.
+//
+// TODO(mknyszek): Modify this function to call callReflect with
+// ABIInternal.
+TEXT ·makeFuncStub<ABIInternal>(SB),(NOSPLIT|WRAPPER),$584
 	NO_LOCAL_POINTERS
+	LEAQ    32(SP), R12
+	CALL    ·runtimeSpillArgs<>(SB)
 	MOVQ	DX, 0(SP)
+	MOVQ    R12, 8(SP)
+	CALL    ·moveMakeFuncArgPtrs<>(SB)
 	LEAQ	argframe+0(FP), CX
 	MOVQ	CX, 8(SP)
 	MOVB	$0, 24(SP)
 	LEAQ	24(SP), AX
 	MOVQ	AX, 16(SP)
 	CALL	·callReflect<ABIInternal>(SB)
+	LEAQ    304(SP), R12
+	CALL    ·runtimeUnspillArgs<>(SB)
 	RET
 
 // methodValueCall is the code half of the function returned by makeMethodValue.
@@ -28,13 +46,30 @@ TEXT ·makeFuncStub<ABIInternal>(SB),(NOSPLIT|WRAPPER),$32
 // No arg size here; runtime pulls arg map out of the func value.
 // methodValueCall must be ABIInternal because it is placed directly
 // in function values.
-TEXT ·methodValueCall<ABIInternal>(SB),(NOSPLIT|WRAPPER),$32
+// The frame size is enormous because this function calls into callMethod
+// which expects an internal/abi.RegArgs and we set that up in the argument
+// space so any pointers passed through are visible to the GC via callMethod's
+// stack map. callMethod then returns another internal/abi.RegArgs value
+// (modifying the argument and returning is unsafe, because the compiler
+// could optimize that away). That's then 32 bytes for the first few arguments
+// to callMethod, then 276 bytes for the calling RegArgs, and 276 bytes for
+// the returned RegArgs.
+//
+// TODO(mknyszek): Modify this function to call callMethod with
+// ABIInternal.
+TEXT ·methodValueCall<ABIInternal>(SB),(NOSPLIT|WRAPPER),$584
 	NO_LOCAL_POINTERS
+	LEAQ    32(SP), R12
+	CALL    ·runtimeSpillArgs<>(SB)
 	MOVQ	DX, 0(SP)
+	MOVQ    R12, 8(SP)
+	CALL    ·moveMakeFuncArgPtrs<>(SB)
 	LEAQ	argframe+0(FP), CX
 	MOVQ	CX, 8(SP)
 	MOVB	$0, 24(SP)
 	LEAQ	24(SP), AX
 	MOVQ	AX, 16(SP)
 	CALL	·callMethod<ABIInternal>(SB)
+	LEAQ    304(SP), R12
+	CALL    ·runtimeUnspillArgs<>(SB)
 	RET
