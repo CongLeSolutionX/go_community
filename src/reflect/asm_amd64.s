dcc7e7820912b86959019fa277cee45a8d3c8d5c
Michael Anthony Knyszek <mknyszek@google.com>
2021-01-28 15:23:05 +0000

    reflect: add register ABI support for makeFuncStub and methodValueCall
    DO NOT SUBMIT. Needs tests.
    
    This change finishes off functionality register ABI for the reflect
    package.
    
    For #40724.
    
    Change-Id: I302820b61fc0a8f94c5525a002bc02776aef41af

--- src/reflect/asm_amd64.s
+++ src/reflect/asm_amd64.s
@@ -4,6 +4,7 @@
 
 #include "textflag.h"
 #include "funcdata.h"
+#include "go_asm.h"
 
 // makeFuncStub is the code half of the function returned by MakeFunc.
 // See the comment on the declaration of makeFuncStub in makefunc.go
@@ -11,15 +12,29 @@
 // No arg size here; runtime pulls arg map out of the func value.
 // makeFuncStub must be ABIInternal because it is placed directly
 // in function values.
-TEXT ·makeFuncStub<ABIInternal>(SB),(NOSPLIT|WRAPPER),$32
+// The frame contains a stack-allocated RegArgs after the space saved for
+// arguments to callReflect. This RegArgs is visible to the GC via a special
+// case in the runtime's stack map code, so its offset is known to the runtime.
+//
+// TODO(mknyszek): Modify this function to call callReflect with
+// ABIInternal.
+TEXT ·makeFuncStub<ABIInternal>(SB),(NOSPLIT|WRAPPER),$312
 	NO_LOCAL_POINTERS
+	LEAQ	40(SP), R12
+	CALL	runtime·spillArgs<ABIInternal>(SB)
 	MOVQ	DX, 0(SP)
+	MOVQ	R12, 8(SP)
+	CALL	·moveMakeFuncArgPtrs(SB)
 	LEAQ	argframe+0(FP), CX
 	MOVQ	CX, 8(SP)
-	MOVB	$0, 24(SP)
-	LEAQ	24(SP), AX
+	MOVB	$0, 32(SP)
+	LEAQ	32(SP), AX
 	MOVQ	AX, 16(SP)
+	LEAQ	40(SP), AX
+	MOVQ	AX, 24(SP)
 	CALL	·callReflect<ABIInternal>(SB)
+	LEAQ	40(SP), R12
+	CALL	runtime·unspillArgs<ABIInternal>(SB)
 	RET
 
 // methodValueCall is the code half of the function returned by makeMethodValue.
@@ -28,13 +43,27 @@ TEXT ·makeFuncStub<ABIInternal>(SB),(NOSPLIT|WRAPPER),$32
 // No arg size here; runtime pulls arg map out of the func value.
 // methodValueCall must be ABIInternal because it is placed directly
 // in function values.
-TEXT ·methodValueCall<ABIInternal>(SB),(NOSPLIT|WRAPPER),$32
+// The frame contains a stack-allocated RegArgs after the space saved for
+// arguments to callMethod. This RegArgs is visible to the GC via a special
+// case in the runtime's stack map code, so its offset is known to the runtime.
+//
+// TODO(mknyszek): Modify this function to call callMethod with
+// ABIInternal.
+TEXT ·methodValueCall<ABIInternal>(SB),(NOSPLIT|WRAPPER),$312
 	NO_LOCAL_POINTERS
+	LEAQ	40(SP), R12
+	CALL	runtime·spillArgs<ABIInternal>(SB)
 	MOVQ	DX, 0(SP)
+	MOVQ	R12, 8(SP)
+	CALL	·moveMakeFuncArgPtrs(SB)
 	LEAQ	argframe+0(FP), CX
 	MOVQ	CX, 8(SP)
-	MOVB	$0, 24(SP)
-	LEAQ	24(SP), AX
+	MOVB	$0, 32(SP)
+	LEAQ	32(SP), AX
 	MOVQ	AX, 16(SP)
+	LEAQ	40(SP), AX
+	MOVQ	AX, 24(SP)
 	CALL	·callMethod<ABIInternal>(SB)
+	LEAQ	40(SP), R12
+	CALL	runtime·unspillArgs<ABIInternal>(SB)
 	RET
