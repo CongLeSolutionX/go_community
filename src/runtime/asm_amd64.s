b6d943942e5597b8621a4f7af89df0527858fbce
Russ Cox <rsc@golang.org>
2021-02-01 22:58:28 -0500

    runtime: unify asmcgocall and systemstack traceback setup
    Both asmcgocall and systemstack need to save the calling Go code's
    context for use by traceback, but they do it differently.
    Systemstack's appraoch is better, because it doesn't require a
    special case in traceback.
    So make them both use that.
    
    While we are here, the fake mstart caller in systemstack is
    no longer needed and can be removed.
    (traceback knows to stop in systemstack because of the writes to SP.)
    
    Also remove the fake mstarts in sys_windows_*.s.
    
    And while we are there, fix the control flow guard code in sys_windows_arm.s.
    The current code is using pointers to a stack frame that technically is gone
    once we hit the RET instruction. Clearly it's working OK, but better not to depend
    on data below SP being preserved, even for just a few instructions.
    Store the value we need in other registers instead.
    (This code is only used for pushing a sigpanic call, which does not
    actually return to the site of the fault and therefore doesn't need to
    preserve any of the registers.)
    
    This CL is part of a stack adding windows/arm64
    support (#36439), intended to land in the Go 1.17 cycle.
    This CL is, however, not windows/arm64-specific.
    It is cleanup meant to make the port (and future ports) easier.
    
    Change-Id: Id1e3ef5e54f7ad786e4b87043f2626eba7c3bbd9

--- src/runtime/asm_amd64.s
+++ src/runtime/asm_amd64.s
@@ -358,19 +358,11 @@ TEXT runtime·systemstack(SB), NOSPLIT, $0-8
 	// switch stacks
 	// save our state in g->sched. Pretend to
 	// be systemstack_switch if the G stack is scanned.
-	MOVQ	$runtime·systemstack_switch(SB), SI
-	MOVQ	SI, (g_sched+gobuf_pc)(AX)
-	MOVQ	SP, (g_sched+gobuf_sp)(AX)
-	MOVQ	AX, (g_sched+gobuf_g)(AX)
-	MOVQ	BP, (g_sched+gobuf_bp)(AX)
+	CALL	gosave_systemstack_switch<>(SB)
 
 	// switch to g0
 	MOVQ	DX, g(CX)
 	MOVQ	(g_sched+gobuf_sp)(DX), BX
-	// make it look like mstart called systemstack on g0, to stop traceback
-	SUBQ	$8, BX
-	MOVQ	$runtime·mstart(SB), DX
-	MOVQ	DX, 0(BX)
 	MOVQ	BX, SP
 
 	// call target function
@@ -604,11 +596,15 @@ TEXT runtime·jmpdefer(SB), NOSPLIT, $0-16
 	MOVQ	0(DX), BX
 	JMP	BX	// but first run the deferred function
 
-// Save state of caller into g->sched. Smashes R8, R9.
-TEXT gosave<>(SB),NOSPLIT,$0
+// Save state of caller into g->sched,
+// but using fake PC from systemstack_switch.
+// Must only be called from functions with no locals ($0)
+// or else unwinding from systemstack_switch is incorrect.
+// Smashes R8, R9.
+TEXT gosave_systemstack_switch<>(SB),NOSPLIT,$0
 	get_tls(R8)
 	MOVQ	g(R8), R8
-	MOVQ	0(SP), R9
+	MOVQ	$runtime·systemstack_switch(SB), R9
 	MOVQ	R9, (g_sched+gobuf_pc)(R8)
 	LEAQ	8(SP), R9
 	MOVQ	R9, (g_sched+gobuf_sp)(R8)
@@ -666,7 +662,7 @@ TEXT ·asmcgocall(SB),NOSPLIT,$0-20
 
 	// Switch to system stack.
 	MOVQ	m_g0(R8), SI
-	CALL	gosave<>(SB)
+	CALL	gosave_systemstack_switch<>(SB)
 	MOVQ	SI, g(CX)
 	MOVQ	(g_sched+gobuf_sp)(SI), SP
 
