fc540cc4b447d2238d5af094bea3c1039a0ae1b7
Jason A. Donenfeld <Jason@zx2c4.com>
2021-02-04 00:11:12 +0100

    cmd/link: recognize ARM64 PE files and relocations
    For now, we only add a single relocation type. TODO: add additional.
    
    This CL is part of a stack adding windows/arm64
    support (#36439), intended to land in the Go 1.17 cycle.
    
    Change-Id: Ia1ee840265e9d12c0b12dd1c5d0810f8b300e557

--- src/cmd/link/internal/ld/lib.go
+++ src/cmd/link/internal/ld/lib.go
@@ -1821,7 +1821,7 @@ func ldobj(ctxt *Link, f *bio.Reader, lib *sym.Library, length int64, pn string,
 		return ldhostobj(ldmacho, ctxt.HeadType, f, pkg, length, pn, file)
 	}
 
-	if /* x86 */ c1 == 0x4c && c2 == 0x01 || /* x86_64 */ c1 == 0x64 && c2 == 0x86 || /* armv7 */ c1 == 0xc4 && c2 == 0x01 {
+	if /* x86 */ c1 == 0x4c && c2 == 0x01 || /* x86_64 */ c1 == 0x64 && c2 == 0x86 || /* armv7 */ c1 == 0xc4 && c2 == 0x01 || /* arm64 */ c1 == 0x64 && c2 == 0xaa {
 		ldpe := func(ctxt *Link, f *bio.Reader, pkg string, length int64, pn string) {
 			textp, rsrc, err := loadpe.Load(ctxt.loader, ctxt.Arch, ctxt.IncVersion(), f, pkg, length, pn)
 			if err != nil {
