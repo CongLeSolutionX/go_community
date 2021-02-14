77782a17e52e769ef83915aebe6e75db704f875f
Jason A. Donenfeld <Jason@zx2c4.com>
2021-02-14 22:23:01 +0100

    runtime/cgo: implement for windows/arm and windows/arm64
    DO NOT SUBMIT
    
    This is a work in progress. There's still work to be done inside the
    linker apparently. Feel free to take this skeleton and as the basis for
    a new commit and discard this one, if it's helpful.
    
    This CL is part of a stack adding windows/arm64
    support (#36439), intended to land in the Go 1.17 cycle.
    
    Fixes #43800.
    
    Change-Id: If9651cf04576dfd183e4706dd4dfe343cee3414e

--- src/runtime/rt0_windows_arm.s
+++ src/runtime/rt0_windows_arm.s
@@ -10,3 +10,15 @@
 // kernel for an ordinary -buildmode=exe program.
 TEXT _rt0_arm_windows(SB),NOSPLIT|NOFRAME,$0
 	B	·rt0_go(SB)
+
+TEXT _rt0_arm_windows_lib(SB),NOSPLIT|NOFRAME,$0
+	MOVW	$_rt0_arm_windows_lib_go(SB), R0
+	MOVW	$0, R1
+	MOVW	_cgo_sys_thread_create(SB), R2
+	B	(R2)
+
+TEXT _rt0_arm_windows_lib_go(SB),NOSPLIT|NOFRAME,$0
+	MOVW	$0, R0
+	MOVW	$0, R1
+	MOVW	$runtime·rt0_go(SB), R2
+	B	(R2)
