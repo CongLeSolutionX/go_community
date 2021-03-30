e1b92a9c3d001cb49147ee1bd43849ea6b4ee05e
Rob Findley <rfindley@google.com>
2021-03-30 08:26:24 -0400

    go/parser: add benchmarks for syntax-only parsing and object resolution
    Change-Id: I73d1b2d18ab4051443d66c60df493d1163d0ba3f

--- src/go/parser/performance_test.go
+++ src/go/parser/performance_test.go
@@ -28,3 +28,27 @@ func BenchmarkParse(b *testing.B) {
 		}
 	}
 }
+
+func BenchmarkParseOnly(b *testing.B) {
+	b.SetBytes(int64(len(src)))
+	for i := 0; i < b.N; i++ {
+		if _, err := ParseFile(token.NewFileSet(), "", src, ParseComments|SkipResolution); err != nil {
+			b.Fatalf("benchmark failed due to parse error: %s", err)
+		}
+	}
+}
+
+func BenchmarkResolve(b *testing.B) {
+	b.SetBytes(int64(len(src)))
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		fset := token.NewFileSet()
+		file, err := ParseFile(fset, "", src, SkipResolution)
+		if err != nil {
+			b.Fatalf("benchmark failed due to parse error: %s", err)
+		}
+		b.StartTimer()
+		handle := fset.File(file.Package)
+		resolveFile(file, handle, nil)
+	}
+}
