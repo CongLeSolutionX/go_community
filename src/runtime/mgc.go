61f04067519956f779f8718194e05876e506a7a1
Michael Anthony Knyszek <mknyszek@google.com>
2021-04-01 19:09:40 +0000

    runtime: pass work.userForced to gcController.endCycle explicitly
    For #44167.
    
    Change-Id: I15817006f1870d6237cd06dabad988da3f23a6d6

--- src/runtime/mgc.go
+++ src/runtime/mgc.go
@@ -894,7 +894,7 @@ top:
 	// endCycle depends on all gcWork cache stats being flushed.
 	// The termination algorithm above ensured that up to
 	// allocations since the ragged barrier.
-	nextTriggerRatio := gcController.endCycle()
+	nextTriggerRatio := gcController.endCycle(work.userForced)
 
 	// Perform mark termination. This will restart the world.
 	gcMarkTermination(nextTriggerRatio)
