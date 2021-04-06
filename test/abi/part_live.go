9127720bd77415d9050b8f1e3f9aa2c08097008a
David Chase <drchase@google.com>
2021-04-06 18:39:15 -0400

    cmd/compile: pre-spill pointers in aggregate-typed register args
    There's a problem in liveness, where liveness of any part of an aggregate
    keeps the whole aggregate alive, but the not-live parts don't get spilled.
    A better fix is to change liveness to work pointer-by-pointer, but that is
    also a riskier, trickier fix.
    
    To avoid this, in the case of
    
    (1) an aggregate input parameter
    (2) containing pointers
    (3) passed in a registers
    
    pre-spill the pointers.
    
    Updates #40724.
    
    Change-Id: I6beb8e0a353b1ae3c68c16072f56698061922c04

---
+++ test/abi/part_live.go
@@ -0,0 +1,43 @@
+// run
+
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// A test for partial liveness / partial spilling / compiler-induced GC failure
+
+package main
+
+import "runtime"
+import "unsafe"
+
+func F(s []int) {
+	for i, x := range s {
+		G(i, x)
+	}
+	GC()
+	G(len(s), cap(s))
+	GC()
+}
+
+//go:noinline
+func G(int, int) {}
+
+func GC() { runtime.GC(); runtime.GC() }
+
+func main() {
+	s := make([]int, 3)
+	escape(s)
+	p := int(uintptr(unsafe.Pointer(&s[2])) + 42) // likely point to unallocated memory
+	poison([3]int{p, p, p})
+	F(s)
+}
+
+//go:noinline
+func poison([3]int) {}
+
+//go:noinline
+func escape(s []int) {
+	g = s
+}
+var g []int
