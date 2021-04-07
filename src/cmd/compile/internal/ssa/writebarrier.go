0578e57d7481312c55a9fcbed9be3bd189a3c207
Cherry Zhang <cherryyz@google.com>
2021-04-07 00:06:06 -0400

    cmd/compile: correct argument area size for typedmemmove/typedmemclr
    When the write barrier pass emits typedmemmove/typedmemclr calls,
    even the arguments are in registers, we still need to leave space
    for the spill slots. Count that space. Otherwise when the callee
    spills arguments it may clobber locals on the caller's frame.
    
    Change-Id: I5326943427feaf66cab7658a5bef55b3baf5d345

--- src/cmd/compile/internal/ssa/writebarrier.go
+++ src/cmd/compile/internal/ssa/writebarrier.go
@@ -500,8 +500,8 @@ func wbcall(pos src.XPos, b *Block, fn, typ *obj.LSym, ptr, val, mem, sp, sb *Va
 			off = round(off, taddr.Type.Alignment())
 			arg := b.NewValue1I(pos, OpOffPtr, taddr.Type.PtrTo(), off, sp)
 			mem = b.NewValue3A(pos, OpStore, types.TypeMem, ptr.Type, arg, taddr, mem)
-			off += taddr.Type.Size()
 		}
+		off += taddr.Type.Size()
 	}
 
 	argTypes = append(argTypes, ptr.Type)
@@ -511,8 +511,8 @@ func wbcall(pos src.XPos, b *Block, fn, typ *obj.LSym, ptr, val, mem, sp, sb *Va
 		off = round(off, ptr.Type.Alignment())
 		arg := b.NewValue1I(pos, OpOffPtr, ptr.Type.PtrTo(), off, sp)
 		mem = b.NewValue3A(pos, OpStore, types.TypeMem, ptr.Type, arg, ptr, mem)
-		off += ptr.Type.Size()
 	}
+	off += ptr.Type.Size()
 
 	if val != nil {
 		argTypes = append(argTypes, val.Type)
@@ -522,8 +522,8 @@ func wbcall(pos src.XPos, b *Block, fn, typ *obj.LSym, ptr, val, mem, sp, sb *Va
 			off = round(off, val.Type.Alignment())
 			arg := b.NewValue1I(pos, OpOffPtr, val.Type.PtrTo(), off, sp)
 			mem = b.NewValue3A(pos, OpStore, types.TypeMem, val.Type, arg, val, mem)
-			off += val.Type.Size()
 		}
+		off += val.Type.Size()
 	}
 	off = round(off, config.PtrSize)
 	wbargs = append(wbargs, mem)
