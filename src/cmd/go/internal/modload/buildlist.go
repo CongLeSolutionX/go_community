fcdcc38049bf0e64c50e9eb5a5040e5b74b2ee32
Bryan C. Mills <bcmills@google.com>
2021-04-09 00:39:25 -0400

    cmd/go/internal/modload: change mvsReqs to store roots instead of a full build list
    The mvsReqs implementation has always been a bit ambivalent about
    whether the root requirements return the full build list, just the
    direct requirements, or some hybrid of the two.
    
    However, a full build list always requires the Target module as the
    first entry, and it's easer to remove a redundant leading element from
    a slice than to add one. Changing the mvsReqs field to contain
    arbitrary roots instead of a full build list eliminates the need to
    add redundant elements, at the cost of needing to remove redundant
    elements in more places.
    
    For #36460
    
    Change-Id: Idd4c2d6bc7b66f67680037dab1fb9c2d1b40ab93

--- src/cmd/go/internal/modload/buildlist.go
+++ src/cmd/go/internal/modload/buildlist.go
@@ -486,7 +486,7 @@ func editRequirements(ctx context.Context, rs *Requirements, add, mustSelect []m
 		// remove roots.)
 	}
 
-	min, err := mvs.Req(Target, rootPaths, &mvsReqs{buildList: final})
+	min, err := mvs.Req(Target, rootPaths, &mvsReqs{roots: final[1:]})
 	if err != nil {
 		return nil, false, err
 	}
@@ -604,7 +604,7 @@ func updateRoots(ctx context.Context, depth modDepth, direct map[string]bool, pk
 			// dependencies, then we can't reliably compute a minimal subset of them.
 			return rs, err
 		}
-		keep = mg.BuildList()
+		keep = mg.BuildList()[1:]
 
 		for _, root := range rs.rootModules {
 			// If the selected version of the root is the same as what was already
@@ -619,7 +619,6 @@ func updateRoots(ctx context.Context, depth modDepth, direct map[string]bool, pk
 			}
 		}
 	} else {
-		keep = append(keep, Target)
 		kept := map[module.Version]bool{Target: true}
 		for _, pkg := range pkgs {
 			if pkg.mod.Path != "" && !kept[pkg.mod] {
@@ -685,7 +684,7 @@ func updateRoots(ctx context.Context, depth modDepth, direct map[string]bool, pk
 		return rs, nil
 	}
 
-	min, err := mvs.Req(Target, rootPaths, &mvsReqs{buildList: keep})
+	min, err := mvs.Req(Target, rootPaths, &mvsReqs{roots: keep})
 	if err != nil {
 		return rs, err
 	}
