4b7741c9784baa4a9f122c79383c1eff0aee0fd9
HowJMay <vulxj0j8j8@gmail.com>
2021-04-11 07:35:15 +0000

    net: Pass MSG_CMSG_CLOEXEC flag in ReadMsgUnix
    As mentioned in #42765, calling "recvmsg" syscall on Linux should come
    with "MSG_CMSG_CLOEXEC" flag.
    
    Fixes #42765
    
    Change-Id: I74347db72b465685d7684bf0f32415d285845ebb
    GitHub-Last-Rev: 51fbaeee1c7b07e94b6f90e1b76be905de5403e8
    GitHub-Pull-Request: golang/go#42768

---
+++ src/net/unixsock_readmsg_posix.go
@@ -0,0 +1,47 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build aix darwin dragonfly freebsd netbsd openbsd solaris
+
+package net
+
+import (
+	"syscall"
+)
+
+func (c *UnixConn) readMsg(b, oob []byte) (n, oobn, flags int, addr *UnixAddr, err error) {
+	var sa syscall.Sockaddr
+	n, oobn, flags, sa, err = c.fd.readMsg(b, oob, 0)
+	setCloOnExec(oob, oobn)
+
+	switch sa := sa.(type) {
+	case *syscall.SockaddrUnix:
+		if sa.Name != "" {
+			addr = &UnixAddr{Name: sa.Name, Net: sotypeToNet(c.fd.sotype)}
+		}
+	}
+	return
+}
+
+func setCloOnExec(oob []byte, oobn int) {
+	if oobn > 0 {
+		scms, err := syscall.ParseSocketControlMessage(oob[:oobn])
+		if err != nil {
+			return
+		}
+
+		for _, scm := range scms {
+			if scm.Header.Level == syscall.SOL_SOCKET &&
+				scm.Header.Type == syscall.SCM_RIGHTS {
+				fds, err := syscall.ParseUnixRights(&scm)
+				if err != nil {
+					continue
+				}
+				for _, fd := range fds {
+					syscall.CloseOnExec(fd)
+				}
+			}
+		}
+	}
+}
