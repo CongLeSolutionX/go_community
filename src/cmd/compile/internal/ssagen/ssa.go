7bf8728fbaae0bc65495fda27ba203590d1f3ee6
Than McIntosh <thanm@google.com>
2021-04-27 12:40:43 -0400

    cmd/compile: regabi support for DWARF location expressions
    Revise the code that generates DWARF location expressions for input
    parameters to get it to work properly with the new register ABI when
    optimization is turned off.
    
    The previously implementation assumed stack locations for all
    input+output parameters when -N (disable optimization) was in effect.
    In the new implementation, a register-resident input parameter is
    given a 2-element location list, the first list element pointing to
    the ABI register(s) containing the param, and the second element
    pointing to the stack home once it has been spilled.
    
    NB, this change fixes a bunch of the Delve pkg/proc unit tests (maybe
    about half of the outstanding failures). Still a good number that need
    to be investigated, however.
    
    Updates #40724.
    
    Change-Id: I743bbb9af187bcdebeb8e690fdd6db58094ca415

--- src/cmd/compile/internal/ssagen/ssa.go
+++ src/cmd/compile/internal/ssagen/ssa.go
@@ -400,7 +400,7 @@ func buildssa(fn *ir.Func, worker int) *ssa.Func {
 	name := ir.FuncName(fn)
 	printssa := false
 	if ssaDump != "" { // match either a simple name e.g. "(*Reader).Reset", package.name e.g. "compress/gzip.(*Reader).Reset", or subpackage name "gzip.(*Reader).Reset"
-		pkgDotName := base.Ctxt.Pkgpath+"."+name
+		pkgDotName := base.Ctxt.Pkgpath + "." + name
 		printssa = name == ssaDump ||
 			strings.HasSuffix(pkgDotName, ssaDump) && (pkgDotName == ssaDump || strings.HasSuffix(pkgDotName, "/"+ssaDump))
 	}
@@ -6961,7 +6961,12 @@ func genssa(f *ssa.Func, pp *objw.Progs) {
 	}
 
 	if base.Ctxt.Flag_locationlists {
-		debugInfo := ssa.BuildFuncDebug(base.Ctxt, f, base.Debug.LocationLists > 1, StackOffset)
+		var debugInfo *ssa.FuncDebug
+		if e.curfn.ABI == obj.ABIInternal && base.Flag.N != 0 {
+			debugInfo = ssa.BuildFuncDebugNoOptimized(base.Ctxt, f, base.Debug.LocationLists > 1, StackOffset)
+		} else {
+			debugInfo = ssa.BuildFuncDebug(base.Ctxt, f, base.Debug.LocationLists > 1, StackOffset)
+		}
 		e.curfn.DebugInfo = debugInfo
 		bstart := s.bstart
 		// Note that at this moment, Prog.Pc is a sequence number; it's
