abb88f6ecbe70a08516acc347021625c6ef98b9b
Than McIntosh <thanm@google.com>
2021-04-28 18:26:54 -0400

    cmd/compile: revise block/func end sentinels in debug analysis
    The SSA code for debug variable location analysis (for DWARF) has two
    special 'sentinel' values that it uses to handshake with the
    debugInfo.GetPC callback when capturing the PC values of debug
    variable ranges after prog generatoin: "BlockStart" and "BlockEnd".
    
    "BlockStart" has the expected semantics: it means "the PC value of the
    first instruction of block B", but "BlockEnd" does not mean "PC value
    of the last instruction of block B", but rather it is implemented as
    "the PC value of the last instruction of the function". This causes
    confusion when reading the code, and seems to to result in implementation
    flaws in the past, leading to incorrect ranges in some cases.
    
    To help with this, add a new sentinel "FuncEnd" (which has the "last
    inst in the function" semantics) and change the implementation of
    "BlockEnd" to actually mean what its name implies (last inst in
    block).
    
    Change-Id: Ic3497fb60413e898d2bfe27805c3db56483d12a2

--- src/cmd/compile/internal/ssagen/ssa.go
+++ src/cmd/compile/internal/ssagen/ssa.go
@@ -400,7 +400,7 @@ func buildssa(fn *ir.Func, worker int) *ssa.Func {
 	name := ir.FuncName(fn)
 	printssa := false
 	if ssaDump != "" { // match either a simple name e.g. "(*Reader).Reset", package.name e.g. "compress/gzip.(*Reader).Reset", or subpackage name "gzip.(*Reader).Reset"
-		pkgDotName := base.Ctxt.Pkgpath+"."+name
+		pkgDotName := base.Ctxt.Pkgpath + "." + name
 		printssa = name == ssaDump ||
 			strings.HasSuffix(pkgDotName, ssaDump) && (pkgDotName == ssaDump || strings.HasSuffix(pkgDotName, "/"+ssaDump))
 	}
@@ -6964,6 +6964,10 @@ func genssa(f *ssa.Func, pp *objw.Progs) {
 		debugInfo := ssa.BuildFuncDebug(base.Ctxt, f, base.Debug.LocationLists > 1, StackOffset)
 		e.curfn.DebugInfo = debugInfo
 		bstart := s.bstart
+		idToIdx := make([]int, f.NumBlocks())
+		for i, b := range f.Blocks {
+			idToIdx[b.ID] = i
+		}
 		// Note that at this moment, Prog.Pc is a sequence number; it's
 		// not a real PC until after assembly, so this mapping has to
 		// be done later.
@@ -6976,6 +6980,10 @@ func genssa(f *ssa.Func, pp *objw.Progs) {
 				}
 				return bstart[b].Pc
 			case ssa.BlockEnd.ID:
+				blk := f.Blocks[idToIdx[b]]
+				nv := len(blk.Values)
+				return valueToProgAfter[blk.Values[nv-1].ID].Pc
+			case ssa.FuncEnd.ID:
 				return e.curfn.LSym.Size
 			default:
 				return valueToProgAfter[v].Pc
