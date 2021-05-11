287f13b22383b24e1a5426dd140c4216d1e00d66
fanzha02 <fannie.zhang@arm.com>
2021-05-11 11:44:24 +0800

    cmd/dist: add asan tests for global objects in testsanitizers package
    Add tests to test that -asan in Go can detect the error memory access
    to the global objects.
    
    Updates #44853.
    
    Change-Id: I612a048460b497d18389160b66e6f818342d3941

--- misc/cgo/testsanitizers/asan_test.go
+++ misc/cgo/testsanitizers/asan_test.go
@@ -5,6 +5,9 @@
 package sanitizers_test
 
 import (
+	"os/exec"
+	"regexp"
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -23,6 +26,40 @@ func TestASAN(t *testing.T) {
 		t.Skipf("skipping on %s/%s; -asan option is not supported.", goos, goarch)
 	}
 
+	// So far, the current implementation is only compatible with the ASan library from
+	// version v7 to v9. Therefore, to use the -asan option, a compatible version of ASan
+	// library must be used, otherwise a segmentation fault will occur.
+	cc, err := goEnv("CC")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if cc != "gcc" && cc != "clang" {
+		t.Skipf("skipping: The expected C compiler is gcc or clang, not %s", cc)
+	}
+	out, err := exec.Command(cc, "--version").CombinedOutput()
+	if err != nil {
+		t.Skipf("skipping: error executing C compiler %s: %v", cc, err)
+	}
+	re := regexp.MustCompile(`([0-9]+)\.([0-9]+)\.0`)
+	matches := re.FindSubmatch(out)
+	if len(matches) < 3 {
+		t.Skipf("skipping: can't determine C compiler %s version from\n%s\n", cc, out)
+	}
+	major, err1 := strconv.Atoi(string(matches[1]))
+	minor, err2 := strconv.Atoi(string(matches[2]))
+	if err1 != nil || err2 != nil {
+		t.Skipf("skipping: can't determine C compiler %s version: %v, %v", cc, err1, err2)
+	}
+	if cc == "gcc" {
+		if major < 7 {
+			t.Skipf("skipping: too old version of gcc %d.%d uses v6 or lower version of ASan library", major, minor)
+		}
+	} else {
+		if major < 4 {
+			t.Skipf("skipping: too old version of clang %d.%d uses v6 or lower version of ASan library", major, minor)
+		}
+	}
+
 	t.Parallel()
 	requireOvercommit(t)
 	config := configure("address")
@@ -39,6 +76,10 @@ func TestASAN(t *testing.T) {
 		{src: "asan3_fail.go"},
 		{src: "asan4_fail.go"},
 		{src: "asan_useAfterReturn.go", noWantErr: true},
+		{src: "asan_global1_fail.go"},
+		{src: "asan_global2_fail.go"},
+		{src: "asan_global3_fail.go"},
+		{src: "asan_global4_fail.go"},
 	}
 	for _, tc := range cases {
 		tc := tc
