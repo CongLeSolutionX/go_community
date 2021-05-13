87df1edf2405cf40372de0ac28fbc839e72cf519
Jason A. Donenfeld <Jason@zx2c4.com>
2021-05-13 13:19:14 +0200

    syscall: do not pass console handles to PROC_THREAD_ATTRIBUTE_HANDLE_LIST on Windows 7
    On Windows 7 (and below), console handles are not real kernel handles
    but are rather userspace objects, with information passed via special
    bits in the handle itself. That means they can't be passed in
    PROC_THREAD_ATTRIBUTE_HANDLE_LIST, even though they can be inherited.
    So, we filter the list passed to PROC_THREAD_ATTRIBUTE_HANDLE_LIST to
    not have any console handles on Windows 7. At the same time, it turns
    out that the presence of a NULL handle in the list is enough to render
    PROC_THREAD_ATTRIBUTE_HANDLE_LIST completely useless, so filter these
    out too.
    
    Fixes #45914.
    
    Change-Id: I70b4ff4874dbf0507d9ec9278f63b9b4dd4f1999

--- src/syscall/exec_windows.go
+++ src/syscall/exec_windows.go
@@ -351,19 +351,42 @@ func StartProcess(argv0 string, argv []string, attr *ProcAttr) (pid int, handle 
 	si.StdErr = fd[2]
 
 	fd = append(fd, sys.AdditionalInheritedHandles...)
-	// Do not accidentally inherit more than these handles.
-	err = updateProcThreadAttribute(si.ProcThreadAttributeList, 0, _PROC_THREAD_ATTRIBUTE_HANDLE_LIST, unsafe.Pointer(&fd[0]), uintptr(len(fd))*unsafe.Sizeof(fd[0]), nil, nil)
-	if err != nil {
-		return 0, 0, err
+	// On Windows 7, console handles aren't real handles, so don't pass them
+	// through to PROC_THREAD_ATTRIBUTE_HANDLE_LIST.
+	var maj, min, build uint32
+	rtlGetNtVersionNumbers(&maj, &min, &build)
+	if maj < 6 || (maj == 6 && min <= 1) {
+		for i := range fd {
+			if ftype, err := GetFileType(fd[i]); err == nil && ftype == FILE_TYPE_CHAR {
+				fd[i] = 0
+			}
+		}
+	}
+	// The presence of a NULL handle in the list is enough to cause PROC_THREAD_ATTRIBUTE_HANDLE_LIST
+	// to treat the entire list as empty, so remove NULL handles.
+	j := 0
+	for i := range fd {
+		if fd[i] != 0 {
+			fd[j] = fd[i]
+			j++
+		}
+	}
+	fd = fd[:j]
+	if len(fd) > 0 {
+		// Do not accidentally inherit more than these handles.
+		err = updateProcThreadAttribute(si.ProcThreadAttributeList, 0, _PROC_THREAD_ATTRIBUTE_HANDLE_LIST, unsafe.Pointer(&fd[0]), uintptr(len(fd))*unsafe.Sizeof(fd[0]), nil, nil)
+		if err != nil {
+			return 0, 0, err
+		}
 	}
 
 	pi := new(ProcessInformation)
 
 	flags := sys.CreationFlags | CREATE_UNICODE_ENVIRONMENT | _EXTENDED_STARTUPINFO_PRESENT
 	if sys.Token != 0 {
-		err = CreateProcessAsUser(sys.Token, argv0p, argvp, sys.ProcessAttributes, sys.ThreadAttributes, !sys.NoInheritHandles, flags, createEnvBlock(attr.Env), dirp, &si.StartupInfo, pi)
+		err = CreateProcessAsUser(sys.Token, argv0p, argvp, sys.ProcessAttributes, sys.ThreadAttributes, len(fd) > 0 && !sys.NoInheritHandles, flags, createEnvBlock(attr.Env), dirp, &si.StartupInfo, pi)
 	} else {
-		err = CreateProcess(argv0p, argvp, sys.ProcessAttributes, sys.ThreadAttributes, !sys.NoInheritHandles, flags, createEnvBlock(attr.Env), dirp, &si.StartupInfo, pi)
+		err = CreateProcess(argv0p, argvp, sys.ProcessAttributes, sys.ThreadAttributes, len(fd) > 0 && !sys.NoInheritHandles, flags, createEnvBlock(attr.Env), dirp, &si.StartupInfo, pi)
 	}
 	if err != nil {
 		return 0, 0, err
