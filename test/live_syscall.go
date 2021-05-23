1003dbefe75597e7a3d77628ea7dd863fe8f57a6
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2021-05-23 11:58:50 +0700

    cmd/compile: mark OVARLIVE variables not require stack objects
    The liveness analysis and the stack maps have already taken care of
    keeping those variables alive, so we don't need to emit stack objects
    to track their liveness.
    
    Does not pass toolstash -cmp, since when we now do not emit stack
    objects for syscall.Syscall-like functions.
    
    Updates #43753
    
    Change-Id: Iebc0b1cb0749a4dcc3e7c735d7852335ddb2aa28

--- test/live_syscall.go
+++ test/live_syscall.go
@@ -19,22 +19,22 @@ func f(uintptr) // ERROR "assuming arg#1 is unsafe uintptr"
 
 func g() { // ERROR "can inline g"
 	var t int
-	f(uintptr(unsafe.Pointer(&t))) // ERROR "live at call to f: .?autotmp" "stack object .autotmp_[0-9]+ unsafe.Pointer$"
+	f(uintptr(unsafe.Pointer(&t))) // ERROR "live at call to f: .?autotmp"
 }
 
 func h() { // ERROR "can inline h"
 	var v int
-	syscall.Syscall(0, 1, uintptr(unsafe.Pointer(&v)), 2) // ERROR "live at call to Syscall: .?autotmp" "stack object .autotmp_[0-9]+ unsafe.Pointer$"
+	syscall.Syscall(0, 1, uintptr(unsafe.Pointer(&v)), 2) // ERROR "live at call to Syscall: .?autotmp"
 }
 
 func i() { // ERROR "can inline i"
 	var t int
 	p := unsafe.Pointer(&t)
-	f(uintptr(p))           // ERROR "live at call to f: .?autotmp" "stack object .autotmp_[0-9]+ unsafe.Pointer$"
+	f(uintptr(p)) // ERROR "live at call to f: .?autotmp"
 }
 
 func j() { // ERROR "can inline j"
 	var v int
 	p := unsafe.Pointer(&v)
-	syscall.Syscall(0, 1, uintptr(p), 2) // ERROR "live at call to Syscall: .?autotmp" "stack object .autotmp_[0-9]+ unsafe.Pointer$"
+	syscall.Syscall(0, 1, uintptr(p), 2) // ERROR "live at call to Syscall: .?autotmp"
 }
