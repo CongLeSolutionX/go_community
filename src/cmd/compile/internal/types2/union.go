dad8f9d69dcae0ec3bfd5eee5be36273d16fcec5
Robert Griesemer <gri@golang.org>
2021-05-27 19:03:16 -0700

    [dev.typeparams] cmd/compile/internal/types2: clean up type set/union intersection
    - Elimiate the need for bottom type: This is now represented by
      an empty union (denoting the set of no types).
    
    - Clean up type set intersection and incorporate tilde information
      in intersection operation and satisfaction tests.
    
    - Minor cleanups along the way.
    
    Change-Id: I7fa19df5996da36a4d8f29300d30a0aa4d8a3e5c

--- src/cmd/compile/internal/types2/union.go
+++ src/cmd/compile/internal/types2/union.go
@@ -16,8 +16,12 @@ type Union struct {
 	tilde []bool // if tilde[i] is set, terms[i] is of the form ~T
 }
 
-func NewUnion(types []Type, tilde []bool) Type { return newUnion(types, tilde) }
+// NewUnion returns a new Union type with the given terms (types[i], tilde[i]).
+// The lengths of both arguments must match. An empty union represents the set
+// of no types.
+func NewUnion(types []Type, tilde []bool) *Union { return newUnion(types, tilde) }
 
+func (u *Union) IsEmpty() bool           { return len(u.types) == 0 }
 func (u *Union) NumTerms() int           { return len(u.types) }
 func (u *Union) Term(i int) (Type, bool) { return u.types[i], u.tilde[i] }
 
@@ -27,10 +31,12 @@ func (u *Union) String() string   { return TypeString(u, nil) }
 // ----------------------------------------------------------------------------
 // Implementation
 
-func newUnion(types []Type, tilde []bool) Type {
+var emptyUnion = new(Union)
+
+func newUnion(types []Type, tilde []bool) *Union {
 	assert(len(types) == len(tilde))
-	if types == nil {
-		return nil
+	if len(types) == 0 {
+		return emptyUnion
 	}
 	t := new(Union)
 	t.types = types
@@ -40,7 +46,7 @@ func newUnion(types []Type, tilde []bool) Type {
 
 // is reports whether f returned true for all terms (type, tilde) of u.
 func (u *Union) is(f func(Type, bool) bool) bool {
-	if u == nil {
+	if u.IsEmpty() {
 		return false
 	}
 	for i, t := range u.types {
@@ -53,7 +59,7 @@ func (u *Union) is(f func(Type, bool) bool) bool {
 
 // is reports whether f returned true for the underlying types of all terms of u.
 func (u *Union) underIs(f func(Type) bool) bool {
-	if u == nil {
+	if u.IsEmpty() {
 		return false
 	}
 	for _, t := range u.types {
@@ -130,22 +136,16 @@ func parseTilde(check *Checker, x syntax.Expr) (Type, bool) {
 	return check.anyType(x), tilde
 }
 
-// intersect computes the intersection of the types x and y.
-// Note: An incomming nil type stands for the top type. A top
-// type result is returned as nil.
+// intersect computes the intersection of the types x and y,
+// A nil type stands for the set of all types; an empty union
+// stands for the set of no types.
 func intersect(x, y Type) (r Type) {
-	defer func() {
-		if r == theTop {
-			r = nil
-		}
-	}()
-
+	// If one of the types is nil (no restrictions)
+	// the result is the other type.
 	switch {
-	case x == theBottom || y == theBottom:
-		return theBottom
-	case x == nil || x == theTop:
+	case x == nil:
 		return y
-	case y == nil || x == theTop:
+	case y == nil:
 		return x
 	}
 
@@ -155,26 +155,34 @@ func intersect(x, y Type) (r Type) {
 	switch {
 	case xu != nil && yu != nil:
 		// Quadratic algorithm, but good enough for now.
-		// TODO(gri) fix this
 		var types []Type
 		var tilde []bool
-		for _, y := range yu.types {
-			if includes(xu.types, y) {
-				types = append(types, y)
-				tilde = append(tilde, true) // TODO(gri) fix this
+		for j, y := range yu.types {
+			if i := xu.indexOf(y); i >= 0 {
+				// Terms x[i] and y[j] match: Select the one that
+				// is not a ~t because that is the intersection
+				// type. If both are ~t, they are identical:
+				//  T ∩  T =  T
+				//  T ∩ ~t =  T
+				// ~t ∩  T =  T
+				// ~t ∩ ~t = ~t
+				typ := y // = yu.types[j]
+				if yu.tilde[j] {
+					typ = xu.types[i]
+				}
+				types = append(types, typ)
+				tilde = append(tilde, xu.tilde[i] && yu.tilde[j])
 			}
 		}
-		if types != nil {
-			return newUnion(types, tilde)
-		}
+		return newUnion(types, tilde)
 
 	case xu != nil:
-		if includes(xu.types, y) {
+		if xu.indexOf(y) >= 0 {
 			return y
 		}
 
 	case yu != nil:
-		if includes(yu.types, x) {
+		if yu.indexOf(x) >= 0 {
 			return x
 		}
 
@@ -184,5 +192,30 @@ func intersect(x, y Type) (r Type) {
 		}
 	}
 
-	return theBottom
+	return emptyUnion
+}
+
+// includes reports whether typ is in list.
+func includes(list []Type, typ Type) bool {
+	for _, e := range list {
+		if Identical(typ, e) {
+			return true
+		}
+	}
+	return false
+}
+
+// indexOf reports the index of the term in the union u that matches typ.
+func (u *Union) indexOf(typ Type) int {
+	utyp := under(typ)
+	for i, t := range u.types {
+		tt := typ
+		if u.tilde[i] {
+			tt = utyp
+		}
+		if Identical(tt, t) {
+			return i
+		}
+	}
+	return -1
 }
