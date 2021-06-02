d5eb3a0aae53dafda468b7e4d9e998474334781c
Robert Griesemer <gri@golang.org>
2021-06-01 23:05:25 -0700

    [dev.typeparams] cmd/compile/internal/importer: review of exportdata.go
    This CL removes the // UNREVIEWED disclaimer at the top of the
    file. This file is essentially a copy of its reviewed version
    at src/go/internal/gcimporter/exportdata.go with adjustments to
    make it work for the compiler and types2. To see the changes
    made with respect to the original, compare patchset 1 against
    patchset 2.
    
    Change-Id: I276d898ef238afa37ec6b9605496407df36cf7d0

--- src/cmd/compile/internal/importer/exportdata.go
+++ src/cmd/compile/internal/importer/exportdata.go
@@ -1,11 +1,10 @@
-// UNREVIEWED
 // Copyright 2011 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 // This file implements FindExportData.
 
-package importer
+package gcimporter
 
 import (
 	"bufio"
