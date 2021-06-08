34be1a5ac54f3111fe7673c246e808ce99082e48
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2021-06-08 19:28:45 +0700

    cmd/compile: prevent duplicated works in WriteRuntimeTypes
    While processing signatset, the entry is deleted immediately after being
    pushed to signatslice. Then calling writeType may add the same type
    to signatset again. That would add more works, though not a big impact
    to the performace, since when writeType is guarded by s.Siggen() check.
    
    Instead, using a map[*types.Type]bool helps removing duplicated works,
    by marking already processed types as true, and only removing them at
    the end of each signatset processing round.
    
    This change does not affect compiler performace, but help debugging
    issue like one in #46386 easier.
    
    Change-Id: Iddafe773885fa21cb7003ba27ddf9554fc3f297d

--- src/cmd/compile/internal/reflectdata/reflect.go
+++ src/cmd/compile/internal/reflectdata/reflect.go
@@ -50,7 +50,7 @@ func CountTabs() (numPTabs, numITabs int) {
 // runtime interface and reflection data structures
 var (
 	signatmu    sync.Mutex // protects signatset and signatslice
-	signatset   = make(map[*types.Type]struct{})
+	signatset   = make(map[*types.Type]bool)
 	signatslice []*types.Type
 
 	gcsymmu  sync.Mutex // protects gcsymset and gcsymslice
@@ -1311,7 +1311,7 @@ func NeedRuntimeType(t *types.Type) {
 		return
 	}
 	if _, ok := signatset[t]; !ok {
-		signatset[t] = struct{}{}
+		signatset[t] = false
 		signatslice = append(signatslice, t)
 	}
 }
@@ -1325,7 +1325,7 @@ func WriteRuntimeTypes() {
 		// Transfer entries to a slice and sort, for reproducible builds.
 		for _, t := range signatslice {
 			signats = append(signats, typeAndStr{t: t, short: types.TypeSymName(t), regular: t.String()})
-			delete(signatset, t)
+			signatset[t] = true
 		}
 		signatslice = signatslice[:0]
 		sort.Sort(typesByString(signats))
@@ -1334,6 +1334,11 @@ func WriteRuntimeTypes() {
 			writeType(t)
 			if t.Sym() != nil {
 				writeType(types.NewPtr(t))
+			}
+		}
+		for t, writtenType := range signatset {
+			if writtenType {
+				delete(signatset, t)
 			}
 		}
 	}
