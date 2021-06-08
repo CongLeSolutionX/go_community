a7b1d083fcc2a1db3c61792868239c06a3dcb713
Lúcás Meier <cronokirby@gmail.com>
2021-06-08 21:36:06 +0200

    crypto/rsa: replace big.Int for encryption and decryption
    Infamously, big.Int does not provide constant-time arithmetic, making
    its use in cryptographic code quite tricky. RSA uses big.Int
    pervasively, in its public API, for key generation, precomputation, and
    for encryption and decryption. This is a known problem. One mitigation,
    blinding, is already in place during decryption. This helps mitigate the
    very leaky exponentiation operation. Because big.Int is fundamentally
    not constant-time, it's unfortunately difficult to guarantee that
    mitigations like these are completely effective.
    
    This patch removes the use of big.Int for encryption and decryption,
    replacing it with an internal nat type instead. Signing and verification
    are also affected, because they depends on encryption and decryption.
    
    Overall, this patch degrades performance by 55% for private key
    operations, and 4-5x for (much faster) public key operations.
    (Signatures do both, so the slowdown is worse than decryption.)
    
    name                    old time/op  new time/op    delta
    DecryptPKCS1v15/2048-8  1.50ms ± 0%    2.34ms ± 0%    +56.44%  (p=0.000 n=8+10)
    DecryptPKCS1v15/3072-8  4.40ms ± 0%    6.79ms ± 0%    +54.33%  (p=0.000 n=10+9)
    DecryptPKCS1v15/4096-8  9.31ms ± 0%   15.14ms ± 0%    +62.60%  (p=0.000 n=10+10)
    EncryptPKCS1v15/2048-8  8.16µs ± 0%  355.58µs ± 0%  +4258.90%  (p=0.000 n=10+9)
    DecryptOAEP/2048-8      1.50ms ± 0%    2.34ms ± 0%    +55.68%  (p=0.000 n=10+9)
    EncryptOAEP/2048-8      8.51µs ± 0%  355.95µs ± 0%  +4082.75%  (p=0.000 n=10+9)
    SignPKCS1v15/2048-8     1.51ms ± 0%    2.69ms ± 0%    +77.94%  (p=0.000 n=10+10)
    VerifyPKCS1v15/2048-8   7.25µs ± 0%  354.34µs ± 0%  +4789.52%  (p=0.000 n=9+9)
    SignPSS/2048-8          1.51ms ± 0%    2.70ms ± 0%    +78.80%  (p=0.000 n=9+10)
    VerifyPSS/2048-8        8.27µs ± 1%  355.65µs ± 0%  +4199.39%  (p=0.000 n=10+10)
    
    Keep in mind that this is without any assembly at all, and that further
    improvements are likely possible. I think having a review of the logic
    and the cryptography would be a good idea at this stage, before we
    complicate the code too much through optimization.
    
    The bulk of the work is in nat.go. This introduces two new types: nat,
    representing natural numbers, and modulus, representing moduli used in
    modular arithmetic.
    
    A nat has an "announced size", which may be larger than its "true size",
    the number of bits needed to represent this number. Operations on a nat
    will only ever leak its announced size, never its true size, or other
    information about its value. The size of a nat is always clear based on
    how its value is set. For example, x.mod(y, m) will make the announced
    size of x match that of m, since x is reduced modulo m.
    
    Operations will require that the announced size of the operands match
    what's expected (with a few exceptions). For example, x.modAdd(y, m)
    will require that x and y have the same announced size as m, and that
    they're reduced modulo m.
    
    Nats are represented over unsatured bits.UintSize - 1 bit limbs. This
    means that we can't reuse the assembly routines for big.Int, which use
    saturated bits.UintSize limbs. The advantage of unsaturated limbs is
    that it makes Montgomery multiplication faster, by needing fewer
    registers in a hot loop. This makes exponentiation faster, which
    consists of many Montgomery multiplications.
    
    Moduli use nat internally. Unlike nat, the true size of a modulus always
    matches its announced size. When creating a modulus, any zero padding is
    removed. Moduli will also precompute constants when created, which is
    another reason why having a separate type is desirable.
    
    Updates #20654
    
    Co-authored-by: Filippo Valsorda <filippo@golang.org>
    Change-Id: I73b61f87d58ab912e80a9644e255d552cbadcced

---
+++ src/crypto/rsa/nat_test.go
@@ -0,0 +1,337 @@
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package rsa
+
+import (
+	"bytes"
+	"math/big"
+	"math/bits"
+	"math/rand"
+	"reflect"
+	"testing"
+	"testing/quick"
+)
+
+// Generate generates an even nat.
+func (*nat) Generate(r *rand.Rand, size int) reflect.Value {
+	limbs := make([]uint, size)
+	for i := 0; i < size; i++ {
+		limbs[i] = uint(r.Uint64()) & ((1 << _W) - 2)
+	}
+	return reflect.ValueOf(&nat{limbs})
+}
+
+func testModAddCommutative(a *nat, b *nat) bool {
+	mLimbs := make([]uint, len(a.limbs))
+	for i := 0; i < len(mLimbs); i++ {
+		mLimbs[i] = _MASK
+	}
+	m := modulusFromNat(&nat{mLimbs})
+	aPlusB := a.clone()
+	aPlusB.modAdd(b, m)
+	bPlusA := b.clone()
+	bPlusA.modAdd(a, m)
+	return aPlusB.cmpEq(bPlusA) == 1
+}
+
+func TestModAddCommutative(t *testing.T) {
+	err := quick.Check(testModAddCommutative, &quick.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func testModSubThenAddIdentity(a *nat, b *nat) bool {
+	mLimbs := make([]uint, len(a.limbs))
+	for i := 0; i < len(mLimbs); i++ {
+		mLimbs[i] = _MASK
+	}
+	m := modulusFromNat(&nat{mLimbs})
+	original := a.clone()
+	a.modSub(b, m)
+	a.modAdd(b, m)
+	return a.cmpEq(original) == 1
+}
+
+func TestModSubThenAddIdentity(t *testing.T) {
+	err := quick.Check(testModSubThenAddIdentity, &quick.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func testMontgomeryRoundtrip(a *nat) bool {
+	one := &nat{make([]uint, len(a.limbs))}
+	one.limbs[0] = 1
+	aPlusOne := a.clone()
+	aPlusOne.add(1, one)
+	m := modulusFromNat(aPlusOne)
+	monty := a.clone()
+	monty.montgomeryRepresentation(m)
+	aAgain := monty.clone()
+	aAgain.montgomeryMul(monty, one, m)
+	return a.cmpEq(aAgain) == 1
+}
+
+func TestMontgomeryRoundtrip(t *testing.T) {
+	err := quick.Check(testMontgomeryRoundtrip, &quick.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestFromBigExamples(t *testing.T) {
+	expected := []byte{0x01, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}
+	theBig := new(big.Int).SetBytes(expected)
+	actual := natFromBig(theBig).fillBytes(make([]byte, len(expected)))
+	if !bytes.Equal(actual, expected) {
+		t.Errorf("%+x != %+x", actual, expected)
+	}
+}
+
+func TestFillBytes(t *testing.T) {
+	xBytes := []byte{0xAA, 0xFF, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88}
+	x := natFromBytes(xBytes)
+	for l := 20; l >= len(xBytes); l-- {
+		buf := make([]byte, l)
+		rand.Read(buf)
+		actual := x.fillBytes(buf)
+		expected := make([]byte, l)
+		copy(expected[l-len(xBytes):], xBytes)
+		if !bytes.Equal(actual, expected) {
+			t.Errorf("%d: %+v != %+v", l, actual, expected)
+		}
+	}
+	for l := len(xBytes) - 1; l >= 0; l-- {
+		(func() {
+			defer func() {
+				if recover() == nil {
+					t.Errorf("%d: expected panic", l)
+				}
+			}()
+			x.fillBytes(make([]byte, l))
+		})()
+	}
+}
+
+func TestFromBytes(t *testing.T) {
+	xBytes := []byte{0xFF, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88}
+	actual := natFromBytes(xBytes).fillBytes(make([]byte, len(xBytes)))
+	if !bytes.Equal(actual, xBytes) {
+		t.Errorf("%+x != %+x", actual, xBytes)
+	}
+}
+
+func TestShiftInExamples(t *testing.T) {
+	if bits.UintSize != 64 {
+		t.Skip("examples are only valid in 64 bit")
+	}
+	examples := []struct {
+		m, x, expected []byte
+		y              uint64
+	}{{
+		m:        []byte{13},
+		x:        []byte{0},
+		y:        0x7FFF_FFFF_FFFF_FFFF,
+		expected: []byte{7},
+	}, {
+		m:        []byte{13},
+		x:        []byte{7},
+		y:        0x7FFF_FFFF_FFFF_FFFF,
+		expected: []byte{11},
+	}, {
+		m:        []byte{0x06, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0d},
+		x:        make([]byte, 9),
+		y:        0x7FFF_FFFF_FFFF_FFFF,
+		expected: []byte{0x00, 0x7f, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+	}, {
+		m:        []byte{0x06, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0d},
+		x:        []byte{0x00, 0x7f, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+		y:        0,
+		expected: []byte{0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08},
+	}}
+
+	for i, tt := range examples {
+		m := modulusFromNat(natFromBytes(tt.m))
+		got := natFromBytes(tt.x).expandFor(m).shiftIn(uint(tt.y), m)
+		if got.cmpEq(natFromBytes(tt.expected).expandFor(m)) != 1 {
+			t.Errorf("%d: got %x, expected %x", i, got, tt.expected)
+		}
+	}
+}
+
+func TestMod(t *testing.T) {
+	m := modulusFromNat(&nat{[]uint{13, 13}})
+	x := &nat{[]uint{1, 1, 1}}
+	out := new(nat)
+	out.mod(x, m)
+	expected := &nat{[]uint{9, 8}}
+	if out.cmpEq(expected) != 1 {
+		t.Errorf("%+v != %+v", out, expected)
+	}
+}
+
+func TestModSubExamples(t *testing.T) {
+	m := modulusFromNat(&nat{[]uint{13}})
+	x := &nat{[]uint{6}}
+	y := &nat{[]uint{7}}
+	x.modSub(y, m)
+	expected := &nat{[]uint{12}}
+	if x.cmpEq(expected) != 1 {
+		t.Errorf("%+v != %+v", x, expected)
+	}
+	x.modSub(y, m)
+	expected = &nat{[]uint{5}}
+	if x.cmpEq(expected) != 1 {
+		t.Errorf("%+v != %+v", x, expected)
+	}
+}
+
+func TestModAddExamples(t *testing.T) {
+	m := modulusFromNat(&nat{[]uint{13}})
+	x := &nat{[]uint{6}}
+	y := &nat{[]uint{7}}
+	x.modAdd(y, m)
+	expected := &nat{[]uint{0}}
+	if x.cmpEq(expected) != 1 {
+		t.Errorf("%+v != %+v", x, expected)
+	}
+	x.modAdd(y, m)
+	expected = &nat{[]uint{7}}
+	if x.cmpEq(expected) != 1 {
+		t.Errorf("%+v != %+v", x, expected)
+	}
+}
+
+func TestExpExamples(t *testing.T) {
+	m := modulusFromNat(&nat{[]uint{13}})
+	x := &nat{[]uint{3}}
+	out := &nat{[]uint{0}}
+	out.exp(x, []byte{12}, m)
+	expected := &nat{[]uint{1}}
+	if out.cmpEq(expected) != 1 {
+		t.Errorf("%+v != %+v", out, expected)
+	}
+}
+
+func makeBenchmarkModulus() *modulus {
+	m := make([]uint, 32)
+	for i := 0; i < 32; i++ {
+		m[i] = _MASK
+	}
+	return modulusFromNat(&nat{limbs: m})
+}
+
+func makeBenchmarkValue() *nat {
+	x := make([]uint, 32)
+	for i := 0; i < 32; i++ {
+		x[i] = _MASK - 1
+	}
+	return &nat{limbs: x}
+}
+
+func makeBenchmarkExponent() []byte {
+	e := make([]byte, 256)
+	for i := 0; i < 32; i++ {
+		e[i] = 0xFF
+	}
+	return e
+}
+
+func BenchmarkModAdd(b *testing.B) {
+	b.StopTimer()
+
+	x := makeBenchmarkValue()
+	y := makeBenchmarkValue()
+	m := makeBenchmarkModulus()
+
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		x.modAdd(y, m)
+	}
+}
+
+func BenchmarkModSub(b *testing.B) {
+	b.StopTimer()
+
+	x := makeBenchmarkValue()
+	y := makeBenchmarkValue()
+	m := makeBenchmarkModulus()
+
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		x.modSub(y, m)
+	}
+}
+
+func BenchmarkMontgomeryRepr(b *testing.B) {
+	b.StopTimer()
+
+	x := makeBenchmarkValue()
+	m := makeBenchmarkModulus()
+
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		x.montgomeryRepresentation(m)
+	}
+}
+
+func BenchmarkMontgomeryMul(b *testing.B) {
+	b.StopTimer()
+
+	x := makeBenchmarkValue()
+	y := makeBenchmarkValue()
+	out := makeBenchmarkValue()
+	m := makeBenchmarkModulus()
+
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		out.montgomeryMul(x, y, m)
+	}
+}
+
+func BenchmarkModMul(b *testing.B) {
+	b.StopTimer()
+
+	x := makeBenchmarkValue()
+	y := makeBenchmarkValue()
+	m := makeBenchmarkModulus()
+
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		x.modMul(y, m)
+	}
+}
+
+func BenchmarkExpBig(b *testing.B) {
+	b.StopTimer()
+
+	out := new(big.Int)
+	exponentBytes := makeBenchmarkExponent()
+	x := new(big.Int).SetBytes(exponentBytes)
+	e := new(big.Int).SetBytes(exponentBytes)
+	n := new(big.Int).SetBytes(exponentBytes)
+	one := new(big.Int).SetUint64(1)
+	n.Add(n, one)
+
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		out.Exp(x, e, n)
+	}
+}
+
+func BenchmarkExp(b *testing.B) {
+	b.StopTimer()
+
+	x := makeBenchmarkValue()
+	e := makeBenchmarkExponent()
+	out := makeBenchmarkValue()
+	m := makeBenchmarkModulus()
+
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		out.exp(x, e, m)
+	}
+}
