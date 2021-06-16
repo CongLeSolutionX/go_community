6af1265f48af22ba7d21e15c8808a9932387df1a
Michael Matloob <matloob@golang.org>
2021-06-16 18:46:23 -0400

    [dev.cmdgo] cmd/go: add go mod editwork command
    go mod editwork behaves similarly to go mod edit:
    it has flags to change the go version, and add and remove
    directory and replace directives.
    
    For #45713
    Change-Id: I1c795c122bfe461d6e87dd731692e0bf1bbe2bf7

--- src/cmd/go/alldocs.go
+++ src/cmd/go/alldocs.go
@@ -1032,6 +1032,7 @@
 //
 // 	download    download modules to local cache
 // 	edit        edit go.mod from tools or scripts
+// 	editwork    edit go.work from tools or scripts
 // 	graph       print module requirement graph
 // 	init        initialize new module in current directory
 // 	initwork    initialize workspace file
@@ -1189,6 +1190,15 @@
 // use 'go list -m -json all'.
 //
 // See https://golang.org/ref/mod#go-mod-edit for more about 'go mod edit'.
+//
+//
+// Edit go.work from tools or scripts
+//
+// Usage:
+//
+// 	go mod editwork [editing flags] [go.work]
+//
+// TODO long form documentatino
 //
 //
 // Print module requirement graph
