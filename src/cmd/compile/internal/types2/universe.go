a8a42325cc7babb4744943b4f2bfdb0236841c4f
Robert Griesemer <gri@golang.org>
2021-08-04 15:18:37 -0700

    [dev.typeparams] cmd/compile/internal/types2: better names for things (cleanup)
    - use the symbol 𝓤 instead of ⊤ to denote the set of all types
      (for better readabilty, ⊤ is hard to distinguish from T in some
      fonts)
    
    - use isAll instead of isTop to test for the set of all types
    
    - use 𝓤termlist instead of topTermlist to denote the termlist
      representing all types
    
    Change-Id: Idcb0b3398782b38653338e65173c0dbb935e430a

--- src/cmd/compile/internal/types2/universe.go
+++ src/cmd/compile/internal/types2/universe.go
@@ -99,7 +99,7 @@ func defPredeclaredTypes() {
 	{
 		obj := NewTypeName(nopos, nil, "comparable", nil)
 		obj.setColor(black)
-		ityp := &Interface{obj, nil, nil, nil, true, &TypeSet{true, nil, topTermlist}}
+		ityp := &Interface{obj, nil, nil, nil, true, &TypeSet{true, nil, 𝓤termlist}}
 		NewNamed(obj, ityp, nil)
 		def(obj)
 	}
