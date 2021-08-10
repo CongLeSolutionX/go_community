509193e7742d221ec4a48b29e4d4bdb55c876e0d
Paul E. Murphy <murp@ibm.com>
2021-08-10 09:59:00 -0500

    cmd/asm,cmd/internal/obj/ppc64: recognize ppc64 ISA 3.1 MMA registers
    Allow the assembler frontend to match MMA register arguments added by
    ISA 3.1. The prefix "A" (for accumulator) is chosen to identify them.
    
    Updates #44549
    
    Change-Id: I363e7d1103aee19d7966829d2079c3d876621efc

--- src/cmd/internal/obj/ppc64/asm_test.go
+++ src/cmd/internal/obj/ppc64/asm_test.go
@@ -469,6 +469,7 @@ func TestAddrClassifier(t *testing.T) {
 		{obj.Addr{Type: obj.TYPE_REG, Reg: REG_SPR0 + 8}, C_LR},
 		{obj.Addr{Type: obj.TYPE_REG, Reg: REG_SPR0 + 9}, C_CTR},
 		{obj.Addr{Type: obj.TYPE_REG, Reg: REG_FPSCR}, C_FPSCR},
+		{obj.Addr{Type: obj.TYPE_REG, Reg: REG_MMA1}, C_MREG},
 
 		// Memory type arguments.
 		{obj.Addr{Type: obj.TYPE_MEM, Name: obj.NAME_GOTREF}, C_ADDR},
