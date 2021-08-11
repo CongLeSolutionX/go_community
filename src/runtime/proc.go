1adfcf9e1ba346a592a42df354b626cd30836a22
Ian Lance Taylor <iant@golang.org>
2021-08-11 15:34:29 -0700

    runtime: don't use systemstack for BeforeFork/AfterFork
    In https://golang.org/cl/140930043 syscall.BeforeFork was changed to
    call beforefork via onM. This was done because at the time BeforeFork
    was written in C but was called from Go. While the runtime was being
    converted to Go, calls to complex C functions used onM to ensure that
    enough stack space was available.
    
    In https://golang.org/cl/172260043 the syscall.BeforeFork and
    beforefork functions were rewritten into Go. In this rewrite
    syscall.BeforeFork continue to call beforefork via onM, although
    because both functions were now in Go that was no longer necessary.
    
    In https://golang.org/cl/174950043 onM was renamed to systemstack,
    producing essentially the code we have today.
    
    Therefore, the use of systemstack in syscall.BeforeFork (and
    syscall.AfterFork) is a historical relic.  Remove it.
    
    Change-Id: Ia570f556b20e8405afa6c5e707bd6f4ad18fd7ca

--- src/runtime/proc.go
+++ src/runtime/proc.go
@@ -4116,7 +4116,10 @@ func exitsyscall0(gp *g) {
 	schedule() // Never returns.
 }
 
-func beforefork() {
+// Called from syscall package before fork.
+//go:linkname syscall_runtime_BeforeFork syscall.runtime_BeforeFork
+//go:nosplit
+func syscall_runtime_BeforeFork() {
 	gp := getg().m.curg
 
 	// Block signals during a fork, so that the child does not run
@@ -4133,14 +4136,10 @@ func beforefork() {
 	gp.stackguard0 = stackFork
 }
 
-// Called from syscall package before fork.
-//go:linkname syscall_runtime_BeforeFork syscall.runtime_BeforeFork
+// Called from syscall package after fork in parent.
+//go:linkname syscall_runtime_AfterFork syscall.runtime_AfterFork
 //go:nosplit
-func syscall_runtime_BeforeFork() {
-	systemstack(beforefork)
-}
-
-func afterfork() {
+func syscall_runtime_AfterFork() {
 	gp := getg().m.curg
 
 	// See the comments in beforefork.
@@ -4149,13 +4148,6 @@ func afterfork() {
 	msigrestore(gp.m.sigmask)
 
 	gp.m.locks--
-}
-
-// Called from syscall package after fork in parent.
-//go:linkname syscall_runtime_AfterFork syscall.runtime_AfterFork
-//go:nosplit
-func syscall_runtime_AfterFork() {
-	systemstack(afterfork)
 }
 
 // inForkedChild is true while manipulating signals in the child process.
