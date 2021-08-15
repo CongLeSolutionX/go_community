7a47cff853d3ffcce6affb58a9d586ef9c4fc96a
Xiaodong Liu <liuxiaodong@loongson.cn>
2021-08-15 16:45:11 +0800

    syscall: port to linux/loong64
    Fixes: golang/go#46229
    
    Change-Id: Ia676bd2875701639314cadbd39d97620afb3f0a2

--- src/syscall/syscall_dup3_linux.go
+++ src/syscall/syscall_dup3_linux.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build android || arm64 || riscv64
-// +build android arm64 riscv64
+//go:build android || arm64 || riscv64 || loong64
+// +build android arm64 riscv64 loong64
 
 package syscall
 
