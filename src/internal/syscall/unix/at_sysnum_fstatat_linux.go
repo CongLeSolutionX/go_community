4185674bfc7700a2ac7b560ec8a8ce7b0fb278c0
Xiaodong Liu <liuxiaodong@loongson.cn>
2021-08-15 16:47:42 +0800

    internal: port to linux/loong64
    Fixes: golang/go#46229
    
    other contributors in Loongson Inc. :
    <luweining@loongson.cn>, <wanglei@loongson.cn> <gonglingqin@loongson.cn>, <zhaoxiaolin@loongson.cn>, <limeidan@loongson.cn>, <zhaixiaojuan@loongson.cn>, <puqiyuan@loongson.cn>, <chenguoqi@loongson.cn>
    
    Change-Id: I39d42e5959391e47bf621b3bdd3d95de72f023cc

--- src/internal/syscall/unix/at_sysnum_fstatat_linux.go
+++ src/internal/syscall/unix/at_sysnum_fstatat_linux.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build arm64 || riscv64
-// +build arm64 riscv64
+//go:build arm64 || riscv64 || loong64
+// +build arm64 riscv64 loong64
 
 package unix
 
