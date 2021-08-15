2a206585f1fb627432595f3bc1f16c1f53a44e2f
Xiaodong Liu <liuxiaodong@loongson.cn>
2021-08-15 17:02:32 +0800

    runtime: implement runtime basic operation for loong64
    Contributors to the loong64 port are:
      Weining Lu <luweining@loongson.cn>
      Lei Wang <wanglei@loongson.cn>
      Lingqin Gong <gonglingqin@loongson.cn>
      Xiaolin Zhao <zhaoxiaolin@loongson.cn>
      Meidan Li <limeidan@loongson.cn>
      Xiaojuan Zhai <zhaixiaojuan@loongson.cn>
      Qiyuan Pu <puqiyuan@loongson.cn>
      Guoqi Chen <chenguoqi@loongson.cn>
    
    This port has been updated to Go 1.15.6:
      https://github.com/loongson/go
    
    Updates #46229
    
    Change-Id: I252ecd0b13580c5e71723715023b1951985045f3

--- src/runtime/os_linux_generic.go
+++ src/runtime/os_linux_generic.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build !mips && !mipsle && !mips64 && !mips64le && !s390x && !ppc64 && linux
-// +build !mips,!mipsle,!mips64,!mips64le,!s390x,!ppc64,linux
+//go:build !mips && !mipsle && !mips64 && !mips64le && !loong64 && !s390x && !ppc64 && linux
+// +build !mips,!mipsle,!mips64,!mips64le,!loong64,!s390x,!ppc64,linux
 
 package runtime
 
