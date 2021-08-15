f5c6a6c2eb72850123aff8b71501cfe1bd23196e
Xiaodong Liu <liuxiaodong@loongson.cn>
2021-08-15 15:57:43 +0800

    cmd/go/internal: port to linux/loong64
    Fixes: golang/go#46229
    
    other contributors in Loongson Inc. :
    <luweining@loongson.cn>, <wanglei@loongson.cn> <gonglingqin@loongson.cn>, <zhaoxiaolin@loongson.cn>, <limeidan@loongson.cn>, <zhaixiaojuan@loongson.cn>, <puqiyuan@loongson.cn>, <chenguoqi@loongson.cn>
    
    Change-Id: I6b537a7d842b0683586917fe7ea7cd4d70d888de

--- src/cmd/go/internal/cfg/cfg.go
+++ src/cmd/go/internal/cfg/cfg.go
@@ -261,12 +261,13 @@ var (
 	GOMODCACHE   = envOr("GOMODCACHE", gopathDir("pkg/mod"))
 
 	// Used in envcmd.MkEnv and build ID computations.
-	GOARM    = envOr("GOARM", fmt.Sprint(buildcfg.GOARM))
-	GO386    = envOr("GO386", buildcfg.GO386)
-	GOMIPS   = envOr("GOMIPS", buildcfg.GOMIPS)
-	GOMIPS64 = envOr("GOMIPS64", buildcfg.GOMIPS64)
-	GOPPC64  = envOr("GOPPC64", fmt.Sprintf("%s%d", "power", buildcfg.GOPPC64))
-	GOWASM   = envOr("GOWASM", fmt.Sprint(buildcfg.GOWASM))
+	GOARM     = envOr("GOARM", fmt.Sprint(buildcfg.GOARM))
+	GO386     = envOr("GO386", buildcfg.GO386)
+	GOMIPS    = envOr("GOMIPS", buildcfg.GOMIPS)
+	GOMIPS64  = envOr("GOMIPS64", buildcfg.GOMIPS64)
+	GOLOONG64 = envOr("GOLOONG64", buildcfg.GOLOONG64)
+	GOPPC64   = envOr("GOPPC64", fmt.Sprintf("%s%d", "power", buildcfg.GOPPC64))
+	GOWASM    = envOr("GOWASM", fmt.Sprint(buildcfg.GOWASM))
 
 	GOPROXY    = envOr("GOPROXY", "https://proxy.golang.org,direct")
 	GOSUMDB    = envOr("GOSUMDB", "sum.golang.org")
@@ -293,6 +294,8 @@ func GetArchEnv() (key, val string) {
 		return "GOMIPS", GOMIPS
 	case "mips64", "mips64le":
 		return "GOMIPS64", GOMIPS64
+	case "loong64":
+		return "GOLOONG64", GOLOONG64
 	case "ppc64", "ppc64le":
 		return "GOPPC64", GOPPC64
 	case "wasm":
