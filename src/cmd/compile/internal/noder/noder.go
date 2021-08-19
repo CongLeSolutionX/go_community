44f56641172ca0388ad591c1c5374df958e4f375
Matthew Dempsky <mdempsky@google.com>
2021-08-19 15:52:53 -0700

    cmd/compile/internal/syntax: add PosBase.Trimmed
    DO NOT REVIEW
    DO NOT SUBMIT
    
    Change-Id: I7383becfb704680a36f7603e3246af38b21f100b

--- src/cmd/compile/internal/noder/noder.go
+++ src/cmd/compile/internal/noder/noder.go
@@ -191,13 +191,16 @@ func (p *noder) errorAt(pos syntax.Pos, format string, args ...interface{}) {
 	base.ErrorfAt(p.makeXPos(pos), format, args...)
 }
 
-// TODO(gri) Can we eliminate fileh in favor of absFilename?
-func fileh(name string) string {
-	return objabi.AbsFile("", name, base.Flag.TrimPath)
-}
+func trimFilename(b *syntax.PosBase) string {
+	if b.Trimmed() {
+		return b.Filename()
+	}
 
-func absFilename(name string) string {
-	return objabi.AbsFile(base.Ctxt.Pathname, name, base.Flag.TrimPath)
+	dir := ""
+	if b.IsFileBase() {
+		dir = base.Ctxt.Pathname
+	}
+	return objabi.AbsFile(dir, b.Filename(), base.Flag.TrimPath)
 }
 
 // noder transforms package syntax's AST into a Node tree.
@@ -1723,7 +1726,7 @@ func (p *noder) pragma(pos syntax.Pos, blankLine bool, text string, old syntax.P
 // (primarily misuse of linker flags), other files are not.
 // See golang.org/issue/23672.
 func isCgoGeneratedFile(pos syntax.Pos) bool {
-	return strings.HasPrefix(filepath.Base(filepath.Clean(fileh(pos.Base().Filename()))), "_cgo_")
+	return strings.HasPrefix(filepath.Base(filepath.Clean(trimFilename(pos.Base()))), "_cgo_")
 }
 
 // safeArg reports whether arg is a "safe" command-line argument,
