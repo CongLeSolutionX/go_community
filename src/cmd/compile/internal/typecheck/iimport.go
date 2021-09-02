957aaa43b15f016c412dcb8c892db1834940645a
Dan Scales <danscales@google.com>
2021-09-02 08:47:40 -0700

    cmd/compile: delay transformAssign if lhs/rhs have typeparam
    This also requires that we sometimes delay transformSelect(), if the
    assignments in the Comm part of the select have not been transformed.
    The delaying of the transformSelect() revealed a bunch of bugs.
    
    First, tcSelect() (hence also transformSelect) were sometimes dropping
    the Init part of assignment nodes as they were transformed to OSELRECV2.
    This is fixed in the previous CL in the stack.
    
    Second, OSELRECV2 nodes and assignment nodes were not correctly
    importing/exporting the Init field or the Def field. Changed things so
    the Init field is explicitly exported via stmtList, so it has a proper
    OEND marker. Also, changed to exporting/importing n.Def.
    
    Third, a quirk of iimport.go is that it automatically converts an ODCL
    node to an ODCL/OAS sequence (where the OAS is to just zero out the
    declared variable). Now that the Inits are properly fixed, o.stmt needed
    adjustment for the OSELRECV2 case to skip over the new OAS nodes
    that are inserted only on re-import.
    
    I need to talk to Matthew about why the unified tests are failing - it
    may be because the preserving of the Init nodes is changing compiler
    output.  I won't check in until I have a fix.
    
    Fixes #48137
    
    Change-Id: I163aa1f999d1e63616280dca807561b12b2aa779

--- src/cmd/compile/internal/typecheck/iimport.go
+++ src/cmd/compile/internal/typecheck/iimport.go
@@ -1607,7 +1607,12 @@ func (r *importReader) node() ir.Node {
 	// 	unreachable - never exported
 
 	case ir.OAS:
-		return ir.NewAssignStmt(r.pos(), r.expr(), r.expr())
+		pos := r.pos()
+		init := r.stmtList()
+		n := ir.NewAssignStmt(pos, r.expr(), r.expr())
+		n.SetInit(init)
+		n.Def = r.bool()
+		return n
 
 	case ir.OASOP:
 		n := ir.NewAssignOpStmt(r.pos(), r.op(), r.expr(), nil)
@@ -1624,7 +1629,12 @@ func (r *importReader) node() ir.Node {
 			// unreachable - mapped to case OAS2 by exporter
 			goto error
 		}
-		return ir.NewAssignListStmt(r.pos(), op, r.exprList(), r.exprList())
+		pos := r.pos()
+		init := r.stmtList()
+		n := ir.NewAssignListStmt(pos, op, r.exprList(), r.exprList())
+		n.SetInit(init)
+		n.Def = r.bool()
+		return n
 
 	case ir.ORETURN:
 		return ir.NewReturnStmt(r.pos(), r.exprList())
@@ -1709,7 +1719,12 @@ func (r *importReader) node() ir.Node {
 		return n
 
 	case ir.OSELRECV2:
-		return ir.NewAssignListStmt(r.pos(), ir.OSELRECV2, r.exprList(), r.exprList())
+		pos := r.pos()
+		init := r.stmtList()
+		n := ir.NewAssignListStmt(pos, ir.OSELRECV2, r.exprList(), r.exprList())
+		n.SetInit(init)
+		n.Def = r.bool()
+		return n
 
 	default:
 		base.Fatalf("cannot import %v (%d) node\n"+
