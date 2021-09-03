3e260d92e14f7c5d2166b0400bbe45427c6e7ebc
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2021-09-03 11:18:16 +0700

    go/ast: clarify when Ident.Obj is nil
    Fixes #48141
    
    Change-Id: Id20b7801d31456ffd74301ed0fd84788b8982fb1

--- src/go/ast/ast.go
+++ src/go/ast/ast.go
@@ -290,7 +290,7 @@ type (
 	Ident struct {
 		NamePos token.Pos // identifier position
 		Name    string    // identifier name
-		Obj     *Object   // denoted object; or nil
+		Obj     *Object   // denoted object; or nil if referenced declaration is in another file
 	}
 
 	// An Ellipsis node stands for the "..." type in a
