f4226fa1d67d0c10bfc10576f9d7b38824cd5376
Keith Randall <khr@golang.org>
2021-09-15 09:56:09 -0700

    reflect: make Elem panic on bad notinheap pointers
    This CL fixes the subtle issue that Elem can promote a
    not-in-heap pointer, which could be any bit pattern, into an
    unsafe.Pointer, which the garbage collector can see. If that
    resulting value is bad, it can crash the GC.
    
    Make sure that we don't introduce bad pointers that way. We can
    make Elem() panic, because any such bad pointers are in the Go heap,
    and not-in-heap pointers are not allowed to point into the Go heap.
    
    Update #48399
    
    Change-Id: Ieaf35a611b16b4dfb5e907e229ed4a2aed30e18c

--- src/reflect/all_test.go
+++ src/reflect/all_test.go
@@ -7550,3 +7550,23 @@ func TestConvertibleTo(t *testing.T) {
 		t.Fatalf("(%s).ConvertibleTo(%s) = true, want false", t3, t4)
 	}
 }
+
+//go:notinheap
+type nih struct{ x int }
+
+var global_nih = nih{x: 7}
+
+func TestNotInHeapDeref(t *testing.T) {
+	// See issue 48399.
+	v := ValueOf((*nih)(nil))
+	v.Elem()
+	shouldPanic("reflect: call of reflect.Value.Field on zero Value", func() { v.Elem().Field(0) })
+
+	v = ValueOf(&global_nih)
+	if got := v.Elem().Field(0).Int(); got != 7 {
+		t.Fatalf("got %d, want 7", got)
+	}
+
+	v = ValueOf((*nih)(unsafe.Pointer(new(int))))
+	shouldPanic("reflect: reflect.Value.Elem on an invalid notinheap pointer", func() { v.Elem() })
+}
