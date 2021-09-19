535996a4ff82d99bfba8e2e96ce5507900d2aa4c
Leonard Wang <wangdeyu0907@gmail.com>
2021-09-20 00:23:40 +0800

    cmd/compile: print expression when invalid operation errors
    Fixes #48472
    
    Change-Id: I5072ebcf53e03fb5515c51a2ad01f02d72b30719

---
+++ test/fixedbugs/issue48472.go
@@ -0,0 +1,13 @@
+// errorcheck
+
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package p
+
+func g() {
+	var s string
+	var i int
+	_ = s + i // ERROR "invalid operation: s \+ i \(mismatched types string and int\)"
+}
