0c8ee2c3fbc7680469574ac3b99597976793aed9
Dan Scales <danscales@google.com>
2021-09-20 15:34:09 -0700

    cmd/compile: don't create or instantiate methods on shape types
    We should never use or need methods on non-interface shape types. We do
    have corresponding helper functions instantiated with the appropriate
    shape types that take the dictionary and the shape-based receiver as the
    first two arguments. Each helper function has the same name as what the
    corresponding method would be, so it's best not to create the methods (which
    would create confusion for import/export).
    
    Keith noticed this issue as part of another CL -- I'm just finishing up
    the change.
    
    Change-Id: I401fbdad791bdb5792617449cad68aa8df1d9911

--- src/cmd/compile/internal/reflectdata/reflect.go
+++ src/cmd/compile/internal/reflectdata/reflect.go
@@ -356,6 +356,12 @@ func methods(t *types.Type) []*typeSig {
 
 // imethods returns the methods of the interface type t, sorted by name.
 func imethods(t *types.Type) []*typeSig {
+	if t.HasShape() && !t.IsInterface() {
+		// Non-interface shape types have no methods. (There are,
+		// corresponding, helper functions that take the dictionary and the
+		// shape type as the first two arguments.)
+		return nil
+	}
 	var methods []*typeSig
 	for _, f := range t.AllMethods().Slice() {
 		if f.Type.Kind() != types.TFUNC || f.Sym == nil {
