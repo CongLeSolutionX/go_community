3249e4aecff190777e4f3e02be1e6a1947dd3331
Josh Bleecher Snyder <josharian@gmail.com>
2021-09-23 18:03:56 -0700

    cmd/link, runtime: convert funcdata relocations to offsets
    Each func has associated numbered funcdata.
    Prior to these change, a funcdata pointer was stored as a relocation.
    Change this to be an offset relative to go.func.*.
    
    This reduces the number of relocations on darwin/arm64 by about 40%.
    It also shrinks externally linked binaries. On darwin/arm64:
    
    file      before    after     Δ        %
    addr2line 3807634   3735426   -72208   -1.896%
    api       5120738   5054450   -66288   -1.295%
    asm       4873746   4812498   -61248   -1.257%
    buildid   2503138   2472482   -30656   -1.225%
    cgo       4358882   4304994   -53888   -1.236%
    compile   22817074  22515810  -301264  -1.320%
    cover     4604338   4536642   -67696   -1.470%
    dist      3205618   3148834   -56784   -1.771%
    doc       3701090   3653874   -47216   -1.276%
    fix       3135858   3094434   -41424   -1.321%
    link      6329906   6242242   -87664   -1.385%
    nm        3757186   3718194   -38992   -1.038%
    objdump   4127442   4084098   -43344   -1.050%
    pack      2218914   2190818   -28096   -1.266%
    pprof     13539058  13317170  -221888  -1.639%
    test2json 2487522   2455442   -32080   -1.290%
    trace     10151122  9972610   -178512  -1.759%
    vet       6905570   6801714   -103856  -1.504%
    total     107644836 106111732 -1533104 -1.424%
    
    It should also incrementally speed up binary launching
    and may reduce linker memory use.
    
    This is another step towards removing relocations so
    that pages that were previously dirtied by the loader may remain clean,
    which will offer memory savings useful in constrained environments.
    
    Change-Id: I83d34c1701d6f3f515b9905941477d522441019d

--- src/cmd/link/internal/ld/symtab.go
+++ src/cmd/link/internal/ld/symtab.go
@@ -537,22 +537,6 @@ func (ctxt *Link) symtab(pcln *pclntab) []sym.SymKind {
 
 		name := ldr.SymName(s)
 		switch {
-		case strings.HasPrefix(name, "type."):
-			if !ctxt.DynlinkingGo() {
-				ldr.SetAttrNotInSymbolTable(s, true)
-			}
-			if ctxt.UseRelro() {
-				symGroupType[s] = sym.STYPERELRO
-				if symtyperel != 0 {
-					ldr.SetCarrierSym(s, symtyperel)
-				}
-			} else {
-				symGroupType[s] = sym.STYPE
-				if symtyperel != 0 {
-					ldr.SetCarrierSym(s, symtype)
-				}
-			}
-
 		case strings.HasPrefix(name, "go.importpath.") && ctxt.UseRelro():
 			// Keep go.importpath symbols in the same section as types and
 			// names, as they can be referred to by a section offset.
@@ -599,6 +583,25 @@ func (ctxt *Link) symtab(pcln *pclntab) []sym.SymKind {
 				align = a
 			}
 			liveness += (ldr.SymSize(s) + int64(align) - 1) &^ (int64(align) - 1)
+
+		// Note: Check for "type." prefix after checking for .arginfo1 suffix.
+		// That way symbols like "type..eq.[2]interface {}.arginfo1" that belong
+		// in go.func.* end up there.
+		case strings.HasPrefix(name, "type."):
+			if !ctxt.DynlinkingGo() {
+				ldr.SetAttrNotInSymbolTable(s, true)
+			}
+			if ctxt.UseRelro() {
+				symGroupType[s] = sym.STYPERELRO
+				if symtyperel != 0 {
+					ldr.SetCarrierSym(s, symtyperel)
+				}
+			} else {
+				symGroupType[s] = sym.STYPE
+				if symtyperel != 0 {
+					ldr.SetCarrierSym(s, symtype)
+				}
+			}
 		}
 	}
 
@@ -676,6 +679,7 @@ func (ctxt *Link) symtab(pcln *pclntab) []sym.SymKind {
 	moduledata.AddAddr(ctxt.Arch, ldr.Lookup("runtime.gcbss", 0))
 	moduledata.AddAddr(ctxt.Arch, ldr.Lookup("runtime.types", 0))
 	moduledata.AddAddr(ctxt.Arch, ldr.Lookup("runtime.etypes", 0))
+	moduledata.AddAddr(ctxt.Arch, ldr.Lookup("go.func.*", 0))
 
 	if ctxt.IsAIX() && ctxt.IsExternal() {
 		// Add R_XCOFFREF relocation to prevent ld's garbage collection of
