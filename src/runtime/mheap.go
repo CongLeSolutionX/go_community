256bab8d3ca2ad7e9683b3d1ba0bffe399b562d6
Michael Anthony Knyszek <mknyszek@google.com>
2021-10-04 19:52:48 +0000

    runtime: update and access scavengeGoal atomically
    The first step toward acquiring the heap lock less frequently in the
    scavenger.
    
    Change-Id: Idc69fd8602be2c83268c155951230d60e20b42fe

--- src/runtime/mheap.go
+++ src/runtime/mheap.go
@@ -113,6 +113,8 @@ type mheap struct {
 	// scavengeGoal is the amount of total retained heap memory (measured by
 	// heapRetained) that the runtime will try to maintain by returning memory
 	// to the OS.
+	//
+	// Accessed atomically.
 	scavengeGoal uint64
 
 	// Page reclaimer state
@@ -1409,9 +1411,10 @@ func (h *mheap) grow(npage uintptr) bool {
 	// By scavenging inline we deal with the failure to allocate out of
 	// memory fragments by scavenging the memory fragments that are least
 	// likely to be re-used.
-	if retained := heapRetained(); retained+uint64(totalGrowth) > h.scavengeGoal {
+	scavengeGoal := atomic.Load64(&h.scavengeGoal)
+	if retained := heapRetained(); retained+uint64(totalGrowth) > scavengeGoal {
 		todo := totalGrowth
-		if overage := uintptr(retained + uint64(totalGrowth) - h.scavengeGoal); todo > overage {
+		if overage := uintptr(retained + uint64(totalGrowth) - scavengeGoal); todo > overage {
 			todo = overage
 		}
 		h.pages.scavenge(todo, false)
