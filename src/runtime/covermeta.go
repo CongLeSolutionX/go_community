b6cdb3307f3d804471bc40e95f0f6693afaa6135
Than McIntosh <thanm@google.com>
2021-10-11 07:34:20 -0400

    runtime/coverage: runtime routines to emit coverage data
    DO NOT SUBMIT
    
    This patch fleshes out the runtime support for emitting coverage data
    at the end of a run of an instrumented binary. Data is emitted in the
    form of a pair of files, a meta-out-file and counter-data-outfile,
    each written to the dir GOCOVERDIR. The meta-out-file is emitted only
    if needed; no need to emit again if an existing meta-data file with
    the same hash and length is present.
    
    Change-Id: I59d20a4b8c05910c933ee29527972f8e401b1685

--- src/runtime/covermeta.go
+++ src/runtime/covermeta.go
@@ -4,12 +4,21 @@
 
 package runtime
 
-import "unsafe"
+import (
+	"unsafe"
+)
 
+// covmetablob is a container for holding the meta-data symbol (an
+// RODATA variable) for an instrumented Go package. Here "P" points to
+// the symbol itself, "Len" is the length of the sym in bytes, and
+// "Hash" is an md5sum for the sym computed by the compiler.
 type covmetablob struct {
-	p    unsafe.Pointer
-	len  uint32
-	hash [16]byte
+	// Important: any changes to this struct should also be made in
+	// the runtime/coverage/emit.go, since that code expects a
+	// specific format here.
+	P    *byte
+	Len  uint32
+	Hash [16]byte
 }
 
 var covmetalist []covmetablob
@@ -25,9 +34,9 @@ func addcovmeta(p unsafe.Pointer, dlen uint32, hash [16]byte, pkpath string, pki
 	slot := len(covmetalist)
 	covmetalist = append(covmetalist,
 		covmetablob{
-			p:    p,
-			len:  dlen,
-			hash: hash,
+			P:    (*byte)(p),
+			Len:  dlen,
+			Hash: hash,
 		})
 	if pkid != -1 && pkid-1 != slot {
 		// If this assert is firing, it means that the compiler's
@@ -42,4 +51,35 @@ func addcovmeta(p unsafe.Pointer, dlen uint32, hash [16]byte, pkpath string, pki
 
 	// ID zero is reserved as invalid.
 	return uint32(slot + 1)
+}
+
+func getcovmetalist() []covmetablob {
+	return covmetalist
+}
+
+// covcounterblob is a container for encapsulating a counter section
+// (BSS variable) for an instrumented Go module. Here "Counters" points to
+// the counter payload and Len is the number of uint32 entries in the
+// section.
+type covcounterblob struct {
+	// Important: any changes to this struct should also be made in
+	// the runtime/coverage/emit.go, since that code expects a
+	// specific format here.
+	Counters *uint32
+	Len      uint64
+}
+
+func getcovcounterlist() []covcounterblob {
+	res := []covcounterblob{}
+	u32sz := unsafe.Sizeof(uint32(0))
+	for datap := &firstmoduledata; datap != nil; datap = datap.next {
+		if datap.covctrs == datap.ecovctrs {
+			continue
+		}
+		res = append(res, covcounterblob{
+			Counters: (*uint32)(unsafe.Pointer(datap.covctrs)),
+			Len:      uint64((datap.ecovctrs - datap.covctrs) / u32sz),
+		})
+	}
+	return res
 }
