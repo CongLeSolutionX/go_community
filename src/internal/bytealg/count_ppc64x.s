be308936156db142bc70c63f0906eb18cb2dbed1
Archana R <aravind5@in.ibm.com>
2021-10-13 03:50:06 -0500

    internal/bytealg: port bytes.Index and bytes.Count to reg ABI on ppc64x
    This change adds support for the reg ABI to the Index and Count
    functions for ppc64/ppc64le.
    Other incidental changes to index_ppc64x.s done:
    -Invoke P9 version of IndexString for POWER9 and above which was missed
    when enablement for Index was done.
    -Modifying condition to determine if separator length crosses 16 bytes to
    BGT from BGE to avoid incorrectly crossing a page.
    
    Most Index and Count benchmarks show improvement in performance on
    POWER9 with this change. Similar numbers observed on POWER8 and POWER10.
    
    name                             old time/op    new time/op    delta
    Index/32                         71.0ns ± 0%    67.9ns ± 0%   -4.42% (p=0.001 n=7+6)
    IndexEasy/10                     17.5ns ± 0%    17.2ns ± 0%   -1.30% (p=0.001 n=7+7)
    
    name             old time/op    new time/op    delta
    Count/10           26.6ns ± 0%    25.0ns ± 1%   -6.02%  (p=0.001 n=7+7)
    Count/32           78.6ns ± 0%    74.7ns ± 0%   -4.97%  (p=0.001 n=7+7)
    Count/4K           5.03µs ± 0%    5.03µs ± 0%   -0.07%  (p=0.000 n=6+7)
    CountEasy/10       26.9ns ± 0%    25.2ns ± 1%   -6.31%  (p=0.001 n=7+7)
    CountSingle/32     11.8ns ± 0%     9.9ns ± 0%  -15.70%  (p=0.002 n=6+6)
    
    Change-Id: Ibd146c04f8107291c55f9e6100b8264dfccc41ae

--- src/internal/bytealg/count_ppc64x.s
+++ src/internal/bytealg/count_ppc64x.s
@@ -8,18 +8,27 @@
 #include "go_asm.h"
 #include "textflag.h"
 
-TEXT ·Count(SB), NOSPLIT|NOFRAME, $0-40
+TEXT ·Count<ABIInternal>(SB),NOSPLIT|NOFRAME,$0-40
+#ifdef GOEXPERIMENT_regabiargs
+        MOVBZ R6,R5
+#else
+
 	MOVD  b_base+0(FP), R3    // R3 = byte array pointer
 	MOVD  b_len+8(FP), R4     // R4 = length
 	MOVBZ c+24(FP), R5        // R5 = byte
 	MOVD  $ret+32(FP), R14    // R14 = &ret
+#endif
 	BR    countbytebody<>(SB)
 
-TEXT ·CountString(SB), NOSPLIT|NOFRAME, $0-32
+TEXT ·CountString<ABIInternal>(SB), NOSPLIT|NOFRAME, $0-32
+#ifdef GOEXPERIMENT_regabiargs
+        MOVBZ R5,R5
+#else
 	MOVD  s_base+0(FP), R3    // R3 = string
 	MOVD  s_len+8(FP), R4     // R4 = length
 	MOVBZ c+16(FP), R5        // R5 = byte
 	MOVD  $ret+24(FP), R14    // R14 = &ret
+#endif
 	BR    countbytebody<>(SB)
 
 // R3: addr of string
@@ -94,5 +103,10 @@ next2:
 	BR  small
 
 done:
+#ifdef GOEXPERIMENT_regabiargs
+        MOVD R18, R3
+#else
 	MOVD R18, (R14) // return count
+#endif
+
 	RET
