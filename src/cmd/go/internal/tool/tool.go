7d93ab4a08de26c38959fb840410c46a1db72c2e
Bryan C. Mills <bcmills@google.com>
2021-10-15 11:35:25 -0400

    cmd/go: run tests when cmd/go is cross-compiled
    When the GOOS or GOARCH of the cmd/go test binary does not match the
    GOOS or GOARCH of the installed 'go' binary itself, the test currently
    attempts to trick 'go test' into thinking that there were no test
    functions to run.
    
    That makes it very difficult to discover how to actually run the
    tests, which in turn makes it difficult to diagnose and fix
    regressions in, say, the linux-386-longtest builders. (We have had a
    few of those lately, and they shouldn't be as much of an ordeal to fix
    as they currently are.)
    
    There are three underlying problems:
    
    1. cmd/go uses its own GOOS and GOARCH to figure out which variant of
       other tools to use, and the cache keys for all installed tools and
       libraries include the IDs of the tools used to build them. So when
       cmd/go's GOARCH changes, all installed tools and binaries appear
       stale *even if* they were just installed by invoking the native
       cmd/go with the appropriate GOARCH value set.
    
    2. The "go/build" library used by cmd/go toggles its default
       CGO_ENABLED behavior depending on whether the GOOS and GOARCH being
       imported match runtime.GOOS and runtime.GOARCH.
    
    3. A handful of cmd/go tests explicitly use gccgo, but the user's
       installed gccgo binary cannot necessarily cross-compile to the same
       platforms as cmd/go.
    
    To address the cache-invalidation problem, we modify the 'testgo'
    build of cmd/go to use the host's native toolchain (as indicated by
    the new TESTGO_GOHOSTOS and TESTGO_GOHOSTARCH environment variables)
    instead of the toolchain matching 'testgo' itself. That allows a
    'testgo' compiled with GOARCH=386 to use libraries and tools
    cross-compiled by the native toolchain, so that
    
    	$ GOARCH=386 go install std cmd
    
    suffices to make the packages in std and cmd non-stale in the
    tests.
    
    To address the CGO_ENABLED mismatch, we set CGO_ENABLED explicitly in
    the test's environment whenever it may differ from the default. Since
    script tests that use cgo are already expected to use a [cgo]
    condition, setting the environment to match that condition fixes the
    cgo-specific tests.
    
    To address the gccgo-specific cross-compilation failures, we add a new
    script condition, [cross], which evaluates to true whenever the
    platform being tested differs from the native platform. We can then
    use that condition to explicitly skip the handful of gccgo tests that
    fail under cross-compilation.
    
    Change-Id: I8633944f674eb5941ccc95df928991660e7e8137

--- src/cmd/go/internal/tool/tool.go
+++ src/cmd/go/internal/tool/tool.go
@@ -8,6 +8,7 @@ package tool
 import (
 	"context"
 	"fmt"
+	"go/build"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -115,7 +116,7 @@ func runTool(ctx context.Context, cmd *base.Command, args []string) {
 
 // listTools prints a list of the available tools in the tools directory.
 func listTools() {
-	f, err := os.Open(base.ToolDir)
+	f, err := os.Open(build.ToolDir)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "go: no tool directory: %s\n", err)
 		base.SetExitStatus(2)
@@ -134,8 +135,8 @@ func listTools() {
 		// Unify presentation by going to lower case.
 		name = strings.ToLower(name)
 		// If it's windows, don't show the .exe suffix.
-		if base.ToolIsWindows && strings.HasSuffix(name, base.ToolWindowsExtension) {
-			name = name[:len(name)-len(base.ToolWindowsExtension)]
+		if suffix := cfg.ToolExeSuffix(); suffix != "" {
+			name = name[:len(name)-len(suffix)]
 		}
 		// The tool directory used by gccgo will have other binaries
 		// in addition to go tools. Only display go tools here.
