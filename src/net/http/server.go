d3e76022c3f369c092ecc03a27ad807e5285dc01
Charlie Getzen <charlie@bolt.com>
2021-10-16 22:56:09 +0000

    net/http: distinguish between timeouts and client hangups in TimeoutHandler
    Fixes #48948
    
    Change-Id: I411e3be99c7979ae289fd937388aae63d81adb59
    GitHub-Last-Rev: 3bd7427b9c79279ae1b076e8020f7e24c60b0caa
    GitHub-Pull-Request: golang/go#48993

--- src/net/http/server.go
+++ src/net/http/server.go
@@ -3305,6 +3305,10 @@ func TimeoutHandler(h Handler, dt time.Duration, msg string) Handler {
 // in handlers which have timed out.
 var ErrHandlerTimeout = errors.New("http: Handler timeout")
 
+// ErrRequestCanceled is returned on ResponseWriter Write calls
+// in handlers which have canceled contexts.
+var ErrRequestCanceled = errors.New("http: Request canceled")
+
 type timeoutHandler struct {
 	handler Handler
 	body    string
@@ -3364,9 +3368,14 @@ func (h *timeoutHandler) ServeHTTP(w ResponseWriter, r *Request) {
 	case <-ctx.Done():
 		tw.mu.Lock()
 		defer tw.mu.Unlock()
-		w.WriteHeader(StatusServiceUnavailable)
-		io.WriteString(w, h.errorBody())
-		tw.timedOut = true
+		switch err := ctx.Err(); err {
+		case context.DeadlineExceeded:
+			w.WriteHeader(StatusServiceUnavailable)
+			io.WriteString(w, h.errorBody())
+			tw.err = ErrHandlerTimeout
+		case context.Canceled:
+			tw.err = ErrRequestCanceled
+		}
 	}
 }
 
@@ -3377,7 +3386,7 @@ type timeoutWriter struct {
 	req  *Request
 
 	mu          sync.Mutex
-	timedOut    bool
+	err         error
 	wroteHeader bool
 	code        int
 }
@@ -3397,8 +3406,8 @@ func (tw *timeoutWriter) Header() Header { return tw.h }
 func (tw *timeoutWriter) Write(p []byte) (int, error) {
 	tw.mu.Lock()
 	defer tw.mu.Unlock()
-	if tw.timedOut {
-		return 0, ErrHandlerTimeout
+	if tw.err != nil {
+		return 0, tw.err
 	}
 	if !tw.wroteHeader {
 		tw.writeHeaderLocked(StatusOK)
@@ -3410,7 +3419,7 @@ func (tw *timeoutWriter) writeHeaderLocked(code int) {
 	checkWriteHeaderCode(code)
 
 	switch {
-	case tw.timedOut:
+	case tw.err != nil:
 		return
 	case tw.wroteHeader:
 		if tw.req != nil {
