a15c31b16e2a8b88f6c594aa07dd2d39874124cf
Michael Matloob <matloob@golang.org>
2021-10-21 14:26:55 -0400

    cmd/go: rename go mod initwork and editwork to go work init and edit
    This brings the workspace related commands into their own namespace.
    
    Fixes #48256
    
    Change-Id: I9d5e3d4c45798913d742c532c1571006e6f9cc57

---
+++ src/cmd/go/internal/workcmd/work.go
@@ -0,0 +1,25 @@
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package workcmd implements the ``go work'' command.
+package workcmd
+
+import (
+	"cmd/go/internal/base"
+)
+
+var CmdWork = &base.Command{
+	UsageLine: "go work",
+	Short:     "workspace maintenance",
+	Long: `Go workspace provides access to operations on worskpaces.
+
+Note that support for modules is built into many other commands,
+not just 'go work'.
+`,
+
+	Commands: []*base.Command{
+		cmdEdit,
+		cmdInit,
+	},
+}
