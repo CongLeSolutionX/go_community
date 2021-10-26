6bec01ab478f861999047ea94ac62a06ea0accf2
Robert Griesemer <gri@golang.org>
2021-10-26 10:06:55 -0700

    cmd/compile/internal/types2: underlying type of a type parameter is its constraint interface
    WORK IN PROGRESS.
    DO NOT REVIEW.
    
    Change the definition of the underlying type of a type parameter from
    itself to its corresponding constraint interface. This solves some
    open problems and also fits better with the theory.
    
    Can be enabled by setting the internal flag underIsIface to true.
    
    Change-Id: I7bb6453f4fe2569d92face222058fb4e17b12f25

--- src/cmd/compile/internal/types2/type.go
+++ src/cmd/compile/internal/types2/type.go
@@ -21,8 +21,13 @@ type Type interface {
 // under must only be called when a type is known
 // to be fully set up.
 func under(t Type) Type {
-	if n := asNamed(t); n != nil {
-		return n.under()
+	switch t := t.(type) {
+	case *Named:
+		return t.under()
+	case *TypeParam:
+		if underIsIface {
+			return t.iface()
+		}
 	}
 	return t
 }
@@ -115,6 +120,10 @@ func asNamed(t Type) *Named {
 
 // If t is a type parameter, asTypeParam returns that type, otherwise it returns nil.
 func asTypeParam(t Type) *TypeParam {
+	if underIsIface {
+		u, _ := t.(*TypeParam)
+		return u
+	}
 	u, _ := under(t).(*TypeParam)
 	return u
 }
