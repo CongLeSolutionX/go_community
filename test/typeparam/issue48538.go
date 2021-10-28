fb836ac06d078afcc419531716df5baedb838b5b
Dan Scales <danscales@google.com>
2021-10-27 19:16:27 -0700

    cmd/compile: use Structure() to get single underlying type of typeparam.
    Use types2.Structure() to get single underlying type of typeparams, to
    handle some unusual cases where a type param is constrained to a single
    underlying struct or map type.
    
    Fixes #48538
    
    Change-Id: I289fb7b31d489f7586f2b04aeb1df74e15a9f965

---
+++ test/typeparam/issue48538.go
@@ -0,0 +1,35 @@
+// compile -G=3
+
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Testing composite literal for type params constraints to be a struct or a map.
+
+package p
+
+type C interface {
+	struct{ b1, b2 string }
+}
+
+func f[T C]() T {
+	return T{
+		b1: "a",
+		b2: "b",
+	}
+}
+
+type D interface {
+	map[string]string | S
+}
+
+type S map[string]string
+
+func g[T D]() T {
+	b1 := "foo"
+	b2 := "bar"
+	return T{
+		b1: "a",
+		b2: "b",
+	}
+}
