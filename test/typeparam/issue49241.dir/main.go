f20bbf05573a2c8934ba088328fe0a755439d7d3
Keith Randall <khr@golang.org>
2021-10-30 14:39:29 -0700

    cmd/compile: add test for 49241
    The types of the two interfaces should be equal, but they aren't.
    
    It is probably new to generics that two types that are supposed to be
    identical can be manufactured in two different packages. I think we'll
    need type deduplication in the linker.
    
    Possibly related to CL 356570, but that CL probably isn't the cause.
    
    Update #49241
    
    DO NOT REVIEW
    DO NOT SUBMIT
    
    Change-Id: I8a6c70da541c6088a92a01392bc83b61cc130eba

---
+++ test/typeparam/issue49241.dir/main.go
@@ -0,0 +1,17 @@
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"b"
+	"c"
+)
+
+func main() {
+	if b.F() != c.F() {
+		println(b.F(), c.F())
+		panic("bad")
+	}
+}
