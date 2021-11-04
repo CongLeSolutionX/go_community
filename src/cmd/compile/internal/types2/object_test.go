b7b71d0eee367af8e04d6fa0a82c744ec696007f
Robert Griesemer <gri@golang.org>
2021-11-04 16:00:34 -0700

    cmd/compile/internal/types2: make object test an external test
    Change-Id: I15c969a799404067f34f600da15b1a97d4857315

--- src/cmd/compile/internal/types2/object_test.go
+++ src/cmd/compile/internal/types2/object_test.go
@@ -2,52 +2,51 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package types2
+package types2_test
 
 import (
 	"cmd/compile/internal/syntax"
-	"strings"
+	"cmd/compile/internal/types2"
 	"testing"
 )
 
-func parseSrc(path, src string) (*syntax.File, error) {
-	return syntax.Parse(syntax.NewFileBase(path), strings.NewReader(src), nil, nil, 0)
-}
-
 func TestIsAlias(t *testing.T) {
-	check := func(obj *TypeName, want bool) {
+	check := func(obj *types2.TypeName, want bool) {
 		if got := obj.IsAlias(); got != want {
 			t.Errorf("%v: got IsAlias = %v; want %v", obj, got, want)
 		}
 	}
 
 	// predeclared types
-	check(Unsafe.Scope().Lookup("Pointer").(*TypeName), false)
-	for _, name := range Universe.Names() {
-		if obj, _ := Universe.Lookup(name).(*TypeName); obj != nil {
+	check(types2.Unsafe.Scope().Lookup("Pointer").(*types2.TypeName), false)
+	for _, name := range types2.Universe.Names() {
+		if obj, _ := types2.Universe.Lookup(name).(*types2.TypeName); obj != nil {
 			check(obj, name == "any" || name == "byte" || name == "rune")
 		}
 	}
 
+	// for convenience
+	newTN := types2.NewTypeName
+
 	// various other types
-	pkg := NewPackage("p", "p")
-	t1 := NewTypeName(nopos, pkg, "t1", nil)
-	n1 := NewNamed(t1, new(Struct), nil)
-	t5 := NewTypeName(nopos, pkg, "t5", nil)
-	NewTypeParam(t5, nil)
+	pkg := types2.NewPackage("p", "p")
+	t1 := newTN(nopos, pkg, "t1", nil)
+	n1 := types2.NewNamed(t1, new(types2.Struct), nil)
+	t5 := newTN(nopos, pkg, "t5", nil)
+	types2.NewTypeParam(t5, nil)
 	for _, test := range []struct {
-		name  *TypeName
+		name  *types2.TypeName
 		alias bool
 	}{
-		{NewTypeName(nopos, nil, "t0", nil), false}, // no type yet
-		{NewTypeName(nopos, pkg, "t0", nil), false}, // no type yet
-		{t1, false}, // type name refers to named type and vice versa
-		{NewTypeName(nopos, nil, "t2", &emptyInterface), true}, // type name refers to unnamed type
-		{NewTypeName(nopos, pkg, "t3", n1), true},              // type name refers to named type with different type name
-		{NewTypeName(nopos, nil, "t4", Typ[Int32]), true},      // type name refers to basic type with different name
-		{NewTypeName(nopos, nil, "int32", Typ[Int32]), false},  // type name refers to basic type with same name
-		{NewTypeName(nopos, pkg, "int32", Typ[Int32]), true},   // type name is declared in user-defined package (outside Universe)
-		{NewTypeName(nopos, nil, "rune", Typ[Rune]), true},     // type name refers to basic type rune which is an alias already
+		{newTN(nopos, nil, "t0", nil), false}, // no type yet
+		{newTN(nopos, pkg, "t0", nil), false}, // no type yet
+		{t1, false},                           // type name refers to named type and vice versa
+		{newTN(nopos, nil, "t2", types2.NewInterfaceType(nil, nil)), true}, // type name refers to unnamed type
+		{newTN(nopos, pkg, "t3", n1), true},                                // type name refers to named type with different type name
+		{newTN(nopos, nil, "t4", types2.Typ[types2.Int32]), true},          // type name refers to basic type with different name
+		{newTN(nopos, nil, "int32", types2.Typ[types2.Int32]), false},      // type name refers to basic type with same name
+		{newTN(nopos, pkg, "int32", types2.Typ[types2.Int32]), true},       // type name is declared in user-defined package (outside Universe)
+		{newTN(nopos, nil, "rune", types2.Typ[types2.Rune]), true},         // type name refers to basic type rune which is an alias already
 		{t5, false}, // type name refers to type parameter and vice versa
 	} {
 		check(test.name, test.alias)
@@ -64,22 +63,22 @@ func TestEmbeddedMethod(t *testing.T) {
 	if err != nil {
 		t.Fatalf("parse failed: %s", err)
 	}
-	var conf Config
+	var conf types2.Config
 	pkg, err := conf.Check(f.PkgName.Value, []*syntax.File{f}, nil)
 	if err != nil {
 		t.Fatalf("typecheck failed: %s", err)
 	}
 
 	// get original error.Error method
-	eface := Universe.Lookup("error")
-	orig, _, _ := LookupFieldOrMethod(eface.Type(), false, nil, "Error")
+	eface := types2.Universe.Lookup("error")
+	orig, _, _ := types2.LookupFieldOrMethod(eface.Type(), false, nil, "Error")
 	if orig == nil {
 		t.Fatalf("original error.Error not found")
 	}
 
 	// get embedded error.Error method
 	iface := pkg.Scope().Lookup("I")
-	embed, _, _ := LookupFieldOrMethod(iface.Type(), false, nil, "Error")
+	embed, _, _ := types2.LookupFieldOrMethod(iface.Type(), false, nil, "Error")
 	if embed == nil {
 		t.Fatalf("embedded error.Error not found")
 	}
