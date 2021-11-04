65a6dfd35509a130a7d14ebd030b355495933124
Matthew Dempsky <mdempsky@google.com>
2021-11-04 13:28:25 -0700

    cmd/compile: gracefully fallback when inline bodies are missing
    Currently, we rely on a "crawling" step during export to identify
    function and method bodies that need to be exported or re-exported so
    we can trim out unnecessary ones and reduce build artifact sizes. To
    catch cases where we expect a function to be inlinable but we failed
    to export its body, we made this condition a fatal compiler error.
    
    However, with generics, it's much harder to perfectly identify all
    function bodies that need to be exported; and several attempts at
    tweaking the algorithm have resulted in still having failure cases.
    So for now, this CL changes a missing inline body into a graceful
    failure instead.
    
    Change-Id: I04b0872d0dcaae9c3de473e92ce584e4ec6fd782

--- src/cmd/compile/internal/typecheck/iimport.go
+++ src/cmd/compile/internal/typecheck/iimport.go
@@ -81,6 +81,23 @@ func ImportBody(fn *ir.Func) {
 	inimport = false
 }
 
+// HaveInlineBody reports whether we have fn's inline body available
+// for inlining.
+func HaveInlineBody(fn *ir.Func) bool {
+	if fn.Inl == nil {
+		return false
+	}
+
+	// Unified IR is much more conservative about pruning unreachable
+	// methods (at the cost of increased build artifact size).
+	if base.Debug.Unified != 0 {
+		return true
+	}
+
+	return fn.Inl.Body != nil ||
+		importReaderFor(fn.Nname.Sym(), inlineImporter) != nil
+}
+
 func importReaderFor(sym *types.Sym, importers map[*types.Sym]iimporterAndOffset) *importReader {
 	x, ok := importers[sym]
 	if !ok {
