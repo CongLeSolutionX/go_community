4a98fe31b0bae86ed690829a2245ef2c62111141
Robert Griesemer <gri@golang.org>
2021-11-04 21:31:08 -0700

    cmd/compile/internal/types2: report error for incomplete struct composite literal type
    Mark a struct as "complete" with a non-nil (but possibly zero length)
    fields list. Add a test when type-checking struct composite literals,
    the same way we do for other composite literal types.
    
    Fixes #49276.
    
    Change-Id: If44a3d790bf7032ddcd155af49bdc47b1cdff4fc

---
+++ src/cmd/compile/internal/types2/testdata/fixedbugs/issue49276.go
@@ -0,0 +1,24 @@
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package p
+
+import "unsafe"
+
+type S /* ERROR illegal cycle in declaration of S */ struct {
+	_ [unsafe.Sizeof(s)]byte
+}
+
+var s S
+
+// Since f is a pointer, this case could be valid.
+// But it's pathological and not worth the expense.
+type T struct {
+	f *[unsafe.Sizeof(T /* ERROR illegal cycle in type declaration */ {})]int
+}
+
+// test case from issue
+type a struct {
+	_ [42 - unsafe.Sizeof(a /* ERROR illegal cycle in type declaration */ {})]byte
+}
