5f919079d8facd3bbccc7f07a37e777613db03ed
hanpro <hanssccv@gmail.com>
2021-11-05 09:47:54 +0800

    cmd/compile: avoid adding LECall to the entry block when has opendefers
    The openDeferRecord always insert vardef/varlive pairs into the entry block, it may destroy the mem chain when LECall's args are writing into the same block. So create a new block before that happens.
    
    Fixes #49282
    
    Change-Id: Ibda6c4a45d960dd412a641f5e02276f663c80785

---
+++ test/fixedbugs/issue49282.go
@@ -0,0 +1,35 @@
+// compile
+
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package p
+
+func f[G uint]() {
+	var a, m []int
+	var s struct {
+		a, b, c, d, e int
+	}
+
+	func(G) {
+		_ = a
+		_ = m
+		_ = s
+		func() {
+			for i := 0; i < 5; i++ {
+				_ = a
+				_ = m
+				_, _ = s, s
+			}
+		}()
+	}(G(1.0))
+
+	defer func() uint {
+		return 0
+	}()
+}
+
+func g() {
+	f[uint]()
+}
