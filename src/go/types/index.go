de5eb73ff5c7064d24a59a924c2082d99a5b7bb5
Robert Findley <rfindley@google.com>
2021-11-08 13:09:41 -0500

    go/types: rename isX predicates to allX, add simple is_X (step 1 of 2)
    WIP
    
    DO NOT REVIEW
    DO NOT SUBMIT
    
    Change-Id: Ia9149ff9c0af68213c579090902ab7989828ddd2

--- src/go/types/index.go
+++ src/go/types/index.go
@@ -51,7 +51,7 @@ func (check *Checker) indexExpr(x *operand, e *typeparams.IndexExpr) (isFuncInst
 	length := int64(-1) // valid if >= 0
 	switch typ := under(x.typ).(type) {
 	case *Basic:
-		if isString(typ) {
+		if allString(typ) {
 			valid = true
 			if x.mode == constant_ {
 				length = int64(len(constant.StringVal(x.val)))
@@ -109,7 +109,7 @@ func (check *Checker) indexExpr(x *operand, e *typeparams.IndexExpr) (isFuncInst
 			var k, e Type  // k is only set for maps
 			switch t := u.(type) {
 			case *Basic:
-				if isString(t) {
+				if allString(t) {
 					e = universeByte
 					mode = value
 				}
@@ -217,7 +217,7 @@ func (check *Checker) sliceExpr(x *operand, e *ast.SliceExpr) {
 		return
 
 	case *Basic:
-		if isString(u) {
+		if allString(u) {
 			if e.Slice3 {
 				check.invalidOp(x, _InvalidSliceExpr, "3-index slice of string")
 				x.mode = invalid
@@ -372,7 +372,7 @@ func (check *Checker) isValidIndex(x *operand, code errorCode, what string, allo
 	}
 
 	// spec: "the index x must be of integer type or an untyped constant"
-	if !isInteger(x.typ) {
+	if !allInteger(x.typ) {
 		check.invalidArg(x, code, "%s %s must be integer", what, x)
 		return false
 	}
