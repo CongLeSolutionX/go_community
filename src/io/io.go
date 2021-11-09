9be0e2a54c10da544f5cffc7c0757206623286c4
Changkun Ou <hi@changkun.de>
2021-11-09 13:57:04 +0100

    io: accept ErrShortWrite if Write reports no error
    For proposal prototype.
    
    DO NOT SUMIT
    
    Fixes #49474
    
    Change-Id: Ib84874d0daeb74d9a2bea9d2e50ff5746e718e44

--- src/io/io.go
+++ src/io/io.go
@@ -425,21 +425,25 @@ func copyBuffer(dst Writer, src Reader, buf []byte) (written int64, err error) {
 	for {
 		nr, er := src.Read(buf)
 		if nr > 0 {
-			nw, ew := dst.Write(buf[0:nr])
-			if nw < 0 || nr < nw {
-				nw = 0
-				if ew == nil {
-					ew = errInvalidWrite
+			n := 0
+			for n != nr {
+				nw, ew := dst.Write(buf[n:nr])
+				if nw < 0 || nr < nw {
+					nw = 0
+					if ew == nil {
+						ew = errInvalidWrite
+					}
 				}
-			}
-			written += int64(nw)
-			if ew != nil {
-				err = ew
-				break
-			}
-			if nr != nw {
-				err = ErrShortWrite
-				break
+				written += int64(nw)
+				if ew != nil {
+					err = ew
+					goto done
+				}
+				if nr != nw && nr == 0 {
+					err = ErrShortWrite
+					goto done
+				}
+				n += nw
 			}
 		}
 		if er != nil {
@@ -449,6 +453,7 @@ func copyBuffer(dst Writer, src Reader, buf []byte) (written int64, err error) {
 			break
 		}
 	}
+done:
 	return written, err
 }
 
