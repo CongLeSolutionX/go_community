cc6a758dcd2c7a2d31a21674572a58999b0bee84
Robert Findley <rfindley@google.com>
2021-11-09 22:28:18 -0500

    go/types: make type hashing less specific to named types
    WIP
    DO NOT REVIEW
    DO NOT SUBMIT
    
    Change-Id: I7aa58e73b81731c3cad3a2fd14124f63cfb685a7

--- src/go/types/context.go
+++ src/go/types/context.go
@@ -47,19 +47,8 @@ func (ctxt *Context) typeHash(typ Type, targs []Type) string {
 	var buf bytes.Buffer
 
 	h := newTypeHasher(&buf, ctxt)
-	// Caution: don't use asNamed here. TypeHash may be called for unexpanded
-	// types. We don't need anything other than name and type arguments below,
-	// which do not require expansion.
-	if named, _ := typ.(*Named); named != nil && len(targs) > 0 {
-		// Don't use WriteType because we need to use the provided targs
-		// and not any targs that might already be with the *Named type.
-		h.typePrefix(named)
-		h.typeName(named.obj)
-		h.typeList(targs)
-	} else {
-		assert(targs == nil)
-		h.typ(typ)
-	}
+	h.typ(typ)
+	h.typeList(targs)
 
 	return strings.Replace(buf.String(), " ", "#", -1) // ReplaceAll is not available in Go1.4
 }
