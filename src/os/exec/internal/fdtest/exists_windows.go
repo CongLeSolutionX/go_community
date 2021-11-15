6cfab7467af54359d083885af787f55fed049c1f
Michael Pratt <mpratt@google.com>
2021-11-15 13:50:39 -0500

    os/exec: avoid NewFile on unknown FDs
    exec_test.go's init function uses os.NewFile(fd) + f.Stat as a portable
    mechanism to determine if an FD is in use.
    
    Unfortunately, the current use is racy: if an unused FD becomes used
    between NewFile and f.Close, then we will unintentionally close an FD we
    do not use.
    
    We cannot simply drop Close, as the finalizer will close the FD. We
    could hold all of the os.Files in a global for the lifetime of the
    process, but the need for such a hack is indicative of the larger
    problem: we should not create an os.File for an FD that we do not own.
    
    Instead, the new fdtest.Exists provides a helper that performs the
    equivalent of fstat(2) on each OS to determine if the FD is valid,
    without using os.File.
    
    We also reuse this helper on a variety of other tests that look at open
    FDs.
    
    Fixes #49533
    
    Change-Id: I36e2bdb15f271ab01e55c18db6564271995a15af

---
+++ src/os/exec/internal/fdtest/exists_windows.go
@@ -0,0 +1,16 @@
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build windows
+
+package fdtest
+
+import (
+	"syscall"
+)
+
+// Exists is not implemented on windows and panics.
+func Exists(fd uintptr) bool {
+	panic("unimplemented")
+}
