golang/go

commit 5e43dc943a9265ec65690242eb8076727c18a958

math/big: special-case a 0 mantissa during Rat parsing
Previously, a 0 mantissa was special-cased during big.Float
parsing, but not during big.Rat parsing. This meant that a value
like 0e9999999999 would parse successfully in big.Float.SetString,
but would hang in big.Rat.SetString. This discrepancy became an
issue in https://golang.org/src/go/constant/value.go?#L250,
where the big.Float would report an exponent of 0, so
big.Rat.SetString would be used and would subsequently hang.

A Go Playground example of this is https://play.golang.org/p/3fy28eUJuF

The solution is to special-case a zero mantissa during big.Rat
parsing as well, so that neither big.Rat nor big.Float will hang when
parsing a value with 0 mantissa but a large exponent.

This was discovered using go-fuzz on CockroachDB:
https://github.com/cockroachdb/go-fuzz/blob/master/examples/parser/main.go

Fixes #16176

Change-Id: I775558a8682adbeba1cc9d20ba10f8ed26259c56
Reviewed-on: https://go-review.googlesource.com/24430
Reviewed-by: Robert Griesemer <gri@golang.org>
Run-TryBot: Robert Griesemer <gri@golang.org>
TryBot-Result: Gobot Gobot <gobot@golang.org>

src/math/big/ratconv.go
@@ -88,6 +88,12 @@ func (z *Rat) SetString(s string) (*Rat, bool) {
 		return nil, false
 	}
 
+	// special-case 0 (see also issue #16176)
+	if len(z.a.abs) == 0 {
+		return z, true
+	}
+	// len(z.a.abs) > 0
+
 	// correct exponent
 	if ecorr < 0 {
 		exp += int64(ecorr)

src/math/big/ratconv_test.go
@@ -48,6 +48,7 @@ var setStringTests = []StringTest{
 	{"53/70893980658822810696", "53/70893980658822810696", true},
 	{"106/141787961317645621392", "53/70893980658822810696", true},
 	{"204211327800791583.81095", "4084226556015831676219/20000", true},
+	{"0e9999999999", "0", true}, // issue #16176
 	{in: "1/0"},
 }
 

src/go/constant/value_test.go
@@ -244,6 +244,7 @@ var stringTests = []struct {
 	{"1e9999", "1e+9999", "0x.f8d4a9da224650a8cb2959e10d985ad92adbd44c62917e608b1f24c0e1b76b6f61edffeb15c135a4b601637315f7662f325f82325422b244286a07663c9415d2p+33216"},
 	{"1e-9999", "1e-9999", "0x.83b01ba6d8c0425eec1b21e96f7742d63c2653ed0a024cf8a2f9686df578d7b07d7a83d84df6a2ec70a921d1f6cd5574893a7eda4d28ee719e13a5dce2700759p-33215"},
 	{"2.71828182845904523536028747135266249775724709369995957496696763", "2.71828", "271828182845904523536028747135266249775724709369995957496696763/100000000000000000000000000000000000000000000000000000000000000"},
+	{"0e9999999999", "0", "0"}, // issue #16176
 
 	// Complex
 	{"0i", "(0 + 0i)", "(0 + 0i)"},

CongLeSolutionX/go_community

commit 4719654a7eaa717f4115df54ea4b2ca77322d675

math/big: call norm when returning success from Rat SetString
I believe this is subtle bug in how SetString behave, introduced by
5e43dc943a92.

Given the following test scenario,

	(0) Given the struct of R, S, and T that contains *Rat.
	(1) Do the calculcation on R that cause the value of *Rat become
            0.
	(2) Convert the result to text, for example to JSON.
	(3) Consume the result as S (by UnmarshalText).
	(4) Prepare the expected value of T, since we know what to test.
	    and the result we set Rat value in T to 0 using SetInt64(0).
	(5) Compare the value of S with T.

The expected result should be S = T, but due to SetString return without
calling norm, the result of DeepEqual(S, T) is always false.

A code snippet to simulate this scenario,

	r1 := big.Rat{}
	r2 := big.Rat{}

	r1.SetString("0")
	r2.SetInt64(0)

	fmt.Printf("r1: %v\n", r1)
	fmt.Printf("r2: %v\n", r2)

	if !reflect.DeepEqual(&r1, &r2) {
		log.Printf("r1 != r2")
	}

Output:

    	r1: {{false []} {false []}} 0/1
    	r2: {{false []} {false [1]}} 0/1
	2021/11/17 01:07:39 r1 != r2

Before this changes, the above command will always print "r1 != r2".
One could suggest that we should use Rat.Cmp, but given S and T as
struct with many fields, including *Rat, comparing them one-by-one
become cumbersome.

This changes fix this by calling z.norm() when returning from
Rat.SetString() when success.

Change-Id: Ib84ae975bf82fe02d1203aa9668a01960c0fd59d

src/math/big/ratconv.go
@@ -113,7 +113,7 @@ func (z *Rat) SetString(s string) (*Rat, bool) {
 
 	// special-case 0 (see also issue #16176)
 	if len(z.a.abs) == 0 {
-		return z, true
+		return z.norm(), true
 	}
 	// len(z.a.abs) > 0
 

src/math/big/ratconv_test.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"io"
 	"math"
+	"reflect"
 	"strconv"
 	"strings"
 	"testing"
@@ -201,6 +202,15 @@ func TestRatSetString(t *testing.T) {
 				t.Errorf("#%d SetString(%q) got %p want nil", i, test.in, x)
 			}
 		}
+	}
+}
+
+func TestRatSetString_0(t *testing.T) {
+	got, _ := new(Rat).SetString("0") // Or set from Unmarshal{Text,JSON}.
+	want := new(Rat).SetInt64(0)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %#+v, want %#+v", got, want)
 	}
 }
 