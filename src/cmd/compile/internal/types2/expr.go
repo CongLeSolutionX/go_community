df3873db019837894f5fd76825eb466d0f325546
Robert Griesemer <gri@golang.org>
2021-12-15 21:25:50 -0800

    cmd/compile/internal/types2: better error for invalid == on type parameters
    DO NOT REVIEW.
    DO NOT SUBMIT.
    
    Fixes #48712.
    
    Change-Id: I6f214cdfdd1815493f2a04828e8f0097f1d8c124

--- src/cmd/compile/internal/types2/expr.go
+++ src/cmd/compile/internal/types2/expr.go
@@ -786,14 +786,18 @@ func (check *Checker) comparison(x, y *operand, op syntax.Operator) {
 			unreachable()
 		}
 		if !defined {
-			typ := x.typ
-			if x.isNil() {
-				typ = y.typ
-			}
-			if check.conf.CompilerErrorMessages {
-				err = check.sprintf("operator %s not defined on %s", op, typ)
+			if isTypeParam(x.typ) || isTypeParam(y.typ) {
+				typ := x.typ
+				if isTypeParam(y.typ) {
+					typ = y.typ
+				}
+				err = check.sprintf("%s is not comparable", typ)
 			} else {
-				err = check.sprintf("operator %s not defined for %s", op, typ)
+				typ := x.typ
+				if x.isNil() {
+					typ = y.typ
+				}
+				err = check.sprintf("operator %s not defined on %s", op, typ)
 			}
 		}
 	} else {
