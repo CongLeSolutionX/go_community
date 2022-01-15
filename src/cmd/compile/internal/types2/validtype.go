bc7cebde9d841c69d4ccd7f78a84488628344065
Robert Griesemer <gri@golang.org>
2022-01-14 17:42:20 -0800

    cmd/compile/internal/types2: consider type parameters for cycle detection
    Fixes #48962.
    
    Change-Id: I196c78bf066e4a56284d53368b2eb71bd8d8a780

--- src/cmd/compile/internal/types2/validtype.go
+++ src/cmd/compile/internal/types2/validtype.go
@@ -10,12 +10,54 @@ package types2
 // (Cycles involving alias types, as in "type A = [10]A" are detected
 // earlier, via the objDecl cycle detection mechanism.)
 func (check *Checker) validType(typ *Named) {
-	check.validType0(typ, nil)
+	check.validType0(typ, nil, nil)
+}
+
+// A tparamEnv provides the environment for looking up the type arguments
+// with which type parameters for a given generic type are instantiated.
+type tparamEnv struct {
+	tmap substMap
+	link *tparamEnv
+}
+
+func (env *tparamEnv) push(typ *Named) *tparamEnv {
+	targs := typ.TypeArgs()
+	if targs == nil {
+		return nil
+	}
+
+	n := targs.Len()
+	tparams := typ.TypeParams()
+	if tparams.Len() < n {
+		// TODO(gri) how is this possible?
+		n = tparams.Len()
+	}
+	tmap := make(substMap, n)
+	for i := 0; i < n; i++ {
+		tmap[typ.TypeParams().At(i)] = targs.At(i)
+	}
+	return &tparamEnv{tmap: tmap, link: env}
+}
+
+func (env *tparamEnv) pop() *tparamEnv {
+	return env.link
+}
+
+func (env *tparamEnv) lookup(tpar *TypeParam) Type {
+	for ; env != nil; env = env.link {
+		// TODO(gri) can't use tmap.lookup because that returns the type parameter
+		//           if no entry for it is found in the map
+		//           => revisit implementation of lookup (at least document better)
+		if targ, found := env.tmap[tpar]; found {
+			return targ
+		}
+	}
+	return nil
 }
 
 type typeInfo uint
 
-func (check *Checker) validType0(typ Type, path []Object) typeInfo {
+func (check *Checker) validType0(typ Type, env *tparamEnv, path []Object) typeInfo {
 	const (
 		unknown typeInfo = iota
 		marked
@@ -25,42 +67,31 @@ func (check *Checker) validType0(typ Type, path []Object) typeInfo {
 
 	switch t := typ.(type) {
 	case *Array:
-		return check.validType0(t.elem, path)
+		return check.validType0(t.elem, env, path)
 
 	case *Struct:
 		for _, f := range t.fields {
-			if check.validType0(f.typ, path) == invalid {
+			if check.validType0(f.typ, env, path) == invalid {
 				return invalid
 			}
 		}
 
 	case *Union:
 		for _, t := range t.terms {
-			if check.validType0(t.typ, path) == invalid {
+			if check.validType0(t.typ, env, path) == invalid {
 				return invalid
 			}
 		}
 
 	case *Interface:
 		for _, etyp := range t.embeddeds {
-			if check.validType0(etyp, path) == invalid {
+			if check.validType0(etyp, env, path) == invalid {
 				return invalid
 			}
 		}
 
 	case *Named:
-		// If t is parameterized, we should be considering the instantiated (expanded)
-		// form of t, but in general we can't with this algorithm: if t is an invalid
-		// type it may be so because it infinitely expands through a type parameter.
-		// Instantiating such a type would lead to an infinite sequence of instantiations.
-		// In general, we need "type flow analysis" to recognize those cases.
-		// Example: type A[T any] struct{ x A[*T] } (issue #48951)
-		// In this algorithm we always only consider the original, uninstantiated type.
-		// This won't recognize some invalid cases with parameterized types, but it
-		// will terminate.
-		t = t.orig
-
-		// don't report a 2nd error if we already know the type is invalid
+		// Don't report a 2nd error if we already know the type is invalid
 		// (e.g., if a cycle was detected earlier, via under).
 		if t.underlying == Typ[Invalid] {
 			check.infoMap[t] = invalid
@@ -70,7 +101,7 @@ func (check *Checker) validType0(typ Type, path []Object) typeInfo {
 		switch check.infoMap[t] {
 		case unknown:
 			check.infoMap[t] = marked
-			check.infoMap[t] = check.validType0(t.fromRHS, append(path, t.obj))
+			check.infoMap[t] = check.validType0(t.orig.fromRHS, env.push(t), append(path, t.obj))
 		case marked:
 			// cycle detected
 			for i, tn := range path {
@@ -87,6 +118,11 @@ func (check *Checker) validType0(typ Type, path []Object) typeInfo {
 			panic("cycle start not found")
 		}
 		return check.infoMap[t]
+
+	case *TypeParam:
+		if targ := env.lookup(t); targ != nil {
+			return check.validType0(targ, env.pop(), path)
+		}
 	}
 
 	return valid
