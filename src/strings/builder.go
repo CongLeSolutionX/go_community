a940a05270bd5fdc0f3357e7f65152904532c8f7
George Looshch <looshch@loosh.ch>
2022-01-15 21:43:05 +0200

    strings: fix typo in comment
    Remove unneded whitespace in noescape comment
    
    Fixes #50634
    
    Change-Id: I1c8d16c020b05678577d349470fac7e7ab8a10b7

--- src/strings/builder.go
+++ src/strings/builder.go
@@ -17,7 +17,7 @@ type Builder struct {
 	buf  []byte
 }
 
-// noescape hides a pointer from escape analysis.  noescape is
+// noescape hides a pointer from escape analysis. noescape is
 // the identity function but escape analysis doesn't think the
 // output depends on the input. noescape is inlined and currently
 // compiles down to zero instructions.
