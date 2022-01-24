269d9ad9c5773d82da40ef4c57298254f302bc7d
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2022-01-24 13:28:00 +0700

    cmd/compile: add constant folding switch statements in deadcode
    Switch statement over constant with many cases are not folded, so it
    makes the outer function cost too high to be inlined.
    
    This CL adds the constant folding ability to deadcode pass, so most
    trivial cases like switch statements over runtime.{GOOS,GOARCH} can now
    be foldable.
    
    Fixes #50253
    
    Change-Id: I8c0f0a0bdf8fbcdf2de27a999e68e5a87e20403d

--- test/inline.go
+++ test/inline.go
@@ -303,3 +303,19 @@ func conv2(v uint64) uint64 { // ERROR "can inline conv2"
 func conv1(v uint64) uint64 { // ERROR "can inline conv1"
 	return uint64(uint64(uint64(uint64(uint64(uint64(uint64(uint64(uint64(uint64(uint64(v)))))))))))
 }
+
+// Issue #50253 - ensure switch statements over constants are inlined
+func OSNameSwitch() string { // ERROR "can inline OSNameSwitch"
+	switch runtime.GOOS {
+	case "linux":
+		return "Leenooks"
+	case "windows":
+		return "Windoze"
+	case "darwin":
+		return "MackBone"
+	case "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100":
+		return "Numbers"
+	default:
+		return "oh nose!"
+	}
+}
