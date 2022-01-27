625be277d5eef7a496e3f760d03ba640b8367e00
David Chase <drchase@google.com>
2022-01-27 11:26:59 -0500

    cmd/compile: remove incorrect arm,arm64 CMP->CMN transformations
    These can go wrong when one of the operands is the minimum integer value.
    
    Fixes #50854.
    
    Change-Id: I238fe284f60c7ee5aeb9dc9a18e8b1578cdb77d0

---
+++ test/fixedbugs/issue50854.go
@@ -0,0 +1,30 @@
+// run
+
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+//go:noinline
+func f(p int64, x, y int64, b bool) bool { return -x <= p && p <= y }
+
+//go:noinline
+func g(p int32, x, y int32, b bool) bool { return -x <= p && p <= y }
+
+//go:noinline
+func h(p int64, x, y int64, b bool) bool { return -(x<<1) <= p && p <= y }
+
+//go:noinline
+func check(b bool) {
+	if b {
+		return
+	}
+	panic("FAILURE")
+}
+
+func main() {
+	check(f(1, -1<<63, 1<<63-1, true))
+	check(g(1, -1<<31, 1<<31-1, true))
+	check(h(1, -1<<62, 1<<63-1, true))
+}
