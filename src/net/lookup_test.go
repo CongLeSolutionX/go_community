1000572d7c5534fbbb22035560ba44e9f5fa793e
Bryan C. Mills <bcmills@google.com>
2022-02-08 17:09:40 -0500

    net: fix a race in TestLookupContextCancel
    If the actual DNS lookup in LookupIPAddr completes quickly enough,
    it may succeed even if the passed-in Context is already canceled.
    That would (rarely) cause TestLookupContextCancel to fail due to an
    unexpectedly-nil error.
    
    This change uses the existing testHookLookupIP hook to delay the
    cancellation until the lookup has started (to try to provoke the code path
    for which the test was added), and then block the lookup result until
    LookupIPAddr has noticed it.
    
    Fixes #51084
    Updates #22724
    
    Change-Id: I331ac61a652ac88f6d4c85bf62466237b76d53ed

--- src/net/lookup_test.go
+++ src/net/lookup_test.go
@@ -883,17 +883,41 @@ func TestLookupNonLDH(t *testing.T) {
 
 func TestLookupContextCancel(t *testing.T) {
 	mustHaveExternalNetwork(t)
-	defer dnsWaitGroup.Wait()
 
-	ctx, ctxCancel := context.WithCancel(context.Background())
-	ctxCancel()
-	_, err := DefaultResolver.LookupIPAddr(ctx, "google.com")
+	origTestHookLookupIP := testHookLookupIP
+	defer func() {
+		dnsWaitGroup.Wait()
+		testHookLookupIP = origTestHookLookupIP
+	}()
+
+	lookupCtx, cancelLookup := context.WithCancel(context.Background())
+	cancelLookup()
+
+	// Set testHookLookupIP to stall until the canceled call to LookupIPAddre
+	// either returns or cancels the Context controlling the lookupGroup.
+	cancelDone := make(chan struct{})
+	testHookLookupIP = func(
+		ctx context.Context,
+		fn func(context.Context, string, string) ([]IPAddr, error),
+		network string,
+		host string,
+	) ([]IPAddr, error) {
+		cancelLookup()
+		select {
+		case <-ctx.Done():
+		case <-cancelDone:
+		}
+		return fn(ctx, network, host)
+	}
+
+	_, err := DefaultResolver.LookupIPAddr(lookupCtx, "google.com")
 	if err.(*DNSError).Err != errCanceled.Error() {
 		testenv.SkipFlakyNet(t)
 		t.Fatal(err)
 	}
-	ctx = context.Background()
-	_, err = DefaultResolver.LookupIPAddr(ctx, "google.com")
+	close(cancelDone)
+
+	_, err = DefaultResolver.LookupIPAddr(context.Background(), "google.com")
 	if err != nil {
 		testenv.SkipFlakyNet(t)
 		t.Fatal(err)
