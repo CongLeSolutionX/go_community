6c8a2e8369c71bb4a70d43a59bd5bf303231bd3f
Michael Anthony Knyszek <mknyszek@google.com>
2022-02-14 22:36:25 +0000

    runtime: remove old pacer and the PacerRedesign goexperiment
    Now that Go 1.18 has been released, remove the old pacer.
    
    Change-Id: Ie7a7596d67f3fc25d3f375a08fc75eafac2eb834

--- src/internal/buildcfg/exp.go
+++ src/internal/buildcfg/exp.go
@@ -56,7 +56,6 @@ func ParseGOEXPERIMENT(goos, goarch, goexp string) (flags, baseline goexperiment
 		RegabiWrappers: regabiSupported,
 		RegabiReflect:  regabiSupported,
 		RegabiArgs:     regabiSupported,
-		PacerRedesign:  true,
 	}
 
 	// Start with the statically enabled set of experiments.
