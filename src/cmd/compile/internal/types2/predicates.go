8435c466e7a8b9318aceee63ee98cfd87d14094d
Robert Griesemer <gri@golang.org>
2022-02-20 12:58:21 -0800

    go/types, types2: implement _IsComparable predicate
    A type implements a comparable interface only if the type
    is statically known to be comparable. Specifically, a type
    cannot cannot contain (component) interfaces that are not
    statically known to be comparable.
    
    This CL implements a new predicate _IsComparable which
    takes an argument to control whether interfaces are
    always (dynamically) comparable. Comparable is now
    implemented in terms of _IsComparable.
    
    To implement _IsComparable, rename the existing (internal)
    comparable predicate to isComparable (for symmetry) and
    add an additional argument to indicate that an interface's
    comparability may only be known dynamically.
    
    Eventually we may decide to rename _IsComparable and export
    it as IsComparable.
    
    Fixes #51257.
    
    Change-Id: If22bc047ee59337deb2e7844b8f488d67e5c5530

--- src/cmd/compile/internal/types2/predicates.go
+++ src/cmd/compile/internal/types2/predicates.go
@@ -102,11 +102,19 @@ func isGeneric(t Type) bool {
 
 // Comparable reports whether values of type T are comparable.
 func Comparable(T Type) bool {
-	return comparable(T, nil, nil)
+	return _IsComparable(T, true)
 }
 
+// _IsComparable reports whether values of type T are comparable.
+// If dynamic is set, interfaces are always comparable; otherwise
+// they are comparable if all types in their type sets are comparable.
+func _IsComparable(T Type, dynamic bool) bool {
+	return isComparable(T, dynamic, nil, nil)
+}
+
+// If dynamic is set, non-type parameter interfaces are always comparable.
 // If reportf != nil, it may be used to report why T is not comparable.
-func comparable(T Type, seen map[Type]bool, reportf func(string, ...interface{})) bool {
+func isComparable(T Type, dynamic bool, seen map[Type]bool, reportf func(string, ...interface{})) bool {
 	if seen[T] {
 		return true
 	}
@@ -124,7 +132,7 @@ func comparable(T Type, seen map[Type]bool, reportf func(string, ...interface{})
 		return true
 	case *Struct:
 		for _, f := range t.fields {
-			if !comparable(f.typ, seen, nil) {
+			if !isComparable(f.typ, dynamic, seen, nil) {
 				if reportf != nil {
 					reportf("struct containing %s cannot be compared", f.typ)
 				}
@@ -133,7 +141,7 @@ func comparable(T Type, seen map[Type]bool, reportf func(string, ...interface{})
 		}
 		return true
 	case *Array:
-		if !comparable(t.elem, seen, nil) {
+		if !isComparable(t.elem, dynamic, seen, nil) {
 			if reportf != nil {
 				reportf("%s cannot be compared", t)
 			}
@@ -141,7 +149,7 @@ func comparable(T Type, seen map[Type]bool, reportf func(string, ...interface{})
 		}
 		return true
 	case *Interface:
-		return !isTypeParam(T) || t.typeSet().IsComparable(seen)
+		return dynamic && !isTypeParam(T) || t.typeSet().IsComparable(seen)
 	}
 	return false
 }
