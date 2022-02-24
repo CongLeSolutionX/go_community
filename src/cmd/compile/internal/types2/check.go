87f6475e2aab7e75f72673dab23ddb5fd767180d
Robert Griesemer <gri@golang.org>
2022-02-24 13:10:34 -0800

    types2: improved tracing output throughout
    DO NOT REVIEW.
    - needs go/types port
    
    Change-Id: Ib5e70e8f40ef564194cdb0e8d12c38e15388b987

--- src/cmd/compile/internal/types2/check.go
+++ src/cmd/compile/internal/types2/check.go
@@ -16,7 +16,7 @@ import (
 var nopos syntax.Pos
 
 // debugging/development support
-const debug = false // leave on during development
+const debug = true // leave on during development
 
 // exprInfo stores information about an untyped expression.
 type exprInfo struct {
@@ -373,11 +373,17 @@ func (check *Checker) processDelayed(top int) {
 	// this is a sufficiently bounded process.
 	for i := top; i < len(check.delayed); i++ {
 		a := &check.delayed[i]
-		if check.conf.Trace && a.desc != nil {
-			fmt.Println()
-			check.trace(a.desc.pos.Pos(), "-- "+a.desc.format, a.desc.args...)
+		if check.conf.Trace {
+			if a.desc != nil {
+				check.trace(a.desc.pos.Pos(), "-- "+a.desc.format, a.desc.args...)
+			} else {
+				check.trace(nopos, "-- delayed %p", a.f)
+			}
 		}
 		a.f() // may append to check.delayed
+		if check.conf.Trace {
+			fmt.Println()
+		}
 	}
 	assert(top <= len(check.delayed)) // stack must not have shrunk
 	check.delayed = check.delayed[:top]
