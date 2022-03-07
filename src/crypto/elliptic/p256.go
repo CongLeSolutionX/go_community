4be490edcf4524aa04257dcd589c435c9a2ba15c
Filippo Valsorda <filippo@golang.org>
2022-03-07 13:11:35 -0500

    crypto/elliptic: don't use big.Int.Mod/Mul in generic P-256
    The generic 32-bit constant time P-256 backend was still using math/big
    for multiplication and modular reduction when converting into and out of
    the internal representation.
    
    We are aiming to isolate the big.Int layer and to only ever use SetBytes
    and FillBytes in it, which we can arguably make constant time.
    
    name                 old time/op    new time/op    delta
    ScalarBaseMult/P256   441µs ± 1%     433µs ± 1%   -1.77%  (p=0.000 n=10+9)
    ScalarMult/P256      1.16ms ± 1%    1.13ms ± 1%   -2.18%  (p=0.000 n=10+9)
    
    name                 old alloc/op   new alloc/op   delta
    ScalarBaseMult/P256    537B ± 2%      240B ± 0%  -55.31%  (p=0.000 n=9+10)
    ScalarMult/P256        860B ± 0%      336B ± 0%  -60.95%  (p=0.000 n=10+10)
    
    name                 old allocs/op  new allocs/op  delta
    ScalarBaseMult/P256    15.0 ± 0%      11.0 ± 0%  -26.67%  (p=0.000 n=10+10)
    ScalarMult/P256        19.0 ± 0%      13.0 ± 0%  -31.58%  (p=0.000 n=10+10)
    
    For #52182
    
    Change-Id: I1bf7bf211e3838dc80ada154b1b80385b315c99d

--- src/crypto/elliptic/p256.go
+++ src/crypto/elliptic/p256.go
@@ -12,9 +12,6 @@ import "math/big"
 
 var p256Params *CurveParams
 
-// RInverse contains 1/R mod p, the inverse of the Montgomery constant 2^257.
-var p256RInverse *big.Int
-
 func initP256() {
 	// See FIPS 186-3, section D.2.3
 	p256Params = &CurveParams{Name: "P-256"}
@@ -24,8 +21,6 @@ func initP256() {
 	p256Params.Gx, _ = new(big.Int).SetString("6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296", 16)
 	p256Params.Gy, _ = new(big.Int).SetString("4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5", 16)
 	p256Params.BitSize = 256
-
-	p256RInverse, _ = new(big.Int).SetString("7fffffff00000001fffffffe8000000100000000ffffffff0000000180000000", 16)
 
 	// Arch-specific initialization, i.e. let a platform dynamically pick a P256 implementation
 	initP256Arch()
