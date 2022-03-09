ccc6611ffb5c1fdf7e548f4e42f445cbe6134677
Bryan C. Mills <bcmills@google.com>
2022-03-09 17:19:23 -0500

    internal/testenv: add GOROOT and use it to fix tests broken with -trimpath
    This fixes many (but not all) of the tests that currently fail
    (due to a bogus path reported by runtime.GOROOT) when run with
    'go test -trimpath std cmd'.
    
    Updates #51461
    
    Change-Id: Ia2cc05705529c4859e7928f32eeceed647f2e986

--- src/cmd/api/goapi.go
+++ src/cmd/api/goapi.go
@@ -33,9 +33,11 @@ func goCmd() string {
 	if runtime.GOOS == "windows" {
 		exeSuffix = ".exe"
 	}
-	path := filepath.Join(runtime.GOROOT(), "bin", "go"+exeSuffix)
-	if _, err := os.Stat(path); err == nil {
-		return path
+	if goroot := build.Default.GOROOT; goroot != "" && goroot != "$GOROOT" {
+		path := filepath.Join(goroot, "bin", "go"+exeSuffix)
+		if _, err := os.Stat(path); err == nil {
+			return path
+		}
 	}
 	return "go"
 }
