eb61ec787d1dd76ff55326a19b2fdd809d0d894d
Than McIntosh <thanm@google.com>
2022-03-10 15:06:43 -0500

    cmd/cover: add hybrid instrumentation mode
    Add a new mode of coverage instrumentation that works as a hybrid
    between purely tool-based and purely compiler-based. The cmd/cover
    tool still does source-to-source rewriting, but it also generates
    information to be used by the compiler to do things like marking
    meta-data vars as read-only.
    
    In hybrid mode, the cmd/cover tool is invoked not on a single source
    file but on all the files in a package, and is passed a config file
    containing the import path of the package in question, along with
    other parameters needed for the run. It writes a series of modified
    files and an output config file to be passed to the compiler when
    compiling the modified files.
    
    Not completely useful by itself, still needs a corresponding set of
    changes in the Go command and in the compiler.
    
    Updates #51430.
    
    Change-Id: I0fcbd93a9a8fc25064187b159152486a2549ea54

--- src/internal/coverage/cmddefs.go
+++ src/internal/coverage/cmddefs.go
@@ -14,3 +14,36 @@ const CounterModeTag = "countermode"
 const CounterVarTag = "countervar"
 const CounterGranularityTag = "countergranularity"
 const CounterPrefixTag = "counterprefix"
+
+// CoverPkgConfig is a bundle of information passed from the Go
+// command to the cover command during "go build -cover" runs. The
+// Go command creates and fills in a struct as below, then passes
+// file containing the encoded JSON for the struct to the "cover"
+// tool when instrumenting the source files in a Go package.
+type CoverPkgConfig struct {
+	// File into which cmd/cover should emit summary info
+	// when instrumentation is complete.
+	OutConfig string
+
+	// Import path for the package being instrumented.
+	PkgPath string
+
+	// Package name.
+	PkgName string
+
+	// Package classification: one of
+	//    "stdlib"  package is part of the Go standard library
+	//    "none"    no go.mod in use
+	//    "mainmod" Go cmd considers package to be in the main module
+	//    "depmod"  go cmd considers this package to be a go.mod dependency
+	PkgClassification string
+
+	// Instrumentation granularity: one of "perfunc" or "perblock" (default)
+	Granularity string
+
+	// Module path for this package (empty if no go.mod in use)
+	ModulePath string
+
+	// Skip instrumentation for this package and emit only registration code.
+	RegOnly bool
+}
