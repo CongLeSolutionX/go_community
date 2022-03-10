52d6f80d51eba0d8127d96af488409044e0a23ad
Than McIntosh <thanm@google.com>
2022-03-10 15:06:43 -0500

    cmd/cover: add hybrid instrumentation mode
    Add a new mode of coverage instrumentation that works as a hybrid
    between purely tool-based and purely compiler-based. The cmd/cover
    tool still does source-to-source rewriting, but it also generates
    information to be used by the compiler to do things like marking
    meta-data vars as read-only.
    
    In hybrid mode, the cmd/cover tool is invoked not on a single source
    file but on all the files in a package, and is passed a config file
    containing the import path of the package in question, along with
    other parameters needed for the run. It writes a series of modified
    files and an output config file to be passed to the compiler when
    compiling the modified files.
    
    Not completely useful by itself, still needs a corresponding set of
    changes in the Go command and in the compiler.
    
    Updates #51430.
    
    Change-Id: I0fcbd93a9a8fc25064187b159152486a2549ea54

---
+++ src/cmd/cover/cfg_test.go
@@ -0,0 +1,179 @@
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main_test
+
+import (
+	"fmt"
+	"internal/testenv"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func writeFile(t *testing.T, path, contents string) {
+	if err := os.WriteFile(path, []byte(contents), 0666); err != nil {
+		t.Fatalf("os.WriteFile(%s) failed: %v", path, err)
+	}
+}
+
+func writePkgConfig(t *testing.T, outdir, tag, ppath, pname, pkclass string, regonly bool, gran string) string {
+	outcfg := filepath.Join(outdir, "outcfg.txt")
+	cfgContents := fmt.Sprintf(`
+# comment
+pkgpath=%s
+pkgname=%s
+pkgclassification=%s
+regonly=%v
+granularity=%s
+outconfig=%s
+`, ppath, pname, pkclass, regonly, gran, outcfg)
+	incfg := filepath.Join(outdir, tag+"incfg.txt")
+	writeFile(t, incfg, cfgContents)
+	return incfg
+}
+
+func runPkgCover(t *testing.T, outdir string, tag string, incfg string, mode string, infiles []string, errExpected bool) ([]string, string, string) {
+	// Write the pkgcfg file.
+	outcfg := filepath.Join(outdir, "outcfg.txt")
+
+	// Form up the arguments and run the tool.
+	outfiles := []string{}
+	for _, inf := range infiles {
+		base := filepath.Base(inf)
+		outfiles = append(outfiles, filepath.Join(outdir, "cov."+base))
+	}
+	ofs := strings.Join(outfiles, ",")
+	args := []string{"-pkgcfg", incfg, "-mode=" + mode, "-var=var" + tag, "-o", ofs}
+	args = append(args, infiles...)
+	cmd := exec.Command(testcover, args...)
+	if errExpected {
+		errmsg := runExpectingError(cmd, t)
+		return nil, "", errmsg
+	} else {
+		run(cmd, t)
+		return outfiles, outcfg, ""
+	}
+}
+
+const debugWorkDir = false
+
+func TestCoverWithCfg(t *testing.T) {
+	t.Parallel()
+	testenv.MustHaveGoRun(t)
+	buildCover(t)
+
+	// Subdir in testdata that has our input files of interest.
+	tpath := filepath.Join("testdata", "pkgcfg")
+
+	// Helper to collect input paths (go files) for a subdir in 'pkgcfg'
+	pfiles := func(subdir string) []string {
+		de, err := os.ReadDir(filepath.Join(tpath, subdir))
+		if err != nil {
+			t.Fatalf("reading subdir %s: %v", subdir, err)
+		}
+		paths := []string{}
+		for _, e := range de {
+			if !strings.HasSuffix(e.Name(), ".go") || strings.HasSuffix(e.Name(), "_test.go") {
+				continue
+			}
+			paths = append(paths, filepath.Join(tpath, subdir, e.Name()))
+		}
+		return paths
+	}
+
+	dir := t.TempDir()
+	if debugWorkDir {
+		dir = "/tmp/qqq"
+		os.RemoveAll(dir)
+		os.Mkdir(dir, 0777)
+	}
+	instdira := filepath.Join(dir, "insta")
+	if err := os.Mkdir(instdira, 0777); err != nil {
+		t.Fatal(err)
+	}
+
+	scenarios := []struct {
+		mode, gran, class string
+		regonly           bool
+	}{
+		{
+			mode:  "count",
+			gran:  "perblock",
+			class: "mainmod",
+		},
+		{
+			mode:  "set",
+			gran:  "perfunc",
+			class: "stdlib",
+		},
+		{
+			mode:    "atomic",
+			gran:    "perblock",
+			class:   "depmod",
+			regonly: true,
+		},
+	}
+
+	tag := "first"
+	var incfg string
+	for _, scenario := range scenarios {
+		// Instrument package "a", producing a set of instrumented output
+		// files and an 'output config' file to pass on to the compiler.
+		ppath := "cfg/a"
+		pname := "a"
+		pkgclass := scenario.class
+		mode := scenario.mode
+		gran := scenario.gran
+		regonly := scenario.regonly
+		incfg = writePkgConfig(t, instdira, tag, ppath, pname,
+			pkgclass, regonly, gran)
+		ofs, outcfg, _ := runPkgCover(t, instdira, tag, incfg, mode,
+			pfiles("a"), false)
+		t.Logf("outfiles: %+v\n", ofs)
+
+		// Run the compiler on the files to make sure the result is
+		// buildable.
+		bargs := []string{"tool", "compile", "-p", "a", "-coveragecfg", outcfg}
+		bargs = append(bargs, ofs...)
+		cmd := exec.Command(testenv.GoToolPath(t), bargs...)
+		cmd.Dir = instdira
+		run(cmd, t)
+	}
+
+	// Do some error testing to ensure that various bad options and
+	// combinations are properly rejected.
+
+	// Expect error if config file inaccessible/unreadable.
+	mode := "atomic"
+	errExpected := true
+	_, _, errmsg := runPkgCover(t, instdira, tag, "/not/a/file", mode,
+		pfiles("a"), errExpected)
+	want := "error reading pkgconfig file"
+	if !strings.Contains(errmsg, want) {
+		t.Errorf("'bad config file' test: wanted %s got %s", want, errmsg)
+	}
+
+	// Expect err if config file contains unknown stuff.
+	t.Logf("mangling in config")
+	writeFile(t, incfg, fmt.Sprintf("blah=foo\n"))
+	_, _, errmsg = runPkgCover(t, instdira, tag, incfg, mode,
+		pfiles("a"), errExpected)
+	want = "unrecognized pkgcfg tag"
+	if !strings.Contains(errmsg, want) {
+		t.Errorf("'bad config file' test: wanted %s got %s", want, errmsg)
+	}
+
+	// Expect errr on empty config file.
+	t.Logf("writing empty config")
+	writeFile(t, incfg, fmt.Sprintf("\n"))
+	_, _, errmsg = runPkgCover(t, instdira, tag, incfg, mode,
+		pfiles("a"), errExpected)
+	want = "got 0 values expected 6"
+	if !strings.Contains(errmsg, want) {
+		t.Errorf("'bad config file' test: wanted %s got %s", want, errmsg)
+	}
+}
