3b122509a1b8b70d84b3aa814d82ac944e141260
Than McIntosh <thanm@google.com>
2022-03-22 08:49:52 -0400

    debug/pe: add APIs for reading section def aux info
    Add hooks to support reading of section definition symbol aux data
    (including COMDAT information) from the aux symbols associated with
    section definition symbols. The COFF symbol array made available by
    "pe.File" includes entries for aux symbols, but doesn't expose their
    structure (since it varies depending on the type of aux symbol). This
    patch adds a function for returning a specific class of aux symbol
    ("type 5") that immediately follows a COFF symbol corresponding to a
    section definition.
    
    Updates #35006.
    Updates #51868.
    
    Change-Id: I21fcc057150f7a3c64f01a5961aabca0fa43399e

---
+++ src/debug/pe/symbols_test.go
@@ -0,0 +1,82 @@
+package pe_test
+
+import (
+	"debug/pe"
+	"fmt"
+	"testing"
+)
+
+type testpoint struct {
+	name   string
+	ok     bool
+	err    string
+	auxstr string
+}
+
+func TestReadCOFFSymbolAuxInfo(t *testing.T) {
+	testpoints := map[int]testpoint{
+		39: testpoint{
+			name:   ".rdata$.refptr.__native_startup_lock",
+			ok:     true,
+			auxstr: "{Size:8 NumRelocs:1 NumLineNumbers:0 Checksum:0 SecNum:16 Selection:2 Padding:[0 0 0]}",
+		},
+		81: testpoint{
+			name:   ".debug_line",
+			ok:     true,
+			auxstr: "{Size:994 NumRelocs:1 NumLineNumbers:0 Checksum:1624223678 SecNum:32 Selection:0 Padding:[0 0 0]}",
+		},
+		155: testpoint{
+			name: ".file",
+			ok:   false,
+			err:  "incorrect symbol storage class",
+		},
+	}
+
+	f, err := pe.Open("testdata/llvm-mingw-20211002-msvcrt-x86_64-crt2")
+	defer f.Close()
+	if err != nil {
+		t.Errorf("open failed with %v", err)
+	}
+	for k := range f.COFFSymbols {
+		tp, ok := testpoints[k]
+		if !ok {
+			continue
+		}
+		sym := &f.COFFSymbols[k]
+		if sym.NumberOfAuxSymbols == 0 {
+			t.Errorf("expected aux symbols for sym %d", k)
+			continue
+		}
+		name, nerr := sym.FullName(f.StringTable)
+		if nerr != nil {
+			t.Errorf("FullName(%d) failed with %v", k, nerr)
+			continue
+		}
+		if name != tp.name {
+			t.Errorf("name check for %d, got %s want %s", k, name, tp.name)
+			continue
+		}
+		ap, err := pe.COFFSymbolReadSectionDefAux(k, f.COFFSymbols)
+		if tp.ok {
+			if err != nil {
+				t.Errorf("unexpected failure on %d, got error %v", k, err)
+				continue
+			}
+			got := fmt.Sprintf("%+v", *ap)
+			if got != tp.auxstr {
+				t.Errorf("COFFSymbolReadSectionDefAux on %d bad return, got:\n%s\nwant:\n%s\n", k, got, tp.auxstr)
+				continue
+			}
+		} else {
+			if err == nil {
+				t.Errorf("unexpected non-failure on %d", k)
+				continue
+			}
+			got := fmt.Sprintf("%v", err)
+			if got != tp.err {
+				t.Errorf("COFFSymbolReadSectionDefAux %d wrong error, got %q want %q", k, got, tp.err)
+				continue
+			}
+		}
+	}
+}
