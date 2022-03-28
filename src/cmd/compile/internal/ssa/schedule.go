49c0ab6a42dd1a533b419953162e089c33dece13
Paul E. Murphy <murp@ibm.com>
2022-03-28 10:08:54 -0500

    WIP: cmd/compile: enable carry chain scheduling for AMD64
    Feedback is welcome, as a PPC64 maintainer, this might not be
    the desired or ideal implementation for AMD64. Likewise, the
    priority changes may not be needed/desired on all architectures.
    
    AMD64 arithmetic seems to universally clobber flags, this causes
    entire carry chains to regenerate when compiling non-trivial
    functions like those in crypto/elliptic/internal/fiat.
    
    Unlike PPC64 where few instructions clobber the isolated carry
    bit, the scheduler needs to minimize scheduling of unrelated ops
    during the chain. To accomplish this, we bump the priorities on
    the select ops and carry bit operations above the regular stuff
    to ensure they are always chosen ahead of unrelated ops.
    
    Likewise, the ssa rules need tweaked to ensure unrelated chains
    are not merged, end of chains are easier to identify and avoid
    folding which may cause overlapping chains.
    
    This results in about a 2x improvement in the crypto/elliptic
    benchmarks which use the go-native curves on AMD64.
    
    Updates #40171
    
    Change-Id: I8ba37356a565f66099498cf5781d50bc3e23ae8c

--- src/cmd/compile/internal/ssa/schedule.go
+++ src/cmd/compile/internal/ssa/schedule.go
@@ -159,7 +159,15 @@ func schedule(f *Func) {
 				// this value is already live. This also removes its
 				// false dependency on the other part of the tuple.
 				// Also ensures tuple is never spilled.
-				score[v.ID] = ScoreReadTuple
+				if (v.Op == OpSelect1 || v.Op == OpSelect0) && v.Args[0].Op.isCarryChainOp() {
+					// Score higher to ensure this doesn't create a priority inversion
+					// when scheduling the next op in a carry chain on targets use the
+					// flag variable. Many unrelated ops may clobber and cause an
+					// expensive flag regeneration.
+					score[v.ID] = ScoreFlags
+				} else {
+					score[v.ID] = ScoreReadTuple
+				}
 			case v.Op.isCarryChainOp():
 				if !v.Op.isCarryChainCreator() {
 					// Score non-tail carry ops as Flags, this trys to minimize carry
@@ -586,7 +594,7 @@ func chooseNextCarryChain(carrychainTails []*Value, uses []int32) int {
 // Return whether this is an operation which starts a carry chain.
 func (op Op) isCarryChainCreator() bool {
 	switch op {
-	case OpPPC64SUBC, OpPPC64ADDC, OpPPC64SUBCconst, OpPPC64ADDCconst:
+	case OpPPC64SUBC, OpPPC64ADDC, OpPPC64SUBCconst, OpPPC64ADDCconst, OpAMD64SUBQborrow, OpAMD64SUBQconstborrow, OpAMD64ADDQcarry, OpAMD64ADDQconstcarry, OpAMD64NEGLflags:
 		return true
 	}
 	return false
@@ -595,7 +603,7 @@ func (op Op) isCarryChainCreator() bool {
 // Return if op is a part of a chain of carrying arithmetic.
 func (op Op) isCarryChainOp() bool {
 	switch op {
-	case OpPPC64SUBE, OpPPC64ADDE, OpPPC64SUBZEzero, OpPPC64ADDZEzero:
+	case OpPPC64SUBE, OpPPC64ADDE, OpPPC64SUBZEzero, OpPPC64ADDZEzero, OpAMD64SBBQ, OpAMD64SBBQconst, OpAMD64ADCQ, OpAMD64ADCQconst, OpAMD64SBBQcarryout:
 		return true
 	}
 	return op.isCarryChainCreator()
@@ -603,7 +611,7 @@ func (op Op) isCarryChainOp() bool {
 
 // Return the producing *Value of the carry bit of this op.
 func (v *Value) getCarryProducer() *Value {
-	// PPC64 carry dependencies are conveyed through their final argument.
+	// PPC64/AMD64 carry dependencies are conveyed through their final argument.
 	// Likewise, there is always an OpSelect1 between them.
 	return v.Args[len(v.Args)-1].Args[0]
 }
