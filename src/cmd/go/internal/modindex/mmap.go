f316bc89594bf3140c0c8cf8eb2f09f0cecc9f07
Michael Matloob <matloob@golang.org>
2022-04-05 18:47:23 -0400

    cmd/go: add functions to read index file
    The data read is used for three primary functions: ImportPackage,
    IsDirWithGoFiles and ScanDir. Functions are also provided to get this
    information from the intermediate package representation to cache
    the information from reads for non-indexed packages.
    
    Change-Id: I5eed629bb0d6ee5b88ab706d06b074475004c081

---
+++ src/cmd/go/internal/modindex/mmap.go
@@ -0,0 +1,30 @@
+// Copyright 2011 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package modindex
+
+import (
+	"log"
+	"os"
+	"runtime/debug"
+)
+
+func init() {
+	debug.SetPanicOnFault(true)
+}
+
+// An mmapData is mmap'ed read-only data from a file.
+type mmapData struct {
+	f *os.File
+	d []byte
+}
+
+// mmap maps the given file into memory.
+func mmap(file string) mmapData {
+	f, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return mmapFile(f)
+}
