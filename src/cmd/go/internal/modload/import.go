c26486b5716f356c9e08c8c0ffeba820b9f145e0
Michael Matloob <matloob@golang.org>
2022-04-05 18:47:23 -0400

    cmd/go: changes to use modindex
    This CL makes the changes to actually use the module index when loading
    packages and instead of scanning their directories to see if they
    contain go files or to extract imports.
    
    Change-Id: I70106181cf64d6fd5a416644ba518b6b90030e0a

--- src/cmd/go/internal/modload/import.go
+++ src/cmd/go/internal/modload/import.go
@@ -20,6 +20,7 @@ import (
 	"cmd/go/internal/cfg"
 	"cmd/go/internal/fsys"
 	"cmd/go/internal/modfetch"
+	"cmd/go/internal/modindex"
 	"cmd/go/internal/par"
 	"cmd/go/internal/search"
 
@@ -650,6 +651,10 @@ func dirInModule(path, mpath, mdir string, isLocal bool) (dir string, haveGoFile
 	// We don't care about build tags, not even "+build ignore".
 	// We're just looking for a plausible directory.
 	res := haveGoFilesCache.Do(dir, func() any {
+		if mi, ok := modindex.Get(dir); ok {
+			isDirWithGoFiles, err := mi.IsDirWithGoFiles(mi.RelPath(dir))
+			return goFilesEntry{isDirWithGoFiles, err}
+		}
 		ok, err := fsys.IsDirWithGoFiles(dir)
 		return goFilesEntry{haveGoFiles: ok, err: err}
 	}).(goFilesEntry)
