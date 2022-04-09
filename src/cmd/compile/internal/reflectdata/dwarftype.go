71ef44efdd6de221224fa58140bac4be74871671
zhouguangyuan <zhouguangyuan.xian@gmail.com>
2022-04-09 21:07:16 +0800

    cmd/compile: implement dwarf.Type interface for types.Type
    Implement cmd/internal/dwarf.Type interface fpr cmd/compile/internal/types.Type with a wrapper.
    
    For #52209
    
    Change-Id: I46ce4735bfcf900d9e17c14b6f69ea542eac0a8a

---
+++ src/cmd/compile/internal/reflectdata/dwarftype.go
@@ -0,0 +1,150 @@
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package reflectdata
+
+import (
+	"cmd/compile/internal/base"
+	"cmd/compile/internal/types"
+	"cmd/internal/dwarf"
+	"cmd/internal/objabi"
+	"strings"
+)
+
+type dwarfType struct {
+	typ *types.Type
+}
+
+func (d dwarfType) DwarfName(interface{}) string {
+	name := types.TypeSymName(d.typ)
+	return strings.Replace(name, `"".`, objabi.PathToPrefix(base.Ctxt.Pkgpath)+".", -1)
+}
+
+func (d dwarfType) Name(interface{}) string {
+	return types.TypeSymName(d.typ)
+}
+
+func (d dwarfType) Size(interface{}) int64 {
+	return d.typ.Size()
+}
+
+func (d dwarfType) Kind(interface{}) objabi.SymKind {
+	return objabi.SymKind(kinds[d.typ.Kind()])
+}
+
+func (d dwarfType) RuntimeType(interface{}) dwarf.Sym {
+	return types.TypeSym(d.typ).Linksym()
+}
+
+func (d dwarfType) Key(interface{}) dwarf.Type {
+	return dwarfType{typ: d.typ.Key()}
+}
+
+func (d dwarfType) Elem(interface{}) dwarf.Type {
+	return dwarfType{d.typ.Elem()}
+}
+
+func (d dwarfType) NumElem(interface{}) int64 {
+	if d.typ.IsArray() {
+		return d.typ.NumElem()
+	}
+	if d.typ.IsStruct() {
+		return int64(d.typ.NumFields())
+	}
+	if d.typ.Kind() == types.TFUNC {
+		return int64(d.typ.NumParams())
+	}
+	panic("unreachable")
+}
+
+func (d dwarfType) NumResult(interface{}) int64 {
+	return int64(d.typ.NumResults())
+}
+
+func (d dwarfType) IsDDD(interface{}) bool {
+	if d.typ.NumParams() > 0 {
+		return d.typ.Params().Field(d.typ.NumParams() - 1).IsDDD()
+	}
+	return false
+}
+
+func (d dwarfType) FieldName(dwctxt interface{}, g dwarf.FieldsGroup, i int) string {
+	switch g {
+	case dwarf.GroupFields:
+		return d.typ.FieldName(i)
+	case dwarf.GroupParams:
+		field := d.typ.Params().Field(i)
+		if field.Sym == nil || len(field.Sym.Name) == 0 {
+			return types.TypeSymName(field.Type)
+		}
+		return field.Sym.Name
+	case dwarf.GroupResults:
+		field := d.typ.Results().Field(i)
+		if field.Sym == nil || len(field.Sym.Name) == 0 {
+			return types.TypeSymName(field.Type)
+		}
+		return field.Sym.Name
+	}
+	panic("implement me")
+}
+
+func (d dwarfType) FieldType(dwctxt interface{}, g dwarf.FieldsGroup, i int) dwarf.Type {
+	switch g {
+	case dwarf.GroupFields:
+		return dwarfType{d.typ.FieldType(i)}
+	case dwarf.GroupParams:
+		return dwarfType{d.typ.Params().FieldType(i)}
+	case dwarf.GroupResults:
+		return dwarfType{d.typ.Results().FieldType(i)}
+	}
+	panic("implement me")
+}
+
+func (d dwarfType) FieldIsEmbed(dwctxt interface{}, i int) bool {
+	return d.typ.Field(i).Embedded != 0
+}
+
+func (d dwarfType) FieldOffset(dwctxt interface{}, i int) int64 {
+	return d.typ.Field(i).Offset
+}
+
+func (d dwarfType) IsEface(interface{}) bool {
+	return d.typ.IsEmptyInterface()
+}
+
+var fixTypes = dwarf.FixTypes{
+	Uintptr: stubUintptr{},
+	Eface:   stubEface{},
+	Iface:   stubIface{},
+}
+
+type stubUintptr struct {
+	dwarf.Type
+}
+
+func (stubUintptr) RuntimeType(dwctxt interface{}) dwarf.Sym {
+	return types.TypeSym(types.Types[types.TUINTPTR]).Linksym()
+}
+
+type stubEface struct {
+	dwarf.Type
+}
+
+func (stubEface) Name(dwctxt interface{}) string {
+	if base.Ctxt.Pkgpath == "runtime" {
+		return `"".eface`
+	}
+	return `runtime.eface`
+}
+
+type stubIface struct {
+	dwarf.Type
+}
+
+func (stubIface) Name(dwctxt interface{}) string {
+	if base.Ctxt.Pkgpath == "runtime" {
+		return `"".iface`
+	}
+	return `runtime.iface`
+}
