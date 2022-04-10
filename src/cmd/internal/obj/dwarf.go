dce5175324951286f70bbd451c115cefb84392cd
zhouguangyuan <zhouguangyuan.xian@gmail.com>
2022-04-11 00:57:22 +0800

    cmd/compile: generate dwarf type info in compiler
    Generate dwarf type info and dump it to objfile. It is not be used in linker now.
    Some types need to synthesize and the prototypes are still generated in linker. I will resolve it in the future.
    
    For #52209
    
    Change-Id: Ie7c67197f930462ae4474f291e02d3009b6651bc

--- src/cmd/internal/obj/dwarf.go
+++ src/cmd/internal/obj/dwarf.go
@@ -369,7 +369,7 @@ func (s *LSym) Length(dwarfContext interface{}) int64 {
 }
 
 func (s *LSym) Invalid() bool {
-	panic("should be used only in the linker")
+	return s == nil
 }
 
 // fileSymbol returns a symbol corresponding to the source file of the
@@ -775,4 +775,72 @@ func (s stubType) Name(dwctxt interface{}) string {
 		return `"".` + s.name
 	}
 	return `runtime.` + s.name
+}
+
+var prototypedies = map[string]*dwarf.DWDie{
+	"runtime.stringStructDWARF": nil,
+	"runtime.slice":             nil,
+	"runtime.hmap":              nil,
+	"runtime.bmap":              nil,
+	"runtime.sudog":             nil,
+	"runtime.waitq":             nil,
+	"runtime.hchan":             nil,
+}
+
+func (ctxt *Link) PopulateDWARFType(typ dwarf.Type) {
+	kind := typ.Kind(nil)
+	if kind == objabi.KindMap || kind == objabi.KindChan || kind == objabi.KindSlice || kind == objabi.KindString {
+		// can't synthesize types now. So still generate them in linker.
+		return
+	}
+	if _, ok := prototypedies[typ.DwarfName(ctxt)]; ok {
+		return
+	}
+	uintptrOnce.Do(func() {
+		fixTypes.Uintptr = ctxt.Lookup(dwarf.InfoPrefix + "uintptr")
+	})
+	dwctxt := dwCtxt{ctxt}
+	s := typ.RuntimeType(dwctxt)
+	typeInfo := s.(*LSym).NewTypeInfo()
+	die, typedefdie, err := dwarf.NewType(typ, dwctxt, fixTypes, &ctxt.dwtypes)
+	if err != nil {
+		ctxt.Diag(err.Error())
+		return
+	}
+	dwarf.NewAttr(die, dwarf.DW_AT_go_runtime_type, dwarf.DW_CLS_GO_TYPEREF, 0, s)
+	if typedefdie != nil {
+		typeInfo.dwarfInfo[0] = typedefdie.Sym.(*LSym)
+		typeInfo.dwarfInfo[1] = die.Sym.(*LSym)
+	} else {
+		typeInfo.dwarfInfo[0] = die.Sym.(*LSym)
+	}
+}
+
+// todo: synthesize types here.
+func (ctxt *Link) DumpDwarfTypes() {
+	dwarf.Reversetree(&ctxt.dwtypes.Child)
+	for die := ctxt.dwtypes.Child; die != nil; die = die.Link {
+		ctxt.putdie([]*LSym{}, die)
+	}
+}
+
+// todo: unify putdie with linker
+func (ctxt *Link) putdie(syms []*LSym, die *dwarf.DWDie) []*LSym {
+	dwctxt := dwCtxt{ctxt}
+	s := die.Sym
+	if s == nil {
+		s = syms[len(syms)-1]
+	} else {
+		syms = append(syms, s.(*LSym))
+	}
+	dwarf.Uleb128put(dwctxt, s, int64(die.Abbrev))
+	dwarf.PutAttrs(dwctxt, s, die.Abbrev, die.Attr)
+	if dwarf.HasChildren(die) {
+		for die := die.Child; die != nil; die = die.Link {
+			syms = dwctxt.putdie(syms, die)
+		}
+		lastsym := syms[len(syms)-1]
+		lastsym.WriteInt(ctxt, lastsym.Size, 1, 0)
+	}
+	return syms
 }
