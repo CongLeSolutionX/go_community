6fb2ca8dc190a1c6ff675c1618bdb33a0b43b7a7
zhouguangyuan <zhouguangyuan.xian@gmail.com>
2022-04-11 00:57:22 +0800

    cmd/compile: generate dwarf type info in compiler
    Generate dwarf type info and dump it to objfile. It is not be used in linker now.
    Some types need to be synthesized. I will resolve it in next CLs.
    
    For #52209
    
    Change-Id: Ie7c67197f930462ae4474f291e02d3009b6651bc

--- src/cmd/compile/internal/reflectdata/reflect.go
+++ src/cmd/compile/internal/reflectdata/reflect.go
@@ -1235,6 +1235,10 @@ func writeType(t *types.Type) *obj.LSym {
 	}
 	lsym.Set(obj.AttrMakeTypelink, keep)
 
+	if base.Flag.DwarfType {
+		base.Ctxt.PopulateDWARFType(DwarfType{t}, dupok == obj.DUPOK)
+	}
+
 	return lsym
 }
 
