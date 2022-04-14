0f52b7aba5c175303525398bc0d2f20dc35927a8
Than McIntosh <thanm@google.com>
2022-04-14 15:44:20 -0400

    runtime/coverage: apis to emit counter data under user control
    DO NOT SUBMIT
    
    Add hooks/apis to support writing of coverage counter data and
    meta-data under user control (from within an executing "-cover"
    binary), so as to provide a way to obtain coverage data from programs
    that do not terminate.
    
    Updates #51430.
    
    Change-Id: I34ee6cee52e5597fa3698b8b04f1b34a2a2a418f

---
+++ src/runtime/coverage/apis.go
@@ -0,0 +1,76 @@
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package coverage
+
+import (
+	"fmt"
+	"io"
+)
+
+// EmitMetaDataToDir writes a coverage meta-data file for the
+// currently running program to the directory specified in 'dir'. An
+// error will be returned if operation can't be completed successfully
+// (for example, if the currently running program was not built with
+// "-cover", or if the directory does not exist).
+func EmitMetaDataToDir(dir string) error {
+	if !finalHashComputed {
+		return fmt.Errorf("error: no meta-data available (binary not built with -cover?)")
+	}
+	return emitMetaDataToDirectory(dir, getCovMetaList())
+}
+
+// EmitMetaDataToWriter writes the meta-data content (the
+// payload that would normally be emitted to a meta-data file) for
+// currently running program to the the writer 'w'. An error will be
+// returned if operation can't be completed successfully (for example,
+// if the currently running program was not built with "-cover", or if
+// a write fails).
+func EmitMetaDataToWriter(w io.Writer) error {
+	if w == nil {
+		return fmt.Errorf("error: nil writer in EmitMetaDataToWriter")
+	}
+	if !finalHashComputed {
+		return fmt.Errorf("error: no meta-data available (binary not built with -cover?)")
+	}
+	ml := getCovMetaList()
+	return writeMetaData(w, ml, cmode, cgran, finalHash)
+}
+
+// EmitCounterDataToDir writes a coverage counter-data file for the
+// currently running program to the directory specified in 'dir'. An
+// error will be returned if operation can't be completed successfully
+// (for example, if the currently running program was not built with
+// "-cover", or if the directory does not exist). The counter data written
+// will be a snapshot taken at the point of the invocation.
+func EmitCounterDataToDir(dir string) error {
+	return emitCounterDataToDirectory(dir)
+}
+
+// EmitCounterDataToWriter writes coverage counter-data
+// content for the currently running program to the writer 'w'. An
+// error will be returned if operation can't be completed successfully
+// (for example, if the currently running program was not built with
+// "-cover", or if a write fails). The counter data written will be a
+// snapshot taken at the point of the invocation.
+func EmitCounterDataToWriter(w io.Writer) error {
+	if w == nil {
+		return fmt.Errorf("error: nil writer in EmitCounterDataToWriter")
+	}
+	// Ask the runtime for the list of coverage counter symbols.
+	cl := getCovCounterList()
+	if len(cl) == 0 {
+		return fmt.Errorf("program not built with -coverage")
+	}
+	if !finalHashComputed {
+		return fmt.Errorf("meta-data not written yet, unable to write counter data")
+	}
+
+	pm := getCovPkgMap()
+	s := &emitState{
+		counterlist: cl,
+		pkgmap:      pm,
+	}
+	return s.emitCounterDataToWriter(w)
+}
