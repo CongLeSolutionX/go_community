b64508bffde298a5c29c651c3f8f592549aaca67
Wayne Zuo <wdvxdr@golangcn.org>
2022-04-19 21:24:33 +0800

    cmd/compile: use dictionary to convert type to shaped interface type
    When we convert a type to a shaped interface type, we are not able
    to recognize the itab. So passing the itab by dictionary as the
    workaround.
    
    Fixes #52026.
    
    Change-Id: I75c23c7dd215daf9761dc24116a8af2c28c6d948

---
+++ test/typeparam/issue52026.go
@@ -0,0 +1,44 @@
+// run
+
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+func returnOption[T any](n int) Option[T] {
+	if n == 1 {
+		return Some[T]{}
+	} else {
+		return None{}
+	}
+}
+
+type Option[T any] interface {
+	sealedOption()
+}
+
+type Some[T any] struct {
+	val T
+}
+
+func (s Some[T]) Value() T {
+	return s.val
+}
+
+func (s Some[T]) sealedOption() {}
+
+type None struct{}
+
+func (s None) sealedOption() {}
+
+func main() {
+	s := returnOption[string](0)
+	_ = s.(None)
+	switch (any)(s).(type) {
+	case Some[int]:
+	case None:
+	default:
+		panic("oops")
+	}
+}
