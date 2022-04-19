61d7d2d2e325b3d6e1472a9206cf4089e689bf83
zhouguangyuan <zhouguangyuan.xian@gmail.com>
2022-04-19 21:46:03 +0800

    cmd/link: use the dwarf type info generated by compiler
    It's time to begin using the dwarf type info generated by compiler in linker.
    
    For #52209
    
    Change-Id: Ic12f4ed1f510454880d6ef9c8608b405c5404a92

--- src/cmd/link/internal/ld/dwarf.go
+++ src/cmd/link/internal/ld/dwarf.go
@@ -62,6 +62,11 @@ type dwctxt struct {
 	typeRuntimeIface loader.Sym
 	uintptrInfoSym   loader.Sym
 
+	// Do not add a type die to workqueue more than once.
+	// So use a map to record them.
+	inqueue   map[loader.Sym]struct{}
+	workqueue heap
+
 	// Used at various points in that parallel portion of DWARF gen to
 	// protect against conflicting updates to globals (such as "gdbscript")
 	dwmu *sync.Mutex
@@ -1131,12 +1136,10 @@ func (d *dwctxt) importInfoSymbol(dsym loader.Sym) {
 			// type already generated
 			continue
 		}
-		// FIXME: is there a way we could avoid materializing the
-		// symbol name here?
-		sn := d.ldr.SymName(rsym)
-		tn := sn[len(dwarf.InfoPrefix):]
-		ts := d.ldr.Lookup("type."+tn, 0)
-		d.defgotype(ts)
+
+		// record the relocation symbol directly, do not look up the runtime type,
+		// it may not exist.
+		d.addQueue(rsym)
 	}
 }
 
@@ -1525,7 +1528,7 @@ func appendSyms(syms []loader.Sym, src []sym.LoaderSym) []loader.Sym {
 
 func (d *dwctxt) writeUnitInfo(u *sym.CompilationUnit, abbrevsym loader.Sym, infoEpilog loader.Sym) []loader.Sym {
 	syms := []loader.Sym{}
-	if len(u.Textp) == 0 && u.DWInfo.Child == nil && len(u.VarDIEs) == 0 {
+	if len(u.Textp) == 0 && u.DWInfo.Child == nil && len(u.VarDIEs) == 0 && len(u.TypeDIES) == 0 {
 		return syms
 	}
 
@@ -1557,6 +1560,7 @@ func (d *dwctxt) writeUnitInfo(u *sym.CompilationUnit, abbrevsym loader.Sym, inf
 		cu = append(cu, loader.Sym(u.Consts))
 	}
 	cu = appendSyms(cu, u.VarDIEs)
+	cu = appendSyms(cu, u.TypeDIES)
 	var cusize int64
 	for _, child := range cu {
 		cusize += int64(len(d.ldr.Data(child)))
@@ -1703,7 +1707,7 @@ func (d *dwctxt) dwarfVisitFunction(fnSym loader.Sym, unit *sym.CompilationUnit)
 		r := drelocs.At(ri)
 		// Look for "use type" relocs.
 		if r.Type() == objabi.R_USETYPE {
-			d.defgotype(r.Sym())
+			d.markTypeDieFromGoType(r.Sym())
 			continue
 		}
 		if r.Type() != objabi.R_DWARFSECREF {
@@ -1733,10 +1737,30 @@ func (d *dwctxt) dwarfVisitFunction(fnSym loader.Sym, unit *sym.CompilationUnit)
 			continue
 		}
 
-		rsn := d.ldr.SymName(rsym)
-		tn := rsn[len(dwarf.InfoPrefix):]
-		ts := d.ldr.Lookup("type."+tn, 0)
-		d.defgotype(ts)
+		// record the relocation symbol directly, do not look up the runtime type,
+		// it may not exist.
+		d.addQueue(rsym)
+	}
+}
+
+// construct the name of type die by the name of gotype. Then add the type die sym
+// into the workqueue.
+func (d *dwctxt) markTypeDieFromGoType(gotype loader.Sym) loader.Sym {
+	sn := d.ldr.SymName(gotype)
+	name := dwarf.InfoPrefix + sn[5:] // could also decode from Type.string
+	dwinfo := d.ldr.Lookup(name, 0)
+	if dwinfo == 0 {
+		d.ldr.Errorf(gotype, "can't find dwarf type info: %s", name)
+	}
+	d.addQueue(dwinfo)
+	return dwinfo
+}
+
+// check if a type die is already in queue, otherwise, add it to the queue.
+func (d *dwctxt) addQueue(typedie loader.Sym) {
+	if _, ok := d.inqueue[typedie]; !ok {
+		d.inqueue[typedie] = struct{}{}
+		d.workqueue.push(typedie)
 	}
 }
 
@@ -1760,8 +1784,6 @@ func dwarfGenerateDebugInfo(ctxt *Link) {
 		tdmap:    make(map[loader.Sym]loader.Sym),
 		rtmap:    make(map[loader.Sym]loader.Sym),
 	}
-	d.typeRuntimeEface = d.lookupOrDiag("type.runtime.eface")
-	d.typeRuntimeIface = d.lookupOrDiag("type.runtime.iface")
 
 	if ctxt.HeadType == objabi.Haix {
 		// Initial map used to store package size for each DWARF section.
@@ -1770,20 +1792,6 @@ func dwarfGenerateDebugInfo(ctxt *Link) {
 
 	// For ctxt.Diagnostic messages.
 	newattr(&dwtypes, dwarf.DW_AT_name, dwarf.DW_CLS_STRING, int64(len("dwtypes")), "dwtypes")
-
-	// Unspecified type. There are no references to this in the symbol table.
-	d.newdie(&dwtypes, dwarf.DW_ABRV_NULLTYPE, "<unspecified>")
-
-	// Some types that must exist to define other ones (uintptr in particular
-	// is needed for array size)
-	d.mkBuiltinType(ctxt, dwarf.DW_ABRV_BARE_PTRTYPE, "unsafe.Pointer")
-	die := d.mkBuiltinType(ctxt, dwarf.DW_ABRV_BASETYPE, "uintptr")
-	newattr(die, dwarf.DW_AT_encoding, dwarf.DW_CLS_CONSTANT, dwarf.DW_ATE_unsigned, 0)
-	newattr(die, dwarf.DW_AT_byte_size, dwarf.DW_CLS_CONSTANT, int64(d.arch.PtrSize), 0)
-	newattr(die, dwarf.DW_AT_go_kind, dwarf.DW_CLS_CONSTANT, objabi.KindUintptr, 0)
-	newattr(die, dwarf.DW_AT_go_runtime_type, dwarf.DW_CLS_ADDRESS, 0, dwSym(d.lookupOrDiag("type.uintptr")))
-
-	d.uintptrInfoSym = d.mustFind("uintptr")
 
 	// Prototypes needed for type synthesis.
 	prototypedies = map[string]*dwarf.DWDie{
@@ -1809,7 +1817,7 @@ func dwarfGenerateDebugInfo(ctxt *Link) {
 		"type.runtime.interfacetype",
 		"type.runtime.itab",
 		"type.runtime.imethod"} {
-		d.defgotype(d.lookupOrDiag(typ))
+		d.markTypeDieFromGoType(d.lookupOrDiag(typ))
 	}
 
 	// fake root DIE for compile unit DIEs
@@ -1920,7 +1928,7 @@ func dwarfGenerateDebugInfo(ctxt *Link) {
 					for i := 0; i < relocs.Count(); i++ {
 						reloc := relocs.At(i)
 						if reloc.Type() == objabi.R_USEIFACE {
-							d.defgotype(reloc.Sym())
+							d.markTypeDieFromGoType(reloc.Sym())
 						}
 					}
 				}
@@ -1949,15 +1957,23 @@ func dwarfGenerateDebugInfo(ctxt *Link) {
 		varDIE := d.ldr.Lookup(dwarf.InfoPrefix+sn, 0)
 		if varDIE != 0 {
 			unit := d.ldr.SymUnit(idx)
-			d.defgotype(gt)
+			d.markTypeDieFromGoType(gt)
 			unit.VarDIEs = append(unit.VarDIEs, sym.LoaderSym(varDIE))
 		}
 	}
 
-	d.synthesizestringtypes(ctxt, dwtypes.Child)
-	d.synthesizeslicetypes(ctxt, dwtypes.Child)
-	d.synthesizemaptypes(ctxt, dwtypes.Child)
-	d.synthesizechantypes(ctxt, dwtypes.Child)
+	for !d.workqueue.empty() {
+		dwinfo := d.workqueue.pop()
+		d.linkctxt.runtimeCU.TypeDIES = append(d.linkctxt.runtimeCU.TypeDIES, sym.LoaderSym(dwinfo))
+		relocs := d.ldr.Relocs(dwinfo)
+		for i := 0; i < relocs.Count(); i++ {
+			rs := relocs.At(i).Sym()
+			if len(d.ldr.Data(rs)) == 0 || d.ldr.SymType(rs) != sym.SDWARFTYPE {
+				continue
+			}
+			d.addQueue(rs)
+		}
+	}
 }
 
 // dwarfGenerateDebugSyms constructs debug_line, debug_frame, and
