154fc524b92e103f6e9d63cb8bde529aaa94c10c
zhouguangyuan <zhouguangyuan.xian@gmail.com>
2022-04-19 23:26:32 +0800

    cmd/compile,link: support generating dwarf info when -linkshared
    Now, all the dwarf type can be generated by compiler, when using static link, every compiler unit generate its own dwarf type die. when using -linkshared, all the types current compile unit need can be collected. So we can generate corresponding dwarf type info for it and subtypes of it.
    For synthesizetype, we also need to keep the type info of them when linkshared
    
    For #52209
    
    Change-Id: Ia70ba39708ce710c45d923d5b86dac91d7e9d616

--- src/cmd/internal/obj/dwarf.go
+++ src/cmd/internal/obj/dwarf.go
@@ -310,6 +310,16 @@ func (c dwCtxt) LookupDwarfSym(name string) (s dwarf.Sym, exist bool) {
 }
 
 func (c dwCtxt) Reference(t dwarf.Type) dwarf.Sym {
+	if c.Flag_linkshared {
+		if c.Pkgpath != "runtime" {
+			name := t.DwarfName()
+			if name == "*runtime.g" || name == "*runtime._type" ||
+				name == "*runtime.mapextra" || name == "*runtime.itab" {
+				return c.Link.Lookup(dwarf.InfoPrefix + "uintptr")
+			}
+		}
+		return c.PopulateDWARFType(t, true)
+	}
 	return c.Link.Lookup(dwarf.InfoPrefix + t.Name())
 }
 
@@ -456,7 +466,7 @@ func (ctxt *Link) populateDWARF(curfn interface{}, s *LSym, myimportpath string)
 
 // DwarfIntConst creates a link symbol for an integer constant with the
 // given name, type and value.
-func (ctxt *Link) DwarfIntConst(myimportpath, name, typename string, val int64) {
+func (ctxt *Link) DwarfIntConst(myimportpath, name string, typeSym *LSym, val int64) {
 	if myimportpath == "" {
 		return
 	}
@@ -464,12 +474,12 @@ func (ctxt *Link) DwarfIntConst(myimportpath, name, typename string, val int64) 
 		s.Type = objabi.SDWARFCONST
 		ctxt.Data = append(ctxt.Data, s)
 	})
-	dwarf.PutIntConst(dwCtxt{ctxt}, s, ctxt.Lookup(dwarf.InfoPrefix+typename), myimportpath+"."+name, val)
+	dwarf.PutIntConst(dwCtxt{ctxt}, s, typeSym, myimportpath+"."+name, val)
 }
 
 // DwarfGlobal creates a link symbol containing a DWARF entry for
 // a global variable.
-func (ctxt *Link) DwarfGlobal(myimportpath, typename string, varSym *LSym) {
+func (ctxt *Link) DwarfGlobal(myimportpath string, typeSym, varSym *LSym) {
 	if myimportpath == "" || varSym.Local() {
 		return
 	}
@@ -488,7 +498,6 @@ func (ctxt *Link) DwarfGlobal(myimportpath, typename string, varSym *LSym) {
 		s.Set(AttrDuplicateOK, true) // needed for shared linkage
 		ctxt.Data = append(ctxt.Data, s)
 	})
-	typeSym := ctxt.Lookup(dwarf.InfoPrefix + typename)
 	dwarf.PutGlobal(dwCtxt{ctxt}, dieSym, typeSym, varSym, varname)
 }
 
@@ -802,7 +811,7 @@ func (ctxt *Link) PopulateDWARFType(typ dwarf.Type, dupok bool) dwarf.Sym {
 	dwsym.Set(AttrDuplicateOK, dupok)
 
 	dwctxt := dwCtxt{ctxt}
-	def, _, err := dwarf.NewType(typ, dwctxt, &ctxt.dwtypes)
+	def, typedef, err := dwarf.NewType(typ, dwctxt, &ctxt.dwtypes)
 	if err != nil {
 		ctxt.Diag(err.Error())
 		return nil
@@ -811,6 +820,10 @@ func (ctxt *Link) PopulateDWARFType(typ dwarf.Type, dupok bool) dwarf.Sym {
 		name := typ.DwarfName()
 		if _, ok := prototypes[name]; ok {
 			prototypes[name] = def
+			if ctxt.Flag_linkshared {
+				//other packages will emit prototype,too. make it dupok
+				typedef.Sym.(*LSym).Set(AttrDuplicateOK, true)
+			}
 		}
 	}
 
@@ -822,17 +835,30 @@ func (ctxt *Link) DumpDwarfTypes(lookupRuntime func(name string) dwarf.Type, Uin
 
 	// We don't expect this type in the package out of runtime.
 	// So use another temp root, we can avoid dumping them when dwtypes is traversed.
-	prototypeRoot := new(dwarf.DWDie)
+	var prototypeRoot *dwarf.DWDie
+	if ctxt.Flag_linkshared {
+		uintptrSym.Set(AttrDuplicateOK, true)
+		dwarf.NewType(Uintptr, dwCtxt{ctxt}, &ctxt.dwtypes)
+		prototypeRoot = &ctxt.dwtypes
+	} else {
+		prototypeRoot = new(dwarf.DWDie)
+	}
+
 	lookupPrototype := func(name string) *dwarf.DWDie {
+
 		die := prototypes[name]
 		if die != nil {
 			return die
 		}
 		t := lookupRuntime(name)
-		die, _, err := dwarf.NewType(t, dwCtxt{ctxt}, prototypeRoot)
+		die, typedef, err := dwarf.NewType(t, dwCtxt{ctxt}, prototypeRoot)
 		if err != nil {
 			ctxt.Diag(err.Error())
 			return nil
+		}
+		if ctxt.Flag_linkshared {
+			// only be emitted when using -linkshared
+			typedef.Sym.(*LSym).Set(AttrDuplicateOK, true)
 		}
 		prototypes[name] = die
 		return die
@@ -848,7 +874,10 @@ func (ctxt *Link) DumpDwarfTypes(lookupRuntime func(name string) dwarf.Type, Uin
 		case dwarf.DW_ABRV_CHANTYPE:
 			dwarf.SynthesizeChanTypes(dwCtxt{ctxt}, die, &ctxt.dwtypes, lookupPrototype)
 		case dwarf.DW_ABRV_IFACETYPE:
-			// runtime must dump iface and eface, do anything
+			if ctxt.Flag_linkshared {
+				lookupPrototype("runtime.eface")
+				lookupPrototype("runtime.iface")
+			}
 		}
 	}
 
