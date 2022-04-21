84f49c9ae38ab767006f5d9136e2d0ce77e43b9d
Bryan C. Mills <bcmills@google.com>
2022-04-21 13:58:06 -0400

    os/exec: implement Interrupt and WaitDelay
    Fixes #50436.
    
    Change-Id: I9dff8caa317a04b7b2b605f810b8f12ef8ca485d

--- src/os/exec/exec_windows_test.go
+++ src/os/exec/exec_windows_test.go
@@ -7,6 +7,8 @@
 package exec_test
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -15,6 +17,11 @@ import (
 	"strings"
 	"syscall"
 	"testing"
+)
+
+var (
+	quitSignal os.Signal = nil
+	pipeSignal           = syscall.SIGPIPE
 )
 
 func init() {
@@ -99,5 +106,30 @@ func TestChildCriticalEnv(t *testing.T) {
 	}
 	if strings.TrimSpace(string(out)) == "" {
 		t.Error("no SYSTEMROOT found")
+	}
+}
+
+func TestStartRejectsUnsupportedInterrupt(t *testing.T) {
+	for _, sig := range []os.Signal{
+		os.Interrupt, // explicitly not implemented
+
+		// “invented values” as described by the syscall package.
+		syscall.SIGHUP,
+		syscall.SIGQUIT,
+
+		// Note that os.Kill actually is supported, and is tested separately.
+	} {
+		t.Run(sig.String(), func(t *testing.T) {
+			cmd := helperCommandContext(t, context.Background(), "exit", "0")
+			cmd.Interrupt = sig
+			err := cmd.Start()
+
+			if err == nil {
+				t.Errorf("Start succeeded unexpectedly")
+				cmd.Wait()
+			} else if !errors.Is(err, syscall.EWINDOWS) {
+				t.Errorf("Start: %v\nwant %v", err, syscall.EWINDOWS)
+			}
+		})
 	}
 }
