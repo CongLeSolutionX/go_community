635fc4f8f9fd4f5028758a41bec79e1ff0494926
Michael Matloob <matloob@golang.org>
2022-04-29 14:45:46 -0400

    cmd/go: save a list of standard packages for use by cmd/go
    In IsStandardPackage, check against the list instead of seeing if the
    package exists on disk.
    
    Change-Id: I07594303a1e9833723522d5ff94577a5510ca6f0

---
+++ src/internal/goroot/stdpkgs_test.go
@@ -0,0 +1,38 @@
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package goroot
+
+import (
+	"log"
+	"os/exec"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func getStdpkgs() []string {
+	allPkgs := make(map[string]bool)
+	for _, t := range []string{"linux", "wasm,js", "windows"} { // covering set?
+		stdpkgs, err := exec.Command("go", "list", "-tags", t, "std", "cmd", "builtin").Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, v := range strings.Fields(string(stdpkgs)) {
+			allPkgs[v] = true
+		}
+	}
+	var l []string
+	for v := range allPkgs {
+		l = append(l, v)
+	}
+	return l
+}
+
+func TestUpToDate(t *testing.T) {
+	wantstd := getStdpkgs()
+	if !reflect.DeepEqual(std, wantstd) {
+		t.Error("List of standard packages not up to date. Please re-run mkstdpkgs.sh")
+	}
+}
