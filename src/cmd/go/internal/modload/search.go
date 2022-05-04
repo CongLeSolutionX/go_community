6d73bf1873b7c213f90f9a39af4ab288a8134b02
Michael Matloob <matloob@golang.org>
2022-05-04 17:11:35 -0400

    cmd/go: parallelize matchPackages work in each module
    In each module matchPackages looks in, when doing the walk, do the
    scanDir call in a par.Queue so all the read work can be done in
    parallel.
    
    Change-Id: I27153dbb3a2ed417ca24972f47134e9e914a55d1

--- src/cmd/go/internal/modload/search.go
+++ src/cmd/go/internal/modload/search.go
@@ -11,12 +11,16 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"runtime"
+	"sort"
 	"strings"
+	"sync"
 
 	"cmd/go/internal/cfg"
 	"cmd/go/internal/fsys"
 	"cmd/go/internal/imports"
 	"cmd/go/internal/modindex"
+	"cmd/go/internal/par"
 	"cmd/go/internal/search"
 	"cmd/go/internal/trace"
 
@@ -46,6 +50,7 @@ func matchPackages(ctx context.Context, m *search.Match, tags map[string]bool, f
 		treeCanMatch = search.TreeCanMatchPattern(m.Pattern())
 	}
 
+	var mu sync.Mutex
 	have := map[string]bool{
 		"builtin": true, // ignore pseudo-package that exists only for documentation
 	}
@@ -58,6 +63,8 @@ func matchPackages(ctx context.Context, m *search.Match, tags map[string]bool, f
 		pruneVendor = pruning(1 << iota)
 		pruneGoMod
 	)
+
+	q := par.NewQueue(runtime.GOMAXPROCS(0))
 
 	walkPkgs := func(root, importPathRoot string, prune pruning) {
 		_, span := trace.StartSpan(ctx, "walkPkgs "+root)
@@ -113,14 +120,23 @@ func matchPackages(ctx context.Context, m *search.Match, tags map[string]bool, f
 				}
 			}
 
-			if !have[name] {
-				have[name] = true
-				if isMatch(name) {
-					if _, _, err := scanDir(path, tags); err != imports.ErrNoGo {
-						m.Pkgs = append(m.Pkgs, name)
+			q.Add(func() {
+				mu.Lock()
+				h := have[name]
+				if !h {
+					have[name] = true
+				}
+				mu.Unlock()
+				if !h {
+					if isMatch(name) {
+						if _, _, err := scanDir(path, tags); err != imports.ErrNoGo {
+							mu.Lock()
+							m.Pkgs = append(m.Pkgs, name)
+							mu.Unlock()
+						}
 					}
 				}
-			}
+			})
 
 			if elem == "vendor" && (prune&pruneVendor != 0) {
 				return filepath.SkipDir
@@ -131,6 +147,13 @@ func matchPackages(ctx context.Context, m *search.Match, tags map[string]bool, f
 			m.AddError(err)
 		}
 	}
+
+	// Wait for all in-flight operations to complete before returning.
+	start := len(m.Pkgs)
+	defer func() {
+		<-q.Idle()
+		sort.Strings(m.Pkgs[start:]) // sort everything we added for determinism
+	}()
 
 	if filter == includeStd {
 		walkPkgs(cfg.GOROOTsrc, "", pruneGoMod)
