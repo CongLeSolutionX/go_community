4f5d7c2d1e04a71fa7c091decbe866ac467e4ad6
Michael Anthony Knyszek <mknyszek@google.com>
2022-05-06 20:11:28 +0000

    runtime/metrics: add CPU stats
    This changes adds a breakdown for estimated CPU usage by time. These
    estimates are not based on real on-CPU counters, so each metric has a
    disclaimer explaining so. They can, however, be more reasonably
    compared to the total CPU time measured the same way.
    
    Fixes #47216.
    
    Change-Id: I125006526be9f8e0d609200e193da5a78d9935be

--- src/runtime/mpagealloc.go
+++ src/runtime/mpagealloc.go
@@ -270,17 +270,10 @@ type pageAlloc struct {
 		// scavenge.
 		index scavengeIndex
 
-		// released is the amount of memory released this generation.
+		// released is the amount of memory released this scavenge cycle.
 		//
 		// Updated atomically.
 		released uintptr
-
-		_ uint32 // Align assistTime for atomics on 32-bit platforms.
-
-		// scavengeAssistTime is the time spent scavenging in the last GC cycle.
-		//
-		// This is reset once a GC cycle ends.
-		assistTime atomic.Int64
 	}
 
 	// mheap_.lock. This level of indirection makes it possible
