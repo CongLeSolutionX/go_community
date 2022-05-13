4bd5b05530b60c07db20109ac5870a00091fdf65
Khaled Yakdan <yakdan@code-intelligence.com>
2022-05-13 15:46:45 +0000

    cmd/compile: support libFuzzer value profiling mode for integer compares
    libFuzzer provides a special mode known as “value profiling” in which it
    tracks the bit-wise progress made by the fuzzer in satisfying tracked
    comparisons. Furthermore, libFuzzer uses the value of the return address
    in its hooks to distinguish the progress for different comparisons.
    
    The original implementation of the interception for integer comparisons
    in Go simply called the libFuzzer hooks from a function written in Go
    assembly. The libFuzzer hooks thus always see the same return address
    (i.e., the address of the call instruction in the assembly snippet) and
    thus can’t distinguish individual comparisons anymore. This drastically
    reduces the usefulness of value profiling.
    
    This is fixed by using an assembly trampoline that injects synthetic but
    valid return addresses on the stack before calling the libFuzzer hook,
    otherwise preserving the calling convention of the respective platform
    (for starters, x86_64 Windows or Unix). These fake PCs are generated
    deterministically based on the location of the compare instruction in
    the IR representation.
    
    Change-Id: Iea68057c83aea7f9dc226fba7128708e8637d07a
    GitHub-Last-Rev: bd20788b449155029373ae4172f1af2e8bda2648
    GitHub-Pull-Request: golang/go#51321

--- src/runtime/libfuzzer_arm64.s
+++ src/runtime/libfuzzer_arm64.s
@@ -9,12 +9,26 @@
 
 // Based on race_arm64.s; see commentary there.
 
-// func runtime·libfuzzerCall(fn, arg0, arg1 uintptr)
-// Calls C function fn from libFuzzer and passes 2 arguments to it.
-TEXT	runtime·libfuzzerCall(SB), NOSPLIT, $0-24
+#define RARG0 R0
+#define RARG1 R1
+
+#define REPEAT_2(a) a a
+#define REPEAT_8(a) REPEAT_2(REPEAT_2(REPEAT_2(a)))
+#define REPEAT_128(a) REPEAT_2(REPEAT_8(REPEAT_8(a)))
+
+// void runtime·libfuzzerCallTraceIntCmp(fn, arg0, arg1, fakePC uintptr)
+// Calls C function fn from libFuzzer and passes 2 arguments to it after
+// manipulating the return address so that libfuzzer's integer compare hooks
+// work.
+// The problem statment and solution are documented in detail in libfuzzer_amd64.s.
+// See commentary there.
+TEXT	runtime·libfuzzerCallTraceIntCmp(SB), NOSPLIT, $8-32
 	MOVD	fn+0(FP), R9
-	MOVD	arg0+8(FP), R0
-	MOVD	arg1+16(FP), R1
+	MOVD	arg0+8(FP), RARG0
+	MOVD	arg1+16(FP), RARG1
+	MOVD    fakePC+24(FP), R8
+	// Save the original return address in a local variable
+	MOVD    R30, savedRetAddr-8(SP)
 
 	MOVD	g_m(g), R10
 
@@ -26,6 +40,29 @@ TEXT	runtime·libfuzzerCall(SB), NOSPLIT, $0-24
 	MOVD	(g_sched+gobuf_sp)(R11), R12
 	MOVD	R12, RSP
 call:
-	BL	R9
+	// Load address of the ret sled into the default register for the return
+	// address (offset of four instructions, which means 16 bytes).
+	ADR $16, R30
+	// Clear the lowest 2 bits of fakePC. All ARM64 instructions are four
+	// bytes long, so we cannot get better return address granularity than
+	// multiples of 4.
+	AND     $-4, R8, R8
+	// Add the offset of the fake_pc-th ret.
+	ADD     R8, R30, R30
+	// Call the function by jumping to it and reusing all registers except
+	// for the modified return address register R30.
+	JMP (R9)
+
+// The ret sled for ARM64 consists of 128 br instructions jumping to the
+// end of the function. Each instruction is 4 bytes long. The sled thus
+// has the same byte length of 4 * 128 = 512 as the x86_64 sled, but
+// coarser granularity.
+#define RET_SLED \
+	JMP end_of_function;
+
+    REPEAT_128(RET_SLED);
+
+end_of_function:
 	MOVD	R19, RSP
+	MOVD    savedRetAddr-8(SP), R30
 	RET
