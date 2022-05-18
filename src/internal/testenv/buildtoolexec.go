1ba9cce3064fda65482374f467f0613ca9caf96c
Than McIntosh <thanm@google.com>
2022-05-18 15:29:31 -0400

    internal/testenv: add BuildToolExec
    Add a new helper function to build a wrapper executable suitable for
    use with the "-toolexec" option to "go build". This is intended to be
    a central version that can be used in cmd/* unit tests, so as to
    avoid the need for replicating this logic in multiple tests.
    
    Change-Id: Ifc34611d585710c0b62031a500fed9156717b5eb

---
+++ src/internal/testenv/buildtoolexec.go
@@ -0,0 +1,72 @@
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package testenv
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+const toolExecTemplate = `
+package main
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	if strings.HasSuffix(strings.TrimSuffix(os.Args[2], ".exe"), "REPLACEME") {
+		os.Args[2] = os.Args[1]
+	}
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		os.Exit(1)
+	}
+}
+`
+
+// BuildToolExec builds a "toolexec" wrapper suitable for use
+// in testing a freshly built copy of 'cmd' with "go build"
+// invocations. This is intended to be used in unit tests in the
+// "cmd" subtree, such as cmd/compile, cmd/link, etc.
+func BuildToolExec(t *testing.T, cmd string) (string, error) {
+	if !HasGoBuild() {
+		return "", fmt.Errorf("BuildToolExec: no 'go build' support")
+	}
+
+	// Double-check the command just to make sure it is on the short
+	// list. There are other tools (ex: pprof) but it doesn't make a
+	// lot of sense to use them here.
+	switch cmd {
+	case "link", "compile", "asm", "cover", "buildid", "cgo", "pack", "vet":
+	default:
+		return "", fmt.Errorf("BuildToolExec: unexpected cmd %q", cmd)
+	}
+
+	// Write a modified version of the source above into
+	// a temp directory.
+	contents := strings.Replace(toolExecTemplate, "REPLACEME", cmd, 1)
+	dir := t.TempDir()
+	srcpath := filepath.Join(dir, "toolexec.go")
+	if err := os.WriteFile(srcpath, []byte(contents), 0666); err != nil {
+		t.Fatalf("os.WriteFile(%s) failed: %v", srcpath, err)
+	}
+
+	// Build tool, return a path to binary.
+	exepath := filepath.Join(dir, "toolexec.exe")
+	out, err := exec.Command(GoToolPath(t), "build", "-o", exepath, srcpath).CombinedOutput()
+	if len(out) > 0 {
+		t.Logf("%s", out)
+	}
+	return exepath, err
+}
