24e3b0e240f8f150bf3a05ad5696c98f7d448736
Matthew Dempsky <mdempsky@google.com>
2022-05-20 12:48:17 -0700

    internal/pkgbits: finish documentation
    This CL adds documentation for all exported pkgbits APIs, and removes
    its UNREVIEWED comments.
    
    Updates #48194.
    
    Change-Id: Ifff548cd9f31a5c5cc5f400a6dae5c98c46ec4ca

--- src/internal/pkgbits/codes.go
+++ src/internal/pkgbits/codes.go
@@ -1,16 +1,22 @@
-// UNREVIEWED
-
 // Copyright 2021 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package pkgbits
 
+// A Code is an enum value that can be encoded into bitstreams.
+//
+// Code types are preferable for enum types, because they allow
+// Decoder to detect desyncs.
 type Code interface {
+	// Marker returns the SyncMarker for the Code's dynamic type.
 	Marker() SyncMarker
+
+	// Value returns the Code's ordinal value.
 	Value() int
 }
 
+// A CodeVal distinguishes among go/constant.Value encodings.
 type CodeVal int
 
 func (c CodeVal) Marker() SyncMarker { return SyncVal }
@@ -25,6 +31,7 @@ const (
 	ValBigFloat
 )
 
+// A CodeType distinguishes among go/types.Type encodings.
 type CodeType int
 
 func (c CodeType) Marker() SyncMarker { return SyncType }
@@ -45,6 +52,7 @@ const (
 	TypeTypeParam
 )
 
+// A CodeObj distinguishes among go/types.Object encodings.
 type CodeObj int
 
 func (c CodeObj) Marker() SyncMarker { return SyncCodeObj }
