6498d3304e32205f09ff5bd453cabdf4bad3ba09
Cherry Mui <cherryyz@google.com>
2022-05-27 18:51:00 -0400

    runtime: check heap to stack pointers
    DO NOT REVIEW
    DO NOT SUBMIT
    
    Heap to stack pointers should not occur except in a few special
    cases. This CL makes the runtime check if a heap pointer points
    to the stack.
    
    This may help detect issues in the previous CL.
    
    Change-Id: I5682a637954555941478a928aa8754b280a5e2aa

--- src/runtime/mgcmark.go
+++ src/runtime/mgcmark.go
@@ -1230,8 +1230,13 @@ func scanblock(b0, n0 uintptr, ptrmask *uint8, gcw *gcWork, stk *stackScanState)
 				if p != 0 {
 					if obj, span, objIndex := findObject(p, b, i); obj != 0 {
 						greyobject(obj, b, i, span, gcw, objIndex)
-					} else if stk != nil && p >= stk.stack.lo && p < stk.stack.hi {
-						stk.putPtr(p, false)
+					} else if stk != nil {
+						if p >= stk.stack.lo && p < stk.stack.hi {
+							stk.putPtr(p, false)
+						}
+					} else if span != nil && span.state.get() == mSpanManual {
+						println("runtime: heap pointer at", hex(b), "points to stack", hex(p))
+						throw("heap to stack pointer")
 					}
 				}
 			}
@@ -1315,11 +1320,11 @@ func scanobject(b uintptr, gcw *gcWork) {
 
 		// Work here is duplicated in scanblock and above.
 		// If you make changes here, make changes there too.
-		obj := *(*uintptr)(unsafe.Pointer(b + i))
+		p := *(*uintptr)(unsafe.Pointer(b + i))
 
 		// At this point we have extracted the next potential pointer.
 		// Quickly filter out nil and pointers back to the current object.
-		if obj != 0 && obj-b >= n {
+		if p != 0 && p-b >= n {
 			// Test if obj points into the Go heap and, if so,
 			// mark the object.
 			//
@@ -1329,8 +1334,13 @@ func scanobject(b uintptr, gcw *gcWork) {
 			// heap. In this case, we know the object was
 			// just allocated and hence will be marked by
 			// allocation itself.
-			if obj, span, objIndex := findObject(obj, b, i); obj != 0 {
+			if obj, span, objIndex := findObject(p, b, i); obj != 0 {
 				greyobject(obj, b, i, span, gcw, objIndex)
+			} else if span != nil && span.state.get() == mSpanManual {
+				if n != deferSize && n != gSize && n != sudogSize { // _defer, g, sudog may contain stack pointers
+					println("runtime: heap pointer at", hex(b), "points to stack", hex(p), "elemsize", n)
+					throw("heap to stack pointer")
+				}
 			}
 		}
 	}
