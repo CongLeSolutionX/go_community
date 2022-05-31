c25173d444eb3e4e8cfefaa4802b553f534fe2f3
Kévin Dunglas <kevin@dunglas.fr>
2022-05-31 17:29:11 +0000

    net/http: reverseproxy: forward 1xx responses
    Support for 1xx responses has recently been merged in
    net/http (#42597).
    
    As discussed in this CL
    (https://go-review.googlesource.com/c/go/+/269997/comments/1ff70bef_c25a829a),
    support for forwarding 1xx responses in ReverseProxy has been extracted
    in this separate patch.
    
    According to RFC 7231, "a proxy MUST forward 1xx responses unless the
    proxy itself requested the generation of the 1xx response".
    Consequently, all received 1xx responses are automatically forwarded as long as the
    underlying transport supports ClientTrace.Got1xxResponse.
    
    Fixes #26088
    Fixes #51914
    
    Change-Id: I3a35ea023b798bfe56b7fb8696d5a49695229cfd
    GitHub-Last-Rev: aa23135a9d2150c4e72f0860374467043e539383
    GitHub-Pull-Request: golang/go#53164

--- src/net/http/httputil/reverseproxy.go
+++ src/net/http/httputil/reverseproxy.go
@@ -14,6 +14,7 @@ import (
 	"mime"
 	"net"
 	"net/http"
+	"net/http/httptrace"
 	"net/http/internal/ascii"
 	"net/textproto"
 	"net/url"
@@ -40,6 +41,9 @@ import (
 // To prevent IP spoofing, be sure to delete any pre-existing
 // X-Forwarded-For header coming from the client or
 // an untrusted proxy.
+//
+// 1xx responses are forwarded to the client if the underlying
+// transport supports ClientTrace.Got1xxResponse.
 type ReverseProxy struct {
 	// Director must be a function which modifies
 	// the request into a new request to be sent
@@ -307,6 +311,23 @@ func (p *ReverseProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		}
 	}
 
+	var headerSet bool
+	trace := &httptrace.ClientTrace{
+		Got1xxResponse: func(code int, header textproto.MIMEHeader) error {
+			h := rw.Header()
+			copyHeader(h, http.Header(header))
+			rw.WriteHeader(code)
+
+			// Clear headers, it's not automatically done by ResponseWriter.WriteHeader() for 1xx responses
+			for k, _ := range h {
+				h.Del(k)
+			}
+
+			return nil
+		},
+	}
+	outreq = outreq.WithContext(httptrace.WithClientTrace(outreq.Context(), trace))
+
 	res, err := transport.RoundTrip(outreq)
 	if err != nil {
 		p.getErrorHandler()(rw, outreq, err)
@@ -332,7 +353,14 @@ func (p *ReverseProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	copyHeader(rw.Header(), res.Header)
+	h := rw.Header()
+	if headerSet {
+		for k, _ := range h {
+			h.Del(k)
+		}
+	}
+
+	copyHeader(h, res.Header)
 
 	// The "Trailer" header isn't included in the Transport's response,
 	// at least for *http.Transport. Build it up from Trailer.
