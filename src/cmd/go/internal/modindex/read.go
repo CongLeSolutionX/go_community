cb5f040df100893813daa27be0893834b79d3233
Michael Matloob <matloob@golang.org>
2022-06-07 15:31:20 -0400

    cmd/go: set Root and target fields for packages in GOPATH
    This change replicates the behavior filed in issue #37015 for packages
    imported from the module index. That behavior is that packages that
    happen to exist in a GOPATH src directory have p.Root and p.Target set
    even when the packages are loaded from modules. This is likely
    unintentional behavior because in module mode, packages shouldn't behave
    differently depending on whether their directories exist in GOPATH. But
    for uniformity, (and because two of our tests depend on this behavior),
    this CL will implement this behavior. We can remove it from the module
    index when we remove it from the go/build logic.
    
    Change-Id: I3f501c92fbb76eaf86b6b9275539f2129b67f884

--- src/cmd/go/internal/modindex/read.go
+++ src/cmd/go/internal/modindex/read.go
@@ -246,22 +246,33 @@ func (mi *ModuleIndex) Import(bctxt build.Context, relpath string, mode build.Im
 	inTestdata := func(sub string) bool {
 		return strings.Contains(sub, "/testdata/") || strings.HasSuffix(sub, "/testdata") || str.HasPathPrefix(sub, "testdata")
 	}
-	if ctxt.GOROOT != "" && str.HasFilePathPrefix(mi.modroot, cfg.GOROOTsrc) && !inTestdata(relpath) {
-		modprefix := str.TrimFilePathPrefix(mi.modroot, cfg.GOROOTsrc)
-		p.Goroot = true
-		p.ImportPath = relpath
-		if modprefix != "" {
-			p.ImportPath = filepath.Join(modprefix, p.ImportPath)
-		}
+	if !inTestdata(relpath) {
 		// In build.go, p.Root should only be set in the non-local-import case, or in
 		// GOROOT or GOPATH. Since module mode only calls Import with path set to "."
 		// and the module index doesn't apply outside modules, the GOROOT case is
 		// the only case where GOROOT needs to be set.
+		// But: p.Root is actually set in the local-import case outside GOROOT, if
+		// the directory is contained in GOPATH/src
 		// TODO(#37015): p.Root actually might be set in the local-import case outside
 		// GOROOT, if the directory is contained in GOPATH/src, even in module
 		// mode, but that's a bug.
-		p.Root = ctxt.GOROOT
+		if ctxt.GOROOT != "" && str.HasFilePathPrefix(mi.modroot, cfg.GOROOTsrc) {
+			p.Root = ctxt.GOROOT
+			p.Goroot = true
+			modprefix := str.TrimFilePathPrefix(mi.modroot, cfg.GOROOTsrc)
+			p.ImportPath = relpath
+			if modprefix != "" {
+				p.ImportPath = filepath.Join(modprefix, p.ImportPath)
+			}
+		}
+		for _, root := range ctxt.gopath() {
+			if rootSrc := filepath.Join(root, "src"); str.HasFilePathPrefix(mi.modroot, rootSrc) {
+				p.Root = ctxt.GOPATH
+			}
+		}
 
+	}
+	if p.Root != "" {
 		// Set GOROOT-specific fields
 		// The fields set below (SrcRoot, PkgRoot, BinDir, PkgTargetRoot, and PkgObj)
 		// are only set in build.Import if p.Root != "". As noted in the comment
