e38f21344f3e3841e078912b8205a221f03b1595
Xiaodong Liu <liuxiaodong@loongson.cn>
2022-06-25 15:47:49 +0800

    cmd/go/internal/work: add GOBUILDMODE_shared macro to support c-shared buildmode on mips64{,le}
    For #43264
    
    Change-Id: I806832d85389df1fb8ecdc8116a8ac65a161e4ac

--- src/cmd/go/internal/work/gc.go
+++ src/cmd/go/internal/work/gc.go
@@ -366,11 +366,14 @@ func asmArgs(a *Action, p *load.Package) []any {
 	inc := filepath.Join(cfg.GOROOT, "pkg", "include")
 	pkgpath := pkgPath(a)
 	args := []any{cfg.BuildToolexec, base.Tool("asm"), "-p", pkgpath, "-trimpath", a.trimpath(), "-I", a.Objdir, "-I", inc, "-D", "GOOS_" + cfg.Goos, "-D", "GOARCH_" + cfg.Goarch, forcedAsmflags, p.Internal.Asmflags}
-	if p.ImportPath == "runtime" && cfg.Goarch == "386" {
-		for _, arg := range forcedAsmflags {
-			if arg == "-dynlink" {
-				args = append(args, "-D=GOBUILDMODE_shared=1")
-			}
+	for _, arg := range forcedAsmflags {
+		if arg == "-dynlink" && p.ImportPath == "runtime" && cfg.Goarch == "386" {
+			args = append(args, "-D=GOBUILDMODE_shared=1")
+			break
+		}
+		if arg == "-shared" && (p.ImportPath == "runtime" || p.ImportPath == "runtime/cgo") && (cfg.Goarch == "mips64" || cfg.Goarch == "mips64le") {
+			args = append(args, "-D", "GOBUILDMODE_shared")
+			break
 		}
 	}
 	if objabi.IsRuntimePackagePath(pkgpath) {
