747a393d629d316de39e27e5afceb8f2136508df
Bryan C. Mills <bcmills@google.com>
2022-07-20 17:02:35 -0400

    cmd/go: extract the TestScript engine into a standalone package
    This change decouples the script engine from both cmd/go and the
    testing package; I intend to reuse it in the replacement for the
    vcs-test.golang.org server.
    
    This change also adds a few new script commands:
    
    	- 'echo' echoes its arguments, useful for verifying argument expansion.
    
    	- 'cat' prints the contents of files, useful for debugging failing script tests.
    
    	- 'help' displays information about script commands and conditions,
    	  reducing the toil of maintaining lists in the README file.
    
    The 'cmp' and 'cmpenv' commands now use internal/diff instead of their
    own separate diff implementation.
    
    The 'env' command now writes to the script log instead of the stdout
    buffer. (This makes it more consistent with the behavior of other
    synchronous builtins.)
    
    The 'stale' command no longer logs output when a target is
    unexpectedly non-stale. (However, the ouput of the 'stale' command is
    not usually very useful anyway.)
    
    The 'grep', 'stdout', and 'stderr' commands now display matching lines
    (like Unix 'grep'), making their negation behavior more consistent
    with running real commands on a command-line.
    
    Likewise, the 'cmp' command now always displays differences. That
    makes it useful with the '?' prefix to produce diffs for informational
    purposes while debugging.
    
    For #27494.
    
    Change-Id: If49fd81d9b922d07c20618a8e2cef908191f9ef6

--- src/cmd/go/go_test.go
+++ src/cmd/go/go_test.go
@@ -47,12 +47,10 @@ func init() {
 }
 
 var (
-	canRace          = false // whether we can run the race detector
-	canCgo           = false // whether we can use cgo
-	canMSan          = false // whether we can run the memory sanitizer
-	canASan          = false // whether we can run the address sanitizer
-	canFuzz          = false // whether we can search for new fuzz failures
-	fuzzInstrumented = false // whether fuzzing uses instrumentation
+	canRace = false // whether we can run the race detector
+	canCgo  = false // whether we can use cgo
+	canMSan = false // whether we can run the memory sanitizer
+	canASan = false // whether we can run the address sanitizer
 )
 
 var (
@@ -267,8 +265,6 @@ func TestMain(m *testing.M) {
 		if isAlpineLinux() || runtime.Compiler == "gccgo" {
 			canRace = false
 		}
-		canFuzz = sys.FuzzSupported(runtime.GOOS, runtime.GOARCH)
-		fuzzInstrumented = sys.FuzzInstrumented(runtime.GOOS, runtime.GOARCH)
 	}
 
 	// Don't let these environment variables confuse the test.
