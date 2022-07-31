3f5c392d0500a252a217629621f4c5bb8cafef26
ianwoolf <btw515wolf2@gmail.com>
2022-07-31 20:35:38 +0800

    net/http: check dir from query parameter or form values if contains DotDot for ServeFile
    Updates #53946
    
    Change-Id: I8bed6d230b971b7ca18c3ddf5e424ec63a34e38e

--- src/net/http/fs.go
+++ src/net/http/fs.go
@@ -713,26 +713,28 @@ func localRedirect(w ResponseWriter, r *Request, newPath string) {
 // r.URL.Path for selecting the file or directory to serve; only the
 // file or directory provided in the name argument is used.
 func ServeFile(w ResponseWriter, r *Request, name string) {
-	if containsDotDot(r.URL.Path) {
-		// Too many programs use r.URL.Path to construct the argument to
-		// serveFile. Reject the request under the assumption that happened
-		// here and ".." may not be wanted.
+	dir, file := filepath.Split(name)
+	if containsDotDot(r.URL.Path, dir) {
+		// Too many programs use r.URL.Path, query parameter or form values
+		// to construct the argument to serveFile. Reject the request under
+		// the assumption that happened here and ".." may not be wanted.
 		// Note that name might not contain "..", for example if code (still
 		// incorrectly) used filepath.Join(myDir, r.URL.Path).
 		Error(w, "invalid URL path", StatusBadRequest)
 		return
 	}
-	dir, file := filepath.Split(name)
 	serveFile(w, r, Dir(dir), file, false)
 }
 
-func containsDotDot(v string) bool {
-	if !strings.Contains(v, "..") {
-		return false
-	}
-	for _, ent := range strings.FieldsFunc(v, isSlashRune) {
-		if ent == ".." {
-			return true
+func containsDotDot(l ...string) bool {
+	for _, v := range l {
+		if !strings.Contains(v, "..") {
+			continue
+		}
+		for _, ent := range strings.FieldsFunc(v, isSlashRune) {
+			if ent == ".." {
+				return true
+			}
 		}
 	}
 	return false
