c2158d472a9526aa2a6481afa11d66664fe82173
Tobias Klauser <tklauser@distanz.ch>
2022-08-17 10:30:38 +0200

    runtime/pprof: report MaxRSS on windows
    Use GetProcessMemoryInfo to report MaxRSS in memory profiles on windows.
    
    Change-Id: I4ac5fe58961b1d5da8a5c1caa8a6e3d0a3281837

---
+++ src/runtime/pprof/pprof_windows.go
@@ -0,0 +1,20 @@
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package pprof
+
+import (
+	"fmt"
+	"internal/syscall/windows"
+	"io"
+	"syscall"
+	"unsafe"
+)
+
+func addMaxRSS(w io.Writer) {
+	var m windows.PROCESS_MEMORY_COUNTERS
+	p, _ := syscall.GetCurrentProcess()
+	windows.GetProcessMemoryInfo(p, &m, uint32(unsafe.Sizeof(m)))
+	fmt.Fprintf(w, "# MaxRSS = %d\n", m.PeakWorkingSetSize)
+}
