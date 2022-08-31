e8e3f53522f207a20f7bc8bdfe870431646eb3c4
Matthew Dempsky <mdempsky@google.com>
2022-08-31 15:48:35 -0700

    cmd/compile: reject not-in-heap types as type arguments
    After running the types2 type checker, walk info.Instances to reject
    any not-in-heap type arguments. This is feasible to check using the
    types2 API now, thanks to #46731.
    
    Fixes #54765.
    
    Change-Id: Idd2acc124d102d5a76f128f13c21a6e593b6790b

---
+++ test/typeparam/issue54765.go
@@ -0,0 +1,25 @@
+// errorcheck
+
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Test that not-in-heap types cannot be used as type
+// arguments. (pointer-to-nih types are okay though.)
+
+package p
+
+import (
+	"runtime/cgo"
+	"sync/atomic"
+)
+
+var _ atomic.Pointer[cgo.Incomplete]  // ERROR "cannot use incomplete \(or unallocatable\) type as type argument: runtime/cgo\.Incomplete"
+var _ atomic.Pointer[*cgo.Incomplete] // ok
+
+func implicit(ptr *cgo.Incomplete) {
+	g(ptr)  // ERROR "cannot use incomplete \(or unallocatable\) type as type argument: runtime/cgo\.Incomplete"
+	g(&ptr) // ok
+}
+
+func g[T any](_ *T) {}
