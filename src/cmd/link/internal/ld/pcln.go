5844fcb8d8b198daa3eee332cd3dc13f15891896
Joel Sing <joel@sing.id.au>
2022-08-31 18:18:19 +1000

    cmd/internal/obj/riscv,cmd/link: add support for internal cgo linking on riscv64
    Make it possible to internally link cgo on riscv64, which also adds
    support for SDYNIMPORT calls without external linking being required.
    
    This reduces the time of an ./all.bash run on a Sifive Hifive Unleashed by
    approximately 20% (~140 minutes down to ~110 minutes).
    
    Change-Id: I43f1348de31672718ae8676cc82f6fdc1dfee054

--- src/cmd/link/internal/ld/pcln.go
+++ src/cmd/link/internal/ld/pcln.go
@@ -15,6 +15,7 @@ import (
 	"internal/buildcfg"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 const funcSize = 11 * 4 // funcSize is the size of the _func object in runtime/runtime2.go
@@ -99,6 +100,17 @@ func makePclntab(ctxt *Link, container loader.Bitmap) (*pclntab, []*sym.Compilat
 }
 
 func emitPcln(ctxt *Link, s loader.Sym, container loader.Bitmap) bool {
+	if ctxt.Target.IsRISCV64() {
+		// Avoid adding local symbols to the pcln table - RISC-V
+		// linking generates a very large number of these, particularly
+		// for "HI20" symbols. Including all of them quickly blows out
+		// the size of the pcln table and hash buckets.
+		symName := ctxt.loader.SymName(s)
+		if symName == "" || strings.HasPrefix(symName, ".L") {
+			return false
+		}
+	}
+
 	// We want to generate func table entries only for the "lowest
 	// level" symbols, not containers of subsymbols.
 	return !container.Has(s)
