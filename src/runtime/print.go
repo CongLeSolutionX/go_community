9af16b6a23899f3040bd7a88071ee94bdb6825c1
shaoliming <shaojacobi@gmail.com>
2022-09-01 08:01:01 +0000

    runtime: fix printlock/printunlock deadlock
    Fixes #54786
    
    Change-Id: I48d2cb00680b987bee6cbde410ea4ceb2a70b69b
    GitHub-Last-Rev: faa3f1e39fbe9df4a2024eafffaa217853351456
    GitHub-Pull-Request: golang/go#54808

--- src/runtime/print.go
+++ src/runtime/print.go
@@ -64,20 +64,33 @@ var debuglock mutex
 
 func printlock() {
 	mp := getg().m
+	// ensure other grountines scheduled on this mp can't modify printlock
+	// this is very similar to readers–writers problems:
+	// https://en.wikipedia.org/wiki/Readers%E2%80%93writers_problem
+	for mp.printlockMutex != 0 {
+		Gosched()
+	}
+	mp.printlockMutex++
 	mp.locks++ // do not reschedule between printlock++ and lock(&debuglock).
 	mp.printlock++
 	if mp.printlock == 1 {
 		lock(&debuglock)
 	}
 	mp.locks-- // now we know debuglock is held and holding up mp.locks for us.
+	mp.printlockMutex--
 }
 
 func printunlock() {
 	mp := getg().m
+	for mp.printlockMutex != 0 {
+		Gosched()
+	}
+	mp.printlockMutex++
 	mp.printlock--
 	if mp.printlock == 0 {
 		unlock(&debuglock)
 	}
+	mp.printlockMutex--
 }
 
 // write to goroutine-local buffer if diverting output,
