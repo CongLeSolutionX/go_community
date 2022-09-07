cf9233bae0960c7e544cb36943c6a3c11c6bbe26
Michael Anthony Knyszek <mknyszek@google.com>
2022-09-07 20:03:25 +0000

    runtime: remove atomic store requirement on pageAlloc.chunks
    pageAlloc.chunks used to require an atomic store when growing the heap
    because the scavenger would look at the list without locking the heap
    lock. However, the scavenger doesn't do that anymore, and it looks like
    nothing really does at all.
    
    We can't actually make the store non-atomic because that invokes a write
    barrier (not safe here) but we can at least update the documentation.
    
    Change-Id: Ib452d147861060f9f6e74e2d98ee111cf89ce8f5

--- src/runtime/mpagealloc.go
+++ src/runtime/mpagealloc.go
@@ -393,13 +393,13 @@ func (p *pageAlloc) grow(base, size uintptr) {
 	for c := chunkIndex(base); c < chunkIndex(limit); c++ {
 		if p.chunks[c.l1()] == nil {
 			// Create the necessary l2 entry.
-			//
-			// Store it atomically to avoid races with readers which
-			// don't acquire the heap lock.
 			r := sysAlloc(unsafe.Sizeof(*p.chunks[0]), p.sysStat)
 			if r == nil {
 				throw("pageAlloc: out of memory")
 			}
+			// Store atomically, but we don't really care about the
+			// atomic part. What we really want is to avoid a write
+			// barrier here.
 			atomic.StorepNoWB(unsafe.Pointer(&p.chunks[c.l1()]), r)
 		}
 		p.chunkOf(c).scavenged.setRange(0, pallocChunkPages)
