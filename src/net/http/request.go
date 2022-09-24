1ca272bad73da283bb5dca59c1a65d956846e6d9
Andy Pan <panjf2000@gmail.com>
2022-09-24 13:13:14 +0800

    net/http: fix the potential leak of textproto.Reader from pool
    Fixes #55835
    
    Change-Id: I6109bab2941b859e8cfef22f65a6a3a5f977a8d7

--- src/net/http/request.go
+++ src/net/http/request.go
@@ -14,6 +14,7 @@ import (
 	"encoding/base64"
 	"errors"
 	"fmt"
+	"golang.org/x/net/idna"
 	"io"
 	"mime"
 	"mime/multipart"
@@ -26,8 +27,6 @@ import (
 	"strconv"
 	"strings"
 	"sync"
-
-	"golang.org/x/net/idna"
 )
 
 const (
@@ -1030,6 +1029,8 @@ func ReadRequest(b *bufio.Reader) (*Request, error) {
 
 func readRequest(b *bufio.Reader) (req *Request, err error) {
 	tp := newTextprotoReader(b)
+	defer putTextprotoReader(tp)
+
 	req = new(Request)
 
 	// First line: GET /index.html HTTP/1.0
@@ -1038,7 +1039,6 @@ func readRequest(b *bufio.Reader) (req *Request, err error) {
 		return nil, err
 	}
 	defer func() {
-		putTextprotoReader(tp)
 		if err == io.EOF {
 			err = io.ErrUnexpectedEOF
 		}
