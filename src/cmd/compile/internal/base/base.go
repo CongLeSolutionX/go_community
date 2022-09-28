540e0558bfb53c0b56f2d26764133a8c08ba51c3
David Chase <drchase@google.com>
2022-09-28 17:10:59 -0400

    cmd/compile: if GOGC is not set, temporarily boost it for rapid heap growth
    This is an experiment to see if it is helpful to build times.
    
    Change-Id: I45df1c927c1a7d7503ade1abd1a3300e27516633

--- src/cmd/compile/internal/base/base.go
+++ src/cmd/compile/internal/base/base.go
@@ -5,7 +5,11 @@
 package base
 
 import (
+	"fmt"
 	"os"
+	"runtime"
+	"runtime/debug"
+	"runtime/metrics"
 )
 
 var atExitFuncs []func()
@@ -25,6 +29,67 @@ func Exit(code int) {
 
 // To enable tracing support (-t flag), set EnableTrace to true.
 const EnableTrace = false
+
+func init() {
+	if os.Getenv("GOGC") != "" {
+		// do not interfere with requested GOGC.
+		return
+	}
+	logHeapTweaks := os.Getenv("GO_gc_LOG_HEAP_TWEAKS") != ""
+	const allocs = "/gc/heap/allocs:bytes"
+	const frees = "/gc/heap/frees:bytes"
+	const goal = "/gc/heap/goal:bytes"
+	const startHeapGoal = uint64(4 * 1024 * 1024) // first GC has this goal and it does not adjust
+	const threshold = uint64(64 * 1024 * 1024)    // 64M -- Once heap size (live + new) exceeds threshold, back off
+
+	sample := []metrics.Sample{metrics.Sample{Name: allocs}, metrics.Sample{Name: frees}, metrics.Sample{Name: goal}}
+	metrics.Read(sample)
+	for _, s := range sample {
+		if s.Value.Kind() == metrics.KindBad {
+			// fmt.Fprintf(os.Stderr, "Unexpected kind-bad for metric %s\n", s.Name)
+			return
+		}
+	}
+
+	// Tinker with GOGC to make the heap grow rapidly at first.
+	currentGoal := sample[2].Value.Uint64() // Believe this will be 4MByte or less, perhaps 512k
+	// Recall goal = live (assumed 2M at start) plus GOC * live, hence initially 4M
+
+	myGogc := (100 + 100*threshold) / currentGoal //
+
+	// Note that notification is driven by finalization, so expect heap to hit desired goal, GC, then finalizer, and allocs-frees will be near-to-above threshold.
+	debug.SetGCPercent(int(myGogc))
+
+	// GC quirk -- despite increasing GOGC, for the first collection that does not affect the goal.
+	// therefore make the finalizer tolerate running very early.
+	var adjustFunc func(pMyGogc *uint64)
+	adjustFunc = func(pMyGogc *uint64) {
+		metrics.Read(sample)
+		inUse := sample[0].Value.Uint64() - sample[1].Value.Uint64()
+		if inUse < startHeapGoal {
+			inUse = startHeapGoal
+		}
+		goal := sample[2].Value.Uint64()
+		if inUse < 2*threshold/3 {
+			oldfactor := *pMyGogc
+			*pMyGogc = 100 * threshold / inUse
+			if logHeapTweaks {
+				fmt.Fprintf(os.Stderr, "Retry GOGC adjust, current inuse %d, current goal %d, gogc was %d, is now %d\n",
+					inUse, goal, oldfactor, *pMyGogc)
+			}
+			debug.SetGCPercent(int(*pMyGogc))
+			runtime.SetFinalizer(pMyGogc, adjustFunc) // Repeat, hoping for a better result.
+			return
+		}
+		debug.SetGCPercent(100)
+		if logHeapTweaks {
+			fmt.Fprintf(os.Stderr, "Reset GC, current inuse %d, old goal %d, gogc was %d\n",
+				inUse, sample[2].Value.Uint64(), *pMyGogc)
+		}
+	}
+
+	runtime.SetFinalizer(&myGogc, adjustFunc)
+}
 
 func Compiling(pkgs []string) bool {
 	if Ctxt.Pkgpath != "" {
