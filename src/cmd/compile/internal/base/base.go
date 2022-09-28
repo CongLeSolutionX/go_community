c2dc9f645face6856a881524d95ad1a597da4904
David Chase <drchase@google.com>
2022-09-28 17:10:59 -0400

    cmd/compile: if GOGC is not set, temporarily boost it for rapid starting heap growth
    Benchmarking suggests about a 15% savings in user build time, about 4% savings
    for wall time.  This helps most builds because small packages are common. See
    https://perf.golang.org/search?q=upload:20221003.8 , for example. We've
    talked about doing this in the past, the "new"(ish) metrics package makes it
    a more tractable proposition.
    
    The method here is:
    
    1. If os.Getenv("GOGC") is empty, then increase GOGC to a  large value,
    calculated to grow the heap to 32 + 4 * compile_parallelism before a GC
    occurs (e.g., on a 4 processor box, 64M).  In practice, sometimes GC occurs
    before that, but this still results in fewer GCs and saved time.
    
    2. Use a finalizer to approximately detect when GC occurs, and use runtime
    metrics to track progress towards the goal heap size, readjusting GOGC to
    retarget it as necessary.  Reset GOGC to 100 when the heap is "close enough"
    to the goal.
    
    One feared failure mode of doing this is that the finalizer will be slow to
    run and the heap will grow exceptionally large before GOGC is reset; I
    monitored the heap size at reset across several boxes with a variety of
    processor counts and extra noise (include several builds in parallel,
    including a laptop with a busy many-tabs browser running) and the worst
    overshoot was 50%.
    
    I considered instead using the maximum heap size + GC-detecting-finalizer +
    reset instead, but to me that seemed like it might have a worse bad-case
    outcome; if the reset is delayed, it's possible the GC would start running
    frequently, making it harder to run the finalizer, reach 50% utilization, and
    the extra GCs would lose the advantage.  In practice, there are very few
    boxes doing go builds that would be inconvenienced by the compiler briefly
    consuming 128MB (a 100% overshoot, which was never observed) and the
    processor-count dependence in the threshold is intended to hedge against
    this.
    
    This version of the CL includes a comment urging anyone who sees the code and
    thinks it would work for them, to update a bug (to be created if CL is
    approved) with information about their situation/experience, so that we may
    consider creating some more official and reliable way of obtaining the same
    result.
    
    Change-Id: I45df1c927c1a7d7503ade1abd1a3300e27516633

--- src/cmd/compile/internal/base/base.go
+++ src/cmd/compile/internal/base/base.go
@@ -5,7 +5,11 @@
 package base
 
 import (
+	"fmt"
 	"os"
+	"runtime"
+	"runtime/debug"
+	"runtime/metrics"
 )
 
 var atExitFuncs []func()
@@ -25,6 +29,107 @@ func Exit(code int) {
 
 // To enable tracing support (-t flag), set EnableTrace to true.
 const EnableTrace = false
+
+// AdjustStartingHeap modifies GOGC so that the heap will grow to the
+// requested size before GC occurs.  Once this size is reached
+// (approximately) GOGC will be reset to 100.  If GOGC is supplied as an
+// environment variable, this function has no effect and the explicit request
+// takes priority.
+//
+// Setting the environment variable GO_gc_LOG_HEAP_TWEAKS to a non-empty
+// string will log GC-provoked GC adjustments to os.Stderr.
+//
+// NOTE: If you think this code would help startup time in your own
+// application and you decide to use it, please benchmark first to see if it
+// actually works for you (it may not: the Go compiler is not typical), and
+// whatever the outcome, please leave a comment on bug #VWXYZ.  This code
+// uses supported interfaces, but depends more than we like on observed
+// behavior of the garbage collector, so if many people need this feature, we
+// should consider/propose a better way to accomplish it.
+func AdjustStartingHeap(requestedHeapGoal uint64) {
+	logHeapTweaks := os.Getenv("GO_gc_LOG_HEAP_TWEAKS") != ""
+	const (
+		allocs = "/gc/heap/allocs:bytes"
+		frees  = "/gc/heap/frees:bytes"
+		goal   = "/gc/heap/goal:bytes"
+		count  = "/gc/cycles/total:gc-cycles"
+	)
+
+	// Go's GC has to guess at its initial heap limit (the point at which its
+	// first GC should complete) and currently that guess is 4MiB.  It is
+	// currently possible that it could be as low as 0.5MiB. The first GC's
+	// goal is also, currently, not adjustable, and it is not entirely clear
+	// when it occurs, so this code checks after each GC and readjusts GOGC
+	// until the goal is approximately reached.
+	const startHeapGoal = uint64(4 * 1024 * 1024)
+
+	sample := []metrics.Sample{{Name: allocs}, {Name: frees}, {Name: goal}, {Name: count}}
+	const (
+		ALLOCS = 0
+		FREES  = 1
+		GOAL   = 2
+		COUNT  = 3
+	)
+	metrics.Read(sample)
+	for _, s := range sample {
+		if s.Value.Kind() == metrics.KindBad {
+			// Just return, a slightly slower compilation is a tolerable outcome.
+			if logHeapTweaks {
+				fmt.Fprintf(os.Stderr, "AdjustStartingHeap: unexpected kind-bad for metric %s\n", s.Name)
+			}
+			return
+		}
+	}
+
+	// Tinker with GOGC to make the heap grow rapidly at first.
+	currentGoal := sample[GOAL].Value.Uint64() // Believe this will be 4MByte or less, perhaps 512k
+	myGogc := 100 * requestedHeapGoal / currentGoal
+	if myGogc <= 150 {
+		return
+	}
+	debug.SetGCPercent(int(myGogc))
+
+	// Set up a finalizer to detect garbage collection, and when that occurs,
+	// check to see what new value for GOGC is most appropriate.  If the
+	// memory in use is close-to-or-above the goal heap size, set GOGC to 100
+	// and stop fiddling with it.
+
+	var adjustFunc func(px *uint64)
+	adjustFunc = func(px *uint64) {
+		metrics.Read(sample)
+		goal := sample[GOAL].Value.Uint64()
+		count := sample[COUNT].Value.Uint64()
+
+		// This calculated value of inUse is NOT the same as goal, per experiments.
+		// If the heap is growing, steady-state assumptions don't hold.
+		inUse := sample[ALLOCS].Value.Uint64() - sample[FREES].Value.Uint64()
+		if inUse < startHeapGoal {
+			inUse = startHeapGoal
+		}
+
+		candidateGogc := 100 * requestedHeapGoal / inUse
+
+		if candidateGogc > 150 {
+			// Not done growing the heap.
+			oldGogc := debug.SetGCPercent(int(candidateGogc))
+			if logHeapTweaks {
+				fmt.Fprintf(os.Stderr, "Retry GOGC adjust, current inuse %d, current goal %d, gogc was %d, is now %d, count is %d\n",
+					inUse, goal, oldGogc, candidateGogc, count)
+			}
+			runtime.SetFinalizer(px, adjustFunc) // Repeat, hoping for a better result.
+			return
+		}
+		// In this case we're done boosting GOGC, set it to 100 and don't set a new finalizer.
+		oldGogc := debug.SetGCPercent(100)
+		if logHeapTweaks {
+			fmt.Fprintf(os.Stderr, "Reset GC, current inuse %d, old goal %d, gogc was %d, count is %d\n",
+				inUse, goal, oldGogc, count)
+		}
+	}
+
+	var x uint64 // the only purpose for x is to have a finalizer attached to it.
+	runtime.SetFinalizer(&x, adjustFunc)
+}
 
 func Compiling(pkgs []string) bool {
 	if Ctxt.Pkgpath != "" {
