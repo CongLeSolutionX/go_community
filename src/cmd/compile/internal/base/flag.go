085c432a006e88aa0f15a611ae3ec042b6583ea0
cui fliter <imcusg@gmail.com>
2022-09-28 16:31:10 +0000

    cmd/compile/internal/base: use strings.Cut
    Change-Id: If8fc77d5b04b2979707032d91112d4f33ef5e3da
    GitHub-Last-Rev: 168a5ee98bca3b8d73796aa3084348975fc01496
    GitHub-Pull-Request: golang/go#55913

--- src/cmd/compile/internal/base/flag.go
+++ src/cmd/compile/internal/base/flag.go
@@ -470,15 +470,13 @@ func readImportCfg(file string) {
 			continue
 		}
 
-		var verb, args string
-		if i := strings.Index(line, " "); i < 0 {
-			verb = line
-		} else {
-			verb, args = line[:i], strings.TrimSpace(line[i+1:])
+		verb, args, found := strings.Cut(line, " ")
+		if found {
+			args = strings.TrimSpace(args)
 		}
-		var before, after string
-		if i := strings.Index(args, "="); i >= 0 {
-			before, after = args[:i], args[i+1:]
+		before, after, found := strings.Cut(args, "=")
+		if !found {
+			before = ""
 		}
 		switch verb {
 		default:
