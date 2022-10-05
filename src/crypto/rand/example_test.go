7806022f1b7dedf9405c974fc475eaf5af0f98cf
Alan Yee <alanyee@users.noreply.github.com>
2022-10-05 07:02:54 +0000

    crypto/rand: add example for Int
    Add example for usage of Int in crypto/rand
    
    Change-Id: I1c80a080d2c80fe050c302a58b8d875479c8fe70
    GitHub-Last-Rev: 1e9b21207410e4c1150c05774d6e1a4998ec353b
    GitHub-Pull-Request: golang/go#56017

--- src/crypto/rand/example_test.go
+++ src/crypto/rand/example_test.go
@@ -8,7 +8,23 @@ import (
 	"bytes"
 	"crypto/rand"
 	"fmt"
+	"math/big"
 )
+
+
+// This example returns a different number each run.
+func ExampleInt() {
+	a, err := rand.Int(rand.Reader, big.NewInt(100))
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	// The output is a signed multi-precision integer.
+	fmt.Println(a.Int64())
+
+	// Output:
+	//
+}
 
 // This example reads 10 cryptographically secure pseudorandom numbers from
 // rand.Reader and writes them to a byte slice.
