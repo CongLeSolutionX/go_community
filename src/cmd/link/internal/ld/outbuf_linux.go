ad5e790934c113d115ef3bf6be38104a9203cb6e
Guoqi Chen <chenguoqi@loongson.cn>
2022-10-27 17:47:35 +0800

    cmd/link: allocate new space at the end of the file, and adjust allocation size on Linux
    Workaround bug of kernel tmpfs: rare corruption of linker generated ELF binaries.
    See discussion at issues #53804 and #53116. This bug was introduced in Linux kernel
    5.16-rc4 and has been fixed in the latest 6.1 kernel.
    
    On linux, we preallocate storage space from the end of the file by specifying it
    explicitly, so the size we give it should be the increment, instead of the total
    size.
    
    [1]: https://lore.kernel.org/linux-mm/33b85d82.7764.1842e9ab207.Coremail.chenguoqic@163.com/
    [2]: https://lore.kernel.org/linux-mm/20221101032248.819360-1-kernel@hev.cc/T/#u
    
    Fixes #53116.
    Related #53804.
    Updates #37310.
    
    Change-Id: If41b357f10b7505be91ca77fc5fa83c2ca20505a

--- src/cmd/link/internal/ld/outbuf_linux.go
+++ src/cmd/link/internal/ld/outbuf_linux.go
@@ -7,5 +7,23 @@ package ld
 import "syscall"
 
 func (out *OutBuf) fallocate(size uint64) error {
-	return syscall.Fallocate(int(out.f.Fd()), 0, 0, int64(size))
+	// 1.Original implementation:
+	//   return syscall.Fallocate(int(out.f.Fd()), 0, 0, int64(size))
+	//
+	// 2.The current implementation is to fix issues #53116 and #53804, if your
+	//   kernel version is not between 5.16-rc4 and 6.1, you can use the original
+	//   simpler version of the implementation.
+	var st syscall.Stat_t
+
+	err := syscall.Fstat(int(out.f.Fd()), &st)
+	if err != nil {
+		return err
+	}
+
+	cursize := uint64(st.Blocks) * 512
+	if size <= cursize {
+		return nil
+	}
+
+	return syscall.Fallocate(int(out.f.Fd()), 0, int64(cursize), int64(size-cursize))
 }
