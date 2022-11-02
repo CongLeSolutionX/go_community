976c806195161ef260853d44cfc0988334a1ffd6
David Chase <drchase@google.com>
2022-11-02 17:23:47 -0400

    cmd/compile: add testing-flag guard to package-is-collected assert
    On advice of the department of garbage collection, forcing
    a garbage collection generally does not improve performance.
    However, this is a good property to be able to test.
    
    Will benchmark this to ensure that this is the case.
    
    Change-Id: I4c4816d619735838a32388acf0cc5eb1cd5f0db5

--- src/cmd/compile/internal/base/debug.go
+++ src/cmd/compile/internal/base/debug.go
@@ -26,6 +26,7 @@ type DebugFlags struct {
 	DwarfInl                   int    `help:"print information about DWARF inlined function creation"`
 	Export                     int    `help:"print export data"`
 	GCAdjust                   int    `help:"log adjustments to GOGC" concurrent:"ok"`
+	GCCheck                    int    `help:"check heap/gc use by compiler" concurrent:"ok"`
 	GCProg                     int    `help:"print dump of GC programs"`
 	Gossahash                  string `help:"hash value for use in debugging the compiler"`
 	InlFuncsWithClosures       int    `help:"allow functions with closures to be inlined"`
