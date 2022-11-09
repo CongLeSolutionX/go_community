08dbd1bc0486515b23270f6e3cf92d3e851ddf32
Roland Shoemaker <roland@golang.org>
2022-11-09 14:04:10 -0800

    crypto/x509: implement SetFallbackRoots
    Adds a method which allows users to set a fallback certificate pool for
    usage during verification if the system certificate pool is empty.
    
    Fixes #43958
    
    Change-Id: I279dd2f753743bce19790f2ae29f063c89c9359d

--- src/crypto/x509/root.go
+++ src/crypto/x509/root.go
@@ -11,22 +11,71 @@ package x509
 //
 //go:generate go run root_ios_gen.go -version 55188.120.1.0.1
 
-import "sync"
+import (
+	"internal/godebug"
+	"sync"
+)
 
 var (
 	once           sync.Once
+	systemRootsMu  sync.RWMutex
 	systemRoots    *CertPool
 	systemRootsErr error
 )
 
 func systemRootsPool() *CertPool {
 	once.Do(initSystemRoots)
+	systemRootsMu.RLock()
+	defer systemRootsMu.RUnlock()
 	return systemRoots
 }
 
 func initSystemRoots() {
+	systemRootsMu.Lock()
+	defer systemRootsMu.Unlock()
 	systemRoots, systemRootsErr = loadSystemRoots()
 	if systemRootsErr != nil {
 		systemRoots = nil
 	}
+}
+
+var (
+	fallbackMu   sync.Mutex
+	fallbacksSet bool
+)
+
+// SetFallbackRoots sets the roots to use during certificate verification, if no
+// custom roots are specified and a platform verifier or a system certificate
+// pool is not available (for instance in a container which does not have a root
+// certificate bundle).
+//
+// SetFallbackRoots may only be called once, if called multiple times it will
+// panic.
+//
+// The fallback behavior can be forced on all platforms, even when there is a
+// system certificate pool, by setting GODEBUG=x509usefallbackroots=1 (note that
+// on Windows and macOS this will disable usage of the platform verification
+// APIs and cause the pure Go verifier to be used.) Setting
+// x509usefallbackroots=1 without calling SetFallbackRoots has no effect.
+func SetFallbackRoots(roots *CertPool) {
+	fallbackMu.Lock()
+	defer fallbackMu.Unlock()
+
+	if fallbacksSet {
+		panic("SetFallbackRoots has already been called")
+	}
+	fallbacksSet = true
+
+	// populate the systemRoots if systemRootsPool has not been called already
+	_ = systemRootsPool()
+
+	forceFallback := godebug.Get("x509usefallbackroots") == "1"
+
+	systemRootsMu.Lock()
+	defer systemRootsMu.Unlock()
+
+	if systemRoots != nil && ((systemRoots.len() > 0 || systemRoots.systemPool) && !forceFallback) {
+		return
+	}
+	systemRoots, systemRootsErr = roots, nil
 }
