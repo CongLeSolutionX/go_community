9ab6277811560f5190cc210612511439ca702c22
Roland Shoemaker <roland@golang.org>
2022-11-09 14:04:10 -0800

    crypto/x509: implement SetFallbackRoots
    Adds a method which allows users to set a fallback certificate pool for
    usage during verification if the system certificate pool is empty.
    
    Fixes #43958
    
    Change-Id: I279dd2f753743bce19790f2ae29f063c89c9359d

--- src/crypto/x509/root.go
+++ src/crypto/x509/root.go
@@ -11,7 +11,10 @@ package x509
 //
 //go:generate go run root_ios_gen.go -version 55188.120.1.0.1
 
-import "sync"
+import (
+	"internal/godebug"
+	"sync"
+)
 
 var (
 	once           sync.Once
@@ -29,4 +32,41 @@ func initSystemRoots() {
 	if systemRootsErr != nil {
 		systemRoots = nil
 	}
+}
+
+var (
+	fallbackMu   sync.Mutex
+	fallbacksSet bool
+)
+
+// SetFallbackRoots sets the roots to use during certificate verification, if no
+// custom roots are specified and a platform verifier or a system certificate
+// pool is not available (for instance in a container which does not have a root
+// certificate bundle).
+//
+// SetFallbackRoots may only be called once, if called multiple times, it will
+// panic.
+//
+// The fallback behavior can be forced, even when there is a system certificate
+// pool, by setting GODEBUG=forcerootfallback=1. Note this behavior cannot be
+// forced on platforms where the platform certificate verifier is used (Windows
+// and macOS) as we have no insight into the contents of the platforms root pool.
+func SetFallbackRoots(roots *CertPool) {
+	fallbackMu.Lock()
+	defer fallbackMu.Unlock()
+
+	if fallbacksSet {
+		panic("SetFallbackRoots has already been called")
+	}
+	fallbacksSet = true
+
+	_ = systemRootsPool()
+
+	forceFallback := godebug.Get("forcerootfallback") == "1"
+
+	if systemRoots != nil && ((systemRoots.len() > 0 && !forceFallback) || systemRoots.systemPool) {
+		return
+	}
+
+	systemRoots, systemRootsErr = roots, nil
 }
