679689fa4a91d411c7cc06fea60ddb5e93f6b0e8
Guoqi Chen <chenguoqi@loongson.cn>
2022-12-15 13:57:52 +0800

    cmd/compile: sign extension the 2nd argument of the AtomicCas32 op on loong64
    The function AtomicCas32 is implemented using the LL-SC instruction pair on
    loong64. However, the LL instruction on loong64 is sign-extended, so it is
    necessary to sign-extend the second parameter "old" of the AtomicCas32, so
    that the instruction BNE after LL can get the desired result.
    
    The function prototype of AtomicCas32 in golang:
        func AtomicCas32(ptr *uint32, old, new uint32) bool
    
    When using an intrinsify implementation:
        case 1: (*ptr) <= 0x80000000 && old < 0x80000000
            E.g: (*ptr) = 0x7FFFFFFF, old = Rarg1= 0x7FFFFFFF
    
            After run the instruction "LL (Rarg0), Rtmp": Rtmp = 0x7FFFFFFF
            Rtmp ! = Rarg1(old) is false, the result we expect
    
        case 2: (*ptr) >= 0x80000000 && old >= 0x80000000
            E.g: (*ptr) = 0x80000000, old = Rarg1= 0x80000000
    
            After run the instruction "LL (Rarg0), Rtmp": Rtmp = 0xFFFFFFFF_80000000
            Rtmp ! = Rarg1(old) is true, which we do not expect
    
    When using an non-intrinsify implementation:
        Because Rarg1 is loaded from the stack using sign-extended instructions
        ld.w, the situation described in Case 2 above does not occur
    
    Benchmarks on linux/loong64:
    name     old time/op  new time/op  delta
    Cas      50.0ns ± 0%  50.1ns ± 0%   ~     (p=1.000 n=1+1)
    Cas64    50.0ns ± 0%  50.1ns ± 0%   ~     (p=1.000 n=1+1)
    Cas-4    56.0ns ± 0%  56.0ns ± 0%   ~     (p=1.000 n=1+1)
    Cas64-4  56.0ns ± 0%  56.0ns ± 0%   ~     (p=1.000 n=1+1)
    
    Fixes #57282
    
    Change-Id: I190a7fc648023b15fa392f7fdda5ac18c1561bac

---
+++ test/fixedbugs/issue57282.go
@@ -0,0 +1,50 @@
+// run
+
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build loong64
+
+package main
+
+import (
+	"fmt"
+	. "sync/atomic"
+)
+
+const (
+	_magic = 0xfbcdefaf
+)
+
+func main() {
+	var x struct {
+		before uint32
+		i      uint32
+		after  uint32
+
+		o uint32
+		n uint32
+	}
+
+	x.before = _magic
+	x.after = _magic
+
+	for t := uint32(0x7FFFFFF0); t < 0x80000003; t += 1 {
+		x.i = t + 0
+		x.o = t + 0
+		x.n = t + 1
+
+		if !CompareAndSwapUint32(&x.i, x.o, x.n) {
+			panic(fmt.Sprintf("should have swapped %#x %#x", x.o, x.n))
+		}
+
+		if x.i != x.n {
+			panic(fmt.Sprintf("wrong x.i after swap: x.i=%#x x.n=%#x", x.i, x.n))
+		}
+
+		if x.before != _magic || x.after != _magic {
+			panic(fmt.Sprintf("wrong magic: %#x _ %#x != %#x _ %#x", x.before, x.after, _magic, _magic))
+		}
+	}
+}
