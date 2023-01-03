cc029edc9711aa6bb0cb2971da452e65a4c1164b
Michael Anthony Knyszek <mknyszek@google.com>
2023-01-03 17:59:48 +0000

    runtime: disable huge pages for GC metadata for small heaps
    For #55328.
    
    Change-Id: I8792161f09906c08d506cc0ace9d07e76ec6baa6

--- src/runtime/mgc.go
+++ src/runtime/mgc.go
@@ -1186,6 +1186,11 @@ func gcMarkTermination() {
 		lc.mspan.setUserArenaChunkToFault()
 	}
 
+	// Enable huge pages on some metadata if we cross a heap threshold.
+	if gcController.heapGoal() > minHeapForMetadataHugePages {
+		mheap_.enableMetadataHugePages()
+	}
+
 	semrelease(&worldsema)
 	semrelease(&gcsema)
 	// Careful: another GC cycle may start now.
