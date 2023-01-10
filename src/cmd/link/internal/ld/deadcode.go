313c702a5bf408bfd0e4ce07349e6be790e5d871
Than McIntosh <thanm@google.com>
2023-01-10 10:21:16 -0500

    cmd/compile,cmd/link: enable deadcode of unreferenced large global maps
    DO NOT SUBMIT
    DO NOT REVIEW
    
    This patch hacks the compiler's static initialization machinery to
    pick out assignments to global maps (e.g.
    
       var mymap = map[string]int{"foo":1, "bar":2}
    
    and extract the initialization code for the map into a separate init
    function (which is then called from the main init function via a weak
    relocation):
    
       var mymap = map[string]int{"foo":1, "bar":2} // KEEP reloc -> map.init.0
    
       func init() {
          map.init.0() // weak relocation
       }
    
       func map.init.0() {
         mymap = map[string]int{"foo":1, "bar":2}
       }
    
    The map init outlining is done selectively (only in the case where the
    RHS code exceeds a size limit of 20 nodes). [TODO: spend some time
    determining whether 20 is the right number here.]
    
    In order to ensure that a given map.init.NNN function is included when
    its corresponding map is live, we add dummy R_KEEP relocation from the
    map variable to the map init function.
    
    In the linker, once deadcode is complete, we look for calls from an
    init function to a map.init.NNN function; if the latter function is
    not marked reachable then we rewrite the relocation in question so
    that it targets a "noop" routine in the runtime. This seems more
    palatable than trying to have the linker modify the init routine text
    in some way to erase the call.
    
    On the "pro" side:
    - allows large dead maps to be deadcoded, which reduces startup time
      and live heap
    - can help with compile time by splitting up giant pkg init funcs into pieces
    
    On the "con" side:
    - we have more functions to compile
    - cost of map init is modestly higher for live maps due to the need to
      call out to the outlined init routine
    
    Change-Id: I1fd6fd6397772be1ebd3eb397caf68ae9a3147e9

--- src/cmd/link/internal/ld/deadcode.go
+++ src/cmd/link/internal/ld/deadcode.go
@@ -12,6 +12,7 @@ import (
 	"cmd/link/internal/sym"
 	"fmt"
 	"internal/buildcfg"
+	"os"
 	"regexp"
 	"sort"
 	"strings"
@@ -144,6 +145,8 @@ func (d *deadcodePass) flood() {
 			}
 			t := r.Type()
 			switch t {
+			case objabi.R_KEEP:
+				d.mark(r.Sym(), symIdx)
 			case objabi.R_METHODOFF:
 				if i+2 >= relocs.Count() {
 					panic("expect three consecutive R_METHODOFF relocs")
@@ -269,6 +272,40 @@ func (d *deadcodePass) flood() {
 	}
 }
 
+// mapcleanup walks all init functions and looks for weak relocations
+// to mapinit symbols that are no longer reachable. It rewrites
+// the relocs to target a new no-op routine in the runtime.
+func (d *deadcodePass) mapcleanup() {
+	noop := d.ldr.Lookup("runtime.mapinitnoop", abiInternalVer)
+	if noop == 0 {
+		panic("could not look up runtime.mapinitnoop")
+	}
+	// TODO: find some more efficient way of doing this.
+	for idx := loader.Sym(1); idx < loader.Sym(d.ldr.NDef()); idx++ {
+		sname := d.ldr.SymName(idx)
+		if !strings.HasSuffix(sname, ".init") {
+			continue
+		}
+		relocs := d.ldr.Relocs(idx)
+		var su *loader.SymbolBuilder
+		for i := 0; i < relocs.Count(); i++ {
+			r := relocs.At(i)
+			if r.Weak() && !d.ldr.AttrReachable(r.Sym()) {
+				// rewrite
+				d.ldr.SetAttrReachable(noop, true)
+				if *flagPruneWeakMapDbg {
+					fmt.Fprintf(os.Stderr, "=-= in %s rewrite %s ref to noop\n",
+						sname, d.ldr.SymName(r.Sym()))
+				}
+				if su == nil {
+					su = d.ldr.MakeSymbolUpdater(idx)
+				}
+				su.SetRelocSym(i, noop)
+			}
+		}
+	}
+}
+
 var initfre = regexp.MustCompile(`^\S+\.init$|^\S+\.init\.\d+$`)
 
 func (d *deadcodePass) postProcess() {
@@ -329,7 +366,6 @@ func (d *deadcodePass) postProcess() {
 		if pi != pj {
 			return pi < pj
 		}
-		return d.ldr.SymName(sl[i]) < d.ldr.SymName(sl[j])
 		return d.ldr.SymName(sl[i]) < d.ldr.SymName(sl[j])
 	})
 	fmt.Printf("Referenced from init only:\n")
@@ -467,6 +503,9 @@ func deadcode(ctxt *Link) {
 			break
 		}
 		d.flood()
+	}
+	if *flagPruneWeakMap {
+		d.mapcleanup()
 	}
 	if *flagShowIdead {
 		d.postProcess()
