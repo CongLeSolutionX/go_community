17bbf3ed3d88f6f9eb43d6c0c4468101312d43da
David Chase <drchase@google.com>
2023-01-13 16:12:47 -0500

    internal/abi,runtime: refactor map constants into one place
    DO NOT SUBMIT
    
    This version is for testing, the constant is changed (4 instead of 3).
    The argument for doing the change in the CL itself, this  traces out
    all the places where magic numbers were copied/derived into the source
    tree.
    
    Change-Id: I9f6669d1eadd02f182d0bc3f959dc5f385fa1683

---
+++ src/internal/abi/map.go
@@ -0,0 +1,13 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package abi
+
+// Map constants common to several packages
+const (
+	MapBucketCountBits = 4 // log2 of number of elements in a bucket.
+	MapBucketCount     = 1 << MapBucketCountBits
+	MapMaxKeyBytes     = 128 // Must fit in a uint8.
+	MapMaxElemBytes    = 128 // Must fit in a uint8.
+)
