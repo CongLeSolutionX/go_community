e07c8beed20c3310163201cbdab8aa85a13835db
qmuntal <quimmuntal@gmail.com>
2023-01-16 16:21:48 +0100

    cmd/internal/obj: generate SEH aux symbols for windows/amd64
    This CL updates the Go compiler so it generate SEH unwind info [1] as a
    function auxiliary symbol when building for windows/amd64.
    
    A follow up CL will teach the Go linker how to assemble these codes
    into the PE .xdata section.
    
    Updates #57302
    
    [1] https://learn.microsoft.com/en-us/cpp/build/exception-handling-x64#struct-unwind_info
    
    Change-Id: I40ae0437bfee326c1a67c2b5e1496f0bf3ecea17

---
+++ src/cmd/internal/obj/x86/seh.go
@@ -0,0 +1,174 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package x86
+
+import (
+	"cmd/internal/obj"
+	"cmd/internal/objabi"
+	"encoding/base64"
+	"fmt"
+)
+
+const (
+	sehOpSaveReg = iota
+	sehOpSetFP
+)
+
+type sehbuf struct {
+	ctxt *obj.Link
+	data []byte
+	off  int
+}
+
+func newsehbuf(ctxt *obj.Link, nodes uint8) sehbuf {
+	// - 8 bytes for the header
+	// - 2 bytes for each node
+	// - 2 bytes in case nodes is not even
+	size := 8 + nodes*2
+	if nodes%2 != 0 {
+		size += 2
+	}
+	return sehbuf{ctxt, make([]byte, size), 0}
+}
+
+func (b *sehbuf) write8(v uint8) {
+	b.data[b.off] = v
+	b.off++
+}
+
+func (b *sehbuf) write16(v uint16) {
+	b.ctxt.Arch.ByteOrder.PutUint16(b.data[b.off:], v)
+	b.off += 2
+}
+
+func (b *sehbuf) write32(v uint32) {
+	b.ctxt.Arch.ByteOrder.PutUint32(b.data[b.off:], v)
+	b.off += 4
+}
+
+func (b *sehbuf) writecode(op, value uint8) {
+	b.write8(value<<4 | op)
+}
+
+// https://learn.microsoft.com/en-us/cpp/build/exception-handling-x64#operation-info
+var sehRegisters = map[int16]uint8{
+	REG_AX:  0,
+	REG_CX:  1,
+	REG_DX:  2,
+	REG_BX:  3,
+	REG_SP:  4,
+	REG_BP:  5,
+	REG_SI:  6,
+	REG_DI:  7,
+	REG_R8:  8,
+	REG_R9:  9,
+	REG_R10: 10,
+	REG_R11: 11,
+	REG_R12: 12,
+	REG_R13: 13,
+	REG_R14: 14,
+	REG_R15: 15,
+}
+
+// populateSeh encodes the SEH unwind information for the symbol s.
+func populateSeh(ctxt *obj.Link, s *obj.LSym) {
+	fn := s.Func()
+	ops := fn.SehUnwindOps
+	if len(ops) == 0 {
+		return
+	}
+	// We don't need to support all unwind operation,
+	// only the necessary to save and set the frame pointer.
+	// Stack allocation operations are not supported on purpose,
+	// they take space and setting the frame pointer is enough.
+	const (
+		// https://learn.microsoft.com/en-us/cpp/build/exception-handling-x64#unwind-operation-code
+		UWOP_PUSH_NONVOL = 0
+		UWOP_SET_FPREG   = 3
+	)
+	var (
+		fpoffset uint8
+		fpreg    int16
+		hasfp    bool
+	)
+	// Traverse ops looking for a sehOpSetFP
+	// and validate the operations.
+	for _, c := range ops {
+		p := c.Prog
+		switch c.Operation {
+		case sehOpSaveReg:
+			if p.As != APUSHQ {
+				ctxt.Diag("unsupported registry save instruction\n%v", p)
+			}
+		case sehOpSetFP:
+			fpoffset = uint8(p.From.Offset)
+			if fpoffset >= 240 || fpoffset%16 != 0 {
+				ctxt.Diag("unsupported frame pointer offset %d", fpoffset)
+			}
+			fpreg = p.To.Reg
+			hasfp = true
+		default:
+			ctxt.Diag("unsupported SEH operation %d", c.Operation)
+		}
+	}
+	if ctxt.Errors > 0 {
+		return
+	}
+	// Fow now we only support operations which are encoded
+	// using a single 2-byte node, so the number of nodes
+	// is the number of operations.
+	nodes := uint8(len(ops))
+	buf := newsehbuf(ctxt, nodes)
+	buf.write8(1)                                   // Flags + version
+	buf.write8(uint8(ops[len(ops)-1].Prog.Link.Pc)) // Size of prolog
+	buf.write8(nodes)                               // Count of nodes
+	if hasfp {
+		buf.write8((fpoffset/16)<<4 | sehRegisters[fpreg])
+	} else {
+		buf.write8(0)
+	}
+	// Unwind operations are expected to appear in reverse order
+	// of appearance, so traverse ops in reverse order.
+	// The encoding of each operation is documented here:
+	// https://learn.microsoft.com/en-us/cpp/build/exception-handling-x64#struct-unwind_code
+	for i := len(ops) - 1; i >= 0; i-- {
+		op := ops[i].Operation
+		p := ops[i].Prog
+		// Offset in prolog of the end of the instruction
+		// that performs this operation, plus 1.
+		// This is equivalent to the offset of the next
+		// instruction.
+		buf.write8(uint8(p.Link.Pc))
+		switch op {
+		case sehOpSaveReg:
+			buf.writecode(UWOP_PUSH_NONVOL, sehRegisters[p.From.Reg])
+		case sehOpSetFP:
+			buf.writecode(UWOP_SET_FPREG, 0)
+		}
+	}
+	if nodes%2 != 0 {
+		// For alignment purposes, this array always has an even number of entries.
+		buf.write16(0)
+	}
+	// The following 4 bytes reference the RVA of the exception handler,
+	// in case the function has one. We don't use it for now.
+	buf.write32(0)
+
+	// The list of unwind infos in a PE binary have very low cardinality
+	// as each info only contains frame pointer operations,
+	// which are very similar across functions.
+	// Dedup them when possible.
+	hash := base64.StdEncoding.EncodeToString(buf.data)
+	symname := fmt.Sprintf("%d.%s", len(buf.data), hash)
+	fn.SehUnwindInfoSym = ctxt.LookupInit("go:sehuw."+symname, func(s *obj.LSym) {
+		s.WriteBytes(ctxt, 0, buf.data)
+		s.Type = objabi.SSEHUNWINDINFO
+		s.Set(obj.AttrDuplicateOK, true)
+		s.Set(obj.AttrLocal, true)
+		// Note: AttrContentAddressable cannot be set here,
+		// because the content-addressable-handling code
+		// does not know about aux symbols.
+	})
+}
