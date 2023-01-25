44255dad2c3e4da77a24610c9f6f413b256eb968
cuiweixie <cuiweixie@gmail.com>
2023-01-25 11:22:29 +0800

    encoding/binary: add var NativeEndian
    Updates #57237
    
    Change-Id: I149c8b7eeac91b87b5810250f96d48ca87135807

---
+++ src/encoding/binary/native_endian_little_test.go
@@ -0,0 +1,15 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+//go:build 386 || amd64 || amd64p32 || alpha || arm || arm64 || loong64 || mipsle || mips64le || mips64p32le || nios2 || ppc64le || riscv || riscv64 || sh
+
+package binary
+
+import "testing"
+
+func TestNativeEndian(t *testing.T) {
+	if NativeEndian.littleEndian != LittleEndian {
+		t.Errorf("native endian should be little endian")
+	}
+}
