de1530d3f319f1e5a731f00b4bdb4757460ffaeb
Felix Geisendörfer <felix.geisendoerfer@datadoghq.com>
2023-01-27 10:20:47 +0100

    runtime: frame pointer unwinding for tracer
    Change-Id: I2928a2fcd8779a31c45ce0f2fbcc0179641190bb

--- src/runtime/proc.go
+++ src/runtime/proc.go
@@ -313,6 +313,7 @@ func forcegchelper() {
 }
 
 //go:nosplit
+//go:noinline
 
 // Gosched yields the processor, allowing other goroutines to run. It does not
 // suspend the current goroutine, so execution resumes automatically.
