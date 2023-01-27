34d9f943fe2201aef36aa41594c352b80da48359
Felix Geisendörfer <felix.geisendoerfer@datadoghq.com>
2023-01-27 10:20:47 +0100

    runtime: frame pointer unwinding for tracer
    Change-Id: I2928a2fcd8779a31c45ce0f2fbcc0179641190bb

--- src/runtime/stubs_amd64.go
+++ src/runtime/stubs_amd64.go
@@ -47,3 +47,6 @@ func asmcgocall_no_g(fn, arg unsafe.Pointer)
 // respectively. Does not follow the Go ABI.
 func spillArgs()
 func unspillArgs()
+
+// getcallerfp returns the address of the frame pointer in the callers frame or 0 if not implemented.
+func getcallerfp() uintptr
