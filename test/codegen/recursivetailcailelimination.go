27bef2ca1a20579eb9f0a67fa2e89f268c274493
Jorropo <jorropo.pgm@gmail.com>
2023-02-11 03:32:35 +0100

    cmd/compile: add new SSA pass which rewrites recursive tail calls into loops
    Unlike previous patches and discussions, this patch dodges the
    runtime.{Stack,Caller{,s}} arguments by conservatively applying the
    optimization in a way that any attempts at observing a change in behaviour
    (such as by noticing missing frames in runtime.Callers) will turn off the
    optimization in this function.
    
    This is hit 49 times when building toolchain3, tools and std.
    
    On each occurance this saves a call and may unlock further optimisations.
    
    The build time cost should be fairly minimal as long as the optimization is
    not triggered because it is designed to fail fast.
    
    Change-Id: I0087ec42ee3f8c79febb0f23bfe9f4193a9ef0c8

---
+++ test/codegen/recursivetailcailelimination.go
@@ -0,0 +1,35 @@
+// asmcheck
+
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package codegen
+
+import "runtime"
+
+func recursiveTailCallElimination(x, y, z uint) uint {
+	if x != 0 {
+		// amd64:-"CALL"
+		// arm64:-"CALL"
+		return recursiveTailCallElimination(y, x, z)
+	}
+	return 42
+}
+
+func recursiveTailCallEliminationDoNotRunIfObservable(x, y, z uint) uint {
+	if x != 0 {
+		// amd64:"CALL"
+		// arm64:"CALL"
+		return recursiveTailCallEliminationDoNotRunIfObservable(y, x, z)
+	}
+	return observeStackTrace()
+}
+
+func observeStackTrace() uint {
+	var b [1024]byte
+	n := runtime.Stack(b[:], false)
+	println(string(b[:n]))
+
+	return 42
+}
