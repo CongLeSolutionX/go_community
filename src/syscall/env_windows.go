527b84249cf9a250a40313aa2607144907869c0f
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2023-02-25 16:32:15 +0700

    syscall: fix invalid unsafe.Pointer conversion on Windows
    By copying the same approach in internal/syscall/execenv.Default
    function.
    
    Fixes #58714
    
    Change-Id: Ifa5c059ed5e358ed98aee7e83b95dd1806b535f7

--- src/syscall/env_windows.go
+++ src/syscall/env_windows.go
@@ -74,21 +74,30 @@ func Clearenv() {
 }
 
 func Environ() []string {
-	s, e := GetEnvironmentStrings()
+	envs, e := GetEnvironmentStrings()
 	if e != nil {
 		return nil
 	}
-	defer FreeEnvironmentStrings(s)
+	defer FreeEnvironmentStrings(envs)
 	r := make([]string, 0, 50) // Empty with room to grow.
-	for from, i, p := 0, 0, (*[1 << 24]uint16)(unsafe.Pointer(s)); true; i++ {
-		if p[i] == 0 {
-			// empty string marks the end
-			if i <= from {
-				break
-			}
-			r = append(r, string(utf16.Decode(p[from:i])))
-			from = i + 1
+	envp := uintptr(unsafe.Pointer(envs))
+	for {
+
+		// find NUL terminator
+		end := unsafe.Pointer(envp)
+		for *(*uint16)(end) != 0 {
+			end = unsafe.Pointer(uintptr(end) + unsafe.Sizeof(*envs))
 		}
+
+		n := (uintptr(end) - uintptr(unsafe.Pointer(envp))) / 2
+		if n == 0 {
+			// environment block ends with empty string
+			break
+		}
+
+		entry := (*[(1 << 30) - 1]uint16)(unsafe.Pointer(envp))[:n:n]
+		r = append(r, string(utf16.Decode(entry)))
+		envp += unsafe.Sizeof(*envs) * (uintptr(len(entry)) + 1)
 	}
 	return r
 }
