015a32b2a254729ef227a2153d411f1ff3f4b9b8
Tobias Klauser <tklauser@distanz.ch>
2023-03-16 22:37:20 +0100

    cmd/link/internal/ld, internal/syscall/unix: use posix_fallocate on freebsd
    The posix_fallocate system call is available since FreeBSD 9.0, see
    https://man.freebsd.org/cgi/man.cgi?query=posix_fallocate
    
    Change-Id: Ie65e0a44341909707617d3b0d9a4f1710c45b935

--- src/cmd/link/internal/ld/outbuf_freebsd.go
+++ src/cmd/link/internal/ld/outbuf_freebsd.go
@@ -1,11 +1,11 @@
-// Copyright 2020 The Go Authors. All rights reserved.
+// Copyright 2023 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build !darwin && !(freebsd && go1.21) && !linux
+//go:build freebsd && go1.21
 
 package ld
 
 func (out *OutBuf) fallocate(size uint64) error {
-	return errNoFallocate
+	return unix.PosixFallocate(int(out.f.Fd()), 0, int64(size))
 }
