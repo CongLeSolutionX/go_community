d64a59c58f37234b259e0591b5c5fa7a89dec420
Michael Anthony Knyszek <mknyszek@google.com>
2023-03-27 15:47:18 +0000

    runtime: fix debug non-concurrent sweep mode after activeSweep changes
    Currently the GC creates a sweepLocker before restarting the world at
    the end of the mark phase, so that it can safely flush mcaches without
    the runtime incorrectly concluding that sweeping is done before that
    happens.
    
    However, with GODEBUG=gcstoptheworld=2, where sweeping happens during
    that STW phase, creating that sweepLocker will fail, since the runtime
    will conclude that sweeping is in fact complete (all the queues will be
    drained). The problem however is that gcSweep, which does the
    non-concurrent sweeping, doesn't actually flush mcaches.
    
    In essence, this failure to create a sweepLocker is indicating a real
    issue: sweeping is marked as complete, but we haven't flush the mcaches
    yet!
    
    The fix to this is to flush mcaches in gcSweep when in a non-concurrent
    sweep. Now that gcSweep actually completes a full sweep, it's safe to
    ignore a failure to create a sweepLocker (and in fact, it *must* fail).
    
    While we're here, let's also remove _ConcurrentSweep, the debug flag.
    There's already an alias for it called concurrentSweep, and there's only
    one use of it in gcSweep.
    
    Lastly, add a dist test for the GODEBUG=gcstoptheworld=2 mode.
    
    Fixes #53885.
    
    Change-Id: I8a1e5b8f362ed8abd03f76e4950d3211f145ab1f

--- src/cmd/dist/test.go
+++ src/cmd/dist/test.go
@@ -689,8 +689,21 @@ func (t *tester) registerTests() {
 			})
 	}
 
-	// morestack tests. We only run these on in long-test mode
-	// (with GO_TEST_SHORT=false) because the runtime test is
+	// GODEBUG=gcstoptheworld=2 tests. We only run these in long-test
+	// mode (with GO_TEST_SHORT=0) because this is just testing a
+	// non-critical debug setting.
+	if !t.compileOnly && !t.short {
+		t.registerTest("runtime:gcstoptheworld2", "GODEBUG=gcstoptheworld=2 archive/zip",
+			&goTest{
+				timeout: 300 * time.Second,
+				short:   true,
+				env:     []string{"GODEBUG=gcstoptheworld=2"},
+				pkg:     "archive/zip",
+			})
+	}
+
+	// morestack tests. We only run these in long-test mode
+	// (with GO_TEST_SHORT=0) because the runtime test is
 	// already quite long and mayMoreStackMove makes it about
 	// twice as slow.
 	if !t.compileOnly && !t.short {
