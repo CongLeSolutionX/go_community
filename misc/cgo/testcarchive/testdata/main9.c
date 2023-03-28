1cdd631607faea92d2a645a85b2ec6eeba038ba7
Cherry Mui <cherryyz@google.com>
2023-03-28 14:48:59 -0400

    runtime: get a better g0 stack bound in needm
    Currently, when C calls into Go the first time, we grab an M
    using needm, which sets m.g0's stack bounds using the SP. We don't
    know how big the stack is, so we simply assume 32K. Previously,
    when the Go function returns to C, we drop the M, and the next
    time C calls into Go, we put a new stack bound on the g0 based on
    the current SP. After CL 392854, we don't drop the M, and the next
    time C calls into Go, we reuse the same g0, without recomputing
    the stack bounds. If the C code uses quite a bit of stack space
    before calling into Go, the SP may be well below the 32K stack
    bound we assumed, so the runtime thinks the g0 stack overflows.
    
    This CL makes needm to get a more accurate stack bound from
    pthread. This is still a guess as we don't know exactly where we
    are in the C stack. But it is probably better than simply assuming
    32K.
    
    For #59294.
    
    Change-Id: Ie52a8f931e0648d8753e4c1dbe45468b8748b527

---
+++ misc/cgo/testcarchive/testdata/main9.c
@@ -0,0 +1,17 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "libgo9.h"
+
+void callGoFWithDeepStack(int n) {
+	if (n > 0) {
+		callGoFWithDeepStack(n - 1);
+	}
+	GoF();
+}
+
+int main() {
+	GoF();                       // call GoF without using much stack
+	callGoFWithDeepStack(10000); // call GoF with a deep stack
+}
