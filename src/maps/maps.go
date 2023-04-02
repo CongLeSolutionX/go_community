959c1b670deae4283c93af193239ff1c8518578e
cuiweixie <cuiweixie@gmail.com>
2023-04-02 15:49:58 +0800

    maps,runtime: improve maps.Keys
    name     old time/op    new time/op    delta
    Keys-10    9.16ms ± 1%    6.52ms ± 1%  -28.84%  (p=0.000 n=10+10)
    
    name     old alloc/op   new alloc/op   delta
    Keys-10    8.00MB ± 0%    8.00MB ± 0%   -0.00%  (p=0.000 n=10+10)
    
    name     old allocs/op  new allocs/op  delta
    Keys-10      1.00 ± 0%      1.00 ± 0%     ~     (all equal)
    
    Change-Id: Ia7061c37be89e906e79bc3ef3bb1ef0d7913f9f6

--- src/maps/maps.go
+++ src/maps/maps.go
@@ -5,13 +5,15 @@
 // Package maps defines various functions useful with maps of any type.
 package maps
 
+import "unsafe"
+
+func keys(m any, p unsafe.Pointer)
+
 // Keys returns the keys of the map m.
 // The keys will be in an indeterminate order.
 func Keys[M ~map[K]V, K comparable, V any](m M) []K {
-	r := make([]K, 0, len(m))
-	for k := range m {
-		r = append(r, k)
-	}
+	r := make([]K, len(m))
+	keys(m, (unsafe.Pointer)(unsafe.SliceData(r)))
 	return r
 }
 
