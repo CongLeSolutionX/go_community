6ee33a34f6704e72161d3a20a8acc42db5cdd0f0
Ian Lance Taylor <iant@golang.org>
2023-04-14 14:36:12 -0700

    runtime: change lfstack support to taggedPointer
    This is a refactoring with no change in behavior, in preparation
    for future netpoll work.
    
    For #59545
    
    Change-Id: I493c5fd0f49f31b75787f7b5b89c544bed73f64f

---
+++ src/runtime/tagptr_32bit.go
@@ -0,0 +1,35 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build 386 || arm || mips || mipsle
+
+package runtime
+
+import "unsafe"
+
+// taggedPointer is a pointer with a numeric tag.
+// The size of the numeric tag is GOARCH-dependent,
+// currently at least 11 bits.
+// Bits that don't fit in the tag are discarded.
+type taggedPointer uint64
+
+// The number of bits stored in the numeric tag of a taggedPointer
+const taggedPointerBits = 32
+
+// On 32-bit systems, taggedPointer has a 32-bit pointer and 32-bit count.
+
+// taggedPointerPack created a taggedPointer from a pointer and a tag.
+func taggedPointerPack(ptr unsafe.Pointer, tag uintptr) taggedPointer {
+	return taggedPointer(uintptr(ptr))<<32 | taggedPointer(tag)
+}
+
+// Pointer returns the pointer from a taggedPointer.
+func (tp taggedPointer) pointer() unsafe.Pointer {
+	return unsafe.Pointer(uintptr(tp >> 32))
+}
+
+// Tag returns the tag from a taggedPointer.
+func (tp taggedPointer) tag() uintptr {
+	return uintptr(tp)
+}
