af5927390706a7dd4e429ad60e5ac777324dc9c7
Bryan C. Mills <bcmills@google.com>
2023-04-24 10:18:07 -0400

    runtime: fix CgoRaceprof and CgoRaceSignal build failures
    TestRaceProf and TestRaceSignal were changed to run on all platforms
    that support the race detector as of CL 487575, but the testprogcgo
    source files needed to run the test rely on POSIX threads and were
    still build-constrained to only linux/amd64 and freebsd/amd64.
    
    Since the C test program appears to require only POSIX APIs, update
    the constraint to build the source file on all Unix platforms, and
    update the tests to skip on Windows.
    
    This may slightly increase testprogcgo build time on Unix platforms
    that do not support the race detector.
    
    Change-Id: I704dd496d475a3cd2e2da2a09c7d2e3bb8e96d02

--- src/runtime/testdata/testprogcgo/raceprof.go
+++ src/runtime/testdata/testprogcgo/raceprof.go
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build (linux && amd64) || (freebsd && amd64)
-// +build linux,amd64 freebsd,amd64
-
 package main
 
 // Test that we can collect a lot of colliding profiling signals from
