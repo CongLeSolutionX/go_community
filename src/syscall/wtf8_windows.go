bc192c0fcd36a3552f8c643a4b5bf589ce608d53
qmuntal <quimmuntal@gmail.com>
2023-05-05 18:17:18 +0200

    os, syscall: support ill-formed UTF-16 strings on Windows
    Windows UTF-16 strings can contain unpaired surrogates, which can't be
    decoded into a valid UTF-8 string. This file defines a set of functions
    that can be used to encode and decode potentially ill-formed UTF-16
    strings by using the
    [the WTF-8 encoding](https://simonsapin.github.io/wtf-8/).
    
    WTF-8 is a strict superset of UTF-8, i.e. any string that is
    well-formed in UTF-8 is also well-formed in WTF-8 and the content
    is unchanged. Also, the conversion never fails and is lossless.
    
    The benefit of using WTF-8 instead of UTF-8 when decoding a UTF-16
    string is that the conversion is lossless even for ill-formed
    UTF-16 strings. This property allows to read an ill-formed UTF-16
    string, convert it to a Go string, and convert it back to the same
    original UTF-16 string.
    
    Fixes #59971
    
    Change-Id: Id6007f6e537844913402b233e73d698688cd5ba6

---
+++ src/syscall/wtf8_windows.go
@@ -0,0 +1,132 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Windows UTF-16 strings can contain unpaired surrogates, which can't be
+// decoded into a valid UTF-8 string. This file defines a set of functions
+// that can be used to encode and decode potentially ill-formed UTF-16 strings
+// by using the [the WTF-8 encoding](https://simonsapin.github.io/wtf-8/).
+//
+// WTF-8 is a strict superset of UTF-8, i.e. any string that is
+// well-formed in UTF-8 is also well-formed in WTF-8 and the content
+// is unchanged. Also, the conversion never fails and is lossless.
+//
+// The benefit of using WTF-8 instead of UTF-8 when decoding a UTF-16 string
+// is that the conversion is lossless even for ill-formed UTF-16 strings.
+// This property allows to read an ill-formed UTF-16 string, convert it
+// to a Go string, and convert it back to the same original UTF-16 string.
+//
+// See go.dev/issues/59971 for more info.
+
+package syscall
+
+import (
+	"unicode/utf16"
+	"unicode/utf8"
+)
+
+// encodeWTF16 returns the potentially ill-formed
+// UTF-16 encoding of s.
+func encodeWTF16(s string, buf []uint16) []uint16 {
+	// Go's range loop over a string decodes and expects
+	// UTF-8 runes. Use a for loop instead to support
+	// invalid UTF-8 runes.
+	for i := 0; i < len(s); {
+		r, size := utf8.DecodeRuneInString(s[i:])
+		if r == utf8.RuneError {
+			// WTF-8 fallback.
+			r, size = decodeRuneInString(s[i:])
+			buf = append(buf, uint16(r))
+			i += size
+			continue
+		}
+		i += size
+		buf = utf16.AppendRune(buf, r)
+	}
+	return buf
+}
+
+// decodeWTF16 returns the WTF-8 encoding of
+// the potentially ill-formed UTF-16 s.
+func decodeWTF16(s []uint16, buf []byte) []byte {
+	const (
+		surr1    = 0xd800
+		surr2    = 0xdc00
+		surr3    = 0xe000
+		surrSelf = 0x10000
+	)
+	for i := 0; i < len(s); i++ {
+		var ar rune
+		switch r := s[i]; {
+		case r < surr1, surr3 <= r:
+			// normal rune
+			ar = rune(r)
+		case surr1 <= r && r < surr2 && i+1 < len(s) &&
+			surr2 <= s[i+1] && s[i+1] < surr3:
+			// valid surrogate sequence
+			ar = utf16.DecodeRune(rune(r), rune(s[i+1]))
+			i++
+		default:
+			// WTF-8 fallback.
+			buf = appendRune(buf, rune(r))
+			continue
+		}
+		buf = utf8.AppendRune(buf, ar)
+
+	}
+	return buf
+}
+
+// decodeRuneInString is like utf8.DecodeRuneInString but
+// also handles runes encoded in the surrogate range.
+func decodeRuneInString(s string) (r rune, size int) {
+	const (
+		maskx = 0b00111111
+		mask2 = 0b00011111
+		mask3 = 0b00001111
+		mask4 = 0b00000111
+	)
+	n := len(s)
+	if n < 1 {
+		return utf8.RuneError, 0
+	}
+	switch c := s[0]; {
+	case c <= 0x7f:
+		return rune(c), 1
+	case n >= 2 && 0xC2 <= c && c <= 0xDF:
+		return rune(c&mask2)<<6 + rune(s[1]&maskx), 2
+	case n >= 3 && 0xE0 <= c && c <= 0xEF:
+		return rune(c&mask3)<<12 + rune(s[1]&maskx)<<6 + rune(s[2]&maskx), 3
+	case n >= 4 && 0xF0 <= c && c <= 0xF4:
+		return rune(c&mask4)<<18 | rune(s[1]&maskx)<<12 | rune(s[2]&maskx)<<6 | rune(s[3]&maskx), 4
+	}
+	return utf8.RuneError, 1
+}
+
+// appendRune is like utf8.AppendRune but
+// also handles runes encoded in the surrogate range.
+func appendRune(p []byte, r rune) []byte {
+	const (
+		tx = 0b10000000
+		t2 = 0b11000000
+		t3 = 0b11100000
+		t4 = 0b11110000
+
+		maskx = 0b00111111
+
+		rune1Max = 1<<7 - 1
+		rune2Max = 1<<11 - 1
+		rune3Max = 1<<16 - 1
+	)
+	// Negative values are erroneous. Making it unsigned addresses the problem.
+	switch i := uint32(r); {
+	case i <= rune1Max:
+		return append(p, byte(r))
+	case i <= rune2Max:
+		return append(p, t2|byte(r>>6), tx|byte(r)&maskx)
+	case i <= rune3Max:
+		return append(p, t3|byte(r>>12), tx|byte(r>>6)&maskx, tx|byte(r)&maskx)
+	default:
+		return append(p, t4|byte(r>>18), tx|byte(r>>12)&maskx, tx|byte(r>>6)&maskx, tx|byte(r)&maskx)
+	}
+}
---
+++ src/syscall/wtf8_windows_test.go
@@ -0,0 +1,192 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package syscall_test
+
+import (
+	"slices"
+	"syscall"
+	"testing"
+	"unicode/utf16"
+	"unicode/utf8"
+)
+
+var wtf8tests = []struct {
+	name    string
+	content []uint8
+	want    []uint16
+}{
+	{
+		name:    "0x0000",
+		content: []uint8{0x00},
+		want:    []uint16{0x00},
+	},
+	{
+		name:    "0x005C",
+		content: []uint8{0x5C},
+		want:    []uint16{0x5C},
+	},
+	{
+		name:    "0x007F",
+		content: []uint8{0x7F},
+		want:    []uint16{0x7F},
+	},
+
+	// 2-byte
+	{
+		name:    "0x0080",
+		content: []uint8{0xC2, 0x80},
+		want:    []uint16{0x80},
+	},
+	{
+		name:    "0x05CA",
+		content: []uint8{0xD7, 0x8A},
+		want:    []uint16{0x05CA},
+	},
+	{
+		name:    "0x07FF",
+		content: []uint8{0xDF, 0xBF},
+		want:    []uint16{0x07FF},
+	},
+
+	// 3-byte
+	{
+		name:    "0x0800",
+		content: []uint8{0xE0, 0xA0, 0x80},
+		want:    []uint16{0x0800},
+	},
+	{
+		name:    "0x2C3C",
+		content: []uint8{0xE2, 0xB0, 0xBC},
+		want:    []uint16{0x2C3C},
+	},
+	{
+		name:    "0xFFFF",
+		content: []uint8{0xEF, 0xBF, 0xBF},
+		want:    []uint16{0xFFFF},
+	},
+	// unmatched surrogate halves
+	// high surrogates: 0xD800 to 0xDBFF
+	{
+		name:    "0xD800",
+		content: []uint8{0xED, 0xA0, 0x80},
+		want:    []uint16{0xD800},
+	},
+	{
+		name:    "High surrogate followed by another high surrogate",
+		content: []uint8{0xED, 0xA0, 0x80, 0xED, 0xA0, 0x80},
+		want:    []uint16{0xD800, 0xD800},
+	},
+	{
+		name:    "High surrogate followed by a symbol that is not a surrogate",
+		content: []uint8{0xED, 0xA0, 0x80, 0xA},
+		want:    []uint16{0xD800, 0xA},
+	},
+	{
+		name:    "Unmatched high surrogate, followed by a surrogate pair, followed by an unmatched high surrogate",
+		content: []uint8{0xED, 0xA0, 0x80, 0xF0, 0x9D, 0x8C, 0x86, 0xED, 0xA0, 0x80},
+		want:    []uint16{0xD800, 0xD834, 0xDF06, 0xD800},
+	},
+	{
+		name:    "0xD9AF",
+		content: []uint8{0xED, 0xA6, 0xAF},
+		want:    []uint16{0xD9AF},
+	},
+	{
+		name:    "0xDBFF",
+		content: []uint8{0xED, 0xAF, 0xBF},
+		want:    []uint16{0xDBFF},
+	},
+	// low surrogates: 0xDC00 to 0xDFFF
+	{
+		name:    "0xDC00",
+		content: []uint8{0xED, 0xB0, 0x80},
+		want:    []uint16{0xDC00},
+	},
+	{
+		name:    "Low surrogate followed by another low surrogate",
+		content: []uint8{0xED, 0xB0, 0x80, 0xED, 0xB0, 0x80},
+		want:    []uint16{0xDC00, 0xDC00},
+	},
+	{
+		name:    "Low surrogate followed by a symbol that is not a surrogate",
+		content: []uint8{0xED, 0xB0, 0x80, 0xA},
+		want:    []uint16{0xDC00, 0xA},
+	},
+	{
+		name:    "Unmatched low surrogate, followed by a surrogate pair, followed by an unmatched low surrogate",
+		content: []uint8{0xED, 0xB0, 0x80, 0xF0, 0x9D, 0x8C, 0x86, 0xED, 0xB0, 0x80},
+		want:    []uint16{0xDC00, 0xD834, 0xDF06, 0xDC00},
+	},
+	{
+		name:    "0xDEEE",
+		content: []uint8{0xED, 0xBB, 0xAE},
+		want:    []uint16{0xDEEE},
+	},
+	{
+		name:    "0xDFFF",
+		content: []uint8{0xED, 0xBF, 0xBF},
+		want:    []uint16{0xDFFF},
+	},
+
+	// 4-byte
+	{
+		name:    "0x010000",
+		want:    []uint16{0xD800, 0xDC00},
+		content: []uint8{0xF0, 0x90, 0x80, 0x80},
+	},
+	{
+		name:    "0x01D306",
+		want:    []uint16{0xD834, 0xDF06},
+		content: []uint8{0xF0, 0x9D, 0x8C, 0x86},
+	},
+	{
+		name:    "0x10FFF",
+		want:    []uint16{0xDBFF, 0xDFFF},
+		content: []uint8{0xF4, 0x8F, 0xBF, 0xBF},
+	},
+}
+
+func TestWTF16Rountrip(t *testing.T) {
+	for _, tt := range wtf8tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := syscall.EncodeWTF16(string(tt.content), nil)
+			got2 := string(syscall.DecodeWTF16(got, nil))
+			if got2 != string(tt.content) {
+				t.Errorf("got:\n%swant:\n%s", got2, string(tt.content))
+			}
+		})
+	}
+}
+
+func TestWTF16Golden(t *testing.T) {
+	for _, tt := range wtf8tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := syscall.EncodeWTF16(string(tt.content), nil)
+			if !slices.Equal(got, tt.want) {
+				t.Errorf("got:\n%vwant:\n%v", got, tt.want)
+			}
+		})
+	}
+}
+
+func FuzzEncodeWTF16(f *testing.F) {
+	for _, tt := range wtf8tests {
+		f.Add(tt.content)
+	}
+	f.Fuzz(func(t *testing.T, b []byte) {
+		// test that there are no panics
+		got := syscall.EncodeWTF16(string(b), nil)
+		syscall.DecodeWTF16(got, nil)
+		if utf8.Valid(b) {
+			// if the input is a valid UTF-8 string, then
+			// test that syscall.EncodeWTF16 behaves as
+			// utf16.Encode/
+			want := utf16.Encode([]rune(string(b)))
+			if !slices.Equal(got, want) {
+				t.Errorf("got:\n%vwant:\n%v", got, want)
+			}
+		}
+	})
+}
