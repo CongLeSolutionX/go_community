11cda3469e83e5b0503cf790dfab9fa7b24b591d
Than McIntosh <thanm@google.com>
2023-05-05 15:53:33 -0400

    cmd/cover: add new "emit meta file" mode for packages without tests
    Introduce a new mode of execution for instrumenting packages that have
    no test files. Instead of just skipping packages with no test files
    (during "go test -cover" runs), the go command will invoke cmd/cover
    on the package passing in an option in the config file indicating that
    it should emit a coverage meta-data file directly for the package if
    it has any functions.
    
    Updates #27261.
    Updates #58770
    Updates #24570.
    
    Change-Id: I01e8a3edb62441698c7246596e4bacbd966591c3

--- src/cmd/cover/cfg_test.go
+++ src/cmd/cover/cfg_test.go
@@ -21,14 +21,15 @@ func writeFile(t *testing.T, path string, contents []byte) {
 	}
 }
 
-func writePkgConfig(t *testing.T, outdir, tag, ppath, pname string, gran string) string {
+func writePkgConfig(t *testing.T, outdir, tag, ppath, pname string, gran string, mpath string) string {
 	incfg := filepath.Join(outdir, tag+"incfg.txt")
 	outcfg := filepath.Join(outdir, "outcfg.txt")
 	p := coverage.CoverPkgConfig{
-		PkgPath:     ppath,
-		PkgName:     pname,
-		Granularity: gran,
-		OutConfig:   outcfg,
+		PkgPath:      ppath,
+		PkgName:      pname,
+		Granularity:  gran,
+		OutConfig:    outcfg,
+		EmitMetaFile: mpath,
 	}
 	data, err := json.Marshal(p)
 	if err != nil {
@@ -72,10 +73,6 @@ func runPkgCover(t *testing.T, outdir string, tag string, incfg string, mode str
 	}
 }
 
-// Set to true when debugging unit test (to inspect debris, etc).
-// Note that this functionality does not work on windows.
-const debugWorkDir = false
-
 func TestCoverWithCfg(t *testing.T) {
 	testenv.MustHaveGoRun(t)
 
@@ -83,29 +80,7 @@ func TestCoverWithCfg(t *testing.T) {
 
 	// Subdir in testdata that has our input files of interest.
 	tpath := filepath.Join("testdata", "pkgcfg")
-
-	// Helper to collect input paths (go files) for a subdir in 'pkgcfg'
-	pfiles := func(subdir string) []string {
-		de, err := os.ReadDir(filepath.Join(tpath, subdir))
-		if err != nil {
-			t.Fatalf("reading subdir %s: %v", subdir, err)
-		}
-		paths := []string{}
-		for _, e := range de {
-			if !strings.HasSuffix(e.Name(), ".go") || strings.HasSuffix(e.Name(), "_test.go") {
-				continue
-			}
-			paths = append(paths, filepath.Join(tpath, subdir, e.Name()))
-		}
-		return paths
-	}
-
 	dir := tempDir(t)
-	if debugWorkDir {
-		dir = "/tmp/qqq"
-		os.RemoveAll(dir)
-		os.Mkdir(dir, 0777)
-	}
 	instdira := filepath.Join(dir, "insta")
 	if err := os.Mkdir(instdira, 0777); err != nil {
 		t.Fatal(err)
@@ -129,6 +104,7 @@ func TestCoverWithCfg(t *testing.T) {
 	}
 
 	var incfg string
+	apkgfiles := []string{filepath.Join(tpath, "a", "a.go")}
 	for _, scenario := range scenarios {
 		// Instrument package "a", producing a set of instrumented output
 		// files and an 'output config' file to pass on to the compiler.
@@ -137,9 +113,9 @@ func TestCoverWithCfg(t *testing.T) {
 		mode := scenario.mode
 		gran := scenario.gran
 		tag := mode + "_" + gran
-		incfg = writePkgConfig(t, instdira, tag, ppath, pname, gran)
+		incfg = writePkgConfig(t, instdira, tag, ppath, pname, gran, "")
 		ofs, outcfg, _ := runPkgCover(t, instdira, tag, incfg, mode,
-			pfiles("a"), false)
+			apkgfiles, false)
 		t.Logf("outfiles: %+v\n", ofs)
 
 		// Run the compiler on the files to make sure the result is
@@ -159,7 +135,7 @@ func TestCoverWithCfg(t *testing.T) {
 	errExpected := true
 	tag := "errors"
 	_, _, errmsg := runPkgCover(t, instdira, tag, "/not/a/file", mode,
-		pfiles("a"), errExpected)
+		apkgfiles, errExpected)
 	want := "error reading pkgconfig file"
 	if !strings.Contains(errmsg, want) {
 		t.Errorf("'bad config file' test: wanted %s got %s", want, errmsg)
@@ -169,7 +145,7 @@ func TestCoverWithCfg(t *testing.T) {
 	t.Logf("mangling in config")
 	writeFile(t, incfg, []byte("blah=foo\n"))
 	_, _, errmsg = runPkgCover(t, instdira, tag, incfg, mode,
-		pfiles("a"), errExpected)
+		apkgfiles, errExpected)
 	want = "error reading pkgconfig file"
 	if !strings.Contains(errmsg, want) {
 		t.Errorf("'bad config file' test: wanted %s got %s", want, errmsg)
@@ -179,8 +155,86 @@ func TestCoverWithCfg(t *testing.T) {
 	t.Logf("writing empty config")
 	writeFile(t, incfg, []byte("\n"))
 	_, _, errmsg = runPkgCover(t, instdira, tag, incfg, mode,
-		pfiles("a"), errExpected)
+		apkgfiles, errExpected)
 	if !strings.Contains(errmsg, want) {
 		t.Errorf("'bad config file' test: wanted %s got %s", want, errmsg)
+	}
+}
+
+func TestCoverOnPackageWithNoTestFiles(t *testing.T) {
+	testenv.MustHaveGoRun(t)
+	t.Parallel()
+	dir := tempDir(t)
+
+	// For packages with no test files, the new "go test -cover"
+	// strategy is to run cmd/cover on the package in a special
+	// "EmitMetaFile" mode. When running in this mode, cmd/cover walks
+	// the package doing instrumention, but when finished, instead of
+	// writing out instrumented source files, it directly emits a
+	// meta-data file for the package in question, essentially
+	// simulating the effect that you would get if you added a dummy
+	// "no-op" x_test.go file and then did a build and run of the test.
+
+	// Case 1: go files with functions but no test files.
+	tpath := filepath.Join("testdata", "pkgcfg")
+	pkg1files := []string{filepath.Join(tpath, "yesFuncsNoTests", "yfnt.go")}
+	ppath := "cfg/yesFuncsNoTests"
+	pname := "yesFuncsNoTests"
+	mode := "count"
+	gran := "perblock"
+	tag := mode + "_" + gran
+	instdir := filepath.Join(dir, "inst")
+	if err := os.Mkdir(instdir, 0777); err != nil {
+		t.Fatal(err)
+	}
+	mdir := filepath.Join(dir, "meta")
+	if err := os.Mkdir(mdir, 0777); err != nil {
+		t.Fatal(err)
+	}
+	mpath := filepath.Join(mdir, "covmeta.xxx")
+	incfg := writePkgConfig(t, instdir, tag, ppath, pname, gran, mpath)
+	_, _, errmsg := runPkgCover(t, instdir, tag, incfg, mode,
+		pkg1files, false)
+	if errmsg != "" {
+		t.Fatalf("runPkgCover err: %q", errmsg)
+	}
+
+	// Check for existence of meta-data file.
+	if inf, err := os.Open(mpath); err != nil {
+		t.Fatalf("meta-data file not created: %v", err)
+	} else {
+		inf.Close()
+	}
+
+	// Make sure it is digestible.
+	cdargs := []string{"tool", "covdata", "percent", "-i", mdir}
+	cmd := testenv.Command(t, testenv.GoToolPath(t), cdargs...)
+	run(cmd, t)
+
+	// Case 1: go file with no functions and no tests.
+	pkg2files := []string{filepath.Join(tpath, "noFuncsNoTests", "nfnt.go")}
+	pname2 := "noFuncsNoTests"
+	ppath2 := "cfg/" + pname
+	tag2 := mode + "_" + gran
+	instdir2 := filepath.Join(dir, "inst2")
+	if err := os.Mkdir(instdir2, 0777); err != nil {
+		t.Fatal(err)
+	}
+	mdir2 := filepath.Join(dir, "meta2")
+	if err := os.Mkdir(mdir2, 0777); err != nil {
+		t.Fatal(err)
+	}
+	mpath2 := filepath.Join(mdir2, "covmeta.yyy")
+	incfg2 := writePkgConfig(t, instdir2, tag2, ppath2, pname2, gran, mpath2)
+	_, _, errmsg2 := runPkgCover(t, instdir2, tag2, incfg2, mode,
+		pkg2files, false)
+	if errmsg2 != "" {
+		t.Fatalf("runPkgCover err: %q", errmsg2)
+	}
+
+	// This time around we don't expect to see the meta-data file.
+	if inf, err := os.Open(mpath2); err == nil {
+		t.Fatalf("meta-data file was created: %v", err)
+		inf.Close()
 	}
 }
