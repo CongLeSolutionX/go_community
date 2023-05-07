e336aad3cd2f492916a5ae29c728b9a7e0bc44e2
Zeke Lu <lvzecai@gmail.com>
2023-05-07 08:53:23 +0000

    cmd/go/internal/modload: reject the -modfile flag in workspace mode
    Currently, in workspace mode, the -modfile flag affects all the modules
    listed in the go.work file. This is not desirable most of the time. And
    when it results in an error, the error message does not help.
    
    For example, when there are more than one modules listed in the go.work
    file, running "go list -m -modfile=path/to/go.mod" gives this error:
      go: module example.com/foo appears multiple times in workspace
    
    This change reject -modfile flag explicitly with this error message:
      go: -modfile cannot be used in workspace mode
    
    While at here, correct some typos in the modload package.
    
    Fixes #59996.
    
    Change-Id: Iff4cd9f3974ea359889dd713a747b6932cf42dfd
    GitHub-Last-Rev: 1e6c4023caba39478711bf21a87cb81deba2a310
    GitHub-Pull-Request: golang/go#60033

--- src/cmd/go/internal/modload/query.go
+++ src/cmd/go/internal/modload/query.go
@@ -117,7 +117,7 @@ func checkReuse(ctx context.Context, path string, old *codehost.Origin) error {
 // version. Any other error indicates the function was unable to determine
 // whether the version should be allowed, for example, the function was unable
 // to fetch or parse a go.mod file containing retractions. Typically, errors
-// other than ErrDisallowd may be ignored.
+// other than ErrDisallowed may be ignored.
 type AllowedFunc func(context.Context, module.Version) error
 
 var errQueryDisabled error = queryDisabledError{}
@@ -369,7 +369,7 @@ var errRevQuery = errors.New("query refers to a non-semver revision")
 // newQueryMatcher returns a new queryMatcher that matches the versions
 // specified by the given query on the module with the given path.
 //
-// If the query can only be resolved by statting a non-SemVer revision,
+// If the query can only be resolved by stating a non-SemVer revision,
 // newQueryMatcher returns errRevQuery.
 func newQueryMatcher(path string, query, current string, allowed AllowedFunc) (*queryMatcher, error) {
 	badVersion := func(v string) (*queryMatcher, error) {
@@ -521,7 +521,7 @@ func (qm *queryMatcher) filterVersions(ctx context.Context, versions []string) (
 		}
 
 		if !needIncompatible {
-			// We're not yet sure whether we need to include +incomptaible versions.
+			// We're not yet sure whether we need to include +incompatible versions.
 			// Keep track of the last compatible version we've seen, and use the
 			// presence (or absence) of a go.mod file in that version to decide: a
 			// go.mod file implies that the module author is supporting modules at a
@@ -1018,7 +1018,7 @@ func (e *PackageNotInModuleError) ImportPath() string {
 // 1.12 at least have a go directive.
 //
 // This function is a heuristic, since it's possible to commit a file that would
-// pass this test. However, we only need a heurstic for determining whether
+// pass this test. However, we only need a heuristic for determining whether
 // +incompatible versions may be "latest", which is what this function is used
 // for.
 //
