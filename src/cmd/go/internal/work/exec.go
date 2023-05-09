624abe6093a50c82308b1bd10af658c5ddbdfb7d
Than McIntosh <thanm@google.com>
2023-05-09 15:40:41 -0400

    cmd/go: improve handling of no-test packages for coverage
    This patch improves the way the go command handles coverage testing
    of packages that have functions but don't have any test files. Up to
    this point if you ran "go test -cover" on such a package, you would
    see:
    
      ?   	mymod/mypack	[no test files]
    
    While "no test files" is true, it is also very unhelpful; if the
    package contains functions, it would be better instead to capture the
    fact that these functions are not executed when "go test -cover" is
    run on the package.
    
    With this patch, for the same no-test package "go test -cover" will
    output:
    
    	mymod/mypack	coverage: 0.0% of statements
    
    The inclusion of such packages in coverage reporting also extends to
    "-coverprofile" as well (we'll see entries for the "mypack" functions
    in this case.
    
    Note that if a package has no functions at all, then we'll still fall
    back to reporting "no test files" in this case; it doesn't make sense
    to report "0.0% statements covered" if there are no statements.
    
    Updates #27261.
    Updates #58770
    Fixes #24570.
    
    Change-Id: I8e916425f4f2beec65861df78265e93db5ce001a

--- src/cmd/go/internal/work/exec.go
+++ src/cmd/go/internal/work/exec.go
@@ -449,6 +449,7 @@ const (
 	needCgoHdr
 	needVet
 	needCompiledGoFiles
+	needCovMetaFile
 	needStale
 )
 
@@ -468,6 +469,7 @@ func (b *Builder) build(ctx context.Context, a *Action) (err error) {
 	need := bit(needBuild, !b.IsCmdList && a.needBuild || b.NeedExport) |
 		bit(needCgoHdr, b.needCgoHdr(a)) |
 		bit(needVet, a.needVet) |
+		bit(needCovMetaFile, genCovMetaFile(a)) |
 		bit(needCompiledGoFiles, b.NeedCompiledGoFiles)
 
 	if !p.BinaryOnly {
@@ -549,6 +551,14 @@ func (b *Builder) build(ctx context.Context, a *Action) (err error) {
 	if cachedBuild && need&needCgoHdr != 0 {
 		if err := b.loadCachedCgoHdr(a); err == nil {
 			need &^= needCgoHdr
+		}
+	}
+
+	// Load cached coverage meta-data file fragment, but only if we're
+	// skipping the main build (cachedBuild==true).
+	if cachedBuild && need&needCovMetaFile != 0 {
+		if err := b.loadCachedCovMetaFile(a); err == nil {
+			need &^= needCovMetaFile
 		}
 	}
 
@@ -691,6 +701,15 @@ OverlayLoop:
 				if mode == "" {
 					panic("covermode should be set at this point")
 				}
+				if genCovMetaFile(a) {
+					// Create a subdirectory within the compile action
+					// objdir, into which the cover tool will deposit
+					// the meta-data file.
+					cdir := CovMetaDestDir(a)
+					if err := b.Mkdir(cdir); err != nil {
+						return err
+					}
+				}
 				if newoutfiles, err := b.cover2(a, infiles, outfiles, coverVar, mode); err != nil {
 					return err
 				} else {
@@ -704,6 +723,9 @@ OverlayLoop:
 				// the empty string so as to signal that we need to do
 				// that.
 				p.Internal.CoverMode = ""
+			}
+			if genCovMetaFile(a) {
+				b.cacheCovMetaFile(a)
 			}
 		}
 	}
@@ -2070,6 +2092,9 @@ func (b *Builder) writeCoverPkgInputs(a *Action, pconfigfile string, covoutputsf
 		OutConfig:   p.Internal.CoverageCfg,
 		Local:       p.Internal.Local,
 	}
+	if genCovMetaFile(a) {
+		pcfg.EmitMetaFile = covMetaDestPath(a)
+	}
 	if a.Package.Module != nil {
 		pcfg.ModulePath = a.Package.Module.Path
 	}
@@ -2077,6 +2102,7 @@ func (b *Builder) writeCoverPkgInputs(a *Action, pconfigfile string, covoutputsf
 	if err != nil {
 		return err
 	}
+	data = append(data, '\n')
 	if err := b.writeFile(pconfigfile, data); err != nil {
 		return err
 	}
