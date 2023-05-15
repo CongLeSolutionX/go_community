ebd0a454e6f545ec5d26468c6faa4e83fea47fc6
Tobias Klauser <tklauser@distanz.ch>
2023-05-15 17:12:25 +0200

    os: set File.appendMode in newFile if file is opened with O_APPEND
    To allow skipping the use of copy_file_range on Linux which isn't
    supported for destination files opened with O_APPEND, see comment in
    (*File).readFrom.
    
    Fixes #60181
    
    Change-Id: Ie0b0050faab16858412928a3d1f96442619581eb

--- src/os/file_unix.go
+++ src/os/file_unix.go
@@ -149,6 +149,11 @@ func newFile(fd uintptr, name string, kind newFileKind) *File {
 
 	pollable := kind == kindOpenFile || kind == kindPipe || kind == kindNonBlock
 
+	flags, _, errno := syscall.Syscall(unix.FcntlSyscall, uintptr(fd), uintptr(syscall.F_GETFL), 0)
+	if errno == 0 {
+		f.appendMode = flags&syscall.O_APPEND != 0
+	}
+
 	// If the caller passed a non-blocking filedes (kindNonBlock),
 	// we assume they know what they are doing so we allow it to be
 	// used with kqueue.
