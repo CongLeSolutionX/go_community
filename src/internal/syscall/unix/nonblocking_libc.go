3f55e2a4d2d2e125066072b8b1723933b1c46207
Tobias Klauser <tklauser@distanz.ch>
2023-05-15 17:45:39 +0200

    internal/poll, internal/syscall/unix, net: move and export fcntl syscall wrapper
    This will allow to use the fcntl syscall in packages other than
    internal/poll.
    
    For #60181
    
    Change-Id: I76703766a655f2343c61dad95faf81aad58e007f

--- src/internal/syscall/unix/nonblocking_libc.go
+++ src/internal/syscall/unix/nonblocking_libc.go
@@ -6,20 +6,12 @@
 
 package unix
 
-import (
-	"syscall"
-	_ "unsafe" // for go:linkname
-)
+import "syscall"
 
 func IsNonblock(fd int) (nonblocking bool, err error) {
-	flag, e1 := fcntl(fd, syscall.F_GETFL, 0)
+	flag, e1 := Fcntl(fd, syscall.F_GETFL, 0)
 	if e1 != nil {
 		return false, e1
 	}
 	return flag&syscall.O_NONBLOCK != 0, nil
 }
-
-// Implemented in the syscall package.
-//
-//go:linkname fcntl syscall.fcntl
-func fcntl(fd int, cmd int, arg int) (int, error)
