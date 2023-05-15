bcc945fffc99cab16d868ce3d4089db83334a443
Tobias Klauser <tklauser@distanz.ch>
2023-05-15 17:45:39 +0200

    internal/poll, internal/syscall/unix, net: move and export fcntl syscall wrapper
    This will allow to use the fcntl syscall in packages other than
    internal/poll.
    
    For #60181
    
    Change-Id: I76703766a655f2343c61dad95faf81aad58e007f

--- src/internal/syscall/unix/nonblocking.go
+++ src/internal/syscall/unix/nonblocking.go
@@ -11,11 +11,3 @@ import "syscall"
 // FcntlSyscall is the number for the fcntl system call. This is
 // usually SYS_FCNTL, but can be overridden to SYS_FCNTL64.
 var FcntlSyscall uintptr = syscall.SYS_FCNTL
-
-func IsNonblock(fd int) (nonblocking bool, err error) {
-	flag, _, e1 := syscall.Syscall(FcntlSyscall, uintptr(fd), uintptr(syscall.F_GETFL), 0)
-	if e1 != 0 {
-		return false, e1
-	}
-	return flag&syscall.O_NONBLOCK != 0, nil
-}
