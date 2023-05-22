2a5374017af843f8327783695e6334e85de599db
Jorropo <jorropo.pgm@gmail.com>
2023-05-22 09:36:09 +0200

    syscall/js: implements ValueOf for slices and maps of Value
    Fixes #60335
    
    Change-Id: I8f5ad776eb9a54b804873d4210c4d24eb4a3950c

--- src/clean.bash
+++ src/clean.bash
@@ -3,6 +3,8 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+# How do I even restart TryBots without hacking it like this ?
+
 set -e
 
 if [ ! -f run.bash ]; then
