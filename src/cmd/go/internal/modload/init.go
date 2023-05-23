19d551666be365088978a52a6e0955d0f7b30f72
Michael Matloob <matloob@golang.org>
2023-05-23 16:13:13 -0400

    cmd/go/internal/modload: make it clear -mod can't be set in some cases
    We check that -mod can't be set to mod in workspace mode, but then we
    set BuildMod to mod for go work sync below. Make it clear that that's
    okay because we can't pass -mod=mod to go work sync (or the other go
    mod commands that can run in workspace mode that set mod=mod: go mod
    graph, go mod verify, and go mod why).
    
    Change-Id: Idfe6fea6a420211886e4f838e050be4bf7d1b71d

--- src/cmd/go/internal/modload/init.go
+++ src/cmd/go/internal/modload/init.go
@@ -1160,9 +1160,16 @@ func requirementsFromModFiles(ctx context.Context, modFiles []*modfile.File) *Re
 func setDefaultBuildMod() {
 	if cfg.BuildModExplicit {
 		if inWorkspaceMode() && cfg.BuildMod != "readonly" {
-			base.Fatalf("go: -mod may only be set to readonly when in workspace mode, but it is set to %q"+
-				"\n\tRemove the -mod flag to use the default readonly value,"+
-				"\n\tor set GOWORK=off to disable workspace mode.", cfg.BuildMod)
+			switch cfg.CmdName {
+			case "work sync", "mod graph", "mod verify", "mod why":
+				// These commands run with BuildMod set to mod, but they don't take the
+				// -mod flag, so we should never get here.
+				panic("in workspace mode and -mod was set explicitly, but command doesn't support setting -mod")
+			default:
+				base.Fatalf("go: -mod may only be set to readonly when in workspace mode, but it is set to %q"+
+					"\n\tRemove the -mod flag to use the default readonly value,"+
+					"\n\tor set GOWORK=off to disable workspace mode.", cfg.BuildMod)
+			}
 		}
 		// Don't override an explicit '-mod=' argument.
 		return
