a9fc76a30e1564d7f4ee4c125e8791fcb08dd97d
Russ Cox <rsc@golang.org>
2023-06-06 10:51:09 -0400

    math/rand/v2: optimize Perm
    [This is a prototype CL for a new math/rand package, math/rand/v2.
    It is being posted as a preliminary demonstration of what that API
    might look like. DO NOT REVIEW, and don't worry about trybot
    failures either.]
    
    The compiler says Perm is being inlined into BenchmarkPerm,
    and yet BenchmarkPerm30ViaShuffle, which you'd think is the
    same code, still runs significantly faster.
    
    goos: linux
    goarch: amd64
    pkg: math/rand/v2
    cpu: Intel(R) Xeon(R) CPU E5-2690 v3 @ 2.60GHz
                            │ edfb4b190ae.txt │           d7436fa5db5.txt            │
                            │     sec/op      │    sec/op     vs base                │
    SourceUint64-48               3.388n ± 0%    3.389n ± 0%        ~ (p=0.973 n=20)
    GlobalInt63-48                3.461n ± 0%    3.457n ± 0%   -0.12% (p=0.000 n=20)
    GlobalUint64-48               3.994n ± 0%    4.008n ± 0%   +0.36% (p=0.000 n=20)
    Int63-48                      4.049n ± 0%    4.026n ± 0%   -0.57% (p=0.000 n=20)
    Uint64-48                     4.078n ± 0%    3.981n ± 0%   -2.38% (p=0.000 n=20)
    Intn1000-48                   7.115n ± 0%    7.149n ± 0%   +0.47% (p=0.000 n=20)
    Int63n1000-48                 5.909n ± 0%    5.892n ± 0%   -0.29% (p=0.000 n=20)
    Int31n1000-48                 6.237n ± 0%    6.220n ± 0%   -0.28% (p=0.000 n=20)
    Float32-48                    4.811n ± 0%    4.777n ± 0%   -0.69% (p=0.000 n=20)
    Float64-48                    4.708n ± 0%    4.720n ± 0%   +0.24% (p=0.004 n=20)
    Perm3-48                      63.08n ± 0%    56.37n ± 4%  -10.65% (p=0.000 n=20)
    Perm30-48                     420.7n ± 4%    385.2n ± 4%   -8.43% (p=0.000 n=20)
    Perm30ViaShuffle-48           270.8n ± 0%    270.6n ± 0%        ~ (p=0.338 n=20)
    ShuffleOverhead-48            256.5n ± 0%    267.7n ± 0%   +4.37% (p=0.000 n=20)
    Concurrent-48                 3.175n ± 0%    3.205n ± 1%        ~ (p=0.080 n=20)
    
    Change-Id: I342b611456c3569520d3c91c849d29eba325d87e

--- src/math/rand/v2/example_test.go
+++ src/math/rand/v2/example_test.go
@@ -92,7 +92,7 @@ func Example_rand() {
 	// IntN(10)    4                   5                   1
 	// Int32N(10)  8                   5                   4
 	// Int64N(10)  2                   6                   3
-	// Perm        [1 4 2 3 0]         [4 0 2 1 3]         [0 2 1 3 4]
+	// Perm        [3 1 2 0 4]         [2 4 0 3 1]         [3 1 2 4 0]
 }
 
 func ExamplePerm() {
