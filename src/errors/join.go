86664bcd40946302148f1bef615e5fd7de69b0fc
Jes Cok <xigua67damn@gmail.com>
2023-06-06 04:26:21 +0000

    errors: optimize *joinError's Error method for less allocation and faster execution
    Compute the length of the returned string to preallocate byte slice for reducing memory allocation, it will panic when the length is overflow.
    
    Use unsafe.String to avoid memory allocation when converting byte slice to string.
    
    Change-Id: Ib23576f72b1d87489e6f17762be483f62ca4998a
    GitHub-Last-Rev: f7f9b6f3d01eb249e25bbbf624ee5118b29cc3df
    GitHub-Pull-Request: golang/go#60026

--- src/errors/join.go
+++ src/errors/join.go
@@ -4,6 +4,10 @@
 
 package errors
 
+import (
+	"unsafe"
+)
+
 // Join returns an error that wraps the given errors.
 // Any nil error values are discarded.
 // Join returns nil if every value in errs is nil.
@@ -38,14 +42,27 @@ type joinError struct {
 }
 
 func (e *joinError) Error() string {
-	var b []byte
-	for i, err := range e.errs {
-		if i > 0 {
-			b = append(b, '\n')
-		}
+	nerrs := len(e.errs)
+	// Since Join returns nil if every value in errs is nil,
+	// e.errs cannot be empty.
+	// TODO: get rid of case 0
+	switch nerrs {
+	case 0: // Impossible but handle.
+		return "<nil>"
+	case 1:
+		return e.errs[0].Error()
+	}
+
+	// To avoid doubling the number of Error calls, skip preallocating 
+	// the slice on overflow and let the runtime handle the panic.
+	b := []byte(e.errs[0].Error())
+	for _, err := range e.errs[1:] {
+		b = append(b, '\n')
 		b = append(b, err.Error()...)
 	}
-	return string(b)
+	// At this point, b has at least one byte '\n'.
+	// TODO: replace with unsafe.String(&b[0], len(b))
+	return unsafe.String(unsafe.SliceData(b), len(b))
 }
 
 func (e *joinError) Unwrap() []error {
