4ed58810b1c40b09ff93680674d7b0cc67907366
Bryan C. Mills <bcmills@google.com>
2023-06-08 17:55:55 -0400

    [release-branch.go1.20] cmd/go: omit checksums for go.mod files needed for go version lines more often in pre-1.21 modules
    This updates the logic from CL 489075 to avoid trying to save extra
    sums if they aren't already expected to be present
    and cfg.BuildMod != "mod" (as in the case of "go list -m -u all" with
    a go.mod file that specifies go < 1.21).
    
    Fixes #60698.
    Updates #60667.
    Updates #56222.
    
    Change-Id: Ied6ed3e80a62f9cd9a328b43a415a42d14481056

--- src/cmd/go/internal/modload/init.go
+++ src/cmd/go/internal/modload/init.go
@@ -1597,7 +1597,21 @@ func keepSums(ctx context.Context, ld *loader, rs *Requirements, which whichSums
 	// paths of loaded packages. We need to retain sums for all of these modules —
 	// not just the modules containing the actual packages — in order to rule out
 	// ambiguous import errors the next time we load the package.
+<<<<<<< HEAD   (100848 [release-branch.go1.20] cmd/cgo: correct _cgo_flags output)
 	if ld != nil {
+=======
+	keepModSumsForZipSums := true
+	if ld == nil {
+		if gover.Compare(MainModules.GoVersion(), gover.TidyGoModSumVersion) < 0 && cfg.BuildMod != "mod" {
+			keepModSumsForZipSums = false
+		}
+	} else {
+		keepPkgGoModSums := true
+		if gover.Compare(ld.requirements.GoVersion(), gover.TidyGoModSumVersion) < 0 && (ld.Tidy || cfg.BuildMod != "mod") {
+			keepPkgGoModSums = false
+			keepModSumsForZipSums = false
+		}
+>>>>>>> CHANGE (5cabee cmd/go: omit checksums for go.mod files needed for go versio)
 		for _, pkg := range ld.pkgs {
 			// We check pkg.mod.Path here instead of pkg.inStd because the
 			// pseudo-package "C" is not in std, but not provided by any module (and
@@ -1671,7 +1685,9 @@ func keepSums(ctx context.Context, ld *loader, rs *Requirements, which whichSums
 		if which == addBuildListZipSums {
 			for _, m := range mg.BuildList() {
 				r := resolveReplacement(m)
-				keep[modkey(r)] = true // we need the go version from the go.mod file to do anything useful with the zipfile
+				if keepModSumsForZipSums {
+					keep[modkey(r)] = true // we need the go version from the go.mod file to do anything useful with the zipfile
+				}
 				keep[r] = true
 			}
 		}
