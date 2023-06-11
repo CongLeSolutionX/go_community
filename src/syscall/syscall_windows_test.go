3fc03fa1e3fdc6bff9a857bf9b148edfc5d0db72
Qi Xiao <xiaq@google.com>
2023-06-11 23:29:26 +0100

    syscall: Fix Getwd on Windows to correctly handle long paths.
    Fixes #60051.
    
    Change-Id: Ia68ca0493912cb09d8c1d36a144bf0725842af1d

--- src/syscall/syscall_windows_test.go
+++ src/syscall/syscall_windows_test.go
@@ -180,6 +180,34 @@ int main(int argc, char *argv[])
 	}
 }
 
+func FuzzGetwd_Panic(f *testing.F) {
+	// Regression test for https://github.com/golang/go/issues/60051.
+
+	f.Add("foo")
+	// The length of a filename is also limited, so we can't reproduce the
+	// crash by creating a single directory with a very long name; we need two
+	// layers.
+	a200 := strings.Repeat("a", 200)
+	f.Add(filepath.Join(a200, a200))
+
+	f.Fuzz(func(t *testing.T, dirname string) {
+		dirname = filepath.Join(t.TempDir(), dirname)
+		err := os.MkdirAll(dirname, 0o700)
+		if err != nil {
+			t.Skipf("MkdirAll failed: %v", err)
+		}
+		err = os.Chdir(dirname)
+		if err != nil {
+			t.Skipf("Chdir failed: %v", err)
+		}
+		// Change out of the temporary directory so that we don't inhibit its
+		// removal during cleanup.
+		defer os.Chdir(`\`)
+
+		syscall.Getwd()
+	})
+}
+
 func FuzzUTF16FromString(f *testing.F) {
 	f.Add("hi")           // ASCII
 	f.Add("â")            // latin1
