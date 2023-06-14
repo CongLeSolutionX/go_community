75054280fcc09b69a54dfe1bed8a71818b7d1c46
Cosmos Nicolaou <cosmos.nicolaou@gmail.com>
2023-06-14 14:33:43 -0700

    runtime,runtime/pprof: get memory mappings on darwin.
    Displaying assembly language has never worked for Apple Silicon
    macs (see #50891). This change uses mach_vm_region to obtain the
    necessary VM mappings to allow for locating assembly instructions
    for a cpu profile.
    
    Fixes #50891
    
    Change-Id: Ib968c55a19b481b82f63337276b552f3b18f69d1

--- src/runtime/pprof/proto_windows.go
+++ src/runtime/pprof/proto_windows.go
@@ -42,10 +42,14 @@ func (b *profileBuilder) readMapping() {
 	}
 }
 
-func readMainModuleMapping() (start, end uint64, err error) {
+func readMainModuleMapping() (start, end uint64, exe string, err error) {
+	exe, err = os.Executable()
+	if err != nil {
+		return 0, 0, "", "", err
+	}
 	snap, err := createModuleSnapshot()
 	if err != nil {
-		return 0, 0, err
+		return 0, 0, "", "", err
 	}
 	defer func() { _ = syscall.CloseHandle(snap) }()
 
@@ -53,10 +57,10 @@ func readMainModuleMapping() (start, end uint64, err error) {
 	module.Size = uint32(windows.SizeofModuleEntry32)
 	err = windows.Module32First(snap, &module)
 	if err != nil {
-		return 0, 0, err
+		return 0, 0, "", "", err
 	}
 
-	return uint64(module.ModBaseAddr), uint64(module.ModBaseAddr) + uint64(module.ModBaseSize), nil
+	return uint64(module.ModBaseAddr), uint64(module.ModBaseAddr) + uint64(module.ModBaseSize), exe, peBuildID(exe), nil
 }
 
 func createModuleSnapshot() (syscall.Handle, error) {
