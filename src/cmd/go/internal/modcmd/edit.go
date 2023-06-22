73e1b3ca8ec59c2083a91da288012e42a665d180
Conrad Irwin <conrad.irwin@gmail.com>
2023-06-22 15:51:11 -0600

    cmd/mod/edit: add -tool and -droptool support
    For golang/go#48429
    
    Change-Id: I1a7bd8ffddbc65e3b687dc1d40f3853702e1b5dc

--- src/cmd/go/internal/modcmd/edit.go
+++ src/cmd/go/internal/modcmd/edit.go
@@ -73,8 +73,12 @@ retraction on the given version. The version may be a single version
 like "v1.2.3" or a closed interval like "[v1.1.0,v1.1.9]". Note that
 -retract=version is a no-op if that retraction already exists.
 
+The -tool=path and -droptool=path flags add and drop a tool declaration
+for the given path.
+
 The -require, -droprequire, -exclude, -dropexclude, -replace,
--dropreplace, -retract, and -dropretract editing flags may be repeated,
+-dropreplace, -retract, and -dropretract, -tool and -droptool
+editing flags may be repeated,
 and the changes are applied in the order given.
 
 The -go=version flag sets the expected Go language version.
@@ -124,6 +128,10 @@ writing it back to go.mod. The JSON output corresponds to these Go types:
 		Rationale string
 	}
 
+	type Tool struct {
+		Path string
+	}
+
 Retract entries representing a single version (not an interval) will have
 the "Low" and "High" fields set to the same value.
 
@@ -163,6 +171,8 @@ func init() {
 	cmdEdit.Flag.Var(flagFunc(flagDropExclude), "dropexclude", "")
 	cmdEdit.Flag.Var(flagFunc(flagRetract), "retract", "")
 	cmdEdit.Flag.Var(flagFunc(flagDropRetract), "dropretract", "")
+	cmdEdit.Flag.Var(flagFunc(flagTool), "tool", "")
+	cmdEdit.Flag.Var(flagFunc(flagDropTool), "droptool", "")
 
 	base.AddBuildFlagsNX(&cmdEdit.Flag)
 	base.AddChdirFlag(&cmdEdit.Flag)
@@ -310,6 +320,22 @@ func parsePath(flag, arg string) (path string) {
 		base.Fatalf("go: -%s=%s: invalid path: %v", flag, arg, err)
 	}
 	return path
+}
+
+// parsePath parses -flag=arg expecting arg to be path to a tool (allows ./)
+func parseToolPath(flag, arg string) (path string) {
+	if strings.Contains(arg, "@") {
+		base.Fatalf("go: -%s=%s: need just path, not path@version", flag, arg)
+	}
+	toCheck := arg
+	if strings.HasPrefix(arg, "./") {
+		toCheck = arg[2:]
+	}
+	if err := module.CheckImportPath(toCheck); err != nil {
+		base.Fatalf("go: -%s=%s: invalid path: %v", flag, arg, err)
+	}
+
+	return arg
 }
 
 // parsePathVersionOptional parses path[@version], using adj to
@@ -476,6 +502,24 @@ func flagDropRetract(arg string) {
 	})
 }
 
+func flagTool(arg string) {
+	path := parseToolPath("tool", arg)
+	edits = append(edits, func(f *modfile.File) {
+		if err := f.AddTool(path); err != nil {
+			base.Fatalf("go: -tool=%s: %v", arg, err)
+		}
+	})
+}
+
+func flagDropTool(arg string) {
+	path := parseToolPath("droptool", arg)
+	edits = append(edits, func(f *modfile.File) {
+		if err := f.DropTool(path); err != nil {
+			base.Fatalf("go: -droptool=%s: %v", arg, err)
+		}
+	})
+}
+
 // fileJSON is the -json output data structure.
 type fileJSON struct {
 	Module    editModuleJSON
@@ -485,6 +529,7 @@ type fileJSON struct {
 	Exclude   []module.Version
 	Replace   []replaceJSON
 	Retract   []retractJSON
+	Tool      []toolJSON
 }
 
 type editModuleJSON struct {
@@ -507,6 +552,10 @@ type retractJSON struct {
 	Low       string `json:",omitempty"`
 	High      string `json:",omitempty"`
 	Rationale string `json:",omitempty"`
+}
+
+type toolJSON struct {
+	Path string
 }
 
 // editPrintJSON prints the -json output.
@@ -535,6 +584,9 @@ func editPrintJSON(modFile *modfile.File) {
 	}
 	for _, r := range modFile.Retract {
 		f.Retract = append(f.Retract, retractJSON{r.Low, r.High, r.Rationale})
+	}
+	for _, t := range modFile.Tool {
+		f.Tool = append(f.Tool, toolJSON{t.Path})
 	}
 	data, err := json.MarshalIndent(&f, "", "\t")
 	if err != nil {
