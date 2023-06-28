e06454e0f3fc460ebee873ff4dc9afc1ee1de96f
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2023-06-29 00:41:32 +0700

    go/types, types2: add Sizes computation to match gc behavior
    Fixes #60431
    Fixes #60734
    Fixes #61035
    
    Change-Id: I82513da3e1714e8271fae220fe242bf2bfb4eb9d

--- src/go/types/sizes_test.go
+++ src/go/types/sizes_test.go
@@ -134,3 +134,69 @@ var s struct {
 		})
 	}
 }
+
+type gcSizeTest struct {
+	name string
+	src  string
+}
+
+var gcSizesTests = []gcSizeTest{
+	{
+		"issue60431",
+		`
+package main
+
+import "unsafe"
+
+// The foo struct size is expected to be rounded up to 16 bytes.
+type foo struct {
+	a int64
+	b bool
+}
+
+func main() {
+	var _ [unsafe.Sizeof(foo{}) - 16]byte
+       println(unsafe.Sizeof(foo{}))
+}`,
+	},
+	{
+		"issue60734",
+		`
+package main
+
+import (
+	"unsafe"
+)
+
+// The Data struct size is expected to be rounded up to 16 bytes.
+type Data struct {
+	Value  uint32   // 4 bytes
+	Label  [10]byte // 10 bytes
+	Active bool     // 1 byte
+	// padded with 1 byte to make it align
+}
+
+const (
+	dataSize = unsafe.Sizeof(Data{})
+	dataSizeLiteral = 16
+)
+
+func main() {
+	_ = [16]byte{0, 132, 95, 237, 80, 104, 111, 110, 101, 0, 0, 0, 0, 0, 1, 0}
+	_ = [dataSize]byte{0, 132, 95, 237, 80, 104, 111, 110, 101, 0, 0, 0, 0, 0, 1, 0}
+	_ = [dataSizeLiteral]byte{0, 132, 95, 237, 80, 104, 111, 110, 101, 0, 0, 0, 0, 0, 1, 0}
+}
+`,
+	},
+}
+
+func TestGCSizes(t *testing.T) {
+	for _, tc := range gcSizesTests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			conf := &types.Config{Importer: importer.Default(), Sizes: types.SizesFor("gc", "amd64")}
+			mustTypecheck(tc.src, conf, nil)
+		})
+	}
+}
