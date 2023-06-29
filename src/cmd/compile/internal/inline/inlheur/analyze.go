fdd06eb0f042b095d6dade632ee4aa396bd09eda
Than McIntosh <thanm@google.com>
2023-06-29 15:37:26 -0400

    [dev.inline] cmd/compile/internal/inline: add framework to compute function "properties"
    Add some machinery to support computing function "properties" for use
    in driving inlining heuristics, and a unit testing framework to check
    to see if the property computations are correct for a given set of
    canned Go source files. This CL is mainly the analysis skeleton and a
    testing framework; the code to compute the actual props will arrive in
    a later patch.
    
    Change-Id: I7970b64f713d17d7fdd7e8e9ccc7d9b0490571bf

---
+++ src/cmd/compile/internal/inline/inlheur/analyze.go
@@ -0,0 +1,107 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package inlheur
+
+import (
+	"cmd/compile/internal/base"
+	"cmd/compile/internal/ir"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+const debugTrace = 0
+
+// fnInlHeur contains inline heuristics state information about
+// a specific Go function being analyzed/considered by the inliner.
+type fnInlHeur struct {
+	fname string
+	file  string
+	line  uint
+	props *FuncProps
+}
+
+// computeFuncProps examines the Go function 'fn' and computes for it
+// a function "properties" object, to be used to drive inlining
+// heuristics. See comments on the FuncProps type for more info.
+func computeFuncProps(fn *ir.Func) *FuncProps {
+	if debugTrace != 0 {
+		fmt.Fprintf(os.Stderr, "=-= starting analysis of func %v:\n%+v\n",
+			fn.Sym().Name, fn)
+	}
+	// implementation stubbed out for now
+	return &FuncProps{}
+}
+
+func fnFileLine(fn *ir.Func) (string, uint) {
+	p := base.Ctxt.InnermostPos(fn.Pos())
+	return filepath.Base(p.Filename()), p.Line()
+}
+
+// DumpFuncProps computes and caches function properties for the func
+// 'fn', or if fn is nil, writes out the cached set of properties to
+// the file given in 'dumpfile'. Used for the "-d=dumpinlfuncprops=..."
+// command line flag, intended for use primarily in unit testing.
+func DumpFuncProps(fn *ir.Func, dumpfile string) {
+	if fn != nil {
+		fp := computeFuncProps(fn)
+		file, line := fnFileLine(fn)
+		entry := fnInlHeur{
+			fname: fn.Sym().Name,
+			file:  file,
+			line:  line,
+			props: fp,
+		}
+		dumpBuffer = append(dumpBuffer, entry)
+		return
+	}
+	outf, err := os.OpenFile(dumpfile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		base.Fatalf("opening function props dump file %q: %v\n", dumpfile, err)
+	}
+	defer outf.Close()
+	dumpFilePreamble(outf)
+	for _, entry := range dumpBuffer {
+		if err := dumpFnPreamble(outf, &entry); err != nil {
+			base.Fatalf("function props dump: %v\n", err)
+		}
+	}
+	dumpBuffer = nil
+}
+
+// dumpFilePreamble writes out a file-level preamble for a given
+// Go function as part of a function properties dump.
+func dumpFilePreamble(w io.Writer) {
+	fmt.Fprintf(w, "// DO NOT EDIT (use 'go test -v -update-expected' instead.)\n")
+	fmt.Fprintf(w, "// See cmd/compile/internal/inline/inlheur/testdata/props/README.txt\n")
+	fmt.Fprintf(w, "// for more information on the format of this file.\n")
+	fmt.Fprintf(w, "// %s\n", preambleDelimiter)
+}
+
+// dumpFilePreamble writes out a function-level preamble for a given
+// Go function as part of a function properties dump.
+func dumpFnPreamble(w io.Writer, fih *fnInlHeur) error {
+	fmt.Fprintf(w, "// %s %s %d\n", fih.file, fih.fname, fih.line)
+	// emit props as comments, followed by delimiter
+	fmt.Fprintf(w, "%s// %s\n", fih.props.ToString("// "), comDelimiter)
+	data, err := json.Marshal(fih.props)
+	if err != nil {
+		return fmt.Errorf("marshall error %v\n", err)
+	}
+	fmt.Fprintf(w, "// %s\n// %s\n", string(data), fnDelimiter)
+	return nil
+}
+
+// delimiters written to various preambles to make parsing of
+// dumps easier.
+const preambleDelimiter = "<endfilepreamble>"
+const fnDelimiter = "<endfuncpreamble>"
+const comDelimiter = "<endpropsdump>"
+
+// dumpBuffer stores up function properties dumps when
+// "-d=dumpinlfuncprops=..." is in effect.
+var dumpBuffer []fnInlHeur
---
+++ src/cmd/compile/internal/inline/inlheur/testdata/props/stub.go
@@ -0,0 +1,31 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// DO NOT EDIT (use 'go test -v -update-expected' instead.)
+// See cmd/compile/internal/inline/inlheur/testdata/props/README.txt
+// for more information on the format of this file.
+// <endfilepreamble>
+
+package stub
+
+var M = map[string]int{}
+
+// stub.go T_stub 18
+// <endpropsdump>
+// {"Flags":0,"RecvrParamFlags":null,"ReturnFlags":null}
+// <endfuncpreamble>
+func T_stub() {
+}
+
+func ThisFunctionShouldBeIgnored(x int) {
+	println(x)
+}
+
+// stub.go init.0 29
+// <endpropsdump>
+// {"Flags":0,"RecvrParamFlags":null,"ReturnFlags":null}
+// <endfuncpreamble>
+func init() {
+	ThisFunctionShouldBeIgnored(1)
+}
