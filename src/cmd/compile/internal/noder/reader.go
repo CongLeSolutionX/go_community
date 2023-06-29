f725be270cf28a683d3f5dbe152bc059fd3fdbb1
Than McIntosh <thanm@google.com>
2023-06-29 13:22:26 -0400

    cmd/compile: write "properties" to export data for inlinable funcs
    Augment the ir.Inline container to include an entry for function
    properties (currently serialized as a string), and if
    GOEXPERIMENT=newinliner is set, compute and store function
    properties for all inline candidates processed by the inliner.
    
    The idea here is that if the function properties are going to drive
    inlining decisions, we'd like to have the same info from non-local /
    imported functions as for local / in-package functions, hence we need
    to include the properties in the export data.
    
    Hand testing on the compiler itself and with k8s kubelet shows that
    this increases the size of export data overall by about 2-3 percent,
    so a pretty modest increase.
    
    Updates #61502.
    
    Change-Id: I9d1c311aa8418d02ffea3629c3dd9d8076886d15

--- src/cmd/compile/internal/noder/reader.go
+++ src/cmd/compile/internal/noder/reader.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"go/constant"
 	"internal/buildcfg"
+	"internal/goexperiment"
 	"internal/pkgbits"
 	"path/filepath"
 	"strings"
@@ -1119,6 +1120,9 @@ func (r *reader) funcExt(name *ir.Name, method *types.Sym) {
 			fn.Inl = &ir.Inline{
 				Cost:            int32(r.Len()),
 				CanDelayResults: r.Bool(),
+			}
+			if goexperiment.NewInliner {
+				fn.Inl.Properties = r.String()
 			}
 		}
 	} else {
