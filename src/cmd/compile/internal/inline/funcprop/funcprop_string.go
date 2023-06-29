483d5f3e4764f84ad5865fd2ce1e852447e79e22
Than McIntosh <thanm@google.com>
2023-06-29 13:43:50 -0400

    [dev.inline] cmd/compile: function "property" defs for inl heuristics
    Add definitions for a set of Go function "properties" intended to be
    useful for driving inlining decisions. This CL just defines a set of
    flags and a container to hold them; a subsequent CL will add code to
    compute the properties for a function given its IR/AST representation.
    
    Change-Id: Ifa26c1ad055c02ca0ce9cf37078cee7b3385e18a

---
+++ src/cmd/compile/internal/inline/funcprop/funcprop_string.go
@@ -0,0 +1,57 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package funcprop
+
+import (
+	"fmt"
+	"strings"
+)
+
+// TODO: convert the following two functions to a single generic
+// function when bootstrap go compiler supports generics.
+
+func paramFlagSliceToSB(sb *strings.Builder, sl []ParamPropBits, prefix string) {
+	var sb2 strings.Builder
+	foundnz := false
+	fmt.Fprintf(&sb2, "%sRecvrParamFlags:\n", prefix)
+	for i, e := range sl {
+		if e != 0 {
+			foundnz = true
+		}
+		fmt.Fprintf(&sb2, "%s  %d: %s\n", prefix, i, e.String())
+	}
+	if foundnz {
+		sb.WriteString(sb2.String())
+	}
+}
+
+func returnFlagSliceToSB(sb *strings.Builder, sl []ReturnPropBits, prefix string) {
+	var sb2 strings.Builder
+	foundnz := false
+	fmt.Fprintf(&sb2, "%sReturnFlags:\n", prefix)
+	for i, e := range sl {
+		if e != 0 {
+			foundnz = true
+		}
+		fmt.Fprintf(&sb2, "%s  %d: %s\n", prefix, i, e.String())
+	}
+	if foundnz {
+		sb.WriteString(sb2.String())
+	}
+}
+
+func (fp *FuncProps) String() string {
+	return fp.ToString("")
+}
+
+func (fp *FuncProps) ToString(prefix string) string {
+	var sb strings.Builder
+	if fp.Flags != 0 {
+		fmt.Fprintf(&sb, "%sFlags: %s\n", prefix, fp.Flags)
+	}
+	paramFlagSliceToSB(&sb, fp.RecvrParamFlags, prefix)
+	returnFlagSliceToSB(&sb, fp.ReturnFlags, prefix)
+	return sb.String()
+}
---
+++ src/cmd/compile/internal/inline/funcprop/serialize.go
@@ -0,0 +1,79 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package funcprop
+
+func (fp *FuncProps) SerializeToString() string {
+	if fp == nil {
+		return ""
+	}
+	sl := make([]byte, 0, 256)
+	sl = writeUleb128(sl, uint64(fp.Flags))
+	sl = writeUleb128(sl, uint64(len(fp.RecvrParamFlags)))
+	for _, pf := range fp.RecvrParamFlags {
+		sl = writeUleb128(sl, uint64(pf))
+	}
+	sl = writeUleb128(sl, uint64(len(fp.ReturnFlags)))
+	for _, rf := range fp.ReturnFlags {
+		sl = writeUleb128(sl, uint64(rf))
+	}
+	return string(sl)
+}
+
+func DeserializeFromString(s string) *FuncProps {
+	if len(s) == 0 {
+		return nil
+	}
+	var fp FuncProps
+	var v uint64
+	sl := []byte(s)
+	v, sl = readULEB128(sl)
+	fp.Flags = FuncPropBits(v)
+	v, sl = readULEB128(sl)
+	fp.RecvrParamFlags = make([]ParamPropBits, v)
+	for i := range fp.RecvrParamFlags {
+		v, sl = readULEB128(sl)
+		fp.RecvrParamFlags[i] = ParamPropBits(v)
+	}
+	v, sl = readULEB128(sl)
+	fp.ReturnFlags = make([]ReturnPropBits, v)
+	for i := range fp.ReturnFlags {
+		v, sl = readULEB128(sl)
+		fp.ReturnFlags[i] = ReturnPropBits(v)
+	}
+	return &fp
+}
+
+func readULEB128(sl []byte) (value uint64, rsl []byte) {
+	var shift uint
+
+	for {
+		b := sl[0]
+		sl = sl[1:]
+		value |= (uint64(b&0x7F) << shift)
+		if b&0x80 == 0 {
+			break
+		}
+		shift += 7
+	}
+	return value, sl
+}
+
+func writeUleb128(sl []byte, v uint64) []byte {
+	if v < 128 {
+		sl = append(sl, uint8(v))
+		return sl
+	}
+	more := true
+	for more {
+		c := uint8(v & 0x7f)
+		v >>= 7
+		more = v != 0
+		if more {
+			c |= 0x80
+		}
+		sl = append(sl, c)
+	}
+	return sl
+}
