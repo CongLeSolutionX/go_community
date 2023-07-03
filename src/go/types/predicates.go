ea8915c419f0f9c82717340893dd93a43456e52d
cui fliter <imcusg@gmail.com>
2023-07-03 13:41:45 +0800

    all: fix function names in comments
    Change-Id: Iba8878420c59d705066d1d9955e91a5c2eb4faf5

--- src/go/types/predicates.go
+++ src/go/types/predicates.go
@@ -467,7 +467,7 @@ func (c *comparer) identical(x, y Type, p *ifacePair) bool {
 	return false
 }
 
-// identicalOrigin reports whether x and y originated in the same declaration.
+// indenticalOrigin reports whether x and y originated in the same declaration.
 func indenticalOrigin(x, y *Named) bool {
 	// TODO(gri) is this correct?
 	return x.Origin().obj == y.Origin().obj
