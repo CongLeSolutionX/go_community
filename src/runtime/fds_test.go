7d48e9cae52e9a97b77a13b76232fa7cf5108c97
Roland Shoemaker <roland@golang.org>
2023-07-12 14:01:26 -0700

    runtime: enforce standard file descriptors open on init on unix
    On Unix-like platforms, enforce that the standard file descriptions (0,
    1, 2) are always open during initialization. If any of the FDs are
    closed, we open them pointing at /dev/null, or fail.
    
    Fixes #60641
    
    Change-Id: Iaab6b3f3e5ca44006ae3ba3544d47da9a613f58f

---
+++ src/runtime/fds_test.go
@@ -0,0 +1,63 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build unix
+
+package runtime_test
+
+import (
+	"internal/testenv"
+	"os"
+	"testing"
+)
+
+func TestCheckFDs(t *testing.T) {
+	if *flagQuick {
+		t.Skip("-quick")
+	}
+
+	testenv.MustHaveGoBuild(t)
+
+	fdsBin, err := buildTestProg(t, "testfds")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	i, err := os.CreateTemp(t.TempDir(), "fds-input")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, err := i.Write([]byte("stdin")); err != nil {
+		t.Fatal(err)
+	}
+
+	o, err := os.CreateTemp(t.TempDir(), "fds-output")
+	if err != nil {
+		t.Fatal(err)
+	}
+	outputPath := o.Name()
+
+	proc, err := os.StartProcess(fdsBin, []string{fdsBin}, &os.ProcAttr{
+		Env:   []string{"TEST_OUTPUT=" + outputPath},
+		Files: []*os.File{},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	ps, err := proc.Wait()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ps.ExitCode() != 0 {
+		t.Fatalf("testfds failed: %d", ps.ExitCode())
+	}
+
+	fc, err := os.ReadFile(outputPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(fc) != "" {
+		t.Errorf("unexpected file content, got: %q", string(fc))
+	}
+}
---
+++ src/runtime/fds_unix.go
@@ -0,0 +1,36 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build unix
+
+package runtime
+
+func checkFDs() {
+	const (
+		// F_GETFD and EBADF are standard across all unixes, define
+		// them here rather than in each of the OS specific files
+		F_GETFD = 0x01
+		EBADF   = 0x09
+	)
+
+	devNull := []byte("/dev/null\x00")
+	for i := 0; i < 3; i++ {
+		ret, errno := fcntl(int32(i), F_GETFD, 0)
+		if ret >= 0 {
+			continue
+		}
+		if errno != EBADF {
+			print("runtime: unexpected error while checking standard file descriptor ", i, ", errno=", errno, "\n")
+			throw("cannot open standard fds")
+		}
+
+		if ret := open(&devNull[0], 2 /* O_RDWR */, 0); ret < 0 {
+			print("runtime: standard file descriptor ", i, " closed, unable to open /dev/null, errno=", errno, "\n")
+			throw("cannot open standard fds")
+		} else if ret != int32(i) {
+			print("runtime: opened unexpected file descriptor ", ret, " when attempting to open ", i, "\n")
+			throw("cannot open standard fds")
+		}
+	}
+}
