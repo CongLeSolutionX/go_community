dbc9a2875c05e227f02f8603ebe9f28e346a5330
Than McIntosh <thanm@google.com>
2023-07-18 11:45:42 -0400

    cmd/compile/internal/inline: add call site flag generation
    Add code to detect call sites that are nested in loops, call sites
    that are on an unconditional path to panic/exit, and call sites within
    "init" functions. The panic-path processing reuses some of the
    logic+state already present for the function flag version of "calls
    panic/exit".
    
    Updates #61502.
    
    Change-Id: I1d728e0763282d3616a9cbc0a07c5cda115660f3

--- src/cmd/compile/internal/inline/inlheur/testdata/props/funcflags.go
+++ src/cmd/compile/internal/inline/inlheur/testdata/props/funcflags.go
@@ -160,8 +160,7 @@ func T_recov(x int) {
 	}
 }
 
-
-// funcflags.go T_forloops1 170 0 1
+// funcflags.go T_forloops1 169 0 1
 // Flags FuncPropNeverReturns
 // <endpropsdump>
 // {"Flags":1,"ParamFlags":[0],"ResultFlags":[]}
@@ -173,7 +172,7 @@ func T_forloops1(x int) {
 	}
 }
 
-// funcflags.go T_forloops2 181 0 1
+// funcflags.go T_forloops2 180 0 1
 // <endpropsdump>
 // {"Flags":0,"ParamFlags":[0],"ResultFlags":[]}
 // <endcallsites>
@@ -188,8 +187,7 @@ func T_forloops2(x int) {
 	}
 }
 
-
-// funcflags.go T_forloops3 197 0 1
+// funcflags.go T_forloops3 195 0 1
 // <endpropsdump>
 // {"Flags":0,"ParamFlags":[0],"ResultFlags":[]}
 // <endcallsites>
@@ -209,8 +207,7 @@ func T_forloops3(x int) {
 	panic("whatev")
 }
 
-
-// funcflags.go T_hasgotos 218 0 1
+// funcflags.go T_hasgotos 215 0 1
 // <endpropsdump>
 // {"Flags":0,"ParamFlags":[0,0],"ResultFlags":[]}
 // <endcallsites>
@@ -238,7 +235,7 @@ func T_hasgotos(x int, y int) {
 	}
 }
 
-// funcflags.go T_callsexit 249 0 1
+// funcflags.go T_callsexit 246 0 1
 // Flags FuncPropNeverReturns
 // ParamFlags
 //   0 ParamFeedsIfOrSwitch
@@ -253,10 +250,10 @@ func T_callsexit(x int) {
 	os.Exit(2)
 }
 
-// funcflags.go T_exitinexpr 262 0 1
+// funcflags.go T_exitinexpr 259 0 1
 // <endpropsdump>
 // {"Flags":0,"ParamFlags":[0],"ResultFlags":[]}
-// callsite: funcflags.go:267:18|0 flagstr "" flagval 0
+// callsite: funcflags.go:264:18|0 flagstr "CallSiteOnPanicPath" flagval 2
 // <endcallsites>
 // <endfuncpreamble>
 func T_exitinexpr(x int) {
@@ -269,7 +266,7 @@ func T_exitinexpr(x int) {
 	}
 }
 
-// funcflags.go T_select_noreturn 278 0 1
+// funcflags.go T_select_noreturn 275 0 1
 // Flags FuncPropNeverReturns
 // <endpropsdump>
 // {"Flags":1,"ParamFlags":[0,0,0],"ResultFlags":[]}
@@ -287,7 +284,7 @@ func T_select_noreturn(chi chan int, chf chan float32, p *int) {
 	panic("bad")
 }
 
-// funcflags.go T_select_mayreturn 295 0 1
+// funcflags.go T_select_mayreturn 292 0 1
 // <endpropsdump>
 // {"Flags":0,"ParamFlags":[0,0,0],"ResultFlags":[0]}
 // <endcallsites>
@@ -305,11 +302,11 @@ func T_select_mayreturn(chi chan int, chf chan float32, p *int) int {
 	panic("bad")
 }
 
-// funcflags.go T_calls_callsexit 315 0 1
+// funcflags.go T_calls_callsexit 312 0 1
 // Flags FuncPropNeverReturns
 // <endpropsdump>
 // {"Flags":1,"ParamFlags":[0],"ResultFlags":[]}
-// callsite: funcflags.go:316:15|0 flagstr "" flagval 0
+// callsite: funcflags.go:313:15|0 flagstr "CallSiteOnPanicPath" flagval 2
 // <endcallsites>
 // <endfuncpreamble>
 func T_calls_callsexit(x int) {
