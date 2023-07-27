f8288d7fe3adebef7a035b0f68aaa838f792c24f
Matthew Dempsky <mdempsky@google.com>
2023-07-27 16:20:36 -0700

    runtime: simplify defer processing
    DO NOT REVIEW
    DO NOT SUBMIT
    
    This CL refactors gopanic, Goexit, and deferreturn to share a single
    "runDefers" routine. The resulting code is simpler, and overall does
    less work. It should also make it easier to implement further
    improvements involving cmd/compile.
    
    Change-Id: I71d3cc8878a6f951d8633505424a191536c8e6b3

--- src/runtime/runtime2.go
+++ src/runtime/runtime2.go
@@ -999,29 +999,18 @@ func extendRandom(r []byte, n int) {
 // initialize them are not required. All defers must be manually scanned,
 // and for heap defers, marked.
 type _defer struct {
-	started bool
-	heap    bool
-	// openDefer indicates that this _defer is for a frame with open-coded
-	// defers. We have only one defer record for the entire frame (which may
-	// currently have 0, 1, or more defers active).
-	openDefer bool
-	sp        uintptr // sp at time of defer
-	pc        uintptr // pc at time of defer
-	fn        func()  // can be nil for open-coded defers
-	_panic    *_panic // panic that is running defer
-	link      *_defer // next defer on G; can point to either heap or stack!
-
-	// If openDefer is true, the fields below record values about the stack
-	// frame and associated function that has the open-coded defer(s). sp
-	// above will be the sp for the frame, and pc will be address of the
-	// deferreturn call in the function.
-	fd   unsafe.Pointer // funcdata for the function associated with the frame
-	varp uintptr        // value of varp for the stack frame
-	// framepc is the current pc associated with the stack frame. Together,
-	// with sp above (which is the sp associated with the stack frame),
-	// framepc/sp can be used as pc/sp pair to continue a stack trace via
-	// gentraceback().
-	framepc uintptr
+	// TODO(mdempsky): Remove blank fields and update cmd/compile.
+	_    bool // was started
+	heap bool
+	_    bool           // was openDefer
+	sp   uintptr        // sp at time of defer
+	pc   uintptr        // pc at time of defer
+	fn   func()         // can be nil for open-coded defers
+	_    unsafe.Pointer // was _panic
+	link *_defer        // next defer on G; can point to either heap or stack!
+	_    unsafe.Pointer // was fd
+	_    uintptr        // was varp
+	_    uintptr        // was framepc
 }
 
 // A _panic holds information about an active panic.
@@ -1039,7 +1028,6 @@ type _panic struct {
 	pc        uintptr        // where to return to in runtime if this panic is bypassed
 	sp        unsafe.Pointer // where to return to in runtime if this panic is bypassed
 	recovered bool           // whether this panic is over
-	aborted   bool           // the panic was aborted
 	goexit    bool
 }
 
