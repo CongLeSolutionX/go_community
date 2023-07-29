ff746b1a3be559f8ac4ebbb5c64f2eceaded4687
Ian Lance Taylor <iant@golang.org>
2023-07-29 08:59:20 -0700

    sync: panic rather than throw on nil *Pool
    Fixes #61651
    
    Change-Id: I27d581719e6bf38910f9d47dcf023bbff74ddf72

--- src/sync/pool.go
+++ src/sync/pool.go
@@ -196,6 +196,11 @@ func (p *Pool) getSlow(pid int) any {
 // returns poolLocal pool for the P and the P's id.
 // Caller must call runtime_procUnpin() when done with the pool.
 func (p *Pool) pin() (*poolLocal, int) {
+	// Check whether p is nil to get a typical panic.
+	// Otherwise the nil dereference is in runtime_LoadAcquintptr,
+	// where it is a fatal error rather than a panic.
+	_ = p.localSize
+
 	pid := runtime_procPin()
 	// In pinSlow we store to local and then to localSize, here we load in opposite order.
 	// Since we've disabled preemption, GC cannot happen in between.
