d5b0b04091499e348ae3b4e1f08e2a5c9e18c206
Andy Pan <panjf2000@gmail.com>
2023-07-30 11:42:14 +0800

    log/slog: print nil values instead of panic
    Fixes #61648
    
    Change-Id: I6b7f4948ca89142a358d74624607daf42ea8b304

--- src/log/slog/handler.go
+++ src/log/slog/handler.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"io"
 	"log/slog/internal/buffer"
+	"reflect"
 	"slices"
 	"strconv"
 	"sync"
@@ -511,7 +512,26 @@ func (s *handleState) appendString(str string) {
 	}
 }
 
+const nilAngleString = "<nil>"
+
 func (s *handleState) appendValue(v Value) {
+	defer func() {
+		if r := recover(); r != nil {
+			// If it panics with a nil pointer, the most likely cases are
+			// an encoding.TextMarshaler or error fails to guard against nil,
+			// in which case "<nil>" seems to be the feasible choice.
+			//
+			// Adapted from the code in fmt/print.go.
+			if v := reflect.ValueOf(v.any); v.Kind() == reflect.Pointer && v.IsNil() {
+				s.appendString(nilAngleString)
+				return
+			}
+
+			// Otherwise just print the original panic message.
+			s.appendString(fmt.Sprintf("!PANIC:%v", r))
+		}
+	}()
+
 	var err error
 	if s.h.json {
 		err = appendJSONValue(s, v)
