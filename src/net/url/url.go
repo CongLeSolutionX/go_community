205a3732d1dbb82f3be3893ac7e4c87dfd361e71
korzhao <korzhao95@gmail.com>
2023-07-31 21:28:00 +0800

    net/url: optimize QueryEscape and PathEscape
    Optimize shouldEscape using a 256 byte lookup table.
    
    Currently we have 7 encoding modes.
    So we use 1-7 bits of a byte to indicate whether it needs to be escaped.
    
    Updates #17860
    benchmark                       old ns/op     new ns/op     delta
    BenchmarkQueryEscape/#00-10     34.2          23.1          -32.44%
    BenchmarkQueryEscape/#01-10     207           68.1          -67.07%
    BenchmarkQueryEscape/#02-10     115           53.5          -53.63%
    BenchmarkQueryEscape/#03-10     252           102           -59.38%
    BenchmarkQueryEscape/#04-10     1555          658           -57.67%
    BenchmarkPathEscape/#00-10      52.5          30.8          -41.39%
    BenchmarkPathEscape/#01-10      206           69.2          -66.30%
    BenchmarkPathEscape/#02-10      115           53.4          -53.65%
    BenchmarkPathEscape/#03-10      251           102           -59.58%
    BenchmarkPathEscape/#04-10      1433          582           -59.41%
    
    Change-Id: Ia0db52a70466f3b374eff765f475b90c85795b50

--- src/net/url/url.go
+++ src/net/url/url.go
@@ -93,86 +93,30 @@ func (e InvalidHostError) Error() string {
 	return "invalid character " + strconv.Quote(string(e)) + " in host name"
 }
 
+const escapeMap = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" +
+	"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" +
+	"\x00\x98\x18\x00\xbe\x00\xbe\x18\x98\x98\x98\xbe\xba\xfe\xfe\x82" +
+	"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x9e\xba\x18\xbe\x18\x80" +
+	"\x86\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff" +
+	"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x18\x00\x18\x00\xfe" +
+	"\x00\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff" +
+	"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x00\x00\x00\xfe\x00" +
+	"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" +
+	"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" +
+	"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" +
+	"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" +
+	"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" +
+	"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" +
+	"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" +
+	"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+
 // Return true if the specified character should be escaped when
 // appearing in a URL string, according to RFC 3986.
 //
 // Please be informed that for now shouldEscape does not check all
 // reserved characters correctly. See golang.org/issue/5684.
 func shouldEscape(c byte, mode encoding) bool {
-	// §2.3 Unreserved characters (alphanum)
-	if 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z' || '0' <= c && c <= '9' {
-		return false
-	}
-
-	if mode == encodeHost || mode == encodeZone {
-		// §3.2.2 Host allows
-		//	sub-delims = "!" / "$" / "&" / "'" / "(" / ")" / "*" / "+" / "," / ";" / "="
-		// as part of reg-name.
-		// We add : because we include :port as part of host.
-		// We add [ ] because we include [ipv6]:port as part of host.
-		// We add < > because they're the only characters left that
-		// we could possibly allow, and Parse will reject them if we
-		// escape them (because hosts can't use %-encoding for
-		// ASCII bytes).
-		switch c {
-		case '!', '$', '&', '\'', '(', ')', '*', '+', ',', ';', '=', ':', '[', ']', '<', '>', '"':
-			return false
-		}
-	}
-
-	switch c {
-	case '-', '_', '.', '~': // §2.3 Unreserved characters (mark)
-		return false
-
-	case '$', '&', '+', ',', '/', ':', ';', '=', '?', '@': // §2.2 Reserved characters (reserved)
-		// Different sections of the URL allow a few of
-		// the reserved characters to appear unescaped.
-		switch mode {
-		case encodePath: // §3.3
-			// The RFC allows : @ & = + $ but saves / ; , for assigning
-			// meaning to individual path segments. This package
-			// only manipulates the path as a whole, so we allow those
-			// last three as well. That leaves only ? to escape.
-			return c == '?'
-
-		case encodePathSegment: // §3.3
-			// The RFC allows : @ & = + $ but saves / ; , for assigning
-			// meaning to individual path segments.
-			return c == '/' || c == ';' || c == ',' || c == '?'
-
-		case encodeUserPassword: // §3.2.1
-			// The RFC allows ';', ':', '&', '=', '+', '$', and ',' in
-			// userinfo, so we must escape only '@', '/', and '?'.
-			// The parsing of userinfo treats ':' as special so we must escape
-			// that too.
-			return c == '@' || c == '/' || c == '?' || c == ':'
-
-		case encodeQueryComponent: // §3.4
-			// The RFC reserves (so we must escape) everything.
-			return true
-
-		case encodeFragment: // §4.1
-			// The RFC text is silent but the grammar allows
-			// everything, so escape nothing.
-			return false
-		}
-	}
-
-	if mode == encodeFragment {
-		// RFC 3986 §2.2 allows not escaping sub-delims. A subset of sub-delims are
-		// included in reserved from RFC 2396 §2.2. The remaining sub-delims do not
-		// need to be escaped. To minimize potential breakage, we apply two restrictions:
-		// (1) we always escape sub-delims outside of the fragment, and (2) we always
-		// escape single quote to avoid breaking callers that had previously assumed that
-		// single quotes would be escaped. See issue #19917.
-		switch c {
-		case '!', '(', ')', '*':
-			return false
-		}
-	}
-
-	// Everything else must be escaped.
-	return true
+	return (escapeMap[int(c)]>>mode)&1 == 0
 }
 
 // QueryUnescape does the inverse transformation of QueryEscape,
@@ -319,21 +263,29 @@ func escape(s string, mode encoding) string {
 		return string(t)
 	}
 
-	j := 0
-	for i := 0; i < len(s); i++ {
-		switch c := s[i]; {
-		case c == ' ' && mode == encodeQueryComponent:
-			t[j] = '+'
-			j++
-		case shouldEscape(c, mode):
-			t[j] = '%'
-			t[j+1] = upperhex[c>>4]
-			t[j+2] = upperhex[c&15]
-			j += 3
-		default:
-			t[j] = s[i]
-			j++
+	var i, j, nCopy = 0, 0, 0
+	for i = 0; i < len(s); i++ {
+		if c := s[i]; shouldEscape(c, mode) {
+			if nCopy != 0 {
+				copy(t[j:j+nCopy], s[i-nCopy:i])
+				j += nCopy
+				nCopy = 0
+			}
+			if c == ' ' && mode == encodeQueryComponent {
+				t[j] = '+'
+				j++
+			} else {
+				t[j] = '%'
+				t[j+1] = upperhex[c>>4]
+				t[j+2] = upperhex[c&15]
+				j += 3
+			}
+		} else {
+			nCopy++
 		}
+	}
+	if nCopy != 0 {
+		copy(t[j:j+nCopy], s[i-nCopy:i])
 	}
 	return string(t)
 }
