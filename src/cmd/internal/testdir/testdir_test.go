025bbe32b91e0f0ef1572b0f023ea86cde95666e
Bryan C. Mills <bcmills@google.com>
2023-08-24 13:16:48 -0400

    cmd/internal/testdir: parse past gofmt'd //go:build lines
    Also gofmt a test file to make sure the parser works.
    
    Fixes #62267.
    
    Change-Id: I9b9f12b06bae7df626231000879b5ed7df3cd9ba

--- src/cmd/internal/testdir/testdir_test.go
+++ src/cmd/internal/testdir/testdir_test.go
@@ -478,15 +478,18 @@ func (t test) run() error {
 	src := string(srcBytes)
 
 	// Execution recipe stops at first blank line.
-	action, _, ok := strings.Cut(src, "\n\n")
-	if !ok {
-		t.Fatalf("double newline ending execution recipe not found in GOROOT/test/%s", t.goFileName())
+	var action string
+	for actionSrc := src; action == "" && actionSrc != ""; {
+		var line string
+		line, actionSrc, _ = strings.Cut(actionSrc, "\n")
+		if constraint.IsGoBuild(line) || constraint.IsPlusBuild(line) {
+			continue
+		}
+		action = strings.TrimSpace(strings.TrimPrefix(line, "//"))
 	}
-	if firstLine, rest, ok := strings.Cut(action, "\n"); ok && strings.Contains(firstLine, "+build") {
-		// skip first line
-		action = rest
+	if action == "" {
+		t.Fatalf("execution recipe not found in GOROOT/test/%s", t.goFileName())
 	}
-	action = strings.TrimPrefix(action, "//")
 
 	// Check for build constraints only up to the actual code.
 	header, _, ok := strings.Cut(src, "\npackage")
