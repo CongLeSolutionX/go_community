445138656a462ffe57dc7332e1cea1d64d57a318
Nikita Vaniasin <nikita.vanyasin@gmail.com>
2023-08-28 15:26:32 +0200

    net: respect `hosts` file when resolving names for Windows
    Change-Id: I896dae8e5905ae98539ab83c9379fd1c9886d44a

--- src/net/hook_windows.go
+++ src/net/hook_windows.go
@@ -6,6 +6,7 @@ package net
 
 import (
 	"internal/syscall/windows"
+	"os"
 	"syscall"
 	"time"
 )
@@ -13,8 +14,18 @@ import (
 var (
 	testHookDialChannel = func() { time.Sleep(time.Millisecond) } // see golang.org/issue/5349
 
+	testHookHostsPath = systemDirPath() + "/Drivers/etc/hosts"
+
 	// Placeholders for socket system calls.
 	wsaSocketFunc func(int32, int32, int32, *syscall.WSAProtocolInfo, uint32, uint32) (syscall.Handle, error) = windows.WSASocket
 	connectFunc   func(syscall.Handle, syscall.Sockaddr) error                                                = syscall.Connect
 	listenFunc    func(syscall.Handle, int) error                                                             = syscall.Listen
 )
+
+func systemDirPath() string {
+	dir, err := windows.GetSystemDirectory()
+	if err != nil {
+		dir = os.Getenv("SystemRoot") + "/System32"
+	}
+	return dir
+}
