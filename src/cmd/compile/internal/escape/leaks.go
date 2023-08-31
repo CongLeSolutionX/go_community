57d50aecd8cb983a14681068c8c00339bdc226bf
thepudds <thepudds1460@gmail.com>
2023-08-31 14:22:46 -0400

    cmd/compile/internal/escape: reserve space in leaks encoding for interface receiver data
    WIP. Feedback welcome.
    
    In preparation for analyzing receivers in interface method calls,
    update the leaks encoding.
    
    The leaks zeroth element still communicates information about
    flows to the heap, but the next element now communicates
    information about use as an interface receiver, which might
    or might not eventually flow to the heap.
    
    While doing so, we also change the magic prefix to purposefully
    break any readers that we do not know about. A future CL might change
    it back to the prior esc: prefix.
    
    TODO: consider keeping the esc: prefix unchanged in this CL.
    
    Updates #8618
    
    Change-Id: Ie3131f41466cdd6df8e6a30399ac5947e5ea4ee1

--- src/cmd/compile/internal/escape/leaks.go
+++ src/cmd/compile/internal/escape/leaks.go
@@ -12,10 +12,12 @@ import (
 
 const numEscResults = 7
 
-// An leaks represents a set of assignment flows from a parameter
-// to the heap or to any of its function's (first numEscResults)
-// result parameters.
-type leaks [1 + numEscResults]uint8
+// A leaks represents a set of assignment flows from a parameter:
+//   - to the heap (the leaks zeroth element), or
+//   - potentially to the heap due to use as an interface receiver (the
+//     leaks first element), or
+//   - to any of its function's first numEscResults result parameters.
+type leaks [2 + numEscResults]uint8
 
 // Empty reports whether l is an empty set (i.e., no assignment flows).
 func (l leaks) Empty() bool { return l == leaks{} }
@@ -24,19 +26,30 @@ func (l leaks) Empty() bool { return l == leaks{} }
 // to the heap. If no such flows exist, Heap returns -1.
 func (l leaks) Heap() int { return l.get(0) }
 
+// IfaceRecv returns the minimum deref count of any assignment flow from l
+// to use as an interface receiver in a call. If no such flows exist,
+// IfaceRecv returns -1.
+// TODO: we could use boolean flag go122UseIfaceRecvEscapeAnalysis
+// to keep the format the same if disabled.
+func (l leaks) IfaceRecv() int { return l.get(1) }
+
 // Result returns the minimum deref count of any assignment flow from
 // l to its function's i'th result parameter. If no such flows exist,
 // Result returns -1.
-func (l leaks) Result(i int) int { return l.get(1 + i) }
+func (l leaks) Result(i int) int { return l.get(2 + i) }
 
 // AddHeap adds an assignment flow from l to the heap.
 func (l *leaks) AddHeap(derefs int) { l.add(0, derefs) }
 
+// AddHeap adds an assignment flow from l to use as an
+// interface receiver in a call.
+func (l *leaks) AddIfaceRecv(derefs int) { l.add(1, derefs) }
+
 // AddResult adds an assignment flow from l to its function's i'th
 // result parameter.
-func (l *leaks) AddResult(i, derefs int) { l.add(1+i, derefs) }
+func (l *leaks) AddResult(i, derefs int) { l.add(2+i, derefs) }
 
-func (l *leaks) setResult(i, derefs int) { l.set(1+i, derefs) }
+func (l *leaks) setResult(i, derefs int) { l.set(2+i, derefs) }
 
 func (l leaks) get(i int) int { return int(l[i]) - 1 }
 
@@ -52,6 +65,7 @@ func (l *leaks) set(i, derefs int) {
 		base.Fatalf("invalid derefs count: %v", derefs)
 	}
 	if v > math.MaxUint8 {
+		// 254 dereferences ought to be enough for anyone.
 		v = math.MaxUint8
 	}
 
@@ -64,10 +78,16 @@ func (l *leaks) Optimize() {
 	// If we have a path to the heap, then there's no use in
 	// keeping equal or longer paths elsewhere.
 	if x := l.Heap(); x >= 0 {
+		// First, remove any leaks to results.
 		for i := 0; i < numEscResults; i++ {
 			if l.Result(i) >= x {
 				l.setResult(i, -1)
 			}
+		}
+		// Second, remove any iface recv.
+		if l.IfaceRecv() >= x {
+			// TODO: check if we have a test that hits this
+			l.set(1, -1)
 		}
 	}
 }
@@ -89,7 +109,8 @@ func (l leaks) Encode() string {
 	for n > 0 && l[n-1] == 0 {
 		n--
 	}
-	s := "esc:" + string(l[:n])
+	// TODO: for now, we try to break any readers we don't know about
+	s := "es2:" + string(l[:n])
 	leakTagCache[l] = s
 	return s
 }
@@ -97,7 +118,7 @@ func (l leaks) Encode() string {
 // parseLeaks parses a binary string representing a leaks.
 func parseLeaks(s string) leaks {
 	var l leaks
-	if !strings.HasPrefix(s, "esc:") {
+	if !strings.HasPrefix(s, "es2:") {
 		l.AddHeap(0)
 		return l
 	}
