15d6602ef4f07595eb14781cd2f6afe77943c2bc
zhouguangyuan <zhouguangyuan.xian@gmail.com>
2023-09-03 20:03:45 +0800

    cmd/internal/obj: mark stack bounds check prologue preemptible
    The stack bounds check prologue is nonpreemptible before. But actually,
    if we mark the unsafepoint of the prologue as "RestartAtEntry", we will
    check the stack bound again if resume at the entry of current function.
    Then the prologue can be async preempted safely.
    
    Fixes #62433
    
    Change-Id: I89907d9c94c970b7e39f8fedc58320c9f9a6d307

--- src/cmd/internal/obj/riscv/obj.go
+++ src/cmd/internal/obj/riscv/obj.go
@@ -821,11 +821,14 @@ func stacksplit(ctxt *obj.Link, p *obj.Prog, cursym *obj.LSym, newprog obj.ProgA
 	p.To.Type = obj.TYPE_REG
 	p.To.Reg = REG_X6
 
-	// Mark the stack bound check and morestack call async nonpreemptible.
-	// If we get preempted here, when resumed the preemption request is
-	// cleared, but we'll still call morestack, which will double the stack
-	// unnecessarily. See issue #35470.
-	p = ctxt.StartUnsafePoint(p, newprog)
+	// When we get preempted here, if resumed at the preempted pc,
+	// the preemption request is cleared, but we'll still call morestack,
+	// which will double the stack unnecessarily. See issue #35470.
+	//
+	// Mark the stack bound check and morestack call async preemptible,
+	// and resume at the entry.
+	// Then we can check the stack bound again. See issue #62433.
+	p = ctxt.StartUnsafePointRestartAtEntry(p, newprog)
 
 	var to_done, to_more *obj.Prog
 
@@ -913,6 +916,9 @@ func stacksplit(ctxt *obj.Link, p *obj.Prog, cursym *obj.LSym, newprog obj.ProgA
 	}
 	jalToSym(ctxt, p, REG_X5)
 
+	// The instructions which unspill regs are still nonpreemptible.
+	// Because the args in registers will be lost if preempt here.
+	p = ctxt.StartUnsafePoint(p, newprog)
 	p = cursym.Func().UnspillRegisterArgs(p, newprog)
 	p = ctxt.EndUnsafePoint(p, newprog, -1)
 
