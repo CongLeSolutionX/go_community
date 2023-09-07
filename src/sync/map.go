d0ebe6521ee21f066e419abe48fdd5bf3a11d7d0
Hiro <laciferin@gmail.com>
2023-09-07 20:04:08 +0000

    sync: add Map.Clear
    For #61696
    
    Change-Id: I0a31afd3bc433fc84280d56f2798bda10da61eba
    GitHub-Last-Rev: 643c116b2e8b3a6ae8addee674ffbcbb983f44fe
    GitHub-Pull-Request: golang/go#61702

--- src/sync/map.go
+++ src/sync/map.go
@@ -155,6 +155,17 @@ func (m *Map) Store(key, value any) {
 	_, _ = m.Swap(key, value)
 }
 
+// Clear deletes all the keys.
+func (m *Map) Clear() {
+
+	defer m.mu.Unlock()
+	m.mu.Lock()
+
+	m.read.Store(new(readOnly))
+
+	clear(m.dirty)
+}
+
 // tryCompareAndSwap compare the entry with the given old value and swaps
 // it with a new value if the entry is equal to the old value, and the entry
 // has not been expunged.
