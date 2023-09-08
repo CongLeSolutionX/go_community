e12b2bd9257bebffce354cb81251f52f982efda2
Egon Elbre <egonelbre@gmail.com>
2023-09-08 11:17:59 +0300

    cmd/cgo: silence unaligned-access
    Clang 14+ introduced a warning when using mixed packed and unpacked structs.
    This can cause problems when taking an address of the unpacked struct,
    which may end up having a different alignment than expected.
    
    This is not a problem in cgo, which does not take pointers from the packed
    struct.
    
    Fixes #62480
    
    Change-Id: If5879eea5e1b77bc6dc7430f68f8c916bff9b090

--- src/cmd/cgo/out.go
+++ src/cmd/cgo/out.go
@@ -910,6 +910,7 @@ func (p *Package) writeExports(fgo2, fm, fgcc, fgcch io.Writer) {
 	fmt.Fprintf(fgcc, "#pragma GCC diagnostic ignored \"-Wunknown-pragmas\"\n")
 	fmt.Fprintf(fgcc, "#pragma GCC diagnostic ignored \"-Wpragmas\"\n")
 	fmt.Fprintf(fgcc, "#pragma GCC diagnostic ignored \"-Waddress-of-packed-member\"\n")
+	fmt.Fprintf(fgcc, "#pragma GCC diagnostic ignored \"-Wunaligned-access\"\n")
 
 	fmt.Fprintf(fgcc, "extern void crosscall2(void (*fn)(void *), void *, int, size_t);\n")
 	fmt.Fprintf(fgcc, "extern size_t _cgo_wait_runtime_init_done(void);\n")
