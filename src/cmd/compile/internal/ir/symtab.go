cb8982dfa066f3f7caecb096673d25faabb48d23
Keith Randall <khr@golang.org>
2023-09-18 10:13:18 -0700

    cmd/compile: use descriptors for type assertion runtime calls
    Mostly a reorganization to make further changes easier.
    
    This reorganization will make it easier to add a cache in front
    of the runtime call.
    
    Leave the old code alone for dynamic type assertions (aka generics).
    
    Change-Id: Ia7dcb7aeb1f63baf93584ccd792e8e31510e8aea

--- src/cmd/compile/internal/ir/symtab.go
+++ src/cmd/compile/internal/ir/symtab.go
@@ -47,6 +47,7 @@ var Syms struct {
 	Racereadrange     *obj.LSym
 	Racewrite         *obj.LSym
 	Racewriterange    *obj.LSym
+	TypeAssert        *obj.LSym
 	WBZero            *obj.LSym
 	WBMove            *obj.LSym
 	// Wasm
