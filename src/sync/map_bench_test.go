66244004a9db13f7bedf4775ab461a0966bf7881
Hiro <laciferin@gmail.com>
2023-09-26 22:33:54 +0000

    sync: add Map.Clear
    Fixes #61696
    
    Change-Id: I0a31afd3bc433fc84280d56f2798bda10da61eba
    GitHub-Last-Rev: 6bf739cb31fbbf22ba094e236faf118505d06f2b
    GitHub-Pull-Request: golang/go#61702

--- src/sync/map_bench_test.go
+++ src/sync/map_bench_test.go
@@ -533,3 +533,32 @@ func BenchmarkCompareAndDeleteMostlyMisses(b *testing.B) {
 		},
 	})
 }
+
+func BenchmarkClear(b *testing.B) {
+	mu := sync.Mutex{}
+
+	benchMap(b, bench{
+		perG: func(b *testing.B, pb *testing.PB, i int, m mapInterface) {
+			for ; pb.Next(); i++ {
+				k, v := i%256, i%256
+				mu.Lock()
+				m.Clear()
+				mu.Unlock()
+				m.Store(k, v)
+				v1, ok := m.Load(k)
+
+				if !ok {
+					b.Logf("failed to load %v", k)
+					// b.Skip() Unsafe to call in perfG
+					continue
+				}
+
+				if v1.(int) != v {
+					b.Errorf("expected %v, got %v", k, v)
+					// b.Skip()
+					continue
+				}
+			}
+		},
+	})
+}
