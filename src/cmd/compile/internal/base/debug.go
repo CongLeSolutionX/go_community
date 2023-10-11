55eac12ad067e59a7d5c1627cdcac28b4e18cd05
David Chase <drchase@google.com>
2023-10-11 16:19:49 -0400

    cmd/compile: revise logopt to ease future changes
    This recognizes that the LSP format has not changed, but
    that we are likely to evolve the "missed optimization"
    messages in the future, and also provides a compiler
    debug flag for routing future/experimental compiler
    instrumentation through the same LSP-based infrastructure.
    
    Change-Id: I1622ac50dd192ccdd4aefc1777d36d188d398a80

--- src/cmd/compile/internal/base/debug.go
+++ src/cmd/compile/internal/base/debug.go
@@ -37,6 +37,7 @@ type DebugFlags struct {
 	InlFuncsWithClosures  int    `help:"allow functions with closures to be inlined" concurrent:"ok"`
 	InlStaticInit         int    `help:"allow static initialization of inlined calls" concurrent:"ok"`
 	InterfaceCycles       int    `help:"allow anonymous interface cycles"`
+	JsonReport            string `help:"enable certain compiler instrumentation in json format, expect integer,directory" concurrent:"ok"`
 	Libfuzzer             int    `help:"enable coverage instrumentation for libfuzzer"`
 	LoopVar               int    `help:"shared (0, default), 1 (private loop variables), 2, private + log"`
 	LoopVarHash           string `help:"for debugging changes in loop behavior. Overrides experiment and loopvar flag."`
