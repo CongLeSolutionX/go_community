592c90d210516c51427ad6bb4661098b41d350c6
Than McIntosh <thanm@google.com>
2023-10-27 08:58:16 -0400

    cmd/compile/internal/inline: rework use of ir.StaticValue
    When running the code to compute function properties that feed
    inlining heuristics, the existing heuristics implementation makes
    fairly extensive use of ir.StaticValue and ir.Reassigned to sharpen
    the analysis. These calls turn out to cause a significant compile time
    increase, due to the fact that each call can potentially walk every
    node in the IR for the function. To help with this problem, switch the
    heuristics code over to using the new "batch mode" reassignment helper
    added in the previous CL. In addition, skip computing accurate
    'result' or 'param' flags for large functions, since they are very
    unlikely to be inline candidates (this also saves some compile time).
    
    Change-Id: Ib15a62416134386e34b7cfa1130a4b413a37b225

--- src/cmd/compile/internal/inline/inlheur/analyze.go
+++ src/cmd/compile/internal/inline/inlheur/analyze.go
@@ -95,9 +95,10 @@ func AnalyzeFunc(fn *ir.Func, canInline func(*ir.Func), budgetForFunc func(*ir.F
 	// doesn't have any interesting properties, then we don't want
 	// the overhead of writing out its inline body).
 	nf := len(funcs)
+	nameFinder := makeNameFinder(fn)
 	for idx := range funcs {
 		f := funcs[nf-idx-1]
-		fp := analyzeFunc(f, inlineMaxBudget)
+		fp := analyzeFunc(f, inlineMaxBudget, nameFinder)
 		revisitInlinability(f, fp, budgetForFunc)
 		if f.Inl != nil {
 			f.Inl.Properties = fp.SerializeToString()
@@ -110,11 +111,11 @@ func TearDown() {
 	fpmap = nil
 }
 
-func analyzeFunc(fn *ir.Func, inlineMaxBudget int) *FuncProps {
+func analyzeFunc(fn *ir.Func, inlineMaxBudget int, nf *nameFinder) *FuncProps {
 	if fih, ok := fpmap[fn]; ok {
 		return fih.props
 	}
-	fp, fcstab := computeFuncProps(fn, inlineMaxBudget)
+	fp, fcstab := computeFuncProps(fn, inlineMaxBudget, nf)
 	file, line := fnFileLine(fn)
 	entry := fnInlHeur{
 		fname: fn.Sym().Name,
@@ -149,7 +150,7 @@ func revisitInlinability(fn *ir.Func, fp *FuncProps, budgetForFunc func(*ir.Func
 // computeFuncProps examines the Go function 'fn' and computes for it
 // a function "properties" object, to be used to drive inlining
 // heuristics. See comments on the FuncProps type for more info.
-func computeFuncProps(fn *ir.Func, inlineMaxBudget int) (*FuncProps, CallSiteTab) {
+func computeFuncProps(fn *ir.Func, inlineMaxBudget int, nf *nameFinder) (*FuncProps, CallSiteTab) {
 	if debugTrace&debugTraceFuncs != 0 {
 		fmt.Fprintf(os.Stderr, "=-= starting analysis of func %v:\n%+v\n",
 			fn, fn)
@@ -157,8 +158,8 @@ func computeFuncProps(fn *ir.Func, inlineMaxBudget int) (*FuncProps, CallSiteTab
 	fp := new(FuncProps)
 	ffa := makeFuncFlagsAnalyzer(fn)
 	analyzers := []propAnalyzer{ffa}
-	analyzers = addResultsAnalyzer(fn, analyzers, fp, inlineMaxBudget)
-	analyzers = addParamsAnalyzer(fn, analyzers, fp)
+	analyzers = addResultsAnalyzer(fn, analyzers, fp, inlineMaxBudget, nf)
+	analyzers = addParamsAnalyzer(fn, analyzers, fp, nf)
 	runAnalyzersOnFunction(fn, analyzers)
 	for _, a := range analyzers {
 		a.setResults(fp)
@@ -167,7 +168,7 @@ func computeFuncProps(fn *ir.Func, inlineMaxBudget int) (*FuncProps, CallSiteTab
 	if debugTrace&debugTraceCalls != 0 {
 		fmt.Fprintf(os.Stderr, "=-= making callsite table for func %v:\n", fn)
 	}
-	cstab := computeCallSiteTable(fn, fn.Body, ffa.panicPathTable(), 0)
+	cstab := computeCallSiteTable(fn, fn.Body, ffa.panicPathTable(), 0, nf)
 	return fp, cstab
 }
 
