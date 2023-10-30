f25974b5a908b5aeb2880a1f5424cc1a3882942a
Keith Randall <khr@golang.org>
2023-10-30 13:23:00 -0700

    cmd/internal/obj/arm64: fix frame pointer restore in epilogue
    For leaf but nonzero-frame functions.
    
    Currently we're not restoring it properly. We also need to restore
    it before popping the stack frame, so that the frame won't get
    clobbered by a signal handler in the meantime.
    
    Fixes #63830
    
    TODO: needs test
    Change-Id: I273a25f2a838f05a959c810145cccc5428eaf164

--- src/cmd/internal/obj/arm64/obj7.go
+++ src/cmd/internal/obj/arm64/obj7.go
@@ -850,21 +850,22 @@ func preprocess(ctxt *obj.Link, cursym *obj.LSym, newprog obj.ProgAlloc) {
 			p.To = obj.Addr{}
 			if c.cursym.Func().Text.Mark&LEAF != 0 {
 				if c.autosize != 0 {
+					// Restore frame pointer.
+					p.As = AMOVD
+					p.From.Type = obj.TYPE_MEM
+					p.From.Offset = -8
+					p.From.Reg = REGSP
+					p.To.Type = obj.TYPE_REG
+					p.To.Reg = REGFP
+
+					// Pop stack frame.
+					p = obj.Appendp(p, c.newprog)
 					p.As = AADD
 					p.From.Type = obj.TYPE_CONST
 					p.From.Offset = int64(c.autosize)
 					p.To.Type = obj.TYPE_REG
 					p.To.Reg = REGSP
 					p.Spadj = -c.autosize
-
-					// Frame pointer.
-					p = obj.Appendp(p, c.newprog)
-					p.As = ASUB
-					p.From.Type = obj.TYPE_CONST
-					p.From.Offset = 8
-					p.Reg = REGSP
-					p.To.Type = obj.TYPE_REG
-					p.To.Reg = REGFP
 				}
 			} else {
 				aoffset := c.autosize
