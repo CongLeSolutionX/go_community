294887db7ca10423c83e291a1cf301506d85cb8f
Michael Anthony Knyszek <mknyszek@google.com>
2023-11-13 21:06:40 +0000

    internal/trace: add task analysis for v2 traces
    DO NOT SUBMIT. Needs tests.
    
    For v1 traces, cmd/trace contains code for analyzing tasks separately
    from the goroutine analysis code present in internal/trace. As I started
    to look into porting that code to v2 traces, I noticed that it wouldn't
    be too hard to just generalize the existing v2 goroutine summary code to
    generate exactly the same information.
    
    This change does exactly that.
    
    For #60773.
    For #63960.
    
    Change-Id: I0cdd9bf9ba11fb292a9ffc37dbf18c2a6a2483b8

--- src/cmd/trace/v2/main.go
+++ src/cmd/trace/v2/main.go
@@ -71,8 +71,8 @@ func Main(traceFile, httpAddr, pprof string, debug int) error {
 	mux.Handle("/static/", traceviewer.StaticHandler())
 
 	// Goroutines handlers.
-	mux.HandleFunc("/goroutines", GoroutinesHandlerFunc(parsed.gSummaries))
-	mux.HandleFunc("/goroutine", GoroutineHandler(parsed.gSummaries))
+	mux.HandleFunc("/goroutines", GoroutinesHandlerFunc(parsed.summary.Goroutines))
+	mux.HandleFunc("/goroutine", GoroutineHandler(parsed.summary.Goroutines))
 
 	// MMU handler.
 	mux.HandleFunc("/mmu", traceviewer.MMUHandlerFunc(ranges, mutatorUtil))
@@ -94,8 +94,8 @@ func Main(traceFile, httpAddr, pprof string, debug int) error {
 }
 
 type parsedTrace struct {
-	events     []tracev2.Event
-	gSummaries map[tracev2.GoID]*trace.GoroutineSummary
+	events  []tracev2.Event
+	summary *trace.Summary
 }
 
 func parseTrace(data []byte) (*parsedTrace, error) {
@@ -103,7 +103,7 @@ func parseTrace(data []byte) (*parsedTrace, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to create trace reader: %w", err)
 	}
-	s := trace.NewGoroutineSummarizer()
+	s := trace.NewSummarizer()
 	t := new(parsedTrace)
 	for {
 		ev, err := r.ReadEvent()
@@ -115,7 +115,7 @@ func parseTrace(data []byte) (*parsedTrace, error) {
 		t.events = append(t.events, ev)
 		s.Event(&t.events[len(t.events)-1])
 	}
-	t.gSummaries = s.Finalize()
+	t.summary = s.Finalize()
 	return t, nil
 }
 
