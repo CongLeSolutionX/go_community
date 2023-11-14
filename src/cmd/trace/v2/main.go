5c44e2197dacb31d64c774cd4c1ccc0aa0de5b3b
Michael Anthony Knyszek <mknyszek@google.com>
2023-11-14 17:03:24 +0000

    cmd/trace: add almost full support for v2 traces in the trace viewer
    This change refactors the cmd/trace package and adds most of the support
    for v2 traces.
    
    The following features of note are missing in this CL and will be
    implemented in follow-up CLs:
    - The focustask filter for the trace viewer
    - The taskid filter for the trace viewer
    - The goid filter for the trace viewer
    - Pprof profiles
    - The MMU graph
    - The goroutine analysis pages
    - The task analysis pages
    - The region analysis pages
    
    This CL makes one notable change to the trace CLI: it makes the -d flag
    accept an integer to set the debug mode. For old traces -d != 0 works
    just like -d. For new traces -d=1 means the high-level events and -d=2
    means the low-level events.
    
    Most of this CL was written and implemented by Felix Geisendörfer
    (felix.geisendoerfer@datadoghq.com), I'm just landing it. :)
    
    For #60773.
    For #63960.
    
    Change-Id: I3626e22473227c5980134a85f1bb6a845f567b1b

---
+++ src/cmd/trace/v2/main.go
@@ -0,0 +1,133 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package trace
+
+import (
+	"bytes"
+	"fmt"
+	"internal/trace/traceviewer"
+	tracev2 "internal/trace/v2"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+	"internal/trace/v2/raw"
+
+	"cmd/internal/browser"
+)
+
+// Main is the main function for cmd/trace v2.
+func Main(traceFile, httpAddr, pprof string, debug int) error {
+	data, err := os.ReadFile(traceFile)
+	if err != nil {
+		return fmt.Errorf("failed to read trace file: %w", err)
+	}
+
+	// Debug flags.
+	switch debug {
+	case 1:
+		return debugProcessedEvents(data)
+	case 2:
+		return debugRawEvents(data)
+	}
+
+	ln, err := net.Listen("tcp", httpAddr)
+	if err != nil {
+		return fmt.Errorf("failed to create server socket: %w", err)
+	}
+
+	addr := "http://" + ln.Addr().String()
+	log.Print("Parsing trace...")
+	parsed, err := parseTrace(data)
+	if err != nil {
+		return err
+	}
+
+	log.Print("Splitting trace...")
+	ranges, err := splitTrace(parsed)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Opening browser. Trace viewer is listening on %s", addr)
+	browser.Open(addr)
+
+	mux := http.NewServeMux()
+	mux.Handle("/", traceviewer.MainHandler(ranges))
+	mux.Handle("/trace", traceviewer.TraceHandler())
+	mux.Handle("/jsontrace", JSONTraceHandler(parsed))
+	mux.Handle("/static/", traceviewer.StaticHandler())
+
+	err = http.Serve(ln, mux)
+	return fmt.Errorf("failed to start http server: %w", err)
+}
+
+type parsedTrace struct {
+	events []tracev2.Event
+}
+
+func parseTrace(data []byte) (parsedTrace, error) {
+	r, err := tracev2.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return parsedTrace{}, fmt.Errorf("failed to create trace reader: %w", err)
+	}
+	var t parsedTrace
+	for {
+		ev, err := r.ReadEvent()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return parsedTrace{}, fmt.Errorf("failed to read event: %w", err)
+		}
+		t.events = append(t.events, ev)
+	}
+	return t, nil
+}
+
+// splitTrace splits the trace into a number of ranges, each resulting in approx 100 MiB of
+// json output (the trace viewer can hardly handle more).
+func splitTrace(parsed parsedTrace) ([]traceviewer.Range, error) {
+	// TODO(mknyszek): Split traces by generation by doing a quick first pass over the
+	// trace to identify all the generation boundaries.
+	s, c := traceviewer.SplittingTraceConsumer(100 << 20) // 100 MiB
+	if err := generateTrace(parsed, c); err != nil {
+		return nil, err
+	}
+	return s.Ranges, nil
+}
+
+func debugProcessedEvents(traceData []byte) error {
+	tr, err := tracev2.NewReader(bytes.NewReader(traceData))
+	if err != nil {
+		return err
+	}
+	for {
+		ev, err := tr.ReadEvent()
+		if err == io.EOF {
+			return nil
+		} else if err != nil {
+			return err
+		}
+		fmt.Println(ev.String())
+	}
+}
+
+func debugRawEvents(traceData []byte) error {
+	rr, err := raw.NewReader(bytes.NewReader(traceData))
+	if err != nil {
+		return err
+	}
+	for {
+		ev, err := rr.ReadEvent()
+		if err == io.EOF {
+			return nil
+		} else if err != nil {
+			return err
+		}
+		fmt.Println(ev.String())
+	}
+}
