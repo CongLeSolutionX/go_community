5394ca24677113fcbee41570ae92a9a7e732a9a7
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2023-11-16 21:30:53 +0000

    net/http: prevent redirect loop in serveFile if "/" is a normal file
    When FileServer(Dir("file")) is used where "file" is a normal file and
    not a directory, the server enters a redirect loop.
    
    The usage of a file inplace of a directory path is not documented in
    http.Dir and it could be considered undefined behavior.
    
    This CL updates (Dir).Open to validate that dir is a directory as well
    as updating the documentation accordingly.
    
    One of the tests has a testcase for Dir("/etc/hosts") and it states this
    is a trick that ServeFile uses, but I'm not sure if this is still relevant
    today.
    
    Fixes #63769
    
    Change-Id: I81e289444e7d0bd72189c2e7b763f5540333e2d0
    GitHub-Last-Rev: 480bda831bb04813e0ef24b5207d9ffa0ab9e1cc
    GitHub-Pull-Request: golang/go#63860

--- src/net/http/fs_test.go
+++ src/net/http/fs_test.go
@@ -475,12 +475,6 @@ func TestDirJoin(t *testing.T) {
 	test(Dir("/etc"), "/hosts")
 	test(Dir("/etc"), "hosts")
 	test(Dir("/etc"), "../../../../hosts")
-
-	// Not really directories, but since we use this trick in
-	// ServeFile, test it:
-	test(Dir("/etc/hosts"), "")
-	test(Dir("/etc/hosts"), "/")
-	test(Dir("/etc/hosts"), "../")
 }
 
 func TestEmptyDirOpenCWD(t *testing.T) {
@@ -495,6 +489,15 @@ func TestEmptyDirOpenCWD(t *testing.T) {
 	test(Dir(""))
 	test(Dir("."))
 	test(Dir("./"))
+}
+
+// issue 63769
+func TestDirNormalFile(t *testing.T) {
+	f, err := Dir("testdata/index.html").Open("")
+	if err == nil {
+		f.Close()
+		t.Fatalf("got nil, want error")
+	}
 }
 
 func TestServeFileContentType(t *testing.T) { run(t, testServeFileContentType) }
@@ -1667,4 +1670,20 @@ func (grw gzipResponseWriter) Flush() {
 	if fw, ok := grw.ResponseWriter.(http.Flusher); ok {
 		fw.Flush()
 	}
+}
+
+// Issue 63769
+func TestFileServerDirWithRootFile(t *testing.T) { run(t, testFileServerDirWithRootFile) }
+func testFileServerDirWithRootFile(t *testing.T, mode testMode) {
+	ts := newClientServerTest(t, mode, FileServer(Dir("testdata/index.html"))).ts
+	defer ts.Close()
+
+	res, err := ts.Client().Get(ts.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if s := res.StatusCode; s != StatusInternalServerError {
+		t.Errorf("got %q, want 500", s)
+	}
+	res.Body.Close()
 }
