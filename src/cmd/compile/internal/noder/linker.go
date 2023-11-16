b0f8673f7bf265068d60b6693ba5a2c2e5e2d23e
Than McIntosh <thanm@google.com>
2023-11-16 15:26:15 -0500

    cmd/compile/internal/noder: fix compatibility issue with fn.Inl.Properties
    When GOEXPERIMENT=newinliner is in effect, an additional "properties"
    string is written out as part of the export data for each inlinable
    function. To avoid problems with the bootstrapping that happens in
    cmd/dist (see the cited bug for more details), switch to a scheme in
    which we always write out the properties string, but just leave it
    empty in the case that the newinliner experiment is turned off.
    
    Fixes #64189.
    
    Change-Id: Id838c3b316d5ad9c585341dc0a16a06c44e469d2

--- src/cmd/compile/internal/noder/linker.go
+++ src/cmd/compile/internal/noder/linker.go
@@ -6,7 +6,6 @@ package noder
 
 import (
 	"internal/buildcfg"
-	"internal/goexperiment"
 	"internal/pkgbits"
 	"io"
 
@@ -297,9 +296,7 @@ func (l *linker) relocFuncExt(w *pkgbits.Encoder, name *ir.Name) {
 	if inl := name.Func.Inl; w.Bool(inl != nil) {
 		w.Len(int(inl.Cost))
 		w.Bool(inl.CanDelayResults)
-		if goexperiment.NewInliner {
-			w.String(inl.Properties)
-		}
+		w.String(inl.Properties)
 	}
 
 	w.Sync(pkgbits.SyncEOF)
--- src/cmd/compile/internal/noder/reader.go
+++ src/cmd/compile/internal/noder/reader.go
@@ -8,7 +8,6 @@ import (
 	"fmt"
 	"go/constant"
 	"internal/buildcfg"
-	"internal/goexperiment"
 	"internal/pkgbits"
 	"path/filepath"
 	"strings"
@@ -1103,9 +1102,7 @@ func (r *reader) funcExt(name *ir.Name, method *types.Sym) {
 				Cost:            int32(r.Len()),
 				CanDelayResults: r.Bool(),
 			}
-			if goexperiment.NewInliner {
-				fn.Inl.Properties = r.String()
-			}
+			fn.Inl.Properties = r.String()
 		}
 	} else {
 		r.addBody(name.Func, method)
