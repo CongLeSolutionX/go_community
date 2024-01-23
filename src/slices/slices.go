c3a279b8815a4686d1e272c83e611cc31c927352
Russ Cox <rsc@golang.org>
2024-01-23 14:49:07 -0500

    slices: add All, Backward, Values, Append, Collect, Sorted, SortedFunc
    DO NOT REVIEW - needs tests
    DO NOT SUBMIT - proposal not yet accepted
    
    For #61899.
    
    Change-Id: I2361706bb34c519d28bff036e1ba9b642ed36740

--- src/slices/slices.go
+++ src/slices/slices.go
@@ -7,6 +7,7 @@ package slices
 
 import (
 	"cmp"
+	"iter"
 	"unsafe"
 )
 
@@ -512,4 +513,67 @@ func Concat[S ~[]E, E any](slices ...S) S {
 		newslice = append(newslice, s...)
 	}
 	return newslice
+}
+
+// All returns an iterator over index-value pairs in the slice.
+// The indexes range in the usual order, from 0 through len(s)-1.
+func All[Slice ~[]Elem, Elem any](s Slice) iter.Seq2[int, Elem] {
+	return func(yield func(int, Elem) bool) {
+		for i, v := range s {
+			if !yield(i, v) {
+				return
+			}
+		}
+	}
+}
+
+// Backward returns an iterator over index-value pairs in the slice,
+// traversing it backward. The indexes range from len(s)-1 down to 0.
+func Backward[Slice ~[]Elem, Elem any](s Slice) iter.Seq2[int, Elem] {
+	return func(yield func(int, Elem) bool) {
+		for i := len(s) - 1; i >= 0; i-- {
+			if !yield(i, s[i]) {
+				return
+			}
+		}
+	}
+}
+
+// Values returns an iterator over the values in the slice,
+// starting with s[0].
+func Values[Slice ~[]Elem, Elem any](s Slice) iter.Seq[Elem] {
+	return func(yield func(Elem) bool) {
+		for _, v := range s {
+			if !yield(v) {
+				return
+			}
+		}
+	}
+}
+
+// Append appends the values from seq to the slice and returns the extended slice.
+func Append[Slice ~[]Elem, Elem any](x Slice, seq iter.Seq[Elem]) Slice {
+	for v := range seq {
+		x = append(x, v)
+	}
+	return x
+}
+
+// Collect collects values from seq into a new slice and returns it.
+func Collect[Elem any](seq iter.Seq[Elem]) []Elem {
+	return Append([]Elem(nil), seq)
+}
+
+// Sorted collects values from seq into a new slice, sorts the slice, and returns it.
+func Sorted[Elem cmp.Ordered](seq iter.Seq[Elem]) []Elem {
+	slice := Collect(seq)
+	Sort(slice)
+	return slice
+}
+
+// SortedFunc collects values from seq into a new slice, sorts the slice, and returns it.
+func SortedFunc[Elem any](seq iter.Seq[Elem], cmp func(Elem, Elem) int) []Elem {
+	slice := Collect(seq)
+	SortFunc(slice, cmp)
+	return slice
 }
