d450648003db76965622afab45abf5be04fed9a0
Russ Cox <rsc@golang.org>
2024-01-23 12:01:53 -0500

    iter: expose fundamental types to Go 1.23
    These were previously only available with GOEXPERIMENT=rangefunc.
    
    For #61897.
    
    Change-Id: I86aea5ae8be1f7a2975b623325811221ed40d384

--- src/iter/iter.go
+++ src/iter/iter.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build goexperiment.rangefunc
-
 // Package iter provides basic definitions and operations
 // related to iteration in Go.
 //
