a1d3ef601d87e31c55a62c71f59134fe36cca1f6
Robert Griesemer <gri@golang.org>
2024-02-08 14:29:26 -0800

    go/types: generate builtins.go from types2 source
    Minor changes to types2.builtin.go to simplify automatic translation.
    
    Added new conversion functions to generate_test.go to handle the
    translation of builtins.go.
    
    While at it, added additional helper functions to generate_test.go
    and simplified some of the existing conversion functions.
    
    This CL reduces the amount of code that needs to be maintained
    manually by about 1000 LOC.
    
    Change-Id: I1bd5c8eda0c0194a0b47e69882d2b987d91eef50

--- src/go/types/generate_test.go
+++ src/go/types/generate_test.go
@@ -99,59 +99,62 @@ var filemap = map[string]action{
 	"array.go":          nil,
 	"api_predicates.go": nil,
 	"basic.go":          nil,
-	"chan.go":           nil,
-	"const.go":          func(f *ast.File) { fixTokenPos(f) },
-	"context.go":        nil,
-	"context_test.go":   nil,
-	"errsupport.go":     nil,
-	"gccgosizes.go":     nil,
-	"gcsizes.go":        func(f *ast.File) { renameIdents(f, "IsSyncAtomicAlign64->_IsSyncAtomicAlign64") },
-	"hilbert_test.go":   func(f *ast.File) { renameImportPath(f, `"cmd/compile/internal/types2"`, `"go/types"`) },
-	"infer.go": func(f *ast.File) {
-		fixTokenPos(f)
-		fixInferSig(f)
+	"builtins.go": func(f *ast.File) {
+		renameImportPath(f, `"cmd/compile/internal/syntax"`, `"go/ast"`)
+		renameIdents(f, "syntax->ast")
+		renameSelectors(f, "ArgList->Args")
+		fixSelValue(f)
+		fixAtPosCall(f)
+		fixDDDErrPosAssignment(f)
+		fixArgErrPosAssignment(f)
 	},
+	"chan.go":         nil,
+	"const.go":        func(f *ast.File) { fixTokenPos(f) },
+	"context.go":      nil,
+	"context_test.go": nil,
+	"errsupport.go":   nil,
+	"gccgosizes.go":   nil,
+	"gcsizes.go":      func(f *ast.File) { renameIdents(f, "IsSyncAtomicAlign64->_IsSyncAtomicAlign64") },
+	"hilbert_test.go": func(f *ast.File) { renameImportPath(f, `"cmd/compile/internal/types2"`, `"go/types"`) },
+	"infer.go":        func(f *ast.File) { fixTokenPos(f); fixInferSig(f) },
 	// "initorder.go": fixErrErrorfCall, // disabled for now due to unresolved error_ use implications for gopls
 	"instantiate.go":      func(f *ast.File) { fixTokenPos(f); fixCheckErrorfCall(f) },
 	"instantiate_test.go": func(f *ast.File) { renameImportPath(f, `"cmd/compile/internal/types2"`, `"go/types"`) },
 	"lookup.go":           func(f *ast.File) { fixTokenPos(f) },
 	"main_test.go":        nil,
 	"map.go":              nil,
-	"named.go":            func(f *ast.File) { fixTokenPos(f); fixTraceSel(f) },
+	"named.go":            func(f *ast.File) { fixTokenPos(f); renameSelectors(f, "Trace->_Trace") },
 	"object.go":           func(f *ast.File) { fixTokenPos(f); renameIdents(f, "NewTypeNameLazy->_NewTypeNameLazy") },
 	"object_test.go":      func(f *ast.File) { renameImportPath(f, `"cmd/compile/internal/types2"`, `"go/types"`) },
 	"objset.go":           nil,
 	"package.go":          nil,
 	"pointer.go":          nil,
 	"predicates.go":       nil,
-	"scope.go": func(f *ast.File) {
-		fixTokenPos(f)
-		renameIdents(f, "Squash->squash", "InsertLazy->_InsertLazy")
-	},
-	"selection.go":     nil,
-	"sizes.go":         func(f *ast.File) { renameIdents(f, "IsSyncAtomicAlign64->_IsSyncAtomicAlign64") },
-	"slice.go":         nil,
-	"subst.go":         func(f *ast.File) { fixTokenPos(f); fixTraceSel(f) },
-	"termlist.go":      nil,
-	"termlist_test.go": nil,
-	"tuple.go":         nil,
-	"typelists.go":     nil,
-	"typeparam.go":     nil,
-	"typeterm_test.go": nil,
-	"typeterm.go":      nil,
-	"under.go":         nil,
-	"unify.go":         fixSprintf,
-	"universe.go":      fixGlobalTypVarDecl,
-	"util_test.go":     fixTokenPos,
-	"validtype.go":     nil,
+	"scope.go":            func(f *ast.File) { fixTokenPos(f); renameIdents(f, "Squash->squash", "InsertLazy->_InsertLazy") },
+	"selection.go":        nil,
+	"sizes.go":            func(f *ast.File) { renameIdents(f, "IsSyncAtomicAlign64->_IsSyncAtomicAlign64") },
+	"slice.go":            nil,
+	"subst.go":            func(f *ast.File) { fixTokenPos(f); renameSelectors(f, "Trace->_Trace") },
+	"termlist.go":         nil,
+	"termlist_test.go":    nil,
+	"tuple.go":            nil,
+	"typelists.go":        nil,
+	"typeparam.go":        nil,
+	"typeterm_test.go":    nil,
+	"typeterm.go":         nil,
+	"under.go":            nil,
+	"unify.go":            fixSprintf,
+	"universe.go":         fixGlobalTypVarDecl,
+	"util_test.go":        fixTokenPos,
+	"validtype.go":        nil,
 }
 
 // TODO(gri) We should be able to make these rewriters more configurable/composable.
 //           For now this is a good starting point.
 
-// renameIdent renames identifiers: each renames entry is of the form from->to.
-// Note: This doesn't change the use of the identifiers in comments.
-func renameIdents(f *ast.File, renames ...string) {
+// renameList splits renames entries of the form "from->to" into []string{from, to}
+// and returns the list of all those slices.
+func renameList(renames ...string) [][]string {
 	var list [][]string
 	for _, r := range renames {
 		s := strings.Split(r, "->")
@@ -160,6 +163,13 @@ func renameIdents(f *ast.File, renames ...string) {
 		}
 		list = append(list, s)
 	}
+	return list
+}
+
+// renameIdents renames identifiers: each renames entry is of the form "from->to".
+// Note: This doesn't change the use of the identifiers in comments.
+func renameIdents(f *ast.File, renames ...string) {
+	list := renameList(renames...)
 	ast.Inspect(f, func(n ast.Node) bool {
 		switch n := n.(type) {
 		case *ast.Ident:
@@ -172,6 +182,24 @@ func renameIdents(f *ast.File, renames ...string) {
 		}
 		return true
 	})
+}
+
+// renameSelectors is like renameIdents but only looks at selectors.
+func renameSelectors(f *ast.File, renames ...string) {
+	list := renameList(renames...)
+	ast.Inspect(f, func(n ast.Node) bool {
+		switch n := n.(type) {
+		case *ast.SelectorExpr:
+			for _, r := range list {
+				if n.Sel.Name == r[0] {
+					n.Sel.Name = r[1]
+				}
+			}
+			return false
+		}
+		return true
+	})
+
 }
 
 // renameImportPath renames an import path.
@@ -201,7 +229,7 @@ func fixTokenPos(f *ast.File) {
 			}
 		case *ast.SelectorExpr:
 			// rewrite syntax.Pos to token.Pos
-			if x, _ := n.X.(*ast.Ident); x != nil && x.Name == "syntax" && n.Sel.Name == "Pos" {
+			if x := asIdent(n.X, "syntax"); x != nil && n.Sel.Name == "Pos" {
 				x.Name = "token"
 				return false
 			}
@@ -210,6 +238,22 @@ func fixTokenPos(f *ast.File) {
 			if fun, _ := n.Fun.(*ast.SelectorExpr); fun != nil && fun.Sel.Name == "IsKnown" && len(n.Args) == 0 {
 				fun.Sel.Name = "IsValid"
 				return false
+			}
+		}
+		return true
+	})
+}
+
+// fixSelValue updates the selector Sel.Value to Sel.Name.
+func fixSelValue(f *ast.File) {
+	ast.Inspect(f, func(n ast.Node) bool {
+		switch n := n.(type) {
+		case *ast.SelectorExpr:
+			if n.Sel.Name == "Value" {
+				if selx, _ := n.X.(*ast.SelectorExpr); selx != nil && selx.Sel.Name == "Sel" {
+					n.Sel.Name = "Name"
+					return false
+				}
 			}
 		}
 		return true
@@ -237,7 +281,7 @@ func fixInferSig(f *ast.File) {
 				switch selx.Sel.Name {
 				case "renameTParams":
 					// rewrite check.renameTParams(pos, ... ) to check.renameTParams(posn.Pos(), ... )
-					if ident, _ := n.Args[0].(*ast.Ident); ident != nil && ident.Name == "pos" {
+					if isIdent(n.Args[0], "pos") {
 						pos := n.Args[0].Pos()
 						fun := &ast.SelectorExpr{X: newIdent(pos, "posn"), Sel: newIdent(pos, "Pos")}
 						arg := &ast.CallExpr{Fun: fun, Lparen: pos, Args: nil, Ellipsis: token.NoPos, Rparen: pos}
@@ -246,7 +290,7 @@ func fixInferSig(f *ast.File) {
 					}
 				case "errorf":
 					// rewrite check.errorf(pos, ...) to check.errorf(posn, ...)
-					if ident, _ := n.Args[0].(*ast.Ident); ident != nil && ident.Name == "pos" {
+					if isIdent(n.Args[0], "pos") {
 						pos := n.Args[0].Pos()
 						arg := newIdent(pos, "posn")
 						n.Args[0] = arg
@@ -254,11 +298,34 @@ func fixInferSig(f *ast.File) {
 					}
 				case "allowVersion":
 					// rewrite check.allowVersion(..., pos, ...) to check.allowVersion(..., posn, ...)
-					if ident, _ := n.Args[1].(*ast.Ident); ident != nil && ident.Name == "pos" {
+					if isIdent(n.Args[1], "pos") {
 						pos := n.Args[1].Pos()
 						arg := newIdent(pos, "posn")
 						n.Args[1] = arg
 						return false
+					}
+				}
+			}
+		}
+		return true
+	})
+}
+
+// fixAtPosCall updates calls of the form atPos(x) to x.Pos() in argument lists of (check).dump calls.
+func fixAtPosCall(f *ast.File) {
+	ast.Inspect(f, func(n ast.Node) bool {
+		switch n := n.(type) {
+		case *ast.CallExpr:
+			if selx, _ := n.Fun.(*ast.SelectorExpr); selx != nil && selx.Sel.Name == "dump" {
+				for i, arg := range n.Args {
+					if call, _ := arg.(*ast.CallExpr); call != nil {
+						// rewrite xxx.dump(..., atPos(x), ...) to xxx.dump(..., x.Pos(), ...)
+						if isIdent(call.Fun, "atPos") {
+							pos := call.Args[0].Pos()
+							fun := &ast.SelectorExpr{X: call.Args[0], Sel: newIdent(pos, "Pos")}
+							n.Args[i] = &ast.CallExpr{Fun: fun, Lparen: pos, Rparen: pos}
+							return false
+						}
 					}
 				}
 			}
@@ -273,15 +340,14 @@ func fixErrErrorfCall(f *ast.File) {
 		switch n := n.(type) {
 		case *ast.CallExpr:
 			if selx, _ := n.Fun.(*ast.SelectorExpr); selx != nil {
-				if ident, _ := selx.X.(*ast.Ident); ident != nil && ident.Name == "err" {
+				if isIdent(selx.X, "err") {
 					switch selx.Sel.Name {
 					case "errorf":
 						// rewrite err.errorf(obj, ... ) to err.errorf(obj.Pos(), ... )
 						if ident, _ := n.Args[0].(*ast.Ident); ident != nil && ident.Name == "obj" {
 							pos := n.Args[0].Pos()
 							fun := &ast.SelectorExpr{X: ident, Sel: newIdent(pos, "Pos")}
-							arg := &ast.CallExpr{Fun: fun, Lparen: pos, Args: nil, Ellipsis: token.NoPos, Rparen: pos}
-							n.Args[0] = arg
+							n.Args[0] = &ast.CallExpr{Fun: fun, Lparen: pos, Rparen: pos}
 							return false
 						}
 					}
@@ -298,15 +364,14 @@ func fixCheckErrorfCall(f *ast.File) {
 		switch n := n.(type) {
 		case *ast.CallExpr:
 			if selx, _ := n.Fun.(*ast.SelectorExpr); selx != nil {
-				if ident, _ := selx.X.(*ast.Ident); ident != nil && ident.Name == "check" {
+				if isIdent(selx.X, "check") {
 					switch selx.Sel.Name {
 					case "errorf":
 						// rewrite check.errorf(pos, ... ) to check.errorf(atPos(pos), ... )
-						if ident, _ := n.Args[0].(*ast.Ident); ident != nil && ident.Name == "pos" {
+						if ident := asIdent(n.Args[0], "pos"); ident != nil {
 							pos := n.Args[0].Pos()
 							fun := newIdent(pos, "atPos")
-							arg := &ast.CallExpr{Fun: fun, Lparen: pos, Args: []ast.Expr{ident}, Ellipsis: token.NoPos, Rparen: pos}
-							n.Args[0] = arg
+							n.Args[0] = &ast.CallExpr{Fun: fun, Lparen: pos, Args: []ast.Expr{ident}, Rparen: pos}
 							return false
 						}
 					}
@@ -317,15 +382,43 @@ func fixCheckErrorfCall(f *ast.File) {
 	})
 }
 
-// fixTraceSel renames uses of x.Trace to x.trace, where x for any x with a Trace field.
-func fixTraceSel(f *ast.File) {
+// fixDDDErrPosAssignment updates the assignment dddErrPos := call to dddErrPos := atPos(call.Ellipsis).
+func fixDDDErrPosAssignment(f *ast.File) {
 	ast.Inspect(f, func(n ast.Node) bool {
 		switch n := n.(type) {
-		case *ast.SelectorExpr:
-			// rewrite x.Trace to x._Trace (for Config.Trace)
-			if n.Sel.Name == "Trace" {
-				n.Sel.Name = "_Trace"
-				return false
+		case *ast.AssignStmt:
+			if len(n.Lhs) == 1 && n.Tok == token.DEFINE && len(n.Rhs) == 1 {
+				if isIdent(n.Lhs[0], "dddErrPos") {
+					if ident := asIdent(n.Rhs[0], "call"); ident != nil {
+						pos := ident.Pos()
+						arg0 := &ast.SelectorExpr{X: ident, Sel: newIdent(pos, "Ellipsis")}
+						fun := newIdent(pos, "atPos")
+						n.Rhs[0] = &ast.CallExpr{Fun: fun, Lparen: pos, Args: []ast.Expr{arg0}, Rparen: pos}
+						return false
+					}
+				}
+			}
+		}
+		return true
+	})
+}
+
+// fixArgErrPosAssignment updates the assignment argErrPos := call to argErrPos := inNode(call, call.Rparen).
+func fixArgErrPosAssignment(f *ast.File) {
+	ast.Inspect(f, func(n ast.Node) bool {
+		switch n := n.(type) {
+		case *ast.AssignStmt:
+			if len(n.Lhs) == 1 && n.Tok == token.DEFINE && len(n.Rhs) == 1 {
+				if isIdent(n.Lhs[0], "argErrPos") {
+					if ident := asIdent(n.Rhs[0], "call"); ident != nil {
+						pos := ident.Pos()
+						arg0 := newIdent(pos, "call")
+						arg1 := &ast.SelectorExpr{X: ident, Sel: newIdent(pos, "Rparen")}
+						fun := newIdent(pos, "inNode")
+						n.Rhs[0] = &ast.CallExpr{Fun: fun, Lparen: pos, Args: []ast.Expr{arg0, arg1}, Rparen: pos}
+						return false
+					}
+				}
 			}
 		}
 		return true
@@ -354,13 +447,26 @@ func fixSprintf(f *ast.File) {
 	ast.Inspect(f, func(n ast.Node) bool {
 		switch n := n.(type) {
 		case *ast.CallExpr:
-			if fun, _ := n.Fun.(*ast.Ident); fun != nil && fun.Name == "sprintf" && len(n.Args) >= 4 /* ... args */ {
+			if isIdent(n.Fun, "sprintf") && len(n.Args) >= 4 /* ... args */ {
 				n.Args = insert(n.Args, 1, newIdent(n.Args[1].Pos(), "nil"))
 				return false
 			}
 		}
 		return true
 	})
+}
+
+// asIdent returns x as *ast.Ident if it is an identifier with the given name.
+func asIdent(x ast.Node, name string) *ast.Ident {
+	if ident, _ := x.(*ast.Ident); ident != nil && ident.Name == name {
+		return ident
+	}
+	return nil
+}
+
+// isIdent reports whether x is an identifier with the given name.
+func isIdent(x ast.Node, name string) bool {
+	return asIdent(x, name) != nil
 }
 
 // newIdent returns a new identifier with the given position and name.
