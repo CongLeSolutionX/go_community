3ba2e174f453e64cf3811dca7fbc2ca197ce6023
Matt Layher <mdlayher@gmail.com>
2024-02-09 10:34:58 -0500

    slices: add Chunk
    DO NOT REVIEW, staging until package iter is usable in tip.
    
    For #53987.
    
    Change-Id: I508274eca388db39550eb9e4d8abd5ce68d29d8d

--- src/slices/example_test.go
+++ src/slices/example_test.go
@@ -318,3 +318,30 @@ func ExampleSortStableFunc() {
 	// Output:
 	// [{Alice 20} {Alice 55} {Bob 24} {Gopher 13}]
 }
+
+func ExampleChunk() {
+	type Person struct {
+		Name string
+		Age  int
+	}
+
+	type People []Person
+
+	people := People{
+		{"Gopher", 13},
+		{"Alice", 20},
+		{"Bob", 5},
+		{"Vera", 24},
+		{"Zac", 15},
+	}
+
+	// Chunk people into []Person 2 elements at a time.
+	for c := range slices.Chunk(people, 2) {
+		fmt.Println(c)
+	}
+
+	// Output:
+	// [{Gopher 13} {Alice 20}]
+	// [{Bob 5} {Vera 24}]
+	// [{Zac 15}]
+}
