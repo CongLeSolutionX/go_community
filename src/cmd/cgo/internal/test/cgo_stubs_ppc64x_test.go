290c77c5a0413b30b08d42d7f2980da7ea317cf2
Paul E. Murphy <murp@ibm.com>
2024-02-14 16:42:27 -0600

    cmd/cgo/internal/test: disable issue8694 and issue9510 on ppc64x
    These link in some libgcc functions which require more advanced
    handling of small toc relocations within the internal linker.
    
    For now, just disable them on all ppc64/ppc64le targets.
    
    Change-Id: Idd4a0264bfbbd7016472120dc4bee84814e8c2eb

---
+++ src/cmd/cgo/internal/test/cgo_stubs_ppc64x_test.go
@@ -0,0 +1,15 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build ppc64 || ppc64le
+
+package cgotest
+
+import "testing"
+
+// If gcc is used, and linking internally, __mulsc3 and __muldc3
+// will be linked in from libgcc which make several R_PPC64_TOC16_DS
+// relocations which may not be resolvable with the internal linker.
+func test8694(t *testing.T) {}
+func test9510(t *testing.T) {}
