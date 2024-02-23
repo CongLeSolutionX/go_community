809893eccd055931df4baafbe53c04c4b34c52f1
Ian Lance Taylor <iant@golang.org>
2024-02-23 12:18:20 -0800

    net: skip TestReadLine if /etc/services does not exist
    Fixes #65905
    
    Change-Id: I42326d3b743e80d5226ea374eca5c9edaef4dd4d

--- src/net/parse_test.go
+++ src/net/parse_test.go
@@ -22,7 +22,8 @@ func TestReadLine(t *testing.T) {
 
 	fd, err := os.Open(filename)
 	if err != nil {
-		t.Fatal(err)
+		// The file is missing even on some Unix systems.
+		t.Skipf("skipping because /etc/services does not exist")
 	}
 	defer fd.Close()
 	br := bufio.NewReader(fd)
