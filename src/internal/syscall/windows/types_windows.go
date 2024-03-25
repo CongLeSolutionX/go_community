ce91e8ef5422622ba2ab9f5506d2b0b86a0010bc
qmuntal <quimmuntal@gmail.com>
2024-03-25 14:35:50 +0100

    os: implement RemoveAll using handles on Windows
    DO NOT REVIEW
    
    Change-Id: I2dfbf43a8c2be67de082d6b869b636c40bdf6bff

--- src/internal/syscall/windows/types_windows.go
+++ src/internal/syscall/windows/types_windows.go
@@ -4,9 +4,80 @@
 
 package windows
 
+import (
+	"syscall"
+)
+
+// NTStatus corresponds with NTSTATUS, error values returned by ntdll.dll and
+// other native functions.
+type NTStatus uint32
+
+func (s NTStatus) Errno() syscall.Errno {
+	return rtlNtStatusToDosError(s)
+}
+
+func (s NTStatus) Error() string {
+	return s.Errno().Error()
+}
+
 // Socket related.
 const (
 	TCP_KEEPIDLE  = 0x03
 	TCP_KEEPCNT   = 0x10
 	TCP_KEEPINTVL = 0x11
+)
+
+const (
+	FILE_DISPOSITION_DELETE                    = 0x00000001
+	FILE_DISPOSITION_POSIX_SEMANTICS           = 0x00000002
+	FILE_DISPOSITION_IGNORE_READONLY_ATTRIBUTE = 0x00000010
+)
+
+type FILE_DISPOSITION_INFORMATION_EX struct {
+	Flags uint32
+}
+
+type OBJECT_ATTRIBUTES struct {
+	Length             uint32
+	RootDirectory      syscall.Handle
+	ObjectName         *UnicodeString
+	Attributes         uint32
+	SecurityDescriptor uintptr
+	SecurityQoS        uintptr
+}
+
+// Values for the Attributes member of OBJECT_ATTRIBUTES.
+const (
+	OBJ_DONT_REPARSE = 0x00001000
+)
+
+type UnicodeString struct {
+	Length        uint16
+	MaximumLength uint16
+	Buffer        *uint16
+}
+
+type IO_STATUS_BLOCK struct {
+	Status      NTStatus
+	Information uintptr
+}
+
+// CreateDisposition flags for NtCreateFile and NtCreateNamedPipeFile.
+const (
+	FILE_OPEN = 0x00000001
+)
+
+// CreateOptions flags for NtCreateFile and NtCreateNamedPipeFile.
+const (
+	FILE_SYNCHRONOUS_IO_NONALERT = 0x00000020
+	FILE_OPEN_FOR_BACKUP_INTENT  = 0x00004000
+)
+
+type ACCESS_MASK uint32
+
+// Constants for type ACCESS_MASK
+const (
+	FILE_READ_ATTRIBUTES = 0x00000080
+	FILE_WRITE_EA        = 0x00000010
+	DELETE               = 0x00010000
 )
