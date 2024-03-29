8c31beb7c959906f81db0921ccb082c2c1c3948e
Alan Donovan <adonovan@google.com>
2024-03-29 15:49:38 -0400

    cmd: update vendored x/tools to 904c6ba
    This fixes one of the many obstacles to enabling
    types.Alias by updating vet.
    
    The 'loopclosure' checker (formerly 'rangeloop') no longer
    reports any findings with go1.22, so the test needed work
    to ensure that it still runs on files with go1.21 semantics.
    
    Updates #65294
    
    Change-Id: I987ff529d4e165b56b7241563c6003e63bf92bb1

--- src/cmd/vet/vet_test.go
+++ src/cmd/vet/vet_test.go
@@ -78,7 +78,6 @@ func TestVet(t *testing.T) {
 		"method",
 		"nilfunc",
 		"print",
-		"rangeloop",
 		"shift",
 		"slog",
 		"structtag",
@@ -120,6 +119,24 @@ func TestVet(t *testing.T) {
 			errchk(cmd, files, t)
 		})
 	}
+
+	// The loopclosure analyzer (aka "rangeloop" before CL 140578)
+	// is a no-op for files whose version >= go1.22, so we use a
+	// go.mod file in the rangeloop directory to "downgrade".
+	t.Run("loopclosure", func(t *testing.T) {
+		cmd := testenv.Command(t, testenv.GoToolPath(t), "vet", "-vettool="+vetPath(t), ".")
+		// REVIEWERS: GOTOOLCHAIN seems like a sledgehammer.
+		// Why does it not suffice to have a go1.21 go.mod
+		// file and a !go1.22 build tag in the file?
+		cmd.Env = append(os.Environ(), "GOWORK=off", "GOTOOLCHAIN=go1.21.0")
+		cmd.Dir = "testdata/rangeloop"
+		cmd.Stderr = new(strings.Builder) // all vet output goes to stderr
+		cmd.Run()
+		t.Log(cmd.Stderr)
+		if err := errorCheck(cmd.Stderr.(fmt.Stringer).String(), false, "testdata/rangeloop/rangeloop.go", "rangeloop.go"); err != nil {
+			t.Errorf("error check failed: %s", err)
+		}
+	})
 }
 
 func cgoEnabled(t *testing.T) bool {
