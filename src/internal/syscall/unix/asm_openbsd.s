c421055eef2f927142fa4d623e9c57a83a951685
Tobias Klauser <tklauser@distanz.ch>
2024-04-04 11:55:54 +0200

    internal/syscall/unix: implement Eaccess on openbsd
    Like on other BSDs, use faccessat(AT_FDCWD, path, mode, AT_EACCESS)
    
    Change-Id: I80f8d327dd152576165b9206e32dfb749b41d187

---
+++ src/internal/syscall/unix/asm_openbsd.s
@@ -0,0 +1,8 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "textflag.h"
+
+TEXT ·libc_faccessat_trampoline(SB),NOSPLIT,$0-0
+        JMP	libc_faccessat(SB)
