24a5a0f154eda7d2ff953bb2661481eff0c693ee
Andy Pan <i@andypan.me>
2024-04-04 16:30:30 +0800

    net: implement the fast path of setting SOCK_NONBLOCK and SOCK_CLOEXEC on Solaris
    For #59359
    
    Change-Id: I632cc39fc21675dd125137e118731f3eeab65a3b

---
+++ src/internal/syscall/unix/kernel_version_solaris_test.go
@@ -0,0 +1,36 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build solaris
+
+package unix_test
+
+import (
+	"errors"
+	"internal/syscall/unix"
+	"syscall"
+	"testing"
+)
+
+func TestSupportSockNonblockCloexec(t *testing.T) {
+	// Test that SupportSockNonblockCloexec returns true if socket succeeds with SOCK_NONBLOCK and SOCK_CLOEXEC.
+	s, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM|syscall.SOCK_NONBLOCK|syscall.SOCK_CLOEXEC, 0)
+	if err == nil {
+		syscall.Close(s)
+	}
+	want := !errors.Is(err, syscall.EPROTONOSUPPORT) && !errors.Is(err, syscall.EINVAL)
+	got := unix.SupportSockNonblockCloexec()
+	if want != got {
+		t.Fatalf("SupportSockNonblockCloexec, got %t; want %t", got, want)
+	}
+
+	// Test that the version returned by KernelVersion matches expectations.
+	major, minor := unix.KernelVersion()
+	if got && (major < 11 || (major == 11 && minor < 4)) {
+		t.Fatalf("SupportSockNonblockCloexec is true, but kernel version is older than 11.4, kernel: %d.%d", major, minor)
+	}
+	if !got && (major > 11 || (major == 11 && minor >= 4)) {
+		t.Errorf("SupportSockNonblockCloexec is false, but kernel version is newer than 11.4, kernel: %d.%d", major, minor)
+	}
+}
