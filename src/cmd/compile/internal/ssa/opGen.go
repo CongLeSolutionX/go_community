752951f582d0a9105ddbab57505cba05fe7c54cd
Guoqi Chen <chenguoqi@loongson.cn>
2024-04-09 15:05:05 +0800

    cmd/compile/internal: intrinsify publicationBarrier on loong64
    The publication barrier is a StoreStore barrier, which is implemented
    by "DBAR 0x1A" on loong64.
    
    Ref: https://loongson.github.io/LoongArch-Documentation/LoongArch-Vol1-EN.html
    
    goos: linux
    goarch: loong64
    pkg: runtime
    cpu: Loongson-3A6000-HV @ 2500.00MHz
                         │   bench.old   │  bench.new                            │
                         │    sec/op     │   sec/op        vs base               │
    Malloc8                 31.76n ± 0%     22.79n ± 0%   -28.24% (p=0.000 n=20)
    Malloc8-2               25.46n ± 0%     18.33n ± 0%   -28.00% (p=0.000 n=20)
    Malloc8-4               25.75n ± 0%     18.43n ± 0%   -28.41% (p=0.000 n=20)
    Malloc16                62.97n ± 0%     42.41n ± 0%   -32.65% (p=0.000 n=20)
    Malloc16-2              49.11n ± 0%     31.68n ± 0%   -35.50% (p=0.000 n=20)
    Malloc16-4              49.64n ± 1%     31.95n ± 0%   -35.62% (p=0.000 n=20)
    MallocTypeInfo8         58.57n ± 0%     46.51n ± 0%   -20.61% (p=0.000 n=20)
    MallocTypeInfo8-2       51.43n ± 0%     38.01n ± 0%   -26.09% (p=0.000 n=20)
    MallocTypeInfo8-4       51.65n ± 0%     38.15n ± 0%   -26.13% (p=0.000 n=20)
    MallocTypeInfo16        68.07n ± 0%     51.62n ± 0%   -24.17% (p=0.000 n=20)
    MallocTypeInfo16-2      54.73n ± 0%     41.13n ± 0%   -24.85% (p=0.000 n=20)
    MallocTypeInfo16-4      55.05n ± 0%     41.28n ± 0%   -25.02% (p=0.000 n=20)
    MallocLargeStruct       491.5n ± 0%     454.8n ± 0%    -7.47% (p=0.000 n=20)
    MallocLargeStruct-2     351.8n ± 1%     323.8n ± 0%    -7.94% (p=0.000 n=20)
    MallocLargeStruct-4     333.6n ± 0%     316.7n ± 0%    -5.10% (p=0.000 n=20)
    geomean                 71.01n          53.78n        -24.26%
    
    Updates #59120.
    
    Change-Id: Ica0c89db6f2bebd55d9b3207a1c462a9454e9268

--- src/cmd/compile/internal/ssa/opGen.go
+++ src/cmd/compile/internal/ssa/opGen.go
@@ -1867,6 +1867,7 @@ const (
 	OpLOONG64LoweredGetCallerSP
 	OpLOONG64LoweredGetCallerPC
 	OpLOONG64LoweredWB
+	OpLOONG64LoweredPubBarrier
 	OpLOONG64LoweredPanicBoundsA
 	OpLOONG64LoweredPanicBoundsB
 	OpLOONG64LoweredPanicBoundsC
@@ -25137,6 +25138,13 @@ var opcodeTable = [...]opInfo{
 				{0, 268435456}, // R29
 			},
 		},
+	},
+	{
+		name:           "LoweredPubBarrier",
+		argLen:         1,
+		hasSideEffects: true,
+		asm:            loong64.ADBAR,
+		reg:            regInfo{},
 	},
 	{
 		name:    "LoweredPanicBoundsA",
