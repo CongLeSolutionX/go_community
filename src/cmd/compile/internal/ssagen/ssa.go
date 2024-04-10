d16ccbc2532d29dfd1683f85a9ac591b57c1106e
David Chase <drchase@google.com>
2024-04-10 12:50:03 -0400

    cmd/compile, cmd/internal: more fiddling with loop alignment
    Do not submit
    
    Change-Id: Id51a056f1672f8095e8f755e01f72836c9686aa3

--- src/cmd/compile/internal/ssagen/ssa.go
+++ src/cmd/compile/internal/ssagen/ssa.go
@@ -7303,23 +7303,22 @@ func genssa(f *ssa.Func, pp *objw.Progs) {
 
 	var argLiveIdx int = -1 // argument liveness info index
 
-	const hot_align = 64
-	const hot_pad = 0
+	// These control cache line alignment; if the required portion of
+	// a cache line is not available, then pad to obtain cache line
+	// alignment.  Not implemented on all architectures, may not be
+	// useful on all architectures.  Does benchmark to help amd64.
+	var hotAlign, hotRequire int64
+	var warmAlign, warmRequire int64
 
-	nopSize := 4
-	if base.Ctxt.Arch.Name == "amd64" || base.Ctxt.Arch.Name == "386" {
-		nopSize = 1
-	} else if base.Ctxt.Arch.Name == "wasm" {
-		nopSize = 0
-	}
+	switch base.Ctxt.Arch.Name {
+	// enable this on a case-by-case basis, with benchmarking.
+	// currently shown good for amd64, testing on arm64
+	case "amd64", "386", "arm64":
+		hotAlign = 64
+		hotRequire = 63
 
-	pad := func() {
-		if nopSize <= 0 {
-			return
-		}
-		for i := 0; i < hot_pad; i += nopSize {
-			Arch.Ginsnop(s.pp)
-		}
+		warmAlign = 32
+		warmRequire = 8
 	}
 
 	// Emit basic blocks
@@ -7328,10 +7327,15 @@ func genssa(f *ssa.Func, pp *objw.Progs) {
 		s.lineRunStart = nil
 		s.SetPos(s.pp.Pos.WithNotStmt()) // It needs a non-empty Pos, but cannot be a statement boundary (yet).
 
-		if b.GoodToAlign {
-			p := s.pp.Prog(obj.APCALIGN)
-			p.From.SetConst(hot_align)
-			pad()
+		// Currently duplicating what was here before, not doing the nonPGO case
+		if hotAlign > 0 && b.Hotness > ssa.NotHot {
+			align, require := hotAlign, hotRequire
+			if b.Hotness == ssa.LHNotThruIn {
+				align, require = warmAlign, warmRequire
+			}
+			p := s.pp.Prog(obj.APCALIGNIF)
+			p.From.SetConst(align)
+			p.To.SetConst(require)
 		}
 
 		s.bstart[b.ID] = s.pp.Next
@@ -7494,7 +7498,8 @@ func genssa(f *ssa.Func, pp *objw.Progs) {
 		// going to emit anyway, and use those instructions instead of the
 		// inline marks.
 		for p := s.pp.Text; p != nil; p = p.Link {
-			if p.As == obj.ANOP || p.As == obj.AFUNCDATA || p.As == obj.APCDATA || p.As == obj.ATEXT || p.As == obj.APCALIGN || Arch.LinkArch.Family == sys.Wasm {
+			if p.As == obj.ANOP || p.As == obj.AFUNCDATA || p.As == obj.APCDATA || p.As == obj.ATEXT ||
+				p.As == obj.APCALIGN || p.As == obj.APCALIGNIF || Arch.LinkArch.Family == sys.Wasm {
 				// Don't use 0-sized instructions as inline marks, because we need
 				// to identify inline mark instructions by pc offset.
 				// (Some of these instructions are sometimes zero-sized, sometimes not.
