cb0f84fa728b8a32e7dcba122dd7a67d8cd1c025
David Chase <drchase@google.com>
2024-04-11 17:22:53 -0400

    cmd/compile: changes to add structs.HostLayout
    This is for the proposal, and does not include
    any wasm32-specific code; that comes next.
    
    Updates #66408.
    Updates #63131.
    
    Change-Id: I3a66e09d707dd579c59f155e7f53367f41214c30

---
+++ src/structs/hostlayout.go
@@ -0,0 +1,13 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package structs defines types used as directives
+// when they appear as field types within a structure.
+
+package structs
+
+// HostLayout, as a field type, signals that the size, alignment,
+// and order of fields conform to requirements of the host
+// platform and may not match the Go compiler’s defaults.
+type HostLayout struct{}
