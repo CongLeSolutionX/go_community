b0ecc521dcc385fdb5ade9454e881801049000a3
Guoqi Chen <chenguoqi@loongson.cn>
2024-04-18 20:22:08 +0800

    cmd/compiler,internal/runtime/atomic: optimize Store{64,32,8} on loong64
    Use Loong64's atomic operation instruction AMSWAPDBx to implement Store{64,32}.
    
    The StoreRelease barrier on Loong64 is "dbar 0x12", using the correct barrier
    in Store8 implementation can improve performance.
    
    goos: linux
    goarch: loong64
    pkg: internal/runtime/atomic
    cpu: Loongson-3A5000 @ 2500.00MHz
                     |   bench.old   |   bench.new                          |
                     |   sec/op      |   sec/op       vs base               |
    AtomicStore64       19.62n ± 0%     13.61n ± 0%   -30.63% (p=0.000 n=20)
    AtomicStore64-2     19.62n ± 0%     13.61n ± 0%   -30.63% (p=0.000 n=20)
    AtomicStore64-4     20.03n ± 1%     13.61n ± 0%   -32.03% (p=0.000 n=20)
    AtomicStore         19.61n ± 0%     13.61n ± 0%   -30.57% (p=0.000 n=20)
    AtomicStore-2       19.62n ± 0%     13.61n ± 0%   -30.61% (p=0.000 n=20)
    AtomicStore-4       19.63n ± 0%     13.61n ± 0%   -30.64% (p=0.000 n=20)
    AtomicStore8        19.96n ± 2%     10.41n ± 0%   -47.85% (p=0.000 n=20)
    AtomicStore8-2      19.63n ± 0%     10.41n ± 0%   -46.97% (p=0.000 n=20)
    AtomicStore8-4      19.62n ± 0%     10.41n ± 0%   -46.94% (p=0.000 n=20)
    geomean             19.70n          12.45n        -36.82%
    
    goos: linux
    goarch: loong64
    pkg: internal/runtime/atomic
    cpu: Loongson-3A6000-HV @ 2500.00MHz
                     |   bench.old   |   bench.new                          |
                     |   sec/op      |   sec/op        vs base              |
    AtomicStore64       18.01n ± 0%     12.81n ± 0%   -28.87% (p=0.000 n=20)
    AtomicStore64-2     18.01n ± 0%     12.81n ± 0%   -28.87% (p=0.000 n=20)
    AtomicStore64-4     18.01n ± 0%     12.81n ± 0%   -28.87% (p=0.000 n=20)
    AtomicStore         18.02n ± 0%     12.81n ± 0%   -28.91% (p=0.000 n=20)
    AtomicStore-2       18.02n ± 0%     12.81n ± 0%   -28.91% (p=0.000 n=20)
    AtomicStore-4       18.01n ± 0%     12.81n ± 0%   -28.87% (p=0.000 n=20)
    AtomicStore8        18.010n ± 0%    5.203n ± 0%   -71.11% (p=0.000 n=20)
    AtomicStore8-2      18.010n ± 0%    5.203n ± 0%   -71.11% (p=0.000 n=20)
    AtomicStore8-4      18.010n ± 0%    5.203n ± 0%   -71.11% (p=0.000 n=20)
    geomean             18.01n          9.487n        -47.33%
    
    Change-Id: I4ae5e8dd0e6f026129b6e503990a763ed40c6097

--- src/cmd/compile/internal/ssa/opGen.go
+++ src/cmd/compile/internal/ssa/opGen.go
@@ -1840,8 +1840,6 @@ const (
 	OpLOONG64LoweredAtomicStore8
 	OpLOONG64LoweredAtomicStore32
 	OpLOONG64LoweredAtomicStore64
-	OpLOONG64LoweredAtomicStorezero32
-	OpLOONG64LoweredAtomicStorezero64
 	OpLOONG64LoweredAtomicExchange32
 	OpLOONG64LoweredAtomicExchange64
 	OpLOONG64LoweredAtomicAnd32
@@ -24660,8 +24658,8 @@ var opcodeTable = [...]opInfo{
 		hasSideEffects: true,
 		reg: regInfo{
 			inputs: []inputInfo{
-				{1, 1073741816},          // R4 R5 R6 R7 R8 R9 R10 R11 R12 R13 R14 R15 R16 R17 R18 R19 R20 R21 g R23 R24 R25 R26 R27 R28 R29 R31
-				{0, 4611686019501129724}, // SP R4 R5 R6 R7 R8 R9 R10 R11 R12 R13 R14 R15 R16 R17 R18 R19 R20 R21 g R23 R24 R25 R26 R27 R28 R29 R31 SB
+				{1, 522240},              // R12 R13 R14 R15 R16 R17 R18 R19
+				{0, 4611686018429487100}, // SP R4 R5 R6 R7 R8 R9 R10 R11 g SB
 			},
 		},
 	},
@@ -24672,8 +24670,8 @@ var opcodeTable = [...]opInfo{
 		hasSideEffects: true,
 		reg: regInfo{
 			inputs: []inputInfo{
-				{1, 1073741816},          // R4 R5 R6 R7 R8 R9 R10 R11 R12 R13 R14 R15 R16 R17 R18 R19 R20 R21 g R23 R24 R25 R26 R27 R28 R29 R31
-				{0, 4611686019501129724}, // SP R4 R5 R6 R7 R8 R9 R10 R11 R12 R13 R14 R15 R16 R17 R18 R19 R20 R21 g R23 R24 R25 R26 R27 R28 R29 R31 SB
+				{1, 522240},              // R12 R13 R14 R15 R16 R17 R18 R19
+				{0, 4611686018429487100}, // SP R4 R5 R6 R7 R8 R9 R10 R11 g SB
 			},
 		},
 	},
@@ -24684,30 +24682,8 @@ var opcodeTable = [...]opInfo{
 		hasSideEffects: true,
 		reg: regInfo{
 			inputs: []inputInfo{
-				{1, 1073741816},          // R4 R5 R6 R7 R8 R9 R10 R11 R12 R13 R14 R15 R16 R17 R18 R19 R20 R21 g R23 R24 R25 R26 R27 R28 R29 R31
-				{0, 4611686019501129724}, // SP R4 R5 R6 R7 R8 R9 R10 R11 R12 R13 R14 R15 R16 R17 R18 R19 R20 R21 g R23 R24 R25 R26 R27 R28 R29 R31 SB
-			},
-		},
-	},
-	{
-		name:           "LoweredAtomicStorezero32",
-		argLen:         2,
-		faultOnNilArg0: true,
-		hasSideEffects: true,
-		reg: regInfo{
-			inputs: []inputInfo{
-				{0, 4611686019501129724}, // SP R4 R5 R6 R7 R8 R9 R10 R11 R12 R13 R14 R15 R16 R17 R18 R19 R20 R21 g R23 R24 R25 R26 R27 R28 R29 R31 SB
-			},
-		},
-	},
-	{
-		name:           "LoweredAtomicStorezero64",
-		argLen:         2,
-		faultOnNilArg0: true,
-		hasSideEffects: true,
-		reg: regInfo{
-			inputs: []inputInfo{
-				{0, 4611686019501129724}, // SP R4 R5 R6 R7 R8 R9 R10 R11 R12 R13 R14 R15 R16 R17 R18 R19 R20 R21 g R23 R24 R25 R26 R27 R28 R29 R31 SB
+				{1, 522240},              // R12 R13 R14 R15 R16 R17 R18 R19
+				{0, 4611686018429487100}, // SP R4 R5 R6 R7 R8 R9 R10 R11 g SB
 			},
 		},
 	},
