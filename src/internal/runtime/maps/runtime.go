57e6d2272ca5589b8cd0a8db96c85604689b01c2
Michael Pratt <mpratt@google.com>
2024-04-22 15:48:57 -0400

    internal/runtime/maps: initial swiss table map implementation
    Add a new package that will contain a new "Swiss Table"
    (https://abseil.io/about/design/swisstables) map implementation, which
    is intended to eventually replace the existing runtime map
    implementation.
    
    This implementation is based on the fabulous
    github.com/cockroachdb/swiss package contributed by Peter Mattis.
    
    This CL adds an extremely minimal hash map implementation. It supports
    insert and lookup on fixed-capacity maps. It does not support deletion,
    growth, or iteration.
    
    ----
    
    A slot is
    
    type slot struct{
      key  Key
      elem Elem
    }
    
    If Elem is struct{}, the type generated by the compiler reserves at
    least 1 byte for Elem (in case we create pointers to elem).
    
    The size computation is slotSize did not take this special case into
    account, treating elem as taking 0 bytes.
    
    This didn't cause immediate failures. Reads and writes to the slot were
    consistent in this inaccuracy, so things generally worked OK.
    
    They only went wrong when the GC ignored values in the memory locations
    that should be Elem values. Pointers there would be ignored, resulting
    in objects getting freed by the GC too early.
    
    For #54766.
    
    Change-Id: I52cf371448c3817d471ddb1f5a78f3513565db41

---
+++ src/internal/runtime/maps/runtime.go
@@ -0,0 +1,22 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package maps
+
+import (
+	"internal/abi"
+	"unsafe"
+)
+
+//go:linkname fastrand64 runtime.fastrand64
+func fastrand64() uint64
+
+//go:linkname typedmemmove runtime.typedmemmove
+func typedmemmove(typ *abi.Type, dst, src unsafe.Pointer)
+
+//go:linkname typedmemclr runtime.typedmemclr
+func typedmemclr(typ *abi.Type, ptr unsafe.Pointer)
+
+//go:linkname newarray runtime.newarray
+func newarray(typ *abi.Type, n int) unsafe.Pointer
