21292e9533271d349d6cd35dd28ed7dad999368d
Felix Geisendörfer <felix.geisendoerfer@datadoghq.com>
2024-04-27 13:41:05 +0200

    runtime: make profstackdepth a GODEBUG option
    This will allow users to decrease the profiling stack depth back to 32
    if in case they experience any problems with the new default of 128. For
    example, this could be used for go programs that expect
    StackRecord.Stack() to return no more than 32 frames or that the
    returned slice is always backed by the Stack0 array.
    
    Change-Id: Ieaab2513024915a223239278dd97a6e161dde1cf

--- src/runtime/proc.go
+++ src/runtime/proc.go
@@ -818,6 +818,9 @@ func schedinit() {
 		MemProfileRate = 0
 	}
 
+	// mcommoninit runs before parsedebugvars, so init profstacks again.
+	mProfStackInit(gp.m)
+
 	lock(&sched.lock)
 	sched.lastpoll.Store(nanotime())
 	procs := ncpu
@@ -926,8 +929,8 @@ func mcommoninit(mp *m, id int64) {
 }
 
 func mProfStackInit(mp *m) {
-	mp.profStack = make([]uintptr, maxStack)
-	mp.mLockProfile.stack = make([]uintptr, maxStack)
+	mp.profStack = make([]uintptr, debug.profstackdepth)
+	mp.mLockProfile.stack = make([]uintptr, debug.profstackdepth)
 }
 
 func (mp *m) becomeSpinning() {
