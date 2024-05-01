johanbrandhorst/go

Introduce wasm32 compiler architecture target
Change-Id: Ide329e333be4ed2f66fe9be43da59ff8fa3a66e8

src/cmd/asm/internal/arch/arch.go
@@ -15,6 +15,7 @@ import (
 	"cmd/internal/obj/riscv"
 	"cmd/internal/obj/s390x"
 	"cmd/internal/obj/wasm"
+	"cmd/internal/obj/wasm32"
 	"cmd/internal/obj/x86"
 	"fmt"
 	"strings"
@@ -81,6 +82,8 @@ func Set(GOARCH string, shared bool) *Arch {
 		return archS390x()
 	case "wasm":
 		return archWasm()
+	case "wasm32":
+		return archWasm32()
 	}
 	return nil
 }
@@ -753,6 +756,27 @@ func archWasm() *Arch {
 		LinkArch:       &wasm.Linkwasm,
 		Instructions:   instructions,
 		Register:       wasm.Register,
+		RegisterPrefix: nil,
+		RegisterNumber: nilRegisterNumber,
+		IsJump:         jumpWasm,
+	}
+}
+
+func archWasm32() *Arch {
+	instructions := make(map[string]obj.As)
+	for i, s := range obj.Anames {
+		instructions[s] = obj.As(i)
+	}
+	for i, s := range wasm32.Anames {
+		if obj.As(i) >= obj.A_ARCHSPECIFIC {
+			instructions[s] = obj.As(i) + obj.ABaseWasm32
+		}
+	}
+
+	return &Arch{
+		LinkArch:       &wasm32.Linkwasm,
+		Instructions:   instructions,
+		Register:       wasm32.Register,
 		RegisterPrefix: nil,
 		RegisterNumber: nilRegisterNumber,
 		IsJump:         jumpWasm,

CongLeSolutionX/go_community

src/cmd/asm/internal/arch/arch.go
@@ -81,6 +81,8 @@ func Set(GOARCH string, shared bool) *Arch {
 		return archS390x()
 	case "wasm":
 		return archWasm()
+	case "wasm32":
+		return archWasm32()
 	}
 	return nil
 }
@@ -751,6 +753,27 @@ func archWasm() *Arch {
 
 	return &Arch{
 		LinkArch:       &wasm.Linkwasm,
+		Instructions:   instructions,
+		Register:       wasm.Register,
+		RegisterPrefix: nil,
+		RegisterNumber: nilRegisterNumber,
+		IsJump:         jumpWasm,
+	}
+}
+
+func archWasm32() *Arch {
+	instructions := make(map[string]obj.As)
+	for i, s := range obj.Anames {
+		instructions[s] = obj.As(i)
+	}
+	for i, s := range wasm.Anames {
+		if obj.As(i) >= obj.A_ARCHSPECIFIC {
+			instructions[s] = obj.As(i) + obj.ABaseWasm
+		}
+	}
+
+	return &Arch{
+		LinkArch:       &wasm.Linkwasm32,
 		Instructions:   instructions,
 		Register:       wasm.Register,
 		RegisterPrefix: nil,