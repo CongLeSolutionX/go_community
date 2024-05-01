623d045e346ffdbb5605e25d70c2d3914c80e627
Michael Matloob <matloob@golang.org>
2024-05-01 13:51:20 -0400

    cmd/go: remove checks that all counters incremented are in counters.txt
    This change removes cmd/go/testdata/counters.txt. It also removes the
    code that prepares it and checks that it contains all registered
    counters as well as counters for all flags and subcommands. It removes
    the counter registration mechanism, and uses telemetry.NewCounter to
    create new counters instead. It keeps the tests that check that at least
    one counter is incremented if the go command is invoked in a script test.
    
    Change-Id: Ic6bda5c64e90f0dd7e221968fce0e375e84d6e17

--- src/cmd/go/internal/toolchain/select.go
+++ src/cmd/go/internal/toolchain/select.go
@@ -26,6 +26,7 @@ import (
 	"cmd/go/internal/modload"
 	"cmd/go/internal/run"
 	"cmd/go/internal/work"
+	"cmd/internal/telemetry"
 
 	"golang.org/x/mod/module"
 )
@@ -81,7 +82,7 @@ func FilterEnv(env []string) []string {
 	return out
 }
 
-var counterErrorsInvalidToolchainInFile = base.NewCounter("go/errors:invalid-toolchain-in-file")
+var counterErrorsInvalidToolchainInFile = telemetry.NewCounter("go/errors:invalid-toolchain-in-file")
 
 // Select invokes a different Go toolchain if directed by
 // the GOTOOLCHAIN environment variable or the user's configuration
