7427f01b1f4eb3bd42c8d82cee1043fb54e66c31
Michael Matloob <matloob@golang.org>
2024-05-08 15:54:12 -0400

    cmd/go: add go telemetry command and support in env and clean
    Add the go telemetry command to support setting and viewing the
    telemetry mode. Also add the non-settable GOTELEMETRY and GOTELEMETRYDIR
    variables to go env, which contain the mode and telemetry dir, and go
    clean -telemetry for cleaning counter files and reports from the
    telemetry directory.
    
    Change-Id: Id7e89cefe30acfe3d865fa467315fe7cda975de9

--- src/cmd/internal/telemetry/telemetry_bootstrap.go
+++ src/cmd/internal/telemetry/telemetry_bootstrap.go
@@ -19,3 +19,7 @@ func NewCounter(name string) dummyCounter                                 { retu
 func NewStackCounter(name string, depth int) dummyCounter                 { return dummyCounter{} }
 func CountFlags(name string, flagSet flag.FlagSet)                        {}
 func CountFlagValue(prefix string, flagSet flag.FlagSet, flagName string) {}
+func Mode() string                                                        { return "" }
+func ModeEffective() time.Time                                            { return time.Time{} }
+func SetMode(mode string) error                                           { return nil }
+func Dir() string                                                         { return "" }
