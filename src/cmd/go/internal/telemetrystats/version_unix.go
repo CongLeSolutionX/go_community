dd6f11ce1ea2137c73c04aa80e5b9bd773014378
Michael Matloob <matloob@golang.org>
2024-05-20 10:38:40 -0400

    cmd/go: add additional counters for mode and host/target OS and arch
    The following counters are added:
    (* means we will record the actual value for the counter, but of course
    the config will limit us to collecting preknown values)
    
        go/mode:{gopath,workspace,module}
        go/platform/{host,target}/{goos,goarch}:*
        go/platform/target/{
            go386,goamd64,goarm,goarm64,gomips,goppc64,goriscv64,gowasm}:*
        For windows and unix:
            go/platform/host/*/version:*
            go/platform/host/*/major-version:*-*
        For windows:
            go/platform/host/windows/build:*
       
    
    Change-Id: I3c865afede2382bae103e5b4b9d1aa6b20c123df

---
+++ src/cmd/go/internal/telemetrystats/version_unix.go
@@ -0,0 +1,49 @@
+package telemetrystats
+
+import (
+	"bytes"
+	"fmt"
+	"runtime"
+	"strings"
+
+	"cmd/internal/telemetry"
+
+	"golang.org/x/sys/unix"
+)
+
+func incrementVersionCounters() {
+	convert := func(nullterm []byte) string {
+		end := bytes.IndexByte(nullterm, 0)
+		if end < 0 {
+			end = len(nullterm)
+		}
+		return string(nullterm[:end])
+	}
+
+	var v unix.Utsname
+	err := unix.Uname(&v)
+	if err != nil {
+		telemetry.Inc("go/platform/host/%d/version:unknown")
+		return
+	}
+	major, minor, ok := majorMinor(convert(v.Sysname[:]))
+	if !ok {
+		telemetry.Inc("go/platform/host/%d/version:unknown")
+		return
+	}
+	telemetry.Inc(fmt.Sprintf("go/platform/host/%d/major-version:%d-%d", runtime.GOOS, major))
+	telemetry.Inc(fmt.Sprintf("go/platform/host/%d/version:%d-%d", runtime.GOOS, major, minor))
+
+}
+
+func majorMinor(v string) (string, string, bool) {
+	firstDot := strings.Index(v, ".")
+	if firstDot < 0 {
+		return "", "", false
+	}
+	major := v[:firstDot]
+	v = v[firstDot+1:]
+	secondDot := strings.Index(v, ".")
+	minor := v[:secondDot]
+	return major, minor, true
+}
