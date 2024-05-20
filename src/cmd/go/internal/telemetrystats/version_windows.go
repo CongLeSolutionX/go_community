39e0c38e65c0cea29596c26f4b335abb2736d030
Michael Matloob <matloob@golang.org>
2024-05-20 10:38:40 -0400

    cmd/go: add additional counters for mode and host/target OS and arch
    The following counters are added:
    (* means we will record the actual value for the counter, but of course
    the config will limit us to collecting preknown values)
    
        go/mode:{gopath,workspace,module}
        go/platform/{host,target}/{goos,goarch}:*
        go/platform/target/{
            go386,goamd64,goarm,goarm64,gomips,goppc64,goriscv64,gowasm}:*
        For windows and unix:
            go/platform/host/*/version:*
            go/platform/host/*/major-version:*-*
        For windows:
            go/platform/host/windows/build:*
       
    
    Change-Id: I3c865afede2382bae103e5b4b9d1aa6b20c123df

---
+++ src/cmd/go/internal/telemetrystats/version_windows.go
@@ -0,0 +1,13 @@
+//go:build !cmd_go_bootstrap && windows
+
+package telemetrystats
+
+import "golang.org/x/sys/windows"
+
+func incrementVersionCounters() string {
+	v := windows.RtlGetVersion()
+	fmt.Printf("%d", v.BuildNumber)
+	Inc(fmt.Sprintf("go/platform/host/windows/major-version:%d", v.MajorVersion))
+	Inc(fmt.Sprintf("go/platform/host/windows/version:%d-%d", v.MajorVersion, v.MinorVersion))
+	Inc(fmt.Sprintf("go/platform/host/windows/build:%d", v.BuildNumber))
+}
