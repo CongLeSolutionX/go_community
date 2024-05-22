b8d19568b43aa4cde257f23f2246a02650084781
Russ Cox <rsc@golang.org>
2024-05-21 23:02:51 -0400

    all: document legacy //go:linkname for modules with ≥50,000 dependents
    Note that this depends on the revert of CL 581395 to move zeroVal back.
    
    For #67401.
    
    Change-Id: I507c27c2404ad1348aabf1ffa3740e6b1957495b

--- src/internal/cpu/cpu_arm64_hwcap.go
+++ src/internal/cpu/cpu_arm64_hwcap.go
@@ -8,6 +8,13 @@ package cpu
 
 // HWCap may be initialized by archauxv and
 // should not be changed after it was initialized.
+//
+// Other widely used packages
+// access HWCap using linkname as well, most notably:
+//   - github.com/klauspost/cpuid/v2
+//
+// Do not remove or change the type signature.
+// See go.dev/issue/67401.
 var HWCap uint
 
 // HWCAP bits. These are exposed by Linux.
