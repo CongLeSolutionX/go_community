7e81026ec871ba6fc4dd3c5a97f48189ace8fc48
data-pata <patrik.sjofors@gmail.com>
2024-06-03 00:17:30 +0200

    bufio: improve doc comment for Scan
    Clarifies by improving the grammar. Also reflows
    the whole comment for consistent line length.
    
    Change-Id: Id5e8b0944d49318edec492915e4e3ce2b6494b9d

--- src/bufio/scan.go
+++ src/bufio/scan.go
@@ -127,15 +127,15 @@ func (s *Scanner) Text() string {
 // See the emptyFinalToken example for a use of this value.
 var ErrFinalToken = errors.New("final token")
 
-// Scan advances the [Scanner] to the next token, which will then be
-// available through the [Scanner.Bytes] or [Scanner.Text] method. It returns false when
-// there are no more tokens, either by reaching the end of the input or an error.
+// Scan advances the [Scanner] to the next token, which will then be available
+// through the [Scanner.Bytes] or [Scanner.Text] method. It returns false when
+// there are no more tokens, either by reaching the end of the input or an
+// error.
 // After Scan returns false, the [Scanner.Err] method will return any error that
-// occurred during scanning, except that if it was [io.EOF], [Scanner.Err]
-// will return nil.
-// Scan panics if the split function returns too many empty
-// tokens without advancing the input. This is a common error mode for
-// scanners.
+// occurred during scanning. Unless the error was [io.EOF], [Scanner.Err] will
+// return nil.
+// Scan panics if the split function returns too many empty tokens without
+// advancing the input. This is a common error mode for scanners.
 func (s *Scanner) Scan() bool {
 	if s.done {
 		return false
