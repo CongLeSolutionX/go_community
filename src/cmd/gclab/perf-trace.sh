5a07f107841260b1e28e2f5b6456df1d6edf0c99
Austin Clements <austin@google.com>
2024-06-06 22:17:08 -0400

    cmd/gclab: use GC scan traces
    This tool processes GC scan traces to do various analysis.
    
    Change-Id: I8e46661331f21ea6ef4ea224881b48b25a944c43

---
+++ src/cmd/gclab/perf-trace.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -ex
+
+if [[ x"$VM" = x ]]; then
+    echo "Create VM and set \$VM"
+    exit 1
+fi
+
+gomote push $VM
+gomote run $VM go/src/make.bash
+
+gomote run -system $VM git clone https://go.googlesource.com/benchmarks
+gomote run -dir benchmarks $VM git fetch https://go.googlesource.com/benchmarks refs/changes/70/600070/2
+gomote run -dir benchmarks $VM git checkout FETCH_HEAD
+
+gomote run -dir benchmarks/sweet $VM ./go/bin/go build ./cmd/sweet
+gomote run $VM ./benchmarks/sweet/sweet get
+
+VMWD=$(gomote run -system $VM pwd)
+
+config=$(mktemp)
+cat >>$config <<EOF
+[[config]]
+  name = "trace"
+  goroot = "$VMWD/go"
+  diagnostics = ["trace"]
+
+[[config]]
+  name = "gcrate"
+  goroot = "$VMWD/go"
+  envexec = ["GODEBUG=gcratetrace=1"]
+EOF
+gomote put $VM $config benchmarks/sweet/config.toml
+
+gomote run $VM ./benchmarks/sweet/sweet run -shell -count 1 config.toml
+
+# Times out
+#gomote gettar -dir ./benchmarks/sweet/results $VM
+gomote run -system $VM tar cz benchmarks/sweet/results > $VM.tar.gz
