f52e28dadda8a46027245be345ecf3a9cf24acb6
Austin Clements <austin@google.com>
2024-07-02 13:18:53 -0400

    cmd/gclab/cnet: AVX-ify some kernels
    $ benchstat -col /impl -filter '.unit:(B/s OR pointers/cpu-cycle)' perf_c2s16.bench
    goos: linux
    goarch: amd64
    pkg: cmd/gclab/cnet
    cpu: Intel(R) Xeon(R) CPU @ 3.10GHz
                           │      go       │                  avx2                  │
                           │      B/s      │      B/s       vs base                 │
    Count32/bytes=64-16      6.043Gi ±  0%    4.973Gi ± 0%   -17.71% (p=0.000 n=10)
    Count32/bytes=128-16     6.467Gi ±  0%    7.639Gi ± 0%   +18.12% (p=0.000 n=10)
    Count32/bytes=256-16     6.711Gi ±  0%   11.857Gi ± 0%   +76.67% (p=0.000 n=10)
    Count32/bytes=512-16     6.324Gi ±  1%   15.462Gi ± 0%  +144.50% (p=0.000 n=10)
    Count32/bytes=1024-16    6.643Gi ±  0%   18.531Gi ± 0%  +178.97% (p=0.000 n=10)
    Count32/bytes=2048-16    6.807Gi ±  0%   20.670Gi ± 0%  +203.66% (p=0.000 n=10)
    Count32/bytes=4096-16    6.857Gi ± 10%   21.862Gi ± 0%  +218.82% (p=0.000 n=10)
    Count32/bytes=8192-16    6.907Gi ±  0%   21.921Gi ± 0%  +217.37% (p=0.000 n=10)
    Count32/bytes=16384-16   6.896Gi ±  3%   22.252Gi ± 0%  +222.66% (p=0.000 n=10)
    Count32/bytes=32768-16   6.880Gi ±  2%   22.339Gi ± 0%  +224.68% (p=0.000 n=10)
    geomean                  6.648Gi          15.15Gi       +127.84%
    
                           │         go         │                    avx2                     │
                           │ pointers/cpu-cycle │ pointers/cpu-cycle  vs base                 │
    Count32/bytes=64-16            430.0m ±  0%          353.6m ± 0%   -17.77% (p=0.000 n=10)
    Count32/bytes=128-16           460.4m ±  0%          543.1m ± 0%   +17.96% (p=0.000 n=10)
    Count32/bytes=256-16           477.7m ±  0%          843.0m ± 0%   +76.48% (p=0.000 n=10)
    Count32/bytes=512-16           450.2m ±  0%         1099.0m ± 0%  +144.11% (p=0.000 n=10)
    Count32/bytes=1024-16          472.8m ±  0%         1317.0m ± 0%  +178.52% (p=0.000 n=10)
    Count32/bytes=2048-16          484.4m ±  0%         1470.0m ± 0%  +203.44% (p=0.000 n=10)
    Count32/bytes=4096-16          488.0m ± 10%         1553.5m ± 0%  +218.34% (p=0.000 n=10)
    Count32/bytes=8192-16          491.8m ±  0%         1557.5m ± 0%  +216.69% (p=0.000 n=10)
    Count32/bytes=16384-16         491.1m ±  3%         1583.5m ± 0%  +222.41% (p=0.000 n=10)
    Count32/bytes=32768-16         489.8m ±  2%         1587.5m ± 0%  +224.14% (p=0.000 n=10)
    geomean                        473.2m                 1.077       +127.55%
    
    Change-Id: I4a33467f70c5878f2b6c7571a9124949763bc317

--- src/cmd/gclab/cnet/cnet_test.go
+++ src/cmd/gclab/cnet/cnet_test.go
@@ -7,28 +7,69 @@ package cnet
 import (
 	"fmt"
 	"math/rand/v2"
+	"runtime"
 	"slices"
 	"testing"
+	"time"
+
+	"github.com/aclements/go-perfevent/events"
+	"github.com/aclements/go-perfevent/perf"
 )
 
-func TestBucketSort32(t *testing.T) {
+func TestCount32(t *testing.T) {
 	const iters = 32
 	const bufLen = 4 << 10
 
-	src := make([]uint32, bufLen)
-	dst := make([]uint32, bufLen)
+	// TODO: Test for counter overflow.
+	// TODO: Test buffers that aren't nice lengths.
+
+	src := make([]LAddr32, bufLen/4)
 
 	rnd := rand.New(rand.NewPCG(0, 0))
 
 	for range iters {
 		// Generate random source data.
 		for i := range src {
-			src[i] = rnd.Uint32()
+			src[i] = LAddr32(rnd.Uint32())
+		}
+		// Generate a random shift.
+		shift := rnd.UintN(31)
+
+		// Do the counting.
+		var counts [16]uint16
+		count32AVX2(src, shift, &counts)
+
+		// Reference implementation.
+		var wantCounts [16]uint16
+		for _, val := range src {
+			wantCounts[(val>>shift)&0xF]++
+		}
+		if counts != wantCounts {
+			t.Fatalf("want %v, got %v", wantCounts, counts)
+		}
+	}
+}
+
+func TestBucketSort32(t *testing.T) {
+	const iters = 32
+	const bufLen = 4 << 10
+
+	src := make([]LAddr32, bufLen)
+	dst := make([]LAddr32, bufLen)
+
+	rnd := rand.New(rand.NewPCG(0, 0))
+
+	for range iters {
+		// Generate random source data.
+		for i := range src {
+			src[i] = LAddr32(rnd.Uint32())
 		}
 		// Generate a random shift.
 		shift := rnd.UintN(31)
 
 		// Do the bucketing.
+		//
+		// TODO: Test different implementations
 		var counts [radixBase]int
 		bucketSort(src, dst, &counts, shift)
 
@@ -39,7 +80,7 @@ func TestBucketSort32(t *testing.T) {
 			sub := dst[pos : pos+count]
 			for i, val := range sub {
 				got := (val >> shift) % radixBase
-				if got != uint32(digit) {
+				if got != LAddr32(digit) {
 					t.Fatalf("dst[%d]=%d, got digit %d, want %d", pos+i, val, got, digit)
 				}
 			}
@@ -48,11 +89,27 @@ func TestBucketSort32(t *testing.T) {
 	}
 }
 
-func BenchmarkBucketSort(b *testing.B) {
+var bufBytes = makeBufBytes()
+
+func makeBufBytes() []int {
 	var bufBytes []int
 	for i := 64; i <= 32<<10; i *= 2 {
 		bufBytes = append(bufBytes, i)
 	}
+	return bufBytes
+}
+
+func BenchmarkBucketSort(b *testing.B) {
+	// I've experimented with some ways to speed this up.
+	//
+	// Computing counts as 4 uint64s split into 4 16 bit lanes using lots of
+	// shifts and branch-free code. This wound up being about 3x slower.
+	//
+	// Computing two histograms in parallel from even/odd elements is closer,
+	// but still slower than the obvious approach. To eliminate bounds and shift
+	// checks, I had to unsafe cast the []LAddr32 source to []uint64, precompute
+	// the larger shift, and check the two shift values before the loop.
+
 	// We can't really change the radix because it's a const, but we can do an
 	// okay job of simulating it in how we generate the input data.
 	var radixes []int
@@ -60,7 +117,7 @@ func BenchmarkBucketSort(b *testing.B) {
 		radixes = append(radixes, i)
 	}
 
-	b.Run("type=uint32", func(b *testing.B) {
+	b.Run("type=uint32/impl=base", func(b *testing.B) {
 		for _, radix := range radixes {
 			b.Run(fmt.Sprintf("radix=%d", radix), func(b *testing.B) {
 				for _, n := range bufBytes {
@@ -76,6 +133,28 @@ func BenchmarkBucketSort(b *testing.B) {
 						for range b.N {
 							var counts [radixBase]int
 							bucketSort(src, dst, &counts, 0)
+						}
+					})
+				}
+			})
+		}
+	})
+	b.Run("type=uint32/impl=avx", func(b *testing.B) {
+		for _, radix := range radixes {
+			b.Run(fmt.Sprintf("radix=%d", radix), func(b *testing.B) {
+				for _, n := range bufBytes {
+					// Generate data.
+					src := make([]LAddr32, n/4)
+					dst := make([]LAddr32, n/4)
+					rnd := rand.New(rand.NewPCG(0, 0))
+					for i := range src {
+						src[i] = LAddr32(rnd.Uint32N(uint32(radix)))
+					}
+					b.Run(fmt.Sprintf("bytes=%d", n), func(b *testing.B) {
+						b.SetBytes(int64(n))
+						for range b.N {
+							var counts [radixBase]uint16
+							bucketSort32AVX(src, dst, &counts, 0)
 						}
 					})
 				}
@@ -129,5 +208,70 @@ func BenchmarkSliceSort(b *testing.B) {
 				}
 			})
 		}
+	})
+}
+
+func BenchmarkCount32(b *testing.B) {
+	gen := func(b *testing.B, run func(b *testing.B, src []LAddr32)) {
+		for _, n := range bufBytes {
+			// Generate data.
+			src := make([]LAddr32, n/4)
+			rnd := rand.New(rand.NewPCG(0, 0))
+			for i := range src {
+				src[i] = LAddr32(rnd.Uint32())
+			}
+			b.Run(fmt.Sprintf("bytes=%d", n), func(b *testing.B) {
+				b.SetBytes(int64(n))
+
+				runtime.LockOSThread()
+				defer runtime.UnlockOSThread()
+				counters, err := perf.OpenCounter(perf.TargetThisGoroutine, events.EventCPUCycles, events.EventTaskClock)
+				if err != nil {
+					b.Logf("error opening perf counters: %s", err)
+				}
+				defer counters.Close()
+				b.ResetTimer()
+				counters.Start()
+				var start [2]perf.Count
+				if err := counters.ReadGroup(start[:]); err != nil {
+					b.Fatalf("error reading perf event: %s", err)
+				}
+
+				run(b, src)
+
+				b.StopTimer()
+				var end [2]perf.Count
+				if err := counters.ReadGroup(end[:]); err != nil {
+					b.Fatalf("error reading perf event: %s", err)
+				}
+
+				nPointers := len(src) * b.N
+				sc, _ := start[0].Value()
+				ec, _ := end[0].Value()
+				cycles := ec - sc
+				b.ReportMetric(float64(nPointers)/float64(cycles), "pointers/cpu-cycle")
+
+				stc, _ := start[1].Value()
+				etc, _ := end[1].Value()
+				ghz := float64(cycles) / ((time.Duration(etc-stc) * time.Nanosecond).Seconds() * 1000 * 1000 * 1000)
+				b.ReportMetric(ghz, "avg-GHz")
+			})
+		}
+	}
+	b.Run("impl=go", func(b *testing.B) {
+		gen(b, func(b *testing.B, src []LAddr32) {
+			var counts [16]uint16
+			for range b.N {
+				count32Go(src, 0, &counts)
+			}
+		})
+	})
+	b.Run("impl=avx2", func(b *testing.B) {
+		gen(b, func(b *testing.B, src []LAddr32) {
+			var counts [16]uint16
+			for range b.N {
+				count32AVX2(src, 0, &counts)
+			}
+		})
 	})
 }
