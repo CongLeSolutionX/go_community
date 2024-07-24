640e3c6d29a22fe4ae4d058ab32bbab9815ddc29
Keith Randall <khr@golang.org>
2024-07-23 17:47:27 -0700

    runtime/secret: implement new secret package
    Implement secret.Do.
    
    - When secret.Do returns:
      - Clear stack that is used by the argument function.
      - Clear all the registers that might contain secrets.
    - On stack growth in secret mode, clear the old stack.
    - When objects are allocated in secret mode, mark them and then zero
      the marked objects immediately when they are freed.
    - If the argument function panics, raise that panic as if it originated
      from secret.Do. This removes anything about the secret function
      from tracebacks.
    
    Fixes #21865
    
    Still some things to do to make this complete.
    1) Other architectures besides amd64/arm64
    2) stack low water mark (performance only)
    
    Change-Id: Id965a51772b7ab9d044352383ca67988f4b32dba

---
+++ src/runtime/secret/secret.go
@@ -0,0 +1,89 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package secret
+
+import (
+	"internal/cpu"
+	"unsafe"
+)
+
+func Do(f func()) {
+	// Place to store any panic value.
+	var p any
+
+	// Step 1: increment the nesting count.
+	inc()
+
+	// Step 2: call helper. The helper just calls f
+	// and captures (recovers) any panic result.
+	p = doHelper(f)
+
+	// Step 3: decrement the nesting count.
+	dec()
+
+	// Step 4: erase everything used by f (stack, registers).
+	eraseSecrets()
+
+	// Step 5: re-raise any caught panic.
+	// This will make the panic appear to come
+	// from a stack whose bottom frame is
+	// runtime/secret.Do.
+	// Anything below that to do with f will be gone.
+	//
+	// Note that the panic value is not erased. It behaves
+	// like any other value that escapes from f. If it is
+	// heap allocated, it will be erased when the garbage
+	// collector notices it is no longer referenced.
+	if p != nil {
+		panic(p)
+	}
+}
+
+func doHelper(f func()) (p any) {
+	// Step 2b: Pop the stack up to the secret.doHelper frame
+	// if we are in the process of panicking.
+	// (It is a no-op if we are not panicking.)
+	// We return any panicked value to secret.Do, who will
+	// re-panic it.
+	defer func() {
+		// Note: we rely on the go1.21+ behavior that
+		// if we are panicking, recover returns non-nil.
+		p = recover()
+	}()
+
+	// Step 2a: call the secret function.
+	f()
+
+	return
+}
+
+func Enabled() bool {
+	return count() > 0
+}
+
+// implemented in runtime
+func count() int64
+func inc()
+func dec()
+func eraseSecrets()
+func getStack() (uintptr, uintptr) // for testing
+
+// Assembly in this package (for testing only)
+
+// load data from p into test registers.
+//
+//go:noescape
+func loadRegisters(p unsafe.Pointer)
+
+// spill data from test registers into p.
+// Returns the amount of space filled in.
+//
+//go:noescape
+func spillRegisters(p unsafe.Pointer) uintptr
+
+const (
+	offsetX86HasAVX     = unsafe.Offsetof(cpu.X86.HasAVX)
+	offsetX86HasAVX512F = unsafe.Offsetof(cpu.X86.HasAVX512F)
+)
