02c0ec04704239de81338c953922e076f678b3a3
Aleksey Markin <alexanius@gmail.com>
2024-07-31 14:04:09 +0300

    cmd/compile: add ast nodes and basic block counters
    Current patch adds the counters to the AST and SSA nodes. The counters
    are loaded from the pprof file, no profile format changes needed.
    
    Current implementation uses counters for more precise likely/unlikely
    branch information.
    
    To use basic block counters you should add an option -pgobb:
    
    go build -a -pgobb -pgo=file.prof
    
    Fixes #65466
    
    Change-Id: Ibcc0f10af813a4c8eae5dd05698586417ffc4ee4

--- src/cmd/compile/internal/ir/sizeof_test.go
+++ src/cmd/compile/internal/ir/sizeof_test.go
@@ -20,8 +20,8 @@ func TestSizeof(t *testing.T) {
 		_32bit uintptr     // size on 32bit platforms
 		_64bit uintptr     // size on 64bit platforms
 	}{
-		{Func{}, 180, 304},
-		{Name{}, 96, 168},
+		{Func{}, 180, 312},
+		{Name{}, 104, 168},
 	}
 
 	for _, tt := range tests {
