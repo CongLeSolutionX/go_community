c006cdaa17faf27dbba1df87e1385c34597579ff
Michael Pratt <mpratt@google.com>
2024-08-14 11:53:53 -0400

    WIP: internal/runtime/maps: directory index optimization
    Might drop this one?
    
    For #54766.
    
    Change-Id: If981a58253a20dcddc1423aca26b7a2d28de6e90

--- src/internal/runtime/maps/map.go
+++ src/internal/runtime/maps/map.go
@@ -262,6 +262,9 @@ func (m *Map) Type() *abi.SwissMapType {
 }
 
 func (m *Map) directoryIndex(hash uintptr) uintptr {
+	if m.dirLen == 1 {
+		return 0
+	}
 	// TODO(prattmic): Store the shift as globalShift, as we need that more
 	// often than globalDepth.
 	if goarch.PtrSize == 4 {
@@ -271,6 +274,10 @@ func (m *Map) directoryIndex(hash uintptr) uintptr {
 }
 
 func (m *Map) directoryAt(i uintptr) *table {
+	if i == 0 {
+		// XXX: doesn't make a difference
+		return *(**table)(unsafe.Pointer(uintptr(m.dirPtr)))
+	}
 	return *(**table)(unsafe.Pointer(uintptr(m.dirPtr) + goarch.PtrSize*i))
 }
 
