9f108638e36ad468367eae66011ea5a6cdb1c120
Kir Kolyshkin <kolyshkin@gmail.com>
2024-08-26 23:04:40 -0700

    os: Getwd: fallback to slow method on ENAMETOOLONG
    As of CL 257637, all currently supported platforms have syscall.Getwd
    implemented, so the code which deduces wd by traversing up to root
    directory is never used (i.e. dead code) and thus can be removed.
    
    Or, as it was suggested by Ian Lance Taylor in CL 607436 review
    comments, it can be reused when syscall.Getwd returns ENAMETOOLONG
    (which usually happens than the current working dir is longer than
    syscall.PathMax).
    
    Let's do that. The only caveat is, such a long path returned from Getwd
    couldn't be used for any file-related operations (they will fail with
    ENAMETOOLONG).
    
    While at it:
     - make the stat(".") code conditional, slightly improving the
       performance on Unix when $PWD is not set;
     - reuse variables dir and err;
     - use openDirNolog instead of openFileNolog to obtain a dirfd;
     - ensure the errors returned are wrapped;
     - add test cases (which fail before this change).
    
    The implementation had to be split into two files because plan9 lacks
    syscall.ENOSYS. The alternative to that is stop relying on
    syscall.ImplementsGetwd (assuming it's always true).
    
    Change-Id: I60f7a70e6ebb1751699416f587688a1a97305fd7

---
+++ src/os/getwd_other_test.go
@@ -0,0 +1,69 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build !plan9
+
+package os_test
+
+import (
+	. "os"
+	"strings"
+	"testing"
+)
+
+func TestGetwdDeep(t *testing.T) {
+	testGetwdDeep(t, false)
+}
+
+func TestGetwdDeepWithPWDSet(t *testing.T) {
+	testGetwdDeep(t, true)
+}
+
+// testGetwdDeep checks that os.Getwd is able to return paths
+// longer than syscall.PathMax (with or without PWD set).
+func testGetwdDeep(t *testing.T, setPWD bool) {
+	dir := t.TempDir()
+	t.Chdir(dir)
+
+	if setPWD {
+		t.Setenv("PWD", dir)
+	} else {
+		// When testing os.Getwd, setting PWD to empty string
+		// is the same as unsetting it, but the latter would
+		// be more complicated since we don't have t.Unsetenv.
+		t.Setenv("PWD", "")
+	}
+
+	name := strings.Repeat("a", 200)
+	for {
+		if err := Mkdir(name, 0o700); err != nil {
+			t.Fatal(err)
+		}
+		if err := Chdir(name); err != nil {
+			t.Fatal(err)
+		}
+		if setPWD {
+			dir += "/" + name
+			if err := Setenv("PWD", dir); err != nil {
+				t.Fatal(err)
+			}
+			t.Logf(" $PWD len: %d", len(dir))
+		}
+
+		wd, err := Getwd()
+		t.Logf("Getwd len: %d %q", len(wd), wd)
+		if err != nil {
+			t.Fatal(err)
+		}
+		// Ideally the success criterion should be len(wd) > syscall.PathMax,
+		// but the latter is not public for some platforms, so use Stat(wd).
+		// When it fails with ENAMETOOLONG, it means:
+		//  - wd is longer than PathMax;
+		//  - Getwd used the slow fallback code.
+		if _, err := Stat(wd); err != nil {
+			t.Logf("success with len(wd)=%d, got error %v from stat", len(wd), err)
+			break
+		}
+	}
+}
