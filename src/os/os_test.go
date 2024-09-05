049ea2363476e309af43654111c3012a9b81363f
Kir Kolyshkin <kolyshkin@gmail.com>
2024-09-04 17:27:05 -0700

    os: internalize test cases for TestOpenError
    These are not used anywhere else and thus should not be in a global
    scope.
    
    Change-Id: I2ade5e077a78f55de09d73b9d4e4d9a4f7a3b473

--- src/os/os_test.go
+++ src/os/os_test.go
@@ -1777,34 +1777,32 @@ func TestSeekError(t *testing.T) {
 	}
 }
 
-type openErrorTest struct {
-	path  string
-	mode  int
-	error error
-}
-
-var openErrorTests = []openErrorTest{
-	{
-		sfdir + "/no-such-file",
-		O_RDONLY,
-		syscall.ENOENT,
-	},
-	{
-		sfdir,
-		O_WRONLY,
-		syscall.EISDIR,
-	},
-	{
-		sfdir + "/" + sfname + "/no-such-file",
-		O_WRONLY,
-		syscall.ENOTDIR,
-	},
-}
-
 func TestOpenError(t *testing.T) {
 	t.Parallel()
 
-	for _, tt := range openErrorTests {
+	testCases := []struct {
+		path  string
+		mode  int
+		error error
+	}{
+		{
+			sfdir + "/no-such-file",
+			O_RDONLY,
+			syscall.ENOENT,
+		},
+		{
+			sfdir,
+			O_WRONLY,
+			syscall.EISDIR,
+		},
+		{
+			sfdir + "/" + sfname + "/no-such-file",
+			O_WRONLY,
+			syscall.ENOTDIR,
+		},
+	}
+
+	for _, tt := range testCases {
 		f, err := OpenFile(tt.path, tt.mode, 0)
 		if err == nil {
 			t.Errorf("Open(%q, %d) succeeded", tt.path, tt.mode)
