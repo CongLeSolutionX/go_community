e068614f2b7393aaff76bb46671fe97dacf5f1a8
Michael Pratt <mpratt@google.com>
2024-09-19 16:06:40 -0400

    runtime,internal/runtime/maps: specialized swissmaps
    At this point, the Get and Put methods on Map/table are effectively
    dead. Should we remove them?
    
    For #54766.
    
    Change-Id: I95297750be6200f34ec483e4cfc897f048c26db7

--- src/runtime/map_fast32_swiss.go
+++ src/runtime/map_fast32_swiss.go
@@ -12,26 +12,15 @@ import (
 	"unsafe"
 )
 
-func mapaccess1_fast32(t *abi.SwissMapType, m *maps.Map, key uint32) unsafe.Pointer {
-	throw("mapaccess1_fast32 unimplemented")
-	panic("unreachable")
-}
+func mapaccess1_fast32(t *abi.SwissMapType, m *maps.Map, key uint32) unsafe.Pointer
 
-func mapaccess2_fast32(t *abi.SwissMapType, m *maps.Map, key uint32) (unsafe.Pointer, bool) {
-	throw("mapaccess2_fast32 unimplemented")
-	panic("unreachable")
-}
+func mapaccess2_fast32(t *abi.SwissMapType, m *maps.Map, key uint32) (unsafe.Pointer, bool)
 
-func mapassign_fast32(t *abi.SwissMapType, m *maps.Map, key uint32) unsafe.Pointer {
-	throw("mapassign_fast32 unimplemented")
-	panic("unreachable")
-}
+func mapassign_fast32(t *abi.SwissMapType, m *maps.Map, key uint32) unsafe.Pointer
 
 func mapassign_fast32ptr(t *abi.SwissMapType, m *maps.Map, key unsafe.Pointer) unsafe.Pointer {
 	throw("mapassign_fast32ptr unimplemented")
 	panic("unreachable")
 }
 
-func mapdelete_fast32(t *abi.SwissMapType, m *maps.Map, key uint32) {
-	throw("mapdelete_fast32 unimplemented")
-}
+func mapdelete_fast32(t *abi.SwissMapType, m *maps.Map, key uint32)
--- src/runtime/map_fast64_swiss.go
+++ src/runtime/map_fast64_swiss.go
@@ -12,26 +12,15 @@ import (
 	"unsafe"
 )
 
-func mapaccess1_fast64(t *abi.SwissMapType, m *maps.Map, key uint64) unsafe.Pointer {
-	throw("mapaccess1_fast64 unimplemented")
-	panic("unreachable")
-}
+func mapaccess1_fast64(t *abi.SwissMapType, m *maps.Map, key uint64) unsafe.Pointer
 
-func mapaccess2_fast64(t *abi.SwissMapType, m *maps.Map, key uint64) (unsafe.Pointer, bool) {
-	throw("mapaccess2_fast64 unimplemented")
-	panic("unreachable")
-}
+func mapaccess2_fast64(t *abi.SwissMapType, m *maps.Map, key uint64) (unsafe.Pointer, bool)
 
-func mapassign_fast64(t *abi.SwissMapType, m *maps.Map, key uint64) unsafe.Pointer {
-	throw("mapassign_fast64 unimplemented")
-	panic("unreachable")
-}
+func mapassign_fast64(t *abi.SwissMapType, m *maps.Map, key uint64) unsafe.Pointer
 
 func mapassign_fast64ptr(t *abi.SwissMapType, m *maps.Map, key unsafe.Pointer) unsafe.Pointer {
 	throw("mapassign_fast64ptr unimplemented")
 	panic("unreachable")
 }
 
-func mapdelete_fast64(t *abi.SwissMapType, m *maps.Map, key uint64) {
-	throw("mapdelete_fast64 unimplemented")
-}
+func mapdelete_fast64(t *abi.SwissMapType, m *maps.Map, key uint64)
