3e3cd7cb26c499f888a7ce8ff88617f72ae58625
Vasily Leonenko <vasiliy.leonenko@gmail.com>
2024-09-22 17:12:37 +0300

    cmd/internal/obj/arm64, cmd/link/internal/arm64: introduce GOARM64 ",func_align_32" suffix
    This patch adds an optional ",func_align_32" suffix to GOARM64 variable
    for aligning all functions to 32-bytes. It might be helpful to get more
    stable performance measurements, while it has certain overhead for code
    size.
    Also, this option can bring performance gains running produced binaries
    on ARM64 SoCs which has recommendations for 32-bytes alignments for
    branch targets.
    
    This option is effective only for ARM64 and disabled by default (arm64
    functions aligned to 16-bytes).
    
    Change-Id: I9e1739aaec61f4b63f9b6ed4c2fc75a6a966cb45

--- src/cmd/go/internal/help/helpdoc.go
+++ src/cmd/go/internal/help/helpdoc.go
@@ -611,9 +611,11 @@ Architecture-specific environment variables:
 		For GOARCH=arm64, the ARM64 architecture for which to compile.
 		Valid values are v8.0 (default), v8.{1-9}, v9.{0-5}.
 		The value can be followed by an option specifying extensions implemented by target hardware.
-		Valid options are ,lse and ,crypto.
+		Valid options are ,lse , ,crypto and ,func_align_32.
 		Note that some extensions are enabled by default starting from a certain GOARM64 version;
 		for example, lse is enabled by default starting from v8.1.
+		func_align_32 is an option to force aligning all functions to 32-bytes, it might be helpful to
+		get more stable performance measuremetns, while it has certain overhead for code size.
 	GO386
 		For GOARCH=386, how to implement floating point instructions.
 		Valid values are sse2 (default), softfloat.
