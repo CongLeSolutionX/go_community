66f1c753ac93c16ab7bf4b1b5712c13f11e685b1
Jason A. Donenfeld <Jason@zx2c4.com>
2024-09-22 03:45:29 +0200

    runtime: use vDSO for getrandom() on linux/amd64
    Linux 6.11 supports calling getrandom() from the vDSO. It operates on a
    thread-local opaque state allocated with mmap using flags specified by
    the vDSO.
    
    Opaque states are allocated in chunks, ideally ncpu at a time as a hint,
    rounding up to as many fit in a complete page. On first use, a state is
    assigned to an m, which owns that state, until the m exits, at which
    point it is given back to the pool.
    
    Performance appears to be quite good:
    
               │    sec/op    │   sec/op       vs base                 │
    Read/4-16    222.45n ± 3%   29.74n ± 5%    -86.63% (p=0.000 n=10)
               │     B/s      │      B/s       vs base                 │
    Read/4-16    17.15Mi ± 3%   128.43Mi ± 6%  +648.79% (p=0.000 n=10)
    
    TODO:
    
    - implement vgetrandom using ABIInternal, so avoid having to move all
      the arguments through the stack and back
    
    - wire up on powerpc32, powerpc64, loongarch64, s390x, and arm64
    
    Fixes #69577.
    
    Change-Id: Ib6f44e8f2f3940c94d970eaada0eb566ec297dc7

---
+++ src/runtime/vgetrandom_linux.go
@@ -0,0 +1,102 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build linux && amd64
+
+package runtime
+
+import "unsafe"
+
+var vgrnd struct {
+	states     []uintptr
+	statesLock mutex
+	stateSize  uintptr
+	mmapProt   uint32
+	mmapFlags  uint32
+}
+
+func vgrndInit() {
+	if vdsoGetrandomSym == 0 {
+		return
+	}
+
+	var params struct {
+		SizeOfOpaqueParams uint32
+		MmapProt           uint32
+		MmapFlags          uint32
+		reserved           [13]uint32
+	}
+	if vgetrandom(nil, 0, 0, uintptr(unsafe.Pointer(&params)), ^uint(0)) < 0 {
+		return
+	}
+	vgrnd.stateSize = uintptr(params.SizeOfOpaqueParams)
+	vgrnd.mmapProt = params.MmapProt
+	vgrnd.mmapFlags = params.MmapFlags
+
+	lockInit(&vgrnd.statesLock, lockRankLeafRank)
+}
+
+func vgrndGetState() uintptr {
+	lock(&vgrnd.statesLock)
+	if len(vgrnd.states) == 0 {
+		num := uintptr(ncpu)
+		allocSize := (num*vgrnd.stateSize + physPageSize - 1) &^ (physPageSize - 1)
+		num = (physPageSize / vgrnd.stateSize) * (allocSize / physPageSize)
+		p, err := mmap(nil, allocSize, int32(vgrnd.mmapProt), int32(vgrnd.mmapFlags), -1, 0)
+		if err != 0 {
+			unlock(&vgrnd.statesLock)
+			return 0
+		}
+		newBlock := uintptr(p)
+		if vgrnd.states == nil {
+			vgrnd.states = make([]uintptr, 0, num)
+		}
+		for i := uintptr(0); i < num; i++ {
+			if (newBlock&(physPageSize-1))+vgrnd.stateSize > physPageSize {
+				newBlock = (newBlock + physPageSize - 1) &^ (physPageSize - 1)
+			}
+			vgrnd.states = append(vgrnd.states, newBlock)
+			newBlock += vgrnd.stateSize
+		}
+	}
+	state := vgrnd.states[len(vgrnd.states)-1]
+	vgrnd.states = vgrnd.states[:len(vgrnd.states)-1]
+	unlock(&vgrnd.statesLock)
+	return state
+}
+
+func vgrndPutState(state uintptr) {
+	lock(&vgrnd.statesLock)
+	vgrnd.states = append(vgrnd.states, state)
+	unlock(&vgrnd.statesLock)
+}
+
+func vgetrandom(buf *byte, length uint, flags uint32, opaquestate uintptr, opaquestateSize uint) (ret int)
+
+//go:linkname getrandomVDSO
+func getrandomVDSO(p []byte, flags uint32) (ret int, supported bool) {
+	if vgrnd.stateSize == 0 {
+		return -1, false
+	}
+
+	mp := acquirem()
+	if mp.vdsoGetRandomState == 0 {
+		state := vgrndGetState()
+		if state == 0 {
+			releasem(mp)
+			return -1, false
+		}
+		mp.vdsoGetRandomState = state
+	}
+
+	supported = true
+
+	var buf *byte
+	if len(p) > 0 {
+		buf = &p[0]
+	}
+	ret = vgetrandom(buf, uint(len(p)), flags, mp.vdsoGetRandomState, uint(vgrnd.stateSize))
+	releasem(mp)
+	return
+}
