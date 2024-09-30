89ced405ffb7db8744e395eb16c932f7cd7b1bc9
xieyuschen <xieyuschen@gmail.com>
2024-09-30 13:53:09 +0800

    os: check permissions of CopyFS copied files
    CopyFS stipulates the permissions of the created files,
    we should test them in the unit test.
    
    * chmod x for testdata/x to test CopyFS for executable
    
    * check the files permissions to ensure CopyFS follows the stipulated
      convention
    
    Change-Id: Id13a8ad920ad0c1ff4b801dec3bfa6869cb3101f

--- src/os/dir.go
+++ src/os/dir.go
@@ -177,6 +177,7 @@ func CopyFS(dir string, fsys fs.FS) error {
 		if err != nil {
 			return err
 		}
+
 		w, err := OpenFile(newPath, O_CREATE|O_EXCL|O_WRONLY, 0666|info.Mode()&0777)
 		if err != nil {
 			return err
