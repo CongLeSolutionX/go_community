8c6b71f1407ad9b0d97fc736823718a224bc954f
Damien Neil <dneil@google.com>
2024-10-04 14:23:54 -0700

    internal/syscall/windows: add NtCreateFile
    Mostly copied from x/sys/windows.
    
    This adds a various related types and functions,
    but the purpose is to give access to NtCreateFile,
    which can be used as an equivalent to openat.
    
    For #67002
    
    Change-Id: I04e6f630445a55c2000c9c323ce8dcdc7fc0d0e0

---
+++ src/internal/syscall/windows/string_windows.go
@@ -0,0 +1,28 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package windows
+
+import "syscall"
+
+// NTUnicodeString is a UTF-16 string for NT native APIs, corresponding to UNICODE_STRING.
+type NTUnicodeString struct {
+	Length        uint16
+	MaximumLength uint16
+	Buffer        *uint16
+}
+
+// NewNTUnicodeString returns a new NTUnicodeString structure for use with native
+// NT APIs that work over the NTUnicodeString type. Note that most Windows APIs
+// do not use NTUnicodeString, and instead UTF16PtrFromString should be used for
+// the more common *uint16 string type.
+func NewNTUnicodeString(s string) (*NTUnicodeString, error) {
+	var u NTUnicodeString
+	s16, err := syscall.UTF16PtrFromString(s)
+	if err != nil {
+		return nil, err
+	}
+	RtlInitUnicodeString(&u, s16)
+	return &u, nil
+}
