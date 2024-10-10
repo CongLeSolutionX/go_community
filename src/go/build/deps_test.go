12d6adb879e4ef684ce63a1d062c8c11aadc03a0
Daniel McCarney <daniel@binaryparadox.net>
2024-10-10 13:10:53 -0400

    crypto/internal/fips: import x/crypto/pbkdf2
    For now the package contents are added internally pending a dedicated
    proposal for an exposed API.
    
    The code is copied verbatim for ease of review with a few small
    exceptions:
    
    * The hash.Hash interface usage was replaced with fips.Hash.
    * Import paths were replaced with corresponding internal FIPS package
      paths, and the hmac.New() arguments updated accordingly.
    * The pbkdf2_test was made external to pbkdf2 so it will be easier to
      move to the public package in the future.
    
    Updates #65269
    For #69536
    
    Change-Id: Ief8f42a88bad165010e7844186f306ea0db34c2e

--- src/go/build/deps_test.go
+++ src/go/build/deps_test.go
@@ -454,6 +454,7 @@ var depsRules = `
 	< crypto/internal/fips/sha512
 	< crypto/internal/fips/sha3
 	< crypto/internal/fips/hmac
+        < crypto/internal/fips/pbkdf2
 	< FIPS;
 
 	NONE < crypto/internal/boring/sig, crypto/internal/boring/syso;
