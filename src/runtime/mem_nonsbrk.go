b95f01fd36de7d657c2cf462b2edff57c1f216f8
Cherry Mui <cherryyz@google.com>
2024-10-21 17:45:01 -0400

    runtime: reserve fewer memory for aligned reservation on sbrk systems
    Sometimes the runtime needs to reserve some memory with a large
    alignment, which the OS usually won't directly satisfy. So, it
    asks size+align bytes instead, and frees the unaligned portions.
    On sbrk systems, this doesn't work that well, as freeing the tail
    portion doesn't really free the memory to the OS. Instead, we
    could simply round the current break up, then reserve the given
    size, without wasting the tail portion.
    
    For #69018.
    
    Change-Id: Iadc2c54d62b00ad7befa5bbf71146523483a8c47

---
+++ src/runtime/mem_nonsbrk.go
@@ -0,0 +1,13 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build !plan9 && !wasm
+
+package runtime
+
+import "unsafe"
+
+func sysReserveAlignedSbrk(v unsafe.Pointer, size, align uintptr) (unsafe.Pointer, uintptr) {
+	panic("unreachable")
+}
--- src/runtime/mem_sbrk.go
+++ src/runtime/mem_sbrk.go
@@ -47,6 +47,13 @@ func (p memHdrPtr) ptr() *memHdr   { return (*memHdr)(unsafe.Pointer(p)) }
 func (p *memHdrPtr) set(x *memHdr) { *p = memHdrPtr(unsafe.Pointer(x)) }
 
 func memAlloc(n uintptr) unsafe.Pointer {
+	if p := memAllocNoGrow(n); p != nil {
+		return p
+	}
+	return sbrk(n)
+}
+
+func memAllocNoGrow(n uintptr) unsafe.Pointer {
 	n = memRound(n)
 	var prevp *memHdr
 	for p := memFreelist.ptr(); p != nil; p = p.next.ptr() {
@@ -66,7 +73,7 @@ func memAlloc(n uintptr) unsafe.Pointer {
 		}
 		prevp = p
 	}
-	return sbrk(n)
+	return nil
 }
 
 func memFree(ap unsafe.Pointer, n uintptr) {
@@ -206,4 +213,38 @@ func sysReserveOS(v unsafe.Pointer, n uintptr) unsafe.Pointer {
 	}
 	unlock(&memlock)
 	return p
+}
+
+func sysReserveAlignedSbrk(v unsafe.Pointer, size, align uintptr) (unsafe.Pointer, uintptr) {
+	if v != nil && uintptr(v) != bloc {
+		return nil, 0
+	}
+	lock(&memlock)
+	if p := memAllocNoGrow(size + align); p != nil {
+		// We can satisfy the reservation from the free list.
+		// Trim off the unaligned parts.
+		pAligned := alignUp(uintptr(p), align)
+		if startLen := pAligned - uintptr(p); startLen > 0 {
+			memFree(p, startLen)
+		}
+		end := pAligned + size
+		if endLen := (uintptr(p) + size + align) - end; endLen > 0 {
+			memFree(unsafe.Pointer(end), endLen)
+		}
+		memCheck()
+		return unsafe.Pointer(pAligned), size
+	}
+
+	// Round up bloc to align, then allocate size.
+	p := alignUp(bloc, align)
+	r := sbrk(p + size - bloc)
+	if r == nil {
+		p, size = 0, 0
+	} else if l := p - uintptr(r); l > 0 {
+		// Free the area we skipped over for alignment.
+		memFree(r, l)
+		memCheck()
+	}
+	unlock(&memlock)
+	return unsafe.Pointer(p), size
 }
