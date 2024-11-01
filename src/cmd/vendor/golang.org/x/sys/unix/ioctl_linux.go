4f4b10281c4f1445ba5ba8654b8c2b38838c0a88
Dmitri Shuralyov <dmitshur@golang.org>
2024-11-01 11:57:48 -0400

    all: update golang.org/x/sys to pull in CL 614082 [generated]
    A part of the keeping Go's vendored dependencies and generated code
    up to date.
    
    For #36905.
    
    [git-generate]
    cd src
    go get golang.org/x/sys@v0.26.1-0.20241021134239-adbb8bbcaf61
    go mod tidy
    go mod vendor
    cd cmd
    go get golang.org/x/sys@v0.26.1-0.20241021134239-adbb8bbcaf61
    go mod tidy
    go mod vendor
    go generate syscall internal/syscall/...
    
    Change-Id: Ia84505f8934399f7c4518c6218892b81d30e5c17
    Cq-Include-Trybots: luci.golang.try:gotip-linux-amd64-longtest

--- src/cmd/vendor/golang.org/x/sys/unix/ioctl_linux.go
+++ src/cmd/vendor/golang.org/x/sys/unix/ioctl_linux.go
@@ -58,6 +58,47 @@ func IoctlGetEthtoolDrvinfo(fd int, ifname string) (*EthtoolDrvinfo, error) {
 	return &value, err
 }
 
+// IoctlGetEthtoolTsInfo fetches ethtool timestamping and PHC
+// association for the network device specified by ifname.
+func IoctlGetEthtoolTsInfo(fd int, ifname string) (*EthtoolTsInfo, error) {
+	ifr, err := NewIfreq(ifname)
+	if err != nil {
+		return nil, err
+	}
+
+	value := EthtoolTsInfo{Cmd: ETHTOOL_GET_TS_INFO}
+	ifrd := ifr.withData(unsafe.Pointer(&value))
+
+	err = ioctlIfreqData(fd, SIOCETHTOOL, &ifrd)
+	return &value, err
+}
+
+// IoctlGetHwTstamp retrieves the hardware timestamping configuration
+// for the network device specified by ifname.
+func IoctlGetHwTstamp(fd int, ifname string) (*HwTstampConfig, error) {
+	ifr, err := NewIfreq(ifname)
+	if err != nil {
+		return nil, err
+	}
+
+	value := HwTstampConfig{}
+	ifrd := ifr.withData(unsafe.Pointer(&value))
+
+	err = ioctlIfreqData(fd, SIOCGHWTSTAMP, &ifrd)
+	return &value, err
+}
+
+// IoctlSetHwTstamp updates the hardware timestamping configuration for
+// the network device specified by ifname.
+func IoctlSetHwTstamp(fd int, ifname string, cfg *HwTstampConfig) error {
+	ifr, err := NewIfreq(ifname)
+	if err != nil {
+		return err
+	}
+	ifrd := ifr.withData(unsafe.Pointer(cfg))
+	return ioctlIfreqData(fd, SIOCSHWTSTAMP, &ifrd)
+}
+
 // IoctlGetWatchdogInfo fetches information about a watchdog device from the
 // Linux watchdog API. For more information, see:
 // https://www.kernel.org/doc/html/latest/watchdog/watchdog-api.html.
