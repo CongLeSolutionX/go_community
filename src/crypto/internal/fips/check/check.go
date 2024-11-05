c996a02301411d19d720b92ad9e1f2f17ea88c44
Russ Cox <rsc@golang.org>
2024-11-05 13:52:13 -0500

    crypto/internal/fips/check: add new package
    This package is in charge of the FIPS init-time code+data verification.
    
    If GODEBUG=fips140=off or the empty string, then no verification
    happens. Otherwise, the setting must be "on", "debug", or "force",
    all of which enable verification. If the setting is "debug", successful
    verification prints a message to that effect. Otherwise successful
    verification is quiet.
    
    The linker leaves special information for this package to use.
    See cmd/internal/obj/fips.go and cmd/link/internal/ld/fips.go,
    both submitted in earlier CLs, for details.
    
    For #69536.
    
    Change-Id: Ie1fe29f316db290e0bd7df0a5a09108be4779d63

---
+++ src/crypto/internal/fips/check/check.go
@@ -0,0 +1,123 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package check implements the FIPS-140 load-time code+data verification.
+// Every FIPS package providing cryptographic functionality except hmac and sha256
+// must import crypto/internal/fips/check, so that the verification happens
+// before initialization of package global variables.
+// The hmac and sha256 packages are used by this package, so they cannot import it.
+// Instead, those packages must be careful not to change global variables during init.
+// (If necessary, we could have check call a PostCheck function in those packages
+// after the check has completed.)
+package check
+
+import (
+	"crypto/internal/fips/hmac"
+	"crypto/internal/fips/sha256"
+	"internal/godebug"
+	"io"
+	"unsafe"
+)
+
+// Enabled reports whether verification was enabled.
+// If Enabled returns true, then verification succeeded,
+// because if it failed the binary would have panicked at init time.
+func Enabled() bool {
+	return enabled
+}
+
+var enabled bool  // set when verification is enabled
+var verified bool // set when verification succeeds, for testing
+
+// linkinfo holds the go:fipsinfo symbol prepared by the linker.
+// See cmd/link/internal/ld/fips.go for details.
+//
+//go:linkname linkinfo go:fipsinfo
+var linkinfo struct {
+	Magic [16]byte
+	Sum   [32]byte
+	Self  uintptr
+	Sects [4]struct {
+		// Note: These must be unsafe.Pointer, not uintptr,
+		// or else checkptr panics about turning uintptrs
+		// into pointers into the data segment during
+		// go test -race.
+		Start unsafe.Pointer
+		End   unsafe.Pointer
+	}
+}
+
+// "\xff"+fipsMagic is the expected linkinfo.Magic.
+// We avoid writing that explicitly so that the string does not appear
+// elsewhere in normal binaries, just as a precaution.
+const fipsMagic = " Go fipsinfo \xff\x00"
+
+// put writes x as a big-endian uint64 to b.
+// The FIPS object file format uses numbers in this form.
+func put(b *[8]byte, x uint64) {
+	b[0] = byte(x >> 56)
+	b[1] = byte(x >> 48)
+	b[2] = byte(x >> 40)
+	b[3] = byte(x >> 32)
+	b[4] = byte(x >> 24)
+	b[5] = byte(x >> 16)
+	b[6] = byte(x >> 8)
+	b[7] = byte(x)
+}
+
+var zeroSum [32]byte
+
+func init() {
+	v := godebug.New("#fips140").Value()
+	enabled = v != "" && v != "off"
+	if !enabled {
+		return
+	}
+
+	switch v {
+	case "on", "debug", "force":
+		// ok
+	default:
+		panic("fips140: unknown GODEBUG setting fips140=" + v)
+	}
+
+	if linkinfo.Magic[0] != 0xff || string(linkinfo.Magic[1:]) != fipsMagic || linkinfo.Sum == zeroSum {
+		panic("fips140: no verification checksum found")
+	}
+
+	h := hmac.New(sha256.New, make([]byte, 32))
+	w := io.Writer(h)
+
+	/*
+		// Uncomment for debugging.
+		// Commented (as opposed to a const bool flag)
+		// to avoid import "os" in default builds.
+		f, err := os.Create("fipscheck.o")
+		if err != nil {
+			panic(err)
+		}
+		w = io.MultiWriter(h, f)
+	*/
+
+	w.Write([]byte("go fips object v1\n"))
+
+	var tmp [8]byte
+	for _, sect := range linkinfo.Sects {
+		n := uintptr(sect.End) - uintptr(sect.Start)
+		put(&tmp, uint64(n))
+		w.Write(tmp[:])
+		w.Write(unsafe.Slice((*byte)(sect.Start), n))
+	}
+	sum := h.Sum(nil)
+
+	if *(*[32]byte)(sum) != linkinfo.Sum {
+		panic("fips140: verification mismatch")
+	}
+
+	if v == "debug" {
+		println("fips140: verified code+data")
+	}
+
+	verified = true
+}
