golang/go

commit c58243aa8a510a0f467715da71a3053a04365038

runtime: support non-cooperative preemption on windows/arm
This adds support for injecting asynchronous preemption calls on
windows/arm. This code follows sigctxt.pushCall for POSIX OSes
on arm, except we subtract 1 from IP, just as in CL 273727.

Updates #10958.
Updates #24543.
Updates #49759.

Change-Id: Id0c2aed28662f50631b8c8cede3b4e6f088dafea
Reviewed-on: https://go-review.googlesource.com/c/go/+/366734
Trust: Jason A. Donenfeld <Jason@zx2c4.com>
Reviewed-by: Austin Clements <austin@google.com>
Reviewed-by: Patrik Nyblom <pnyb@google.com>
Reviewed-by: Cherry Mui <cherryyz@google.com>

src/runtime/os_windows.go
@@ -1306,7 +1306,7 @@ func setThreadCPUProfiler(hz int32) {
 	atomic.Store((*uint32)(unsafe.Pointer(&getg().m.profilehz)), uint32(hz))
 }
 
-const preemptMSupported = GOARCH == "386" || GOARCH == "amd64"
+const preemptMSupported = GOARCH != "arm64"
 
 // suspendLock protects simultaneous SuspendThread operations from
 // suspending each other.
@@ -1399,8 +1399,20 @@ func preemptM(mp *m) {
 				*(*uintptr)(unsafe.Pointer(sp)) = newpc
 				c.set_sp(sp)
 				c.set_ip(targetPC)
-			}
 
+			case "arm":
+				// Push LR. The injected call is responsible
+				// for restoring LR. gentraceback is aware of
+				// this extra slot. See sigctxt.pushCall in
+				// signal_arm.go, which is similar except we
+				// subtract 1 from IP here.
+				sp := c.sp()
+				sp -= goarch.PtrSize
+				c.set_sp(sp)
+				*(*uint32)(unsafe.Pointer(sp)) = uint32(c.lr())
+				c.set_lr(newpc - 1)
+				c.set_ip(targetPC)
+			}
 			stdcall2(_SetThreadContext, thread, uintptr(unsafe.Pointer(c)))
 		}
 	}

CongLeSolutionX/go_community

commit d898a0e356c6bb8cc8cb70837943595ffd0ffd1f

Fix issue with preemption on Windows ARM32
fix break in arm32 windows build introduced in https://github.com/golang/go/commit/c58243aa8a510a0f467715da71a3053a04365038

Change-Id: I648ae591462deafaa49dfe15ac700c82adb78088
GitHub-Last-Rev: b7b1296cd5745069e37587882877b24d727717fd
GitHub-Pull-Request: golang/go#70234

src/runtime/os_windows.go
@@ -1320,13 +1320,12 @@ func preemptM(mp *m) {
 				// Push LR. The injected call is responsible
 				// for restoring LR. gentraceback is aware of
 				// this extra slot. See sigctxt.pushCall in
-				// signal_arm.go, which is similar except we
-				// subtract 1 from IP here.
+				// signal_arm.go.
 				sp := c.sp()
 				sp -= goarch.PtrSize
 				c.set_sp(sp)
 				*(*uint32)(unsafe.Pointer(sp)) = uint32(c.lr())
-				c.set_lr(newpc - 1)
+				c.set_lr(newpc)
 				c.set_ip(targetPC)
 
 			case "arm64":