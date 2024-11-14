21d0b18e5464d010401ea4fc85a4347ba4aabcf5
Daniel McCarney <daniel@binaryparadox.net>
2024-11-14 14:09:13 -0500

    crypto/internal/fips/pbkdf2: fips import pbkdf2
    This commit lifts the internals of crypto/pbkdf2 into
    crypto/internal/fips140/pbkdf2, in the FIPS module. The code
    remains unchanged except for the following adjustments:
    
    * The hash and hmac imports now come from the FIPS equivalents.
    * The FIPS service indicator status is set based on the SP 800-132
      requirements for PBKDF2.
    
    For #69536
    
    Change-Id: I61f47a652cef10505a5b40a70be5240b161a97ba

--- src/crypto/internal/fips140/hkdf/hkdf.go
+++ src/crypto/internal/fips140/hkdf/hkdf.go
@@ -17,7 +17,7 @@ func Extract[H fips140.Hash](h func() H, secret, salt []byte) []byte {
 		salt = make([]byte, h().Size())
 	}
 	extractor := hmac.New(h, salt)
-	hmac.MarkAsUsedInHKDF(extractor)
+	hmac.MarkAsUsedInKDF(extractor)
 	extractor.Write(secret)
 	return extractor.Sum(nil)
 }
@@ -25,7 +25,7 @@ func Extract[H fips140.Hash](h func() H, secret, salt []byte) []byte {
 func Expand[H fips140.Hash](h func() H, pseudorandomKey, info []byte, keyLen int) []byte {
 	out := make([]byte, 0, keyLen)
 	expander := hmac.New(h, pseudorandomKey)
-	hmac.MarkAsUsedInHKDF(expander)
+	hmac.MarkAsUsedInKDF(expander)
 	var counter uint8
 	var buf []byte
 
