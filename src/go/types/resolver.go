898ed6621f01add550e48ecbda67352602e1bac4
Robert Griesemer <gri@golang.org>
2024-11-18 12:45:17 -0800

    go/types, types2: disallow new methods on generic alias and instantiated types
    If the receiver is an alias declaring type parameters, report
    an error and ensure that the receiver type remains invalid.
    Collect type parameters etc. as before but do not attempt to
    find their constraints or instantiate the receiver type.
    The constraints of the type parameters will be invalid by
    default. The receiver type will not be (lazily) instantiated
    which causes problems with existing invariants.
    
    If a receiver denotes an instantiated (alias or defined) type,
    report an error and ensure that the receiver type remains invalid.
    
    While at it, add more comments and bring go/types and types2
    closer together where there were differences.
    
    Fixes #70417.
    
    Change-Id: I87ef0b42d2f70464664cacc410f4b7eb1c994241

--- src/go/types/resolver.go
+++ src/go/types/resolver.go
@@ -583,15 +583,13 @@ func (check *Checker) unpackRecv(rtyp ast.Expr, unpackParams bool) (ptr bool, ba
 // name exists, the returned base is nil.
 func (check *Checker) resolveBaseTypeName(seenPtr bool, typ ast.Expr, lookupScope func(*ast.Ident) *Scope) (ptr bool, base *TypeName) {
 	// Algorithm: Starting from a type expression, which may be a name,
-	// we follow that type through alias declarations until we reach a
-	// non-alias type name. If we encounter anything but pointer types or
-	// parentheses we're done. If we encounter more than one pointer type
-	// we're done.
+	// we follow that type through non-generic alias declarations until
+	// we reach a non-alias type name. A single pointer indirection and
+	// references to cgo types are permitted.
 	ptr = seenPtr
 	var seen map[*TypeName]bool
 	for {
-		// Note: this differs from types2, but is necessary. The syntax parser
-		// strips unnecessary parens.
+		// The go/parser keeps parentheses; strip them.
 		typ = ast.Unparen(typ)
 
 		// check if we have a pointer type
@@ -631,6 +629,11 @@ func (check *Checker) resolveBaseTypeName(seenPtr bool, typ ast.Expr, lookupScop
 			if name == "" {
 				return false, nil
 			}
+		// An instantiated type may appear on the RHS of an alias declaration.
+		// Defining new methods with receivers that are generic aliases (or
+		// which refer to generic aliases) is not permitted, so we're done.
+		// Treat like the default cases.
+		// case *ast.IndexExpr, *ast.IndexListExpr:
 		default:
 			return false, nil
 		}
@@ -658,6 +661,11 @@ func (check *Checker) resolveBaseTypeName(seenPtr bool, typ ast.Expr, lookupScop
 		tdecl := check.objMap[tname].tdecl // must exist for objects in package scope
 		if !tdecl.Assign.IsValid() {
 			return ptr, tname
+		}
+
+		// we're done if tdecl defined a generic alias
+		if tdecl.TypeParams != nil {
+			return false, nil
 		}
 
 		// otherwise, continue resolving
